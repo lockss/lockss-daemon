9ab7826d079073a62e71fcc202edacaafb0d3a79
troberts <claire@lockss.org>
2002-09-05 17:36:37 +0000

    org.lockss.test.PrivilegedAccessor org.lockss.test.TestPrivilegedAccessor *class that allows one to test private methods (and it's test cases)
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@65 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/test/TestPrivilegedAccessor.java
@@ -0,0 +1,128 @@
+package org.lockss.test;
+import junit.framework.*;
+
+public class TestPrivilegedAccessor extends TestCase {
+
+    public void testParent() throws Exception {
+        MockParent parent = new MockParent("Charlie");
+        assertEquals(PrivilegedAccessor.getValue(parent, "m_name"), "Charlie");
+
+        PrivilegedAccessor.invokeMethod(parent, "setName", "Timmah!");
+
+        assertEquals(PrivilegedAccessor.getValue(parent,"m_name"), "Timmah!");
+    }
+
+    public void testChild() throws Exception {
+        MockChild child = new MockChild("Charlie",8);
+        assertEquals(PrivilegedAccessor.getValue(child, "m_name"), "Charlie");
+        assertEquals(PrivilegedAccessor.getValue(child, "m_number"), new Integer(8));
+
+        PrivilegedAccessor.invokeMethod(child, "setName", "Timmah!");
+        PrivilegedAccessor.invokeMethod(child, "setNumber", new Integer(3));
+
+        assertEquals(PrivilegedAccessor.getValue(child,"m_name"), "Timmah!");
+        assertEquals(PrivilegedAccessor.getValue(child, "m_number"), new Integer(3));
+    }
+
+    public void testChildWithParentReference() throws Exception {
+        MockParent parent = new MockChild("Charlie",8);
+        assertEquals(PrivilegedAccessor.getValue(parent, "m_name"), "Charlie");
+        assertEquals(PrivilegedAccessor.getValue(parent, "m_number"), new Integer(8));
+
+        Object args[] = {"Timmah!", new Integer(3)};
+        PrivilegedAccessor.invokeMethod(parent, "setData", args);
+
+        assertEquals(PrivilegedAccessor.getValue(parent,"m_name"), "Timmah!");
+        assertEquals(PrivilegedAccessor.getValue(parent, "m_number"), new Integer(3));
+
+        PrivilegedAccessor.invokeMethod(parent, "setName", "prashant");
+        assertEquals(PrivilegedAccessor.getValue(parent,"m_name"), "prashant");
+    }
+
+    public void testInvalidField() throws Exception {
+        MockParent parent = new MockParent("Charlie");
+        try {
+            Object value = PrivilegedAccessor.getValue(parent, "zzz");
+        }
+        catch(NoSuchFieldException e) {
+            return;
+        }
+        fail("Should throw NoSuchFieldException");
+    }
+
+    public void testInvalidMethodName() throws Exception {
+        MockChild child = new MockChild("Charlie",8);
+        try {
+            PrivilegedAccessor.invokeMethod(child, "zzz", "Timmah!");
+        }
+        catch(NoSuchMethodException e) {
+            return;
+        }
+        fail("Should throw NoSuchMethodException");
+    }
+
+    public void testInvalidArguments() throws Exception {
+        MockChild child = new MockChild("Charlie",8);
+        try {
+            PrivilegedAccessor.invokeMethod(child, "setData", "Timmah!");
+        }
+        catch(NoSuchMethodException e) {
+            return;
+        }
+        fail("Should throw NoSuchMethodException");
+    }
+
+
+    public TestPrivilegedAccessor( String name ) {
+        super( name );
+    }
+
+    public static void main(String args[]) {
+        junit.textui.TestRunner.run(TestPrivilegedAccessor.suite());
+    }
+
+    public static Test suite() {
+        return new TestSuite(TestPrivilegedAccessor.class);
+    }
+
+    // Test utility classes
+
+    public class MockChild extends MockParent {
+        private int m_number;
+
+        public MockChild( String name, int number ) {
+            super( name );
+            m_number = number;
+        }
+
+        public int getNumber() {
+            return m_number;
+        }
+
+        private void setNumber( Integer number ) {
+            m_number = number.intValue();
+        }
+        private void setData( String name, Integer number ) {
+            setName(name);
+            m_number = number.intValue();
+        }
+    }
+
+    public class MockParent {
+        private String m_name;
+
+        public MockParent() {
+        }
+        public MockParent(String name) {
+            m_name = name;
+        }
+
+        public String getName() {
+            return m_name;
+        }
+
+        protected void setName( String newName ) {
+            m_name = newName;
+        }
+    }
+}
