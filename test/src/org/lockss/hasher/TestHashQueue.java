a42acb8db85f702239b0a583e0f66eda3020231a
tlipkis <tal@pss.com>
2002-10-01 06:16:53 +0000

    Still needs final test completed.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@109 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/hasher/TestHashQueue.java
+++ test/src/org/lockss/hasher/TestHashQueue.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestHashQueue.java,v 1.2 2002-09-19 22:18:56 tal Exp $
+ * $Id: TestHashQueue.java,v 1.3 2002-10-01 06:16:53 tal Exp $
  */
 
 /*
@@ -50,7 +50,8 @@ public class TestHashQueue extends LockssTestCase {
     org.lockss.hasher.HashQueue.class
   };
 
-  static CachedUrlSet cus = HashServiceTestPlugin.getCUS();
+  static Logger log = Logger.getLogger("HashQueue");
+  static HashServiceTestPlugin.CUS cus = HashServiceTestPlugin.getCUS();
   static final String hashAlgorithm = "SHA-1";
   static MessageDigest dig;
 
@@ -58,26 +59,60 @@ public class TestHashQueue extends LockssTestCase {
     super(msg);
   }
 
-  public void setUp() {
+  public void setUp() throws Exception {
+    log.setLevel(Logger.LEVEL_DEBUG);
     if (dig == null) {
-      try {
-	dig = MessageDigest.getInstance(hashAlgorithm);
-      } catch (java.security.NoSuchAlgorithmException e) {
-	fail("Couldn't get MessageDigest: " + e);
-      }
+      dig = MessageDigest.getInstance(hashAlgorithm);
     }
   }
+
+//  	HashQueue.Request(CachedUrlSet urlset,
+//  			  MessageDigest hasher,
+//  			  Deadline deadline,
+//  			  HashService.Callback callback,
+//  			  Object cookie,
+//  			  CachedUrlSetHasher urlsetHasher,
+//  			  long estimatedDuration);
 
   static HashQueue.Request simpleReq(long deadline, int duration) {
     return new HashQueue.Request(cus, dig, new Deadline(deadline),
 				 null, null, null, duration);
   }
+
+  static HashQueue.Request req(long deadline,
+			       int duration,
+			       int bytes,
+			       HashService.Callback callback) {
+    cus.setHashDuration(duration, bytes);
+    HashQueue.Request req =
+      new HashQueue.Request(cus, dig, new Deadline(deadline),
+			    callback, null,
+			    cus.getContentHasher(dig),
+			    duration);
+    req.cookie = req;
+    return req;
+  }
+
+  static HashQueue.Request req(String cookie,
+			       long deadline,
+			       int duration,
+			       int bytes,
+			       HashService.Callback callback) {
+    cus.setHashDuration(duration, bytes);
+    HashQueue.Request req =
+      new HashQueue.Request(cus, dig, new Deadline(deadline),
+			    callback, cookie,
+			    cus.getContentHasher(dig),
+			    duration);
+    return req;
+  }
+
   public void testReq() {
     HashQueue q = new HashQueue();
     HashQueue.Request req1 = simpleReq(1000, 100);
     HashQueue.Request req2 = simpleReq(2000, 100);
-    assertTrue(q.runBefore(req1, req2));
-    assertTrue(!q.runBefore(req2, req1));
+    assertTrue(req1.runBefore(req2));
+    assertTrue(!req2.runBefore(req1));
   }
 
   // test request acceptance
@@ -116,26 +151,63 @@ public class TestHashQueue extends LockssTestCase {
   // test completion & callback
   public void testDone() throws Exception {
     HashQueue q = new HashQueue();
+    final List doneList = new LinkedList();
+    HashService.Callback cb = new HashService.Callback() {
+	public void hashingFinished(CachedUrlSet urlset,
+				    Object cookie,
+				    MessageDigest hasher,
+				    Exception e) {
+	  doneList.add(cookie);
+	}
+      };
     HashQueue.Request r1, r2, r3 ,r4, r5;
-    r1 = simpleReq(2000, 0);
-    r2 = simpleReq(3000, 0);
-    r3 = simpleReq(5000, 0);
-    r4 = simpleReq(2500, 0);
-    r5 = simpleReq(200, 0);
-    Object ord[] = {r5, r1, r4, r2, r3};
+    r1 = req(2000, 0, 100, cb);
+    r2 = req(10000, 0, 200, cb);
+    r3 = req(20000, 0, 0, cb);
+    assertTrue(q.insert(r1));
+    assertTrue(q.insert(r2));
+    assertEquals(0, doneList.size());
+    q.removeCompleted();
+    assertEquals(0, doneList.size());
+    r1.deadline = new Deadline(-1);
+    q.removeCompleted();
+    Object exp[] = {r1};
+    assertIsomorphic(exp, doneList);
+    r2.deadline = new Deadline(-1);
+    assertTrue(q.insert(r3));
+    q.removeCompleted();
+    Object exp2[] = {r1, r2, r3};
+    assertIsomorphic(exp2, doneList);
+  }
+
+  // test stepper
+  /* needs to be finished
+  public void testStep() throws Exception {
+    HashQueue q = new HashQueue();
+    final List doneList = new LinkedList();
+    HashService.Callback cb = new HashService.Callback() {
+	public void hashingFinished(CachedUrlSet urlset,
+				    Object cookie,
+				    MessageDigest hasher,
+				    Exception e) {
+	  doneList.add(cookie);
+	  doneList.add(e);
+	}
+      };
+    HashQueue.Request r1, r2, r3 ,r4, r5;
+    r1 = req("1", 20000, 10000, 100, cb);
+    r2 = req("2", 100000, 20000, 200, cb);
+    r3 = req("3", 200000, 30000, 0, cb);
     assertTrue(q.insert(r1));
     assertTrue(q.insert(r2));
     assertTrue(q.insert(r3));
-    assertTrue(q.insert(r4));
-    assertTrue(q.insert(r5));
-    assertIsomorphic(ord, (Collection)PrivilegedAccessor.getValue(q, "qlist"));
+    q.runAndNotify(3, 75, Boolean.TRUE);
+    q.runAndNotify(3, 75, Boolean.TRUE);
+    q.runAndNotify(3, 75, Boolean.TRUE);
+    q.runAndNotify(3, 75, Boolean.TRUE);
+    q.runAndNotify(3, 75, Boolean.TRUE);
+    q.runAndNotify(1, 75, Boolean.TRUE);
+    q.runAndNotify(1, 75, Boolean.TRUE);
   }
-//  	HashQueue.Request(CachedUrlSet urlset,
-//  			  MessageDigest hasher,
-//  			  Deadline deadline,
-//  			  HashService.Callback callback,
-//  			  Object cookie,
-//  			  CachedUrlSetHasher urlsetHasher,
-//  			  long estimatedDuration);
-
+  */
 }
