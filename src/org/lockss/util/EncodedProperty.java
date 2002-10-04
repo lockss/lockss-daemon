e1ff85e526f6604c47b2fab40c3b61170059029a
clairegriffin <clairetg@stanford.edu>
2002-10-04 17:36:00 +0000

    fix getBoolean Boolean.getBoolean(value) always returns false
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@114 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/util/EncodedProperty.java
+++ src/org/lockss/util/EncodedProperty.java
@@ -1,4 +1,34 @@
-package org.lockss.util;
+/*
+ * $Id: EncodedProperty.java,v 1.2 2002-10-04 17:36:00 claire Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/package org.lockss.util;
 
 import java.util.Properties;
 import java.util.Set;
@@ -21,8 +51,7 @@ public class EncodedProperty extends Properties {
 
 
   /**
-   * Constructs a new, empty property map with the specified initial capacity
-   * and default load factor, which is <tt>0.75</tt>.
+   * Constructs a new, empty property map.
    *
    * @param   defaults the default property map.
     */
@@ -118,10 +147,12 @@ public class EncodedProperty extends Properties {
 
   public boolean getBoolean(String key, boolean def) {
     boolean ret = def;
+
     String value = getProperty(key);
+
     if(value != null) {
-      ret = Boolean.getBoolean(value);
-    }
+      ret = new Boolean(value).booleanValue();
+     }
     return ret;
   }
 
@@ -137,6 +168,7 @@ public class EncodedProperty extends Properties {
 
   public double getDouble(String key, double def) {
     double ret = def;
+
     try {
       String value = getProperty(key);
       if(value != null) {
