5ee469a39ea7b4bd57f17df6eb6947a8fd4614a1
eaalto <claire@lockss.org>
2002-10-23 23:50:08 +0000

    A simple util class which copies an InputStream to an OutputStream.  It closes neither.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@148 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/util/StreamUtil.java
@@ -0,0 +1,57 @@
+/*
+ * $Id: StreamUtil.java,v 1.1 2002-10-23 23:50:08 aalto Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+
+import java.io.*;
+
+/**
+ * This is a class to contain generic stream utilities
+ *
+ * @author  Emil Aalto
+ * @version 0.0
+ */
+
+public class StreamUtil {
+
+  /**
+   * This function copies the contents of in InputStream to an Outputstream.
+   * It closes neither.
+   */
+   public static void copy(InputStream is, OutputStream os) throws IOException {
+     byte[] bytes = new byte[256];
+     int byteCount;
+     while ((byteCount = is.read(bytes))>0) os.write(bytes, 0, byteCount);
+     os.flush();
+   }
+}
+
