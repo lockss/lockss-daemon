e1eb51d1bebe91f97caa69a1790838c4c494778d
troberts <claire@lockss.org>
2002-10-31 01:45:08 +0000

    Mock implementation of java.security.MessageDigest
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@173 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/test/MockMessageDigest.java
@@ -0,0 +1,136 @@
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.test;
+import java.security.*;
+import java.util.*;
+
+/**
+ * This is a mock version of <code>java.security.MessageDigest</code> used for testing
+ *
+ * @author  Thomas S. Robertson
+ * @version 0.0
+ */
+
+public class MockMessageDigest extends MessageDigest {
+
+  Vector inputList;
+
+
+  public MockMessageDigest(){
+    super("Mock hash algorithm");
+    inputList = new Vector();
+  }
+
+  /**
+   * Mock implementation of the standard Message digest function (stores bytes on a queue for
+   * later checking via <code>getUpdatedByte<code> or <code>getUpdatedBytes</code>)
+   * @param input byte to hash
+   * @see getUpdatedByte()
+   * @see getUpdatedBytes(byte[])
+   */
+  public void update(byte input){
+    inputList.add(new Byte(input));
+  }
+
+  /**
+   * Mock implementation of the standard Message digest function (stores bytes on a queue for
+   * later checking via <code>getUpdatedByte<code> or <code>getUpdatedBytes</code>)
+   * @param input byte array to hash
+   * @see getUpdatedByte()
+   * @see getUpdatedBytes(byte[])
+   */
+  public void update(byte[] input){
+    for (int i=0; i<input.length; i++){
+      inputList.add(new Byte(input[i]));
+    }
+  }
+
+  /**
+   * Mock implementation of the standard Message digest function (stores bytes on a queue for
+   * later checking via <code>getUpdatedByte<code> or <code>getUpdatedBytes</code>)
+   * @param input byte array to hash
+   * @param offset index of first element in array to start hashing
+   * @param len number of elements in array to hash
+   * @see getUpdatedByte()
+   * @see getUpdatedBytes(byte[])
+   */
+  public void update(byte[] input, int offset, int len){
+    for (int i=offset; i<offset+len; i++){
+      inputList.add(new Byte(input[i]));
+    }
+  }
+
+
+
+  /**
+   * Method to retrieve bytes fed in by <code>update</code>
+   * @return next byte in the queue or -1 if none are left
+   * @see update(byte)
+   * @see update(byte[])
+   */
+  public byte getUpdatedByte(){
+    if (inputList.size() <= 0){
+      return -1;
+    }
+    Byte ret = (Byte)inputList.remove(0);
+    return ret.byteValue();
+  }
+
+  /**
+   * Method to retrieve bytes fed in by <code>update</code>
+   * @param output byte[] to fill with bytes from the queue
+   * @return number of bytes written to output
+   * @see update(byte)
+   * @see update(byte[])
+   */
+  public int getUpdatedBytes(byte[] output){
+    int length = inputList.size() >= output.length ? output.length : inputList.size();
+
+    for (int i=0; i<length; i++){
+      output[i] = getUpdatedByte();
+    }
+    return length;
+  }
+
+  protected void engineUpdate(byte input){
+  }
+
+  protected void engineReset(){
+  }
+
+  protected void engineUpdate(byte[] input, int offset, int len){
+  }
+
+  protected byte[] engineDigest(){
+    return null;
+  }
+
+
+}
---
+++ test/src/org/lockss/test/TestMockMessageDigest.java
@@ -0,0 +1,98 @@
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.test;
+
+import junit.framework.TestCase;
+
+public class TestMockMessageDigest extends TestCase{
+
+  public TestMockMessageDigest(String msg){
+    super(msg);
+  }
+
+  public void testUpdateNoBytes(){
+    MockMessageDigest dig = new MockMessageDigest();
+    assertEquals(-1, dig.getUpdatedByte());
+  }
+
+  public void testUpdateSingleByte(){
+    MockMessageDigest dig = new MockMessageDigest();
+    byte input = 4;
+    dig.update(input);
+    assertEquals(input, dig.getUpdatedByte());
+  }
+
+  public void testUpdatedSetOfBytes(){
+    MockMessageDigest dig = new MockMessageDigest();
+    dig.update((byte)1);
+    dig.update((byte)2);
+    dig.update((byte)3);
+    dig.update((byte)4);
+
+    assertEquals((byte)1, dig.getUpdatedByte());
+    assertEquals((byte)2, dig.getUpdatedByte());
+    assertEquals((byte)3, dig.getUpdatedByte());
+    assertEquals((byte)4, dig.getUpdatedByte());
+  }
+
+  public void testUpdatedByteArray(){
+    MockMessageDigest dig = new MockMessageDigest();
+    byte[] input = {1, 2, 3, 4};
+    dig.update(input);
+
+    assertEquals((byte)1, dig.getUpdatedByte());
+    assertEquals((byte)2, dig.getUpdatedByte());
+    assertEquals((byte)3, dig.getUpdatedByte());
+    assertEquals((byte)4, dig.getUpdatedByte());
+  }
+
+  public void testUpdatedByteArrayWithOffset(){
+    MockMessageDigest dig = new MockMessageDigest();
+    byte[] input = {1, 2, 3, 4, 5, 6, 7};
+    dig.update(input, 1, 3);
+
+    assertEquals((byte)2, dig.getUpdatedByte());
+    assertEquals((byte)3, dig.getUpdatedByte());
+    assertEquals((byte)4, dig.getUpdatedByte());
+    assertEquals((byte)-1, dig.getUpdatedByte());
+  }
+
+  public void testGetUpdatedBytes(){
+    MockMessageDigest dig = new MockMessageDigest();
+    byte[] input = {1, 2, 3, 4};
+    dig.update(input);
+    byte[] output = new byte[4];
+
+    assertEquals(output.length, dig.getUpdatedBytes(output));
+    for (int i=0; i<4; i++){
+      assertEquals(input[i], output[i]);
+    }
+  }
+
+}
