32e3fb6493577a1a95a9a0e7daa0725cab14028e
troberts <claire@lockss.org>
2002-11-20 00:31:55 +0000

    moved from org.lockss.protocol.TestIdentity and org.lockss.protocol.TestMessage
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@284 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/protocol/TestLcapIdentity.java
@@ -0,0 +1,234 @@
+package org.lockss.protocol;
+
+import java.io.*;
+import java.net.*;
+import java.util.Random;
+import org.mortbay.util.B64Code;
+import junit.framework.TestCase;
+import org.lockss.daemon.*;
+import org.lockss.test.*;
+import org.lockss.util.*;
+
+
+/** JUnitTest case for class: org.lockss.protocol.Identity */
+public class TestLcapIdentity extends TestCase {
+
+  static String fakeIdString = "123.145.167";
+  static LcapIdentity fakeId = new LcapIdentity(fakeIdString);
+  InetAddress testAddress;
+  int testReputation;
+  Object testIdKey;
+  LcapMessage testMsg= null;
+  private static String urlstr = "http://www.test.org";
+  private static String regexp = "*.doc";
+  private static byte[] testbytes = {1,2,3,4,5,6,7,8,9,10};
+  private static String[] testentries = {"test1.doc", 
+					 "test2.doc", "test3.doc"};
+
+  public TestLcapIdentity(String _name) {
+    super(_name);
+  }
+
+  /** setUp method for test case */
+  protected void setUp() {
+    try {
+      testAddress = InetAddress.getByName("127.0.0.1");
+    }
+    catch (UnknownHostException ex) {
+      fail("can't open test host");
+    }
+    testReputation = LcapIdentity.INITIAL_REPUTATION;
+    testIdKey = LcapIdentity.makeIdKey(testAddress);
+    try {
+      testMsg = LcapMessage.makeRequestMsg(urlstr,
+					   regexp,
+					   testentries,
+					   testAddress,
+					   (byte)5,
+					   testbytes,
+					   testbytes,
+					   LcapMessage.CONTENT_POLL_REQ,
+					   100000,
+					   fakeId);
+    }
+    catch (Exception ex) {
+      fail("message request creation failed.");
+    }
+  }
+
+  /** test for method getIdentity(..) */
+  public void testGetIdentity() {
+    LcapIdentity id1 = LcapIdentity.getIdentity(testAddress);
+    assertTrue(id1 != null);
+    // try and get the identity we just added
+    LcapIdentity id2 = LcapIdentity.findIdentity(id1.m_idKey);
+    assertTrue(id2.isEqual(id1));
+  }
+
+  /** test for method getLocalIdentity(..) */
+  public void testGetLocalIdentity() throws IOException {
+    String host = "1.2.3.4";
+    String prop = "org.lockss.localIPAddress="+host;
+    TestConfiguration.
+      setCurrentConfigFromUrlList(ListUtil.list(FileUtil.urlOfString(prop)));
+
+    LcapIdentity ident = LcapIdentity.getLocalIdentity();
+    assertEquals(host, ident.toHost());
+  }
+
+  /** test for method isLocalIdentity(..) */
+  public void testIsLocalIdentity() {
+  }
+
+  /** test for method isEqual(..) */
+  public void testIsEqual() {
+    LcapIdentity id1 = LcapIdentity.getIdentity(testAddress);
+    LcapIdentity id2 = LcapIdentity.findIdentity(id1.m_idKey);
+    assertEquals((String)id1.m_idKey,(String)id2.m_idKey);
+  }
+
+  /** test for method toString(..) */
+  public void testToString() {
+    String s = fakeId.toString();
+    assertTrue(s.equals((String)fakeId.m_idKey));
+  }
+
+  /** test for method toHost(..) */
+  public void testToHost() {
+  }
+
+  void checkReputation(int orgRep, int maxDelta, int curRep) {
+    if(maxDelta > 0) {
+      assertTrue(curRep <= orgRep + maxDelta);
+    }
+    else if(maxDelta < 0) {
+      assertTrue(curRep >= orgRep + maxDelta);
+    }
+  }
+  // -----------------------------------------------------
+
+  /** test for method agreeWithVote(..) */
+  public void testAgreeWithVote() {
+    int rep = fakeId.getReputation();
+    fakeId.agreeWithVote();
+    checkReputation(rep, LcapIdentity.AGREE_DELTA, fakeId.getReputation());
+
+  }
+
+  /** test for method disagreeWithVote(..) */
+  public void testDisagreeWithVote() {
+    int rep = fakeId.getReputation();
+    fakeId.disagreeWithVote();
+    checkReputation(rep, LcapIdentity.DISAGREE_DELTA, fakeId.getReputation());
+  }
+
+  /** test for method callInternalPoll(..) */
+  public void testCallInternalPoll() {
+    int rep = fakeId.getReputation();
+    fakeId.callInternalPoll();
+    checkReputation(rep, LcapIdentity.CALL_INTERNAL_DELTA, 
+		    fakeId.getReputation());
+  }
+
+  /** test for method spoofDetected(..) */
+  public void testSpoofDetected() {
+    int rep = fakeId.getReputation();
+    fakeId.spoofDetected();
+    checkReputation(rep, LcapIdentity.SPOOF_DETECTED, fakeId.getReputation());
+  }
+
+  /** test for method replayDected(..) */
+  public void testReplayDected() {
+    int rep = fakeId.getReputation();
+    fakeId.replayDetected();
+    checkReputation(rep, LcapIdentity.REPLAY_DETECTED, fakeId.getReputation());
+  }
+
+  /** test for method acttackDetected(..) */
+  public void testActtackDetected() {
+    int rep = fakeId.getReputation();
+    fakeId.attackDetected();
+    checkReputation(rep, LcapIdentity.ATTACK_DETECTED, fakeId.getReputation());
+  }
+
+  /** test for method voteNotVerify(..) */
+  public void testVoteNotVerify() {
+    int rep = fakeId.getReputation();
+    fakeId.voteNotVerify();
+    checkReputation(rep, LcapIdentity.VOTE_NOT_VERIFIED, 
+		    fakeId.getReputation());
+  }
+
+  /** test for method voteVerify(..) */
+  public void testVoteVerify() {
+    int rep = fakeId.getReputation();
+    fakeId.voteVerify();
+    checkReputation(rep, LcapIdentity.VOTE_VERIFIED, fakeId.getReputation());
+  }
+
+  /** test for method voteDisown(..) */
+  public void testVoteDisown() {
+    int rep = fakeId.getReputation();
+    fakeId.voteDisown();
+    checkReputation(rep, LcapIdentity.VOTE_DISOWNED, fakeId.getReputation());
+  }
+
+  /** test for method changeReputation(..) */
+  public void testChangeReputation() {
+    // test simple change
+    int rep = fakeId.getReputation();
+    fakeId.changeReputation(50);
+    assertTrue(rep+50 >= fakeId.getReputation());
+    fakeId.changeReputation(-150);
+    rep = fakeId.getReputation();
+    assertTrue(rep-150 <= fakeId.getReputation());
+
+    // test upper and lower bounds
+    rep = fakeId.getReputation();
+    fakeId.changeReputation(LcapIdentity.MAX_REPUTATION_DELTA * 10);
+    assertTrue(fakeId.getReputation() <= 
+	       rep +LcapIdentity.MAX_REPUTATION_DELTA);
+    rep = fakeId.getReputation();
+    fakeId.changeReputation(-LcapIdentity.MAX_REPUTATION_DELTA * 10);
+    assertTrue(fakeId.getReputation() >= 
+	       rep - LcapIdentity.MAX_REPUTATION_DELTA);
+  }
+
+  /** test for method rememberActive(..) */
+  public void testRememberActive() {
+    long inc_pkts = fakeId.m_incrPackets;
+    long tot_pkts = fakeId.m_totalPackets;
+
+    fakeId.rememberActive(false,testMsg);
+    assertEquals(inc_pkts + 1, fakeId.m_incrPackets);
+    assertEquals(tot_pkts + 1, fakeId.m_totalPackets);
+
+  }
+
+  /** test for method rememberValidOriginator(..) */
+  public void testRememberValidOriginator() {
+    long val_originator = fakeId.m_origPackets;
+    fakeId.rememberValidOriginator(testMsg);
+    assertEquals(val_originator + 1, fakeId.m_origPackets);
+  }
+
+  /** test for method rememberValidForward(..) */
+  public void testRememberValidForward() {
+    long val_forward = fakeId.m_forwPackets;
+    fakeId.rememberValidForward(testMsg);
+    assertEquals(val_forward + 1, fakeId.m_forwPackets);
+  }
+
+  /** test for method rememberDuplicate(..) */
+  public void testRememberDuplicate() {
+    long duplicates = fakeId.m_duplPackets;
+    fakeId.rememberDuplicate(testMsg);
+    assertEquals(duplicates + 1, fakeId.m_duplPackets);
+  }
+
+  /** Executes the test case */
+  public static void main(String[] argv) {
+    String[] testCaseList = {TestLcapIdentity.class.getName()};
+    junit.swingui.TestRunner.main(testCaseList);
+  }
+}
---
+++ test/src/org/lockss/protocol/TestLcapMessage.java
@@ -0,0 +1,204 @@
+/*
+ * $Id: TestLcapMessage.java,v 1.1 2002-11-20 00:31:55 troberts Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.protocol;
+
+import java.io.DataInputStream;
+import org.lockss.util.Logger;
+import org.lockss.test.MockCachedUrlSetSpec;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import junit.framework.TestCase;
+
+/** JUnitTest case for class: org.lockss.protocol.Message */
+public class TestLcapMessage extends TestCase {
+
+  private static String urlstr = "http://www.test.org";
+  private static String regexp = "*.doc";
+  private static byte[] testbytes = {1,2,3,4,5,6,7,8,9,10};
+  private static String[] testentries = {"test1.doc", 
+					 "test2.doc", "test3.doc"};
+
+  protected InetAddress testaddr;
+  protected LcapIdentity testID;
+  protected LcapMessage testmsg;
+
+  public TestLcapMessage(String _name) {
+    super(_name);
+  }
+
+
+  public void setUp() {
+
+    try {
+      testaddr = InetAddress.getByName("127.0.0.1");
+    }
+    catch (UnknownHostException ex) {
+      fail("can't open test host");
+    }
+    try {
+      testmsg = new LcapMessage();
+    }
+    catch (IOException ex) {
+      fail("can't create test message");
+    }
+    // assign the data
+    testmsg.m_targetUrl = urlstr;
+    testmsg.m_regExp = regexp;
+    testID = new LcapIdentity(testaddr);
+
+    testmsg.m_originID = new LcapIdentity(testaddr);
+
+    testmsg.m_startTime = 123456789;
+    testmsg.m_stopTime = 987654321;
+    testmsg.m_multicast = false;
+    testmsg.m_hopCount = 2;
+
+    testmsg.m_group = testaddr;
+    testmsg.m_ttl = 5;
+    testmsg.m_challenge = testbytes;
+    testmsg.m_verifier = testbytes;
+    testmsg.m_hashed = testbytes;
+    testmsg.m_opcode = LcapMessage.CONTENT_POLL_REQ;
+    testmsg.m_entries = testentries;
+
+  }
+
+  public void testEntriesTranslation() {
+    String encstr = testmsg.entriesToString();
+    String[] decoded = testmsg.stringToEntries(encstr);
+    assertTrue(Arrays.equals(testmsg.m_entries,decoded));
+  }
+
+
+  public void testReplyMessageCreation() {
+    LcapMessage rep_msg = null;
+
+    try {
+      rep_msg = LcapMessage.makeReplyMsg(testmsg,
+					 testbytes,
+					 testbytes,
+					 LcapMessage.CONTENT_POLL_REP,
+					 100000,
+					 testID);
+    }
+    catch(IOException ex) {
+      fail("message reply creation failed");
+    }
+
+    // now test to see if we got back what we expected
+    assertTrue(rep_msg.m_group.getHostAddress().equals(testaddr.getHostAddress()));
+    assertTrue(rep_msg.m_originID.isEqual(testID));
+    assertEquals(rep_msg.m_ttl,5);
+    assertEquals(rep_msg.m_opcode,LcapMessage.CONTENT_POLL_REP);
+    // TODO: figure out how to test time
+    assertEquals(rep_msg.m_multicast ,false);
+    assertEquals(rep_msg.m_hopCount,2);
+
+    assertTrue(Arrays.equals(rep_msg.m_challenge,testmsg.m_challenge));
+    assertTrue(Arrays.equals(rep_msg.m_verifier,testmsg.m_verifier));
+    assertTrue(Arrays.equals(rep_msg.m_hashed,testmsg.m_hashed));
+    assertTrue(Arrays.equals(rep_msg.m_entries,testentries));
+
+  }
+
+  public void testRequestMessageCreation() {
+    LcapMessage req_msg = null;
+    try {
+      req_msg = LcapMessage.makeRequestMsg(urlstr,
+					   regexp,
+					   testentries,
+					   testaddr,
+					   (byte)5,
+					   testbytes,
+					   testbytes,
+					   LcapMessage.CONTENT_POLL_REQ,
+					   100000,
+					   testID);
+    }
+    catch (Exception ex) {
+      fail("message request creation failed.");
+    }
+    assertTrue(req_msg.m_originID.isEqual(testID));
+    assertTrue(req_msg.m_group.getHostAddress().equals(testaddr.getHostAddress()));
+    assertEquals(req_msg.m_ttl,5);
+    assertEquals(req_msg.m_opcode,LcapMessage.CONTENT_POLL_REQ);
+    assertEquals(req_msg.m_multicast ,false);
+    assertEquals(req_msg.m_hopCount,5);
+
+    assertTrue(Arrays.equals(req_msg.m_challenge,testbytes));
+    assertTrue(Arrays.equals(req_msg.m_verifier,testbytes));
+    assertTrue(Arrays.equals(req_msg.m_hashed,new byte[0]));
+    assertTrue(Arrays.equals(req_msg.m_entries,testentries));
+
+  }
+
+  public void testMessageEncoding() {
+
+    byte[] msgbytes = new byte[0];
+    try {
+      msgbytes = testmsg.encodeMsg();
+    }
+    catch (IOException ex) {
+      fail("encode failed!");
+    }
+
+    try {
+      LcapMessage msg = new LcapMessage(msgbytes);
+      // now test to see if we got back what we started with
+      assertTrue(msg.m_group.getHostAddress().equals(testaddr.getHostAddress()));
+      assertTrue(msg.m_originID.isEqual(testmsg.m_originID));
+      assertEquals(msg.m_ttl,5);
+      assertEquals(msg.m_opcode,LcapMessage.CONTENT_POLL_REQ);
+      // TODO: figure out how to test time
+      assertEquals(msg.m_multicast ,false);
+      assertEquals(msg.m_hopCount,1); // we decremented this on decode
+
+      assertTrue(Arrays.equals(msg.m_challenge,testbytes));
+      assertTrue(Arrays.equals(msg.m_verifier,testbytes));
+      assertTrue(Arrays.equals(msg.m_hashed,testbytes));
+      assertTrue(Arrays.equals(msg.m_entries,testentries));
+    }
+    catch (IOException ex) {
+      fail("message decode failed");
+    }
+  }
+
+
+
+  /** Executes the test case */
+  public static void main(String[] argv) {
+    String[] testCaseList = {TestLcapMessage.class.getName()};
+    junit.swingui.TestRunner.main(testCaseList);
+  }
+}
