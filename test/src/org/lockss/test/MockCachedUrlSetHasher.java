cea1c4ed1f81d039d3fd398d458a027d8084651b
tlipkis <tal@pss.com>
2002-11-25 21:34:02 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@350 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/test/MockCachedUrlSetHasher.java
@@ -0,0 +1,83 @@
+/*
+ * $Id: MockCachedUrlSetHasher.java,v 1.1 2002-11-25 21:34:02 tal Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.test;
+
+import java.util.*;
+import java.security.MessageDigest;
+import org.lockss.daemon.*;
+import org.lockss.plugin.*;
+
+/**
+ * Mock version of <code>CachedUrlSetHasher</code> used for testing
+ */
+public class MockCachedUrlSetHasher implements CachedUrlSetHasher {
+  long duration;
+  int bytes;
+  Error toThrow;
+
+  public MockCachedUrlSetHasher(int numbytes) {
+    this.bytes = numbytes;
+  }
+
+  public MockCachedUrlSetHasher() {
+  }
+
+  public boolean finished() {
+    return bytes <= 0;
+  }
+
+  public int hashStep(int numBytes) {
+    if (toThrow != null) {
+      throw toThrow;
+    }
+    if (finished()) {
+      return 0;
+    }
+    numBytes = Math.max(1, Math.min(bytes, numBytes));
+    bytes -= numBytes;
+    return numBytes;
+  }
+
+  public void setNumBytes(int n) {
+    bytes = n;
+  }
+
+  public long getBytesLeft() {
+    return bytes;
+  }
+
+  public void throwThis(Error e) {
+    toThrow = e;
+  }
+
+}
