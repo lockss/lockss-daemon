b830b56d861729636ce6c302d58bee30f0248ba1
eaalto <claire@lockss.org>
2002-12-12 23:08:17 +0000

    Bean forms of PollHistory and Vote, used for XML marshalling.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@422 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/state/PollHistoryBean.java
@@ -0,0 +1,95 @@
+/*
+ * $Id: PollHistoryBean.java,v 1.1 2002-12-12 23:08:17 aalto Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+
+package org.lockss.state;
+
+import java.util.*;
+import org.lockss.protocol.LcapIdentity;
+
+/**
+ * PollHistoryBean is a settable version of PollHistory used purely for
+ * marshalling purposes.
+ */
+public class PollHistoryBean extends PollHistory {
+  public PollHistoryBean() { }
+
+  /**
+   * Sets the type.
+   * @param type the new type
+   */
+  public void setType(int type) {
+    super.type = type;
+  }
+
+  /**
+   * Sets the regular expression.
+   * @param regexp the new regexp
+   */
+  public void setRegExp(String regexp) {
+    super.regExp = regexp;
+  }
+
+  /**
+   * Sets the status.
+   * @param status the new status
+   */
+  public void setStatus(int status) {
+    this.status = status;
+  }
+
+  /**
+   * Sets the start time.
+   * @param startTime the new start time
+   */
+  public void setStartTime(long startTime) {
+    super.startTime = startTime;
+  }
+
+  /**
+   * Sets the duration.
+   * @param duration the new duration
+   */
+  public void setDuration(long duration) {
+    super.duration = duration;
+  }
+
+  /**
+   * Sets the vote list.
+   * @param votes an ArrayList of Poll.Vote objects
+   */
+  public void setVotes(ArrayList votes) {
+    super.votes = votes;
+  }
+
+
+}
---
+++ src/org/lockss/state/VoteBean.java
@@ -0,0 +1,76 @@
+/*
+ * $Id: VoteBean.java,v 1.1 2002-12-12 23:08:17 aalto Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.state;
+
+public class VoteBean {
+  public String idStr = null;
+  public boolean agree = false;
+
+  /**
+   * Empty constructor for bean creation during marshalling
+   */
+  public VoteBean() {
+  }
+
+  /**
+   * Returns the vote's id string.
+   * @return the id string
+   */
+  public String getId() {
+    return idStr;
+  }
+
+  /**
+   * Sets the id string.
+   * @param idStr the new id
+   */
+  public void setId(String idStr) {
+    this.idStr = idStr;
+  }
+
+  /**
+   * Returns the agree/disagree state.
+   * @return the agree state
+   */
+  public boolean getAgreeState() {
+    return agree;
+  }
+
+  /**
+   * Sets the agree/disagree state.
+   * @param agree_state the new agree state
+   */
+  public void setAgreeState(boolean agree_state) {
+    agree = agree_state;
+  }
+}
