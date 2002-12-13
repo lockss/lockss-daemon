7febfa9556aef58ed8a5f87ff834ef3012cebf61
tlipkis <tal@pss.com>
2002-12-13 02:18:14 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@426 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/util/ByteArray.java
@@ -0,0 +1,94 @@
+/*
+ * $Id: ByteArray.java,v 1.1 2002-12-13 02:18:14 tal Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+
+import java.io.*;
+
+/**
+ * Byte array utilities
+ */
+public class ByteArray {
+  private static final char[] HEX_CHARS = {
+    '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
+  };
+
+  /**
+   * Decode 4 byte at pos into an int
+   * @param b byte array
+   * @param pos starting position in array
+   */
+  public static int decodeInt(byte[] b, int pos) {
+    return
+      ((((int)b[pos    ]) & 0xFF) << 24) +
+      ((((int)b[pos + 1]) & 0xFF) << 16) +
+      ((((int)b[pos + 2]) & 0xFF) << 8) +
+      (((int)b[pos + 3]) & 0xFF);
+  }
+
+  /**
+   * Insert the 4 byte big-endian encoding of an int at the specified
+   * position in a byte array
+   * @param n  int to encode
+   * @param b byte array
+   * @param pos starting position in array
+   */
+  public static void encodeInt(int n, byte[] b, int pos) {
+    b[pos + 3] = (byte)(n & 0xFF);
+    n >>>= 8;
+    b[pos + 2] = (byte)(n & 0xFF);
+    n >>>= 8;
+    b[pos + 1] = (byte)(n & 0xFF);
+    n >>>= 8;
+    b[pos    ] = (byte)(n & 0xFF);
+  }
+
+  /** Return the concatenation of two byte arrays. */
+  public static byte[] concat(byte[] a, byte[] b) {
+    byte[] res = new byte[a.length + b.length];
+    System.arraycopy(a, 0, res, 0, a.length);
+    System.arraycopy(b, 0, res, a.length, b.length);
+    return res;
+  }
+
+  /** Return hex representation of bytes in array */
+  public static String toHexString(byte[] b) {
+    char[] buf = new char[b.length * 2];
+
+    for (int ix = 0, bufx = 0; ix < b.length; ix++) {
+      int tmp = b[ix];
+      buf[bufx++] = HEX_CHARS[(tmp >>> 4) & 0x0F];
+      buf[bufx++] = HEX_CHARS[tmp & 0x0F];
+    }
+    return new String(buf);
+  }
+}
---
+++ test/src/org/lockss/util/TestByteArray.java
@@ -0,0 +1,92 @@
+/*
+ * $Id: TestByteArray.java,v 1.1 2002-12-13 02:18:14 tal Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+import org.lockss.test.*;
+
+/**
+ * Test class for org.lockss.util.ByteArray
+ *
+ */
+
+public class TestByteArray extends LockssTestCase {
+  public static Class testedClasses[] = {
+    org.lockss.util.ByteArray.class
+  };
+
+  public TestByteArray(String msg) {
+    super(msg);
+  }
+
+  public void testConcat() {
+    String s1 = "hi there";
+    String s2 = "I am an apteryx";
+    byte c[] = ByteArray.concat(s1.getBytes(), s2.getBytes());
+    assertEquals(s1.concat(s2), new String(c));
+  }
+
+  public void testToHexString() {
+    byte t1[] = {1, 0x42, 0x7e, (byte)0xff};
+    byte t2[] = {(byte)255, (byte)255, (byte)255, (byte)255};
+    assertEquals("01427EFF", ByteArray.toHexString(t1));
+    assertEquals("FFFFFFFF", ByteArray.toHexString(t2));
+  }
+
+  public void testEncode() {
+    byte exp1[] = {0, 0, 1, 5};
+    byte exp2[] = {42, 75, 1, (byte)255};
+    byte tst[] = {(byte)255, (byte)255, (byte)255, (byte)255};
+    ByteArray.encodeInt(256 + 5, tst, 0);
+    assertEquals(exp1, tst);
+    ByteArray.encodeInt(((42 * 256 + 75) * 256 + 1) * 256 + 255, tst, 0);
+    assertEquals(exp2, tst);
+  }
+
+  public void testDecode() {
+    byte tst[] = {0, 0, 1, 5,
+		  42, 75, 1, (byte)255,
+		  (byte)255, (byte)255, (byte)255, (byte)255};
+    ByteArray.encodeInt(256 + 5, tst, 0);
+    assertEquals(256 + 5, ByteArray.decodeInt(tst, 0));
+    assertEquals(((42 * 256 + 75) * 256 + 1) * 256 + 255,
+		 ByteArray.decodeInt(tst, 4));
+    assertEquals(-1, ByteArray.decodeInt(tst, 8));
+  }
+
+  public void testEncodeDecode() {
+    byte tst[] = new byte[8];
+    ByteArray.encodeInt(12345679, tst, 0);
+    ByteArray.encodeInt(-1, tst, 4);
+    assertEquals(12345679, ByteArray.decodeInt(tst, 0));
+    assertEquals(-1, ByteArray.decodeInt(tst, 4));
+  }
+}
