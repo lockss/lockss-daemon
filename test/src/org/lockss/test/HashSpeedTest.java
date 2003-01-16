96c7cce4117ef705441a9bbb884cfa129c80f17f
eaalto <claire@lockss.org>
2003-01-16 22:11:12 +0000

    A simple hash speed test which creates simulated content and hashes it.  It doesn't really work because there's currently no method to clear the cache. FuncMailTarget just tests the MailTarget.  It is currently hardcoded with my email information.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@571 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/test/HashSpeedTest.java
@@ -0,0 +1,128 @@
+/*
+ * $Id: HashSpeedTest.java,v 1.1 2003-01-16 22:11:12 aalto Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.test;
+
+import java.io.File;
+import java.security.MessageDigest;
+import org.lockss.daemon.*;
+import org.lockss.plugin.simulated.*;
+import org.lockss.repository.LockssRepositoryImpl;
+import org.lockss.util.ListUtil;
+import org.lockss.poller.PollManager;
+import org.lockss.crawler.GoslingCrawlerImpl;
+import org.lockss.util.Deadline;
+
+public class HashSpeedTest extends LockssTestCase {
+  private SimulatedArchivalUnit sau;
+  private static final int DEFAULT_DURATION = 1000;
+  private static final int DEFAULT_BYTESTEP = 1024;
+  private static final int DEFAULT_FILESIZE = 3000;
+  private static int duration = DEFAULT_DURATION;
+  private static int byteStep = DEFAULT_BYTESTEP;
+  private static int fileSize = DEFAULT_FILESIZE;
+
+  public HashSpeedTest(String msg) {
+    super(msg);
+  }
+
+  public static void main(String[] args) throws Exception {
+    HashSpeedTest test = new HashSpeedTest("");
+    if (args.length>0) {
+      try {
+        duration = Integer.parseInt(args[0]);
+        if (args.length>1) {
+          byteStep = Integer.parseInt(args[1]);
+          if (args.length>2) {
+            fileSize = Integer.parseInt(args[2]);
+          }
+        }
+      } catch (NumberFormatException ex) { }
+    }
+    test.setUp(duration, byteStep);
+    test.runSelf();
+    test.tearDown();
+  }
+
+  public void setUp(int duration, int byteStep) throws Exception {
+    super.setUp();
+    String tempDirPath = getTempDir().getAbsolutePath() + File.separator;
+    sau = new SimulatedArchivalUnit(tempDirPath);
+    String s = SystemMetrics.PARAM_HASH_TEST_DURATION + "=" + duration;
+    String s2 = SystemMetrics.PARAM_HASH_TEST_BYTE_STEP + "=" + byteStep;
+    String s3 = LockssRepositoryImpl.PARAM_CACHE_LOCATION + "=" + tempDirPath;
+    TestConfiguration.setCurrentConfigFromUrlList(ListUtil.list(FileUtil.urlOfString(s),
+      FileUtil.urlOfString(s2), FileUtil.urlOfString(s3)));
+  }
+
+  public void runSelf() throws Exception {
+    createContent();
+    crawlContent();
+    hashContent();
+  }
+
+  private void createContent() {
+    SimulatedContentGenerator scg = sau.getContentGenerator();
+    scg.setTreeDepth(3);
+    scg.setNumBranches(5);
+    scg.setNumFilesPerBranch(5);
+    scg.setBinaryFileSize(fileSize);
+    scg.setFileTypes(scg.FILE_TYPE_BIN);
+    System.out.println("Generating tree of size 3x5x5 with "+fileSize+"byte files...");
+    scg.generateContentTree();
+  }
+
+  private void crawlContent() {
+    System.out.println("Crawling tree...");
+    CrawlSpec spec = new CrawlSpec(sau.SIMULATED_URL_START, null);
+    Crawler crawler = new GoslingCrawlerImpl();
+    crawler.doCrawl(sau, spec.getStartingUrls(),
+                    true, Deadline.NEVER);
+  }
+
+  private void hashContent() throws Exception {
+    MessageDigest digest = PollManager.getPollManager().getHasher();
+    System.out.println("Hashing-");
+    System.out.println("  Algorithm: "+digest.getAlgorithm());
+    System.out.println("  Duration: "+duration+"ms");
+    System.out.println("  Byte/step: "+byteStep+"bytes");
+    CachedUrlSetHasher hasher = sau.getAUCachedUrlSet().getContentHasher(digest);
+
+    SystemMetrics metrics = SystemMetrics.getSystemMetrics();
+    double estimate = metrics.getBytesPerMsHashEstimate(hasher, digest);
+    System.out.println("Estimate-");
+    System.out.println("  Bytes/ms: "+estimate);
+    System.out.println("  GB/hr: "+
+                       ((estimate*1000*60*60)/(1024*1024*1024)));
+  }
+}
+
---
+++ test/src/org/lockss/util/FuncMailTarget.java
@@ -0,0 +1,69 @@
+/*
+ * $Id: FuncMailTarget.java,v 1.1 2003-01-16 22:11:12 aalto Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+
+import org.lockss.test.*;
+import org.lockss.daemon.TestConfiguration;
+
+/**
+ * Test class for functional tests on the mail target.
+ */
+public class FuncMailTarget extends LockssTestCase {
+  private MailTarget target;
+  private static final String emailTo = "aalto@cs.stanford.edu";
+  private static final String emailFrom = "lime@leland.stanford.edu";
+  protected static Logger logger = Logger.getLogger("MailTarget");
+
+  public FuncMailTarget(String msg) {
+    super(msg);
+  }
+
+  public void setUp() throws Exception {
+    super.setUp();
+    String s = MailTarget.PARAM_SMTPHOST + "=smtp.stanford.edu";
+    String s2 = MailTarget.PARAM_LOG_EMAIL_TO + "=" + emailTo;
+    String s3 = MailTarget.PARAM_LOG_EMAIL_FROM + "=" + emailFrom;
+    TestConfiguration.setCurrentConfigFromUrlList(ListUtil.list(
+        FileUtil.urlOfString(s), FileUtil.urlOfString(s2),
+        FileUtil.urlOfString(s3)));
+    target = new MailTarget();
+    target.init();
+  }
+
+  public void testEmailLogging() throws Exception {
+    target.handleMessage(null, Logger.LEVEL_INFO, "Email test 1");
+    target.emailEnabled = false;
+    target.handleMessage(null, Logger.LEVEL_INFO,
+                         "Email test 2: should not be received");
+  }
+}
