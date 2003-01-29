8f64919c56b5af87911fcbca80502370e0cc8316
clairegriffin <clairetg@stanford.edu>
2003-01-29 21:04:25 +0000

    stub for hasVoted
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@598 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/poller/TestPoll.java
+++ test/src/org/lockss/poller/TestPoll.java
@@ -272,6 +272,10 @@ public class TestPoll extends TestCase {
     assertEquals(2, p.m_pendingVotes);
   }
 
+  public void testHasVoted() {
+
+  }
+
   public static Poll createCompletedPoll(LcapMessage testmsg, int numAgree,
                                 int numDisagree) throws Exception {
     testau = PollTestPlugin.PTArchivalUnit.createFromListOfRootUrls(rooturls);
