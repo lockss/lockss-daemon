626e3f781d78861d840d67f876264872069afc50
eaalto <claire@lockss.org>
2003-02-13 00:00:54 +0000

    Javadoc fix.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@658 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/app/LockssDaemon.java
+++ src/org/lockss/app/LockssDaemon.java
@@ -1,5 +1,5 @@
 /*
- * $Id: LockssDaemon.java,v 1.4 2003-02-11 23:57:01 claire Exp $
+ * $Id: LockssDaemon.java,v 1.5 2003-02-13 00:00:54 aalto Exp $
  */
 
 /*
@@ -209,7 +209,7 @@ public class LockssDaemon {
   /**
    * return the proxy handler instance
    * @return the ProxyHandler
-   * @throws IllegalAccessException if the manager is not available.
+   * @throws IllegalArgumentException if the manager is not available.
   */
   public ProxyHandler getProxyHandler() {
     return (ProxyHandler) getManager(PROXY_HANDLER);
