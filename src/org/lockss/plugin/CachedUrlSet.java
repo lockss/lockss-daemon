35a50919be3663c9d487756a82e5b93ed61a09bc
tlipkis <tal@pss.com>
2003-02-24 23:27:05 +0000

    Reindented.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@694 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/plugin/CachedUrlSet.java
+++ src/org/lockss/plugin/CachedUrlSet.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CachedUrlSet.java,v 1.1 2003-02-24 22:13:41 claire Exp $
+ * $Id: CachedUrlSet.java,v 1.2 2003-02-24 23:27:05 tal Exp $
  */
 
 /*
@@ -53,104 +53,104 @@ public interface CachedUrlSet extends CachedUrlSetNode {
    */
   public ArchivalUnit getArchivalUnit();
 
-    /**
-     * Return the <code>CachedUrlSetSpec</code>
-     * describing the set of URLs that are members of this
-     * <code>CachedUrlSet</code>.
-     * @return the CachedUrlSet
-     */
-    public CachedUrlSetSpec getSpec();
+  /**
+   * Return the <code>CachedUrlSetSpec</code>
+   * describing the set of URLs that are members of this
+   * <code>CachedUrlSet</code>.
+   * @return the CachedUrlSet
+   */
+  public CachedUrlSetSpec getSpec();
 
-    /**
-     * Return true if the url matches an entry in the
-     * <code>CachedUrlSet</code> object's list.
-     * @param url    url to be matched
-     * @return <code>true</code> if the url matches an entry in the list,
-     *         i.e. is in the file set, <code>false</code> otherwise
-     */
-    public boolean containsUrl(String url);
+  /**
+   * Return true if the url matches an entry in the
+   * <code>CachedUrlSet</code> object's list.
+   * @param url    url to be matched
+   * @return <code>true</code> if the url matches an entry in the list,
+   *         i.e. is in the file set, <code>false</code> otherwise
+   */
+  public boolean containsUrl(String url);
 
-    // Methods used by the poller
+  // Methods used by the poller
 
-    public CachedUrlSetHasher getContentHasher(MessageDigest hasher);
+  public CachedUrlSetHasher getContentHasher(MessageDigest hasher);
 
-    /**
-     * Return an object that can be used to hash the names of cached urls
-     * that match the list of <code>CachedUrlSetSpec</code>
-     * entries.
-     * @param hasher a <code>MessageDigest</code> object to which the
-     *               names will be supplied.
-     * @return a <code>CachedUrlSetHasher</code> object that will
-     *         hash the names of cached urls matching this
-     *         <code>CachedUrlSet</code>.
-     */
-    public CachedUrlSetHasher getNameHasher(MessageDigest hasher);
+  /**
+   * Return an object that can be used to hash the names of cached urls
+   * that match the list of <code>CachedUrlSetSpec</code>
+   * entries.
+   * @param hasher a <code>MessageDigest</code> object to which the
+   *               names will be supplied.
+   * @return a <code>CachedUrlSetHasher</code> object that will
+   *         hash the names of cached urls matching this
+   *         <code>CachedUrlSet</code>.
+   */
+  public CachedUrlSetHasher getNameHasher(MessageDigest hasher);
 
-    /**
-     * Return an <code>Iterator</code> of <code>UrlElement</code>
-     * objects representing the direct descendants of this
-     * <code>CachedUrlSet</code>.  These are CachedUrlSets for internal nodes
-     * and CachedUrls for leaf nodes.
-     * @return an <code>Iterator</code> of the <code>UrlElement</code>
-     *         matching the members of the
-     *         <code>CachedUrlSetSpec</code> list.
-     */
-    public Iterator flatSetIterator();
+  /**
+   * Return an <code>Iterator</code> of <code>UrlElement</code>
+   * objects representing the direct descendants of this
+   * <code>CachedUrlSet</code>.  These are CachedUrlSets for internal nodes
+   * and CachedUrls for leaf nodes.
+   * @return an <code>Iterator</code> of the <code>UrlElement</code>
+   *         matching the members of the
+   *         <code>CachedUrlSetSpec</code> list.
+   */
+  public Iterator flatSetIterator();
 
-    /**
-     * Return an <code>Iterator</code> of <code>UrlElement</code>
-     * objects representing all the nodes of the tree rooted at this
-     * <code>CachedUrlSet</code>.  These are CachedUrlSets for internal nodes
-     * and CachedUrls for leaf nodes.
-     * @return an <code>Iterator</code> of <code>UrlElement</code>s
-     *         for all the nodes matching the members of the
-     *         <code>CachedUrlSetSpec</code> list.
-     */
-    public Iterator treeIterator();
+  /**
+   * Return an <code>Iterator</code> of <code>UrlElement</code>
+   * objects representing all the nodes of the tree rooted at this
+   * <code>CachedUrlSet</code>.  These are CachedUrlSets for internal nodes
+   * and CachedUrls for leaf nodes.
+   * @return an <code>Iterator</code> of <code>UrlElement</code>s
+   *         for all the nodes matching the members of the
+   *         <code>CachedUrlSetSpec</code> list.
+   */
+  public Iterator treeIterator();
 
-    /**
-     * Return an estimate of the time required to hash the content.
-     * @return an estimate of the time required to hash the content.
-     */
-    public long estimatedHashDuration();
+  /**
+   * Return an estimate of the time required to hash the content.
+   * @return an estimate of the time required to hash the content.
+   */
+  public long estimatedHashDuration();
 
-    /**
-     * Provide the measured duration of a hash attempt and an
-     * indication of success or failure.
-     * @param elapsed the measured duration of a hash attempt.
-     * @param err the exception that terminated the hash, or null if it
-     * succeeded
-     */
-    public void storeActualHashDuration(long elapsed, Exception err);
+  /**
+   * Provide the measured duration of a hash attempt and an
+   * indication of success or failure.
+   * @param elapsed the measured duration of a hash attempt.
+   * @param err the exception that terminated the hash, or null if it
+   * succeeded
+   */
+  public void storeActualHashDuration(long elapsed, Exception err);
 
-    // Methods used by readers
+  // Methods used by readers
 
-    /**
-     * Create a <code>CachedUrl</code> object within the set.
-     * @param url the url of interest
-     * @return a <code>CachedUrl</code> object representing the url.
-     */
-    public CachedUrl makeCachedUrl(String url);
+  /**
+   * Create a <code>CachedUrl</code> object within the set.
+   * @param url the url of interest
+   * @return a <code>CachedUrl</code> object representing the url.
+   */
+  public CachedUrl makeCachedUrl(String url);
 
-    // Methods used by writers
+  // Methods used by writers
 
-    /**
-     * Create a <code>UrlCacher</code> object within the set.
-     * @param url the url of interest
-     * @return a <code>UrlCacher</code> object representing the url.
-     */
-    public UrlCacher makeUrlCacher(String url);
+  /**
+   * Create a <code>UrlCacher</code> object within the set.
+   * @param url the url of interest
+   * @return a <code>UrlCacher</code> object representing the url.
+   */
+  public UrlCacher makeUrlCacher(String url);
 
-    /**
-     * Needs to be overridden to hash CachedUrlSets properly.
-     * @return the hashcode
-     */
-    public int hashCode();
+  /**
+   * Needs to be overridden to hash CachedUrlSets properly.
+   * @return the hashcode
+   */
+  public int hashCode();
 
-    /**
-     * Needs to be overridden to hash CachedUrlSets properly.
-     * @param obj the object to compare to
-     * @return true if equal
-     */
-    public boolean equals(Object obj);
+  /**
+   * Needs to be overridden to hash CachedUrlSets properly.
+   * @param obj the object to compare to
+   * @return true if equal
+   */
+  public boolean equals(Object obj);
 }
