2ecb422b6f9202b4f34d61862b7ecf8709179a35
troberts <claire@lockss.org>
2003-03-03 19:33:50 +0000

    org.lockss.test.MockNodeManager *mock implementation of node manager
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@789 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/test/MockNodeManager.java
@@ -0,0 +1,115 @@
+/*
+ * $Id: MockNodeManager.java,v 1.1 2003-03-03 19:33:50 troberts Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.test;
+
+import java.util.Iterator;
+import org.lockss.plugin.*;
+import org.lockss.poller.*;
+import org.lockss.util.*;
+import org.lockss.state.*;
+import org.lockss.app.*;
+
+public class MockNodeManager implements NodeManager, LockssManager {
+  private static Logger logger = Logger.getLogger("MockNodeManager");
+
+  private MockAuState aus;
+
+  public void initService(LockssDaemon daemon) 
+      throws LockssDaemonException {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public void startService() {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public void stopService() {
+    logger.debug3("Service stopped");
+  }
+
+  public NodeManager managerFactory(ArchivalUnit au) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public void startPoll(CachedUrlSet cus, Poll.VoteTally state) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public void updatePollResults(CachedUrlSet cus, Poll.VoteTally results) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public NodeState getNodeState(CachedUrlSet cus) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public AuState getAuState() {
+    if (aus == null) {
+      aus = new MockAuState();
+    }
+    return aus;
+  }
+
+  public void setAuState(MockAuState aus) {
+    this.aus = aus;
+  }
+
+  public Iterator getActiveCrawledNodes(CachedUrlSet cus) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public Iterator getFilteredPolledNodes(CachedUrlSet cus, int filter) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public Iterator getNodeHistories(CachedUrlSet cus, int maxNumber) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public Iterator getNodeHistoriesSince(CachedUrlSet cus, Deadline since) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public long getEstimatedTreeWalkDuration() {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+
+  public void startTreeWalk() {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public void newTopLevelCrawlFinished() {
+    aus.newCrawlFinished();
+  }
+}
