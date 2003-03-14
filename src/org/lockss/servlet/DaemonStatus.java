ddef63741d67f67dbc77fbf3d7197b04c61b7c4b
tlipkis <tal@pss.com>
2003-03-14 01:51:47 +0000

    Now works.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@892 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/servlet/DaemonStatus.java
+++ src/org/lockss/servlet/DaemonStatus.java
@@ -1,5 +1,5 @@
 // ========================================================================
-// $Id: DaemonStatus.java,v 1.1 2003-03-12 22:15:35 tal Exp $
+// $Id: DaemonStatus.java,v 1.2 2003-03-14 01:51:47 tal Exp $
 // ========================================================================
 
 /*
@@ -44,6 +44,7 @@ import org.mortbay.html.*;
 import org.mortbay.tools.*;
 import org.lockss.util.*;
 import org.lockss.daemon.*;
+import org.lockss.daemon.status.*;
 
 /** DaemonStatus servlet
  */
@@ -52,108 +53,234 @@ public class DaemonStatus extends LockssServlet {
   private static final String bAddRem = "Add/Remove Cluster Clients";
   private static final String bDelete = "Delete";
   private static final String bAdd = "Add";
+  public static final DateFormat df =
+    DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+
   private boolean isForm = false;
+  private boolean html = false;
   private String errorMsg = null;
+  private StatusService statSvc;
+
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+    statSvc = getLockssDaemon().getStatusService();
+  }
+
 
   public void lockssHandle() throws IOException {
+    PrintWriter wrtr = resp.getWriter();
     Page page = null;
+    Date now = new Date();
 
-    // Display table
-    page = newPage();
-    page.add(getMachineTable());
-    page.add(getFooter());
-    page.write(resp.getWriter());
+    html = req.getParameter("text") == null;
+
+    resp.setContentType(html ? "text/html" : "text/plain");
+
+    String tableName = req.getParameter("table");
+    String key = req.getParameter("key");
+    if (StringUtil.isNullString(tableName)) {
+      tableName = StatusService.ALL_TABLES_TABLE;
+    }
+    if (StringUtil.isNullString(key)) {
+      key = null;
+    }
+
+    if (html) {
+      page = newPage();
+
+      page.add("<center>" + getMachineName() + " at " +
+	       df.format(now) + "</center>");
+
+//       page.add("<center>");
+//       page.add(srvLink(SERVLET_DAEMON_STATUS, ".",
+// 		       concatParams("text=1", req.getQueryString())));
+//       page.add("</center><br><br>");
+    } else {
+      wrtr.println("host=" + getLcapIPAddr() +
+		   ",time=" + now.getTime() +
+		   ",version=" + "0.0");
+    }
+
+    doStatusTable(page, wrtr, tableName, key);
+    if (html) {
+      page.add(getFooter());
+      page.write(wrtr);
+    }
   }
 
   // Build the table
-  private Composite getMachineTable() throws IOException {
-    Composite comp;
-    Table table =
-      new Table(0, "align=center border=0 cellspacing=5 cellpadding=0");
+  private void doStatusTable(Page page, PrintWriter wrtr,
+			     String tableName, String tableKey)
+      throws IOException {
+    StatusTable statTable;
+    try {
+      statTable = statSvc.getTable(tableName, tableKey);
+    } catch (StatusService.NoSuchTableException e) {
+      if (html) {
+	page.add("Can't get table: ");
+	page.add(e.toString());
+      } else {
+	wrtr.println("Error getting table: " + e.toString());
+      }
+      return;
+    }
+    java.util.List colList = statTable.getColumnDescriptors();
+    java.util.List rowList = statTable.getSortedRows();
+//     String title = statTable.getTitle();
+//     String titleFoot = statTable.getTitleFootnote();
+    String title = tableName;
+    String titleFoot = null;
 
-    comp = table;
+    Table table = null;
 
-    table.newRow();
-    table.newCell();
-    table.add("<b>Machine Name&nbsp&nbsp</b>");
+    StatusTable.ColumnDescriptor cds[] =
+      (StatusTable.ColumnDescriptor [])colList.toArray(new StatusTable.ColumnDescriptor[0]);
+    int cols = cds.length;
+    Iterator rowIter = rowList.iterator();
+    if (rowIter.hasNext()) {
+      // if table not empty, output column headings
+      table = new Table(0, "CELLSPACING=2 CELLPADDING=0 WIDTH=\"100%\"");
+      if (html) {
+	// ensure title footnote numbered before ColDesc.HEADs
+	title = title + addFootnote(titleFoot);
 
-    table.newCell();
-    table.add("<b>IP Address&nbsp&nbsp</b>");
+	table.newRow();
+	table.addHeading(title, "ALIGN=CENTER COLSPAN=" +
+			   (cols * 2 - 1));
+	table.newRow();
 
-    table.newCell();
-    table.add("<b>Links</b>");
-
-    table.newRow();
-    table.newCell("COLSPAN=3");
-    table.add("<hr noshade>");
-
-    table.newRow();
-    table.newCell();
-
-    boolean odd=false;
-    String clients[] = {"foobar"};
-    // arrange for me to be first
-    for (int i = 0; i < clients.length; i++) {
-      String c = clients[i];
-      if (c.equals(adminAddr)) {
-	if (i != 0) {
-	  clients[i] = clients[0];
-	  clients[0] = c;
+	for (int ix = 0; ix < cols; ix++) {
+	  StatusTable.ColumnDescriptor cd = cds[ix];
+	  String head = cd.getTitle() + addFootnote(null/*cd.getFootnote()*/);
+	  table.addHeading(head, "align=" + getColAlignment(cd));
+	  if (ix < cols) {
+	    table.newCell("width = 4");
+	  }
 	}
-	break;
+      } else {
+	wrtr.println();
+	wrtr.println("table=" + title);
+	if (tableKey != null) {
+	  wrtr.println("key=" + tableKey);
+	}	  
+      }
+
+    }
+    while (rowIter.hasNext()) {
+      Map rowMap = (Map)rowIter.next();
+      if (html) {
+	table.newRow();
+	for (int ix = 0; ix < cols; ix++) {
+	  StatusTable.ColumnDescriptor cd = cds[ix];
+	  Object val = rowMap.get(cd.getColumnName());
+	  String disp;
+
+	  table.newCell("align=" + getColAlignment(cd));
+	  table.add(dispString(rowMap.get(cd.getColumnName()), cd));
+	  if (ix < (cols)) {
+	    table.newCell();	// empty column for spacing
+	  }
+	}
+      } else {
+	Iterator iter = rowMap.keySet().iterator();
+	while (iter.hasNext()) {
+	  String key = (String)iter.next();
+	  wrtr.print(key + "=" + rowMap.get(key).toString());
+	  if (iter.hasNext()) {
+	    wrtr.print(",");
+	  } else {
+	    wrtr.println();
+	  }
+	}
       }
     }
-    for (int i = 0; i < clients.length; i++) {
-      String client = clients[i];
-      File f = getClientPropFile(client);
-
-      if (f.exists()) {
-        if (odd) {
-          table.newRow("bgcolor="+cellContrastColor);
-        } else {
-          table.newRow();
-	}
-	odd = !odd;
-        // display configuration parameters for dir name(machine-ip)
-        getTableEntry(client, table);
-      }
+    if (html && table != null) {
+      page.add(table);
+      page.add("<br>");
     }
-    if (comp != table) {
-      comp.add(table);
-    }
-    comp.add("<p>");
-
-    return comp;
   }
 
-  // Add a client row to the table
-  public void getTableEntry(String ip, Table table)
-      throws IOException {
+  private String getColAlignment(StatusTable.ColumnDescriptor cd) {
+    switch (cd.getType()) {
+    case StatusTable.TYPE_STRING:
+    case StatusTable.TYPE_FLOAT:	// tk - should align decimal points?
+    case StatusTable.TYPE_DATE:
+    case StatusTable.TYPE_IP_ADDRESS:
+    case StatusTable.TYPE_TIME_INTERVAL:
+    default:
+      return "LEFT";
+    case StatusTable.TYPE_INT:
+    case StatusTable.TYPE_PERCENT:
+      return "RIGHT";
+    }
+  }
 
-    String machineName = getMachineName(ip);
 
-    table.newCell();
-    table.add(machineName);
-
-    table.newCell();
-    table.add(ip);
-
-    table.newCell();
-    // XXX need api to pass client to srvLink
-    // XXX barring that, rebind it
-    String saveClient = clientAddr;
-    try {
-      clientAddr = ip;
-//       table.add(srvLink(SERVLET_JOURNAL_STATUS, null));
-//       table.add("<br>");
-//       table.add(srvLink(SERVLET_JOURNAL_SETUP, null));
-      if (isForm && !ip.equals(adminAddr)) {
-	table.newCell();
-	table.add("&nbsp&nbsp&nbsp&nbsp&nbsp");
-	table.add(new Input(Input.Submit, "action", bDelete + " " + ip));
+  private String dispString(Object val, StatusTable.ColumnDescriptor cd) {
+    if (val instanceof StatusTable.Reference) {
+      StatusTable.Reference ref = (StatusTable.Reference)val;
+      StringBuffer sb = new StringBuffer();
+      sb.append("table=");
+      sb.append(ref.getTableName());
+      String key = ref.getKey();
+      if (!StringUtil.isNullString(key)) {
+	sb.append("&key=");
+	sb.append(key);
       }
-    } finally {
-      clientAddr = saveClient;
+      return srvLink(SERVLET_DAEMON_STATUS, dispString1(ref.getValue(), cd),
+		     sb.toString());
+    } else {
+      return dispString1(val, cd);
+    }
+  }
+
+  private String dispString1(Object val, StatusTable.ColumnDescriptor cd) {
+    try {
+      switch (cd.getType()) {
+      case StatusTable.TYPE_STRING:
+      case StatusTable.TYPE_FLOAT:
+      case StatusTable.TYPE_INT:
+      default:
+	return val.toString();
+      case StatusTable.TYPE_PERCENT:
+	float fv = ((Number)val).floatValue();
+	return Integer.toString(Math.round(fv * 100)) + "%";
+      case StatusTable.TYPE_DATE:
+	long lv = ((Number)val).longValue();
+	return df.format(new Date(lv));
+      case StatusTable.TYPE_IP_ADDRESS:
+	return ((InetAddress)val).getHostAddress();
+      case StatusTable.TYPE_TIME_INTERVAL:
+	long millis = ((Number)val).longValue();
+	StringBuffer sb = new StringBuffer();
+	long days = millis / Constants.DAY;
+	millis %= Constants.DAY;
+	if (days > 0) {
+	  sb.append(days);
+	  sb.append("d");
+	}
+	long hours = millis / Constants.HOUR;
+	millis %= Constants.HOUR;
+	if (days != 0 || hours != 0) {
+	  sb.append(hours);
+	  sb.append("h");
+	}
+	long mins = millis / Constants.MINUTE;
+	millis %= Constants.MINUTE;
+	if (days != 0 || hours != 0 || mins != 0) {
+	  sb.append(mins);
+	  sb.append("m");
+	}
+	long secs = millis / Constants.SECOND;
+	if (days == 0) {
+	  sb.append(secs);
+	  sb.append("s");
+	}
+	return sb.toString();
+      }
+    } catch (NumberFormatException e) {
+      log.warning("bad number: " + e.toString());
+      return val.toString();
     }
   }
 
