079aa70d9d304623de17e9d46c06cb923672e45c
tlipkis <tal@pss.com>
2003-03-18 01:05:18 +0000

    Initial revision.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@936 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/daemon/status/ObjectReferenceAccessor.java
@@ -0,0 +1,54 @@
+/*
+ * $Id: ObjectReferenceAccessor.java,v 1.1 2003-03-18 01:03:20 tal Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.daemon.status;
+import java.util.*;
+
+/**
+ * Objects wishing to provide references to tables should implement this
+ *
+ * Used by {@link StatusService} to generate {@link StatusTable.Reference}s.  
+ */
+
+public interface ObjectReferenceAccessor {
+  /**
+   * Create a {@link StatusTable.Reference} to the named table with a key
+   * appropriate to select and/or filter on the supplied object.
+   * @param obj Object used to generate the key
+   * @param tableName table name under which this ObjectReferenceAccessor
+   * was registered.
+   * @return a {@link StatusTable.Reference} with a key derived from the
+   * object, and a value appropriate to display in a link to the
+   * referred-to table.
+   */
+  public StatusTable.Reference getReference(Object obj, String tableName);
+}
