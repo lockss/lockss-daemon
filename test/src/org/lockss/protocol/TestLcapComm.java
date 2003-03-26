0e7f378d4244ccb739928a0d131040bad893d17d
tlipkis <tal@pss.com>
2003-03-26 00:48:51 +0000

    Fix param constant names.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@1012 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/protocol/TestLcapComm.java
+++ test/src/org/lockss/protocol/TestLcapComm.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestLcapComm.java,v 1.7 2003-03-24 19:40:48 troberts Exp $
+ * $Id: TestLcapComm.java,v 1.8 2003-03-26 00:48:51 tal Exp $
  */
 
 /*
@@ -62,14 +62,14 @@ public class TestLcapComm extends LockssTestCase {
   static final String PARAM_MULTI_PORT = LcapComm.PARAM_MULTI_PORT;
   static final String PARAM_UNI_PORT = LcapComm.PARAM_UNI_PORT;
   static final String PARAM_UNI_PORT_SEND = LcapComm.PARAM_UNI_PORT_SEND;
-  static final String PARAM_VERIFY_MULTICAST = LcapComm.PARAM_MULTI_VERIFY;
+  static final String PARAM_MULTI_VERIFY = LcapComm.PARAM_MULTI_VERIFY;
 
   private static final String c1 =
     PARAM_MULTI_GROUP + "=239.3.4.5" + "\n" +
     PARAM_MULTI_PORT + "=5432" + "\n" +
     PARAM_UNI_PORT + "=2345" + "\n" +
 //     PARAM_UNI_PORT_SEND + "=" + "\n" +
-    PARAM_VERIFY_MULTICAST + "=yes";
+    PARAM_MULTI_VERIFY + "=yes";
 
   String testStr = "This is test data";
   byte[] testData;
