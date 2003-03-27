2422b7b1f6d0e795bab0bf7d32e9c268f3a79076
tlipkis <tal@pss.com>
2003-03-27 17:49:39 +0000

    Initial Revision.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@1035 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/protocol/TestLcapRouter.java
@@ -0,0 +1,116 @@
+/*
+ * $Id: TestLcapRouter.java,v 1.1 2003-03-27 17:49:39 tal Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.protocol;
+
+import java.io.DataInputStream;
+import org.lockss.util.Logger;
+import org.lockss.test.MockCachedUrlSetSpec;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import org.lockss.test.*;
+import gnu.regexp.*;
+import org.lockss.poller.TestPoll;
+import org.lockss.poller.*;
+
+/** JUnitTest case for class: org.lockss.protocol.Message */
+public class TestLcapRouter extends LockssTestCase {
+
+  private static String urlstr = "http://www.example.com";
+  private static byte[] testbytes = {1,2,3,4,5,6,7,8,9,10};
+  private static String lwrbnd = "test1.doc";
+  private static String uprbnd = "test3.doc";
+  private static String[] testentries;
+
+  private static MockLockssDaemon daemon = new MockLockssDaemon(null);
+  private IdentityManager idmgr = daemon.getIdentityManager();
+  protected InetAddress testaddr;
+  protected LcapIdentity testID;
+  protected LcapMessage testmsg;
+  protected static String pluginID = "TestPlugin_1.0";
+  protected static String archivalID = "TestAU_1.0";
+  LockssDatagram dg;
+  LockssReceivedDatagram rdg;
+
+  public void setUp() throws Exception {
+    super.setUp();
+
+  }
+
+  LcapMessage createTestMsg() throws IOException {
+    try {
+      testaddr = InetAddress.getByName("127.0.0.1");
+    }
+    catch (UnknownHostException ex) {
+      fail("can't open test host");
+    }
+    try {
+      testmsg = new LcapMessage();
+    }
+    catch (IOException ex) {
+      fail("can't create test message");
+    }
+    // assign the data
+    testmsg.m_targetUrl = urlstr;
+    testmsg.m_lwrBound = lwrbnd;
+    testmsg.m_uprBound = uprbnd;
+    testID = new LcapIdentity(testaddr);
+
+    testmsg.m_originAddr = testaddr;
+    testmsg.m_hashAlgorithm = LcapMessage.getDefaultHashAlgorithm();
+    testmsg.m_startTime = 123456789;
+    testmsg.m_stopTime = 987654321;
+    testmsg.m_multicast = false;
+    testmsg.m_hopCount = 2;
+
+    testmsg.m_ttl = 5;
+    testmsg.m_challenge = testbytes;
+    testmsg.m_verifier = testbytes;
+    testmsg.m_hashed = testbytes;
+    testmsg.m_opcode = LcapMessage.CONTENT_POLL_REQ;
+    testmsg.m_entries = testentries = TestPoll.makeEntries(1, 25);
+    testmsg.m_pluginID = pluginID;
+    testmsg.m_archivalID = archivalID;
+    dg = new LockssDatagram(LockssDatagram.PROTOCOL_LCAP, testmsg.encodeMsg());
+    return testmsg;
+  }
+
+  public void testIsEligibleToForward() {
+  }
+
+  public static void main(String[] argv) {
+    String[] testCaseList = {TestLcapRouter.class.getName()};
+    junit.swingui.TestRunner.main(testCaseList);
+  }
+}
+
