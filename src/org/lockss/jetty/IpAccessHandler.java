21694514c9643a990f945857e3494639404bb5fa
tlipkis <tal@pss.com>
2003-04-14 07:29:40 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@1213 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/jetty/IpAccessHandler.java
@@ -0,0 +1,121 @@
+/*
+ * $Id: IpAccessHandler.java,v 1.1 2003-04-14 07:29:40 tal Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+// ===========================================================================
+// Copyright (c) 1996-2002 Mort Bay Consulting Pty. Ltd. All rights reserved.
+// $Id: IpAccessHandler.java,v 1.1 2003-04-14 07:29:40 tal Exp $
+// ---------------------------------------------------------------------------
+
+package org.lockss.jetty;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.text.DateFormat;
+import java.util.*;
+import org.mortbay.http.*;
+import org.mortbay.http.handler.*;
+import org.mortbay.util.*;
+import org.lockss.util.*;
+
+/** Extension of ResourceHandler that allows flexibility in finding the
+ * Resource.  Mostly copied here because some things in ResourceHandler
+ * aren't public or protected. */
+public class IpAccessHandler extends AbstractHttpHandler {
+  private static Logger log = Logger.getLogger("IpAccess");
+
+  private IpFilter filter = new IpFilter();
+  private String serverName;
+  private boolean allowLocal = false;
+  private Set localIps;
+  private boolean logForbidden;
+
+  public IpAccessHandler(String serverName) {
+    this.serverName = serverName;
+  }
+
+  public void setFilter(IpFilter filter) {
+    this.filter = filter;
+  }
+
+  public void setLogForbidden(boolean logForbidden) {
+    this.logForbidden = logForbidden;
+  }
+
+  public void setAllowLocal(boolean allowLocal) {
+    if (localIps == null) {
+      localIps = new HashSet();
+      localIps.add("127.0.0.1");
+      // tk - add local interfaces
+    }
+    this.allowLocal = allowLocal;
+  }
+
+  boolean isAuthorized(String ip) throws IpFilter.MalformedException {
+    return (filter.isIpAllowed(ip) || (allowLocal && localIps.contains(ip)));
+  }
+
+  /**
+   * Handles the incoming request
+   *
+   * @param pathInContext	
+   * @param pathParams	
+   * @param request	The incoming HTTP-request
+   * @param response	The outgoing HTTP-response
+   */
+  public void handle(String pathInContext,
+		     String pathParams,
+		     HttpRequest request,
+		     HttpResponse response)
+      throws HttpException, IOException {
+
+    try	{
+      String ip = request.getRemoteAddr();
+      boolean authorized = isAuthorized(ip);
+		
+      if (!authorized) {
+	// The IP is NOT allowed
+	if (logForbidden) {
+	  log.info("Access to " + serverName + " forbidden from " + ip);
+	}
+	response.sendError(HttpResponse.__403_Forbidden);
+	request.setHandled(true);
+	return; 
+      } else {
+	// The IP is allowed
+	return;
+      }
+    } catch (Exception e) {
+      log.warning("Error checking IP", e);
+      response.sendError(HttpResponse.__500_Internal_Server_Error);
+      request.setHandled(true);
+    }
+  }
+}
---
+++ src/org/lockss/util/IpFilter.java
@@ -0,0 +1,253 @@
+/*
+ * $Id: IpFilter.java,v 1.1 2003-04-14 07:29:40 tal Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+
+import java.util.*;
+import org.lockss.daemon.*;
+import org.lockss.util.*;
+import org.lockss.app.*;
+
+/**
+ * IpFilter objects represent either an IP address or a range of addresses
+ * (addr and mask).
+ * Static methods support a single pair of include and exclude filter arrays,
+ * and lookups.
+ */
+public class IpFilter {
+
+  private Mask[] inclFilters;
+  private Mask[] exclFilters;
+
+  /** Set include and exclude access lists from delim-separated strings.
+   */
+  public void setFilters(String includeList, String excludeList, char delim)
+      throws MalformedException {
+    setFilters(StringUtil.breakAt(includeList, delim),
+	       StringUtil.breakAt(excludeList, delim));
+  }
+
+  /** Set include and exclude access lists from vectors of Mask strings
+   */
+  public void setFilters(Vector inclVec, Vector exclVec)
+      throws MalformedException {
+    inclFilters = new Mask[inclVec.size()];
+    exclFilters = new Mask[exclVec.size()];
+    int i = 0;
+    for (Iterator iter = inclVec.iterator(); iter.hasNext();) {
+      inclFilters[i++] = new Mask((String)iter.next(), true);
+    }
+    i = 0;
+    for (Iterator iter = exclVec.iterator(); iter.hasNext();) {
+      exclFilters[i++] = new Mask((String)iter.next(), true);
+    }
+//      logFilters();
+  }
+
+  /** Search for matching element in filter list
+   */
+  private boolean isInList(Mask ip, Mask[] list) {
+    for (int i = 0; i < list.length; i++) {
+      if (ip.match(list[i])) {
+	return true;
+      }
+    }
+    return false;
+  }
+
+  /** Return true if ip addr string is allowed by filters
+   */
+  public boolean isIpAllowed(String ipstr)
+      throws MalformedException {
+    Addr ip = new Addr(ipstr);
+    return isIpAllowed(ip);
+  }
+
+  /** Return true if Addr instance is allowed by filters
+   */
+  public boolean isIpAllowed(Addr ip) {
+    if (inclFilters == null) return false;
+    if (!isInList(ip, inclFilters)) return false;
+    if (isInList(ip, exclFilters)) return false;
+    return true;
+  }
+
+  private void logFilters() {
+    StringBuffer sb = new StringBuffer();
+    sb.append("Include: ");
+    for (int i = 0; i < inclFilters.length; i++) {
+      sb.append(inclFilters[i]);
+      sb.append(" ");
+    }
+    sb.append("\nExclude: ");
+    for (int i = 0; i < exclFilters.length; i++) {
+      sb.append(exclFilters[i]);
+      sb.append(" ");
+    }
+    sb.append("\n");
+    System.err.print(sb.toString());
+  }
+
+  /** Represents an IP address mask */
+  public static class Mask {
+
+    private int addr = 0;
+    private int mask = -1;
+    private int cidr = 32;
+
+    /**/
+    /** Constructor for an IP address possibly including a mask
+     */
+    public Mask(String s, boolean maskOk) throws MalformedException {
+      s = s.trim();
+      StringTokenizer en = new StringTokenizer(s, "./", true);
+      boolean seenStar = false;
+      int n = 0;
+      int b;
+      try {
+	while (en.hasMoreTokens()) {
+	  String tok = en.nextToken();
+
+	  if ("*".equals(tok)) {
+	    b = -1;
+	  } else {
+	    b = Integer.parseInt(tok);
+	  }
+	  if (b == -1) {
+	    if (!seenStar) {
+	      cidr = n * 8;
+	    }
+	    seenStar = true;
+	    b = 0;
+	  } else if (seenStar) {
+	    throw new MalformedException("* in illegal position", s);
+	  }
+	  addr = (addr << 8) | b;
+	  n++;
+	  if (en.hasMoreTokens()) {
+	    tok = en.nextToken();
+	    if (".".equals(tok)) {
+	      if (n < 4) {
+		continue;
+	      } else {
+		throw new MalformedException("too many dots", s);
+	      }
+	    }
+	    if ("/".equals(tok)) {
+	      if (n < 4 || seenStar || !en.hasMoreTokens()) {
+		throw new MalformedException("illegal cidr notation", s);
+	      }
+	      tok = en.nextToken();
+	      cidr = Integer.parseInt(tok);
+	      if (cidr < 0 || cidr > 32) {
+		throw new MalformedException("illegal cidr notation", s);
+	      }
+	      if (en.hasMoreTokens()) {
+		throw new MalformedException("junk at end", s);
+	      }
+	    }
+	  }
+	}
+      } catch (NumberFormatException e) {
+	throw new MalformedException("illegal number", s);
+      }
+      if (n != 4) {
+	throw new MalformedException("must have 4 bytes", s);
+      }
+      mask = (cidr == 32) ? -1 : -(1 << (32 - cidr));
+      if (mask != -1 && !maskOk) {
+	throw new MalformedException("mask not allowed", s);
+      }
+      if ((addr & ~mask) != 0) {
+	throw new MalformedException("illegal cidr notation", s);
+      }
+    }
+
+    /** Return true if obj equal this.
+     */
+    public boolean equals(Object obj) {
+      if (obj instanceof Mask) {
+	Mask ip = (Mask)obj;
+	return (addr == ip.addr) && (mask == ip.mask);
+      }
+      return false;
+    }
+
+    /** Return true if ip is equal to this, or if either ip or this is a
+     * mask that matches the other.
+     */
+    public boolean match(Mask ip) {
+      if (ip.mask == -1) {
+	return (ip.addr & mask) == addr;
+      } else if (mask == -1) {
+	return ip.match(this);
+      } else {
+	// both have mask, treat as equals()
+	return equals(ip);
+      }
+    }
+
+    /** Return string representation
+     */
+    public String toString () {
+      StringBuffer sb = new StringBuffer(19);
+      for (int i = 24; i >= 0; i -= 8) {
+	sb.append(Integer.toString((addr >>> i) & 0xff));
+	if (i > 0) {
+	  sb.append(".");
+	}
+      }
+      if (mask != -1) {
+	sb.append("/");
+	sb.append(Integer.toString(cidr));
+      }
+      return sb.toString();
+    }
+  }
+
+  public static class Addr extends Mask {
+    /**
+     * Constructor for single IP address (no mask)
+     */
+    public Addr(String ipAddr) throws MalformedException {
+      super(ipAddr, false);
+    }
+  }
+
+
+  public static class MalformedException extends Exception {
+    public MalformedException (String msg, String ip) {
+      super(msg + ": \"" + ip + "\"");
+    }
+  }
+
+}
