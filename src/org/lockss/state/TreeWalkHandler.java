525131912ed581cb01b832da28c13a63f149ca7e
eaalto <claire@lockss.org>
2003-04-18 21:39:33 +0000

    Log changes.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@1277 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/state/TreeWalkHandler.java
+++ src/org/lockss/state/TreeWalkHandler.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TreeWalkHandler.java,v 1.19 2003-04-18 21:34:45 aalto Exp $
+ * $Id: TreeWalkHandler.java,v 1.20 2003-04-18 21:39:33 aalto Exp $
  */
 
 /*
@@ -253,7 +253,8 @@ public class TreeWalkHandler {
     long lastTreeWalkTime = manager.getAuState().getLastTreeWalkTime();
     long timeSinceLastTW = TimeBase.msSince(lastTreeWalkTime);
     logger.debug3(timeSinceLastTW+" since last treewalk");
-    logger.debug2("Treewalks should happen every "+treeWalkInterval+"ms");
+    logger.debug3("Treewalks should happen every "+
+                  StringUtil.timeIntervalToString(treeWalkInterval));
     return treeWalkInterval - timeSinceLastTW;
   }
 
@@ -272,7 +273,7 @@ public class TreeWalkHandler {
    * Starts the treewalk process.
    */
   public void start() {
-    logger.debug3("TreeWalkHandler started.");
+    logger.debug2("TreeWalkHandler started.");
     treeWalkAborted = false;
     if (treeWalkThread==null) {
       treeWalkThread = new TreeWalkThread();
