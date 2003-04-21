d2081c5c1f2e06697f2a5eed148f6c74e91723d7
tlipkis <tal@pss.com>
2003-04-21 05:40:58 +0000

    Print duration as an interval.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@1295 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/util/Deadline.java
+++ src/org/lockss/util/Deadline.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Deadline.java,v 1.20 2003-04-02 23:29:08 tal Exp $
+ * $Id: Deadline.java,v 1.21 2003-04-21 05:40:58 tal Exp $
  */
 
 /*
@@ -402,11 +402,27 @@ public class Deadline implements Comparable {
   private static final DateFormat df = DateFormat.getTimeInstance();
 
   public String toString() {
-    return "[deadline: in " + duration + ", at " +
-      (TimeBase.isSimulated()
-       ? ("sim " + expiration.getTime())
-       : df1.format(expiration))
-      + "]";
+    boolean isSim = TimeBase.isSimulated();
+    StringBuffer sb = new StringBuffer();
+    sb.append("[deadline: in ");
+    sb.append(isSim ? Long.toString(duration)
+	      : StringUtil.timeIntervalToString(duration));
+    sb.append(", at ");
+    if (isSim) {
+      sb.append("sim ");
+      sb.append(expiration.getTime());
+    } else {
+      sb.append(df1.format(expiration));
+    }
+    sb.append("]");
+    return sb.toString();
+//     String in = (isSim ? Long.toString(duration)
+// 		 : StringUtil.timeIntervalToString(duration));
+//     return "[deadline: in " + in +
+//     ", at " + (isSim
+// 	       ? ("sim " + expiration.getTime())
+// 	       : df1.format(expiration))
+// 	       + "]";
   }
 
   /**
