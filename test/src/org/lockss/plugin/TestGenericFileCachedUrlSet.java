948cdc891c90acf2cf26df4d02b090467077f440
tlipkis <tal@pss.com>
2003-05-01 20:02:39 +0000

    Cosmetic.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@1384 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/plugin/TestGenericFileCachedUrlSet.java
+++ test/src/org/lockss/plugin/TestGenericFileCachedUrlSet.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestGenericFileCachedUrlSet.java,v 1.35 2003-05-01 20:00:33 tal Exp $
+ * $Id: TestGenericFileCachedUrlSet.java,v 1.36 2003-05-01 20:02:39 tal Exp $
  */
 
 /*
@@ -429,7 +429,7 @@ public class TestGenericFileCachedUrlSet extends LockssTestCase {
     // simulate a timeout
     fileSet.storeActualHashDuration(200, new HashService.Timeout("test"));
     assertEquals(300, nodeMan.getNodeState(fileSet).getAverageHashDuration());
-    // and another,less that current estimate, should change it
+    // and another,less than current estimate, shouldn't change it
     fileSet.storeActualHashDuration(100, new HashService.Timeout("test"));
     assertEquals(300, nodeMan.getNodeState(fileSet).getAverageHashDuration());
   }
