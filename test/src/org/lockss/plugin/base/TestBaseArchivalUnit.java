c7d6ce539f4e67538ffb2bd4a9b141c711b37f54
eaalto <claire@lockss.org>
2003-05-06 20:04:10 +0000

    New test files.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@1435 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/plugin/base/TestBaseArchivalUnit.java
@@ -0,0 +1,146 @@
+/*
+ * $Id: TestBaseArchivalUnit.java,v 1.1 2003-05-06 20:04:10 aalto Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.base;
+
+import java.io.*;
+import java.util.*;
+import org.lockss.daemon.*;
+import org.lockss.test.*;
+import org.lockss.plugin.*;
+import org.lockss.util.TimeBase;
+
+/**
+ * This is the test class for org.lockss.plugin.simulated.GenericFileUrlCacher
+ *
+ * @author  Emil Aalto
+ * @version 0.0
+ */
+public class TestBaseArchivalUnit extends LockssTestCase {
+  MockBaseArchivalUnit mbau;
+
+  public void setUp() throws Exception {
+    super.setUp();
+    Properties props = new Properties();
+    props.setProperty(BaseArchivalUnit.PARAM_TOP_LEVEL_POLL_INTERVAL_MIN, "5s");
+    props.setProperty(BaseArchivalUnit.PARAM_TOP_LEVEL_POLL_INTERVAL_MAX, "10s");
+    props.setProperty(BaseArchivalUnit.PARAM_TOPLEVEL_POLL_PROB_INITIAL, "50");
+    props.setProperty(BaseArchivalUnit.PARAM_TOPLEVEL_POLL_PROB_INCREMENT, "5");
+    props.setProperty(BaseArchivalUnit.PARAM_TOPLEVEL_POLL_PROB_MAX, "85");
+    ConfigurationUtil.setCurrentConfigFromProps(props);
+
+    mbau = new MockBaseArchivalUnit(null);
+  }
+
+  public void tearDown() throws Exception {
+    TimeBase.setReal();
+    super.tearDown();
+  }
+
+  public void testGetNextPollInterval() {
+    for (int ii=0; ii<10; ii++) {
+      long nextInterval = mbau.getNextPollInterval();
+      assertTrue(nextInterval >= 5000);
+      assertTrue(nextInterval <= 10000);
+    }
+  }
+
+  public void testIncrementPollProb() {
+    assertEquals(0.15, mbau.incrementPollProb(0.10), 0.001);
+    assertEquals(0.50, mbau.incrementPollProb(0.45), 0.001);
+    // shouldn't increment past max
+    assertEquals(0.85, mbau.incrementPollProb(0.83), 0.001);
+    assertEquals(0.85, mbau.incrementPollProb(0.85), 0.001);
+  }
+
+  public void testShouldCallTopLevelPoll() throws IOException {
+    TimeBase.setSimulated(100);
+    MockAuState state = new MockAuState(mbau, -1, TimeBase.nowMs(), -1, null);
+
+    // no interval yet
+    assertEquals(-1, mbau.nextPollInterval);
+    assertEquals(-1.0, mbau.curTopLevelPollProb, 0);
+    assertFalse(mbau.shouldCallTopLevelPoll(state));
+    // should determine random interval
+    assertTrue(mbau.nextPollInterval >= 5000);
+    assertTrue(mbau.nextPollInterval <= 10000);
+    assertEquals(0.5, mbau.curTopLevelPollProb, 0.001);
+
+    // move to proper time
+    TimeBase.step(mbau.nextPollInterval);
+    boolean result = mbau.shouldCallTopLevelPoll(state);
+    // should have reset interval
+    assertEquals(-1, mbau.nextPollInterval);
+    // may or may not have allowed poll
+    if (mbau.curTopLevelPollProb != -1) {
+      assertEquals(0.55, mbau.curTopLevelPollProb, 0.001);
+      assertFalse(result);
+    } else {
+      assertEquals(-1.0, mbau.curTopLevelPollProb, 0);
+      assertTrue(result);
+    }
+
+    TimeBase.setReal();
+  }
+
+  public static void main(String[] argv) {
+    String[] testCaseList = { TestBaseArchivalUnit.class.getName()};
+    junit.swingui.TestRunner.main(testCaseList);
+  }
+
+  private static class MockBaseArchivalUnit extends BaseArchivalUnit {
+    public MockBaseArchivalUnit(Plugin myPlugin) {
+      super(myPlugin);
+    }
+
+    public String getName() {
+      return "MockBaseArchivalUnit";
+    }
+
+    public CachedUrlSet cachedUrlSetFactory(ArchivalUnit owner,
+                                            CachedUrlSetSpec cuss) {
+      throw new UnsupportedOperationException("Not supported.");
+    }
+
+    public CachedUrl cachedUrlFactory(CachedUrlSet owner, String url) {
+      throw new UnsupportedOperationException("Not supported.");
+    }
+
+    public UrlCacher urlCacherFactory(CachedUrlSet owner, String url) {
+      throw new UnsupportedOperationException("Not supported.");
+    }
+
+    public List getNewContentCrawlUrls() {
+      throw new UnsupportedOperationException("Not supported.");
+    }
+  }
+}
---
+++ test/src/org/lockss/plugin/base/TestBaseUrlCacher.java
@@ -0,0 +1,110 @@
+/*
+ * $Id: TestBaseUrlCacher.java,v 1.1 2003-05-06 20:04:10 aalto Exp $
+ */
+
+/*
+
+Copyright (c) 2002 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.base;
+
+import java.io.*;
+import java.util.Properties;
+import org.lockss.plugin.*;
+import org.lockss.test.*;
+
+/**
+ * This is the test class for org.lockss.plugin.simulated.GenericFileUrlCacher
+ *
+ * @author  Emil Aalto
+ * @version 0.0
+ */
+public class TestBaseUrlCacher extends LockssTestCase {
+  MockBaseUrlCacher cacher;
+
+  public void setUp() throws Exception {
+    super.setUp();
+    cacher = new MockBaseUrlCacher(new MockCachedUrlSet("test url"),
+                                   "test url");
+  }
+
+  public void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  public void testCache() throws IOException {
+    cacher.input = new StringInputStream("test stream");
+    cacher.headers = null;
+    try {
+      cacher.cache();
+      fail("Should have thrown CachingException.");
+    } catch (BaseUrlCacher.CachingException ce) { }
+    assertFalse(cacher.wasStored);
+
+    cacher.input = null;
+    cacher.headers = new Properties();
+    try {
+      cacher.cache();
+      fail("Should have thrown CachingException.");
+    } catch (BaseUrlCacher.CachingException ce) { }
+    assertFalse(cacher.wasStored);
+
+    cacher.input = new StringInputStream("test stream");
+    cacher.headers = new Properties();
+    cacher.cache();
+    assertTrue(cacher.wasStored);
+
+  }
+
+  public static void main(String[] argv) {
+    String[] testCaseList = { TestBaseUrlCacher.class.getName()};
+    junit.swingui.TestRunner.main(testCaseList);
+  }
+
+  private static class MockBaseUrlCacher extends BaseUrlCacher {
+    InputStream input = null;
+    Properties headers = null;
+    boolean wasStored = false;
+
+    public MockBaseUrlCacher(CachedUrlSet owner, String url) {
+      super(owner, url);
+    }
+
+    public InputStream getUncachedInputStream() {
+      return input;
+    }
+
+    public Properties getUncachedProperties() {
+      return headers;
+    }
+
+    public void storeContent(InputStream input, Properties headers) {
+      wasStored = true;
+    }
+  }
+
+}
