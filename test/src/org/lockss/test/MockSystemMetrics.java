8bbe7eddd663f78abfd15adb74c9c43c70a1eee4
eaalto <claire@lockss.org>
2003-09-23 07:43:35 +0000

    Mock extension of SystemMetrics.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@1980 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/test/MockSystemMetrics.java
@@ -0,0 +1,68 @@
+/*
+* $Id: MockSystemMetrics.java,v 1.1 2003-09-23 07:43:35 eaalto Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+package org.lockss.test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.security.MessageDigest;
+import org.lockss.daemon.*;
+
+/**
+ * Mock override of SystemMetrics.  Mainly useful for setting the hash speed
+ * to avoid test failure on slow machines.
+ */
+public class MockSystemMetrics extends SystemMetrics {
+  private int hashSpeed = -1;
+
+  public MockSystemMetrics() {
+    super();
+  }
+
+  public void setHashSpeed(int newSpeed) {
+    hashSpeed = newSpeed;
+  }
+
+  public int getHashSpeed() {
+    return hashSpeed;
+  }
+
+  public int measureHashSpeed(CachedUrlSetHasher hasher, MessageDigest digest)
+      throws IOException {
+    if (hashSpeed >= 0) {
+      return hashSpeed;
+    } else {
+      return super.measureHashSpeed(hasher, digest);
+    }
+  }
+
+
+}
