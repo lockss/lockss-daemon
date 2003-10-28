51785bc95bcd07c1928912e2c843e61265fff982
eaalto <claire@lockss.org>
2003-10-28 23:44:26 +0000

    Compile fix.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2063 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/test/RunHashTest.java
+++ test/src/org/lockss/test/RunHashTest.java
@@ -1,5 +1,5 @@
 /*
- * $Id: RunHashTest.java,v 1.3 2003-08-21 19:28:40 eaalto Exp $
+ * $Id: RunHashTest.java,v 1.4 2003-10-28 23:44:26 eaalto Exp $
  */
 
 /*
@@ -28,7 +28,7 @@ package org.lockss.test;
 
 import java.io.*;
 import org.lockss.util.*;
-import org.lockss.crawler.HtmlTagFilter;
+import org.lockss.filter.HtmlTagFilter;
 import org.lockss.plugin.CachedUrl;
 
 public class RunHashTest {
