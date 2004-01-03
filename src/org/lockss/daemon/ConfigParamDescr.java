046a12e9e0150d85304165f164c8680c2ed09fc4
tlipkis <tal@pss.com>
2004-01-03 06:16:01 +0000

    Added equals(), hashCode(), so can put in sets.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2258 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/daemon/ConfigParamDescr.java
+++ src/org/lockss/daemon/ConfigParamDescr.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ConfigParamDescr.java,v 1.5 2003-12-23 00:29:35 tlipkis Exp $
+ * $Id: ConfigParamDescr.java,v 1.6 2004-01-03 06:16:01 tlipkis Exp $
  */
 
 /*
@@ -190,4 +190,26 @@ public class ConfigParamDescr implements Comparable {
     return getDisplayName().compareTo(od.getDisplayName());
   }
 
+  public String toString() {
+    StringBuffer sb = new StringBuffer(40);
+    sb.append("[CPD: key: ");
+    sb.append(getKey());
+    sb.append("]");
+    return sb.toString();
+  }
+
+  public boolean equals(Object o) {
+    if (! (o instanceof ConfigParamDescr)) {
+      return false;
+    }
+    ConfigParamDescr opd = (ConfigParamDescr)o;
+    return type == opd.getType() && size == opd.getSize() &&
+      key.equals(opd.getKey());
+  }
+
+  public int hashCode() {
+    int hash = 0x46600555;
+    hash += key.hashCode();
+    return hash;
+  }
 }
---
+++ test/src/org/lockss/daemon/TestConfigParamDescr.java
@@ -0,0 +1,100 @@
+/*
+ * $Id: TestConfigParamDescr.java,v 1.1 2004-01-03 06:16:01 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.daemon;
+
+import java.util.*;
+import org.lockss.test.*;
+import org.lockss.util.*;
+
+/**
+ * This is the test class for org.lockss.daemon.ConfigParamDescr
+ */
+
+public class TestConfigParamDescr extends LockssTestCase {
+  ConfigParamDescr d1;
+  ConfigParamDescr d2;
+
+  public void setUp() {
+    d1 = new ConfigParamDescr("key1");
+    d2 = new ConfigParamDescr("key2");
+  }
+
+  public void testAccessors() {
+    ConfigParamDescr d1 = new ConfigParamDescr("k1");
+    assertEquals("k1", d1.getKey());
+    d1.setKey("k2");
+    assertEquals("k2", d1.getKey());
+    assertEquals("k2", d1.getDisplayName());
+    d1.setKey("foob");
+    assertEquals("foob", d1.getDisplayName());
+    assertNull(d1.getDescription());
+    d1.setDescription("ddd");
+    assertEquals("ddd", d1.getDescription());
+    d1.setType(3);
+    d1.setSize(47);
+    assertEquals(3, d1.getType());
+    assertEquals(47, d1.getSize());
+  }
+
+  public void testCompareTo() {
+    ConfigParamDescr d1 = new ConfigParamDescr("cc");
+    ConfigParamDescr d2 = new ConfigParamDescr("dd");
+    assertTrue(d1.compareTo(d2) < 0);
+    d2.setDisplayName("bb");
+    assertTrue(d1.compareTo(d2) > 0);
+    d1.setKey("bb");
+    assertEquals(0, d1.compareTo(d2));
+  }
+
+  public void testEquals() {
+    ConfigParamDescr d1 = new ConfigParamDescr("k1");
+    ConfigParamDescr d2 = new ConfigParamDescr(new String("k1"));
+    ConfigParamDescr d3 = new ConfigParamDescr("k2");
+    assertEquals(d1, d1);
+    assertEquals(d1, d2);
+    assertNotEquals(d1, d3);
+    d2.setType(3);
+    assertNotEquals(d1, d2);
+    d2 = new ConfigParamDescr(new String("k1"));
+    assertEquals(d1, d2);
+    d2.setSize(10);
+    assertNotEquals(d1, d2);
+  }
+
+  public void testHash() {
+    ConfigParamDescr d1 = new ConfigParamDescr("foo");
+    ConfigParamDescr d2 = new ConfigParamDescr("foo");
+    assertEquals(d1.hashCode(), d2.hashCode());
+  }
+
+}
