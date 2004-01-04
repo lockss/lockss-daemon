ea52cba6506d0cffb31c2c7becc564de5d940e9e
tlipkis <tal@pss.com>
2004-01-04 06:18:35 +0000

    APIs for remote agents.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2285 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/remote/RemoteApi.java
@@ -0,0 +1,256 @@
+/*
+ * $Id: RemoteApi.java,v 1.1 2004-01-04 06:18:35 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+n
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.remote;
+
+import java.io.*;
+import java.util.*;
+import org.lockss.app.*;
+import org.lockss.daemon.*;
+import org.lockss.plugin.*;
+import org.lockss.util.*;
+import org.apache.commons.collections.ReferenceMap;
+
+/**
+ * API for use by UIs and other remote agents.  Provides access to a
+ * variety of daemon status and services using only datastructure-like
+ * classes that are easy to serialize.
+ */
+public class RemoteApi extends BaseLockssManager {
+  private static Logger log = Logger.getLogger("RemoteApi");
+
+  private PluginManager pluginMgr;
+
+  // cache for proxy objects
+  private ReferenceMap auProxies = new ReferenceMap(ReferenceMap.WEAK,
+						    ReferenceMap.WEAK);
+  private ReferenceMap pluginProxies = new ReferenceMap(ReferenceMap.WEAK,
+							ReferenceMap.WEAK);
+
+  public RemoteApi() {
+  }
+
+  public void startService() {
+    super.startService();
+    pluginMgr = getDaemon().getPluginManager();
+  }
+
+  /** No config */
+  protected void setConfig(Configuration config, Configuration oldConfig,
+			   Set changedKeys) {
+  }
+
+  /** Create or return an AuProxy for the AU corresponding to the auid.
+   * @param auid the auid
+   * @return an AuProxy for the AU, or null if no AU exists with the given
+   * id.
+   */
+  public AuProxy findAuProxy(String auid) {
+    return findAuProxy(getAuFromId(auid));
+  }
+
+  /** Create or return an AuProxy for the AU
+   * @param au the AU
+   * @return an AuProxy for the AU, or null if the au is null
+   */
+  synchronized AuProxy findAuProxy(ArchivalUnit au) {
+    if (au == null) {
+      return null;
+    }
+    AuProxy aup = (AuProxy)auProxies.get(au);
+    if (aup == null || aup.getAu() != au) {
+      aup = new AuProxy(au, this);
+      auProxies.put(au, aup);
+    }
+    return aup;
+  }
+
+  /** Create or return a PluginProxy for the Plugin corresponding to the id.
+   * @param pluginid the plugin id
+   * @return a PluginProxy for the Plugin, or null if no Plugin exists with
+   * the given id.
+   */
+  public PluginProxy findPluginProxy(String pluginid) {
+    return findPluginProxy(getPluginFromId(pluginid));
+  }
+
+  /** Create or return  PluginProxy for the Plugin
+   * @param plugin the Plugin
+   * @return an PluginProxy for the Plugin, or null if the plugin is null
+   */
+  synchronized PluginProxy findPluginProxy(Plugin plugin) {
+    if (plugin == null) {
+      return null;
+    }
+    PluginProxy pluginp = (PluginProxy)pluginProxies.get(plugin);
+    if (pluginp == null || pluginp.getPlugin() != plugin) {
+      pluginp = new PluginProxy(plugin, this);
+      pluginProxies.put(plugin, pluginp);
+    }
+    return pluginp;
+  }
+
+  /** Find or create an AuProxy for each au in the collection */
+  List mapAusToProxies(Collection aus) {
+    List res = new ArrayList();
+    for (Iterator iter = aus.iterator(); iter.hasNext(); ) {
+      AuProxy aup = findAuProxy((ArchivalUnit)iter.next());
+      res.add(aup);
+    }
+    return res;
+  }
+
+  /** Find or create a PluginProxy for each Plugin in the collection */
+  List mapPluginsToProxies(Collection plugins) {
+    List res = new ArrayList();
+    for (Iterator iter = plugins.iterator(); iter.hasNext(); ) {
+      PluginProxy pluginp = findPluginProxy((Plugin)iter.next());
+      res.add(pluginp);
+    }
+    return res;
+  }
+
+  // Forward useful PluginManager methods, translating between real objects
+  // and proxies as appropriate.
+
+  /**
+   * Convert plugin id to key suitable for property file.  Plugin id is
+   * currently the same as plugin class name, but that may change.
+   * @param id the plugin id
+   * @return String the plugin key
+   */
+  public static String pluginKeyFromId(String id) {
+    return PluginManager.pluginKeyFromId(id);
+  }
+
+  /**
+   * Reconfigure an AU and save the new configuration in the local config
+   * file.
+   * @param aup the AuProxy
+   * @param auConf the new AU configuration, using simple prop keys (not
+   * prefixed with org.lockss.au.<i>auid</i>)
+   * @throws ArchivalUnit.ConfigurationException
+   * @throws IOException
+   */
+  public void setAndSaveAuConfiguration(AuProxy aup,
+					Configuration auConf)
+      throws ArchivalUnit.ConfigurationException, IOException {
+    ArchivalUnit au = aup.getAu();
+    pluginMgr.setAndSaveAuConfiguration(au, auConf);
+  }
+
+  /**
+   * Create an AU and save its configuration in the local config
+   * file.
+   * @param pluginp the PluginProxy in which to create the AU
+   * @param auConf the new AU configuration, using simple prop keys (not
+   * prefixed with org.lockss.au.<i>auid</i>)
+   * @return the new AuProxy
+   * @throws ArchivalUnit.ConfigurationException
+   * @throws IOException
+   */
+  public AuProxy createAndSaveAuConfiguration(PluginProxy pluginp,
+					     Configuration auConf)
+      throws ArchivalUnit.ConfigurationException, IOException {
+    Plugin plugin = pluginp.getPlugin();
+    ArchivalUnit au = pluginMgr.createAndSaveAuConfiguration(plugin, auConf);
+    return findAuProxy(au);
+  }
+
+  /**
+   * Delete AU configuration from the local config file.
+   * @param aup the AuProxy
+   * @throws ArchivalUnit.ConfigurationException
+   * @throws IOException
+   */
+  public void deleteAuConfiguration(AuProxy aup)
+      throws ArchivalUnit.ConfigurationException, IOException {
+    ArchivalUnit au = aup.getAu();
+    pluginMgr.deleteAuConfiguration(au);
+  }
+
+  /**
+   * Deactivate an AU
+   * @param aup the AuProxy
+   * @throws ArchivalUnit.ConfigurationException
+   * @throws IOException
+   */
+  public void deactivateAuConfiguration(AuProxy aup)
+      throws ArchivalUnit.ConfigurationException, IOException {
+    ArchivalUnit au = aup.getAu();
+    pluginMgr.deactivateAuConfiguration(au);
+  }
+
+  /**
+   * Return the stored config info for an AU (from config file, not from
+   * AU instance).
+   * @param auid the id of the AU to be deactivated
+   * @return the AU's Configuration, with unprefixed keys.
+   */
+  public Configuration getStoredAuConfiguration(AuProxy aup) {
+    ArchivalUnit au = aup.getAu();
+    return pluginMgr.getStoredAuConfiguration(au.getAuId());
+  }
+
+  /**
+   * Return a list of AuProxies for all configured ArchivalUnits.
+   * @return the List of AuProxies
+   */
+  public List getAllAus() {
+    return mapAusToProxies(pluginMgr.getAllAus());
+  }
+
+  /** Return all the known titles from the title db */
+  public Collection findAllTitles() {
+    return pluginMgr.findAllTitles();
+  }
+
+  /** Find all the plugins that support the given title */
+  public Collection getTitlePlugins(String title) {
+    return mapPluginsToProxies(pluginMgr.getTitlePlugins(title));
+  }
+
+  /** @return Collection of PluginProxies for all plugins that have been
+   * registered.  <i>Ie</i>, that are either listed in
+   * org.lockss.plugin.registry, or were loaded by a configured AU */
+  public Collection getRegisteredPlugins() {
+    return mapPluginsToProxies(pluginMgr.getRegisteredPlugins());
+  }
+
+  ArchivalUnit getAuFromId(String auid) {
+    return pluginMgr.getAuFromId(auid);
+  }
+
+  Plugin getPluginFromId(String pluginid) {
+    return pluginMgr.getPlugin(pluginKeyFromId(pluginid));
+  }
+}
---
+++ test/src/org/lockss/remote/TestRemoteApi.java
@@ -0,0 +1,137 @@
+/*
+ * $Id: TestRemoteApi.java,v 1.1 2004-01-04 06:18:35 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.remote;
+
+import java.io.*;
+import java.util.*;
+import junit.framework.*;
+import org.lockss.daemon.*;
+import org.lockss.plugin.*;
+import org.lockss.poller.*;
+import org.lockss.util.*;
+import org.lockss.test.*;
+import org.lockss.repository.*;
+
+/**
+ * Test class for org.lockss.remote.RemoteApi
+ */
+public class TestRemoteApi extends LockssTestCase {
+
+  static final String AUID1 = "AUID_1";
+  static final String PID1 = "PID_1";
+
+  MockLockssDaemon daemon;
+  MockPluginManager mpm;
+  RemoteApi rapi;
+
+  public void setUp() throws Exception {
+    super.setUp();
+
+    daemon = getMockLockssDaemon();
+    mpm = new MockPluginManager();
+    mpm.mockInit();
+    daemon.setPluginManager(mpm);
+    rapi = new RemoteApi();
+    daemon.setRemoteApi(rapi);
+    daemon.setDaemonInited(true);
+    rapi.initService(daemon);
+    rapi.startService();
+  }
+
+  public void tearDown() throws Exception {
+    rapi.stopService();
+    daemon.stopDaemon();
+    super.tearDown();
+  }
+
+  public void testFindAuProxy() {
+    MockArchivalUnit mau1 = new MockArchivalUnit();
+    AuProxy aup = rapi.findAuProxy(mau1);
+    assertNotNull(aup);
+    assertSame(mau1, aup.getAu());
+    assertSame(aup, rapi.findAuProxy(mau1));
+    ArchivalUnit mau2 = mpm.getAuFromId(AUID1);
+    assertNotNull(mau2);
+    AuProxy aup2b = rapi.findAuProxy(mau2);
+    AuProxy aup2a = rapi.findAuProxy(AUID1);
+    assertNotNull(aup2a);
+    assertSame(aup2a, aup2b);
+  }
+
+  public void testMapAus() {
+    MockArchivalUnit mau1 = new MockArchivalUnit();
+    ArchivalUnit mau2 = mpm.getAuFromId(AUID1);
+    List mapped = rapi.mapAusToProxies(ListUtil.list(mau1, mau2));
+    assertEquals(2, mapped.size());
+    assertNotNull(mapped.get(0));
+    assertNotNull(mapped.get(1));
+    assertSame(rapi.findAuProxy(mau1), (AuProxy)mapped.get(0));
+    assertSame(rapi.findAuProxy(mau2), (AuProxy)mapped.get(1));
+  }
+
+  public void testFindPluginProxy() {
+    MockPlugin mp1 = new MockPlugin();
+    PluginProxy pp1 = rapi.findPluginProxy(mp1);
+    assertNotNull(pp1);
+    assertSame(mp1, pp1.getPlugin());
+    assertSame(pp1, rapi.findPluginProxy(mp1));
+    Plugin mp2 = mpm.getPlugin(mpm.pluginKeyFromId(PID1));
+    assertNotNull(mp2);
+    PluginProxy pp2b = rapi.findPluginProxy(mp2);
+    PluginProxy pp2a = rapi.findPluginProxy(PID1);
+    assertNotNull(pp2a);
+    assertSame(pp2a, pp2b);
+  }
+
+  public void testMapPlugins() {
+    MockPlugin mp1 = new MockPlugin();
+    Plugin mp2 = mpm.getPlugin(mpm.pluginKeyFromId(PID1));
+    List mapped = rapi.mapPluginsToProxies(ListUtil.list(mp1, mp2));
+    assertEquals(2, mapped.size());
+    assertNotNull(mapped.get(0));
+    assertNotNull(mapped.get(1));
+    assertSame(rapi.findPluginProxy(mp1), (PluginProxy)mapped.get(0));
+    assertSame(rapi.findPluginProxy(mp2), (PluginProxy)mapped.get(1));
+  }
+
+  class MockPluginManager extends PluginManager {
+    void mockInit() {
+      MockArchivalUnit mau1 = new MockArchivalUnit();
+      mau1.setAuId(AUID1);
+      putAuInMap(mau1);
+      MockPlugin mp1 = new MockPlugin();
+      mp1.setPluginId(PID1);
+      setPlugin(pluginKeyFromId(mp1.getPluginId()), mp1);
+    }
+  }
+}
