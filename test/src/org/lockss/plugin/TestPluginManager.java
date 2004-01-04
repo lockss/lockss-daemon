e1b3217223a8f2be36bb6725f9073a243d6c4932
tlipkis <tal@pss.com>
2004-01-04 06:23:20 +0000

    Removed unneeded imports.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2289 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/plugin/TestPluginManager.java
+++ test/src/org/lockss/plugin/TestPluginManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestPluginManager.java,v 1.27 2004-01-03 06:19:23 tlipkis Exp $
+ * $Id: TestPluginManager.java,v 1.28 2004-01-04 06:23:20 tlipkis Exp $
  */
 
 /*
@@ -33,7 +33,6 @@ in this Software without prior written authorization from Stanford University.
 package org.lockss.plugin;
 
 import java.io.*;
-import java.net.*;
 import java.util.*;
 import junit.framework.*;
 import org.lockss.daemon.*;
@@ -42,7 +41,6 @@ import org.lockss.poller.*;
 import org.lockss.util.*;
 import org.lockss.test.*;
 import org.lockss.repository.*;
-import org.lockss.poller.PollSpec;
 
 /**
  * Test class for org.lockss.plugin.PluginManager
