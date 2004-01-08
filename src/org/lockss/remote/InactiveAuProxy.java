d6c374711f6089b9fad4b9e0321660ccfc175205
tlipkis <tal@pss.com>
2004-01-08 22:42:35 +0000

    Proxy for an inactive AU.  Allows clients compatible access to name and config.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2299 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/remote/InactiveAuProxy.java
@@ -0,0 +1,115 @@
+/*
+ * $Id: InactiveAuProxy.java,v 1.1 2004-01-08 22:42:35 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.remote;
+
+import java.io.*;
+import java.util.*;
+import org.lockss.app.*;
+import org.lockss.daemon.*;
+import org.lockss.plugin.*;
+import org.lockss.util.*;
+
+/**
+ * Proxy object for inactive AU, for compatible access to name and config
+ * from UI code.  Throws on attempts to access info only available from a
+ * running AU */
+public class InactiveAuProxy extends AuProxy {
+  private String auid;
+
+  /** Create an InactiveAuProxy for the AU with the given ID.
+   * @param auid the AU ID string.
+   * @param remoteApi the RemoteApi service
+   */
+  public InactiveAuProxy(String auid, RemoteApi remoteApi) {
+    super(remoteApi);
+    this.auid = auid;
+  }
+
+  /** @throw UnsupportedOperationException */
+  ArchivalUnit getAu() {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Return the AU's current configuration.
+   * @return a Configuration
+   */
+  public Configuration getConfiguration() {
+    return getRemoteApi().getStoredAuConfiguration(this);
+  }
+
+  /**
+   * Returns the {@link PluginProxy} for the {@link Plugin} to which this
+   * AU belongs
+   * @return the plugin
+   */
+  public PluginProxy getPlugin() {
+    RemoteApi rmt = getRemoteApi();
+    return rmt.findPluginProxy(rmt.pluginIdFromAuId(getAuId()));
+  }
+
+  /**
+   * Returns the plugin's ID
+   * @return the plugin's ID
+   */
+  public String getPluginId() {
+    PluginProxy pluginp = getPlugin();
+    return pluginp.getPluginId();
+  }
+
+  /**
+   * Returns the AU's ID
+   * @return the AU's ID
+   */
+  public String getAuId() {
+    return auid;
+  }
+
+  /**
+   * Returns a human-readable name for the ArchivalUnit.
+   * @return the AU name
+   */
+  public String getName() {
+    Configuration config = getConfiguration();
+    String name = config.get(PluginManager.AU_PARAM_DISPLAY_NAME);
+    if (StringUtil.isNullString(name)) {
+      return auid;
+    }
+    return name;
+  }
+
+  /** @return false */
+  public boolean isActiveAu() {
+    return false;
+  }
+}
---
+++ test/src/org/lockss/remote/TestInactiveAuProxy.java
@@ -0,0 +1,83 @@
+/*
+ * $Id: TestInactiveAuProxy.java,v 1.1 2004-01-08 22:42:34 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.remote;
+
+import java.io.*;
+import java.util.*;
+import org.lockss.daemon.*;
+import org.lockss.plugin.*;
+import org.lockss.test.*;
+
+/**
+ * Test class for org.lockss.remote.InactiveAuProxy
+ */
+public class TestInactiveAuProxy extends LockssTestCase {
+
+  static final String AUID1 = "AUID_1";
+  static final String PID1 = "PID_1";
+
+  MockLockssDaemon daemon;
+  MockRemoteApi mrapi;
+  MockArchivalUnit mau;
+
+  public void setUp() throws Exception {
+    super.setUp();
+    daemon = getMockLockssDaemon();
+    mrapi = new MockRemoteApi();
+    daemon.setRemoteApi(mrapi);
+    daemon.setDaemonInited(true);
+    mau = new MockArchivalUnit();
+  }
+
+  public void tearDown() throws Exception {
+    daemon.stopDaemon();
+    super.tearDown();
+  }
+
+  public void testIsActiveAu() {
+    AuProxy aup = new InactiveAuProxy("idid", mrapi);
+    assertFalse(aup.isActiveAu());
+  }
+
+  class MockRemoteApi extends RemoteApi {
+    Map aumap = new HashMap();
+
+    ArchivalUnit getAuFromId(String auid) {
+      return (ArchivalUnit)aumap.get(auid);
+    }
+
+    void setAuFromId(String auid, ArchivalUnit au) {
+      aumap.put(auid, au);
+    }
+  }
+}
