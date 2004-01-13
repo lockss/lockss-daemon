d45d9fe9ede675475b856171466191f231a32dab
troberts <claire@lockss.org>
2004-01-13 00:45:48 +0000

    moved HTML parsing functionality to org.lockss.crawler.GoslingHtmlParser
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2335 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/crawler/GoslingCrawlerImpl.java
+++ src/org/lockss/crawler/GoslingCrawlerImpl.java
@@ -1,5 +1,5 @@
 /*
- * $Id: GoslingCrawlerImpl.java,v 1.54 2004-01-10 00:53:57 troberts Exp $
+ * $Id: GoslingCrawlerImpl.java,v 1.55 2004-01-13 00:45:48 troberts Exp $
  */
 
 /*
@@ -97,20 +97,6 @@ public class GoslingCrawlerImpl implements Crawler {
    */
 
 
-  private static final String IMGTAG = "img";
-  private static final String ATAG = "a";
-  private static final String FRAMETAG = "frame";
-  private static final String LINKTAG = "link";
-  private static final String SCRIPTTAG = "script";
-  private static final String SCRIPTTAGEND = "/script";
-  private static final String BODYTAG = "body";
-  private static final String TABLETAG = "table";
-  private static final String TDTAG = "tc";
-
-  private static final String ASRC = "href";
-  private static final String SRC = "src";
-  private static final String BACKGROUNDSRC = "background";
-
   private static Logger logger = Logger.getLogger("GoslingCrawlerImpl");
 
   private ArchivalUnit au;
@@ -130,6 +116,8 @@ public class GoslingCrawlerImpl implements Crawler {
   private AuState aus = null;
 
   private Set failedUrls = new HashSet();
+
+  private GoslingHtmlParser htmlParser = null;
 
   private static final String PARAM_RETRY_TIMES =
     Configuration.PREFIX + "GoslingCrawlerImpl.numCacheRetries";
@@ -191,6 +179,7 @@ public class GoslingCrawlerImpl implements Crawler {
     this.type = type;
     this.aus = aus;
     crawlStatus = new Crawler.Status(au, spec.getStartingUrls(), type);
+    htmlParser = new GoslingHtmlParser(au);
   }
 
   private GoslingCrawlerImpl(ArchivalUnit au, CrawlSpec spec,
@@ -202,42 +191,13 @@ public class GoslingCrawlerImpl implements Crawler {
     crawlStatus = new Crawler.Status(au, repairUrls, type);
   }
 
-//   public long getNumFetched() {
-//     return numUrlsFetched;
-//   }
-
-//   public long getNumParsed() {
-//     return numUrlsParsed;
-//   }
-
-//   public long getStartTime() {
-//     return startTime;
-//   }
-
-//   public long getEndTime() {
-//     return endTime;
-//   }
-
   public ArchivalUnit getAu() {
     return au;
   }
 
-//   public Collection getStartUrls() {
-//     return spec.getStartingUrls();
-//   }
-
   public int getType() {
     return type;
   }
-
-//   public int getStatus() {
-//     if (endTime == -1) {
-//       return Crawler.STATUS_INCOMPLETE;
-//     } else if (crawlError != 0) {
-//       return crawlError;
-//     }
-//     return Crawler.STATUS_SUCCESSFUL;
-//   }
 
   public Crawler.Status getStatus() {
     return crawlStatus;
@@ -265,7 +225,6 @@ public class GoslingCrawlerImpl implements Crawler {
     }
     boolean windowClosed = false;
     logger.info("Beginning crawl of "+au);
-//     startTime = TimeBase.nowMs();
     crawlStatus.signalCrawlStarted();
     CachedUrlSet cus = au.getAuCachedUrlSet();
     Set parsedPages = new HashSet();
@@ -301,10 +260,8 @@ public class GoslingCrawlerImpl implements Crawler {
         }
  	if (spec.isIncluded(url)) {
 	  if (!doCrawlLoop(url, extractedUrls, parsedPages, cus, true, true)) {
-//   	    if (crawlError == 0) {
 	    if (crawlStatus.getCrawlError() == 0) {
 	      crawlStatus.setCrawlError(Crawler.STATUS_ERROR);
-// 	      crawlError = Crawler.STATUS_ERROR;
 	    }
 	  }
 	} else {
@@ -339,10 +296,8 @@ public class GoslingCrawlerImpl implements Crawler {
         break;
       }
       if (!doCrawlLoop(nextUrl, urlsToCrawl, parsedPages, cus, false, false)) {
-// 	if (crawlError == 0) {
 	if (crawlStatus.getCrawlError() == 0) {
 	  crawlStatus.setCrawlError(Crawler.STATUS_ERROR);
-// 	  crawlError = Crawler.STATUS_ERROR;
 	}
       }
       aus.updatedCrawlUrls(false);
@@ -350,9 +305,7 @@ public class GoslingCrawlerImpl implements Crawler {
     // unsuccessful crawl if window closed
     if (windowClosed) {
       crawlStatus.setCrawlError(Crawler.STATUS_WINDOW_CLOSED);
-//       crawlError = Crawler.STATUS_WINDOW_CLOSED;
     }
-//     if (crawlError != 0) {
     if (crawlStatus.getCrawlError() != 0) {
       logger.info("Finished crawl (errors) of "+au);
     } else {
@@ -402,11 +355,9 @@ public class GoslingCrawlerImpl implements Crawler {
     } catch (IOException ex) {
       logger.warning("Exception reading manifest: "+ex);
       crawlStatus.setCrawlError(Crawler.STATUS_FETCH_ERROR);
-//       crawlError = Crawler.STATUS_FETCH_ERROR;
     }
     if (!crawl_ok) {
       crawlStatus.setCrawlError(Crawler.STATUS_FETCH_ERROR);
-//       crawlError = err;
     }
     return crawl_ok;
   }
@@ -424,8 +375,8 @@ public class GoslingCrawlerImpl implements Crawler {
    * @return true if there were no errors
    */
   protected boolean doCrawlLoop(String url, Collection extractedUrls,
-			     Set parsedPages, CachedUrlSet cus,
-			     boolean overWrite, boolean reparse) {
+				Set parsedPages, CachedUrlSet cus,
+				boolean overWrite, boolean reparse) {
     int error = 0;
     logger.debug2("Dequeued url from list: "+url);
     Plugin plugin = au.getPlugin();
@@ -468,10 +419,15 @@ public class GoslingCrawlerImpl implements Crawler {
 	//XXX quick fix; if statement should be removed when we rework
 	//handling of error condition
 	if (cu.hasContent()) {
-	  addUrlsToSet(cu, extractedUrls, parsedPages);//IOException if the CU can't be read
+	  ContentParser parser = getContentParser(cu);
+	  if (parser != null) {
+	    //IOException if the CU can't be read
+	    parser.parseForUrls(cu, extractedUrls,
+				new MyUrlCheckCallback(parsedPages, au));
+	  }
+
  	  crawlStatus.signalUrlParsed();
 	  parsedPages.add(uc.getUrl());
-// 	  numUrlsParsed++;
 	}
       }
     } catch (IOException ioe) {
@@ -481,6 +437,34 @@ public class GoslingCrawlerImpl implements Crawler {
     }
     logger.debug2("Removing from list: "+uc.getUrl());
     return (error == 0);
+  }
+
+  private ContentParser getContentParser(CachedUrl cu) {
+    Properties props = cu.getProperties();
+    if (props != null) {
+      String contentType = props.getProperty("content-type");
+      if (contentType == null) {
+	return null;
+      } 
+      if (contentType.toLowerCase().startsWith("text/html")) {
+	return htmlParser;
+      }
+    }
+    return null;
+  }
+
+  protected static boolean isSupportedUrlProtocol(String url) {
+    try {
+//       URL ur = new URL(srcUrl, url);
+      URL ur = new URL(url);
+      // some 1.4 machines will allow this, so we explictly exclude it for now.
+      if (StringUtil.getIndexIgnoringCase(ur.toString(), "https") != 0) {
+        return true;
+      }
+    }
+    catch (Exception ex) {
+    }
+    return false;
   }
 
   private void cacheWithRetries(UrlCacher uc, int type, int maxRetries)
@@ -523,282 +507,31 @@ public class GoslingCrawlerImpl implements Crawler {
     }
   }
 
-
-  /**
-   * Method which will parse the html file represented by cu and add all
-   * urls in it which should be cached to set
-   *
-   * @param cu object representing a html file in the local file system
-   * @param set set to which all the urs in cu should be added
-   * @param urlsToIgnore urls which should not be added to set
-   * @throws IOException
-   */
-  protected void addUrlsToSet(CachedUrl cu, Collection set, Set urlsToIgnore)
-      throws IOException {
-    if (shouldExtractLinksFromCachedUrl(cu)) {
-      String cuStr = cu.getUrl();
-      if (cuStr == null) {
-	logger.error("CachedUrl has null getUrl() value: "+cu);
-	return;
-      }
-
-      InputStream is = cu.openForReading();
-      // set the reader to our default encoding
-      //XXX try to extract encoding from source
-      Reader reader = new InputStreamReader(is, Constants.DEFAULT_ENCODING); //should do this elsewhere
-      URL srcUrl = new URL(cuStr);
-      logger.debug2("Extracting urls from srcUrl");
-      String nextUrl = null;
-      while ((nextUrl = extractNextLink(reader, srcUrl)) != null) {
-	logger.debug2("Extracted "+nextUrl);
-
-	//should check if this is something we should cache first
- 	if (!set.contains(nextUrl)
-            && isSupportedUrlProtocol(srcUrl,nextUrl)
-	    && !urlsToIgnore.contains(nextUrl)
-	    && au.shouldBeCached(nextUrl)) {
-	  set.add(nextUrl);
-	}
-      }
-    }
-  }
-
-  /**
-   * Determine if this is a CachedUrl that we can parse for new urls; currently
-   * this is just done by verifying that the "content-type" property exists
-   * and equals "text/html"
-   *
-   * @param cu CachedUrl representing the web page we may parse
-   * @return true if cu has "content-type" set to "text/html", false otherwise
-   */
-  protected static boolean shouldExtractLinksFromCachedUrl(CachedUrl cu) {
-    boolean returnVal = false;
-    Properties props = cu.getProperties();
-    if (props != null) {
-      String contentType = props.getProperty("content-type");
-      if (contentType != null) {
-	//XXX check if the string starts with this
-	returnVal = contentType.toLowerCase().startsWith("text/html");
-      }
-    }
-    if (returnVal) {
-      logger.debug2("I should try to extract links from "+cu);
-    } else {
-      logger.debug2("I shouldn't try to extract links from "+cu);
-    }
-
-    return returnVal;
-  }
-
-  protected static boolean isSupportedUrlProtocol(URL srcUrl, String url) {
-    try {
-      URL ur = new URL(srcUrl, url);
-      // some 1.4 machines will allow this, so we explictly exclude it for now.
-      if (StringUtil.getIndexIgnoringCase(ur.toString(), "https") != 0) {
-        return true;
-      }
-    }
-    catch (Exception ex) {
-    }
-    return false;
-  }
-
-  /**
-   * Read through the reader stream, extract and return the next url found
-   *
-   * @param reader Reader object to extract the link from
-   * @param srcUrl URL object representing the page we are looking at
-   * (for resolving relative links)
-   * @return String representing the next url in reader
-   * @throws IOException
-   * @throws MalformedURLException
-   */
-  protected static String extractNextLink(Reader reader, URL srcUrl)
-      throws IOException, MalformedURLException {
-    if (reader == null) {
-      return null;
-    }
-    boolean inscript = false; //FIXME or I will break when we look at scripts
-    String nextLink = null;
-    int c = 0;
-    StringBuffer lineBuf = new StringBuffer();
-
-    while(nextLink == null && c >=0) {
-      //skip to the next tag
-      do {
-	c = reader.read();
-      } while (c >= 0 && c != '<');
-
-      if (c == '<') {
-	int pos = 0;
-	c = reader.read();
-	while (c >= 0 && c != '>') {
-	  if(pos==2 && c=='-' && lineBuf.charAt(0)=='!'
-	     && lineBuf.charAt(1)=='-') {
-	    // we're in a HTML comment
-	    pos = 0;
-	    int lc1 = 0;
-	    int lc2 = 0;
-	    while((c = reader.read()) >= 0
-		  && (c != '>' || lc1 != '-' || lc2 != '-')) {
-	      lc1 = lc2;
-	      lc2 = c;
-	    }
-	    break;
-	  }
-	  lineBuf.append((char)c);
-	  pos++;
-	  c = reader.read();
-	}
-	if (inscript) {
-	  //FIXME when you deal with the script problems
-	  //	  if(lookingAt(lineBuf, 0, pos, scripttagend)) {
-	  inscript = false;
-	  //}
-	} else if (lineBuf.length() >= 5) { //see if the lineBuf has a link tag
-	  nextLink = parseLink(lineBuf, srcUrl);
-	}
-	lineBuf = new StringBuffer();
-      }
-    }
-    return nextLink;
-  }
-
-
-  private static boolean beginsWithTag(String s1, String tag) {
-    if (StringUtil.getIndexIgnoringCase(s1, tag) == 0) {
-      int len = tag.length();
-      if (s1.length() > len && Character.isWhitespace(s1.charAt(len))) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-
-  /**
-   * Method to take a link tag, and parse out the URL it points to, returning
-   * a string representation of the url (lifted and rewritten from the Gosling
-   * crawler)
-   *
-   * @param link StringBuffer containing the text of a link tag (everything
-   * between < and > (ie, "a href=http://www.test.org")
-   * @param srcUrl URL object representing the page on which this
-   * url was taken from (for resolving relative tags)
-   * @return string representation of the url from the link tag
-   * @throws MalformedURLException
-   */
-  protected static String parseLink(StringBuffer link, URL srcUrl)
-      throws MalformedURLException {
-    String returnStr = null;
-
-    switch (link.charAt(0)) {
-      case 'a': //<a href=http://www.yahoo.com>
-      case 'A':
-        if (beginsWithTag(link.toString(),ATAG)) {
-          returnStr = getAttributeValue(ASRC, link.toString());
-        }
-        break;
-      case 'f': //<frame src=frame1.html>
-      case 'F':
-        if (beginsWithTag(link.toString(),FRAMETAG)) {
-          returnStr = getAttributeValue(SRC, link.toString());
-        }
-        break;
-      case 'i': //<img src=image.gif>
-      case 'I':
-        if (beginsWithTag(link.toString(),IMGTAG)) {
-          returnStr = getAttributeValue(SRC, link.toString());
-        }
-        break;
-      case 'l': //<link href=blah.css>
-      case 'L':
-        if (beginsWithTag(link.toString(),LINKTAG)) {
-          returnStr = getAttributeValue(ASRC, link.toString());
-        }
-        break;
-      case 'b': //<body backgroung=background.gif>
-      case 'B':
-        if (beginsWithTag(link.toString(),BODYTAG)) {
-          returnStr = getAttributeValue(BACKGROUNDSRC, link.toString());
-        }
-        break;
-      case 's': //<script src=blah.js>
-      case 'S':
-        if (beginsWithTag(link.toString(),SCRIPTTAG)) {
-          returnStr = getAttributeValue(SRC, link.toString());
-        }
-        break;
-      case 't': //<tc background=back.gif> or <table background=back.gif>
-      case 'T':
-        if (beginsWithTag(link.toString(),TABLETAG) ||
-          beginsWithTag(link.toString(),TDTAG)) {
-          returnStr = getAttributeValue(BACKGROUNDSRC, link.toString());
-        }
-        break;
-      default:
-        return null;
-    }
-    if (returnStr != null) {
-      returnStr = StringUtil.trimAfterChars(returnStr, " #\"");
-      if (!isSupportedUrlProtocol(srcUrl, returnStr)) {
-        logger.debug("skipping unsupported url " + returnStr);
-      }
-      else {
-        logger.debug2("Generating url from: " + srcUrl + " and " + returnStr);
-        URL retUrl = new URL(srcUrl, returnStr);
-        returnStr = retUrl.toString();
-      }
-      logger.debug2("Parsed: " + returnStr);
-     return returnStr;
-    }
-    return null;
-  }
-
-  private static String getAttributeValue(String attribute, String src) {
-    logger.debug2("looking for "+attribute+" in "+src);
-//  we need to allow for all whitespace in our tokenizer;
-    StringTokenizer st = new StringTokenizer(src, "\n\t\r =\"", true);
-    String lastToken = null;
-    while (st.hasMoreTokens()) {
-      String token = st.nextToken();
-      if (!token.equals("=")) {
-	if (!token.equals(" ") && !token.equals("\"")) {
-	  lastToken = token;
-	}
-      } else {
-        if (attribute.equalsIgnoreCase(lastToken))
-          while (st.hasMoreTokens()) {
-            token = st.nextToken();
-            // we need to allow for arguments in the url which use '='
-            if (!token.equals(" ") && !token.equals("\"")) {
-              StringBuffer sb = new StringBuffer(token);
-              while (st.hasMoreTokens() &&
-                     !token.equals(" ") && !token.equals("\"")) {
-                token = st.nextToken();
-                sb.append(token);
-              }
-              return sb.toString();
-            }
-          }
-
-//	if (attribute.equalsIgnoreCase(lastToken))
-//	  while (st.hasMoreTokens()) {
-//	    token = st.nextToken();
-//	    if (!token.equals(" ") && !token.equals("\"")) {
-//	      return token;
-//	    }
-//	  }
-      }
-    }
-    return null;
-  }
-
   public String toString() {
     StringBuffer sb = new StringBuffer();
     sb.append("[GoslingCrawlerImpl: ");
     sb.append(au.toString());
     sb.append("]");
     return sb.toString();
+  }
+
+  private static class MyUrlCheckCallback
+    implements ContentParser.UrlCheckCallback {
+    Set parsedPages = null;
+    ArchivalUnit au = null;
+
+    public MyUrlCheckCallback(Set parsedPages, ArchivalUnit au) {
+      this.parsedPages = parsedPages;
+      this.au = au;
+    }
+    
+    /**
+     * Check that we should cache this url and haven't already parsed it
+     */
+    public boolean shouldCacheUrl(String url) {
+      return (isSupportedUrlProtocol(url)
+	      && !parsedPages.contains(url)
+	      && au.shouldBeCached(url));
+    }
   }
 }
