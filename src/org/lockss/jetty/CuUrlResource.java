d1d1e5dcf1e703d509817a554e8a9c0dd1785ec2
tlipkis <tal@pss.com>
2004-01-22 02:07:40 +0000

    Use UrlUtil.equalUrls() instead of URL.equals()
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2404 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/jetty/CuUrlResource.java
+++ src/org/lockss/jetty/CuUrlResource.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CuUrlResource.java,v 1.2 2003-06-20 22:34:51 claire Exp $
+ * $Id: CuUrlResource.java,v 1.3 2004-01-22 02:07:40 tlipkis Exp $
  */
 
 /*
@@ -36,6 +36,7 @@ import java.io.*;
 import java.net.*;
 import java.security.Permission;
 import org.mortbay.util.*;
+import org.lockss.util.*;
 
 /** URLResource tailored to LOCKSS CuUrls
  */
@@ -92,7 +93,7 @@ public class CuUrlResource extends URLResource {
   }
 
   public boolean equals( Object o) {
-    return o instanceof CuUrlResource &&
-      _url.equals(((CuUrlResource)o)._url);
+    return (o instanceof CuUrlResource) &&
+      UrlUtil.equalUrls(_url,((CuUrlResource)o)._url);
   }
 }
