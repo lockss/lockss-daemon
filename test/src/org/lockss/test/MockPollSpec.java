e6ebe7ecb6431f3e158289d182de67e41faf35c6
tlipkis <tal@pss.com>
2004-01-31 23:03:40 +0000

    Mock PollSpec for testing, creates CUS, AU and plugin as needed.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2454 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/test/MockPollSpec.java
@@ -0,0 +1,128 @@
+/*
+ * $Id: MockPollSpec.java,v 1.1 2004-01-31 23:03:40 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.test;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import org.lockss.util.*;
+import org.lockss.daemon.*;
+import org.lockss.plugin.*;
+import org.lockss.poller.*;
+import org.lockss.protocol.*;
+import org.lockss.app.*;
+import org.lockss.test.*;
+
+/**
+ * MockPollSpec can be created from an AUID, will create mock AU and Plugin
+ * as necessary
+ */
+
+public class MockPollSpec extends PollSpec {
+  private static Logger log = Logger.getLogger("MockPollSpec");
+
+  private String overridePluginVersion = null;
+
+  /**
+   * Constructor for a "mock" poll spec, for debugging
+   * @param auId the archival unit id
+   * @param url the url
+   * @param lwrBound the lower bound of the url
+   * @param uprBound the upper bound of the url
+   * @param cus the cached url set
+   */
+  public MockPollSpec(String auId, String url,
+		      String lwrBound, String uprBound) {
+    this(auId, url, lwrBound, uprBound, null);
+  }
+
+  public MockPollSpec(ArchivalUnit au, String url,
+		      String lwrBound, String uprBound) {
+    super(makeCus(au, url, lwrBound, uprBound, null),
+	  lwrBound, uprBound);
+  }
+
+  public MockPollSpec(String auId, String url,
+		      String lwrBound, String uprBound, String pluginVersion) {
+    super(makeCus(auId, url, lwrBound, uprBound, pluginVersion),
+	  lwrBound, uprBound);
+  }
+
+  public String getPluginVersion() {
+    return (overridePluginVersion != null)
+      ? overridePluginVersion : super.getPluginVersion();
+  }
+
+  public void setPluginVersion(String version) {
+    overridePluginVersion = version;
+  }
+
+  private static CachedUrlSet makeCus(String auId, String url,
+				      String lwrBound, String uprBound,
+				      String pluginVersion) {
+    MockArchivalUnit au = new MockArchivalUnit();
+    au.setAuId(auId);
+    return makeCus(au, url, lwrBound, uprBound, pluginVersion);
+  }
+
+  private static CachedUrlSet makeCus(ArchivalUnit au, String url,
+				      String lwrBound, String uprBound,
+				      String pluginVersion) {
+    Plugin plug = au.getPlugin();
+    if (plug == null) {
+      log.warning("Making plugin");
+      if (au instanceof MockArchivalUnit) {
+	MockArchivalUnit mau = (MockArchivalUnit)au;
+	MockPlugin mplug = new MockPlugin();
+	mau.setPlugin(mplug);
+	plug = mplug;
+      } else {
+	throw
+	  new RuntimeException("Can't add plugin to a non MockArchivalUnit");
+      }
+    }
+    if (pluginVersion != null) {
+      if (plug instanceof MockPlugin) {
+	((MockPlugin)plug).setVersion(pluginVersion);
+      } else {
+	throw
+	  new RuntimeException("Can't add plugin version to a non MockPlugin");
+      }
+    }
+    CachedUrlSet cus =
+      plug.makeCachedUrlSet(au, new RangeCachedUrlSetSpec(url,
+							  lwrBound,
+							  uprBound));
+    return cus;
+  }
+}
