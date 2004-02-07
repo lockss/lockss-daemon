fe985ca0886df59d73abc9654c8e4bfa20feb3a7
troberts <claire@lockss.org>
2004-02-07 00:12:52 +0000

    implemented storeIdentityAgreement and loadIdentityAgreement to throw UnsupportedOperationExceptions (Emil will fill in)
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2482 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/state/HistoryRepository.java
+++ src/org/lockss/state/HistoryRepository.java
@@ -1,5 +1,5 @@
 /*
- * $Id: HistoryRepository.java,v 1.8 2004-02-03 02:48:39 eaalto Exp $
+ * $Id: HistoryRepository.java,v 1.9 2004-02-07 00:12:52 troberts Exp $
  */
 
 /*
@@ -32,6 +32,7 @@ in this Software without prior written authorization from Stanford University.
 
 package org.lockss.state;
 
+import java.util.List;
 import org.lockss.plugin.*;
 import org.lockss.app.LockssAuManager;
 
@@ -66,6 +67,16 @@ public interface HistoryRepository extends LockssAuManager {
    * @param nodeState the NodeState
    */
   public void loadPollHistories(NodeState nodeState);
+
+  /**
+   * Stores IdentityAgreement list
+   */
+  public void storeIdentityAgreement(List list);
+
+  /**
+   * Loads and returns IdentityAgreement list
+   */
+  public List loadIdentityAgreement();
 
   /**
    * Stores the AuState for this ArchivalUnit
