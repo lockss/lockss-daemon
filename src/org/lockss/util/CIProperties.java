8a1e82691583a047f1301d8289af2563e5644d0f
tlipkis <tal@pss.com>
2004-03-09 22:58:42 +0000

    Case-insensitive Properties.  Lowercases all keys.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2660 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/util/CIProperties.java
@@ -0,0 +1,105 @@
+/*
+ * $Id: CIProperties.java,v 1.1 2004-03-09 22:58:42 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+import java.util.*;
+
+import org.mortbay.util.StringMap;
+
+import java.io.Externalizable;
+// import java.util.AbstractMap;
+// import java.util.Collections;
+// import java.util.HashMap;
+// import java.util.HashSet;
+// import java.util.Map;
+// import java.util.Set;
+
+/** Properties with case-independent string keys. Canonicalizes all keys
+ * by lowercasing them.
+*/
+public class CIProperties extends Properties {
+  /** Create a case-independent Properties. 
+   */
+  public CIProperties() {
+  }
+    
+  /** Factory to create from a Properties */
+  public static CIProperties fromProperties(Properties props) {
+    CIProperties res = new CIProperties();
+    for (Iterator iter = props.keySet().iterator(); iter.hasNext(); ) {
+      String key = (String)iter.next();
+      res.setProperty(key, props.getProperty(key));
+    }
+    return res;
+  }
+
+  private String keyObj(Object obj) {
+    return obj.toString().toLowerCase();
+  }
+
+  private String keyObj(String obj) {
+    return obj.toLowerCase();
+  }
+
+  public Object put(Object key, Object value) {
+    return super.put(keyObj(key), value);
+  }
+        
+  public Object put(String key, Object value) {
+    return super.put(keyObj(key), value);
+  }
+    
+  public Object get(Object key) {
+    return super.get(keyObj(key));
+  }
+    
+  public Object get(String key) {
+    return super.get(keyObj(key));
+  }
+    
+  public String getProperty(String key) {
+    return super.getProperty(keyObj(key));
+  }
+
+  public Object remove(Object key) {
+    return super.remove(keyObj(key));
+  }
+    
+  public Object remove(String key) {
+    return super.remove(keyObj(key));
+  }
+
+  public boolean containsKey(String key) {
+    return super.containsKey(keyObj(key));
+  }
+    
+}
---
+++ test/src/org/lockss/util/TestCIProperties.java
@@ -0,0 +1,95 @@
+/*
+ * $Id: TestCIProperties.java,v 1.1 2004-03-09 22:58:42 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+
+import junit.framework.TestCase;
+import java.io.*;
+import java.util.*;
+import java.lang.reflect.*;
+import org.lockss.util.*;
+import org.lockss.test.*;
+
+/**
+ * This is the test class for org.lockss.util.CIProperties
+ */
+public class TestCIProperties extends LockssTestCase {
+  CIProperties p;
+
+  public void setUp() throws Exception {
+    p = new CIProperties();
+  }
+
+  public void testIsEmpty() {
+    assertTrue(p.isEmpty());
+    p.put("foo", "bar");
+    assertFalse(p.isEmpty());
+    p.remove("foo");
+    assertTrue(p.isEmpty());
+  }
+
+  public void testIgn() {
+    p.put("foo", "bar");
+    assertEquals("bar", p.get("foo"));
+    assertEquals("bar", p.get("FOO"));
+    p.put("FOO", "123");
+    assertEquals("123", p.get("foo"));
+    assertEquals("123", p.get("Foo"));
+    assertEquals("123", p.get("FOO"));
+    p.remove("fOo");
+    assertTrue(p.isEmpty());
+  }
+
+  public void testIgn2() {
+    p.setProperty("foo", "bar");
+    assertEquals("bar", p.getProperty("foo"));
+    assertEquals("bar", p.getProperty("FOO"));
+    p.setProperty("FOO", "123");
+    assertEquals("123", p.getProperty("foo"));
+    assertEquals("123", p.getProperty("Foo"));
+    assertEquals("123", p.getProperty("FOO"));
+    p.remove("fOo");
+    assertTrue(p.isEmpty());
+  }
+
+  public void testFromProps() {
+    Properties props = new Properties();
+    props.setProperty("foo", "123");
+    props.setProperty("BAR", "456");
+    CIProperties t = CIProperties.fromProperties(props);
+    assertEquals("123", t.getProperty("foo"));
+    assertEquals("123", t.getProperty("FOO"));
+    assertEquals("456", t.getProperty("Bar"));
+    assertEquals("456", t.getProperty("BAR"));
+    assertEquals("456", t.getProperty("bar"));
+  }
+}
