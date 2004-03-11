9d03324999b57453001e7e06554177f2f540fffa
tlipkis <tal@pss.com>
2004-03-11 10:34:49 +0000

    Ensure close permissions page input stream.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2704 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/crawler/CrawlerImpl.java
+++ src/org/lockss/crawler/CrawlerImpl.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CrawlerImpl.java,v 1.14 2004-03-11 09:39:47 tlipkis Exp $
+ * $Id: CrawlerImpl.java,v 1.15 2004-03-11 10:34:49 tlipkis Exp $
  */
 
 /*
@@ -156,24 +156,28 @@ public abstract class CrawlerImpl implements Crawler {
           // check for the permission on the page without storing
           InputStream is =
 	    new BufferedInputStream(uc.getUncachedInputStream());
-	  // allow us to reread contents if reasonable size
-	  is.mark(PERM_BUFFER_MAX);
-          // set the reader to our default encoding
-          //XXX try to extract encoding from source
-          Reader reader =
-	    new InputStreamReader(is, Constants.DEFAULT_ENCODING);
-          crawl_ok = au.checkCrawlPermission(reader);
-          if (!crawl_ok) {
-            logger.error("Couldn't start crawl due to missing permission.");
-          } else {
-            logger.debug2("Permission granted. Caching permission page.");
-	    try {
-	      is.reset();
-	      uc.storeContent(is, uc.getUncachedProperties());
-	    } catch (IOException e) {
-	      uc.cache();
+	  try {
+	    // allow us to reread contents if reasonable size
+	    is.mark(PERM_BUFFER_MAX);
+	    // set the reader to our default encoding
+	    //XXX try to extract encoding from source
+	    Reader reader =
+	      new InputStreamReader(is, Constants.DEFAULT_ENCODING);
+	    crawl_ok = au.checkCrawlPermission(reader);
+	    if (!crawl_ok) {
+	      logger.error("Couldn't start crawl due to missing permission.");
+	    } else {
+	      logger.debug2("Permission granted. Caching permission page.");
+	      try {
+		is.reset();
+		uc.storeContent(is, uc.getUncachedProperties());
+	      } catch (IOException e) {
+		uc.cache();
+	      }
 	    }
-          }
+	  } finally {
+	    is.close();
+	  }
         } else {
           logger.debug("Couldn't start crawl due to crawl window restrictions.");
           err = Crawler.STATUS_WINDOW_CLOSED;
