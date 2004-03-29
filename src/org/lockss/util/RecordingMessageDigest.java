ad6b2760dd7414907fa056bff3f190ed4786898c
tlipkis <tal@pss.com>
2004-03-29 09:16:10 +0000

    Message digest that also records byte stream in a file.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2772 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/util/RecordingMessageDigest.java
@@ -0,0 +1,130 @@
+/*
+ * $Id: RecordingMessageDigest.java,v 1.1 2004-03-29 09:16:10 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+import java.io.*;
+import java.util.*;
+import java.security.*;
+
+/** Wrapper for a MessageDigest that writes the digested bytes to a file */
+public class RecordingMessageDigest extends MessageDigest {
+
+  MessageDigest dig;
+  String fileName;
+  OutputStream out;
+  long maxLen = -1;
+  long bytes = 0;
+
+  /** Create a message digest that records in the specified file */
+  public RecordingMessageDigest(MessageDigest wrapped, File file)
+      throws FileNotFoundException {
+    super("Recording digest");
+    dig = wrapped;
+    this.fileName = file.getName();
+    out = new BufferedOutputStream(new FileOutputStream(file));
+  }
+
+  /** Create a message digest that records in the specified file */
+  public RecordingMessageDigest(MessageDigest wrapped, String fileName)
+      throws FileNotFoundException {
+    this(wrapped, new File(fileName));
+  }
+
+  /** Create a message digest that records in the specified file, up to
+   * maxLen bytes */
+  public RecordingMessageDigest(MessageDigest wrapped, String fileName,
+				long maxLen)
+      throws FileNotFoundException {
+    this(wrapped, fileName);
+    this.maxLen = maxLen;
+  }
+
+  /** Create a message digest that records in the specified file, up to
+   * maxLen bytes */
+  public RecordingMessageDigest(MessageDigest wrapped, File file, long maxLen)
+      throws FileNotFoundException {
+    this(wrapped, file);
+    this.maxLen = maxLen;
+  }
+
+  protected void engineUpdate(byte input) {
+    try {
+      if (maxLen < 0 || bytes < maxLen) {
+	out.write(input);
+	bytes += 1;
+      }
+    } catch (IOException e) {
+      throw new RuntimeException("Error recording digest: " + e.toString());
+    }
+    dig.update(input);
+  }
+
+  protected void engineUpdate(byte[] input, int offset, int len){
+    try {
+      int outlen = maxLen < 0 ? len : Math.min(len, (int)(maxLen - bytes));
+      if (outlen > 0) {
+	out.write(input, offset, outlen);
+	bytes += outlen;
+      }
+    } catch (IOException e) {
+      throw new RuntimeException("Error recording digest: " + e.toString());
+    }
+    dig.update(input, offset, len);
+  }
+
+  public String toString() {
+    return "[RecordingDigest: "+ fileName +"]";
+  }
+
+  protected void engineReset() {
+    dig.reset();
+  }
+
+  protected byte[] engineDigest() {
+    try {
+      out.close();
+    } catch (IOException e) {
+      throw new RuntimeException("Error closing digest record: " + e.toString());
+    }
+    return dig.digest();
+  }
+
+  protected int engineDigest(byte[] buf, int offset, int len)
+      throws DigestException {
+    try {
+      out.close();
+    } catch (IOException e) {
+      throw new RuntimeException("Error closing digest record: " + e.toString());
+    }
+    return dig.digest(buf, offset, len);
+  }
+}
---
+++ test/src/org/lockss/util/TestRecordingMessageDigest.java
@@ -0,0 +1,104 @@
+/*
+ * $Id: TestRecordingMessageDigest.java,v 1.1 2004-03-29 09:16:00 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+
+import java.io.*;
+import java.util.*;
+import java.security.*;
+import org.lockss.test.*;
+import org.lockss.util.*;
+
+public class TestRecordingMessageDigest extends LockssTestCase {
+
+  private MessageDigest dig;
+  private RecordingMessageDigest rmd;
+  private File file;
+
+  protected void setUp() throws Exception {
+    dig = MessageDigest.getInstance("SHA-1");
+    file = FileTestUtil.tempFile("digrec");
+    rmd = new RecordingMessageDigest(newDigest(), file);
+  }
+
+  MessageDigest newDigest() throws NoSuchAlgorithmException {
+    return MessageDigest.getInstance("SHA-1");
+  }
+
+  void assertSameDigest(String input) {
+    byte[] bytes = input.getBytes();
+    dig.update(bytes);
+    rmd.update(bytes);
+    assertEquals(dig.digest(), rmd.digest());
+  }    
+
+  public void testEquiv0() {
+    assertSameDigest("");
+  }
+  public void testEquiv1() {
+    assertSameDigest("foo");
+  }
+  public void testEquiv2() {
+    assertSameDigest("sdlkjasd;fklasd;lkjasdf1239812349871234897");
+  }
+
+  public void testEquiv3() {
+    dig.update((byte)47);
+    rmd.update((byte)47);
+    assertEquals(dig.digest(), rmd.digest());
+  }
+
+  public void testRecord() throws Exception {
+    String testStr = "asdf;klsdf;lkajsdf;jl";
+    rmd.update(testStr.getBytes());
+    rmd.digest();
+    assertEquals(testStr, StringUtil.fromFile(file));
+  }
+
+  public void testMaxRecord0() throws Exception {
+    rmd = new RecordingMessageDigest(newDigest(), file, 3);
+    rmd.update((byte)'a');
+    rmd.update((byte)'b');
+    rmd.update((byte)'c');
+    rmd.update((byte)'d');
+    rmd.digest();
+    assertEquals("abc", StringUtil.fromFile(file));
+  }
+
+  public void testMaxRecord1() throws Exception {
+    rmd = new RecordingMessageDigest(newDigest(), file, 11);
+    String testStr = "asdf;klsdf;lkajsdf;jl";
+    rmd.update(testStr.getBytes());
+    rmd.digest();
+    assertEquals(testStr.substring(0,11), StringUtil.fromFile(file));
+  }
+}
