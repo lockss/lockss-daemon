4e4271fa1c30056eaeba3bb3a16beba1a68f76dc
dcfok <claire@lockss.org>
2004-05-17 20:19:58 +0000

    Improve title sorting
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2918 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/util/CatalogueOrderComparator.java
+++ src/org/lockss/util/CatalogueOrderComparator.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CatalogueOrderComparator.java,v 1.3 2004-04-29 10:15:29 tlipkis Exp $
+ * $Id: CatalogueOrderComparator.java,v 1.4 2004-05-17 20:19:58 dcfok Exp $
  */
 
 /*
@@ -49,11 +49,12 @@ public class CatalogueOrderComparator implements Comparator {
   public static final CatalogueOrderComparator SINGLETON =
     new CatalogueOrderComparator();
 
-  Map keyMap = new HashMap();
+  Map keyMap = Collections.synchronizedMap(new HashMap());
 
   public int compare(Object o1, Object o2) {
     if (!((o1 instanceof String)
-	 && (o2 instanceof String))) {
+	  && (o2 instanceof String))) {
+
       throw new IllegalArgumentException("CatalogueOrderComparator(" +
 					 o1.getClass().getName() + "," +
 					 o2.getClass().getName() + ")");
@@ -73,13 +74,15 @@ public class CatalogueOrderComparator implements Comparator {
     }
     return key;
   }    
-
+ 
   String xlate(String s) {
     s = s.trim();
+    s = deleteAll(s, PUNCTUATION);
+    s = deleteSpaceBetweenInitials(s);
     s = deleteInitial(s, "a");
     s = deleteInitial(s, "an");
     s = deleteInitial(s, "the");
-    s = deleteAll(s, PUNCTUATION);
+    s = replaceAccentedChar(s);
     return s;
   }
 
@@ -94,6 +97,33 @@ public class CatalogueOrderComparator implements Comparator {
     return s;
   }
 
+  // assume all the letter in the abbreviation are in uppercase
+  // The method will also turn consecutive whitespace in to one whitespace
+  String deleteSpaceBetweenInitials(String s) {
+    boolean deleteStart = false;
+    StringBuffer sTmp = new StringBuffer();
+    StringTokenizer st = new StringTokenizer(s," ");
+    while (st.hasMoreTokens()) {
+	String token = st.nextToken();
+        //check the token is a single uppercase character 
+	if (token.length() == 1 &&
+            Character.isUpperCase(token.charAt(0))) {
+	    if (deleteStart) {
+	      sTmp = sTmp.append(token);
+            }
+	    else { //when encounter the first Letter in the abbreviation
+	      sTmp = sTmp.append(" ").append(token);
+	      deleteStart = true;
+	    }
+	}
+	else { 
+	  sTmp = sTmp.append(" ").append(token);
+	  deleteStart = false;
+	}
+    }
+    return sTmp.toString().trim(); 
+  }
+
   String deleteAll(String s, String chars) {
     for (int ix = 0; ix < chars.length(); ix++) {
       String c = chars.substring(ix, ix + 1);
@@ -101,4 +131,73 @@ public class CatalogueOrderComparator implements Comparator {
     }
     return s;
   }
+
+    /** the accented character table except {Ð(\u00d0),Þ(\u00de),ß(\u00df), ð(\u00f0),þ(\u00fe)} */
+  static final String[][] ACCENTTABLE = {
+      {"\u00c0","A"}, // À, A with grave
+      {"\u00c1","A"}, // Á, A with acute
+      {"\u00c2","A"}, // Â, A with circumflex
+      {"\u00c3","A"}, // Â, A with tilde
+      {"\u00c4","A"}, // Ä, A with diaeresis 
+      {"\u00c5","A"}, // Å, A with ring above
+      {"\u00c6","AE"}, // Æ, AE
+      {"\u00c7","C"}, // Ç, C with cedilla 
+      {"\u00c8","E"}, // È, E with grave
+      {"\u00c9","E"}, // É, E with acute
+      {"\u00ca","E"}, // Ê, E with circumflex
+      {"\u00cb","E"}, // Ë, E with diaeresis
+      {"\u00cc","I"}, // Ì, I with grave
+      {"\u00cd","I"}, // Í, I with acute
+      {"\u00ce","I"}, // Î, I with circumflex
+      {"\u00cf","I"}, // Ï, I with diaeresis
+      {"\u00d1","N"}, // Ñ, N with tilde
+      {"\u00d2","O"}, // Ò, O with grave
+      {"\u00d3","O"}, // Ó, O with acute
+      {"\u00d4","O"}, // Ô, O with circumflex
+      {"\u00d5","O"}, // Õ, O with tilde
+      {"\u00d6","O"}, // Ö, O with diaeresis
+      {"\u00d8","O"}, // Ø, O with a stroke
+      {"\u00d9","U"}, // Ù, U with grave
+      {"\u00da","U"}, // Ú, U with acute
+      {"\u00db","U"}, // Û, U with circumflex
+      {"\u00dc","U"}, // Ü, U with diaeresis
+      {"\u00dd","Y"}, // Ý, Y with acute
+      {"\u00e0","a"}, // à, a with grave
+      {"\u00e1","a"}, // á, a with acute
+      {"\u00e2","a"}, // â, a with circumflex
+      {"\u00e3","a"}, // ã, a with tilde
+      {"\u00e4","a"}, // ä, a with diaeresis 
+      {"\u00e5","a"}, // å, a with ring above
+      {"\u00e6","ae"}, // æ, ae
+      {"\u00e7","c"}, // ç, c with cedilla
+      {"\u00e8","e"}, // è, e with grave
+      {"\u00e9","e"}, // é, e with acute
+      {"\u00ea","e"}, // ê, e with circumflex
+      {"\u00eb","e"}, // ë, e with diaeresis
+      {"\u00ec","i"}, // ì, i with grave
+      {"\u00ed","i"}, // í, i with acute
+      {"\u00ee","i"}, // î, i with circumflex
+      {"\u00ef","i"}, // ï, i with diaeresis
+      {"\u00f1","n"}, // ñ, n with tilde
+      {"\u00f2","o"}, // ò, o with grave
+      {"\u00f3","o"}, // ó, o with acute
+      {"\u00f4","o"}, // ô, o with circumflex
+      {"\u00f5","o"}, // õ, o with tilde
+      {"\u00f6","o"}, // ö, o with diaeresis
+      {"\u00f8","o"}, // ø, o with stroke
+      {"\u00f9","u"}, // ù, u with grave
+      {"\u00fa","u"}, // ú, u with acute
+      {"\u00fb","u"}, // û, u with circumflex 
+      {"\u00fc","u"}, // ü, u with diaeresis
+      {"\u00fd","y"}, // ý, y with acute
+      {"\u00ff","y"}, // ÿ, y with diaeresis
+                                        };
+
+  String replaceAccentedChar(String s) {
+    for (int iy = 0; iy < ACCENTTABLE.length; iy++) {
+      s = StringUtil.replaceString(s, ACCENTTABLE[iy][0], ACCENTTABLE[iy][1]);
+    }
+    return s;
+  }
+
 }
--- test/src/org/lockss/util/TestCatalogueOrderComparator.java
+++ test/src/org/lockss/util/TestCatalogueOrderComparator.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestCatalogueOrderComparator.java,v 1.2 2004-04-29 10:15:29 tlipkis Exp $
+ * $Id: TestCatalogueOrderComparator.java,v 1.3 2004-05-17 20:19:58 dcfok Exp $
  */
 
 /*
@@ -60,11 +60,29 @@ public class TestCatalogueOrderComparator extends LockssTestCase {
     assertEquals("foo", coc.deleteAll("fo--o", "--!"));
   }
 
+  public void testDeleteSpaceBetweenInitials() {
+    assertEquals("Journal of IBM 2004", coc.deleteSpaceBetweenInitials("Journal of I B M 2004"));
+    assertEquals("UPS manuel", coc.deleteSpaceBetweenInitials("U P S manuel"));
+    assertEquals("Journal of HP", coc.deleteSpaceBetweenInitials("Journal of H P"));
+    assertEquals("Who am I magazine", coc.deleteSpaceBetweenInitials("Who am I magazine"));
+    assertEquals("Wild Space", coc.deleteSpaceBetweenInitials("Wild         Space"));
+  }
+
+  public void testReplaceAccentedChar(){
+    assertEquals("toe", coc.replaceAccentedChar("toë"));
+    assertEquals("Apple", coc.replaceAccentedChar("Àpple"));
+    assertEquals("low", coc.replaceAccentedChar("löw"));
+    assertEquals("coupe", coc.replaceAccentedChar("coupé"));
+    assertEquals("naive", coc.replaceAccentedChar("naïve"));
+    assertEquals("garcon", coc.replaceAccentedChar("garçon"));
+  }
+
   public void testXlate() {
     assertEquals("Tao of Pooh", coc.xlate("The Tao of Pooh"));
     assertEquals("Tao of Pooh", coc.xlate("A  Tao of Pooh"));
     assertEquals("Tao of Pooh", coc.xlate("An Tao of Pooh"));
     assertEquals("IBM Tech Journal", coc.xlate("I.B.M. Tech Journal"));
+    assertEquals("Journal of IBM", coc.xlate("Journal of I. B. M."));
   }
 
   public void testOrder() {
@@ -74,9 +92,11 @@ public class TestCatalogueOrderComparator extends LockssTestCase {
       "An Apple and its Eve",
       "a boy and his bog",
       "A Boy and his Dog",
+      "Garçon Magazine",
       "IBM Tech Journak",
       "I.B.M. Tech. Journal",
       "IBM Tech Journam",
+      "Journal of I B M 2004"
     };
     List tl = ListUtil.fromArray(titles);
     Collections.reverse(tl);
@@ -97,7 +117,15 @@ public class TestCatalogueOrderComparator extends LockssTestCase {
 
   public void testCache() {
     CountingCOC ccoc = new CountingCOC();
-    List lst = ListUtil.list("1", "2", "the 3", "a 4", "an 5");
+
+    List lst = ListUtil.list("1", 
+			     "2", 
+			     "the 3", 
+			     "a 4", 
+			     "an 5", 
+			     "6 I B M Journal",
+			     " 7 Garçon Magazine");
+
     ccoc.xlateCnt = 0;
     Collections.sort(lst, ccoc);
     // should have xlated each item once
