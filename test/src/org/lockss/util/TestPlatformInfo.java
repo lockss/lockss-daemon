c1afed19bd9f51dbb21f058476167f0ae639648d
tlipkis <tal@pss.com>
2004-05-24 22:18:21 +0000

    Public interface to platform info.  Currently uses Runtime.exec()
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@2946 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/util/TestPlatformInfo.java
@@ -0,0 +1,60 @@
+/*
+ * $Id: TestPlatformInfo.java,v 1.2 2004-05-24 22:18:21 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+
+import junit.framework.TestCase;
+import java.io.*;
+import java.util.*;
+import java.lang.reflect.*;
+import org.lockss.util.*;
+import org.lockss.test.*;
+
+/**
+ * test class for org.lockss.util.PlatformInfo
+ */
+public class TestPlatformInfo extends LockssTestCase {
+  PlatformInfo info;
+
+  public void setUp() {
+    info = PlatformInfo.getInstance();
+  }
+
+  public void testDF() throws Exception {
+    PlatformInfo.DF df =
+      info.getDF(System.getProperty("java.io.tmpdir"));
+    assertNotNull(df);
+    df = info.getDF(System.getProperty("/very_unlik_elyd_irect_oryname/4x2"));
+    assertNull(df);
+  }
+  
+}
