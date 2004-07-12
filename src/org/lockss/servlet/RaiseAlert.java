f82b1a38ab0ed6acc6949fb327aeeed9f68e7f2c
tlipkis <tal@pss.com>
2004-07-12 06:19:53 +0000

    Initial revision.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@3104 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/servlet/RaiseAlert.java
@@ -0,0 +1,270 @@
+/*
+ * $Id: RaiseAlert.java,v 1.1 2004-07-12 06:18:24 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2004 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.servlet;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import java.text.*;
+import java.security.*;
+import org.mortbay.html.*;
+import org.mortbay.tools.*;
+import org.mortbay.util.B64Code;
+import org.lockss.app.*;
+import org.lockss.util.*;
+import org.lockss.alert.*;
+import org.lockss.plugin.*;
+import org.lockss.poller.*;
+import org.lockss.protocol.*;
+import org.lockss.daemon.*;
+import org.lockss.daemon.status.*;
+
+/** Raise an alert on demand.  For testing alerts
+ */
+public class RaiseAlert extends LockssServlet {
+  static final String KEY_ACTION = "action";
+  static final String KEY_NAME_SEL = "name_sel";
+  static final String KEY_NAME_TYPE = "name_type";
+  static final String KEY_AUID = "auid";
+  static final String KEY_TEXT = "text";
+
+  static final String ACTION_RAISE = "Raise";
+
+  static final String COL2 = "colspan=2";
+  static final String COL2CENTER = COL2 + " align=center";
+
+  static Logger log = Logger.getLogger("RaiseAlert");
+
+  private LockssDaemon daemon;
+  private PluginManager pluginMgr;
+  private AlertManager alertMgr;
+
+  String auid;
+  String name;
+  String text;
+  ArchivalUnit au;
+  boolean showResult;
+  private String errMsg;
+  private String statusMsg;
+
+  protected void resetLocals() {
+    resetVars();
+    super.resetLocals();
+  }
+
+  void resetVars() {
+    auid = null;
+    errMsg = null;
+    statusMsg = null;
+  }
+
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+    daemon = getLockssDaemon();
+    alertMgr = daemon.getAlertManager();
+    pluginMgr = daemon.getPluginManager();
+  }
+
+  public void lockssHandleRequest() throws IOException {
+    resetVars();
+    String action = getParameter(KEY_ACTION);
+
+    if (ACTION_RAISE.equals(action)) {
+      doRaise();
+    }
+    displayPage();
+  }
+
+  private boolean doRaise() {
+    auid = getParameter(KEY_AUID);
+    au = pluginMgr.getAuFromId(auid);
+    name = getParameter(KEY_NAME_SEL);
+    if (StringUtil.isNullString(name)) {
+      name = getParameter(KEY_NAME_TYPE);
+    }
+    text = getParameter(KEY_TEXT);
+    
+    Alert alert;
+    Alert proto = findProtoAlert(name);
+    if (proto == null) {
+      proto = new Alert(name);
+    }
+    if (au != null && (proto == null ||
+		       proto.getBool(Alert.ATTR_IS_CONTENT))) {
+      alert = Alert.auAlert(proto, au);
+    } else {
+      alert = Alert.cacheAlert(proto);
+    }
+    alert.setAttribute(Alert.ATTR_TEXT, text);
+    alertMgr.raiseAlert(alert);
+    return true;
+  }
+
+  private void displayPage() throws IOException {
+    Page page = newPage();
+    page.add(getErrBlock());
+    page.add(getExplanationBlock("Raise an Alert"));
+    page.add(makeForm());
+    page.add("<br>");
+    page.add(getFooter());
+    page.write(resp.getWriter());
+  }
+
+  void addResultRow(Table tbl, String head, Object value) {
+    tbl.newRow();
+    tbl.newCell();
+    tbl.add(head);
+    tbl.add(":");
+    tbl.newCell();
+    tbl.add(value.toString());
+  }
+
+  private Element makeForm() {
+    Composite comp = new Composite();
+    Block centeredBlock = new Block(Block.Center);
+
+    Form frm = new Form(srvURL(myServletDescr(), null));
+    frm.method("POST");
+
+    Table autbl = new Table(0, "cellpadding=0");
+    autbl.newRow();
+    autbl.addHeading("Select AU");
+    Select sel = new Select(KEY_AUID, false);
+    sel.add("", auid == null, "");
+    for (Iterator iter = pluginMgr.getAllAus().iterator(); iter.hasNext(); ) {
+      ArchivalUnit au = (ArchivalUnit)iter.next();
+      String id = au.getAuId();
+      sel.add(au.getName(), id.equals(auid), id);
+    }
+    autbl.newRow(); autbl.newCell();
+    setTabOrder(sel);
+    autbl.add(sel);
+
+    Table alrtbl = new Table(0, "cellpadding=0");
+      alrtbl.newRow();
+    alrtbl.addHeading("Select Alert Name");
+    Select sel2 = new Select(KEY_NAME_SEL, false);
+    sel2.add("", auid == null, "");
+    for (int ix = 0; ix < protoAlerts.length; ix++) {
+      Alert proto = protoAlerts[ix];
+      String aname = proto.getName();
+      sel2.add(aname, aname.equals(name), aname);
+    }
+    alrtbl.newRow(); alrtbl.newCell();
+    setTabOrder(sel2);
+    alrtbl.add(sel2);
+
+    Table tbl = new Table(0, "cellpadding=0");
+    tbl.newRow();
+    tbl.newCell(COL2CENTER);
+    tbl.add(autbl);
+    tbl.newRow();
+    tbl.newCell(COL2CENTER);
+    tbl.add(alrtbl);
+    tbl.newRow();
+    tbl.newCell();
+    tbl.add("&nbsp;");
+
+    addInputRow(tbl, "Name", KEY_NAME_TYPE, 50, name);
+    addInputRow(tbl, "Text", KEY_TEXT, 50, text);
+
+    centeredBlock.add(tbl);
+    frm.add(centeredBlock);
+    Input submit = new Input(Input.Submit, KEY_ACTION, ACTION_RAISE);
+    setTabOrder(submit);
+    frm.add("<br><center>"+submit+"</center>");
+    comp.add(frm);
+    return comp;
+  }
+
+  void addInputRow(Table tbl, String label, String key,
+		   int size, String initVal) {
+    tbl.newRow();
+//     tbl.newCell();
+    tbl.addHeading(label + ":", "align=right");
+    tbl.newCell();
+    Input in = new Input(Input.Text, key, initVal);
+    in.setSize(size);
+    setTabOrder(in);
+    tbl.add(in);
+  }
+
+  /** Create message and error message block */
+  private Composite getErrBlock() {
+    Composite comp = new Composite();
+    if (errMsg != null) {
+      comp.add("<center><font color=red size=+1>");
+      comp.add(errMsg);
+      comp.add("</font></center><br>");
+    }
+    if (statusMsg != null) {
+      comp.add("<center><font size=+1>");
+      comp.add(statusMsg);
+      comp.add("</font></center><br>");
+    }
+    return comp;
+  }
+
+  Alert findProtoAlert(String name) {
+    if (name == null) return null;
+    for (int ix = 0; ix < protoAlerts.length; ix++) {
+      Alert proto = protoAlerts[ix];
+      if (name.equals(proto.getName())) {
+	return proto;
+      }
+    }
+    return null;
+  }
+
+  Alert[] protoAlerts = {
+    Alert.PERMISSION_PAGE_FETCH_ERROR,
+    Alert.NO_CRAWL_PERMISSION,
+    Alert.NEW_CONTENT,
+    Alert.NO_NEW_CONTENT,
+    Alert.VOLUME_CLOSED,
+    Alert.PUBLISHER_UNREACHABLE,
+    Alert.PUBLISHER_CONTENT_CHANGED,
+    Alert.DAMAGE_DETECTED,
+    Alert.PERSISTENT_DAMAGE,
+    Alert.REPAIR_COMPLETE,
+    Alert.PERSISTENT_NO_QUORUM,
+    Alert.INCONCLUSIVE_POLL,
+    Alert.CACHE_DOWN,
+    Alert.CACHE_UP,
+    Alert.DISK_SPACE_LOW,
+    Alert.DISK_SPACE_FULL,
+    Alert.INTERNAL_ERROR,
+  };
+}
---
+++ src/org/lockss/servlet/TinyUi.java
@@ -0,0 +1,362 @@
+/*
+ * $Id: TinyUi.java,v 1.1 2004-07-12 06:19:53 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2004 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.servlet;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import org.lockss.app.*;
+import org.lockss.daemon.*;
+import org.lockss.util.*;
+import org.lockss.jetty.*;
+import org.mortbay.http.*;
+import org.mortbay.http.handler.*;
+import org.mortbay.jetty.servlet.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.mortbay.html.*;
+
+/**
+ * Starts a tiny context running a tiny servlet that says the cache isn't
+ * up, and displays an error mesage from the context.
+ */
+public class TinyUi extends JettyManager {
+  private static Logger log = Logger.getLogger("TinyUi");
+
+
+  public static final String UI_REALM = "LOCKSS Admin";
+  public static final String PASSWORD_PROPERTY_FILE = "admin.props";
+
+  public static final String PREFIX = Configuration.PREFIX + "ui.";
+  public static final String PARAM_START = PREFIX + "start";
+  public static final String PARAM_PORT = PREFIX + "port";
+
+  public static final String IP_ACCESS_PREFIX = PREFIX + "access.ip.";
+  public static final String PARAM_IP_INCLUDE = IP_ACCESS_PREFIX + "include";
+  public static final String PARAM_IP_EXCLUDE = IP_ACCESS_PREFIX + "exclude";
+  public static final String PARAM_LOG_FORBIDDEN =
+    IP_ACCESS_PREFIX + "logForbidden";
+  public static final boolean DEFAULT_LOG_FORBIDDEN = true;
+
+  public static final String PARAM_USER_AUTH = PREFIX + "access.auth";
+  public static final boolean DEFAULT_USER_AUTH = false;
+  public static final String PARAM_LOGDIR =
+    Configuration.PREFIX +  "platform.logdirectory";
+
+  public static final boolean DEFAULT_START = true;
+  public static final int DEFAULT_PORT = 8081;
+
+  public static final String PARAM_PLATFORM_USERNAME =
+    Configuration.PLATFORM + "ui.username";
+  public static final String PARAM_PLATFORM_PASSWORD =
+    Configuration.PLATFORM + "ui.password";
+
+
+  private HttpServer server;
+
+  private int port = DEFAULT_PORT;
+  private boolean start;
+  private String includeIps;
+  private String excludeIps;
+  private boolean doAuth = DEFAULT_USER_AUTH;
+  private HashUserRealm realm;
+  private String platUser;
+  private String platPass;
+  private String[] tinyData;
+
+  java.util.List accessHandlers = new ArrayList();
+
+  public TinyUi() {
+  }
+
+  public TinyUi(String[] tinyData) {
+    this.tinyData = tinyData;
+  }
+
+  /** Start servlets  */
+  public void startTiny() {
+    log.debug("Starting");
+    Configuration config = ConfigManager.getCurrentConfig();
+    setConfig(config, ConfigManager.EMPTY_CONFIGURATION, config.keySet());
+    startServlets();
+  }
+
+  public void stopTiny() {
+    log.debug("Stopping");
+    try {
+      if (server != null) {
+	log.debug("Stopping2");
+	server.stop();
+	server = null;
+      }
+    } catch (InterruptedException e) {
+      log.warning("Interrupted while stopping server");
+    }
+  }
+
+  protected void setConfig(Configuration config, Configuration prevConfig,
+			   Set changedKeys) {
+    super.setConfig(config, prevConfig, changedKeys);
+    port = config.getInt(PARAM_PORT, DEFAULT_PORT);
+    start = config.getBoolean(PARAM_START, DEFAULT_START);
+    doAuth = config.getBoolean(PARAM_USER_AUTH, DEFAULT_USER_AUTH);
+    platUser = config.get(PARAM_PLATFORM_USERNAME);
+    platPass = config.get(PARAM_PLATFORM_PASSWORD);
+
+    if (changedKeys.contains(PARAM_IP_INCLUDE) ||
+	changedKeys.contains(PARAM_IP_EXCLUDE) ||
+	changedKeys.contains(PARAM_LOG_FORBIDDEN)) {
+      includeIps = config.get(PARAM_IP_INCLUDE, "");
+      excludeIps = config.get(PARAM_IP_EXCLUDE, "");
+      log.debug("Installing new ip filter: incl: " + includeIps +
+		", excl: " + excludeIps);
+      setIpFilters();
+    }
+  }
+
+  void setIpFilters() {
+    for (Iterator iter = accessHandlers.iterator(); iter.hasNext(); ) {
+      setIpFilter((IpAccessHandler)iter.next());
+    }
+  }
+
+  void setIpFilter(IpAccessHandler ah) {
+    try {
+      IpFilter filter = new IpFilter();
+      filter.setFilters(includeIps, excludeIps, ';');
+      ah.setFilter(filter);
+    } catch (IpFilter.MalformedException e) {
+      log.warning("Malformed IP filter, filters not changed", e);
+    }
+    ah.setLogForbidden(true);
+    ah.setAllowLocal(true);
+  }
+
+  // Manually install password set by platform config.
+  // XXX Doesn't handle roles, will need to be integrated with daemon
+  // password setting mechanism
+  private void setConfiguredPasswords(HashUserRealm realm) {
+    if (!StringUtil.isNullString(platUser) &&
+	!StringUtil.isNullString(platPass)) {
+      realm.put(platUser, platPass);
+    }
+  }
+
+  public void startServlets() {
+    try {
+      // Create the server
+      server = new HttpServer();
+
+      // Create a port listener
+      HttpListener listener =
+	server.addListener(new org.mortbay.util.InetAddrPort(port));
+
+      // create auth realm
+      if (doAuth) {
+	try {
+	  URL propsUrl = this.getClass().getResource(PASSWORD_PROPERTY_FILE);
+	  if (propsUrl != null) {
+	    log.debug("passwd props file: " + propsUrl);
+	    realm = new HashUserRealm(UI_REALM, propsUrl.toString());
+	  }
+	} catch (IOException e) {
+	  log.warning("Error loading admin.props", e);
+	}
+	if (realm == null) {
+	  realm = new HashUserRealm(UI_REALM);
+	}
+	setConfiguredPasswords(realm);
+	if (realm.isEmpty()) {
+	  log.warning("No users created, UI is effectively disabled.");
+	}
+      }
+
+      configureTinyContexts();
+
+      // Start the http server
+      startServer();
+    } catch (Exception e) {
+      log.warning("Couldn't start servlets", e);
+    }
+  }
+
+  long[] delayTime = {10 * Constants.SECOND, 60 * Constants.SECOND, 0};
+
+  boolean startServer() {
+    try {
+      for (int ix = 0; ix < delayTime.length; ix++) {
+	try {
+	  server.start ();
+	  return true;
+	} catch (org.mortbay.util.MultiException e) {
+	  log.warning("multi", e);
+	  log.warning("first", e.getException(0));
+	  log.warning("Addr in use, sleeping " +
+		      StringUtil.timeIntervalToString(delayTime[ix]));
+	  Deadline.in(delayTime[ix]).sleep();
+	}
+      }
+    } catch (Exception e) {
+      log.warning("Couldn't start servlets", e);
+    }
+    return false;
+  }
+
+  private void setContextAuthHandler(HttpContext context, UserRealm realm) {
+    if (realm != null) {
+      context.setRealm(realm);
+      context.setAuthenticator(new BasicAuthenticator());
+      context.addHandler(new SecurityHandler());
+      context.addSecurityConstraint("/",
+				    new SecurityConstraint("Admin", "*"));
+    }
+  }
+
+  public void configureTinyContexts() {
+    try {
+      setupTinyContext();
+      setupImageContext();
+
+    } catch (Exception e) {
+      log.warning("Couldn't start admin UI contexts", e);
+    }
+  }
+
+
+
+  void setupTinyContext() throws MalformedURLException {
+    HttpContext context = makeContext("/");
+
+    // add handlers in the order they should be tried.
+
+    // user authentication handler
+//     setContextAuthHandler(context, realm);
+
+    // Create a servlet container
+    ServletHandler handler = new ServletHandler();
+
+    // Request dump servlet
+    handler.addServlet("Tiny", "/", TinyServlet.class.getName());
+    context.addHandler(handler);
+
+    // NotFoundHandler
+    context.addHandler(new NotFoundHandler());
+
+    //       context.addHandler(new DumpHandler());
+  }
+
+  void setupImageContext() throws MalformedURLException {
+    HttpContext context = makeContext("/images");
+
+    // add handlers in the order they should be tried.
+
+    // ResourceHandler for /images dir
+    // find the htdocs directory, set as resource base
+    ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    URL resourceUrl=loader.getResource("org/lockss/htdocs/images/");
+    log.debug("Images resource URL: " + resourceUrl);
+
+    context.setResourceBase(resourceUrl.toString());
+    ResourceHandler rHandler = new ResourceHandler();
+    rHandler.setDirAllowed(false);
+    context.addHandler(rHandler);
+
+    // NotFoundHandler
+    context.addHandler(new NotFoundHandler());
+  }
+
+  // common context setup
+  // adds IpAccessHandler as all contexts want it
+  // doesn't add AuthHandler as not all contexts want it
+  HttpContext makeContext(String path) {
+    HttpContext context = server.getContext(path);
+    context.setAttribute("TinyData", tinyData);
+    // In this environment there is no point in consuming memory with
+    // cached resources
+    context.setMaxCachedFileSize(0);
+
+    // IpAccessHandler is always first handler
+//     addAccessHandler(context);
+    return context;
+  }
+
+  void addAccessHandler(HttpContext context) {
+    IpAccessHandler ah = new IpAccessHandler("UI");
+//     setIpFilter(ah);
+    context.addHandler(ah);
+    accessHandlers.add(ah);
+  }
+
+
+  public static class TinyServlet extends HttpServlet {
+    private ServletContext context;
+    private String[] tinyData;
+    
+    public void init(ServletConfig config) throws ServletException {
+      super.init(config);
+      context = config.getServletContext();
+      tinyData = (String[])context.getAttribute("TinyData");
+    }
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) 
+        throws ServletException, IOException {
+      Page page= new Page();
+      page.title("LOCKSS cache");
+      page.addHeader("");
+
+      Composite b = new Font(1, true);
+      b.add("LOCKSS cache ");
+      Configuration pc = Configuration.getPlatformConfig();
+      String name = Configuration.getPlatformHostname();
+      if (name != null) {
+	b.add(" (");
+	b.add(name);
+	b.add(")");
+      }
+      b.add(" has not started because ");
+      b.add("it is unable to load configuration data.<br>");
+      if (tinyData[0] != null) {
+	b.add(tinyData[0]);
+      }
+//       page.add(new Block(Block.Bold).add(b));
+      page.add(b);
+      response.setContentType("text/html");
+      response.setHeader("Pragma", "no-cache");
+      response.setHeader("Cache-Control", "no-cache,no-store");
+      Writer writer=response.getWriter();
+      page.write(writer);
+      writer.flush();
+    }
+  }
+}
