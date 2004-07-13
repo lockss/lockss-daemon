c7092f726473ef56dc86fc665b51da53fc9b3e8f
smorabito <claire@lockss.org>
2004-07-13 00:43:18 +0000

    Oops -- forgot the configuration prefix!
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@3129 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/plugin/PluginManager.java
+++ src/org/lockss/plugin/PluginManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: PluginManager.java,v 1.87 2004-07-13 00:41:07 smorabito Exp $
+ * $Id: PluginManager.java,v 1.88 2004-07-13 00:43:18 smorabito Exp $
  */
 
 /*
@@ -85,7 +85,7 @@ public class PluginManager extends BaseLockssManager {
     "password";
 
   static String PARAM_REGISTRY_CRAWL_INTERVAL =
-    "plugin.registries.crawl.interval";
+    Configuration.PREFIX + "plugin.registries.crawl.interval";
   static String DEFAULT_REGISTRY_CRAWL_INTERVAL =
     "1d"; // not specified as a long value because this will be passed as
           // a string literal to the AU config.
