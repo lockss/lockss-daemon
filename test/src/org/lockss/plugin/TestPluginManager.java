8c4fdf80233a79042a20d71d2d90227e35bb7c3b
tlipkis <tal@pss.com>
2004-07-26 21:46:24 +0000

    Always start service, so can stop in tearDown()
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@3190 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/plugin/TestPluginManager.java
+++ test/src/org/lockss/plugin/TestPluginManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestPluginManager.java,v 1.39 2004-07-16 22:53:07 smorabito Exp $
+ * $Id: TestPluginManager.java,v 1.40 2004-07-26 21:46:24 tlipkis Exp $
  */
 
 /*
@@ -89,6 +89,7 @@ public class TestPluginManager extends LockssTestCase {
     theDaemon.setDaemonInited(true);
     mgr.setLoadablePluginsReady(true);
     mgr.initService(theDaemon);
+    mgr.startService();
   }
 
   public void tearDown() throws Exception {
@@ -98,7 +99,6 @@ public class TestPluginManager extends LockssTestCase {
   }
 
   private void doConfig() throws Exception {
-    mgr.startService();
     String tempDirPath = getTempDir().getAbsolutePath() + File.separator;
     Properties p = new Properties();
     p.setProperty(p1a1param+MockPlugin.CONFIG_PROP_1, "val1");
@@ -111,7 +111,6 @@ public class TestPluginManager extends LockssTestCase {
   }
 
   private void minimalConfig() throws Exception {
-    mgr.startService();
     String tempDirPath = getTempDir().getAbsolutePath() + File.separator;
     ConfigurationUtil.setFromArgs(LockssRepositoryImpl.PARAM_CACHE_LOCATION,
 				  tempDirPath,
@@ -509,7 +508,6 @@ public class TestPluginManager extends LockssTestCase {
   public void testWrappedAu() {
     if (!WrapperState.isUsingWrapping()) {
       try {
-        mgr.startService();
         String tempDirPath = getTempDir().getAbsolutePath() + File.separator;
         Properties p = new Properties();
         p.setProperty(p1a1param + MockPlugin.CONFIG_PROP_1, "val1");
