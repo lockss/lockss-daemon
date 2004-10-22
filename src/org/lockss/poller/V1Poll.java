e0b0e2c47e365ca8438ad0d6960c36502d628fc2
tlipkis <tal@pss.com>
2004-10-22 16:12:58 +0000

    Set pollstate *before* scheduling hash.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@3614 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/poller/V1Poll.java
+++ src/org/lockss/poller/V1Poll.java
@@ -1,5 +1,5 @@
 /*
-* $Id: V1Poll.java,v 1.19 2004-10-02 13:08:24 dshr Exp $
+* $Id: V1Poll.java,v 1.20 2004-10-22 16:12:58 tlipkis Exp $
  */
 
 /*
@@ -298,6 +298,7 @@ public abstract class V1Poll extends BasePoll {
   void startPoll() {
     if (m_pollstate != PS_INITING)
       return;
+    m_pollstate = PS_WAIT_HASH;
     if (!scheduleOurHash()) {
       m_pollstate = ERR_SCHEDULE_HASH;
       log.debug("couldn't schedule our hash:" + m_voteTime + ", stopping poll.");
@@ -306,8 +307,6 @@ public abstract class V1Poll extends BasePoll {
     }
     log.debug3("scheduling poll to complete by " + m_deadline);
     TimerQueue.schedule(m_deadline, new PollTimerCallback(), this);
-    m_pollstate = PS_WAIT_HASH;
-
   }
 
   /**
