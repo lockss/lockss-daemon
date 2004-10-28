5c019d927252fccb0254349a1d6fba5b87527780
smorabito <claire@lockss.org>
2004-10-28 07:14:06 +0000

    Content migration hack
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@3630 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/proxy/ProxyManager.java
+++ src/org/lockss/proxy/ProxyManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ProxyManager.java,v 1.28 2004-10-27 23:53:55 tlipkis Exp $
+ * $Id: ProxyManager.java,v 1.29 2004-10-28 07:13:50 smorabito Exp $
  */
 
 /*
@@ -118,6 +118,12 @@ public class ProxyManager extends BaseProxyManager {
     PREFIX + "quickTimeout.data";
   public static final long DEFAULT_PROXY_QUICK_DATA_TIMEOUT =
     5  * Constants.MINUTE;
+
+  /** Content Re-Writing Support - GIF to PNG */
+  public static final String PARAM_REWRITE_GIF_PNG =
+    PREFIX + "contentRewrite.gifToPng";
+  public static final boolean DEFAULT_REWRITE_GIF_PNG =
+    false;
 
   private boolean lockssUserAgentImpliesRepair =
     DEFAULT_LOCKSS_USER_AGENT_IMPLIES_REPAIR;
