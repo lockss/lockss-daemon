aa910851ff8a8acf3dd9d0f3b65c32c511c49ebd
troberts <claire@lockss.org>
2004-11-30 00:49:41 +0000

    url normalizer for IOP titles
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@3711 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/plugin/iop/IOPNormalizer.java
@@ -0,0 +1,51 @@
+/*
+ * $Id: IOPNormalizer.java,v 1.1 2004-11-30 00:49:41 troberts Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.iop;
+
+import org.lockss.plugin.*;
+import org.lockss.util.*;
+
+public class IOPNormalizer implements UrlNormalizer {
+  public String normalizeUrl(String url, ArchivalUnit au) {
+    if (url == null) {
+      return null;
+    }
+    StringBuffer sb = new StringBuffer(url.length());
+    sb.append(url.substring(0, StringUtil.nthIndexOf(7, url, "/")));
+    sb.append(url.substring(StringUtil.nthIndexOf(8, url, "/")));
+
+    return sb.toString();
+//     http://www.iop.org/EJ/S/3/418/SXFqtUP87MBWu6AwxR,8Hw/toc/0266-5611/20/6
+  }
+
+}
