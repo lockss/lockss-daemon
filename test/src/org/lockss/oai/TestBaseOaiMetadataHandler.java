7973a9baa3e024a40a26ad3685c71e0ec43020b6
dcfok <claire@lockss.org>
2005-01-20 01:51:57 +0000

    code clean up in TestBaseOaiMetadataHandler
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@3932 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/oai/TestBaseOaiMetadataHandler.java
+++ test/src/org/lockss/oai/TestBaseOaiMetadataHandler.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestBaseOaiMetadataHandler.java,v 1.3 2005-01-20 01:35:15 dcfok Exp $
+ * $Id: TestBaseOaiMetadataHandler.java,v 1.4 2005-01-20 01:51:57 dcfok Exp $
  */
 
 /*
@@ -90,7 +90,7 @@ public class TestBaseOaiMetadataHandler extends LockssTestCase {
 	assertEquals(urlContainerTagName, baseOaiMetadataHandler.getUrlContainerTagName());
     }
 
-    public void testGetArticleUrls(){
+    public void testExtractUrlsFromMetadataNodeList(){
 	String prefix = "abc";
 	String nsUrl = "http://www.younameit.com/";
 	String tag = "foo";
@@ -114,41 +114,19 @@ public class TestBaseOaiMetadataHandler extends LockssTestCase {
     private NodeList createMockMetadataNodeList(Set urls, String prefix, String namespaceUrl, String tagName){
       
         DocumentBuilder builder = null;
-	//        Element namespaceElement = null;
 
 	/* Load DOM Document */
 	try {
 	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 	    factory.setNamespaceAware(true);
 	    builder = factory.newDocumentBuilder();
-	    
-// 	    DOMImplementation impl = builder.getDOMImplementation();
-// 	    Document namespaceHolder
-// 		= impl.createDocument("http://www.lockss.org/oai/harvester", "harvester:namespaceHolder", null);
-// 	    namespaceElement = namespaceHolder.getDocumentElement();
-// 	    namespaceElement.setAttributeNS("http://www.w3.org/2000/xmlns/",
-// 					    "xmlns:harvester",
-// 					    "http://www.lockss.org/research/software/oai/harvester");
-// 	    namespaceElement.setAttributeNS("http://www.w3.org/2000/xmlns/",
-//                                             "xmlns:xsi",
-// 					    "http://www.w3.org/2001/XMLSchema-instance");
-// 	    namespaceElement.setAttributeNS("http://www.w3.org/2000/xmlns/",
-// 					    "xmlns:oai20",
-// 					    "http://www.openarchives.org/OAI/2.0/");
-// 	    namespaceElement.setAttributeNS("http://www.w3.org/2000/xmlns/",
-// 					    "xmlns:"+prefix,
-// 					    "http://www.example.com/"+prefix);
-// 	    namespaceElement.setAttributeNS("http://www.w3.org/2000/xmlns/",
-// 					    "xmlns:xyz",
-// 					    namespaceUrl);
-					    
 	} catch (ParserConfigurationException pce) {
 	    logger.error("", pce);
 	}
 
 
 	NodeSet nodeSet = new NodeSet();
-	// create sth like
+	// create something like
 	/*
 	  <metadata>
 	  <oai_dc:dc 
@@ -168,18 +146,13 @@ public class TestBaseOaiMetadataHandler extends LockssTestCase {
 
 	    StringBuffer xmlSBText = new StringBuffer();
 	    xmlSBText.append("<metadata>");
-	    xmlSBText.append("<"+prefix+":xyz "); //note: xyz is a namespaceUrl qualified name
+	    xmlSBText.append("<"+prefix+":xyz "); //note: xyz is a qualified namespace uri name
 	                                          //xyz is just for human read only. For the machine
-	                                          //will check the whole namespaceUrl instead of this
+	                                          //will check the namespace uri instead of this
 	                                          //qualified name.
 
 	    xmlSBText.append("xmlns:"+prefix+"=\"http://www.example.com/"+prefix+"\" ");
-//	    xmlSBText.append("xmlns:xyz=\""+namespaceUrl+"\" ");
 	    xmlSBText.append("xmlns:xyz=\""+namespaceUrl+"\">");
-
-// 	    xmlSBText.append("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ");
-// 	    xmlSBText.append("xsi:schemaLocation=\"http://www.openarchives.org/OAI/2.0/oai_dc/ ");
-// 	    xmlSBText.append("http://www.openarchives.org/OAI/2.0/oai_dc.xsd\">");
 	    xmlSBText.append("<xyz:"+tagName+">"+it.next()+"</xyz:"+tagName+">");
 	    xmlSBText.append("</"+prefix+":xyz>");
 	    xmlSBText.append("</metadata>");
@@ -194,99 +167,7 @@ public class TestBaseOaiMetadataHandler extends LockssTestCase {
 	    } catch (IOException ioe) {
 		logger.error("", ioe);
 	    } 
-// 	    catch (XmlDomException xde) {
-// 	      logger.error("", xde);
-// 	    }
-
-	    
 	}
-
-//===============================================================================
-//String docString =
-    //"<?xml version=""1.0"" encoding=""UTF-8""?> "+
-// "<OAI-PMH xmlns=""http://www.openarchives.org/OAI/2.0/"" "+
-// "         xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" "+
-// "         xsi:schemaLocation=""http://www.openarchives.org/OAI/2.0/ "+
-// "         http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd""> "+
-// "  <responseDate>2002-06-01T19:20:30Z</responseDate> "+
-// " <request verb=""ListRecords"" from=""2002-05-01T14:15:00Z"" "+
-// "          until=""2002-05-01T14:20:00Z"" metadataPrefix=""oai_dc""> "+
-// "          http://www.perseus.tufts.edu/cgi-bin/pdataprov</request> "+
-// " <ListRecords> "+
-// "  <record> "+
-// "    <header> "+
-// "      <identifier>oai:perseus:Perseus:text:1999.02.0084</identifier> "+
-// "     <datestamp>2002-05-01T14:16:12Z</datestamp> "+
-// "    </header> "+
-// "    <metadata> "+
-// "      <oai_dc:dc  "+
-// "          xmlns:oai_dc=""http://www.openarchives.org/OAI/2.0/oai_dc/""  "+
-// "          xmlns:dc=""http://purl.org/dc/elements/1.1/""  "+
-// "          xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""  "+
-// "          xsi:schemaLocation=""http://www.openarchives.org/OAI/2.0/oai_dc/  "+
-// "          http://www.openarchives.org/OAI/2.0/oai_dc.xsd""> "+
-// "        <dc:title>Opera Minora</dc:title> "+
-// "        <dc:creator>Cornelius Tacitus</dc:creator> "+
-// "        <dc:type>text</dc:type> "+
-// "        <dc:source>Opera Minora. Cornelius Tacitus. Henry Furneaux.  "+
-// "         Clarendon Press. Oxford. 1900.</dc:source> "+
-// "        <dc:language>latin</dc:language> "+
-// "        <dc:identifier>http://www.perseus.tufts.edu/cgi-bin/ptext? "+
-// "          doc=Perseus:text:1999.02.0084</dc:identifier> "+
-// "      </oai_dc:dc> "+
-// "    </metadata> "+
-// "  </record> "+
-// "  <record> "+
-// "    <header> "+
-// "      <identifier>oai:perseus:Perseus:text:1999.02.0083</identifier> "+
-// "      <datestamp>2002-05-01T14:20:55Z</datestamp> "+
-// "    </header> "+ 
-// "    <metadata> "+
-// "      <oai_dc:dc  "+
-// "          xmlns:oai_dc=""http://www.openarchives.org/OAI/2.0/oai_dc/""  "+
-// "          xmlns:dc=""http://purl.org/dc/elements/1.1/""  "+
-// "          xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""  "+
-// "          xsi:schemaLocation=""http://www.openarchives.org/OAI/2.0/oai_dc/  "+
-// "          http://www.openarchives.org/OAI/2.0/oai_dc.xsd""> "+
-// "        <dc:title>Germany and its Tribes</dc:title> "+
-// "        <dc:creator>Tacitus</dc:creator> "+
-// "        <dc:type>text</dc:type> "+
-// "        <dc:source>Complete Works of Tacitus. Tacitus. Alfred John Church.  "+
-// "         William Jackson Brodribb. Lisa Cerrato. edited for Perseus.  "+
-// "         New York: Random House, Inc. Random House, Inc. reprinted 1942. "+
-// "          </dc:source> "+
-// "        <dc:language>english</dc:language> "+
-// "        <dc:identifier>http://www.perseus.tufts.edu/cgi-bin/ptext? "+
-// "         doc=Perseus:text:1999.02.0083</dc:identifier> "+
-// "      </oai_dc:dc> "+
-//     "      </metadata> ";
-//     "  </record> ";
-// " </ListRecords> "+
-//     "</OAI-PMH> ";
-
-
-// 	try {
-// 	    XmlDomBuilder xmlDomBuilder = new XmlDomBuilder();
-// 	    Document testDoc = xmlDomBuilder.parseXmlFile("docTemp.xml");
-// 	    NodeList testNodeList = testDoc.getElementsByTagName("metadata");
-// 	    logger.debug3("Printing the testing doc....");
-// 	    for (int i=0; i<testNodeList.getLength(); i++) {
-// 		Node node = testNodeList.item(i);
-// 		if (node != null) {
-// 		    logger.debug3(nodeToString(node));
-// 		}
-// 	    }
-// 	    logger.debug3("Finish Printing the testing doc.");
-
-// 	} catch (XmlDomException xde) {
-// 	    logger.error("" ,xde);
-// 	}
-
-
-//==============================================================================
-
-
-
 	return (NodeList) nodeSet;
     }
     
