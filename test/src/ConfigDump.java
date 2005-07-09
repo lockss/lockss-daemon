b10b4b0dd504b3864addc3c1a1897e0ea919517e
tlipkis <tal@pss.com>
2005-07-09 22:01:38 +0000

    Ensure temp dir gets deleted.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@4367 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/ConfigDump.java
+++ test/src/ConfigDump.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ConfigDump.java,v 1.9 2005-06-15 01:18:35 tlipkis Exp $
+ * $Id: ConfigDump.java,v 1.10 2005-07-09 22:01:38 tlipkis Exp $
  */
 
 /*
@@ -132,8 +132,10 @@ public class ConfigDump {
   static String createLocalConfig(String hostName) {
     String result = null;
     try {
-      File localTxt = new File(FileTestUtil.createTempDir("configdump", null),
-			       "local.txt");
+      File tmpdir = FileTestUtil.createTempDir("configdump", null);
+      tmpdir.deleteOnExit();
+      File localTxt = new File(tmpdir, "local.txt");
+      localTxt.deleteOnExit();
       Properties localProps = new Properties();
       localProps.setProperty(ConfigManager.PARAM_PLATFORM_FQDN,
 			     hostName);
