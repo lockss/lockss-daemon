526ae83c64f42f6318dc8aa48d992af06ee74f16
troberts <claire@lockss.org>
2005-07-13 17:48:39 +0000

    * reimplemented as a true mock object * old MockIdentityManager is now an inner class of TestNodeManagerImpl (which still uses it)
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@4387 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/protocol/MockIdentityManager.java
+++ test/src/org/lockss/protocol/MockIdentityManager.java
@@ -1,5 +1,5 @@
 /*
-* $Id: MockIdentityManager.java,v 1.2 2005-02-02 09:42:20 tlipkis Exp $
+* $Id: MockIdentityManager.java,v 1.3 2005-07-13 17:48:39 troberts Exp $
  */
 
 /*
@@ -40,81 +40,215 @@ import org.lockss.plugin.*;
 /**
  * Mock override of IdentityManager.
  */
+
 public class MockIdentityManager extends IdentityManager {
-  public HashMap idMap = null;
+  public HashMap idMap = new HashMap();
+
+  public HashMap piMap = new HashMap();
+
+  public HashMap repMap = new HashMap();
 
   public Map agreeMap = new HashMap();
+
+  int maxRep = 5000;
 
   public MockIdentityManager() {
     super();
   }
 
   public void initService(LockssDaemon daemon) throws LockssAppException {
-    log.debug("MockIdentityManager: initService");
-    super.initService(daemon);
-  }
-
-  protected String getLocalIpParam(Configuration config) {
-    String res = config.get(PARAM_LOCAL_IP);
-    if (res == null) {
-      res = "127.7.7.7";
-    }
-    return res;
+//     throw new UnsupportedOperationException("not implemented");
   }
 
   public void startService() {
-    log.debug("MockIdentityManager: startService");
-    super.startService();
-    idMap = new HashMap();
+    throw new UnsupportedOperationException("not implemented");
   }
+
   public void stopService() {
-    log.debug("MockIdentityManager: stopService");
-    super.stopService();
-    idMap = null;
+    throw new UnsupportedOperationException("not implemented");
   }
 
-  public void changeReputation(PeerIdentity id, int changeKind) {
-    idMap.put(id, new Integer(changeKind));
+
+  public PeerIdentity findPeerIdentity(String key) {
+    throw new UnsupportedOperationException("not implemented");
   }
 
-  public int lastChange(PeerIdentity id) {
-    Integer change = (Integer)idMap.get(id);
-    if (change==null) {
-      return -1;
+  public PeerIdentity ipAddrToPeerIdentity(IPAddr addr, int port) {
+    String key = ""+addr+port;
+    return new MockPeerIdentity(key);
+  }
+
+  public PeerIdentity ipAddrToPeerIdentity(IPAddr addr) {
+    throw new UnsupportedOperationException("not implemented");
+  }
+
+  public PeerIdentity stringToPeerIdentity(String idKey)
+      throws IdentityManager.MalformedIdentityKeyException {
+    return (PeerIdentity)piMap.get(idKey);
+  }
+
+  public void addPeerIdentity(String idKey, PeerIdentity pi) {
+    piMap.put(idKey, pi);
+  }
+
+  public IPAddr identityToIPAddr(PeerIdentity pid) {
+    throw new UnsupportedOperationException("not implemented");
+  }
+
+  public PeerIdentity getLocalPeerIdentity(int pollVersion) {
+    return new MockPeerIdentity("127.0.0.1");
+  }
+
+  public IPAddr getLocalIPAddr() {
+    throw new UnsupportedOperationException("not implemented");
+  }
+
+  public boolean isLocalIdentity(PeerIdentity id) {
+     return false;
+//      throw new UnsupportedOperationException("not implemented");
+  }
+
+  public boolean isLocalIdentity(String idStr) {
+    throw new UnsupportedOperationException("not implemented");
+  }
+
+  public void rememberEvent(PeerIdentity id, int event, LcapMessage msg) {
+    throw new UnsupportedOperationException("not implemented");
+  }
+
+  public int getMaxReputation() {
+    return maxRep;
+  }
+
+  public void setMaxReputation(int maxRep) {
+    this.maxRep = maxRep;
+  }
+
+  public int getReputation(PeerIdentity id) {
+    Integer rep = (Integer)repMap.get(id);
+    if (rep == null) {
+      return 0;
     }
-    return change.intValue();
+    return rep.intValue();
   }
 
-  public void signalAgreed(PeerIdentity id, ArchivalUnit au) {
+  public void setReputation(PeerIdentity id, int rep) {
+    repMap.put(id, new Integer(rep));
+  }
+
+//   public void storeIdentities() throws ProtocolException {
+//     throw new UnsupportedOperationException("not implemented");
+//   }
+
+
+  public IdentityListBean getIdentityListBean() {
     throw new UnsupportedOperationException("not implemented");
   }
 
-  public void signalDisagreed(PeerIdentity id, ArchivalUnit au) {
+  public void signalAgreed(PeerIdentity pid, ArchivalUnit au) {
     throw new UnsupportedOperationException("not implemented");
   }
+
+  public void signalDisagreed(PeerIdentity pid, ArchivalUnit au) {
+    throw new UnsupportedOperationException("not implemented");
+  }
+
+  public Map getCachesToRepairFrom(ArchivalUnit au) {
+    throw new UnsupportedOperationException("not implemented");
+  }
+
+  public void setConfig(Configuration config, Configuration oldConfig,
+			Configuration.Differences changedKeys) {
+    throw new UnsupportedOperationException("not implemented");
+  }
+
+//   public void initService(LockssDaemon daemon) throws LockssAppException {
+//     log.debug("MockIdentityManager: initService");
+//     super.initService(daemon);
+//   }
+
+//   protected String getLocalIpParam(Configuration config) {
+//     String res = config.get(PARAM_LOCAL_IP);
+//     if (res == null) {
+//       res = "127.7.7.7";
+//     }
+//     return res;
+//   }
+
+//   public void startService() {
+//     log.debug("MockIdentityManager: startService");
+//     super.startService();
+//     idMap = new HashMap();
+//   }
+//   public void stopService() {
+//     log.debug("MockIdentityManager: stopService");
+//     super.stopService();
+//     idMap = null;
+//   }
+
+//    public void changeReputation(PeerIdentity id, int changeKind) {
+//      throw new UnsupportedOperationException("not implemented");
+// //      idMap.put(id, new Integer(changeKind));
+//    }
+
+   public int lastChange(PeerIdentity id) {
+     Integer change = (Integer)idMap.get(id);
+     if (change == null) {
+       return -1;
+     }
+     return change.intValue();
+   }
+
+//   public void signalAgreed(PeerIdentity id, ArchivalUnit au) {
+//     throw new UnsupportedOperationException("not implemented");
+//   }
+
+//   public void signalDisagreed(PeerIdentity id, ArchivalUnit au) {
+//     throw new UnsupportedOperationException("not implemented");
+//   }
 
   public Map getAgreed(ArchivalUnit au) {
     return (Map)agreeMap.get(au);
   }
   
-  /**
-   * Change the the reputation of the peer
-   * @param peer the PeerIdentity
-   * @param reputation the new reputation
-   */
-  public void setReputation(PeerIdentity peer, int reputation) {
-    try {
-      LcapIdentity lid = findLcapIdentity(peer, peer.getIdString());
-      lid.changeReputation(reputation - lid.getReputation());
-    } catch (IdentityManager.MalformedIdentityKeyException e) {
-      throw new RuntimeException(e.toString());
-    }
+   /**
+    * Change the the reputation of the peer
+    * @param peer the PeerIdentity
+    * @param reputation the new reputation
+    */
+//    public void setReputation(PeerIdentity peer, int reputation) {
+//      try {
+//        LcapIdentity lid = findLcapIdentity(peer, peer.getIdString());
+//        lid.changeReputation(reputation - lid.getReputation());
+//      } catch (IdentityManager.MalformedIdentityKeyException e) {
+//        throw new RuntimeException(e.toString());
+//      }
+//    }
+
+   public void setAgeedForAu(ArchivalUnit au, Map map) {
+     agreeMap.put(au, map);
+   }
+
+  //protected
+//   protected IdentityManagerStatus makeStatusAccessor(Map theIdentities) {
+//     throw new UnsupportedOperationException("not implemented");
+//   }
+
+//   protected LcapIdentity findLcapIdentity(PeerIdentity pid, String key) {
+//     throw new UnsupportedOperationException("not implemented");
+//   }
+
+  protected LcapIdentity findLcapIdentity(PeerIdentity pid,
+					  IPAddr addr, int port) {
+    throw new UnsupportedOperationException("not implemented");
+  }
+  protected int getReputationDelta(int changeKind) {
+    throw new UnsupportedOperationException("not implemented");
   }
 
-  public void setAgeedForAu(ArchivalUnit au, Map map) {
-    agreeMap.put(au, map);
-  }
-
+//   protected String getLocalIpParam(Configuration config) {
+//     throw new UnsupportedOperationException("not implemented");
+//   }
   
 
 }
