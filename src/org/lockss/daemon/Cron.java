c420d8bf68c6829096834d040735f124b4924614
tlipkis <tal@pss.com>
2005-09-06 19:56:55 +0000

    Simple cron-like facility.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@4607 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/daemon/Cron.java
@@ -0,0 +1,266 @@
+/*
+ * $Id: Cron.java,v 1.1 2005-09-06 19:56:55 tlipkis Exp $
+ *
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.daemon;
+import java.io.*;
+import java.util.*;
+import org.lockss.app.*;
+import org.lockss.config.*;
+import org.lockss.util.*;
+import org.lockss.remote.*;
+
+/** A rudimentary cron facility.  Tasks are added programmatically, checked
+ * at discreet interval (default one hour) to see if time to run.  Last run
+ * times are stored persistently.
+ */
+public class Cron
+  extends BaseLockssDaemonManager implements ConfigurableManager  {
+
+  protected static Logger log = Logger.getLogger("Cron");
+
+  static final String PREFIX = Configuration.PREFIX + "cron.";
+
+  /** File in config dir where last-run info is kept */
+  public static final String CONFIG_FILE_CRON_STATE = "cronstate.xml";
+
+  /** Enable Cron facility */
+  static final String PARAM_ENABLED = PREFIX + "enabled";
+  static final boolean DEFAULT_ENABLED = true;
+
+  /** Interval at which Cron wakes up to check for runnable tasks */
+  static final String PARAM_SLEEP = PREFIX + "sleep";
+  static final long DEFAULT_SLEEP = Constants.HOUR;
+
+  private File cronStateFile = null;
+  private Cron.State state;
+  private Collection tasks = new ArrayList();
+  private boolean enabled = false;
+  private long interval;
+  private TimerQueue.Request req;
+
+  public void startService() {
+    super.startService();
+    ConfigManager configMgr = getDaemon().getConfigManager();
+    installTasks();
+    cronStateFile = configMgr.getCacheConfigFile(CONFIG_FILE_CRON_STATE);
+    log.debug2("State file: " + cronStateFile);
+    loadState(cronStateFile);
+    resetConfig();
+  }
+
+  public synchronized void stopService() {
+    disable();
+    super.stopService();
+  }
+
+  public synchronized void setConfig(Configuration config,
+				     Configuration prevConfig,
+				     Configuration.Differences changedKeys) {
+
+    if (!getDaemon().isDaemonInited()) {
+      return;
+    }
+
+    boolean doEnable = config.getBoolean(PARAM_ENABLED, DEFAULT_ENABLED);
+    if (changedKeys.contains(PREFIX)) {
+      interval = config.getTimeInterval(PARAM_SLEEP, DEFAULT_SLEEP);
+    }
+    if (doEnable != enabled) {
+      if (doEnable) {
+	enable();
+      } else {
+	disable();
+      }
+      enabled = doEnable;
+    }
+  }
+
+  /** Install standard tasks */
+  void installTasks() {
+    addTask(new MailBackupFile(getDaemon()));
+  }
+
+  /** Add a task */
+  public void addTask(Cron.Task task) {
+    tasks.add(task);
+  }
+
+  /** Return state object containing last-run times; mostly for testing */
+  Cron.State getState() {
+    return state;
+  }
+
+  void loadState(File file) {
+    try {
+      state = (Cron.State)makeObjectSerializer().deserialize(file);
+    } catch (Exception e) {
+      log.warning("Couldn't load cron state, creating a new one");
+      // Default value
+      state = new Cron.State();
+    }
+  }
+
+  void storeState(File file) {
+    try {
+      makeObjectSerializer().serialize(file, state);
+    } catch (Exception e) {
+      log.error("Couldn't store cron state, disabling cron: ", e);
+      disable();
+    }
+  }
+
+  private ObjectSerializer makeObjectSerializer() {
+    return new XStreamSerializer();
+  }
+
+  private synchronized void enable() {
+    stopRunning();		      // First, ensure no current timer req
+    enabled = true;
+    log.info("Enabling");
+    schedNext();
+  }
+
+  private synchronized void disable() {
+    if (enabled) {
+      log.info("Disabling");
+    }
+    stopRunning();
+  }
+
+  private void stopRunning() {
+    enabled = false;
+    if (req != null) {
+      TimerQueue.cancel(req);
+    }
+  }
+
+  void schedNext() {
+    req = TimerQueue.schedule(Deadline.in(interval), timerCallback, null);
+  }
+
+  // TimerQueue callback.  Not inline because don't need a new instance
+  // each time.
+  private TimerQueue.Callback timerCallback =
+    new TimerQueue.Callback() {
+      public void timerExpired(Object cookie) {
+	checkTasks();
+      }
+      public String toString() {
+	return "Cron";
+      }
+    };
+
+  /** Run any tasks that need to be run.  Probably should eventually run
+   * them in another thread */
+  private synchronized void checkTasks() {
+    log.debug3("check");
+    req = null;
+    if (tasks != null && !tasks.isEmpty()) {
+      long now = TimeBase.nowMs();
+      boolean needStore = false;
+      for (Iterator iter = tasks.iterator(); iter.hasNext(); ) {
+	Cron.Task task = (Cron.Task)iter.next();
+	if (task.nextTime(state.getLastTime(task.getId())) <= now) {
+	  task.execute();
+	  state.setLastTime(task.getId(), now);
+	  needStore = true;
+	}
+      }
+      if (needStore) {
+	storeState(cronStateFile);
+      }
+    }
+    if (enabled) {
+      schedNext();
+    }
+  }
+
+  /** Interface that cron tasks must implement */
+  public interface Task {
+    String getId();
+    void execute();
+    long nextTime(long lastTime);
+  }
+
+  /** State object to record last-run times */
+  static class State implements LockssSerializable {
+    int version = 1;
+    Map times = new HashMap();
+
+    long getLastTime(String id) {
+      Long l = (Long)times.get(id);
+      if (l == null) {
+	return 0;
+      }
+      return l.longValue();
+    }      
+
+    void setLastTime(String id, long time) {
+      times.put(id, new Long(time));
+    }      
+  }
+
+  /** Cron.Task to periodically mail back file to cache admin.  Doesn't
+   * belong here. */
+  static class MailBackupFile implements Cron.Task {
+    LockssDaemon daemon;
+
+    MailBackupFile(LockssDaemon daemon) {
+      this.daemon = daemon;
+    }
+
+    public String getId() {
+      return "MailBackup";
+    }
+
+    public long nextTime(long lastTime) {
+      int day = 1;
+      Calendar cal = Calendar.getInstance(Constants.DEFAULT_TIMEZONE);
+      cal.setTimeInMillis(lastTime);
+      if (cal.get(Calendar.DAY_OF_MONTH) >= day) {
+	cal.add(Calendar.MONTH, 1);
+      }
+      cal.set(Calendar.DAY_OF_MONTH, day);
+      cal.set(Calendar.HOUR, 0);
+      cal.set(Calendar.MINUTE, 0);
+      cal.set(Calendar.SECOND, 0);
+      return cal.getTimeInMillis();
+    }
+
+    public void execute() {
+      RemoteApi rmtApi = daemon.getRemoteApi();
+      try {
+	rmtApi.sendMailBackup();
+      } catch (IOException e) {
+	log.warning("Failed to mail backup file", e); 
+      }
+    }
+  }
+}
---
+++ test/src/org/lockss/daemon/TestCron.java
@@ -0,0 +1,196 @@
+/*
+ * $Id: TestCron.java,v 1.1 2005-09-06 19:56:55 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+n
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.daemon;
+
+import junit.framework.TestCase;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import org.lockss.app.*;
+import org.lockss.util.*;
+import org.lockss.config.*;
+import org.lockss.remote.*;
+import org.lockss.test.*;
+
+/**
+ * This is the test class for org.lockss.daemon.Cron
+ */
+public class TestCron extends LockssTestCase {
+  MockLockssDaemon daemon;
+  MyCron cron;
+
+  public void setUp() throws Exception {
+    super.setUp();
+    daemon = getMockLockssDaemon();
+  }
+
+  void initCron() {
+    initCron(null);
+  }
+
+  void initCron(Cron.Task task) {
+    cron = new MyCron(task);
+    cron.initService(daemon);
+    daemon.setDaemonInited(true);
+  }
+
+  public void testLoadStateNoFile() {
+    initCron();
+    cron.loadState(new File("no/such/file"));
+    assertEmpty(cron.getState().times);
+  }
+
+  public void testState() throws IOException {
+    initCron();
+    File file = FileTestUtil.tempFile("fff");
+    cron.loadState(file);
+    assertEmpty(cron.getState().times);
+    cron.getState().setLastTime("foo", 1234);
+    cron.getState().setLastTime("bar", 777);
+    assertEquals(1234, cron.getState().getLastTime("foo"));
+    assertEquals(777, cron.getState().getLastTime("bar"));
+    cron.storeState(file);
+    assertEquals(1234, cron.getState().getLastTime("foo"));
+    assertEquals(777, cron.getState().getLastTime("bar"));
+    cron.getState().setLastTime("foo", 1);
+    cron.getState().setLastTime("bar", 2);
+    assertEquals(1, cron.getState().getLastTime("foo"));
+    assertEquals(2, cron.getState().getLastTime("bar"));
+    cron.loadState(file);
+    assertEquals(1234, cron.getState().getLastTime("foo"));
+    assertEquals(777, cron.getState().getLastTime("bar"));
+  }
+
+  static DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm");
+  static {
+    df.setTimeZone(Constants.DEFAULT_TIMEZONE);
+  }
+
+  public void testCron() throws IOException {
+    TimeBase.setSimulated(1000);
+    TestTask task = new TestTask(daemon);
+    initCron(task);
+    File dir = getTempDir();
+    Properties p = new Properties();
+    p.put(ConfigManager.PARAM_PLATFORM_DISK_SPACE_LIST, dir.toString());
+    p.put(Cron.PARAM_SLEEP, "10");
+    p.put(Cron.PARAM_ENABLED, "true");
+    ConfigurationUtil.setCurrentConfigFromProps(p);
+    cron.startService();
+    TimeBase.step(10);
+    assertEquals(ListUtil.list(new Long(1010)), task.getTrace());
+    TimeBase.step(10);
+    assertEquals(ListUtil.list(new Long(1010)), task.getTrace());
+    TimeBase.step(10);
+    assertEquals(ListUtil.list(new Long(1010)), task.getTrace());
+    TimeBase.step(70);
+    assertEquals(ListUtil.list(new Long(1010)), task.getTrace());
+    TimeBase.step(20);
+    assertEquals(ListUtil.list(new Long(1010), new Long(1120)),
+		 task.getTrace());
+  }
+
+  public void testMailBackupFileNext() throws Exception {
+    Cron.MailBackupFile task = new Cron.MailBackupFile(daemon);
+    // time 0 is midnight Jan 1, result s.b. Feb 1
+    assertEquals(31*Constants.DAY, task.nextTime(0));
+    assertEquals(df.parse("2/1/2005 0:00").getTime(),
+		 task.nextTime(df.parse("1/3/2005 1:00").getTime()));
+    assertEquals(df.parse("12/1/2007 0:00").getTime(),
+		 task.nextTime(df.parse("11/30/2007 4:00").getTime()));
+    assertEquals(df.parse("2/1/2008 0:00").getTime(),
+		 task.nextTime(df.parse("1/7/2008 4:00").getTime()));
+    assertEquals(df.parse("1/1/2008 0:00").getTime(),
+		 task.nextTime(df.parse("12/7/2007 4:00").getTime()));
+  }
+
+  public void testMailBackupFileMail() {
+    MyRemoteApi rmt = new MyRemoteApi();
+    daemon.setRemoteApi(rmt);
+    Cron.MailBackupFile task = new Cron.MailBackupFile(daemon);
+    assertFalse(rmt.sent);
+    task.execute();
+    assertTrue(rmt.sent);
+  }
+
+  static class MyCron extends Cron {
+    Cron.Task task = null;
+    MyCron() {
+      super();
+    }
+    MyCron(Cron.Task task) {
+      super();
+      this.task = task;
+    }
+
+    void installTasks() {
+      if (false) super.installTasks();
+      if (task != null) {
+	addTask(task);
+      }
+    }
+  }
+
+  static class TestTask implements Cron.Task {
+    List trace = new ArrayList();
+
+    TestTask(LockssDaemon daemon) {
+    }
+
+    public String getId() {
+      return "TestTask";
+    }
+
+    public long nextTime(long lastTime) {
+      return lastTime + 100;
+    }
+
+    public void execute() {
+      trace.add(new Long(TimeBase.nowMs()));
+    }
+
+    List getTrace() {
+      return trace;
+    }
+  }
+
+
+  static class MyRemoteApi extends RemoteApi {
+    boolean sent = false;
+
+    public void sendMailBackup() throws IOException {
+      sent = true;
+    }
+  }
+
+}
