e2c551b6ab86e049bb7532330eeec81f4811cf88
thibgc <github@thib.org>
2006-01-11 21:54:47 +0000

    Cosmetic.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@4935 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/servlet/ProxyAndContent.java
+++ src/org/lockss/servlet/ProxyAndContent.java
@@ -1,10 +1,10 @@
 /*
- * $Id: ProxyAndContent.java,v 1.5 2005-12-01 23:28:01 troberts Exp $
+ * $Id: ProxyAndContent.java,v 1.6 2006-01-11 21:54:47 thib_gc Exp $
  */
 
 /*
 
-Copyright (c) 2000-2005 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2006 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
