f7d67a1ea028a110de04accd2308fff8aaad4432
tlipkis <tal@pss.com>
2006-01-27 04:26:18 +0000

    Add missing comma to backup email Date: header.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/branches/branch_1-14@4963 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/remote/RemoteApi.java
+++ src/org/lockss/remote/RemoteApi.java
@@ -1,5 +1,5 @@
 /*
- * $Id: RemoteApi.java,v 1.48 2006-01-09 21:56:16 tlipkis Exp $
+ * $Id: RemoteApi.java,v 1.48.2.1 2006-01-27 04:26:18 tlipkis Exp $
  */
 
 /*
@@ -1451,7 +1451,7 @@ public class RemoteApi
 
 
 
-  DateFormat headerDf = new SimpleDateFormat("EEE dd MMM yyyy HH:mm:ss zzz"
+  DateFormat headerDf = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz"
 					     /*, Locale.US */);
 //   headerDf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
