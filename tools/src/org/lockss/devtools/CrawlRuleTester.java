b46ca4882ff3b382ef7b9f05b539e012ca499a65
tlipkis <tal@pss.com>
2006-02-04 05:34:43 +0000

    Switched from URLConnection to LockssUrlConnection (HttpClient). Bugfix: was fetching many URLs repeatedly at different levels. Improved reporting.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@5003 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- tools/src/org/lockss/devtools/CrawlRuleTester.java
+++ tools/src/org/lockss/devtools/CrawlRuleTester.java
@@ -3,11 +3,13 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.lockss.crawler.*;
 import org.lockss.daemon.*;
 import org.lockss.plugin.*;
 import org.lockss.plugin.base.*;
 import org.lockss.util.*;
+import org.lockss.util.urlconn.*;
 
 public class CrawlRuleTester extends Thread {
   /* Message Types */
@@ -17,29 +19,32 @@ public class CrawlRuleTester extends Thread {
   public static final int URL_SUMMARY_MESSAGE = 3;
   public static final int TEST_SUMMARY_MESSAGE = 4;
 
-  public static long DEFAULT_DELAY =
-      BaseArchivalUnit.DEFAULT_FETCH_DELAY;
+  public static long MIN_DELAY = BaseArchivalUnit.MIN_FETCH_DELAY;
 
   private String m_baseUrl;
   private CrawlSpec m_crawlSpec;
   private int m_crawlDepth;
   private long m_crawlDelay;
+  private int m_curDepth;
   private String m_outputFile = null;
   private  BufferedWriter m_outWriter = null;
   private Deadline fetchDeadline = Deadline.in(0);
   private boolean useLocalWriter = true;
   private MessageHandler m_msgHandler;
+  private LockssUrlConnectionPool connectionPool =
+    new LockssUrlConnectionPool();
 
   // our storage for extracted urls
   private TreeSet m_extracted = new TreeSet();
   private TreeSet m_incls = new TreeSet();
   private TreeSet m_excls = new TreeSet();
+  private TreeSet m_reported = new TreeSet();
 
   public CrawlRuleTester(int crawlDepth, long crawlDelay, String baseUrl,
                          CrawlSpec crawlSpec) {
     super("crawlrule tester");
     m_crawlDepth = crawlDepth;
-    m_crawlDelay = Math.max(crawlDelay, DEFAULT_DELAY);
+    m_crawlDelay = Math.max(crawlDelay, MIN_DELAY);
     m_baseUrl = baseUrl;
     m_crawlSpec = crawlSpec;
 
@@ -134,20 +139,34 @@ public class CrawlRuleTester extends Thread {
     }
   }
 
+  int[] depth_incl;
+  int[] depth_fetched;
+  int[] depth_parsed;
+
   private void checkRules() {
     outputMessage("\nChecking " + m_baseUrl, TEST_SUMMARY_MESSAGE);
     outputMessage("crawl depth: " + m_crawlDepth +
-                  "     crawl delay: " + m_crawlDelay + " millisecs.\n",
+                  "     crawl delay: " + m_crawlDelay + " ms.",
                   PLAIN_MESSAGE);
 
     TreeSet crawlList = new TreeSet();
     TreeSet fetched = new TreeSet();
     // inialize with the baseUrl
     crawlList.add(m_baseUrl);
+    depth_incl = new int[m_crawlDepth];
+    depth_fetched = new int[m_crawlDepth];
+    depth_parsed = new int[m_crawlDepth];
     long start_time = TimeBase.nowMs();
-    for (int depth = 0; depth < m_crawlDepth; depth++) {
+    for (int depth = 1; depth <= m_crawlDepth; depth++) {
+      m_curDepth = depth;
+      if (crawlList.isEmpty() && depth <= m_crawlDepth) {
+	outputMessage("\nNothing left to crawl, exiting after depth " +
+		      (depth - 1), PLAIN_MESSAGE);
+	break;
+      }
       String[] urls = (String[]) crawlList.toArray(new String[0]);
       crawlList.clear();
+      outputMessage("\nDepth " + depth, PLAIN_MESSAGE);
       for (int ix = 0; ix < urls.length; ix++) {
         pauseBeforeFetch();
         String urlstr = urls[ix];
@@ -158,31 +177,57 @@ public class CrawlRuleTester extends Thread {
         // crawl the page
         buildUrlSets(urlstr);
         fetched.add(urlstr);
-        // output incl/excl results
-        outputUrlResults(urlstr, m_incls, m_excls);
-        // add the m_incls to the crawlList for next crawl depth loop
-        crawlList.addAll(m_incls);
+        // output incl/excl results,
+        // add the new_incls to the crawlList for next crawl depth loop
+        crawlList.addAll(outputUrlResults(urlstr, m_incls, m_excls));
       }
     }
     long elapsed_time = TimeBase.nowMs() - start_time;
-    outputSummary(m_baseUrl, fetched, elapsed_time);
+    outputSummary(m_baseUrl, fetched, crawlList, elapsed_time);
   }
 
 
   private void buildUrlSets(String url) {
 
     try {
+      outputMessage("\nFetching " + url, TEST_SUMMARY_MESSAGE);
       URL srcUrl = new URL(url);
-      URLConnection conn = srcUrl.openConnection();
-      String type = conn.getContentType();
-      type = conn.getHeaderField("content-type");
-      if (type == null || !type.toLowerCase().startsWith("text/html"))
-        return;
-      InputStreamReader reader = new InputStreamReader(conn.getInputStream());
-//       MyMockCachedUrl mcu = new MyMockCachedUrl(srcUrl.toString(), reader);
-      GoslingHtmlParser parser = new GoslingHtmlParser();
-      parser.parseForUrls(reader, srcUrl.toString() ,
-			  new MyFoundUrlCallback());
+//       URLConnection conn = srcUrl.openConnection();
+//       String type = conn.getContentType();
+//       type = conn.getHeaderField("content-type");
+//       InputStream istr = conn.getInputStream();
+      LockssUrlConnection conn = UrlUtil.openConnection(url, connectionPool);
+      try {
+	conn.execute();
+	int resp = conn.getResponseCode();
+	if (resp != 200) {
+	  outputMessage("Resp: " + resp + ": " + conn.getResponseMessage(),
+			TEST_SUMMARY_MESSAGE);
+	  return;
+	}
+	depth_fetched[m_curDepth - 1]++;
+	String cookies = conn.getResponseHeaderValue("Set-Cookie");
+	if (cookies != null) {
+	  outputMessage("Cookies: " + cookies, PLAIN_MESSAGE);
+	}
+	String type = conn.getResponseContentType();
+	if (type == null || !type.toLowerCase().startsWith("text/html")) {
+	  outputMessage("Type: " + type + ", not parsing",URL_SUMMARY_MESSAGE);
+	  return;
+	}
+	outputMessage("Type: " + type + ", extracting Urls",
+		      URL_SUMMARY_MESSAGE);
+	InputStream istr = conn.getResponseInputStream();
+	InputStreamReader reader = new InputStreamReader(istr);
+	//       MyMockCachedUrl mcu = new MyMockCachedUrl(srcUrl.toString(), reader);
+	GoslingHtmlParser parser = new GoslingHtmlParser();
+	parser.parseForUrls(reader, srcUrl.toString() ,
+			    new MyFoundUrlCallback());
+	istr.close();
+	depth_parsed[m_curDepth - 1]++;
+      } finally {
+	conn.release();
+      }
     }
     catch (MalformedURLException murle) {
       murle.printStackTrace();
@@ -226,23 +271,32 @@ public class CrawlRuleTester extends Thread {
                   ERROR_MESSAGE);
   }
 
-  private void outputUrlResults(String url, Set m_inclset, Set m_exclset) {
-    outputMessage("\nExtracted Urls from: " + url,URL_SUMMARY_MESSAGE);
-
-    Iterator it = m_inclset.iterator();
-    if (it.hasNext()) {
-      outputMessage("\nIncluded Urls:", URL_SUMMARY_MESSAGE);
+  private Set outputUrlResults(String url, Set m_inclset, Set m_exclset) {
+    Set new_incls =
+      new TreeSet(CollectionUtils.subtract(m_inclset, m_reported));
+    Set new_excls =
+      new TreeSet(CollectionUtils.subtract(m_exclset, m_reported));
+    if (!m_inclset.isEmpty()) {
+      outputMessage("\nIncluded Urls: (" + new_incls.size() + " new, " +
+		    (m_inclset.size() - new_incls.size()) + " old)",
+		    URL_SUMMARY_MESSAGE);
+      depth_incl[m_curDepth - 1] += new_incls.size();
     }
-    while (it.hasNext()) {
+    for (Iterator it = new_incls.iterator(); it.hasNext(); ) {
       outputMessage(it.next().toString(), PLAIN_MESSAGE);
     }
 
-    it = m_exclset.iterator();
-    if (it.hasNext())
-      outputMessage("\nExcluded Urls:", URL_SUMMARY_MESSAGE);
-    while (it.hasNext()) {
+    if (!m_exclset.isEmpty()) {
+      outputMessage("\nExcluded Urls: (" + new_excls.size() + " new, " +
+		    (m_exclset.size() - new_excls.size()) + " old)",
+		    URL_SUMMARY_MESSAGE);
+    }
+    for (Iterator it = new_excls.iterator(); it.hasNext(); ) {
       outputMessage(it.next().toString(), PLAIN_MESSAGE);
     }
+    m_reported.addAll(new_incls);
+    m_reported.addAll(new_excls);
+
     if(m_outWriter != null) {
       try {
         m_outWriter.flush();
@@ -250,14 +304,37 @@ public class CrawlRuleTester extends Thread {
       catch (IOException ex) {
       }
     }
+    return new_incls;
   }
 
-  private void outputSummary(String baseUrl, Set fetched, long elapsedTime) {
+  private void outputSummary(String baseUrl, Set fetched, Set toCrawl,
+			     long elapsedTime) {
     int fetchCount = fetched.size();
     outputMessage("\n\nSummary for starting Url: " + baseUrl +
                   " and depth: " + m_crawlDepth, TEST_SUMMARY_MESSAGE);
     outputMessage("\nUrls fetched: " + fetchCount +
                   "    Urls extracted: " + m_extracted.size(), PLAIN_MESSAGE);
+
+    outputMessage("\nDepth  Fetched  Parsed  New URLs", PLAIN_MESSAGE);
+    for (int depth = 1; depth <= m_crawlDepth; depth++) {
+      PrintfFormat pf = new PrintfFormat("%5d  %7d  %6d  %8d");
+      Integer[] args = new Integer[]{
+	new Integer(depth),
+	new Integer(depth_fetched[depth - 1]),
+	new Integer(depth_parsed[depth - 1]),
+	new Integer(depth_incl[depth - 1]),
+      };
+      String s = pf.sprintf(args);
+      outputMessage(s, PLAIN_MESSAGE);
+    }
+
+    outputMessage("\nRemaining unfetched: " + toCrawl.size(), PLAIN_MESSAGE);
+    if (false) {
+      for (Iterator iter = toCrawl.iterator(); iter.hasNext(); ) {
+	String url = (String)iter.next();
+	outputMessage(url, PLAIN_MESSAGE);
+      }
+    }
     long secs = elapsedTime / Constants.SECOND;
     long fetchRate = 0;
     if(secs > 0) {
@@ -281,20 +358,18 @@ public class CrawlRuleTester extends Thread {
 
     public void foundUrl(String url) {
 
-      if (!m_extracted.contains(url)) {
-        m_extracted.add(url);
-	try {
-	  String normUrl = UrlUtil.normalizeUrl(url);
-	  if (BaseCrawler.isSupportedUrlProtocol(normUrl) &&
-	      m_crawlSpec.isIncluded(normUrl)) {
-	    m_incls.add(normUrl);
-	  }
-	  else {
-	    m_excls.add(normUrl);
-	  }
-	} catch (MalformedURLException e) {
-	  m_excls.add(url);
+      m_extracted.add(url);
+      try {
+	String normUrl = UrlUtil.normalizeUrl(url);
+	if (BaseCrawler.isSupportedUrlProtocol(normUrl) &&
+	    m_crawlSpec.isIncluded(normUrl)) {
+	  m_incls.add(normUrl);
 	}
+	else {
+	  m_excls.add(normUrl);
+	}
+      } catch (MalformedURLException e) {
+	m_excls.add(url);
       }
 
     }
