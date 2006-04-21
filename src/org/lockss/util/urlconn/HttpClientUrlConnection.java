0738a5ca835e28167ee661c7077dbad34b4609a3
tlipkis <tal@pss.com>
2006-04-21 08:07:59 +0000

    Improved logging of MalformedURLExceptions.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/branches/branch_1-16@5190 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/util/urlconn/HttpClientUrlConnection.java
+++ src/org/lockss/util/urlconn/HttpClientUrlConnection.java
@@ -1,5 +1,5 @@
 /*
- * $Id: HttpClientUrlConnection.java,v 1.22 2006-02-01 08:43:58 tlipkis Exp $
+ * $Id: HttpClientUrlConnection.java,v 1.22.4.1 2006-04-21 08:07:59 tlipkis Exp $
  *
 
 Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
@@ -121,12 +121,19 @@ public class HttpClientUrlConnection extends BaseLockssUrlConnection {
     } catch (IllegalArgumentException e) {
       // HttpMethodBase throws IllegalArgumentException on illegal URLs
       // Canonicalize that to Java's MalformedURLException
-      throw new java.net.MalformedURLException(urlString);
+      throw newMalformedURLException(urlString, e);
     } catch (IllegalStateException e) {
       // HttpMethodBase throws IllegalArgumentException on illegal protocols
       // Canonicalize that to Java's MalformedURLException
-      throw new java.net.MalformedURLException(urlString);
+      throw newMalformedURLException(urlString, e);
     }
+  }
+
+  java.net.MalformedURLException newMalformedURLException(String msg,
+							  Throwable cause) {
+    java.net.MalformedURLException e = new java.net.MalformedURLException(msg);
+    e.initCause(cause);
+    return e;
   }
 
   /** for testing */
