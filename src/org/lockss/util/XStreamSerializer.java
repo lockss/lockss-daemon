2d2197c93e34cc9ad5b0d85d7f690ef6a1b333f7
thibgc <github@thib.org>
2006-04-22 18:21:08 +0000

    Improved logging and nesting of exceptions. Raised logging level of NotSerializableException.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/branches/branch_1-16@5193 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/util/XStreamSerializer.java
+++ src/org/lockss/util/XStreamSerializer.java
@@ -1,5 +1,5 @@
 /*
- * $Id: XStreamSerializer.java,v 1.16.2.1 2006-04-20 19:33:25 thib_gc Exp $
+ * $Id: XStreamSerializer.java,v 1.16.2.2 2006-04-22 18:21:08 thib_gc Exp $
  */
 
 /*
@@ -92,8 +92,7 @@ public class XStreamSerializer extends ObjectSerializer {
    * {@link XStreamSerializer} only.</p>
    * @author Thib Guicherd-Callin
    */
-  private static class LockssNotSerializableException
-      extends RuntimeException {
+  private static class LockssNotSerializableException extends RuntimeException {
 
     /*
      * IMPLEMENTATION NOTES
@@ -534,7 +533,9 @@ public class XStreamSerializer extends ObjectSerializer {
     catch (StreamException se) {
       logger.debug2("Deserialization failed; StreamException thrown", se);
       throwIfInterrupted(se);
-      throw new IOException(se.toString());
+      IOException ioe = new IOException();
+      ioe.initCause(se);
+      throw ioe;
     }
     catch (CannotResolveClassException crce) {
       throw failDeserialize(crce);
@@ -556,11 +557,17 @@ public class XStreamSerializer extends ObjectSerializer {
       xs.toXML(obj, writer); // lazy instantiation
     }
     catch (LockssNotSerializableException lnse) {
-      throw new NotSerializableException(lnse.getMessage());
+      logger.error("Not Serializable or LockssSerializable", lnse);
+      NotSerializableException nse = new NotSerializableException();
+      nse.initCause(lnse);
+      throw nse;
     }
     catch (StreamException se) {
-      logger.debug("StreamException", se);
-      throw new IOException(se.getMessage());
+      logger.debug("Serialization failed; StreamException thrown", se);
+      throwIfInterrupted(se);
+      IOException ioe = new IOException();
+      ioe.initCause(se);
+      throw ioe;
     }
     catch (CannotResolveClassException crce) {
       throw failSerialize(crce, obj);
