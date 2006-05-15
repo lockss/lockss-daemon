6dc04bfbd3ed884f3c2c1a9e3f8066dddb5996d2
tlipkis <tal@pss.com>
2006-05-15 00:13:40 +0000

    Improved permutation test.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@5245 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/util/TestCollectionUtil.java
+++ test/src/org/lockss/util/TestCollectionUtil.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestCollectionUtil.java,v 1.13 2006-04-11 08:29:47 tlipkis Exp $
+ * $Id: TestCollectionUtil.java,v 1.14 2006-05-15 00:13:40 tlipkis Exp $
  */
 
 /*
@@ -251,7 +251,9 @@ public class TestCollectionUtil extends LockssTestCase {
     Set set = new HashSet();
     int rpt = 0;
     while (++rpt < 100000) {
-      set.add(CollectionUtil.randomPermutation(coll));
+      List lst = CollectionUtil.randomPermutation(coll);
+      assertEquals(5, lst.size());
+      set.add(lst);
       if (set.size() == 120) {
 	log.debug(""+rpt);
 	return;
