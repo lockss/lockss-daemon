b5b8ac1e05fd9ae81590561eb4d519a7a6aca59b
tlipkis <tal@pss.com>
2006-05-27 06:33:26 +0000

    FilenameFilter that matches numeric filenames.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@5274 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/util/TestNumericFilenameFilter.java
@@ -0,0 +1,58 @@
+/*
+ * $Id: TestNumericFilenameFilter.java,v 1.1 2006-05-27 06:33:26 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2006 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+
+import java.util.*;
+import java.io.*;
+import org.lockss.test.*;
+
+
+/**
+ * Test class for org.lockss.util.ArrayIterator
+ */
+
+public class TestNumericFilenameFilter extends LockssTestCase {
+
+  public void testMatch() {
+    File dir = new File("/");
+    NumericFilenameFilter filt1 = NumericFilenameFilter.INSTANCE;
+    assertTrue(filt1.accept(dir, ""));
+    assertTrue(filt1.accept(dir, "0"));
+    assertTrue(filt1.accept(dir, "00"));
+    assertTrue(filt1.accept(dir, "12349"));
+    assertFalse(filt1.accept(dir, "abc"));
+    assertFalse(filt1.accept(dir, "123a"));
+    assertFalse(filt1.accept(dir, "a123"));
+    assertFalse(filt1.accept(dir, "123a333"));
+  }
+}
