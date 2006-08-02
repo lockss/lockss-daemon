65111e191d4cc8a69329044aeacb45ca60c908b8
tlipkis <tal@pss.com>
2006-08-02 02:14:51 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@5480 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/test/src/org/lockss/plugin/blackwell/TestBlackwellArchivalUnit.java
+++ plugins/test/src/org/lockss/plugin/blackwell/TestBlackwellArchivalUnit.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestBlackwellArchivalUnit.java,v 1.1 2006-08-02 02:10:39 tlipkis Exp $
+ * $Id: TestBlackwellArchivalUnit.java,v 1.2 2006-08-02 02:14:51 tlipkis Exp $
  */
 
 /*
@@ -171,8 +171,6 @@ public class TestBlackwellArchivalUnit extends LockssPluginTestCase {
     DefinableArchivalUnit bwAu = makeAu(ROOT_URL, "pace", 2);
     assertEquals(expectedStr,
 		 ((SpiderCrawlSpec)bwAu.getCrawlSpec()).getStartingUrls().get(0));
-//                  bwAu.getProperties().getString(ArchivalUnit.AU_START_URL, null));
-    //au.getCrawlSpec().getStartingUrls()
   }
 
   public void testGetUrlStems() throws Exception {
