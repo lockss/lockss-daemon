c431d175c2c6d5e9c31320ed28d3b0c1e25e61ef
tlipkis <tal@pss.com>
2006-09-16 22:55:44 +0000

    Static utilities for filters and filter factories.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@5633 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/filter/FilterUtil.java
@@ -0,0 +1,76 @@
+/*
+ * $Id: FilterUtil.java,v 1.1 2006-09-16 22:55:44 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2006 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.filter;
+
+import java.io.*;
+import java.util.List;
+import org.lockss.util.*;
+
+/** Convenience methods for filters and filter factories */
+public class FilterUtil {
+  static Logger log = Logger.getLogger("FilterUtil");
+
+  // no instances
+  private FilterUtil() {
+  }
+
+  /** Return a Reader that reads from the InputStream.  If the specified
+   * encoding is not found, tries {@link Constants#DEFAULT_ENCODING}.  If
+   * the supplied InputStream is a ReaderInputStream, returns the
+   * underlying Reader.
+   * @param in the InputStream to be wrapped
+   * @param encoding the charset
+   */
+  public static Reader getReader(InputStream in, String encoding) {
+    if (in instanceof ReaderInputStream) {
+      ReaderInputStream ris = (ReaderInputStream)in;
+      return ris.getReader();
+    }
+    try {
+      return new InputStreamReader(in, encoding);
+    } catch (UnsupportedEncodingException e1) {
+      log.error("No such encoding: " + encoding + ", trying " +
+		Constants.DEFAULT_ENCODING);
+      try {
+	return new InputStreamReader(in, Constants.DEFAULT_ENCODING);
+      } catch (UnsupportedEncodingException e2) {
+	log.critical("Default encoding not found: " +
+		     Constants.DEFAULT_ENCODING);
+	throw new RuntimeException(("UnsupportedEncodingException for both " +
+				    encoding + " and " +
+				    Constants.DEFAULT_ENCODING),
+				   e1);
+      }
+    }
+  }
+}
---
+++ test/src/org/lockss/filter/TestFilterUtil.java
@@ -0,0 +1,63 @@
+/*
+ * $Id: TestFilterUtil.java,v 1.1 2006-09-16 22:55:44 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.filter;
+
+import java.io.*;
+import java.nio.charset.*;
+import java.util.*;
+import org.lockss.test.*;
+import org.lockss.util.*;
+
+/**
+ * Test class for org.lockss.filter.FilterUtil
+ */
+public class TestFilterUtil extends LockssTestCase {
+
+  public void testGetReader() {
+    InputStream in = new StringInputStream("123");
+    InputStreamReader rdr;
+    Charset def = Charset.forName(Constants.DEFAULT_ENCODING);
+    Charset utf = Charset.forName("UTF-8");
+    try {
+      FilterUtil.getReader(in, null);
+      fail("getReader(..., null) should throw NPE");
+    } catch (NullPointerException e) {
+    }
+    rdr = (InputStreamReader)FilterUtil.getReader(in, "ISO-8859-1");
+    assertTrue(def.aliases().contains(rdr.getEncoding()));
+    rdr = (InputStreamReader)FilterUtil.getReader(in, "utf-8");
+    assertTrue(utf.aliases().contains(rdr.getEncoding()));
+    rdr = (InputStreamReader)FilterUtil.getReader(in, "NoSuchCharset");
+    assertTrue(def.aliases().contains(rdr.getEncoding()));
+  }
+}
