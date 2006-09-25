0f07c152551cf076deca2abed20e5015c99325c1
thibgc <github@thib.org>
2006-09-25 08:20:17 +0000

    Logging.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@5696 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/highwire/HighWirePdfFilterFactory.java
+++ plugins/src/org/lockss/plugin/highwire/HighWirePdfFilterFactory.java
@@ -1,5 +1,5 @@
 /*
- * $Id: HighWirePdfFilterFactory.java,v 1.1 2006-09-21 05:50:52 thib_gc Exp $
+ * $Id: HighWirePdfFilterFactory.java,v 1.2 2006-09-25 08:20:17 thib_gc Exp $
  */
 
 /*
@@ -36,7 +36,7 @@ import java.io.*;
 
 import org.lockss.filter.pdf.DocumentTransform;
 import org.lockss.plugin.*;
-import org.lockss.util.PdfDocument;
+import org.lockss.util.*;
 import org.pdfbox.cos.*;
 
 public class HighWirePdfFilterFactory implements FilterFactory {
@@ -65,5 +65,7 @@ public class HighWirePdfFilterFactory implements FilterFactory {
                                                String encoding) {
     throw new UnsupportedOperationException("Unimplemented");
   }
+
+  private static Logger logger = Logger.getLogger("HighWirePdfFilterFactory");
 
 }
