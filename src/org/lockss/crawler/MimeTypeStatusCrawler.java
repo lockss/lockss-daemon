e6cbee72c57818235dfbfecfc18f1b7f9df5b722
thibgc <github@thib.org>
2006-10-11 18:58:11 +0000

    Fixed a class cast exception.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@5764 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/crawler/MimeTypeStatusCrawler.java
+++ src/org/lockss/crawler/MimeTypeStatusCrawler.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MimeTypeStatusCrawler.java,v 1.1 2006-10-11 02:39:35 adriz Exp $
+ * $Id: MimeTypeStatusCrawler.java,v 1.2 2006-10-11 18:58:11 thib_gc Exp $
  */
 
 /*
@@ -41,31 +41,31 @@ import org.lockss.plugin.*;
 import org.lockss.util.*;
 
 public class MimeTypeStatusCrawler implements StatusAccessor {
-  
+
   private static final String IX = "ix";
   private static final String MIME_TYPE_NAME = "mime_type_name";
   private static final String MIME_TYPE_NUM_URLS = "mime_type_num_urls";
   //private static final String MIME_TYPE = "mime_type";
   private static final String MIMETYPES_TABLE_NAME = "mime-type";
-  
-  private List colDescsMimeTypes = 
+
+  private List colDescsMimeTypes =
     ListUtil.fromArray(new ColumnDescriptor[] {
       new ColumnDescriptor(MIME_TYPE_NAME, "Mime Type Name",
                            ColumnDescriptor.TYPE_STRING),
       new ColumnDescriptor(MIME_TYPE_NUM_URLS, "URLs Found",
                            ColumnDescriptor.TYPE_INT,
                            "Number of URLs found with this Mime Type"),
-    });                         
- 
+    });
+
   private static final List statusSortRules =
     ListUtil.list(new StatusTable.SortRule(IX, true));
 
   private CrawlManager.StatusSource statusSource;
-  
+
   public MimeTypeStatusCrawler(CrawlManager.StatusSource statusSource) {
     this.statusSource = statusSource;
   }
-  
+
   public void populateTable(StatusTable table)
   throws StatusService.NoSuchTableException{
     if (table == null) {
@@ -88,7 +88,7 @@ public class MimeTypeStatusCrawler implements StatusAccessor {
     }
     table.setDefaultSortRules(statusSortRules);
     table.setColumnDescriptors(colDescsMimeTypes);  //  was     table.setColumnDescriptors(getColDescs(tableStr));
-    table.setTitle(getTableTitle(status));          //was table.setTitle(getTableTitle(status, tableStr));  
+    table.setTitle(getTableTitle(status));          //was table.setTitle(getTableTitle(status, tableStr));
     table.setRows(getRows(status, key));                // was table.setTitle(getTableTitle(status, tableStr));
 }
 
@@ -97,14 +97,14 @@ public class MimeTypeStatusCrawler implements StatusAccessor {
     return "Content Types found during crawl of "+au.getName();
   }
 
-  /* 
+  /*
    *   getRows(Crawler.Status status)
    *   1.  get status +  String key because we are here since its mime-type
    *   2.  iterate over the status-all of the mime-types - mapRecord
-   *   3.  values for each mimeType-value, int (recMap.numOfUrls) will call makeRow 
+   *   3.  values for each mimeType-value, int (recMap.numOfUrls) will call makeRow
    */
   private List getRows(Crawler.Status status, String key) {
-    List allMimeTypeKeys = (ArrayList)status.getMimeTypesVals();
+    Collection allMimeTypeKeys = status.getMimeTypesVals();
     List rows = new ArrayList();
     if (allMimeTypeKeys != null) {
       String keyMimeType;
@@ -129,17 +129,17 @@ public class MimeTypeStatusCrawler implements StatusAccessor {
     //ArchivalUnit au = status.getAu();
     row.put(MIME_TYPE_NAME, keyMimeTypeName);
     //ArrayList urls = (ArrayList)status.getUrlsArrayOfMimeType(keyMimeTypeName);
-    if ( status.keepMimeTypeUrls() ){  
+    if ( status.keepMimeTypeUrls() ){
       // put a row with a refrence to get into 3rd status level which will bring a list of urls
-      String urlsRef =  keyMimeTypeName +":"+ MIMETYPES_TABLE_NAME +"."+key ; // this key ise the value of the status ix forwarded 
+      String urlsRef =  keyMimeTypeName +":"+ MIMETYPES_TABLE_NAME +"."+key ; // this key ise the value of the status ix forwarded
       row.put(MIME_TYPE_NUM_URLS,
                 makeRef(numOfUrls,
-                        "single_crawl_status", urlsRef));                    
+                        "single_crawl_status", urlsRef));
     }else{      //urls.isEmpty()
       row.put(MIME_TYPE_NUM_URLS,
               new Long(status.getNumUrlsOfMimeType(keyMimeTypeName)) );
-    } 
-     
+    }
+
     return row;
   }
   /**
@@ -148,8 +148,8 @@ public class MimeTypeStatusCrawler implements StatusAccessor {
   private Object makeRef(long value, String tableName, String key) {
     return new StatusTable.Reference(new Long(value), tableName, key);
   }
-  
-  
+
+
   public String getDisplayName() {
     throw new UnsupportedOperationException("No generic name for MimeTypeStatusCrawler");
   }
@@ -158,6 +158,6 @@ public class MimeTypeStatusCrawler implements StatusAccessor {
     return true;
   }
 
- 
+
 
 }
