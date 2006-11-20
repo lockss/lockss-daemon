5f39f41c315a753b1d005c61fe964f45c4c60a62
thibgc <github@thib.org>
2006-11-20 22:45:58 +0000

    Issue 2109: PDF transforms for the American Medical Association (first attempt).
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@5886 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/highwire/AmericanMedicalAssociationPdfTransform.java
@@ -0,0 +1,66 @@
+/*
+ * $Id: AmericanMedicalAssociationPdfTransform.java,v 1.1 2006-11-20 22:45:58 thib_gc Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2006 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.highwire;
+
+import java.io.IOException;
+
+import org.lockss.filter.pdf.*;
+import org.lockss.plugin.highwire.HighWirePdfFilterFactory.*;
+
+public class AmericanMedicalAssociationPdfTransform extends SimpleOutputDocumentTransform {
+
+  public static class Simplified extends TextScrapingDocumentTransform {
+    
+    public DocumentTransform makePreliminaryTransform() throws IOException {
+      return new ConditionalDocumentTransform(// If on the first page...
+                                              new TransformFirstPage(// ...normalizing "Downloaded from" succeeds,
+                                                                     new NormalizeDownloadedFrom()),
+                                              // Then on all other pages...
+                                              new TransformEachPageExceptFirst(// ...normalize "Downloaded from"
+                                                                               new NormalizeDownloadedFrom()));
+    }
+    
+  }
+  
+  public AmericanMedicalAssociationPdfTransform() throws IOException {
+    super(new ConditionalDocumentTransform(// If on the first page...
+                                           new TransformFirstPage(// ...collapsing "Downloaded from" succeeds,
+                                                                  new CollapseDownloadedFrom()),
+                                           // Then on all other pages...
+                                           new TransformEachPageExceptFirst(// ...collapse "Downloaded from",
+                                                                            new CollapseDownloadedFrom()),
+                                           // ...and normalize the metadata
+                                           new NormalizeMetadata()));
+  }
+  
+}
