7c38b158cd0bbc31192ad4633073af3633215a37
troberts <claire@lockss.org>
2007-01-03 22:37:41 +0000

    initial filter for BMC
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@5973 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/bmc/BMCHtmlFilterFactory.java
@@ -0,0 +1,64 @@
+/*
+ * $Id: BMCHtmlFilterFactory.java,v 1.1 2007-01-03 22:37:41 troberts Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2006 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.bmc;
+
+import java.io.*;
+import java.util.List;
+import org.lockss.util.*;
+import org.lockss.filter.*;
+import org.lockss.plugin.*;
+import org.lockss.plugin.base.*;
+
+public class BMCHtmlFilterFactory implements FilterFactory {
+
+  public InputStream createFilteredInputStream(ArchivalUnit au,
+					       InputStream in,
+					       String encoding) {
+    Reader reader = FilterUtil.getReader(in, encoding);
+    Reader filtReader = makeFilteredReader(reader);
+    return new ReaderInputStream(filtReader);
+  }
+
+  static Reader makeFilteredReader(Reader reader) {
+    List tagList = ListUtil.list(
+//        new HtmlTagFilter.TagPair("<!--", "-->", true)
+        new HtmlTagFilter.TagPair("<table", "</table>", true, false),
+        new HtmlTagFilter.TagPair("<script", "</script>", true, false),
+        new HtmlTagFilter.TagPair("<noscript", "</noscript>", true, false),
+        new HtmlTagFilter.TagPair("<", ">", false, false)
+        );
+    Reader tagFilter = HtmlTagFilter.makeNestedFilter(reader, tagList);
+    return new WhiteSpaceFilter(tagFilter);
+  }
+
+}
