0c9573243a58cbfd610fbcee320ef2f0e70ff74c
thibgc <github@thib.org>
2007-01-30 02:13:34 +0000

    Issue 1766: Switching from error and warning to siteError and siteWarning in GoslingHtmlParser and CssParser.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/branches/branch_1-22@6069 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/crawler/CssParser.java
+++ src/org/lockss/crawler/CssParser.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CssParser.java,v 1.2 2007-01-16 08:17:09 thib_gc Exp $
+ * $Id: CssParser.java,v 1.2.2.1 2007-01-30 02:13:34 thib_gc Exp $
  */
 
 /*
@@ -175,8 +175,8 @@ public class CssParser implements ContentParser {
     catch (MalformedUrlException lockssMalformedUrlException) {
       MalformedURLException javaMalformedUrlException =
         lockssMalformedUrlException.getJavaMalformedUrlException();
-      logger.error("Malformed URL while parsing " + srcUrl,
-                   javaMalformedUrlException);
+      logger.siteError("Malformed URL while parsing " + srcUrl,
+                       javaMalformedUrlException);
       throw javaMalformedUrlException;
     }
   }
