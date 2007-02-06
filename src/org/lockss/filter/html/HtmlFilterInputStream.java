b38e96bdeca6dfa65217eaab024dea733ec5eda5
tlipkis <tal@pss.com>
2007-02-06 00:53:07 +0000

    Allow caller to specify charset.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@6096 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/filter/html/HtmlFilterInputStream.java
+++ src/org/lockss/filter/html/HtmlFilterInputStream.java
@@ -1,5 +1,5 @@
 /*
- * $Id: HtmlFilterInputStream.java,v 1.3 2007-01-14 08:10:43 tlipkis Exp $
+ * $Id: HtmlFilterInputStream.java,v 1.4 2007-02-06 00:53:07 tlipkis Exp $
  */
 
 /*
@@ -89,6 +89,7 @@ public class HtmlFilterInputStream extends InputStream {
   private FeedbackLogger fl = new FeedbackLogger();
 
   private InputStream in;
+  private String charset;
   private InputStream out = null;
   private HtmlTransform xform;
 
@@ -98,10 +99,16 @@ public class HtmlFilterInputStream extends InputStream {
    * @param xform HtmlTransform to apply to parsed NodeList
    */
   public HtmlFilterInputStream(InputStream in, HtmlTransform xform) {
+    this(in, null, xform);
+  }
+
+  public HtmlFilterInputStream(InputStream in, String charset,
+			       HtmlTransform xform) {
     if (in == null || xform == null) {
       throw new IllegalArgumentException("Called with a null argument");
     }
     this.in = in;
+    this.charset = charset;
     this.xform = xform;
   }
 
@@ -138,7 +145,7 @@ public class HtmlFilterInputStream extends InputStream {
     if (mark > 0) {
       in.mark(mark);
     }
-    Page pg = new Page(new InputStreamSource(in));
+    Page pg = new Page(new InputStreamSource(in, charset));
     Lexer lx = new Lexer(pg);
     Parser parser = new Parser(lx, fl);
 
