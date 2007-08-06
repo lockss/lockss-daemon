b34bb7039f80110a853135edbf1b353499d12f79
thibgc <github@thib.org>
2007-08-06 18:45:23 +0000

    Added package declaration.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@6474 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- tools/src/org/lockss/keystore/BuildTempKeyStores.java
+++ tools/src/org/lockss/keystore/BuildTempKeyStores.java
@@ -1,10 +1,10 @@
 /*
- * $Id: BuildTempKeyStores.java,v 1.1 2007-08-03 23:06:35 dshr Exp $
+ * $Id: BuildTempKeyStores.java,v 1.2 2007-08-06 18:45:23 thib_gc Exp $
  */
 
 /*
 
-Copyright (c) 2006 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2007 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -29,6 +29,8 @@ be used in advertising or otherwise to promote the sale, use or other dealings
 in this Software without prior written authorization from Stanford University.
 
 */
+
+package org.lockss.keystore;
 
 import java.util.*;
 import java.io.*;
