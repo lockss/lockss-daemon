2a7fcb4e6343862655a225ade7fe2c1d01cee080
thibgc <github@thib.org>
2007-09-22 23:08:46 +0000

    Converted to XStream. Enhanced rule to include CSS.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@6638 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/projmuse/ProjectMuseFilterRule.java
+++ plugins/src/org/lockss/plugin/projmuse/ProjectMuseFilterRule.java
@@ -1,10 +1,10 @@
 /*
- * $Id: ProjectMuseFilterRule.java,v 1.1 2005-03-15 07:43:31 tlipkis Exp $
+ * $Id: ProjectMuseFilterRule.java,v 1.2 2007-09-22 23:08:46 thib_gc Exp $
  */
 
 /*
 
-Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2007 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
