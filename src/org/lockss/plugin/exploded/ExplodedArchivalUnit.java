45c26172e492c7e66e28fbd282c832885df2ea45
dshrosenthal <dshr@stanford.edu>
2007-09-24 19:20:02 +0000

    These two got missed somehow in the checkin.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@6643 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/plugin/exploded/ExplodedArchivalUnit.java
@@ -0,0 +1,162 @@
+/*
+ * $Id: ExplodedArchivalUnit.java,v 1.2 2007-09-24 19:20:02 dshr Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2007 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+package org.lockss.plugin.exploded;
+
+import java.util.*;
+
+import org.lockss.util.*;
+import org.lockss.config.Configuration;
+import org.lockss.daemon.*;
+import org.lockss.plugin.base.*;
+import org.lockss.state.AuState;
+
+/**
+ * <p>ExplodedArchivalUnit: The Archival Unit Class for ExplodedPlugin.
+ * This archival unit uses a base url to define an archival unit.
+ * @author David Rosenthal
+ * @version 1.0
+ */
+
+public class ExplodedArchivalUnit extends BaseArchivalUnit {
+  private String m_explodedBaseUrl = null;
+  private String m_auName = null;
+  protected Logger logger = Logger.getLogger("ExplodedArchivalUnit");
+
+  public ExplodedArchivalUnit(ExplodedPlugin plugin) {
+    super(plugin);
+  }
+
+  public void loadAuConfigDescrs(Configuration config)
+      throws ConfigurationException {
+    super.loadAuConfigDescrs(config);
+    this.m_explodedBaseUrl = config.get(ConfigParamDescr.BASE_URL.getKey());
+    String pubName = config.get(ConfigParamDescr.PUBLISHER_NAME.getKey());
+    if (pubName != null) {
+      auName = pubName;
+      // We have some config info to work with
+      String journalId = config.get(ConfigParamDescr.JOURNAL_ID.getKey());
+      if (journalId == null) {
+	journalId = config.get(ConfigParamDescr.JOURNAL_ABBR.getKey());
+	if (journalId == null) {
+	  journalId = config.get(ConfigParamDescr.JOURNAL_DIR.getKey());
+	}
+      }
+      if (journalId != null) {
+	auName += " " + journalId;
+      }
+    }
+  }
+
+
+  // Called by RegistryPlugin iff any config below RegistryPlugin.PREFIX
+  // has changed
+  protected void setConfig(Configuration config,
+			   Configuration prevConfig,
+			   Configuration.Differences changedKeys) {
+    // XXX
+  }
+
+  /**
+   * return a string that represents the plugin registry.  This is
+   * just the base URL unless overridden by the config.
+   * @return The base URL.
+   */
+  protected String makeName() {
+    return (auName != null ? auName : m_explodedBaseUrl);
+  }
+
+  /**
+   * return a string that points to the plugin registry page.
+   * @return a string that points to the plugin registry page for
+   * this registry.  This is just the base URL.
+   */
+  protected String makeStartUrl() {
+    return m_explodedBaseUrl;
+  }
+
+  /**
+   * Determine whether the url falls within the CrawlSpec.
+   * @param url the url
+   * @return true if it is included
+   */
+  public boolean shouldBeCached(String url) {
+    logger.debug3(url + " vs. " + m_explodedBaseUrl);
+    return url.startsWith(m_explodedBaseUrl);
+  }
+
+  protected CrawlRule makeRules() {
+    return new ExplodedCrawlRule();
+  }
+
+  private class ExplodedCrawlRule implements CrawlRule {
+    protected ExplodedCrawlRule() {
+    }
+
+    public int match(String url) {
+      if (url.startsWith(m_explodedBaseUrl)) {
+	return INCLUDE;
+      }
+      return IGNORE;
+    }
+  }
+  /**
+   * Return a new CrawlSpec with the appropriate collect AND redistribute
+   * permissions, and with the maximum refetch depth.
+   *
+   * @return CrawlSpec
+   */
+  protected CrawlSpec makeCrawlSpec() throws LockssRegexpException {
+    ArrayList startUrls = new ArrayList();
+    startUrls.add(startUrlString);
+    return new ExplodedCrawlSpec(startUrls);
+  }
+
+  /**
+   * ExplodedArchivalUnits should never be crawled.
+   * @param aus ignored
+   * @return false
+   */
+  public boolean shouldCrawlForNewContent(AuState aus) {
+    return false;
+  }
+
+  // Exploded AU crawl spec implementation
+  private class ExplodedCrawlSpec extends BaseCrawlSpec {
+    ExplodedCrawlSpec(List startUrls) {
+      // null CrawlRule is always INCLUDE
+      // null PermissionChecker is XXX
+      // null loginPageChecker is always false
+      super(startUrls, null, null, null);
+    }
+  }
+    
+}
---
+++ src/org/lockss/plugin/exploded/ExplodedPlugin.java
@@ -0,0 +1,122 @@
+/*
+ * $Id: ExplodedPlugin.java,v 1.2 2007-09-24 19:20:02 dshr Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2007 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.exploded;
+
+import java.util.*;
+
+import org.lockss.config.Configuration;
+import org.lockss.daemon.*;
+import org.lockss.plugin.base.BasePlugin;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.util.*;
+
+/**
+ * <p>ExplodedPlugin: A plugin for AUs full of files ingested by
+ * explosion rather than crawling.
+ * @version 1.0
+ * @author David Rosenthal
+ */
+
+public class ExplodedPlugin extends BasePlugin {
+  protected static final Logger log = Logger.getLogger("ExplodedPlugin");
+
+  private static String PLUGIN_NAME = "Exploded";
+  private static String CURRENT_VERSION = "1";
+
+  public static final String PREFIX =
+    Configuration.PREFIX + "plugin.exploded.";
+
+  // List of defining properties (only base_url for Exploded plugins)
+  private static final List m_auConfigDescrs =
+    ListUtil.list(ConfigParamDescr.BASE_URL);
+
+  public ExplodedPlugin() {
+  }
+
+  protected ArchivalUnit createAu0(Configuration auConfig)
+      throws ArchivalUnit.ConfigurationException {
+    // create a new archival unit
+    ArchivalUnit au = newExplodedArchivalUnit();
+
+    // Now configure it.
+    au.setConfiguration(auConfig);
+
+    return au;
+  }
+
+  protected ExplodedArchivalUnit newExplodedArchivalUnit() {
+    return new ExplodedArchivalUnit(this);
+  }
+
+  /**
+   * The global config has changed
+   */
+  protected void setConfig(Configuration newConfig,
+			   Configuration prevConfig,
+			   Configuration.Differences changedKeys) {
+    if (changedKeys.contains(PREFIX)) {
+      for (Iterator iter = getAllAus().iterator(); iter.hasNext(); ) {
+	//  They should all be ExplodedArchivalUnits, but just in case...
+	try {
+	  ExplodedArchivalUnit au = (ExplodedArchivalUnit)iter.next();
+	  au.setConfig(newConfig, prevConfig, changedKeys);
+	} catch (Exception e) {
+	  log.warning("setConfig: " + this, e);
+	}
+      }
+    }
+  }
+
+  /**
+   * ExplodedPlugin does not have a configuration.  This is overridden
+   * to force no implementation.
+   */
+  protected void setTitleConfigFromConfig(Configuration allTitles) {
+    // No implementation.
+  }
+
+  public String getVersion() {
+    return CURRENT_VERSION;
+  }
+
+  public String getPluginName() {
+    return PLUGIN_NAME;
+  }
+
+  /**
+   * We only have one defining attribute, a base URL.
+   */
+  public List getLocalAuConfigDescrs() {
+    return m_auConfigDescrs;
+  }
+}
