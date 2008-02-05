a7f3636b3c99eefb57e77419e73fcba13f105c4e
thibgc <github@thib.org>
2008-02-05 20:07:21 +0000

    Cosmetic.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@6892 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/acs/AcsHtmlFilterFactory.java
+++ plugins/src/org/lockss/plugin/acs/AcsHtmlFilterFactory.java
@@ -1,10 +1,10 @@
 /*
- * $Id: AcsHtmlFilterFactory.java,v 1.1 2007-06-12 23:28:43 troberts Exp $
+ * $Id: AcsHtmlFilterFactory.java,v 1.2 2008-02-05 20:07:21 thib_gc Exp $
  */
 
 /*
 
-Copyright (c) 2000-2006 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2008 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
