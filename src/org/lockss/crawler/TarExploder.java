1612140d20a7543fc9573f7b91205f201e31c57b
dshrosenthal <dshr@stanford.edu>
2008-02-10 03:10:07 +0000

    Issue 2788 - exploder reports "bytes fetched" incorrectly
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@6915 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/crawler/TarExploder.java
+++ src/org/lockss/crawler/TarExploder.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TarExploder.java,v 1.7 2008-02-06 20:37:21 dshr Exp $
+ * $Id: TarExploder.java,v 1.8 2008-02-10 03:10:07 dshr Exp $
  */
 
 /*
@@ -117,8 +117,9 @@ public class TarExploder extends Exploder {
 					     te.getSize(),
 					     te.getModTime().getTime(),
 					     tis, crawlSpec);
+	  long bytesStored = ae.getSize();
 	  logger.debug3("ArchiveEntry: " + ae.getName()
-			+ " bytes "  + ae.getSize());
+			+ " bytes "  + bytesStored);
           try {
 	    helper.process(ae);
           } catch (PluginException ex) {
@@ -131,6 +132,7 @@ public class TarExploder extends Exploder {
 	      storeEntry(ae);
 	      handleAddText(ae);
 	      goodEntries++;
+	      crawler.getCrawlerStatus().addContentBytesFetched(bytesStored);
 	    } else {
 	      ignoredEntries++;
 	    }
@@ -160,7 +162,6 @@ public class TarExploder extends Exploder {
 	  // Leave stub archive behind to prevent re-fetch
 	  byte[] dummy = { 0, };
 	  urlCacher.storeContent(new ByteArrayInputStream(dummy), arcProps);
-	  // XXX update stats
 	}
 	reTry = maxRetries+1;
       }
