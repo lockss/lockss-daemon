305ab737f5142d1fa18ac1490f6d658ba74766e2
thibgc <github@thib.org>
2008-02-23 00:37:21 +0000

    First cut at the BioMed Central plugin.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@6966 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/bmc/BioMedCentralHtmlFilterFactory.java
@@ -0,0 +1,67 @@
+/*
+ * $Id: BioMedCentralHtmlFilterFactory.java,v 1.1 2008-02-23 00:37:21 thib_gc Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2008 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.bmc;
+
+import java.io.InputStream;
+
+import org.htmlparser.filters.TagNameFilter;
+import org.lockss.daemon.PluginException;
+import org.lockss.filter.html.*;
+import org.lockss.plugin.*;
+import org.lockss.plugin.anthrosource.AnthroSourceHtmlFilterFactory.HtmlCompoundTransform;
+
+public class BioMedCentralHtmlFilterFactory implements FilterFactory {
+
+  public InputStream createFilteredInputStream(ArchivalUnit au, InputStream in,
+                                               String encoding)
+      throws PluginException {
+    HtmlTransform[] transforms = new HtmlTransform[] {
+        // Filter out <td class="topad">...</td>
+        HtmlNodeFilterTransform.exclude(HtmlNodeFilters.tagWithAttribute("td",
+                                                                         "class",
+                                                                         "topad")),
+        // Filter out <td class="topnav">...</td>
+        HtmlNodeFilterTransform.exclude(HtmlNodeFilters.tagWithAttribute("td",
+                                                                         "class",
+                                                                         "topnav")),
+        // Filter out <noscript>...</noscript>
+        HtmlNodeFilterTransform.exclude(new TagNameFilter("noscript")),
+        // Filter out <iframe>...</iframe>
+        HtmlNodeFilterTransform.exclude(new TagNameFilter("iframe")),
+    };
+    return new HtmlFilterInputStream(in,
+                                     encoding,
+                                     new HtmlCompoundTransform(transforms));
+  }
+
+}
