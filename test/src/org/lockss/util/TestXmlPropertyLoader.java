5406f3c08f4730f9e30c9534e0719599a61a868d
edwardsb1 <claire@lockss.org>
2008-02-29 01:16:54 +0000

    Changing 0x8c2 characters to normal spaces.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@6983 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/util/TestXmlPropertyLoader.java
+++ test/src/org/lockss/util/TestXmlPropertyLoader.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestXmlPropertyLoader.java,v 1.23 2008-02-28 23:09:32 edwardsb1 Exp $
+ * $Id: TestXmlPropertyLoader.java,v 1.24 2008-02-29 01:16:54 edwardsb1 Exp $
  */
 
 /*
@@ -932,25 +932,25 @@ public class TestXmlPropertyLoader extends LockssTestCase {
    * Tom would like XML files to be able to add lists in multiple points.  The following
    * new action would be allowed:
    * 
-   *   <property name="d">
-   *      <list>
-   *        <value>1</value>
-   *        <value>2</value>
-   *        <value>3</value>
-   *        <value>4</value>
-   *        <value>5</value>
-   *      </list>
-   *   </property>
+   *   <property name="d">
+   *      <list>
+   *        <value>1</value>
+   *        <value>2</value>
+   *        <value>3</value>
+   *        <value>4</value>
+   *        <value>5</value>
+   *      </list>
+   *   </property>
    *
-   *     . . . 
+   *     . . . 
    *
-   *   <property name="d">
-   *      <list append="true">
-   *        <value>6</value>
-   *        <value>7</value>
-   *        <value>8</value>
-   *      </list>
-   *  </property>
+   *   <property name="d">
+   *      <list append="true">
+   *        <value>6</value>
+   *        <value>7</value>
+   *        <value>8</value>
+   *      </list>
+   *  </property>
    */
   public void testListAppend() throws Exception {
     PropertyTree props = new PropertyTree();
