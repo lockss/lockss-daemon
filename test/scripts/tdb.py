4c2152430cb6acd436bc688310b51db051637c07
thibgc <github@thib.org>
2008-06-03 23:25:43 +0000

    Removed toy code. Added license statement.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@7243 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/scripts/tdb.py
+++ test/scripts/tdb.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python
 
+# $Id: tdb.py,v 1.2 2008-06-03 23:25:43 thib_gc Exp $
+#
+# Copyright (c) 2000-2008 Board of Trustees of Leland Stanford Jr. University,
+# all rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+# IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# Except as contained in this notice, the name of Stanford University shall not
+# be used in advertising or otherwise to promote the sale, use or other dealings
+# in this Software without prior written authorization from Stanford University.
+
 class TdbObject(object):
 
     def __init__(self):
@@ -112,20 +138,3 @@ class Tdb(object):
 
     def internal_print(self):
         print (self.__publishers, self.__titles, self.__aus)
-
-if __name__ == '__main__':
-    tdb = Tdb()
-    p_foo = Publisher()
-    p_foo.set_name('Society of Foo')
-    t_foo = Title()
-    t_foo.set_publisher(p_foo)
-    t_foo.set_name('Journal of Foo')
-    a_foo1 = AU()
-    a_foo1.set_title(t_foo)
-    a_foo1.set_name('Journal of Foo Volume 1')
-    a_foo2 = AU()
-    a_foo2.set_title(t_foo)
-    a_foo2.set_name('Journal of Foo Volume 2')
-    print a_foo1.name()
-    print a_foo1.title().name()
-    print a_foo1.title().publisher().name()
--- test/scripts/tdbparse.py
+++ test/scripts/tdbparse.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python
 
+# $Id: tdbparse.py,v 1.8 2008-06-03 23:25:41 thib_gc Exp $
+#
+# Copyright (c) 2000-2008 Board of Trustees of Leland Stanford Jr. University,
+# all rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+# IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# Except as contained in this notice, the name of Stanford University shall not
+# be used in advertising or otherwise to promote the sale, use or other dealings
+# in this Software without prior written authorization from Stanford University.
+
 import re
 from tdb import *
 
@@ -567,10 +593,3 @@ class TdbParser(object):
         self.__current_title = None
         self.__current_au = [ None ]
         self.__stack = []
-
-if __name__ == '__main__':
-    from sys import stdin
-    scanner = TdbScanner(stdin)
-    parser = TdbParser(scanner)
-    tdb = parser.parse()
-    tdb.internal_print()
