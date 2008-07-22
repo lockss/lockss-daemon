39ac6c52680fa92f172feef7f8a1d2e02475fd29
tlipkis <tal@pss.com>
2008-07-22 07:17:59 +0000

    Added/improved release() methods to ensure files held by  DiskVoteBlocks.Iterator are closed promptly. Ensure userdata released promptly.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@7369 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/protocol/VoteBlocksIterator.java
+++ src/org/lockss/protocol/VoteBlocksIterator.java
@@ -1,3 +1,35 @@
+/*
+ * $Id: VoteBlocksIterator.java,v 1.6 2008-07-22 07:17:58 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2008 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
 package org.lockss.protocol;
 
 import java.util.*;
@@ -9,14 +41,15 @@ import org.lockss.util.*;
  */
 public interface VoteBlocksIterator extends LockssSerializable {
   
-  /** An empty, immutable VoteBlock iterator.  Calling next() or
-   * peek() throws NoSuchElementException.
+  /** An empty, immutable VoteBlock iterator.  Calling next() throws
+   * NoSuchElementException.
    */
   public static final VoteBlocksIterator EMPTY_ITERATOR =
     new VoteBlocksIterator() {
       public boolean hasNext() { return false; }
       public VoteBlock next() { throw new NoSuchElementException(); }
-      public VoteBlock peek() { throw new NoSuchElementException(); }
+      public VoteBlock peek() { return null; }
+      public void release() { }
     };
 
   /**
@@ -27,19 +60,26 @@ public interface VoteBlocksIterator extends LockssSerializable {
   public boolean hasNext() throws IOException;
   
   /**
-   * Returns the next element in the iteration.
+   * Returns the next VoteBlock in the iteration.
    * 
-   * @return The next element in the iteration.
+   * @return The next VoteBlock in the iteration
+   * VoteBlocks
    * @throws IOException
+   * @throws NoSuchElementException
    */
   public VoteBlock next() throws IOException;
   
   /**
-   * Returns the next element in the iteration, but does not move the iterator
-   * cursor forward. This method is idempotent.
+   * Returns the VoteBlock that next() would return, or null if next()
+   * would throw NoSuchElementException.  Does not advance the iterator
+   * cursor forward.
    * 
-   * @return The next element in the iteration.
+   * @return The next VoteBlock in the iteration, or null if no more
+   * VoteBlocks.
    */
   public VoteBlock peek() throws IOException;
+
+  /** Release any resources held by the iterator */
+  public void release();
 
 }
