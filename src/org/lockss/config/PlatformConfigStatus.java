bf7173fe18692253d7f9c1ce746934bc44d40bee
tlipkis <tal@pss.com>
2008-10-02 06:44:28 +0000

    Simplify code.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@7539 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/config/PlatformConfigStatus.java
+++ src/org/lockss/config/PlatformConfigStatus.java
@@ -1,5 +1,5 @@
 /*
- * $Id: PlatformConfigStatus.java,v 1.4 2008-09-19 14:14:35 dshr Exp $
+ * $Id: PlatformConfigStatus.java,v 1.5 2008-10-02 06:44:28 tlipkis Exp $
  */
 
 /*
@@ -123,17 +123,9 @@ public class PlatformConfigStatus extends BaseLockssDaemonManager {
 	     seplist(config.getList(PARAM_PLATFORM_DISK_SPACE_LIST)));
       addSum(res, "Cwd",
 	     PlatformUtil.getInstance().getCwd());
-      StringBuffer sb = new StringBuffer();
       List propsUrls = ConfigManager.getConfigManager().getConfigUrlList();
       if (propsUrls != null) {
-	for (ListIterator it = propsUrls.listIterator(); it.hasNext(); ) {
-	  String url = (String)it.next();
-	  sb.append(url);
-	  if (it.hasNext()) {
-	    sb.append(", ");
-	  }
-	}
-	addSum(res, "Props", sb.toString());
+	addSum(res, "Props", StringUtil.separatedString(propsUrls, ", "));
       }
       return res;
     }
