da9b93ddaec250f5e39086e25ab2503822fb3f3a
tlipkis <tal@pss.com>
2008-10-02 06:45:59 +0000

    Comment.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@7543 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/plugin/base/BaseUrlCacher.java
+++ src/org/lockss/plugin/base/BaseUrlCacher.java
@@ -1,5 +1,5 @@
 /*
- * $Id: BaseUrlCacher.java,v 1.78 2008-09-14 22:10:28 tlipkis Exp $
+ * $Id: BaseUrlCacher.java,v 1.79 2008-10-02 06:45:59 tlipkis Exp $
  */
 
 /*
@@ -648,7 +648,7 @@ public class BaseUrlCacher implements UrlCacher {
 
   String getUserPass() {
     Configuration auConfig = au.getConfiguration();
-    if (auConfig != null) {
+    if (auConfig != null) {		// can be null in unit tests
       return auConfig.get(ConfigParamDescr.USER_CREDENTIALS.getKey());
     }
     return null;
