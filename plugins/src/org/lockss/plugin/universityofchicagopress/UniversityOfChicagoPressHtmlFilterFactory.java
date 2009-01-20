7d3e1198c8c0e2839fb708dfa3be4e4403a4337d
thibgc <github@thib.org>
2009-01-20 22:25:44 +0000

    Cosmetic.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@7803 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/universityofchicagopress/UniversityOfChicagoPressHtmlFilterFactory.java
+++ plugins/src/org/lockss/plugin/universityofchicagopress/UniversityOfChicagoPressHtmlFilterFactory.java
@@ -1,10 +1,10 @@
 /*
- * $Id: UniversityOfChicagoPressHtmlFilterFactory.java,v 1.3 2009-01-20 22:15:17 thib_gc Exp $
+ * $Id: UniversityOfChicagoPressHtmlFilterFactory.java,v 1.4 2009-01-20 22:25:44 thib_gc Exp $
  */
 
 /*
 
-Copyright (c) 2000-2008 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2009 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
--- plugins/src/org/lockss/plugin/universityofchicagopress/UniversityOfChicagoPressHtmlLinkExtractorFactory.java
+++ plugins/src/org/lockss/plugin/universityofchicagopress/UniversityOfChicagoPressHtmlLinkExtractorFactory.java
@@ -1,10 +1,10 @@
 /*
- * $Id: UniversityOfChicagoPressHtmlLinkExtractorFactory.java,v 1.1 2008-06-10 21:05:02 thib_gc Exp $
+ * $Id: UniversityOfChicagoPressHtmlLinkExtractorFactory.java,v 1.2 2009-01-20 22:25:44 thib_gc Exp $
  */
 
 /*
 
-Copyright (c) 2000-2008 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2009 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
