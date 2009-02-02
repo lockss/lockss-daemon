567be20e9e248562265bfdd5bbd0ca78426c1a83
edwardsb1 <claire@lockss.org>
2009-02-02 18:23:24 +0000

    Changed 'serialize' from protected to public; it's needed by operiations outside org.lockss.util.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@7834 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/util/XStreamSerializer.java
+++ src/org/lockss/util/XStreamSerializer.java
@@ -1,5 +1,5 @@
 /*
- * $Id: XStreamSerializer.java,v 1.24 2006-12-06 18:21:11 thib_gc Exp $
+ * $Id: XStreamSerializer.java,v 1.25 2009-02-02 18:23:24 edwardsb1 Exp $
  */
 
 /*
@@ -641,7 +641,11 @@ public class XStreamSerializer extends ObjectSerializer {
   }
 
   /* Inherit documentation */
-  protected void serialize(Writer writer,
+  /**
+   * BEE - We need a two-way street.  If we're going to deserialize, then we
+   * need to serialize.
+   */
+  public void serialize(Writer writer,
                            Object obj)
       throws SerializationException,
              InterruptedIOException {
