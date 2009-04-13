71a23d09a9958cb1d5606742886c5808ab43e587
thibgc <github@thib.org>
2009-04-13 18:53:09 +0000

    Plugin cleanup.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@7999 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/applevalleyreview/AppleValleyReviewHtmlFilterFactory.java
+++ plugins/src/org/lockss/plugin/applevalleyreview/AppleValleyReviewHtmlFilterFactory.java
@@ -1,10 +1,10 @@
 /*
- * $Id: AppleValleyReviewHtmlFilterFactory.java,v 1.1 2007-03-23 23:12:07 troberts Exp $
+ * $Id: AppleValleyReviewHtmlFilterFactory.java,v 1.2 2009-04-13 18:53:09 thib_gc Exp $
  */
 
 /*
 
-Copyright (c) 2000-2006 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2009 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -33,11 +33,9 @@ in this Software without prior written authorization from Stanford University.
 package org.lockss.plugin.applevalleyreview;
 
 import java.io.*;
-import java.util.List;
 import org.lockss.util.*;
 import org.lockss.filter.*;
 import org.lockss.plugin.*;
-import org.lockss.plugin.base.*;
 
 public class AppleValleyReviewHtmlFilterFactory implements FilterFactory {
 
