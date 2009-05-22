2d4f02f5fc6cf64eb0d75d396180aff7a873f54e
thibgc <github@thib.org>
2009-05-22 19:14:49 +0000

    HTML link extractor for Ingenta
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@8095 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/ingenta/IngentaLinkExtractor.java
@@ -0,0 +1,62 @@
+/*
+ * $Id: IngentaLinkExtractor.java,v 1.1 2009-05-22 19:14:49 thib_gc Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2009 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.ingenta;
+
+import java.io.IOException;
+
+import org.lockss.extractor.GoslingHtmlLinkExtractor;
+import org.lockss.plugin.ArchivalUnit;
+
+public class IngentaLinkExtractor extends GoslingHtmlLinkExtractor {
+
+  public IngentaLinkExtractor() {
+    super();
+  }
+  
+  @Override
+  protected String extractLinkFromTag(StringBuffer link,
+                                      ArchivalUnit au,
+                                      Callback cb)
+      throws IOException {
+    char ch = link.charAt(0);
+    if ((ch == 'm' || ch == 'M') && beginsWithTag(link, METATAG)) {
+      String key = getAttributeValue("name", link);
+      if (key != null && key.startsWith("CRAWLER.")) {
+        return getAttributeValue("content", link);
+      }
+    }
+    
+    return super.extractLinkFromTag(link, au, cb);
+  }
+  
+}
