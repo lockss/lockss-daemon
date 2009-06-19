2b58d21882b9a77ef276a3cea9f537dc1086f4a6
tlipkis <tal@pss.com>
2009-06-19 08:22:32 +0000

    Fixed and refactored shouldParamBeLogged() logic.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/branches/branch_1-38@8229 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/config/ConfigManager.java
+++ src/org/lockss/config/ConfigManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ConfigManager.java,v 1.64.2.1 2009-06-09 05:48:29 tlipkis Exp $
+ * $Id: ConfigManager.java,v 1.64.2.2 2009-06-19 08:22:32 tlipkis Exp $
  */
 
 /*
@@ -1152,6 +1152,12 @@ public class ConfigManager implements LockssManager {
 	}
       }
     }
+  }
+
+  public static boolean shouldParamBeLogged(String key) {
+    return !(key.startsWith(ConfigManager.PARAM_TITLE_DB)
+	     || key.startsWith(PluginManager.PARAM_AU_TREE + ".")
+	     || StringUtils.endsWithIgnoreCase(key, "password"));
   }
 
   /**
