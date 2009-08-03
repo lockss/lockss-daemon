6208607277d5e717d930a2a59d34e0515db4a147
tlipkis <tal@pss.com>
2009-08-03 04:31:16 +0000

    A queue of CrawlUrls with user-specified sort order, plus a map of  String -> CrawlUrl
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@8355 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/crawler/CrawlQueue.java
@@ -0,0 +1,116 @@
+/*
+ * $Id: CrawlQueue.java,v 1.2 2009-08-03 04:31:16 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2009 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.crawler;
+
+import java.util.*;
+import java.io.*;
+import java.text.*;
+import org.apache.commons.collections.buffer.*;
+import org.lockss.util.*;
+
+/**
+ * CrawlQueue is a Map<String,CrawlUrl> sorted by value.  It is not
+ * synchronized.
+ */
+public class CrawlQueue {
+  static Logger log = Logger.getLogger("CrawlQueue");
+
+  PriorityBuffer sorted;
+  Map<String,CrawlUrl> map;
+
+  /** Create a CrawlQueue that sorts CrawlUrls by the specified comparator.
+   * If comparator is null, the urls are sorted first by depth then
+   * alphabetically, resulting in a breadth-first crawl.  The comparator
+   * should never return 0; if it does the results are undefined */
+  public CrawlQueue(Comparator<CrawlUrl> comparator) {
+    if (comparator == null) {
+      comparator = new BreadthFirstUrlOrderComparator();
+    }
+    sorted = new PriorityBuffer(comparator);
+    map = new HashMap<String,CrawlUrl>();
+  }
+
+  /** Add CrawlUrl to the queue.
+   * @throws IllegalArgumentException if the CrawlUrl is already in the
+   * queue
+   */
+  public void add(CrawlUrl curl) {
+    String url = curl.getUrl();
+    if (map.containsKey(url)) {
+      throw new IllegalArgumentException("Illegal to replace entry");
+    }
+    map.put(url, curl);
+    int size = sorted.size();
+    sorted.add(curl);
+    if (sorted.size() != size + 1) {
+      log.error("Adding " + curl +
+		" didn't increase size of crawl queue; "
+		+ "CrawlUrlComparator must have returned zero");
+    }
+  }
+
+  /** Return the first CrawlUrl in the queue */
+  public CrawlUrl first() {
+    return (CrawlUrl)sorted.get();
+  }
+
+  /** Remove the first CrawlUrl from the queue and return it */
+  public CrawlUrl remove() {
+    CrawlUrl res = (CrawlUrl)sorted.remove();
+    map.remove(res.getUrl());
+    return res;
+  }
+
+  /** Retun the CrawlUrl corresponding to the URL */
+  public CrawlUrl get(String url) {
+    return map.get(url);
+  }
+
+  public boolean isEmpty() {
+    return map.isEmpty();
+  }
+
+  public String toString() {
+    return "[" + StringUtil.separatedString(sorted, ", ") + "]";
+  }
+
+  static class BreadthFirstUrlOrderComparator implements Comparator<CrawlUrl> {
+    public int compare(CrawlUrl curl1, CrawlUrl curl2) {
+      int res = curl1.getDepth() - curl2.getDepth();
+      if (res == 0) {
+	res = curl1.getUrl().compareTo(curl2.getUrl());
+      }
+      return res;
+    }
+  }
+}
---
+++ test/src/org/lockss/crawler/TestCrawlQueue.java
@@ -0,0 +1,126 @@
+/*
+ * $Id: TestCrawlQueue.java,v 1.2 2009-08-03 04:31:16 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2009 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.crawler;
+
+import java.util.*;
+import org.lockss.test.*;
+import org.lockss.util.*;
+
+public class TestCrawlQueue extends LockssTestCase {
+  public void testIll() {
+    CrawlQueue cq = new CrawlQueue(null);
+    CrawlUrl c1 = new CrawlUrl("u1", 0);
+    assertTrue(cq.isEmpty());
+    cq.add(c1);
+    assertFalse(cq.isEmpty());
+    try {
+      cq.add(c1);
+      fail("Should not be able to re-add element");
+    } catch (IllegalArgumentException e) {
+    }
+  }
+
+  public void testDefaultSort() {
+    CrawlQueue cq = new CrawlQueue(null);
+    CrawlUrl c1 = new CrawlUrl("x1", 0);
+    CrawlUrl c2 = new CrawlUrl("u3", 1);
+    CrawlUrl c3 = new CrawlUrl("u2", 1);
+    CrawlUrl c4 = new CrawlUrl("u4", 2);
+    CrawlUrl c5 = new CrawlUrl("a5", 3);
+    
+    assertTrue(cq.isEmpty());
+    assertNull(cq.get(c4.getUrl()));
+    cq.add(c4);
+    assertFalse(cq.isEmpty());
+    assertSame(c4, cq.get(c4.getUrl()));
+    assertSame(c4, cq.first());
+    cq.add(c3);
+    assertSame(c3, cq.first());
+    assertSame(c3, cq.get(c3.getUrl()));
+    assertSame(c4, cq.get(c4.getUrl()));
+    cq.add(c2);
+    assertSame(c3, cq.first());
+    cq.add(c1);
+    assertSame(c1, cq.first());
+    cq.add(c5);
+    assertSame(c1, cq.first());
+
+    List lst = new ArrayList();
+    while (!cq.isEmpty()) {
+      lst.add(cq.remove());
+    }
+    assertEquals(ListUtil.list(c1, c3, c2, c4, c5), lst);
+  }
+
+  public void testCustomSort() {
+    CrawlQueue cq = new CrawlQueue(new DeepestFirstUrlOrderComparator());
+    CrawlUrl c1 = new CrawlUrl("u1", 0);
+    CrawlUrl c2 = new CrawlUrl("u3", 1);
+    CrawlUrl c3 = new CrawlUrl("u2", 1);
+    CrawlUrl c4 = new CrawlUrl("u4", 2);
+    CrawlUrl c5 = new CrawlUrl("u5", 3);
+    
+    assertTrue(cq.isEmpty());
+    assertNull(cq.get(c4.getUrl()));
+    cq.add(c4);
+    assertFalse(cq.isEmpty());
+    assertSame(c4, cq.get(c4.getUrl()));
+    assertSame(c4, cq.first());
+    cq.add(c3);
+    assertSame(c4, cq.first());
+    assertSame(c3, cq.get(c3.getUrl()));
+    assertSame(c4, cq.get(c4.getUrl()));
+    cq.add(c2);
+    assertSame(c4, cq.first());
+    cq.add(c1);
+    assertSame(c4, cq.first());
+    cq.add(c5);
+    assertSame(c5, cq.first());
+
+    List lst = new ArrayList();
+    while (!cq.isEmpty()) {
+      lst.add(cq.remove());
+    }
+    assertEquals(ListUtil.list(c5, c4, c3, c2, c1), lst);
+  }
+
+  class DeepestFirstUrlOrderComparator implements Comparator<CrawlUrl> {
+    public int compare(CrawlUrl curl1, CrawlUrl curl2) {
+      int res = curl2.getDepth() - curl1.getDepth();
+      if (res == 0) {
+	res = curl1.getUrl().compareTo(curl2.getUrl());
+      }
+      return res;
+    }
+  }
+}
