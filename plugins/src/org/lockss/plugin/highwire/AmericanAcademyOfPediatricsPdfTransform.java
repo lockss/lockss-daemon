6df528b542dd0081b3e05ff5128afe5c7109cb7a
thibgc <github@thib.org>
2009-09-22 20:57:25 +0000

    Additional AAP support.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@8593 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/highwire/AmericanAcademyOfPediatricsPdfTransform.java
+++ plugins/src/org/lockss/plugin/highwire/AmericanAcademyOfPediatricsPdfTransform.java
@@ -1,3 +1,35 @@
+/*
+ * $Id: AmericanAcademyOfPediatricsPdfTransform.java,v 1.2 2009-09-22 20:57:25 thib_gc Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2009 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
 package org.lockss.plugin.highwire;
 
 import java.io.*;
@@ -11,6 +43,28 @@ import org.lockss.util.*;
 public class AmericanAcademyOfPediatricsPdfTransform implements
     OutputDocumentTransform,
     ArchivalUnitDependent {
+
+  public static class Simplified
+      extends ResilientTextScrapingDocumentTransform
+      implements ArchivalUnitDependent {
+
+    protected ArchivalUnit au;
+
+    public void setArchivalUnit(ArchivalUnit au) {
+      this.au = au;
+    }
+
+    public DocumentTransform makePreliminaryTransform() throws IOException {
+      if (au == null) throw new IOException("Uninitialized AU-dependent transform");
+      return new ConditionalDocumentTransform(// If on the first page...
+                                              new TransformFirstPage(// ...collapsing "Downloaded from" succeeds,
+                                                                     new CollapseDownloadedFrom(au)),
+                                              // Then on all other pages...
+                                              new TransformEachPageExceptFirst(// ...collapse "Downloaded from"
+                                                                               new CollapseDownloadedFrom(au)));
+    }
+
+  }
 
   protected ArchivalUnit au;
 
