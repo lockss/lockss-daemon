91ebb8e1142177d2c78ac35cae722f89646428b1
edwardsb1 <claire@lockss.org>
2010-02-02 01:05:14 +0000

    Adding the monthly DOI report generating programs to the source.
    IMPORTANT: these programs are usually run by a program, "run-burp.sh", which is NOT checked into SourceForge.  "run-burp.sh" contains password information that can't be shared outside LOCKSS people. Contact me (Brent) for copies of this program.
    
    
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@8962 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/scripts/BurpReport.py
@@ -0,0 +1,252 @@
+#!/usr/bin/python
+'''
+Created on Dec 14, 2009
+
+@author: edwardsb
+
+This program generates the Burp report, in the format that Vicky requested.
+
+[We would like...] a table that was organized with all the data in one spreadsheet, for example:
+              PUBLISHER           PUBLISHER           PUBLISHER
+YEAR      # articles ingested # articles ingested # articles ingested
+YEAR      # articles ingested # articles ingested # articles ingested
+YEAR     # articles ingested # articles ingested # articles ingested 
+'''
+
+from datetime import date
+import MySQLdb
+import optparse
+import os
+import sys
+
+# Constants
+OPTION_BURP_VERSION             = '0.2.1'
+
+OPTION_SHORT                    = '-'
+OPTION_LONG                     = '--'
+
+OPTION_DATABASE_PASSWORD        = 'dbpassword'
+OPTION_DATABASE_PASSWORD_SHORT  = 'D'
+
+OPTION_YEAR_CURRENT             = 'currentyear'
+OPTION_YEAR_CURRENT_SHORT       = 'c'
+DEFAULT_YEAR_CURRENT            = date.today().year
+
+OPTION_YEAR_MINIMUM             = 'minimumyear'
+OPTION_YEAR_MINIMUM_SHORT       = 'm'
+DEFAULT_YEAR_MINIMUM            = 1883
+
+OPTION_DATE_REPORT              = 'reportdatestart'
+OPTION_DATE_REPORT_SHORT        = 'r'
+DEFAULT_DATE_REPORT             = date.today()
+
+OPTION_DATE_REPORT_END          = 'reportdateend'
+OPTION_DATE_REPORT_END_SHORT    = 'e'
+DEFAULT_DATE_REPORT_END         = date.today()
+
+OPTION_FILENAME_SHORT           = 'f'
+OPTION_FILENAME                 = 'filename'
+DEFAULT_FILENAME                = 'current.csv'
+
+OPTION_SUMMARY_SHORT            = 's'
+OPTION_SUMMARY                  = 'summary'
+DEFAULT_SUMMARY                 = 'summary.csv'
+
+def _make_command_line_parser():
+    from optparse import OptionGroup, OptionParser
+
+    parser = OptionParser(version=OPTION_BURP_VERSION)
+
+    # The database password
+    parser.add_option(OPTION_SHORT + OPTION_DATABASE_PASSWORD_SHORT,
+                      OPTION_LONG + OPTION_DATABASE_PASSWORD,
+                      dest=OPTION_DATABASE_PASSWORD,
+                      help="The password for the database (required)")
+    
+    # Minimum and current years
+    parser.add_option(OPTION_SHORT + OPTION_YEAR_CURRENT_SHORT,
+                      OPTION_LONG + OPTION_YEAR_CURRENT,
+                      dest=OPTION_YEAR_CURRENT,
+                      default=DEFAULT_YEAR_CURRENT,
+                      help="The current year (default: %default)")
+    parser.add_option(OPTION_SHORT + OPTION_YEAR_MINIMUM_SHORT,
+                      OPTION_LONG + OPTION_YEAR_MINIMUM,
+                      dest=OPTION_YEAR_MINIMUM,
+                      default=DEFAULT_YEAR_MINIMUM,
+                      help="The first year to report on (default: %default)")
+
+    # The date for the report: the date that the 'burp.py' program was run.
+    parser.add_option(OPTION_SHORT + OPTION_DATE_REPORT_SHORT,
+                      OPTION_LONG + OPTION_DATE_REPORT,
+                      dest=OPTION_DATE_REPORT,
+                      default=DEFAULT_DATE_REPORT,
+                      help="The start date the information was gathered (default: %default)")
+
+    parser.add_option(OPTION_SHORT + OPTION_DATE_REPORT_END_SHORT,
+                      OPTION_LONG + OPTION_DATE_REPORT_END,
+                      dest=OPTION_DATE_REPORT_END,
+                      default=DEFAULT_DATE_REPORT_END,
+                      help="The end date the information was gathered (default: %default)")
+
+    parser.add_option(OPTION_SHORT + OPTION_FILENAME_SHORT,
+                      OPTION_LONG + OPTION_FILENAME,
+                      dest=OPTION_FILENAME,
+                      default=DEFAULT_FILENAME,
+                      help="The filename for the main report (default: %default)")
+
+    parser.add_option(OPTION_SHORT + OPTION_SUMMARY_SHORT,
+                      OPTION_LONG + OPTION_SUMMARY,
+                      dest=OPTION_SUMMARY,
+                      default=DEFAULT_SUMMARY,
+                      help="The filename for the summary (default: %default)")
+
+    return parser
+
+
+def _check_required_options(parser, options):
+    if options.dbpassword is None:
+        parser.error('%s%s/%s%s is required' % (OPTION_LONG, OPTION_DATABASE_PASSWORD, OPTION_SHORT, OPTION_DATABASE_PASSWORD_SHORT))
+
+def _print_summary_line(summary, publishername, publisherid, currentyear, pubyear, total):
+    summary.write(publishername + "," + publisherid.upper() + ",")
+    summary.write(str(pubyear[publisherid][currentyear]) + ",")
+    summary.write(str(total[publisherid]) + "\n")
+
+def _main_procedure():
+    parser = _make_command_line_parser()
+    (options, args) = parser.parse_args(values=parser.get_default_values())
+    _check_required_options(parser, options)
+
+    # Initialize the hashes.
+    # WARNING: If you update this list, you need to update the
+    # summary report as well.
+    publishers = ['aap', 'aip', 'ama', 'aps', 'acm', 'bep', 'bmc', 'eup', 'elsevier', 'iop', 'wiley', 'lup', 'npg', 'oup', 'rsc', 'sage', 'springer', 'tf', 'rup', 'rs']
+
+    total = {}
+    pubyear = {}
+
+    for publisher in publishers:
+        pubyear[publisher] = {}
+        total[publisher] = 0
+
+        for year in range(options.currentyear, options.minimumyear, -1) + ["0"]:
+            strYear = str(year)
+            pubyear[publisher][strYear] = 0
+
+    db = MySQLdb.connect(host="localhost", user="edwardsb", passwd=options.dbpassword, db="burp")
+
+    cursorAuid = db.cursor()
+    cursorAuid.execute("SELECT DISTINCT(auid) from burp WHERE rundate >= DATE('" + str(options.reportdatestart) + "') AND rundate <= DATE('" + str(options.reportdateend) + "') order by auid;")
+    
+    auid = cursorAuid.fetchone()
+    while auid is not None:
+        cursorArticles = db.cursor()
+        cursorArticles.execute("SELECT MAX(numarticles), auyear FROM burp WHERE auid = \"" + auid[0] + "\" and rundate >= DATE('" + str(options.reportdatestart) + "') AND rundate <= DATE('" + str(options.reportdateend) +"');")
+        articles = cursorArticles.fetchone()
+        strYear = articles[1]
+        if "-" in strYear:
+            strYear = strYear[5:9]
+            
+        if (auid[0].find("ClockssHighWirePlugin") != -1) and (auid[0].find("aappublications") != -1):
+            pubyear['aap'][strYear] += articles[0]
+            total['aap'] += articles[0]
+            
+        if (auid[0].find("ClockssHighWirePlugin") != -1) and (auid[0].find("ama-assn") != -1):
+            pubyear['ama'][strYear] += articles[0]
+            total['ama'] += articles[0]
+            
+        if (auid[0].find("ClockssHighWirePlugin") != -1) and (auid[0].find("physiology%2Eorg") != -1):
+            pubyear['aps'][strYear] += articles[0]
+            total['aps'] += articles[0]
+            
+        if (auid[0].find("ClockssBerkeleyElectronicPressPlugin") != -1):
+            pubyear['bep'][strYear] += articles[0]
+            total['bep'] += articles[0]
+            
+        if (auid[0].find("ClockssNaturePublishingGroupPlugin") != -1):
+            pubyear['npg'][strYear] += articles[0]
+            total['npg'] += articles[0]
+            
+        if (auid[0].find("HighWire") != -1) and (auid[0].find("oxfordjournals") != -1):
+            pubyear['oup'][strYear] += articles[0]
+            total['oup'] += articles[0]
+            
+        if (auid[0].find("ClockssRoyalSocietyOfChemistryPlugin") != -1):
+            pubyear['rsc'][strYear] += articles[0]
+            total['rsc'] += articles[0]
+            
+        if (auid[0].find("HighWire") != -1) and (auid[0].find("sagepub") != -1):
+            pubyear['sage'][strYear] += articles[0]
+            total['sage'] += articles[0]
+        
+        auid = cursorAuid.fetchone()  
+
+    # OUP has some AUIDs that are listed as year 0.  This code assumes that the
+    # titles are split between 2008 and 2009 in the same proportion.
+    oup0 = float(pubyear['oup']["0"])
+
+    if oup0 > 0 and pubyear['oup']['2009'] > 0 and pubyear['oup']['2008'] > 0:
+        pubyear['oup']["2009"] += oup0 * (74.0 / 104.0)
+        pubyear['oup']["2008"] += oup0 * (30.0 / 104.0)
+
+    # Output the main report.
+    filename = open(options.filename, 'w')
+    filename.write("Date of ingest," + str(options.reportdatestart) + "\n")
+    filename.write("Date of report," + str(date.today()) + "\n")
+    filename.write("year,")
+    for publisher in publishers:
+        filename.write(publisher.upper() + ",")
+    # Yes, there's an extra comma at the end.  Same with everything else.
+    # If that's a problem, we can fix it.
+    filename.write("\n"); 
+
+    for year in range(options.currentyear, options.minimumyear, -1):
+        strYear = str(year)
+        printYear = strYear
+        if (year == options.currentyear):
+            printYear = "Current Ingest " + strYear
+        filename.write( printYear + "," )
+
+        for publisher in publishers:
+            filename.write(str(pubyear[publisher][strYear]) + ",")
+        filename.write("\n")
+
+    filename.write("Back Ingest Total,")
+    for publisher in publishers:
+        filename.write(str(total[publisher]) + ",")
+    filename.write("\n")
+
+    # Output the new summary report.
+    # WARNING: I don't have a simple way to keep track of names compared
+    # against publishers.  You need to update these in two places: the
+    # list called 'publishers' and here.
+    
+    currentyear = str(options.currentyear)
+    summary = open(options.summary, "w")
+
+    summary.write("Official Publisher Name,Publisher ID, Total Ingest for " + str(currentyear) + ", Total Ingest For all time\n")
+    
+    _print_summary_line(summary, "American Academy of Pediatrics", "aap", currentyear, pubyear, total)
+    _print_summary_line(summary, "American Institute of Physics", "aip", currentyear, pubyear, total)
+    _print_summary_line(summary, "American Medical Association", "ama", currentyear, pubyear, total)
+    _print_summary_line(summary, "American Physiological Society", "aps", currentyear, pubyear, total)
+    _print_summary_line(summary, "Association for Computing Machinery", "acm", currentyear, pubyear, total)
+    _print_summary_line(summary, "Berkeley Electronic Press", "bep", currentyear, pubyear, total)
+    _print_summary_line(summary, "BioMed Central", "bmc", currentyear, pubyear, total)
+    _print_summary_line(summary, "Edinburgh University Press", "eup", currentyear, pubyear, total)
+    _print_summary_line(summary, "Elsevier", "elsevier", currentyear, pubyear, total)
+    _print_summary_line(summary, "IOP Publishing", "iop", currentyear, pubyear, total)
+    _print_summary_line(summary, "John Wiley and Sons", "wiley", currentyear, pubyear, total)
+    _print_summary_line(summary, "Liverpool University Press", "lup", currentyear, pubyear, total)
+    _print_summary_line(summary, "Nature Publishing Group", "npg", currentyear, pubyear, total)
+    _print_summary_line(summary, "Oxford University Press", "oup", currentyear, pubyear, total)
+    _print_summary_line(summary, "RSC Publishing", "rsc", currentyear, pubyear, total)
+    _print_summary_line(summary, "Sage", "sage", currentyear, pubyear, total)
+    _print_summary_line(summary, "Springer", "springer", currentyear, pubyear, total)
+    _print_summary_line(summary, "Taylor and Francis", "tf", currentyear, pubyear, total)
+    _print_summary_line(summary, "The Rockefeller University Press", "rup", currentyear, pubyear, total)
+    _print_summary_line(summary, "The Royal Society", "rs", currentyear, pubyear, total)
+
+
+if __name__ == '__main__':    
+    _main_procedure()
---
+++ test/scripts/burp.py
@@ -0,0 +1,205 @@
+#!/usr/bin/python
+
+'''
+Created on Dec 4, 2009
+
+@author: edwardsb, thibgc
+
+This script is part of the toolset to generate the monthly DOI report.
+(The other two parts are "BurpReport.py", and "run-burp.py".)  It queries
+one other machine (given as the -H parameter), and inserts its results into
+a database.
+
+It assumes that there is a MySQL database program running, with the following 
+table created:
+
+create table burp(machinename VARCHAR(127), port INT, rundate DATE, auname VARCHAR(255), 
+auid VARCHAR(2047), auyear VARCHAR(32), austatus VARCHAR(255), aulastcrawlresult VARCHAR(255), 
+aucontentsize VARCHAR(255), audisksize REAL, aurepository VARCHAR(255), numarticles INT, 
+publisher VARCHAR(16));
+'''
+
+import MySQLdb
+import optparse
+import os
+import sys
+
+sys.path.append(os.path.realpath(os.path.join(os.path.dirname(sys.argv[0]), '../frameworks/lib')))
+import lockss_daemon
+
+
+BURP_VERSION = '0.2.1'
+
+OPTION_LONG  = '--'
+OPTION_SHORT = '-'
+
+OPTION_ARTICLE_REPORT           = 'article'
+OPTION_ARTICLE_REPORT_SHORT     = 'a'
+#OPTION_REPLICATION_REPORT       = 'replication'
+#OPTION_REPLICATION_REPORT_SHORT = 'r'
+
+OPTION_TYPE                     = 'type'
+OPTION_TYPE_SHORT               = 't'
+DEFAULT_TYPE                    = OPTION_ARTICLE_REPORT
+
+OPTION_HOST                     = 'host'
+OPTION_HOST_SHORT               = 'H'
+OPTION_PASSWORD                 = 'password'
+OPTION_PASSWORD_SHORT           = 'P'
+DEFAULT_PASSWORD                = 'lockss-p'
+OPTION_USERNAME                 = 'username'
+OPTION_USERNAME_SHORT           = 'U'
+DEFAULT_USERNAME                = 'lockss-u'
+
+OPTION_DATABASE_PASSWORD        = 'dbpassword'
+OPTION_DATABASE_PASSWORD_SHORT  = 'D'
+
+PARAM_READY_TIMEOUT             = 30
+PARAM_REPEAT_LIST_ARTICLES      = 5  
+
+def _make_command_line_parser():
+    from optparse import OptionGroup, OptionParser
+    parser = OptionParser(version=BURP_VERSION)
+
+    parser.add_option(OPTION_SHORT + OPTION_ARTICLE_REPORT_SHORT,
+                      OPTION_LONG + OPTION_ARTICLE_REPORT,
+                      dest=OPTION_TYPE,
+                      action='store_const',
+                      const=OPTION_ARTICLE_REPORT,
+                      help='produce an article report')
+    parser.add_option(OPTION_SHORT + OPTION_HOST_SHORT,
+                      OPTION_LONG + OPTION_HOST,
+                      dest=OPTION_HOST,
+                      help='daemon hostname:port (required)')
+    parser.add_option(OPTION_SHORT + OPTION_PASSWORD_SHORT,
+                      OPTION_LONG + OPTION_PASSWORD,
+                      dest=OPTION_PASSWORD,
+                      default=DEFAULT_PASSWORD,
+                      help='daemon UI password (default: %default)')
+    parser.add_option(OPTION_SHORT + OPTION_TYPE_SHORT,
+                      OPTION_LONG + OPTION_TYPE,
+                      dest=OPTION_TYPE,
+                      default=DEFAULT_TYPE,
+                      help='type of report to produce (default: %default)')
+    parser.add_option(OPTION_SHORT + OPTION_USERNAME_SHORT,
+                      OPTION_LONG + OPTION_USERNAME,
+                      dest=OPTION_USERNAME,
+                      default=DEFAULT_USERNAME,
+                      help='daemon UI username (default: %default)')
+    
+    # The database password
+    parser.add_option(OPTION_SHORT + OPTION_DATABASE_PASSWORD_SHORT,
+                      OPTION_LONG + OPTION_DATABASE_PASSWORD,
+                      dest=OPTION_DATABASE_PASSWORD,
+                      help="The password for the database (required)")
+    
+    return parser
+    
+def _check_required_options(parser, options):
+    if options.host is None:
+        parser.error('%s%s/%s%s is required' % (OPTION_LONG, OPTION_HOST, OPTION_SHORT, OPTION_HOST_SHORT))
+    if options.dbpassword is None:
+        parser.error('%s%s/%s%s is required' % (OPTION_LONG, OPTION_DATABASE_PASSWORD, OPTION_SHORT, OPTION_DATABASE_PASSWORD_SHORT))
+
+def _make_client(options):
+    client =  lockss_daemon.Client(options.host.split(':',1)[0],
+                                   options.host.split(':',1)[1],
+                                   options.username,
+                                   options.password)
+    if not client.waitForDaemonReady(PARAM_READY_TIMEOUT):
+        raise RuntimeError, '%s is not ready after %d seconds' % (options.host, PARAM_READY_TIMEOUT)
+    return client
+    
+def _get_auids(client):
+    table = client._getStatusTable('AuIds')[1]
+    auids = []
+    auname = {}
+    for map in table:
+        id = map['AuId']
+        auids.append(id)
+        auname[id] = map['AuName']['value']
+    return auids, auname
+
+def _get_list_articles(client, auid, auarticles):
+    reps = 0
+
+    while (reps < PARAM_REPEAT_LIST_ARTICLES):
+        try:
+            lst = client.getListOfArticles(lockss_daemon.AU(auid))
+            val = []
+            if lst is not None and len(lst) > 0: val = lst.splitlines()[2:]
+            auarticles[auid] = val
+            break
+        except urllib2.URLError:
+            reps = reps + 1
+            print "_get_list_articles has URLError.  This is repeat %d." % (reps,)
+    else:
+        raise RuntimeError, '%s did not give the list of articles after %d tries' % (options.host, PARAM_REPEAT_LIST_ARTICLES)
+
+def _get_list_urls(client, auid, auarticles):
+    lst = client.getListOfUrls(lockss_daemon.AU(auid))
+    val = []
+    if lst is not None and len(lst) > 0:
+        for art in lst.splitlines()[2:]:
+            if art.startswith('http://www.rsc.org/publishing/journals/') and art.find('/article.asp?doi=') >= 0:
+                val.append(art)
+    auarticles[auid] = val
+    
+def _article_report(client, db, options):
+    auids, auname = _get_auids(client)
+    auyear = {}
+    austatus = {}
+    aulastcrawlresult = {}
+    aucontentsize = {}
+    audisksize = {}
+    aurepository = {}
+    auarticles = {}
+    cursor = db.cursor()
+    for auid in auids:
+        summary, table = client._getStatusTable('ArchivalUnitTable', auid)
+        auyear[auid] = summary.get('Year', 0)
+        austatus[auid] = summary.get('Status')
+        aulastcrawlresult[auid] = summary.get('Last Crawl Result', 'n/a')
+        aucontentsize[auid] = summary.get('Content Size').replace(",", "")
+        audisksize[auid] = summary.get('Disk Usage (MB)', 'n/a')
+        aurepository[auid] = summary.get('Repository')
+        _get_list_articles(client, auid, auarticles)
+      
+        # Because it's hard to know if the Burp is running without SOME feedback...
+        print options.host + ":" + auname[auid]
+        
+        # Standard article...
+        cursor.execute("""INSERT INTO burp(machinename, port, rundate, 
+auname, auid, auyear, austatus, aulastcrawlresult, aucontentsize, audisksize, 
+aurepository, numarticles, publisher)
+VALUES ("%s", "%s", CURRENT_DATE(), "%s", "%s", "%s", "%s", "%s", "%s", "%s", 
+"%s", "%s", "default")"""  % \
+                       (options.host.split(':',1)[0], options.host.split(':',1)[1], auname[auid], auid, auyear[auid], austatus[auid], aulastcrawlresult[auid], aucontentsize[auid], audisksize[auid], aurepository[auid], int(len(auarticles[auid]))))
+        # Note: There is no article iterator for RSC.  This is a work-around.
+        if auid.find('ClockssRoyalSocietyOfChemistryPlugin') >= 0 and (options.host.find("ingest") >= 0):
+            _get_list_urls(client, auid, auarticles)
+            cursor.execute("""INSERT INTO burp(machinename, port, rundate, 
+auname, auid, auyear, numarticles, publisher)
+VALUES ("%s", %d, CURRENT_DATE(), "%s", "%s", "%s", %d, "rsc")""" % \
+                            (options.host.split(':',1)[0], int(options.host.split(':',1)[1]), auname[auid], auid, auyear[auid], len(auarticles[auid])))
+    
+    print "****** " + options.host + " finished ******"
+
+            
+def _main_procedure():
+    parser = _make_command_line_parser()
+    (options, args) = parser.parse_args(values=parser.get_default_values())
+    _check_required_options(parser, options)
+    client = _make_client(options)
+
+    db = MySQLdb.connect(host="localhost", user="edwardsb", passwd=options.dbpassword, db="burp")
+    
+# Send the reports
+    if (options.type == OPTION_ARTICLE_REPORT): _article_report(client, db, options)
+    else: parser.error('unknown report type: %s' % (options.type,))
+
+    db.commit()
+    db.close()
+
+if __name__ == '__main__':    
+    _main_procedure()
