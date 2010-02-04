8be8ac7f73a704a2f0b37bc3f0b40d77cea2923f
tlipkis <tal@pss.com>
2010-02-04 06:53:21 +0000

    Logging.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@8980 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/protocol/BlockingStreamComm.java
+++ src/org/lockss/protocol/BlockingStreamComm.java
@@ -1,5 +1,5 @@
 /*
- * $Id: BlockingStreamComm.java,v 1.49 2009-11-03 05:56:06 tlipkis Exp $
+ * $Id: BlockingStreamComm.java,v 1.50 2010-02-04 06:53:21 tlipkis Exp $
  */
 
 /*
@@ -1416,7 +1416,7 @@ public class BlockingStreamComm
       java.security.cert.Certificate[] certs = session.getPeerCertificates();
       log.debug(session.getPeerHost() + " via " + session.getProtocol() + " verified");
     } catch (SSLPeerUnverifiedException ex) {
-      log.error(s.getInetAddress() + " not verified");
+      log.error(s.getInetAddress() + ":" + s.getPort() + " not verified");
       try {
         s.close();
       } catch (IOException ex2) {
