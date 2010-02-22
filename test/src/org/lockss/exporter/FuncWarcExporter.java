b0015dedfdaae720e96d4e6a4bad45dcb1b4ffc2
tlipkis <tal@pss.com>
2010-02-22 08:07:46 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@9081 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/exporter/FuncWarcExporter.java
+++ test/src/org/lockss/exporter/FuncWarcExporter.java
@@ -1,10 +1,10 @@
 /*
- * $Id: FuncWarcExporter.java,v 1.2 2010-02-22 07:01:07 tlipkis Exp $
+ * $Id: FuncWarcExporter.java,v 1.3 2010-02-22 08:07:46 tlipkis Exp $
  */
 
 /*
 
-Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
