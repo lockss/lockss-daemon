b4a40038f13cbd5d2392cb63dc4d4a9f287faa70
tlipkis <tal@pss.com>
2010-02-23 06:26:06 +0000

    Display per-AU proxy iff it actually overrides global proxy.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@9110 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/state/ArchivalUnitStatus.java
+++ src/org/lockss/state/ArchivalUnitStatus.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ArchivalUnitStatus.java,v 1.88 2010-02-22 07:03:49 tlipkis Exp $
+ * $Id: ArchivalUnitStatus.java,v 1.89 2010-02-23 06:26:06 tlipkis Exp $
  */
 
 /*
@@ -835,13 +835,11 @@ public class ArchivalUnitStatus
 					  ColumnDescriptor.TYPE_DATE,
 					  new Long(state.getAuCreationTime())));
 
-      String auProxySpec =
-	AuUtil.getStringValue(AuUtil.getAuParamOrTitleDefault(au,
-							      ConfigParamDescr.CRAWL_PROXY),
-			      null);
-      if (!StringUtil.isNullString(auProxySpec)) {
-	String disp = (HostPortParser.isDirect(auProxySpec)
-		       ? "Direct connection" : auProxySpec);
+      AuUtil.AuProxyInfo aupinfo = AuUtil.getAuProxyInfo(au);
+      if (aupinfo.isAuOverride()) {
+	String disp = (aupinfo.getHost() == null
+		       ? "Direct connection"
+		       : aupinfo.getHost() + ":" + aupinfo.getPort());
 	res.add(new StatusTable.SummaryInfo("Crawl proxy",
 					    ColumnDescriptor.TYPE_STRING,
 					    disp));
