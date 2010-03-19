44e8b079528818ef47bf3230111fc8f8b879d2fb
thibgc <github@thib.org>
2010-03-19 07:20:16 +0000

    Fixed apostrophe inside XPath inside XML attribute bug.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@9306 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/scripts/tdbxml.py
+++ test/scripts/tdbxml.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # coding: utf-8
 
-# $Id: tdbxml.py,v 1.18 2010-03-11 02:14:03 thib_gc Exp $
+# $Id: tdbxml.py,v 1.19 2010-03-19 07:20:16 thib_gc Exp $
 #
 # Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
 # all rights reserved.
@@ -182,14 +182,16 @@ def _process(tdb, options):
   <property name="%(publisher)s">
    <property name="name" value="All %(publisher)s Titles" />
    <property name="class" value="xpath" />
-   <property name="xpath" value="[attributes/publisher='%(publisher2)s']" />
+   <property name="xpath" value=%(outer)s[attributes/publisher=%(inner)s%(publisher2)s%(inner)s]%(outer)s />
   </property>
   
  </property>
  
  <property name="org.lockss.title">
 ''' % { 'publisher': _escape(current_pub.name()),
-        'publisher2': re.sub(r'\'', '&apos;', _escape(current_pub.name())) }
+        'publisher2': re.sub(r'\'', '&apos;', _escape(current_pub.name())),
+        'outer': '"' if current_pub.name().find('\'') < 0 else '\'',
+        'inner': '\'' if current_pub.name().find('\'') < 0 else '"' }
         _process_au(au, options)
     else:
         if options.style == TDB_STYLE_XML: print '''\
