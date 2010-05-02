0d67dfcd95170fd51bcbacc0536bc15993742ec4
edwardsb1 <claire@lockss.org>
2010-05-02 21:31:11 +0000

    Automatically copying z_* files to a_* files.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@9616 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/scripts/ConvertTransformedTDB.sh
+++ test/scripts/ConvertTransformedTDB.sh
@@ -1,2 +1,4 @@
 perl TransformPlugin.pl < ../../tdb/clockssingest/sage_publications.tdb | ./tdbproc.py -s xml -T > ../frameworks/title_db_files/z_sage_publications.xml
 perl TransformPlugin.pl < ../../tdb/clockssingest/oxford_university_press.tdb | ./tdbproc.py -s xml -T > ../frameworks/title_db_files/z_oxford_university_press.xml
+cp ../frameworks/title_db_files/z_sage_publications.xml ../frameworks/title_db_files/a_sage_publications.xml
+cp ../frameworks/title_db_files/z_oxford_university_press.xml ../frameworks/title_db_files/a_oxford_university_press.xml
