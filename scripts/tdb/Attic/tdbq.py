23d03c226293075dfa7fe1cb24238feb1bda860b
thibgc <github@thib.org>
2010-08-04 22:31:03 +0000

    Bug fix (new AU.Status class)
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@9921 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/Attic/tdbq.py
+++ scripts/tdb/Attic/tdbq.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# $Id: tdbq.py,v 1.2 2010-08-04 18:49:00 thib_gc Exp $
+# $Id: tdbq.py,v 1.3 2010-08-04 22:31:03 thib_gc Exp $
 
 # Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
 # all rights reserved.
@@ -506,26 +506,26 @@ def tdbq_reprocess(tdb, options):
     if options.query: query = TdbqParser(TdbqScanner(options.query, options), options).parse()
     statuses = None
     if options.productionStatuses:
-        statuses = TdbqPredicate(lambda au: au.status() in [AU.STATUS_RELEASED,
-                                                            AU.STATUS_DOWN,
-                                                            AU.STATUS_SUPERSEDED,
-                                                            AU.STATUS_RETRACTED])
+        statuses = TdbqPredicate(lambda au: au.status() in [AU.Status.RELEASED,
+                                                            AU.Status.DOWN,
+                                                            AU.Status.SUPERSEDED,
+                                                            AU.Status.RETRACTED])
     elif options.testingStatuses:
-        statuses = TdbqPredicate(lambda au: au.status() in [AU.STATUS_EXISTS,
-                                                            AU.STATUS_MANIFEST,
-                                                            AU.STATUS_WANTED,
-                                                            AU.STATUS_TESTING,
-                                                            AU.STATUS_NOT_READY,
-                                                            AU.STATUS_TESTED,
-                                                            AU.STATUS_RETESTING,
-                                                            AU.STATUS_READY,
-                                                            AU.STATUS_PRE_RELEASING,
-                                                            AU.STATUS_PRE_RELEASED,
-                                                            AU.STATUS_RELEASING,
-                                                            AU.STATUS_RELEASED,
-                                                            AU.STATUS_DOWN,
-                                                            AU.STATUS_SUPERSEDED,
-                                                            AU.STATUS_RETRACTED])
+        statuses = TdbqPredicate(lambda au: au.status() in [AU.Status.EXISTS,
+                                                            AU.Status.MANIFEST,
+                                                            AU.Status.WANTED,
+                                                            AU.Status.TESTING,
+                                                            AU.Status.NOT_READY,
+                                                            AU.Status.TESTED,
+                                                            AU.Status.RETESTING,
+                                                            AU.Status.READY,
+                                                            AU.Status.PRE_RELEASING,
+                                                            AU.Status.PRE_RELEASED,
+                                                            AU.Status.RELEASING,
+                                                            AU.Status.RELEASED,
+                                                            AU.Status.DOWN,
+                                                            AU.Status.SUPERSEDED,
+                                                            AU.Status.RETRACTED])
     if query and statuses: prog = TdbqAnd(query, statuses)
     elif query: prog = query
     else: prog = statuses
