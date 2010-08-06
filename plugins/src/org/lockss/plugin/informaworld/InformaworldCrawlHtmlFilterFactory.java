a818225c9ea0bdc989a4aeafcddf2ff40a524c59
arusbridge <claire@lockss.org>
2010-08-06 15:15:25 +0000

    Adding a pre-filter to the Informaworld platform
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@9942 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/informaworld/InformaworldCrawlHtmlFilterFactory.java
@@ -0,0 +1,26 @@
+package org.lockss.plugin.informaworld;
+
+import java.io.InputStream;
+
+import org.htmlparser.NodeFilter;
+import org.htmlparser.filters.*;
+import org.lockss.daemon.PluginException;
+import org.lockss.filter.html.*;
+import org.lockss.plugin.*;
+
+public class InformaworldCrawlHtmlFilterFactory implements FilterFactory {
+
+  public InputStream createFilteredInputStream(ArchivalUnit au,
+                                               InputStream in,
+                                               String encoding)
+      throws PluginException {
+    NodeFilter[] filters = new NodeFilter[] {
+        // Remove URLs to articles outwith current volume
+        HtmlNodeFilters.tagWithAttribute("div", "class", "sandbox"),
+    };
+    return new HtmlFilterInputStream(in,
+                                     encoding,
+                                     HtmlNodeFilterTransform.exclude(new OrFilter(filters)));
+  }
+
+}
