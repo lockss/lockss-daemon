d10e9b7e22fd108794b605fe51c73e064a23c99f
thibgc <github@thib.org>
2010-10-06 22:51:48 +0000

    Added an article iterator
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@10174 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/internationalunionofcrystallography/InternationalUnionOfCrystallographyArticleIteratorFactory.java
@@ -0,0 +1,114 @@
+/*
+ * $Id: InternationalUnionOfCrystallographyArticleIteratorFactory.java,v 1.1 2010-10-06 22:51:48 thib_gc Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.internationalunionofcrystallography;
+
+import java.util.Iterator;
+import java.util.regex.*;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.*;
+import org.lockss.util.Logger;
+
+public class InternationalUnionOfCrystallographyArticleIteratorFactory implements ArticleIteratorFactory {
+
+  protected static Logger log = Logger.getLogger("InternationalUnionOfCrystallographyArticleIteratorFactory");
+
+  protected static final String ROOT_TEMPLATE = "\"%s%s/issues/%d/\", base_url, journal_id, year";
+  
+  protected static final String PATTERN_TEMPLATE = "\"^%s%s/issues/%d/[0-9]+/[0-9]+/[^/]+/index\\.html$\", base_url, journal_id, year";
+
+  @Override
+  public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au,
+                                                      MetadataTarget target)
+      throws PluginException {
+    return new InternationalUnionOfCrystallographyArticleIterator(au, new SubTreeArticleIterator.Spec()
+                                                                      .setTarget(target)
+                                                                      .setRootTemplate(ROOT_TEMPLATE)
+                                                                      .setPatternTemplate(PATTERN_TEMPLATE));
+  }
+  
+  protected static class InternationalUnionOfCrystallographyArticleIterator extends SubTreeArticleIterator {
+
+    protected static Pattern PATTERN = Pattern.compile("/([^/]+)/index\\.html$", Pattern.CASE_INSENSITIVE);
+    
+    public InternationalUnionOfCrystallographyArticleIterator(ArchivalUnit au,
+                                                              SubTreeArticleIterator.Spec spec) {
+      super(au, spec);
+    }
+    
+    @Override
+    protected ArticleFiles createArticleFiles(CachedUrl cu) {
+      String url = cu.getUrl();
+      Matcher mat = PATTERN.matcher(url);
+      if (mat.find()) {
+        return processFullTextHtml(cu, mat);
+      }
+      log.warning("Mismatch between article iterator factory and article iterator: " + url);
+      return null;
+    }
+    
+    protected ArticleFiles processFullTextHtml(CachedUrl htmlCu, Matcher htmlMat) {
+      ArticleFiles af = new ArticleFiles();
+      af.setFullTextCu(htmlCu);
+      af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_HTML, htmlCu);
+      guessFullTextPdf(af, htmlMat);
+      guessAbstract(af, htmlMat);
+      guessSupplementaryMaterials(af, htmlMat);
+      return af;
+    }
+
+    protected void guessFullTextPdf(ArticleFiles af, Matcher mat) {
+      CachedUrl pdfCu = au.makeCachedUrl(mat.replaceFirst("/$1/$1.pdf"));
+      if (pdfCu != null && pdfCu.hasContent()) {
+        af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_PDF, pdfCu);
+      }
+    }
+    
+    protected void guessAbstract(ArticleFiles af, Matcher mat) {
+      CachedUrl absCu = au.makeCachedUrl(mat.replaceFirst("/$1/$1abs.html"));
+      if (absCu != null && absCu.hasContent()) {
+        af.setRoleCu(ArticleFiles.ROLE_ABSTRACT, absCu);
+      }
+    }
+    
+    protected void guessSupplementaryMaterials(ArticleFiles af, Matcher mat) {
+      CachedUrl absCu = au.makeCachedUrl(mat.replaceFirst("/$1/$1sup0.html"));
+      if (absCu != null && absCu.hasContent()) {
+        af.setRoleCu(ArticleFiles.ROLE_SUPPLEMENTARY_MATERIALS, absCu);
+      }
+    }
+    
+  }
+
+}
