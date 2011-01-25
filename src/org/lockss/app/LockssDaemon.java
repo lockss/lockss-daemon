f0563a57aac350234c17199c2d4b8058637c279e
pgust <claire@lockss.org>
2011-01-25 00:04:17 +0000

    Add code to start MetadataManager (in 1.48, only if org.lockss.daemon.metadataManager.enabled is "true" -- false by default)
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@10630 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/app/LockssDaemon.java
+++ src/org/lockss/app/LockssDaemon.java
@@ -1,5 +1,5 @@
 /*
- * $Id: LockssDaemon.java,v 1.105 2011-01-10 09:07:59 tlipkis Exp $
+ * $Id: LockssDaemon.java,v 1.106 2011-01-25 00:04:17 pgust Exp $
  */
 
 /*
@@ -109,7 +109,7 @@ private final static String LOCKSS_USER_AGENT = "LOCKSS cache";
   public static final String IDENTITY_MANAGER = "IdentityManager";
   public static final String CRAWL_MANAGER = "CrawlManager";
   public static final String PLUGIN_MANAGER = "PluginManager";
-//PJG  public static final String METADATA_MANAGER = "MetadataManager";
+  public static final String METADATA_MANAGER = "MetadataManager";
   public static final String POLL_MANAGER = "PollManager";
   public static final String PSM_MANAGER = "PsmManager";
   public static final String REPOSITORY_MANAGER = "RepositoryManager";
@@ -161,7 +161,11 @@ private final static String LOCKSS_USER_AGENT = "LOCKSS cache";
     // start plugin manager after generic services
     new ManagerDesc(PLUGIN_MANAGER, "org.lockss.plugin.PluginManager"),
     // start metadata manager after pluggin manager
-//PJG    new ManagerDesc(METADATA_MANAGER, "org.lockss.daemon.MetadataManager"),
+    new ManagerDesc(METADATA_MANAGER, "org.lockss.daemon.MetadataManager") {
+      public boolean shouldStart() { 
+        return ConfigManager.getCurrentConfig().getBoolean("org.lockss.daemon.metadataManager.enabled", false); 
+      }
+    },
     // start proxy and servlets after plugin manager
     new ManagerDesc(REMOTE_API, "org.lockss.remote.RemoteApi"),
     new ManagerDesc(SERVLET_MANAGER, "org.lockss.servlet.AdminServletManager"),
@@ -433,9 +437,9 @@ private final static String LOCKSS_USER_AGENT = "LOCKSS cache";
    * @return the MetadataManager
    * @throws IllegalArgumentException if the manager is not available.
    */
-//PJG  public MetadataManager getMetadataManager() {
-//PJG    return (MetadataManager) getManager(METADATA_MANAGER);
-//PJG  }
+  public MetadataManager getMetadataManager() {
+    return (MetadataManager) getManager(METADATA_MANAGER);
+  }
 
   /**
    * return the Account Manager
