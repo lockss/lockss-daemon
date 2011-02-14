ef014918b0b9918f7fdbd98f718e5117303e5a29
tlipkis <tal@pss.com>
2011-02-14 00:22:12 +0000

    Typo.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/branches/branch_1-48@10698 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/util/MetadataUtil.java
+++ src/org/lockss/util/MetadataUtil.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MetadataUtil.java,v 1.6 2011-01-22 08:19:16 tlipkis Exp $
+ * $Id: MetadataUtil.java,v 1.6.2.1 2011-02-14 00:22:12 tlipkis Exp $
  */
 
 /*
@@ -208,6 +208,7 @@ public class MetadataUtil {
     lstr = lstr.trim();
     if (StringUtil.isNullString(lstr)) {
       defaultLocale = DEFAULT_DEFAULT_LOCALE;
+      return;
     }
     try {
       Locale loc = LocaleUtils.toLocale(lstr);
