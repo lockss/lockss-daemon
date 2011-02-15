4608ea3ffdf01faa0ce991c21f607e8004801128
tlipkis <tal@pss.com>
2011-02-15 09:02:48 +0000

    Don't spuriously alert AuDelete when restarting AUs.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@10705 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/plugin/PluginManager.java
+++ src/org/lockss/plugin/PluginManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: PluginManager.java,v 1.212 2011-01-25 07:14:00 tlipkis Exp $
+ * $Id: PluginManager.java,v 1.213 2011-02-15 09:01:11 tlipkis Exp $
  */
 
 /*
@@ -830,7 +830,7 @@ public class PluginManager
     }
     delHostAus(au);
 
-    signalAuEvent(au, AuEvent.Delete, null);
+    signalAuEvent(au, event, null);
 
     try {
       Plugin plugin = au.getPlugin();
