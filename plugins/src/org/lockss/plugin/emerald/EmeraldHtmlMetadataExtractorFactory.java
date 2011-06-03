75539a4b261cb12b3053acfa050aa885171c9e4c
arusbridge <claire@lockss.org>
2011-06-03 09:02:09 +0000

    Metadata extractor for Emerald
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@11379 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/emerald/EmeraldHtmlMetadataExtractorFactory.java
@@ -0,0 +1,78 @@
+/*
+
+Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.emerald;
+
+import java.io.*;
+import org.apache.commons.collections.MultiMap;
+import org.apache.commons.collections.map.MultiValueMap;
+
+import org.lockss.util.*;
+import org.lockss.daemon.*;
+import org.lockss.extractor.*;
+import org.lockss.plugin.*;
+
+
+public class EmeraldHtmlMetadataExtractorFactory implements FileMetadataExtractorFactory {
+  static Logger log = Logger.getLogger("EmeraldHtmlMetadataExtractorFactory");
+
+  public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
+							   String contentType)
+      throws PluginException {
+    return new EmeraldHtmlMetadataExtractor();
+  }
+
+  public static class EmeraldHtmlMetadataExtractor
+    extends SimpleHtmlMetaTagMetadataExtractor {
+
+    // Map Emerald's Google Scholar HTML meta tag names to cooked metadata fields
+    private static MultiMap tagMap = new MultiValueMap();
+    static {
+      tagMap.put("citation_doi", MetadataField.FIELD_DOI);
+      tagMap.put("citation_date", MetadataField.FIELD_DATE);
+      tagMap.put("citation_issn", MetadataField.FIELD_ISSN);
+      tagMap.put("citation_volume", MetadataField.FIELD_VOLUME);
+      tagMap.put("citation_issue", MetadataField.FIELD_ISSUE);
+      tagMap.put("citation_firstpage", MetadataField.FIELD_START_PAGE);
+      tagMap.put("citation_authors",
+                 new MetadataField(MetadataField.FIELD_AUTHOR,
+                                   MetadataField.splitAt(";")));
+      tagMap.put("citation_title", MetadataField.FIELD_ARTICLE_TITLE);
+      tagMap.put("citation_journal_title", MetadataField.FIELD_JOURNAL_TITLE);
+    }
+
+    @Override
+    public ArticleMetadata extract(MetadataTarget target, CachedUrl cu)
+	throws IOException {
+      ArticleMetadata am = super.extract(target, cu);
+      am.cook(tagMap);
+      return am;
+    }
+  }
+
+}
