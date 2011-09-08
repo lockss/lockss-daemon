531ddc265d5cccb9a2f2e9cb1d2e82365523b486
tlipkis <tal@pss.com>
2011-09-08 23:41:23 +0000

    Base class for ArticleIterator tests.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@12694 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/test/ArticleIteratorTestCase.java
@@ -0,0 +1,114 @@
+/*
+ * $Id: ArticleIteratorTestCase.java,v 1.1 2011-09-08 23:41:23 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.test;
+
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import java.util.regex.*;
+
+import org.lockss.util.*;
+import org.lockss.daemon.*;
+import org.lockss.plugin.*;
+import org.lockss.repository.*;
+import org.lockss.extractor.*;
+
+/** Framework for ArticleIterator tests. */
+public abstract class ArticleIteratorTestCase extends LockssTestCase {
+  protected ArchivalUnit au;
+  protected String tempDirPath;
+
+  public void setUp() throws Exception {
+    super.setUp();
+    tempDirPath = getTempDir().getAbsolutePath() + File.separator;
+    ConfigurationUtil.addFromArgs(LockssRepositoryImpl.PARAM_CACHE_LOCATION,
+				  tempDirPath);
+    MockLockssDaemon daemon = getMockLockssDaemon();
+    PluginManager pluginMgr = daemon.getPluginManager();
+    pluginMgr.setLoadablePluginsReady(true);
+    daemon.setDaemonInited(true);
+    pluginMgr.startService();
+    daemon.getAlertManager();
+    daemon.getCrawlManager();
+  }
+
+  public void tearDown() throws Exception {
+    getMockLockssDaemon().stopDaemon();
+    super.tearDown();
+  }
+
+  protected SubTreeArticleIterator createSubTreeIter() {
+    Iterator iter =  au.getArticleIterator(MetadataTarget.Any);
+    assertNotNull("ArticleIterator is null", iter);
+    if (iter instanceof SubTreeArticleIterator) {
+      return (SubTreeArticleIterator)iter;
+    }
+    fail("ArticleIterator isn't a SubTreeArticleIterator: " + iter.getClass());
+    return null;
+  }
+
+  protected Pattern getPattern(SubTreeArticleIterator iter) {
+    try {
+      return (Pattern)PrivilegedAccessor.getValue(iter, "pat");
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  protected Collection<String> getRootUrls(SubTreeArticleIterator iter) {
+    try {
+      List<CachedUrlSet> roots =
+	(List<CachedUrlSet>)PrivilegedAccessor.getValue(iter, "roots");
+      List<String> res = new ArrayList<String>();
+      for (CachedUrlSet cus : roots) {
+	res.add(cus.getUrl());
+      }
+      return res;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  protected ArticleFiles createArticleFiles(SubTreeArticleIterator artIter,
+					    CachedUrl cu) {
+    try {
+      return (ArticleFiles)PrivilegedAccessor.invokeMethod(artIter,
+							   "createArticleFiles",
+							   cu);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+}
