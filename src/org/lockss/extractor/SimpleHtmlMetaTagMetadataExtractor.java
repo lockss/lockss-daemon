907f753a90d1afaf45b400770d7e3352a972f88a
pgust <claire@lockss.org>
2011-11-09 05:19:17 +0000

    Ok, the decision is to leave the HTML tag filtering in for 1.52, and revisit the right place to do this for 1.53.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@13322 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/extractor/SimpleHtmlMetaTagMetadataExtractor.java
+++ src/org/lockss/extractor/SimpleHtmlMetaTagMetadataExtractor.java
@@ -1,5 +1,5 @@
 /*
- * $Id: SimpleHtmlMetaTagMetadataExtractor.java,v 1.8 2011-11-09 05:15:17 pgust Exp $
+ * $Id: SimpleHtmlMetaTagMetadataExtractor.java,v 1.9 2011-11-09 05:19:17 pgust Exp $
  */
 
 /*
@@ -128,6 +128,9 @@ public class SimpleHtmlMetaTagMetadataExtractor
     }
       
     String content = line.substring(contentBegin, contentEnd);
+    // filter out raw HTML tags embedded within content value
+    // -- publishers sometimes get sloppy
+    content = content.replaceAll("<[^>]+>", "");
     content = StringEscapeUtils.unescapeHtml(content);
 
     if (log.isDebug3()) log.debug3("Add: " + name + " = " + content);
