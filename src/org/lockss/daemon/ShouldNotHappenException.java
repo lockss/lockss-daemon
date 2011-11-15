6c585953550f9d21fa534e2718402b00ad7dc787
bhayes <claire@lockss.org>
2011-11-15 21:38:44 +0000

    Add a RuntimeException to be thrown for never-happens sanity checks and detected programming errors.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@13388 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/daemon/ShouldNotHappenException.java
@@ -0,0 +1,58 @@
+/*
+ * $Id: ShouldNotHappenException.java,v 1.1 2011-11-15 21:38:44 barry409 Exp $
+ */
+
+/*
+
+Copyright (c) 2011 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.daemon;
+
+/** <p>Thrown when a situation is detected that should never occur,
+  * such as an internal consistency check that can fail only as a
+  * result of a programming error.  This exception is not expected to
+  * be handled other than by terminating the affected process or
+  * computation.</p>
+  */
+public class ShouldNotHappenException extends RuntimeException {
+
+  public ShouldNotHappenException() {
+    super();
+  }
+
+  public ShouldNotHappenException(String msg) {
+    super(msg);
+  }
+
+  public ShouldNotHappenException(Throwable cause) {
+    super(cause);
+  }
+
+  public ShouldNotHappenException(String msg, Throwable cause) {
+    super(msg, cause);
+  }
+}
