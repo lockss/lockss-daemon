3b9e9b839d1069515cf9f273b743f1998da3245a
mellen22 <maryellen@lockss.org>
2011-11-19 00:37:11 +0000

    New methods.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@13418 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/util/SqlStoredProcedures.java
+++ src/org/lockss/util/SqlStoredProcedures.java
@@ -1,5 +1,5 @@
 /*
- * $Id: SqlStoredProcedures.java,v 1.2 2011-11-18 19:28:12 mellen22 Exp $
+ * $Id: SqlStoredProcedures.java,v 1.3 2011-11-19 00:37:11 mellen22 Exp $
  */
 
 /*
@@ -31,8 +31,14 @@ in this Software without prior written authorization from Stanford University.
 */
 package org.lockss.util;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import org.lockss.app.LockssDaemon;
+import org.lockss.config.ConfigManager;
+import org.lockss.config.Tdb;
 import org.lockss.config.TdbAu;
+import org.lockss.config.TdbPublisher;
+import org.lockss.config.TdbTitle;
 import org.lockss.daemon.TitleConfig;
 import org.lockss.plugin.ArchivalUnit;
 import org.lockss.plugin.CachedUrl;
@@ -42,11 +48,12 @@ import org.lockss.plugin.PluginManager;
  * This utility class contains static methods that enable SQL stored
  * procedures to access LOCKSS functionality.
  *  
- * @author pgust
+ * @author pgust mellen
  *
  */
 public class SqlStoredProcedures {
-  
+  static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
   /**
    * Constructor prevents creating instances.
    */
@@ -132,6 +139,92 @@ public class SqlStoredProcedures {
     
     // return the publisher
     return publisher;
+  }
+  
+  /**
+   * Return the ingest date from the title database that corresponds
+   * to the URL of an article in that publisher.
+   * 
+   * @param articleUrl the URL of the article
+   * @return the ingest date for the given URL
+   */
+  static public String getDateOfIngestFromArticleUrl(String articleUrl) {
+    // get lockss daemon
+    LockssDaemon daemon = LockssDaemon.getLockssDaemon();
+    if (daemon == null) {
+      throw new IllegalStateException("no LOCKSS daemon");
+    }
+    
+    // get the CachedUrl from the article URL
+    PluginManager pluginManager = daemon.getPluginManager();
+    CachedUrl cu = pluginManager.findCachedUrl(articleUrl);
+    if (cu == null) {
+      return null;
+    }
+    
+    // get the ingest date from the CachedUrl
+    String ingestDate = cu.getProperties().getProperty(CachedUrl.PROPERTY_LAST_MODIFIED);
+    if (ingestDate == null) {
+      return null;
+    }
+    
+    // get formatted date and return null if there is an exception
+    try {
+      long date = Long.parseLong(ingestDate);
+      ingestDate = formatter.format(new Date(date));
+      return ingestDate;
+    } catch (NumberFormatException ex) {
+      return null;
+    }
+  }
+
+  /**
+   * Return the title from the title database that corresponds
+   * to the ISSN of the journal.
+   * 
+   * @param journalISSN the ISSN of the journal
+   * @return the title for the given ISSN
+   */
+  static public String getTitleFromISSN(String journalISSN) {
+    if (journalISSN == null) {
+      throw new IllegalArgumentException("null journalISSN");
+    }
+
+    // get the tdb
+    Tdb tdb = ConfigManager.getCurrentConfig().getTdb(); 
+    if (tdb == null) {
+      throw new IllegalStateException("No tdb.");
+    }
+  
+    // get the title from the ISSN
+    TdbTitle title = tdb.getTdbTitleByIssn(journalISSN);
+  
+    // return the title
+    return title == null ? null : title.getName();
+  }
+
+  /**
+   * Return the publisher from the publisher database that corresponds
+   * to the ISSN of the journal.
+   * 
+   * @param journalISSN the ISSN of the journal
+   * @return the publisher for the given ISSN
+   */
+  static public String getPublisherFromISSN(String journalISSN) {
+    if (journalISSN == null) {
+      throw new IllegalArgumentException("null journalISSN");
+    }
+
+    Tdb tdb = ConfigManager.getCurrentConfig().getTdb(); 
+    if (tdb == null) {
+      throw new IllegalStateException("No tdb.");
+    }
+  
+    // get the publisher from the ISSN
+    TdbPublisher publisher = tdb.getTdbTitleByIssn(journalISSN).getTdbPublisher();
+  
+    // return the publisher
+    return publisher == null ? null : publisher.getName();
   }
 
 }
