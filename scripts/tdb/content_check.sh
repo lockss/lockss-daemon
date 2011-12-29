97a3b238a1c1e3c2ec8bffb4781b88418a0f7b31
mellen22 <maryellen@lockss.org>
2011-12-29 21:03:13 +0000

    add section delimiters
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@14000 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/content_check.sh
+++ scripts/tdb/content_check.sh
@@ -6,6 +6,8 @@
 cat ../../tdb/prod/*.tdb | ./tdbout -Aa | sort > /tmp/foo
 uniq /tmp/foo > /tmp/bar
 allAUs=`cat /tmp/foo | wc -l`
+echo "---------------------"
+echo "---------------------"
 echo "GLN. Num AUs with duplicates = $allAUs"
 uniqAUs=`cat /tmp/bar | wc -l`
 echo "GLN. Num AUs without duplicates = $uniqAUs"
@@ -16,6 +18,8 @@ echo " "
 cat ../../tdb/clockssingest/*.tdb | ./tdbout -Aa | sort > /tmp/foo
 uniq /tmp/foo > /tmp/bar
 allAUs=`cat /tmp/foo | wc -l`
+echo "---------------------"
+echo "---------------------"
 echo "CLOCKSS. Num AUs with duplicates = $allAUs"
 uniqAUs=`cat /tmp/bar | wc -l`
 echo "CLOCKSS. Num AUs without duplicates = $uniqAUs"
@@ -24,11 +28,15 @@ echo " "
 #
 # Find issn problems in gln title database
 cat ../../tdb/prod/*.tdb | ./tdbout -t publisher,title,issn,eissn | sort -u > /tmp/foo
+echo "---------------------"
+echo "---------------------"
 echo "GLN. ISSN issues"
 ./scrub_table.pl /tmp/foo
 #
 # Find issn problems in clockss title database
 cat ../../tdb/clockssingest/*.tdb | ./tdbout -t publisher,title,issn,eissn | sort -u > /tmp/foo
+echo "---------------------"
+echo "---------------------"
 echo "CLOCKSS. ISSN issues"
 ./scrub_table.pl /tmp/foo
 
