d3b2d19a5b5c02cf3db82b89d25cfa9d5e69b1b9
mellen22 <maryellen@lockss.org>
2012-01-07 01:08:27 +0000

    reorder lines. change file names
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@14188 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/content_check.sh
+++ scripts/tdb/content_check.sh
@@ -3,45 +3,45 @@
 # Script to provide alerts to problems in the title database
 #
 # Find duplicates in the gln title database
-cat ../../tdb/prod/*.tdb | ./tdbout -Aa | sort > /tmp/foo
-uniq /tmp/foo > /tmp/bar
-allAUs=`cat /tmp/foo | wc -l`
+cat ../../tdb/prod/*.tdb | ./tdbout -Aa | sort > /tmp/allAUs
+uniq /tmp/allAUs > /tmp/dedupedAUs
+allAUs=`cat /tmp/allAUs | wc -l`
+uniqAUs=`cat /tmp/dedupedAUs | wc -l`
 echo "---------------------"
 echo "---------------------"
 echo "GLN. AUs with all duplicates = $allAUs"
-uniqAUs=`cat /tmp/bar | wc -l`
 echo "GLN. AUs without duplicates = $uniqAUs"
-diff /tmp/foo /tmp/bar | grep "<" | sed s/..//
+diff /tmp/allAUs /tmp/dedupedAUs | grep "<" | sed s/..//
 echo " "
 #
 # Find duplicates in the clockss title database
 cat ../../tdb/clockssingest/*.tdb | ./tdbout -Aa | sort > /tmp/foo
-uniq /tmp/foo > /tmp/bar
-allAUs=`cat /tmp/foo | wc -l`
+uniq /tmp/allAUs > /tmp/dedupedAUs
+allAUs=`cat /tmp/allAUs | wc -l`
+uniqAUs=`cat /tmp/dedupedAUs | wc -l`
 echo "---------------------"
 echo "---------------------"
 echo "CLOCKSS. AUs with all duplicates = $allAUs"
-uniqAUs=`cat /tmp/bar | wc -l`
 echo "CLOCKSS. AUs without duplicates = $uniqAUs"
-diff /tmp/foo /tmp/bar | grep "<" | sed s/..//
+diff /tmp/allAUs /tmp/dedupedAUs | grep "<" | sed s/..//
 echo " "
 #
 # Find issn problems in gln title database
-cat ../../tdb/prod/*.tdb | ./tdbout -t publisher,title,issn,eissn | sort -u > /tmp/foo
+cat ../../tdb/prod/*.tdb | ./tdbout -t publisher,title,issn,eissn | sort -u > /tmp/issn
 echo "---------------------"
 echo "---------------------"
 echo "GLN. ISSN issues"
-./scrub_table.pl /tmp/foo
+./scrub_table.pl /tmp/issn
 #
 # Find issn problems in clockss title database
-cat ../../tdb/clockssingest/*.tdb | ./tdbout -t publisher,title,issn,eissn | sort -u > /tmp/foo
+cat ../../tdb/clockssingest/*.tdb | ./tdbout -t publisher,title,issn,eissn | sort -u > /tmp/issn
 echo "---------------------"
 echo "---------------------"
 echo "CLOCKSS. ISSN issues"
-./scrub_table.pl /tmp/foo
+./scrub_table.pl /tmp/issn
 #
 # Find Muse titles that don't have attr[journal_id]
 echo "---------------------"
 echo "---------------------"
 echo "GLN. Muse. Titles missing journal_id"
-cat *.tdb | ../../scripts/tdb/tdbout -t publisher,param[journal_dir] -Q 'plugin ~ "Muse" and attr[journal_id] is not set' | sort -u
+cat ../../tdb/prod/*.tdb | ./tdbout -t publisher,param[journal_dir] -Q 'plugin ~ "Muse" and attr[journal_id] is not set' | sort -u
