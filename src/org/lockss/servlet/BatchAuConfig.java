3bb5f2fd49477025700d208ace7e9ad2f4bad4bc
tlipkis <tal@pss.com>
2012-01-18 03:34:18 +0000

    Add AUs to best disk if no disk selection on form.  Issue 3977.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@14429 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/servlet/BatchAuConfig.java
+++ src/org/lockss/servlet/BatchAuConfig.java
@@ -1,10 +1,10 @@
 /*
- * $Id: BatchAuConfig.java,v 1.43 2011-10-03 05:55:10 tlipkis Exp $
+ * $Id: BatchAuConfig.java,v 1.44 2012-01-18 03:34:18 tlipkis Exp $
  */
 
 /*
 
-Copyright (c) 2000-2006 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -475,7 +475,10 @@ public class BatchAuConfig extends LockssServlet {
     String[] auids = req.getParameterValues(KEY_AUID);
     String defaultRepo = null;
     String defRepoId = getParameter(KEY_DEFAULT_REPO);
-    if (!StringUtil.isNullString(defRepoId) && repoMap != null) {
+    if (StringUtil.isNullString(defRepoId)) {
+      defaultRepo =
+	remoteApi.findLeastFullRepository(remoteApi.getRepositoryMap());
+    } else if (repoMap != null) {
       try {
 	int n = Integer.parseInt(defRepoId);
 	defaultRepo = (String)repoMap.get(n - 1);
