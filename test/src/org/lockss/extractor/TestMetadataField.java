ba3e41004b531f1d0a8b90a99547d2d4feecbd1a
akanshab01 <claire@lockss.org>
2012-02-11 00:15:06 +0000

    Made changes to the test case for extractor method.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@14925 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/extractor/TestMetadataField.java
+++ test/src/org/lockss/extractor/TestMetadataField.java
@@ -1,34 +1,34 @@
 /*
- * $Id: TestMetadataField.java,v 1.7 2012-01-26 23:26:46 akanshab01 Exp $
+ * $Id: TestMetadataField.java,v 1.8 2012-02-11 00:15:06 akanshab01 Exp $
  */
 
 /*
 
-Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
-all rights reserved.
+ Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-Except as contained in this notice, the name of Stanford University shall not
-be used in advertising or otherwise to promote the sale, use or other dealings
-in this Software without prior written authorization from Stanford University.
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
 
-*/
+ */
 
 package org.lockss.extractor;
 
@@ -65,49 +65,46 @@ public class TestMetadataField extends LockssTestCase {
     assertField(KEY_PUBLISHER, Cardinality.Single, FIELD_PUBLISHER);
     assertField(KEY_START_PAGE, Cardinality.Single, FIELD_START_PAGE);
     assertField(KEY_VOLUME, Cardinality.Single, FIELD_VOLUME);
-    
-    assertField(DC_KEY_CITATION_CHAPTER, 
-                Cardinality.Single, DC_FIELD_CITATION_CHAPTER);
-    assertField(DC_KEY_CITATION_EPAGE, 
-                Cardinality.Single, DC_FIELD_CITATION_EPAGE);
-    assertField(DC_KEY_CITATION_ISSUE, 
-                Cardinality.Single, DC_FIELD_CITATION_ISSUE);
-    assertField(DC_KEY_CITATION_SPAGE, 
-                Cardinality.Single, DC_FIELD_CITATION_SPAGE);
-    assertField(DC_KEY_CITATION_VOLUME, 
-                Cardinality.Single, DC_FIELD_CITATION_VOLUME);
+
+    assertField(DC_KEY_CITATION_CHAPTER, Cardinality.Single,
+        DC_FIELD_CITATION_CHAPTER);
+    assertField(DC_KEY_CITATION_EPAGE, Cardinality.Single,
+        DC_FIELD_CITATION_EPAGE);
+    assertField(DC_KEY_CITATION_ISSUE, Cardinality.Single,
+        DC_FIELD_CITATION_ISSUE);
+    assertField(DC_KEY_CITATION_SPAGE, Cardinality.Single,
+        DC_FIELD_CITATION_SPAGE);
+    assertField(DC_KEY_CITATION_VOLUME, Cardinality.Single,
+        DC_FIELD_CITATION_VOLUME);
     assertField(DC_KEY_CONTRIBUTOR, Cardinality.Multi, DC_FIELD_CONTRIBUTOR);
     assertField(DC_KEY_DATE, Cardinality.Single, DC_FIELD_DATE);
-    assertField(DC_KEY_COVERAGE, 
-                Cardinality.Single, DC_FIELD_COVERAGE);
+    assertField(DC_KEY_COVERAGE, Cardinality.Single, DC_FIELD_COVERAGE);
     assertField(DC_KEY_DESCRIPTION, Cardinality.Single, DC_FIELD_DESCRIPTION);
-    assertField(DC_KEY_FORMAT, 
-                Cardinality.Single, DC_FIELD_FORMAT);
+    assertField(DC_KEY_FORMAT, Cardinality.Single, DC_FIELD_FORMAT);
     assertField(DC_KEY_IDENTIFIER, Cardinality.Multi, DC_FIELD_IDENTIFIER);
-    assertField(DC_KEY_IDENTIFIER_ISSN, 
-                Cardinality.Single, DC_FIELD_IDENTIFIER_ISSN);
-    assertField(DC_KEY_IDENTIFIER_EISSN, 
-        Cardinality.Single, DC_FIELD_IDENTIFIER_EISSN);
-    assertField(DC_KEY_IDENTIFIER_ISSNL, 
-        Cardinality.Single, DC_FIELD_IDENTIFIER_ISSNL);
-    assertField(DC_KEY_IDENTIFIER_ISBN, 
-        Cardinality.Single, DC_FIELD_IDENTIFIER_ISBN);
-    assertField(DC_KEY_ISSUED, 
-                Cardinality.Single, DC_FIELD_ISSUED);
+    assertField(DC_KEY_IDENTIFIER_ISSN, Cardinality.Single,
+        DC_FIELD_IDENTIFIER_ISSN);
+    assertField(DC_KEY_IDENTIFIER_EISSN, Cardinality.Single,
+        DC_FIELD_IDENTIFIER_EISSN);
+    assertField(DC_KEY_IDENTIFIER_ISSNL, Cardinality.Single,
+        DC_FIELD_IDENTIFIER_ISSNL);
+    assertField(DC_KEY_IDENTIFIER_ISBN, Cardinality.Single,
+        DC_FIELD_IDENTIFIER_ISBN);
+    assertField(DC_KEY_ISSUED, Cardinality.Single, DC_FIELD_ISSUED);
     assertField(DC_KEY_LANGUAGE, Cardinality.Single, DC_FIELD_LANGUAGE);
     assertField(DC_KEY_PUBLISHER, Cardinality.Single, DC_FIELD_PUBLISHER);
     assertField(DC_KEY_RELATION, Cardinality.Multi, DC_FIELD_RELATION);
-    assertField(DC_KEY_RELATION_ISPARTOF, 
-                Cardinality.Single, DC_FIELD_RELATION_ISPARTOF);
+    assertField(DC_KEY_RELATION_ISPARTOF, Cardinality.Single,
+        DC_FIELD_RELATION_ISPARTOF);
     assertField(DC_KEY_RIGHTS, Cardinality.Single, DC_FIELD_RIGHTS);
     assertField(DC_KEY_RIGHTS, Cardinality.Single, DC_FIELD_RIGHTS);
     assertField(DC_KEY_SUBJECT, Cardinality.Single, DC_FIELD_SUBJECT);
     assertField(DC_KEY_TITLE, Cardinality.Single, DC_FIELD_TITLE);
   }
 
-  public void testDefault() throws MetadataException. ValidationException {
+  public void testDefault() throws MetadataException.ValidationException {
     MetadataField f1 = new MetadataField.Default("key1");
-    assertField("key1", Cardinality.Single, f1);    
+    assertField("key1", Cardinality.Single, f1);
     String val = "value";
     assertSame(val, f1.validate(am, val));
   }
@@ -115,12 +112,13 @@ public class TestMetadataField extends LockssTestCase {
   static class TestValidator implements MetadataField.Validator {
     @Override
     public String validate(ArticleMetadata am, MetadataField field, String value)
-	throws MetadataException.ValidationException {
+        throws MetadataException.ValidationException {
       if (value.matches(".*ill.*")) {
-	throw new MetadataException.ValidationException("Invalid: " + value);
+        throw new MetadataException.ValidationException("Invalid: " + value);
       }
       return value.toLowerCase();
-    }};	
+    }
+  };
 
   public void testDoi() throws MetadataException.ValidationException {
     MetadataField f1 = FIELD_DOI;
@@ -155,7 +153,7 @@ public class TestMetadataField extends LockssTestCase {
       fail("Should not throw ValidationException");
     }
   }
-  
+
   public void testIssn() throws MetadataException.ValidationException {
     MetadataField f1 = FIELD_ISSN;
     assertEquals("1234-5679", f1.validate(am, "1234-5679"));
@@ -193,19 +191,19 @@ public class TestMetadataField extends LockssTestCase {
   public void testConstructors() throws MetadataException.ValidationException {
     MetadataField f1;
     f1 = new MetadataField("foo");
-    assertField("foo", Cardinality.Single, f1);    
+    assertField("foo", Cardinality.Single, f1);
     String val = "value";
     assertSame(val, f1.validate(am, val));
     assertFalse(f1.hasSplitter());
 
     f1 = new MetadataField("bar", Cardinality.Multi);
-    assertField("bar", Cardinality.Multi, f1);    
+    assertField("bar", Cardinality.Multi, f1);
     assertSame(val, f1.validate(am, val));
     assertFalse(f1.hasSplitter());
     assertEquals(ListUtil.list("aval;bval"), f1.split(am, "aval;bval"));
 
     f1 = new MetadataField("bar", Cardinality.Single, new TestValidator());
-    assertField("bar", Cardinality.Single, f1);    
+    assertField("bar", Cardinality.Single, f1);
     assertFalse(f1.hasSplitter());
     assertEquals(ListUtil.list("aval;bval"), f1.split(am, "aval;bval"));
     assertEquals("foo", f1.validate(am, "Foo"));
@@ -216,48 +214,46 @@ public class TestMetadataField extends LockssTestCase {
     }
 
     try {
-      new MetadataField("bar", Cardinality.Single,
-			MetadataField.splitAt(";"));
+      new MetadataField("bar", Cardinality.Single, MetadataField.splitAt(";"));
       fail("Should throw IllegalArgumentException");
     } catch (IllegalArgumentException e) {
-    }      
+    }
     f1 = new MetadataField("keym", Cardinality.Multi,
-			   MetadataField.splitAt(";"));
-    assertField("keym", Cardinality.Multi, f1);    
+        MetadataField.splitAt(";"));
+    assertField("keym", Cardinality.Multi, f1);
     assertTrue(f1.hasSplitter());
     assertEquals("illness", f1.validate(am, "illness"));
     assertEquals(ListUtil.list("one", "two", "four"),
-		 f1.split(am, "one;two;four"));
+        f1.split(am, "one;two;four"));
   }
 
   public void testSplitter1() {
     MetadataField.Splitter spl = MetadataField.splitAt(";");
     assertEquals(ListUtil.list("one", "two", "four"),
-		 spl.split(am, null, "one;two;four"));
+        spl.split(am, null, "one;two;four"));
     assertEquals(ListUtil.list("one", "two", "four"),
-		 spl.split(am, null, "  one;two  ;  four  "));
+        spl.split(am, null, "  one;two  ;  four  "));
     assertEquals(ListUtil.list("\"one", "two\""),
-		 spl.split(am, null, "\"one;;two\""));
+        spl.split(am, null, "\"one;;two\""));
   }
 
   public void testSplitter2() {
     MetadataField.Splitter spl = MetadataField.splitAt(";", "\"");
     assertEquals(ListUtil.list("one", "two", "four"),
-		 spl.split(am, null, "one;two;four"));
+        spl.split(am, null, "one;two;four"));
     assertEquals(ListUtil.list("one", "two"),
-		 spl.split(am, null, "\"one;two\""));
+        spl.split(am, null, "\"one;two\""));
     assertEquals(ListUtil.list("one", "two"),
-		 spl.split(am, null, " \"one; two\"  "));
+        spl.split(am, null, " \"one; two\"  "));
   }
 
   public void testSplitter3() {
     MetadataField.Splitter spl = MetadataField.splitAt(";", "(", ")");
     assertEquals(ListUtil.list("one", "two", "four"),
-		 spl.split(am, null, "one;two;four"));
+        spl.split(am, null, "one;two;four"));
+    assertEquals(ListUtil.list("one", "two"), spl.split(am, null, "(one;two)"));
     assertEquals(ListUtil.list("one", "two"),
-		 spl.split(am, null, "(one;two)"));
-    assertEquals(ListUtil.list("one", "two"),
-		 spl.split(am, null, "  (one ; ; two)  "));
+        spl.split(am, null, "  (one ; ; two)  "));
   }
 
   public void testFindField() {
@@ -265,30 +261,63 @@ public class TestMetadataField extends LockssTestCase {
     assertNull(MetadataField.findField("nosuchfield"));
   }
 
-//   public void testValidate() {
-//     MetadataField field = new MetadataField(
-//     assertSame(FIELD_VOLUME, MetadataField.findField(KEY_VOLUME));
-//     assertNull(MetadataField.findField("nosuchfield"));
-//   }
-
-
+  // public void testValidate() {
+  // MetadataField field = new MetadataField(
+  // assertSame(FIELD_VOLUME, MetadataField.findField(KEY_VOLUME));
+  // assertNull(MetadataField.findField("nosuchfield"));
+  // }
 
   public void testPutMulti() {
     assertSame(FIELD_VOLUME, MetadataField.findField(KEY_VOLUME));
     assertNull(MetadataField.findField("nosuchfield"));
   }
+
+  public void testExtractor()throws MetadataException.ValidationException  {
+   
+    MetadataField f1 = FIELD_START_PAGE;
+    assertEquals("pp. 23",f1.validate(am, "pp. 23"));
+    assertEquals("p23",f1.validate(am, "p23"));
+    assertEquals("pp. 23-45",f1.validate(am, "pp. 23-45"));
+
+    //String testpagepattern2 = "[pP\\. ]*([^-]+)(?:-(.+)$h)?";
   
-  public void testExtractor() {
-    String startpattern = "pp\\.[ ]+(.*)-.*";
-    String endpattern = "pp\\.[ ]+.*-(.*)";
-    ArticleMetadata articleMetadata =new ArticleMetadata();
-     MetadataField testvalf =new MetadataField(MetadataField.FIELD_START_PAGE,MetadataField.extract(startpattern));
-     MetadataField testvale =new MetadataField(MetadataField.FIELD_END_PAGE,MetadataField.extract(endpattern));
-  
-   assertEquals("23", testvalf.extract(articleMetadata,"pp. 23-45"));
-   assertEquals("45", testvale.extract(articleMetadata,"pp. 23-45"));
-   assertNull(testvalf.extract(articleMetadata,"pp. 2345"));
-   assertNull(testvalf.extract(articleMetadata,"23-45"));
+     String testpagepattern1 = "pp. (23)-(45)";
+     String testpagepattern2 = "p(23)";
+     String testpagepattern3 = "(23)-(45)";
+     String testpagepattern4 = "P(23)";
+    
+    ArticleMetadata articleMetadata = new ArticleMetadata();
+    MetadataField testvalf0 = new MetadataField(MetadataField.FIELD_START_PAGE,
+        MetadataField.groupExtractor(testpagepattern1,1));
+    MetadataField testvalf1 = new MetadataField(MetadataField.FIELD_START_PAGE,
+        MetadataField.groupExtractor(testpagepattern2,1));
+    MetadataField testvalf2 = new MetadataField(MetadataField.FIELD_START_PAGE,
+        MetadataField.groupExtractor(testpagepattern3,1));
+    MetadataField testvalf3 = new MetadataField(MetadataField.FIELD_START_PAGE,
+        MetadataField.groupExtractor(testpagepattern4,1));
+    
+   
+    MetadataField testvalf4 = new MetadataField(MetadataField.FIELD_START_PAGE,
+        MetadataField.groupExtractor(testpagepattern1,1));
+   
+    MetadataField testvalf5 = new MetadataField(MetadataField.FIELD_START_PAGE,
+        MetadataField.groupExtractor(testpagepattern3,1));
+   
+    
+    
+    //test for start page pattern 
+    assertEquals("23", testvalf1.extract(articleMetadata,"p23"));
+    assertEquals("23", testvalf0.extract(articleMetadata, "pp. 23-45"));
+    assertEquals("23", testvalf2.extract(articleMetadata, "23-45"));
+    assertEquals("23", testvalf3.extract(articleMetadata, "P23"));
+    
+   //test for end page pattern
+    
+    assertEquals("45", testvalf4.extract(articleMetadata, "pp. 23-45"));
+    assertEquals("45", testvalf5.extract(articleMetadata, "23-45"));
+   
+    
+ 
   }
-  
+
 }
