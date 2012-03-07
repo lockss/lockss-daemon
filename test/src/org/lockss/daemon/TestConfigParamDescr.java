f3a12c4dafc5b30ef0ab698c17ece5eac8c76036
thibgc <github@thib.org>
2012-03-07 00:07:37 +0000

    Cosmetic
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@15459 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/daemon/TestConfigParamDescr.java
+++ test/src/org/lockss/daemon/TestConfigParamDescr.java
@@ -1,10 +1,10 @@
 /*
- * $Id: TestConfigParamDescr.java,v 1.15 2012-03-07 00:06:58 thib_gc Exp $
+ * $Id: TestConfigParamDescr.java,v 1.16 2012-03-07 00:07:37 thib_gc Exp $
  */
 
 /*
 
-Copyright (c) 2000-2008 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
