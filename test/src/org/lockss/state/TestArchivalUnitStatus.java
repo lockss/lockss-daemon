0da0235ef3dc76547b339ccc30b8159cc680148a
bhayes <claire@lockss.org>
2012-03-12 21:33:18 +0000

    Correct (c) date.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@15600 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/state/TestArchivalUnitStatus.java
+++ test/src/org/lockss/state/TestArchivalUnitStatus.java
@@ -1,10 +1,10 @@
 /*
- * $Id: TestArchivalUnitStatus.java,v 1.1 2012-03-12 21:32:25 barry409 Exp $
+ * $Id: TestArchivalUnitStatus.java,v 1.2 2012-03-12 21:33:18 barry409 Exp $
  */
 
 /*
 
-Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2012 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
