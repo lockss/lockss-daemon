83dea47929289ae53e2bf4f12a2c5ff314f59d89
akanshab01 <claire@lockss.org>
2012-04-10 23:02:13 +0000

    Modified extract method in BaseArticleMetadataExtractor which handles the case of IO Exception and null FileMetadataExtractor.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@16479 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/extractor/BaseArticleMetadataExtractor.java
+++ src/org/lockss/extractor/BaseArticleMetadataExtractor.java
@@ -1,34 +1,34 @@
 /*
- * $Id: BaseArticleMetadataExtractor.java,v 1.10 2012-04-09 21:26:48 pgust Exp $
+ * $Id: BaseArticleMetadataExtractor.java,v 1.11 2012-04-10 23:02:13 akanshab01 Exp $
  */
 
 /*
 
-Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
-all rights reserved.
+ Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-Except as contained in this notice, the name of Stanford University shall not
-be used in advertising or otherwise to promote the sale, use or other dealings
-in this Software without prior written authorization from Stanford University.
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
 
-*/
+ */
 
 package org.lockss.extractor;
 
@@ -39,28 +39,22 @@ import org.lockss.config.TdbAu;
 import org.lockss.daemon.*;
 import org.lockss.plugin.*;
 
-/** Base class for metadata extractors that return a single ArticleMetadata
- * or null.  This was the previous ArticleMetadataExtractor interface. */
-public class BaseArticleMetadataExtractor
-  implements ArticleMetadataExtractor {
+/**
+ * Base class for metadata extractors that return a single ArticleMetadata or
+ * null. This was the previous ArticleMetadataExtractor interface.
+ */
+public class BaseArticleMetadataExtractor implements ArticleMetadataExtractor {
 
   private static Logger log = Logger.getLogger("BaseArticleMetadataExtractor");
-
 
   protected String cuRole = null;
   protected boolean emitDefaultIfNone = false;
 
   public BaseArticleMetadataExtractor() {
   }
-  
+
   public BaseArticleMetadataExtractor(String cuRole) {
     this.cuRole = cuRole;
-  }
-
-  protected void addAccessUrl(ArticleMetadata am, ArticleFiles af) {
-    if (!am.hasValidValue(MetadataField.FIELD_ACCESS_URL)) {
-      am.put(MetadataField.FIELD_ACCESS_URL, af.getFullTextUrl());
-    }
   }
 
   protected CachedUrl getCuToExtract(ArticleFiles af) {
@@ -70,7 +64,7 @@ public class BaseArticleMetadataExtractor
   class MyEmitter implements FileMetadataExtractor.Emitter {
     private Emitter parent;
     private ArticleFiles af;
-    private boolean isEmpty = true;
+   
 
     MyEmitter(ArticleFiles af, Emitter parent) {
       this.af = af;
@@ -78,7 +72,7 @@ public class BaseArticleMetadataExtractor
     }
 
     public void emitMetadata(CachedUrl cu, ArticleMetadata am) {
-      
+
       TitleConfig tc = cu.getArchivalUnit().getTitleConfig();
       TdbAu tdbau = (tc == null) ? null : tc.getTdbAu();
       String isbn = (tdbau == null) ? null : tdbau.getIsbn();
@@ -88,38 +82,38 @@ public class BaseArticleMetadataExtractor
       String volume = (tdbau == null) ? null : tdbau.getStartVolume();
       String issue = (tdbau == null) ? null : tdbau.getStartIssue();
       String journalTitle = (tdbau == null) ? null : tdbau.getJournalTitle();
-      
+
       if (am.get(MetadataField.FIELD_ISSN) == null
           || am.hasInvalidValue(MetadataField.FIELD_ISSN)) {
-          am.put(MetadataField.FIELD_ISSN, issn);
+        am.put(MetadataField.FIELD_ISSN, issn);
       }
       if (am.get(MetadataField.FIELD_EISSN) == null
           || am.hasInvalidValue(MetadataField.FIELD_EISSN)) {
-          am.put(MetadataField.FIELD_EISSN, eissn);
+        am.put(MetadataField.FIELD_EISSN, eissn);
       }
       if (am.get(MetadataField.FIELD_VOLUME) == null
           || am.hasInvalidValue(MetadataField.FIELD_VOLUME)) {
-          am.put(MetadataField.FIELD_VOLUME, volume);
+        am.put(MetadataField.FIELD_VOLUME, volume);
       }
       if (am.get(MetadataField.FIELD_DATE) == null
           || am.hasInvalidValue(MetadataField.FIELD_DATE)) {
-          am.put(MetadataField.FIELD_DATE, year);
+        am.put(MetadataField.FIELD_DATE, year);
       }
       if (am.get(MetadataField.FIELD_ISSUE) == null
           || am.hasInvalidValue(MetadataField.FIELD_ISSUE)) {
-          am.put(MetadataField.FIELD_ISSUE, issue);
+        am.put(MetadataField.FIELD_ISSUE, issue);
       }
       if (am.get(MetadataField.FIELD_ISBN) == null
           || am.hasInvalidValue(MetadataField.FIELD_ISBN)) {
-          am.put(MetadataField.FIELD_ISBN, isbn);
+        am.put(MetadataField.FIELD_ISBN, isbn);
       }
       if (am.get(MetadataField.FIELD_JOURNAL_TITLE) == null
           || am.hasInvalidValue(MetadataField.FIELD_JOURNAL_TITLE)) {
-          am.put(MetadataField.FIELD_JOURNAL_TITLE, journalTitle);
+        am.put(MetadataField.FIELD_JOURNAL_TITLE, journalTitle);
       }
-      
-      isEmpty = false;
-      addAccessUrl(am, af);
+      if (!am.hasValidValue(MetadataField.FIELD_ACCESS_URL)) {
+        am.put(MetadataField.FIELD_ACCESS_URL, af.getFullTextUrl());
+      }
       parent.emitMetadata(af, am);
     }
 
@@ -131,20 +125,12 @@ public class BaseArticleMetadataExtractor
       emitDefaultIfNone = val;
     }
 
-    void finishArticle() {
-      if (isEmpty && emitDefaultIfNone) {
-	log.info("adding default");
-	ArticleMetadata am = new ArticleMetadata();
-	addAccessUrl(am, af);
-	parent.emitMetadata(af, am);
-      }
-    }
   }
-  
-  public void extract(MetadataTarget target,
-		      ArticleFiles af,
-		      ArticleMetadataExtractor.Emitter emitter)
-      throws IOException, PluginException {
+
+  public void extract(MetadataTarget target, ArticleFiles af,
+      ArticleMetadataExtractor.Emitter emitter) throws IOException,
+      PluginException {
+
     MyEmitter myEmitter = new MyEmitter(af, emitter);
     CachedUrl cu = getCuToExtract(af);
     log.debug3("extract(" + af + ")");
@@ -152,23 +138,21 @@ public class BaseArticleMetadataExtractor
       FileMetadataExtractor me = null;
       try {
         me = cu.getFileMetadataExtractor(target);
-        me.extract(target, cu, myEmitter);
-      } catch (RuntimeException ex) {
-        // If our plugin is missing a file metadata extractor or 
-        // the extractor specified is not of the appropriate type,
-        // emit default metadata from the TDB file 
-        log.debug3("retry with default metadata for (" + af + ")", ex);
-        try {
-          ArticleMetadata am = new ArticleMetadata();
-          myEmitter.emitMetadata(cu, am);
-        } catch (RuntimeException ex2) {
-          // just move on because the problem may be in the emitter
-          log.debug3("unable to emit metadata for af (" + af + ")", ex2);
+        if (me != null) {
+          me.extract(target, cu, myEmitter);
+          AuUtil.safeRelease(cu);
+          return;
         }
-      } finally {
-	AuUtil.safeRelease(cu);
+      } catch (IOException ex) {
+        // unable to read input stream or some error during plugin filtering.
+       log.debug3("IO Exception has been caught",ex);
       }
-      myEmitter.finishArticle();
+      // generate default metadata in case of null filemetadataextractor or
+      //IOException.
+      
+      ArticleMetadata am = new ArticleMetadata();
+      myEmitter.emitMetadata(cu, am);
+      AuUtil.safeRelease(cu);
     }
   }
 }
