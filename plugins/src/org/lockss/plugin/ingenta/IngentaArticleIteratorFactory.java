0c2b43364c6b89991dcabe4d0f2316947383f350
akanshab01 <claire@lockss.org>
2012-04-17 19:05:06 +0000

    modified the pattern template, processfullTextCu method to generate articles  with their metadata through base article metadata extractor.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@16633 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/ingenta/IngentaArticleIteratorFactory.java
+++ plugins/src/org/lockss/plugin/ingenta/IngentaArticleIteratorFactory.java
@@ -1,34 +1,34 @@
 /*
- * $Id: IngentaArticleIteratorFactory.java,v 1.2 2011-11-22 01:38:01 thib_gc Exp $
- */ 
+ * $Id: IngentaArticleIteratorFactory.java,v 1.3 2012-04-17 19:05:06 akanshab01 Exp $
+ */
 
 /*
 
-Copyright (c) 2000-2011 Board of Trustees of Leland Stanford Jr. University,
-all rights reserved.
+ Copyright (c) 2000-2011 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-Except as contained in this notice, the name of Stanford University shall not
-be used in advertising or otherwise to promote the sale, use or other dealings
-in this Software without prior written authorization from Stanford University.
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
 
-*/
+ */
 
 package org.lockss.plugin.ingenta;
 
@@ -40,74 +40,66 @@ import org.lockss.extractor.*;
 import org.lockss.plugin.*;
 import org.lockss.util.Logger;
 
-public class IngentaArticleIteratorFactory
-    implements ArticleIteratorFactory,
-               ArticleMetadataExtractorFactory {
+public class IngentaArticleIteratorFactory implements ArticleIteratorFactory,
+    ArticleMetadataExtractorFactory {
 
-  protected static Logger log = Logger.getLogger("IngentaArticleIteratorFactory");
+  protected static Logger log = Logger
+      .getLogger("IngentaArticleIteratorFactory");
 
   protected static final String ROOT_TEMPLATE = "\"%scontent/%s/%s\", api_url, publisher_id, journal_id";
-  
-  protected static final String PATTERN_TEMPLATE = "\"^%scontent/%s/%s/[0-9]{4}/0*%s/.{8}/art[0-9]{5}\\?crawler=true(&mimetype=(application/pdf|text/html))?$\", api_url, publisher_id, journal_id, volume_name";
+
+  protected static final String PATTERN_TEMPLATE = "\"^%scontent/%s/%s/[0-9]{4}/0*%s/.{8}/art[0-9]{5}\\?crawler=true$\", api_url, publisher_id, journal_id, volume_name";
 
   @Override
   public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au,
-                                                      MetadataTarget target)
-      throws PluginException {
+      MetadataTarget target) throws PluginException {
     return new IngentaArticleIterator(au, new SubTreeArticleIterator.Spec()
-                                              .setTarget(target)
-                                              .setRootTemplate(ROOT_TEMPLATE)
-                                              .setPatternTemplate(PATTERN_TEMPLATE));
+        .setTarget(target).setRootTemplate(ROOT_TEMPLATE)
+        .setPatternTemplate(PATTERN_TEMPLATE), target);
   }
- 
+
   protected static class IngentaArticleIterator extends SubTreeArticleIterator {
 
-    protected static Pattern PLAIN_PATTERN = Pattern.compile("^(.*)content/([^/]+/[^/]+/[0-9]{4}/[^/]+/[^/]+/[^/]+)\\?crawler=true$", Pattern.CASE_INSENSITIVE);
-    
-    protected static Pattern HTML_PATTERN = Pattern.compile("^(.*)content/([^/]+/[^/]+/[0-9]{4}/[^/]+/[^/]+/[^/]+)\\?crawler=true&mimetype=text/html$", Pattern.CASE_INSENSITIVE);
-    
-    protected static Pattern PDF_PATTERN = Pattern.compile("^(.*)content/([^/]+/[^/]+/[0-9]{4}/[^/]+/[^/]+/[^/]+)\\?crawler=true&mimetype=application/pdf$", Pattern.CASE_INSENSITIVE);
-    
+    protected static Pattern PLAIN_PATTERN = Pattern
+        .compile(
+            "^(.*)content/([^/]+/[^/]+/[0-9]{4}/[^/]+/[^/]+/[^/]+)\\?crawler=true$",
+            Pattern.CASE_INSENSITIVE);
+
     protected String baseUrl;
-    
+    protected MetadataTarget target;
+
     public IngentaArticleIterator(ArchivalUnit au,
-                                  SubTreeArticleIterator.Spec spec) {
+        SubTreeArticleIterator.Spec spec, MetadataTarget target) {
       super(au, spec);
-      this.baseUrl = au.getConfiguration().get(ConfigParamDescr.BASE_URL.getKey());
+      this.baseUrl = au.getConfiguration().get(
+          ConfigParamDescr.BASE_URL.getKey());
+      this.target = target;
     }
-    
+
     @Override
     protected ArticleFiles createArticleFiles(CachedUrl cu) {
       String url = cu.getUrl();
       log.debug3("Entry point: " + url);
-      
+
       Matcher mat = PLAIN_PATTERN.matcher(url);
       if (mat.find()) {
-        return processPlainFullTextCu(cu, mat);
+       return processPlainFullTextCu(cu, mat);
       }
-      
-      mat = HTML_PATTERN.matcher(url);
-      if (mat.find()) {
-        return processFullTextHtml(cu, mat);
-      }
-      
-      mat = PDF_PATTERN.matcher(url);
-      if (mat.find()) {
-        return processFullTextPdf(cu, mat);
-      }
-      
-      log.warning("Mismatch between article iterator factory and article iterator: " + url);
+
+      log.warning("Mismatch between article iterator factory and article iterator: "
+          + url);
       return null;
     }
 
     protected ArticleFiles processFullTextHtml(CachedUrl htmlCu, Matcher htmlMat) {
-      CachedUrl plainCu = au.makeCachedUrl(htmlMat.replaceFirst("$1content/$2?crawler=true"));
+      CachedUrl plainCu = au.makeCachedUrl(htmlMat
+          .replaceFirst("$1content/$2?crawler=true"));
       if (plainCu != null && plainCu.hasContent()) {
         log.debug3("Defer to plain URL");
         AuUtil.safeRelease(plainCu);
         return null; // Defer to plain URL
       }
-      
+
       ArticleFiles af = new ArticleFiles();
       af.setFullTextCu(htmlCu);
       guessFullTextPdf(af, htmlMat);
@@ -119,19 +111,29 @@ public class IngentaArticleIteratorFactory
     }
 
     protected ArticleFiles processFullTextPdf(CachedUrl pdfCu, Matcher pdfMat) {
-      CachedUrl plainCu = au.makeCachedUrl(pdfMat.replaceFirst("$1content/$2?crawler=true"));
+      CachedUrl plainCu = au.makeCachedUrl(pdfMat
+          .replaceFirst("$1content/$2?crawler=true"));
       if (plainCu != null && plainCu.hasContent()) {
         log.debug3("Defer to plain URL");
         AuUtil.safeRelease(plainCu);
         return null; // Defer to plain URL
       }
-      
-      CachedUrl htmlCu = au.makeCachedUrl(pdfMat.replaceFirst("$1content/$2?crawler=true&mimetype=text/html"));
+
+      CachedUrl plainMetaCu = au.makeCachedUrl(pdfMat
+          .replaceFirst("$1content/$2"));
+      if (plainMetaCu != null && plainMetaCu.hasContent()) {
+        log.debug3("Defer to plain URL");
+        AuUtil.safeRelease(plainMetaCu);
+        return null; // Defer to plain URL
+      }
+
+      CachedUrl htmlCu = au.makeCachedUrl(pdfMat
+          .replaceFirst("$1content/$2?crawler=true&mimetype=text/html"));
       if (htmlCu != null && htmlCu.hasContent()) {
         AuUtil.safeRelease(htmlCu);
         return null; // Defer to HTML URL
       }
-      
+
       ArticleFiles af = new ArticleFiles();
       af.setFullTextCu(pdfCu);
       if (spec.getTarget() != MetadataTarget.Article) {
@@ -141,26 +143,18 @@ public class IngentaArticleIteratorFactory
       return af;
     }
 
-    protected ArticleFiles processPlainFullTextCu(CachedUrl plainCu, Matcher plainMat) {
-      if (!plainCu.hasContent()) {
-        log.debug3("Plain URL has no content");
-        return null;
-      }
+    protected ArticleFiles processPlainFullTextCu(CachedUrl plainCu,
+        Matcher plainMat) {
       
       ArticleFiles af = new ArticleFiles();
       af.setFullTextCu(plainCu);
-      if (plainCu.getContentType().toLowerCase().startsWith("text/html")) {
-        log.debug3("Underlying type: HTML");
-        af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_HTML, plainCu);
-        guessFullTextPdf(af, plainMat);
-      }
-      else if (plainCu.getContentType().toLowerCase().startsWith("application/pdf")) {
-        log.debug3("Underlying type: PDF");
-        af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_PDF, plainCu);
+      af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_PDF, plainCu);
+      if (target != MetadataTarget.Article) {
         guessFullTextHtml(af, plainMat);
       }
-      else {
-        log.warning("Unexpected content type of " + plainCu.getUrl() + ": " + plainCu.getContentType());
+     else {
+        log.warning("Unexpected content type of " + plainCu.getUrl() + ": "
+            + plainCu.getContentType());
       }
       if (spec.getTarget() != MetadataTarget.Article) {
         guessAbstract(af, plainMat);
@@ -168,45 +162,51 @@ public class IngentaArticleIteratorFactory
       }
       return af;
     }
-    
+
     protected void guessFullTextHtml(ArticleFiles af, Matcher mat) {
-      CachedUrl htmlCu = au.makeCachedUrl(mat.replaceFirst("$1content/$2?crawler=true&mimetype=text/html"));
+
+      String modStr = mat
+          .replaceFirst("http://www.ingentaconnect.com/content/$2");
+      CachedUrl htmlCu = au.makeCachedUrl(modStr);
       if (htmlCu != null && htmlCu.hasContent()) {
         af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_HTML, htmlCu);
         AuUtil.safeRelease(htmlCu);
       }
     }
-    
+
     protected void guessFullTextPdf(ArticleFiles af, Matcher mat) {
-      CachedUrl pdfCu = au.makeCachedUrl(mat.replaceFirst("$1content/$2?crawler=true&mimetype=application/pdf"));
+      CachedUrl pdfCu = au.makeCachedUrl(mat
+          .replaceFirst("$1content/$2?crawler=true&mimetype=application/pdf"));
       if (pdfCu != null && pdfCu.hasContent()) {
         af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_PDF, pdfCu);
         AuUtil.safeRelease(pdfCu);
       }
     }
-    
+
     protected void guessAbstract(ArticleFiles af, Matcher mat) {
-      CachedUrl absCu = au.makeCachedUrl(mat.replaceFirst(String.format("%scontent/$2", baseUrl)));
+      CachedUrl absCu = au.makeCachedUrl(mat.replaceFirst(String.format(
+          "%scontent/$2", baseUrl)));
       if (absCu != null && absCu.hasContent()) {
         af.setRoleCu(ArticleFiles.ROLE_ABSTRACT, absCu);
         AuUtil.safeRelease(absCu);
       }
     }
-    
+
     protected void guessReferences(ArticleFiles af, Matcher mat) {
-      CachedUrl refCu = au.makeCachedUrl(mat.replaceFirst(String.format("%scontent/$2/references", baseUrl)));
+      CachedUrl refCu = au.makeCachedUrl(mat.replaceFirst(String.format(
+          "%scontent/$2/references", baseUrl)));
       if (refCu != null && refCu.hasContent()) {
         af.setRoleCu(ArticleFiles.ROLE_REFERENCES, refCu);
         AuUtil.safeRelease(refCu);
       }
     }
-    
+
   }
 
   @Override
-  public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target)
-      throws PluginException {
-    return new BaseArticleMetadataExtractor(null);
+  public ArticleMetadataExtractor createArticleMetadataExtractor(
+      MetadataTarget target) throws PluginException {
+    return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_FULL_TEXT_HTML);
   }
-  
+
 }
