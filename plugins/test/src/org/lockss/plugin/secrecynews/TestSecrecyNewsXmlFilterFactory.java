662d8c593b531328a8aab977c58d8e6e7d9dea9d
davidecorcoran <claire@lockss.org>
2012-05-04 22:30:59 +0000

    Initial commit
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@17074 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/test/src/org/lockss/plugin/secrecynews/TestSecrecyNewsXmlFilterFactory.java
@@ -0,0 +1,70 @@
+/*
+ * $Id: TestSecrecyNewsXmlFilterFactory.java,v 1.1 2012-05-04 22:30:59 davidecorcoran Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.secrecynews;
+
+import java.io.*;
+
+import org.lockss.util.*;
+import org.lockss.daemon.PluginException;
+import org.lockss.test.*;
+
+public class TestSecrecyNewsXmlFilterFactory extends LockssTestCase {
+  static String ENC = Constants.DEFAULT_ENCODING;
+
+  private SecrecyNewsXmlFilterFactory fact;
+  private MockArchivalUnit mau;
+
+  public void setUp() throws Exception {
+    super.setUp();
+    fact = new SecrecyNewsXmlFilterFactory();
+    mau = new MockArchivalUnit();
+  }
+
+  private static final String inst1 = 
+    "<lastBuildDate>Sat, 07 May 2012 22:39:18 +0000</lastBuildDate>";
+  
+  private static final String inst2 =
+    "<lastBuildDate>Wed, 25 May 2012 13:54:18 +0000</lastBuildDate>";
+
+  public void testFiltering() throws PluginException, IOException {
+    InputStream inA;
+    InputStream inB;
+
+    inA = fact.createFilteredInputStream(mau,
+           new StringInputStream(inst1), ENC);
+    inB = fact.createFilteredInputStream(mau,
+           new StringInputStream(inst2), ENC);
+    assertEquals(StringUtil.fromInputStream(inA),
+                 StringUtil.fromInputStream(inB));
+  }
+}
