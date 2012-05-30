6a3170871d05447cc2b0e9b7b88f98bac71dbcf5
tlipkis <tal@pss.com>
2012-05-30 08:30:39 +0000

    Clarify exception.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@17681 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/test/ConfigurationUtil.java
+++ test/src/org/lockss/test/ConfigurationUtil.java
@@ -1,8 +1,10 @@
 /*
- * $Id: ConfigurationUtil.java,v 1.20 2010-04-05 17:21:50 pgust Exp $
- *
+ * $Id: ConfigurationUtil.java,v 1.21 2012-05-30 08:30:39 tlipkis Exp $
+ */
 
-Copyright (c) 2000-2005 Board of Trustees of Leland Stanford Jr. University,
+/*
+
+Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -238,8 +240,7 @@ public class ConfigurationUtil {
       PrivilegedAccessor.invokeMethod(mgr(), "updateConfig",
 				      ListUtil.list(cf));
     } catch (Exception e) {
-      //      throw new RuntimeException(e.toString());
-      throw new RuntimeException(StringUtil.stackTraceString(e));
+      throw new RuntimeException(e);
     }
     return true;
   }
