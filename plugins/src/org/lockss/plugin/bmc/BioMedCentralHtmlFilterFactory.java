20f2eff36d4409893e95b07884297b505adac24a
davidecorcoran <claire@lockss.org>
2012-06-18 23:39:12 +0000

    Updated HTML filters
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@18181 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/bmc/BioMedCentralHtmlFilterFactory.java
+++ plugins/src/org/lockss/plugin/bmc/BioMedCentralHtmlFilterFactory.java
@@ -1,5 +1,5 @@
 /*
- * $Id: BioMedCentralHtmlFilterFactory.java,v 1.4 2010-11-17 19:59:27 thib_gc Exp $
+ * $Id: BioMedCentralHtmlFilterFactory.java,v 1.5 2012-06-18 23:39:12 davidecorcoran Exp $
  */
 
 /*
@@ -70,6 +70,8 @@ public class BioMedCentralHtmlFilterFactory implements FilterFactory {
         HtmlNodeFilters.tagWithAttribute("table", "class", "footer2t"),
         // Institution-dependent link resolvers
         HtmlNodeFilters.tagWithAttributeRegex("a", "href", "^/sfx_links\\.asp"),
+        // Institution-dependent greeting
+        HtmlNodeFilters.tagWithAttribute("span", "id", "username")
     };
     return new HtmlFilterInputStream(in, encoding, HtmlNodeFilterTransform.exclude(new OrFilter(filters)));
   }
