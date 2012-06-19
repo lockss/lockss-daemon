7744cf608e46664a159c5321d73329ec6f7d989b
mellen22 <maryellen@lockss.org>
2012-06-19 00:24:11 +0000

    new script to evaluate poll count
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@18184 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ scripts/tdb/pollcheck.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#
+#Checks content servers to see if they are available
+#for i in content1 content2 content3 content4 content5 content6
+echo "Use the ingest machines with the lowest number of polls in progress."
+export http_proxy=http://lockss.org:8888
+export no_proxy=localhost,127.0.0.1
+for i in ingest1 ingest2 ingest3 ingest4 
+do
+echo "$i:"
+wget -o /dev/null -O - --tries=1 --timeout=4 --http-user=lockss-u --http-password=lockss-p http://${i}.clockss.org:8081/DaemonStatus?table=V3PollerTable 2>&1 | grep -A 1 "<b>Polls</b>:" | tail -1 | sed s/.....// | sed s/....$// | perl -ne 'print $_; $t=0; if (m/(\d+) started, (\d+) Complete/) {$t=$1-$2; if (m/(\d+) No Quorum/) {$t=$t-$1}; print "Polls in progress: $t\n"}'
+
+done
+
+exit 0
+
+
+
+
+
