283a9a70299316e4486af5c8b291bcb07c1b23e1
tlipkis <tal@pss.com>
2012-06-25 05:53:43 +0000

    Named intern() pools for Strings.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@18325 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/util/StringPool.java
@@ -0,0 +1,92 @@
+/*
+ * $Id: StringPool.java,v 1.1 2012-06-25 05:53:43 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+import java.util.*;
+
+
+/**
+ * Named intern() pools for Strings.  Similer to String.intern(), but use
+ * of context-dependent pools should allow for smaller maps with less
+ * waste.
+ */
+public class StringPool {
+
+  public static StringPool AU_CONFIG_PROPS = new StringPool("AU config props");
+  public static StringPool HTTP_HEADERS = new StringPool("HTTP headers");
+
+  private String name;
+  private Map<String,String> map;
+
+  public StringPool(String name) {
+    this(name, 20);
+  }
+
+  /** Create a StringPool with a name and initial size */
+  public StringPool(String name, int initialSize) {
+    this.name = name;
+    map = new HashMap<String,String>(initialSize);
+  }
+
+  /** Return the instance of the string already in the pool, if any, else
+   * add this instance and return it. */
+  public synchronized String intern(String str) {
+    if (true) {
+      String res = map.get(str);
+      if (res == null) {
+	map.put(str, str);
+	res = str;
+      }
+      return res;
+    } else {
+      return str;
+    }
+  }
+
+  private int sumStringChars() {
+    int res = 0;
+    for (String val : map.values()) {
+      res += val.length();
+    }
+    return res;
+  }
+
+  public String toString() {
+    return "[StringPool " + name + ", " + map.size() + " entries]";
+  }
+
+  public String toStats() {
+    return "[StringPool " + name + ", " + map.size() + " entries, " +
+      sumStringChars() + " total chars]";
+  }
+
+}
