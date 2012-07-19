6beab04fa05ad3d226a30d8b56879dea0c0dd971
thibgc <github@thib.org>
2012-07-19 09:57:23 +0000

    Prevent NPE.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@18925 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/pdf/pdfbox/PdfBoxDocument.java
+++ src/org/lockss/pdf/pdfbox/PdfBoxDocument.java
@@ -1,5 +1,5 @@
 /*
- * $Id: PdfBoxDocument.java,v 1.4 2012-07-19 04:01:53 thib_gc Exp $
+ * $Id: PdfBoxDocument.java,v 1.5 2012-07-19 09:57:23 thib_gc Exp $
  */
 
 /*
@@ -176,7 +176,11 @@ public class PdfBoxDocument implements PdfDocument {
   @Override
   public String getMetadata() throws PdfException {
     try {
-      return pdDocument.getDocumentCatalog().getMetadata().getInputStreamAsString();
+      PDMetadata metadata = pdDocument.getDocumentCatalog().getMetadata();
+      if (metadata == null) {
+        return null;
+      }
+      return metadata.getInputStreamAsString();
     }
     catch (IOException ioe) {
       throw new PdfException("Error converting metadata stream to string", ioe);
@@ -186,7 +190,11 @@ public class PdfBoxDocument implements PdfDocument {
   @Override
   public Document getMetadataAsXmp() throws PdfException {
     try {
-      return pdDocument.getDocumentCatalog().getMetadata().exportXMPMetadata().getXMPDocument();
+      PDMetadata metadata = pdDocument.getDocumentCatalog().getMetadata();
+      if (metadata == null) {
+        return null;
+      }
+      return metadata.exportXMPMetadata().getXMPDocument();
     }
     catch (IOException ioe) {
       throw new PdfException("Error parsing XMP data", ioe);
