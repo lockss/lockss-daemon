babfa2a18ae2537c573ac98c98d31283afdd90c9
mellen22 <maryellen@lockss.org>
2012-07-26 17:14:45 +0000

    Removed clockss folder from AU counts. Added column for plugin parent.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@19076 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/statuses_plugins.sh
+++ scripts/tdb/statuses_plugins.sh
@@ -8,13 +8,18 @@ mkdir -p $tpath
 
 cd ../../plugins/src
 
+
+
+
 #To get the plugins with AU counts:
 grep -rl --include "*.xml" "plugin_identifier" * | sed 's/\(.*\).xml/\1/' | sort -u > $tpath/ab.txt
 cat ../../tdb/*/*.tdb | ../../scripts/tdb/tdbout -t plugin | sort -u | sed 's/\./\//g' > $tpath/ac.txt
 #plugins that have no AUs.
 diff $tpath/ab.txt $tpath/ac.txt | grep "^< " | sed 's/..\(.*\)/\1,0/' > $tpath/foo0.txt
 #plugins that have AUs
-cat ../../tdb/*/*.tdb | ../../scripts/tdb/tdbout -t plugin | sed 's/\./\//g' | awk -F"\t" 'BEGIN {OFS=","} {foo[$0]++} END {for(x in foo) {print x,foo[x]}}' >> $tpath/foo0.txt
+shopt -s extglob
+cat ../../tdb/!(clockss)/*.tdb | ../../scripts/tdb/tdbout -t plugin | sed 's/\./\//g' | awk -F"\t" 'BEGIN {OFS=","} {foo[$0]++} END {for(x in foo) {print x,foo[x]}}' >> $tpath/foo0.txt
+shopt -u extglob
 
 #To get the plugins with and without status:
 grep -r -A 1 --include "*Plugin.xml" "plugin_status" * | grep "\- " | sed 's/\(.*Plugin\).xml-.*<string>\(.*\)<\/string>/\1,\2/' > $tpath/foo1.txt
@@ -36,14 +41,19 @@ grep -rL --include "*Plugin.xml" "plugin_article_metadata_extractor_factory" * |
 grep -r --include "*Plugin.xml" "au_feature_urls" * | sed 's/\(.*Plugin\).xml.*<string>.*<\/string>/\1,openurl/' > $tpath/foo5.txt
 grep -rL --include "*Plugin.xml" "au_feature_urls" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo5.txt
 
+#To get the plugins with and without the parent plugin:
+grep -r -A 1 --include "*Plugin.xml" "plugin_parent<" * | grep "\- " | sed 's/\(.*\).xml-.*<string>.*\.\(.*\)<\/string>/\1,\2/' > $tpath/foo6.txt
+grep -rL --include "*Plugin.xml" "plugin_parent<" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo6.txt
+
 cat $tpath/foo0.txt | sort -t, > $tpath/blatz0.txt
 cat $tpath/foo1.txt | sort -t, > $tpath/blatz1.txt
 cat $tpath/foo2.txt | sort -t, > $tpath/blatz2.txt
 cat $tpath/foo3.txt | sort -t, > $tpath/blatz3.txt
 cat $tpath/foo4.txt | sort -t, > $tpath/blatz4.txt
 cat $tpath/foo5.txt | sort -t, > $tpath/blatz5.txt
+cat $tpath/foo6.txt | sort -t, > $tpath/blatz6.txt
 
-echo "Plugin,AUs,Substance,Art.It.,MetadataEx,OpenURL,Status"
-join -t, $tpath/blatz0.txt $tpath/blatz2.txt | join -t, - $tpath/blatz3.txt | join -t, - $tpath/blatz4.txt | join -t, - $tpath/blatz5.txt | join -t, - $tpath/blatz1.txt | sort -t, -k 2 -nr 
+echo "Plugin,Parent,AUs,Substance,Art.It.,MetadataEx,OpenURL,Status"
+join -t, $tpath/blatz6.txt $tpath/blatz0.txt | join -t, - $tpath/blatz2.txt | join -t, - $tpath/blatz3.txt | join -t, - $tpath/blatz4.txt | join -t, - $tpath/blatz5.txt | join -t, - $tpath/blatz1.txt | sort -t, -k 3 -nr 
 
 exit 0
