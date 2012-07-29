f9394be9cb65884670dd149bc5beb3e7f7b22b7d
pgust <claire@lockss.org>
2012-07-29 15:08:47 +0000

    Next iteration of metadata table. Now shows headers and summary information.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@19110 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/daemon/MetadataManagerStatusAccessor.java
+++ src/org/lockss/daemon/MetadataManagerStatusAccessor.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MetadataManagerStatusAccessor.java,v 1.1 2012-07-29 00:49:43 pgust Exp $
+ * $Id: MetadataManagerStatusAccessor.java,v 1.2 2012-07-29 15:08:47 pgust Exp $
  */
 
 /*
@@ -35,10 +35,13 @@ import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
 
+import org.lockss.daemon.status.ColumnDescriptor;
 import org.lockss.daemon.status.OverviewAccessor;
 import org.lockss.daemon.status.StatusAccessor;
 import org.lockss.daemon.status.StatusService.NoSuchTableException;
 import org.lockss.daemon.status.StatusTable;
+import org.lockss.util.CatalogueOrderComparator;
+import org.lockss.util.ListUtil;
 import org.lockss.util.StringUtil;
 
 /**
@@ -54,20 +57,121 @@ public class MetadataManagerStatusAccessor implements StatusAccessor {
 
   final MetadataManager metadataMgr;
   
+  private static final String AU_COL_NAME = "au";
+  private static final String INDEX_TYPE = "crawl_type";
+  private static final String START_TIME_COL_NAME = "start";
+  private static final String END_TIME_COL_NAME = "end";
+  private static final String DURATION_COL_NAME = "dur";
+  private static final String INDEX_STATUS = "status";
+  private static final String NUM_INDEXED = "num_indexed";
+  private static final String NUM_ERRORS = "num_errors";
+
+  final private List<ColumnDescriptor> colDescs =
+      ListUtil.fromArray(new ColumnDescriptor[] {
+        new ColumnDescriptor(AU_COL_NAME, "Journal Volume",
+                             ColumnDescriptor.TYPE_STRING)
+        .setComparator(CatalogueOrderComparator.SINGLETON),
+        new ColumnDescriptor(INDEX_TYPE, "Index Type",
+                             ColumnDescriptor.TYPE_STRING),
+        new ColumnDescriptor(START_TIME_COL_NAME, "Start Time",
+                             ColumnDescriptor.TYPE_DATE),
+        new ColumnDescriptor(END_TIME_COL_NAME, "End Time",
+                             ColumnDescriptor.TYPE_DATE),
+        new ColumnDescriptor(DURATION_COL_NAME, "Duration",
+                             ColumnDescriptor.TYPE_TIME_INTERVAL),
+        new ColumnDescriptor(INDEX_STATUS, "Status",
+                             ColumnDescriptor.TYPE_STRING),
+        new ColumnDescriptor(NUM_INDEXED, "Articles Indexed",
+                                 ColumnDescriptor.TYPE_INT),
+        new ColumnDescriptor(NUM_ERRORS, "Errors",
+                             ColumnDescriptor.TYPE_INT,
+                             "Number of articles that could not be indexed"),
+      });
+
+  private static final String SORT_KEY1 = "sort1";
+  private static final String SORT_KEY2 = "sort2";
+
+  // ascending by category, descending start or end time
+  private final List sortRules =
+    ListUtil.list(new StatusTable.SortRule(SORT_KEY1, true),
+                  new StatusTable.SortRule(SORT_KEY2, false));
+
+  /**
+   * Create new instance for metadata manager
+   * @param metadataMgr the metadata manager
+   */
   public MetadataManagerStatusAccessor(MetadataManager metadataMgr) {
     this.metadataMgr = metadataMgr;
   }
   
+  private void addIfNonZero(List res, String head, int val) {
+    if (val != 0) {
+      res.add(new StatusTable.SummaryInfo(head,
+                                          ColumnDescriptor.TYPE_INT,
+                                          new Long(val)));
+    }
+  }
+
+  private List getSummaryInfo() {
+    List res = new ArrayList();
+    long totalTime = 0;
+    int activeOps = metadataMgr.getReindexingTaskCount();
+    int pendingOps = 0; // metadataMgr.getPendingTaskCount();
+    int successfulOps = 0; // metadataMgr.getSuccessfulTaskCount();
+    int failedOps = 0; // metadataMgr.getFailedTaskCount();
+    boolean indexingEnabled = metadataMgr.isIndexingEnabled();
+    
+    res.add(new StatusTable.SummaryInfo(
+        "Active Indexing Operations",
+        ColumnDescriptor.TYPE_INT,
+        activeOps));
+
+    res.add(new StatusTable.SummaryInfo(
+        "Pending Indexing Operations",
+        ColumnDescriptor.TYPE_INT,
+        pendingOps));
+
+    res.add(new StatusTable.SummaryInfo(
+        "Successful Indexing Operations",
+        ColumnDescriptor.TYPE_INT,
+        successfulOps));
+
+    res.add(new StatusTable.SummaryInfo(
+        "Failed Indexing Operations",
+        ColumnDescriptor.TYPE_INT,
+        failedOps));
+
+    res.add(new StatusTable.SummaryInfo("Indexing enabled",
+                                          ColumnDescriptor.TYPE_STRING,
+                                          indexingEnabled));
+    return res;
+  }
+
+  private List getRows() {
+    List rows = new ArrayList();
+    return rows;
+  }
+
+  private List getColDescs() {
+    List res = new ArrayList(colDescs.size());
+    for (ColumnDescriptor desc : colDescs) {
+      res.add(desc);
+    }
+    return res;
+  }
+
   @Override
   public void populateTable(StatusTable table) throws NoSuchTableException {
-    // TODO Auto-generated method stub
-    
+    table.setRows(getRows());
+    table.setDefaultSortRules(sortRules);
+    table.setColumnDescriptors(getColDescs());
+    table.setSummaryInfo(getSummaryInfo());
   }
 
   @Override
   public String getDisplayName() {
     // TODO Auto-generated method stub
-    return "Metadata Indexing Table";
+    return "Metadata Indexing Status";
   }
 
   @Override
