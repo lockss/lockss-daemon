810851d3056c05b8e069c7797b04bc2ae3f284d9
mellen22 <maryellen@lockss.org>
2012-08-01 02:50:55 +0000

    Added parent_plugin_version. Improved capture of data.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@19186 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/statuses_plugins.sh
+++ scripts/tdb/statuses_plugins.sh
@@ -8,9 +8,6 @@ mkdir -p $tpath
 
 cd ../../plugins/src
 
-
-
-
 #To get the plugins with AU counts:
 grep -rl --include "*.xml" "plugin_identifier" * | sed 's/\(.*\).xml/\1/' | sort -u > $tpath/ab.txt
 cat ../../tdb/*/*.tdb | ../../scripts/tdb/tdbout -t plugin | sort -u | sed 's/\./\//g' > $tpath/ac.txt
@@ -18,32 +15,40 @@ cat ../../tdb/*/*.tdb | ../../scripts/tdb/tdbout -t plugin | sort -u | sed 's/\.
 diff $tpath/ab.txt $tpath/ac.txt | grep "^< " | sed 's/..\(.*\)/\1,0/' > $tpath/foo0.txt
 #plugins that have AUs
 shopt -s extglob
-cat ../../tdb/!(clockss)/*.tdb | ../../scripts/tdb/tdbout -t plugin | sed 's/\./\//g' | awk -F"\t" 'BEGIN {OFS=","} {foo[$0]++} END {for(x in foo) {print x,foo[x]}}' >> $tpath/foo0.txt
+cat ../../tdb/!(clockssingest)/*.tdb | ../../scripts/tdb/tdbout -t plugin | sed 's/\./\//g' | awk -F"\t" 'BEGIN {OFS=","} {foo[$0]++} END {for(x in foo) {print x,foo[x]}}' >> $tpath/foo0.txt
 shopt -u extglob
 
 #To get the plugins with and without status:
-grep -r -A 1 --include "*Plugin.xml" "plugin_status" * | grep "\- " | sed 's/\(.*Plugin\).xml-.*<string>\(.*\)<\/string>/\1,\2/' > $tpath/foo1.txt
-grep -rL --include "*Plugin.xml" "plugin_status" * | sed 's/\(.*\).xml/\1,!/' | sort >> $tpath/foo1.txt
+grep -r -A 1 --include "*.xml" "<string>plugin_status</string>" * | grep "\- " | sed 's/\(.*\).xml-.*<string>\(.*\)<\/string>/\1,\2/' > $tpath/foo1.txt
+grep -rL --include "*.xml" "<string>plugin_status</string>" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo1.txt
 
 #To get the plugins with and without substance checkers:
-grep -r --include "*Plugin.xml" "au_substance_url_pattern" * | sed 's/\(.*Plugin\).xml.*<string>.*<\/string>/\1,substance/' > $tpath/foo2.txt
-grep -rL --include "*Plugin.xml" "au_substance_url_pattern" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo2.txt
+grep -r --include "*.xml" "<string>au_substance_url_pattern</string>" * | sed 's/\(.*\).xml.*<string>.*<\/string>/\1,substance/' > $tpath/foo2.txt
+grep -rL --include "*.xml" "<string>au_substance_url_pattern</string>" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo2.txt
 
 #To get the plugins with and without article iterators:
-grep -r --include "*Plugin.xml" "plugin_article_iterator_factory" * | sed 's/\(.*Plugin\).xml.*<string>.*<\/string>/\1,artit/' > $tpath/foo3.txt
-grep -rL --include "*Plugin.xml" "plugin_article_iterator_factory" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo3.txt
+grep -r --include "*.xml" "<string>plugin_article_iterator_factory</string>" * | sed 's/\(.*\).xml.*<string>.*<\/string>/\1,artit/' > $tpath/foo3.txt
+grep -rL --include "*.xml" "<string>plugin_article_iterator_factory</string>" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo3.txt
 
 #To get the plugins with and without metadata extractors:
-grep -r --include "*Plugin.xml" "plugin_article_metadata_extractor_factory" * | sed 's/\(.*Plugin\).xml.*<string>.*<\/string>/\1,metadex/' > $tpath/foo4.txt
-grep -rL --include "*Plugin.xml" "plugin_article_metadata_extractor_factory" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo4.txt
+grep -r --include "*.xml" "<string>plugin_article_metadata_extractor_factory</string>" * | sed 's/\(.*\).xml.*<string>.*<\/string>/\1,metadex/' > $tpath/foo4.txt
+grep -rL --include "*.xml" "<string>plugin_article_metadata_extractor_factory</string>" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo4.txt
 
 #To get the plugins with and without OpenURL resolver feature URLs:
-grep -r --include "*Plugin.xml" "au_feature_urls" * | sed 's/\(.*Plugin\).xml.*<string>.*<\/string>/\1,openurl/' > $tpath/foo5.txt
-grep -rL --include "*Plugin.xml" "au_feature_urls" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo5.txt
+grep -r --include "*.xml" "<string>au_feature_urls</string>" * | sed 's/\(.*\).xml.*<string>.*<\/string>/\1,openurl/' > $tpath/foo5.txt
+grep -rL --include "*.xml" "<string>au_feature_urls</string>" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo5.txt
 
 #To get the plugins with and without the parent plugin:
-grep -r -A 1 --include "*Plugin.xml" "plugin_parent<" * | grep "\- " | sed 's/\(.*\).xml-.*<string>.*\.\(.*\)<\/string>/\1,\2/' > $tpath/foo6.txt
-grep -rL --include "*Plugin.xml" "plugin_parent<" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo6.txt
+grep -r -A 1 --include "*.xml" "<string>plugin_parent</string>" * | grep "\- " | sed 's/\(.*\).xml-.*<string>.*\.\(.*\)<\/string>/\1,\2/' > $tpath/foo6.txt
+grep -rL --include "*.xml" "<string>plugin_parent</string>" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo6.txt
+
+#To get the plugins with and without the parent plugin version:
+grep -r -A 1 --include "*.xml" "<string>plugin_parent_version</string>" * | grep "\- " | sed 's/\(.*\).xml-.*<string>\(.*\)<\/string>/\1,\2/' > $tpath/foo7.txt
+grep -rL --include "*.xml" "<string>plugin_parent_version</string>" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo7.txt
+
+#To get the plugins with and without the plugin version:
+grep -r -A 1 --include "*.xml" "<string>plugin_version</string>" * | grep "\- " | sed 's/\(.*\).xml-.*<string>\(.*\)<\/string>/\1,\2/' > $tpath/foo8.txt
+grep -rL --include "*.xml" "<string>plugin_version</string>" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo8.txt
 
 cat $tpath/foo0.txt | sort -t, > $tpath/blatz0.txt
 cat $tpath/foo1.txt | sort -t, > $tpath/blatz1.txt
@@ -52,8 +57,14 @@ cat $tpath/foo3.txt | sort -t, > $tpath/blatz3.txt
 cat $tpath/foo4.txt | sort -t, > $tpath/blatz4.txt
 cat $tpath/foo5.txt | sort -t, > $tpath/blatz5.txt
 cat $tpath/foo6.txt | sort -t, > $tpath/blatz6.txt
+cat $tpath/foo7.txt | sort -t, > $tpath/blatz7.txt
+cat $tpath/foo8.txt | sort -t, > $tpath/blatz8.txt
 
-echo "Plugin,Parent,AUs,Substance,Art.It.,MetadataEx,OpenURL,Status"
-join -t, $tpath/blatz6.txt $tpath/blatz0.txt | join -t, - $tpath/blatz2.txt | join -t, - $tpath/blatz3.txt | join -t, - $tpath/blatz4.txt | join -t, - $tpath/blatz5.txt | join -t, - $tpath/blatz1.txt | sort -t, -k 3 -nr 
+#echo "Plugin,V,Parent,PV,AUs,Substance,Art.It.,MetadataEx,OpenURL,Status"
+#join -e -t, -e EMPTY $tpath/blatz8.txt $tpath/blatz6.txt | join -t, -e EMPTY - $tpath/blatz7.txt | join -t, -e EMPTY - $tpath/blatz0.txt | join -t, -e EMPTY - $tpath/blatz2.txt | join -t, -e EMPTY - $tpath/blatz3.txt | join -t, -e EMPTY - $tpath/blatz4.txt | join -t, -e EMPTY - $tpath/blatz5.txt | join -t, -e EMPTY - $tpath/blatz1.txt | sort -t, -k 5 -nr 
+echo "Plugin,Parent,PV,AUs,Substance,Art.It.,MetadataEx,OpenURL,Status"
+join -t, $tpath/blatz6.txt $tpath/blatz7.txt | join -t, - $tpath/blatz0.txt | join -t, - $tpath/blatz2.txt | join -t, - $tpath/blatz3.txt | join -t, - $tpath/blatz4.txt | join -t, - $tpath/blatz5.txt | join -t, - $tpath/blatz1.txt | sort -t, -k 4 -nr 
+#echo "Plugin,Parent,AUs,Substance,Art.It.,MetadataEx,OpenURL,Status"
+#join -t, $tpath/blatz6.txt $tpath/blatz0.txt | join -t, - $tpath/blatz2.txt | join -t, - $tpath/blatz3.txt | join -t, - $tpath/blatz4.txt | join -t, - $tpath/blatz5.txt | join -t, - $tpath/blatz1.txt | sort -t, -k 3 -nr 
 
 exit 0
