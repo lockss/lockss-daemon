ab14183a832326ca0ca499a728008739e329820a
mellen22 <maryellen@lockss.org>
2012-08-01 22:39:13 +0000

    Improve sort.  Use find instead of shell command. Add plugin version.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@19206 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/statuses_plugins.sh
+++ scripts/tdb/statuses_plugins.sh
@@ -14,9 +14,9 @@ cat ../../tdb/*/*.tdb | ../../scripts/tdb/tdbout -t plugin | sort -u | sed 's/\.
 #plugins that have no AUs.
 diff $tpath/ab.txt $tpath/ac.txt | grep "^< " | sed 's/..\(.*\)/\1,0/' > $tpath/foo0.txt
 #plugins that have AUs
-shopt -s extglob
-cat ../../tdb/!(clockssingest)/*.tdb | ../../scripts/tdb/tdbout -t plugin | sed 's/\./\//g' | awk -F"\t" 'BEGIN {OFS=","} {foo[$0]++} END {for(x in foo) {print x,foo[x]}}' >> $tpath/foo0.txt
-shopt -u extglob
+#shopt -s extglob
+find ../../tdb -name clockssingest -prune -o -name \*.tdb -print | xargs cat | ../../scripts/tdb/tdbout -t plugin | sed 's/\./\//g' | awk -F"\t" 'BEGIN {OFS=","} {foo[$0]++} END {for(x in foo) {print x,foo[x]}}' >> $tpath/foo0.txt
+#shopt -u extglob
 
 #To get the plugins with and without status:
 grep -r -A 1 --include "*.xml" "<string>plugin_status</string>" * | grep "\- " | sed 's/\(.*\).xml-.*<string>\(.*\)<\/string>/\1,\2/' > $tpath/foo1.txt
@@ -50,21 +50,17 @@ grep -rL --include "*.xml" "<string>plugin_parent_version</string>" * | sed 's/\
 grep -r -A 1 --include "*.xml" "<string>plugin_version</string>" * | grep "\- " | sed 's/\(.*\).xml-.*<string>\(.*\)<\/string>/\1,\2/' > $tpath/foo8.txt
 grep -rL --include "*.xml" "<string>plugin_version</string>" * | sed 's/\(.*\).xml/\1,!/' >> $tpath/foo8.txt
 
-cat $tpath/foo0.txt | sort -t, > $tpath/blatz0.txt
-cat $tpath/foo1.txt | sort -t, > $tpath/blatz1.txt
-cat $tpath/foo2.txt | sort -t, > $tpath/blatz2.txt
-cat $tpath/foo3.txt | sort -t, > $tpath/blatz3.txt
-cat $tpath/foo4.txt | sort -t, > $tpath/blatz4.txt
-cat $tpath/foo5.txt | sort -t, > $tpath/blatz5.txt
-cat $tpath/foo6.txt | sort -t, > $tpath/blatz6.txt
-cat $tpath/foo7.txt | sort -t, > $tpath/blatz7.txt
-cat $tpath/foo8.txt | sort -t, > $tpath/blatz8.txt
+cat $tpath/foo0.txt | sort -t, -k 1,1 > $tpath/blatz0.txt
+cat $tpath/foo1.txt | sort -t, -k 1,1 > $tpath/blatz1.txt
+cat $tpath/foo2.txt | sort -t, -k 1,1 > $tpath/blatz2.txt
+cat $tpath/foo3.txt | sort -t, -k 1,1 > $tpath/blatz3.txt
+cat $tpath/foo4.txt | sort -t, -k 1,1 > $tpath/blatz4.txt
+cat $tpath/foo5.txt | sort -t, -k 1,1 > $tpath/blatz5.txt
+cat $tpath/foo6.txt | sort -t, -k 1,1 > $tpath/blatz6.txt
+cat $tpath/foo7.txt | sort -t, -k 1,1 > $tpath/blatz7.txt
+cat $tpath/foo8.txt | sort -t, -k 1,1 > $tpath/blatz8.txt
 
-#echo "Plugin,V,Parent,PV,AUs,Substance,Art.It.,MetadataEx,OpenURL,Status"
-#join -e -t, -e EMPTY $tpath/blatz8.txt $tpath/blatz6.txt | join -t, -e EMPTY - $tpath/blatz7.txt | join -t, -e EMPTY - $tpath/blatz0.txt | join -t, -e EMPTY - $tpath/blatz2.txt | join -t, -e EMPTY - $tpath/blatz3.txt | join -t, -e EMPTY - $tpath/blatz4.txt | join -t, -e EMPTY - $tpath/blatz5.txt | join -t, -e EMPTY - $tpath/blatz1.txt | sort -t, -k 5 -nr 
-echo "Plugin,Parent,PV,AUs,Substance,Art.It.,MetadataEx,OpenURL,Status"
-join -t, $tpath/blatz6.txt $tpath/blatz7.txt | join -t, - $tpath/blatz0.txt | join -t, - $tpath/blatz2.txt | join -t, - $tpath/blatz3.txt | join -t, - $tpath/blatz4.txt | join -t, - $tpath/blatz5.txt | join -t, - $tpath/blatz1.txt | sort -t, -k 4 -nr 
-#echo "Plugin,Parent,AUs,Substance,Art.It.,MetadataEx,OpenURL,Status"
-#join -t, $tpath/blatz6.txt $tpath/blatz0.txt | join -t, - $tpath/blatz2.txt | join -t, - $tpath/blatz3.txt | join -t, - $tpath/blatz4.txt | join -t, - $tpath/blatz5.txt | join -t, - $tpath/blatz1.txt | sort -t, -k 3 -nr 
+echo "Plugin,V,Parent,PV,AUs,Substance,Art.It.,MetadataEx,OpenURL,Status"
+join -t, -e EMPTY $tpath/blatz8.txt $tpath/blatz6.txt | join -t, -e EMPTY - $tpath/blatz7.txt | join -t, -e EMPTY - $tpath/blatz0.txt | join -t, -e EMPTY - $tpath/blatz2.txt | join -t, -e EMPTY - $tpath/blatz3.txt | join -t, -e EMPTY - $tpath/blatz4.txt | join -t, -e EMPTY - $tpath/blatz5.txt | join -t, -e EMPTY - $tpath/blatz1.txt | sort -t, -k 5 -nr 
 
 exit 0
