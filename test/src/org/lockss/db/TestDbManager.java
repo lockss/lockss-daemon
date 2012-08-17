28453f203ba085375ab52bb5bfd5ebd4fd0f832f
fergaloy <fergaloy@stanford.edu>
2012-08-17 16:13:38 +0000

    Tests for DbManager.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@19644 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/db/TestDbManager.java
@@ -0,0 +1,239 @@
+/*
+ * $Id: TestDbManager.java,v 1.1 2012-08-17 16:13:38 fergaloy-sf Exp $
+ */
+
+/*
+
+ Copyright (c) 2012 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+/**
+ * Test class for org.lockss.db.DbManager.
+ * 
+ * @author Fernando Garcia-Loygorri
+ * @version 1.0
+ */
+package org.lockss.db;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.lockss.config.ConfigManager;
+import org.lockss.repository.LockssRepositoryImpl;
+import org.lockss.test.ConfigurationUtil;
+import org.lockss.test.LockssTestCase;
+import org.lockss.test.MockLockssDaemon;
+import org.lockss.util.Logger;
+
+public class TestDbManager extends LockssTestCase {
+  private static String TABLE_CREATE_SQL =
+      "create table testtable (id bigint NOT NULL, name varchar(512))";
+
+  private MockLockssDaemon theDaemon;
+  private String tempDirPath;
+  private DbManager dbManager;
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+
+    // Get the temporary directory used during the test.
+    tempDirPath = getTempDir().getAbsolutePath();
+
+    // Set the database log.
+    System.setProperty("derby.stream.error.file",
+		       new File(tempDirPath, "derby.log").getAbsolutePath());
+
+    theDaemon = getMockLockssDaemon();
+    theDaemon.setDaemonInited(true);
+  }
+
+  /**
+   * Tests table creation with the minimal configuration.
+   * 
+   * @throws Exception
+   */
+  public void testCreateTable1() throws Exception {
+    Properties props = new Properties();
+    props.setProperty(LockssRepositoryImpl.PARAM_CACHE_LOCATION, tempDirPath);
+    props
+	.setProperty(ConfigManager.PARAM_PLATFORM_DISK_SPACE_LIST, tempDirPath);
+    ConfigurationUtil.setCurrentConfigFromProps(props);
+
+    createTable();
+  }
+
+  /**
+   * Tests table creation with an absolute database name path.
+   * 
+   * @throws Exception
+   */
+  public void testCreateTable2() throws Exception {
+    Properties props = new Properties();
+    props.setProperty(LockssRepositoryImpl.PARAM_CACHE_LOCATION, tempDirPath);
+    props
+	.setProperty(ConfigManager.PARAM_PLATFORM_DISK_SPACE_LIST, tempDirPath);
+    props.setProperty(DbManager.PARAM_DATASOURCE_DATABASENAME,
+		      new File(tempDirPath, "db/TestDbManager")
+			  .getCanonicalPath());
+    ConfigurationUtil.setCurrentConfigFromProps(props);
+
+    createTable();
+  }
+
+  /**
+   * Tests table creation with the client datasource.
+   * 
+   * @throws Exception
+   */
+  public void testCreateTable3() throws Exception {
+    Properties props = new Properties();
+    props.setProperty(LockssRepositoryImpl.PARAM_CACHE_LOCATION, tempDirPath);
+    props
+	.setProperty(ConfigManager.PARAM_PLATFORM_DISK_SPACE_LIST, tempDirPath);
+    props.setProperty(DbManager.PARAM_DATASOURCE_CLASSNAME,
+		      "org.apache.derby.jdbc.ClientDataSource");
+    ConfigurationUtil.setCurrentConfigFromProps(props);
+
+    createTable();
+  }
+
+  /**
+   * Tests a misconfigured datasource.
+   * 
+   * @throws Exception
+   */
+  public void testNotReady() throws Exception {
+    Properties props = new Properties();
+    props.setProperty(LockssRepositoryImpl.PARAM_CACHE_LOCATION, tempDirPath);
+    props
+	.setProperty(ConfigManager.PARAM_PLATFORM_DISK_SPACE_LIST, tempDirPath);
+    props.setProperty(DbManager.PARAM_DATASOURCE_CLASSNAME, "java.lang.String");
+    ConfigurationUtil.setCurrentConfigFromProps(props);
+
+    startService();
+    assertEquals(false, dbManager.isReady());
+
+    Connection conn = null;
+    try {
+      conn = dbManager.getConnection();
+    } catch (SQLException sqle) {
+    }
+    assertNull(conn);
+
+  }
+
+  /**
+   * Tests the safe roll back.
+   * 
+   * @throws Exception
+   */
+  public void testRollback() throws Exception {
+    Properties props = new Properties();
+    props.setProperty(LockssRepositoryImpl.PARAM_CACHE_LOCATION, tempDirPath);
+    props
+	.setProperty(ConfigManager.PARAM_PLATFORM_DISK_SPACE_LIST, tempDirPath);
+    ConfigurationUtil.setCurrentConfigFromProps(props);
+
+    startService();
+    Connection conn = dbManager.getConnection();
+    assertNotNull(conn);
+    assertFalse(dbManager.tableExists(conn, "testtable"));
+    assertTrue(dbManager.createTableIfMissing(conn, "testtable",
+					      TABLE_CREATE_SQL));
+    assertTrue(dbManager.tableExists(conn, "testtable"));
+
+    DbManager.safeRollbackAndClose(conn);
+    conn = dbManager.getConnection();
+    assertNotNull(conn);
+    assertFalse(dbManager.tableExists(conn, "testtable"));
+  }
+
+  /**
+   * Tests the commit or rollback method.
+   * 
+   * @throws Exception
+   */
+  public void testCommitOrRollback() throws Exception {
+    Properties props = new Properties();
+    props.setProperty(LockssRepositoryImpl.PARAM_CACHE_LOCATION, tempDirPath);
+    props
+	.setProperty(ConfigManager.PARAM_PLATFORM_DISK_SPACE_LIST, tempDirPath);
+    ConfigurationUtil.setCurrentConfigFromProps(props);
+
+    startService();
+    Connection conn = dbManager.getConnection();
+    Logger logger = Logger.getLogger("testCommitOrRollback");
+    DbManager.commitOrRollback(conn, logger);
+    DbManager.safeCloseConnection(conn);
+
+    conn = null;
+    try {
+      DbManager.commitOrRollback(conn, logger);
+    } catch (NullPointerException sqle) {
+    }
+  }
+
+  public void tearDown() throws Exception {
+    dbManager.stopService();
+    theDaemon.stopDaemon();
+    super.tearDown();
+  }
+
+  /**
+   * Creates a table and verifies that it exists.
+   * 
+   * @throws Exception
+   */
+  protected void createTable() throws Exception {
+    startService();
+    assertEquals(true, dbManager.isReady());
+
+    Connection conn = dbManager.getConnection();
+    assertNotNull(conn);
+    assertFalse(dbManager.tableExists(conn, "testtable"));
+    assertTrue(dbManager.createTableIfMissing(conn, "testtable",
+					      TABLE_CREATE_SQL));
+    assertTrue(dbManager.tableExists(conn, "testtable"));
+    dbManager.logTableSchema(conn, "testtable");
+    assertFalse(dbManager.createTableIfMissing(conn, "testtable",
+					       TABLE_CREATE_SQL));
+  }
+
+  /**
+   * Creates and starts the DbManager.
+   * 
+   * @throws Exception
+   */
+  protected void startService() throws Exception {
+    dbManager = new DbManager();
+    theDaemon.setDbManager(dbManager);
+    dbManager.initService(theDaemon);
+    dbManager.startService();
+  }
+}
