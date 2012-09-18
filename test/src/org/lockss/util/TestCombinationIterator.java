b1ed0e3702cc7dd525a457708047de283993855c
clairegriffin <clairetg@stanford.edu>
2012-09-18 21:59:09 +0000

    The test classes for the new iterators.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@20544 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ test/src/org/lockss/util/TestCombinationIterator.java
@@ -0,0 +1,196 @@
+/*
+ * $Id: TestCombinationIterator.java,v 1.1 2012-09-18 21:59:09 clairegriffin Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+package org.lockss.util;
+
+import org.lockss.test.*;
+
+import java.math.*;
+import java.util.*;
+
+/**
+ * CombinationIterator Tester.
+ *
+ * @author Claire Griffin
+ * @since <pre>Sep 14, 2012</pre>
+ * @version 1.0
+ */
+public class TestCombinationIterator extends LockssTestCase {
+  Integer[]   m_ints   = { 1, 2, 3, 4, 5 };
+  Integer[][] m_iCombi1 = {{1},{2},{3},{4},{5}};
+  Integer[][] m_iCombi2 = {{1,2}, {1,3} ,{1,4}, {1,5},
+                           {2,3}, {2,4}, {2,5}, {3,4}, {3,5},{4,5}};
+  Integer[][] m_iCombi3 = {{1,2,3},{1,2,4},{1,2,5},{1,3,4},{1,3,5},{1,4,5},
+                           {2,3,4},{2,3,5},{2,4,5},{3,4,5}};
+  Integer[][] m_iCombi4 = {{1,2,3,4},{1,2,3,5},{1,2,4,5},{1,3,4,5},{2,3,4,5}};
+  Integer[][] m_iCombi5 = {{1,2,3,4,5}};
+
+  String[] m_strs   = { "One", "Two", "Three"};
+  String[][] m_sCombi1 = {{"One"},{"Two"}, {"Three"}};
+  String[][] m_sCombi2 = {{"One","Two"}, {"One","Three"}, {"Two","Three"}};
+  String[][] m_sCombi3 = {{"One","Two","Three"}};
+
+
+  List<Integer> m_iList = Arrays.asList(m_ints);
+  List<String>  m_sList = Arrays.asList(m_strs);
+
+  List<List<Integer>> m_iCombiList;
+  List<List<String>> m_sCombiList;
+
+  public void setUp() throws Exception {
+    m_iList = Arrays.asList(m_ints);
+    m_sList = Arrays.asList(m_strs);
+  }
+
+  public void tearDown() throws Exception {
+  }
+
+  /**
+   *
+   * Method: size(final int N, final int K)
+   *
+   */
+  public void testSize() throws Exception {
+    BigInteger expected = BigInteger.valueOf(m_iCombi3.length);
+
+    assertEquals(expected, CombinationIterator.size(5, 3));
+  }
+
+  /**
+   *
+   * Method: hasNext()
+   *
+   */
+  public void testHasNext() throws Exception {
+    CombinationIterator<Integer> ci = new CombinationIterator<Integer>
+        (m_iList, 3);
+    assertTrue(ci.hasNext());
+    // now call next until for result set time
+    for(int i = 0; i < m_iCombi3.length; i++)
+    {
+      ci.next();
+    }
+    assertFalse(ci.hasNext());
+
+  }
+
+  /**
+   *
+   * Method: next()
+   *
+   */
+  public void testNext() throws Exception {
+     // test the int version
+    checkIntLists(m_iList, 1, m_iCombi1);
+    checkIntLists(m_iList, 2, m_iCombi2);
+    checkIntLists(m_iList, 3, m_iCombi3);
+    checkIntLists(m_iList, 4, m_iCombi4);
+    checkIntLists(m_iList, 5, m_iCombi5);
+     // test the string version
+    checkStringLists(m_sList, 1, m_sCombi1);
+    checkStringLists(m_sList, 2, m_sCombi2);
+    checkStringLists(m_sList, 3, m_sCombi3);
+  }
+
+  /**
+   *
+   * Method: remove()
+   *
+   */
+  public void testRemove() throws Exception {
+    CombinationIterator<Integer> ci = new CombinationIterator<Integer>
+        (m_iList, 3);
+
+    try {
+      ci.remove();
+      assertTrue("Failed to throw exception", false);
+    } catch (Exception ex) {
+      assertTrue(ex instanceof UnsupportedOperationException);
+    }
+  }
+
+  public void testNullList() throws Exception {
+    // test a null list
+    ArrayList<Integer> null_list = new ArrayList<Integer>();
+    try {
+      CombinationIterator<Integer> ci =
+          new CombinationIterator<Integer>(null_list, 1);
+      assertTrue("Null set did not throw!", false);
+    }
+    catch(Exception ex)
+    {
+      assertTrue(ex instanceof IllegalArgumentException);
+
+    }
+  }
+
+  private void checkIntLists(List<Integer> iList, int choose,
+                      Integer[][] combinations)
+  {
+    List<List<Integer>> iCombiList = new ArrayList<List<Integer>>();
+    for(int i=0; i<combinations.length; i++)
+    {
+      iCombiList.add(Arrays.asList(combinations[i]));
+    }
+    CombinationIterator<Integer> ci
+        = new CombinationIterator<Integer>(iList,choose);
+    List<Integer> li;
+
+    while(ci.hasNext()) {
+      li = ci.next();
+      assertTrue(iCombiList.contains(li));
+      iCombiList.remove(li);
+    }
+    assertTrue(iCombiList.isEmpty());
+  }
+
+  private void checkStringLists(List<String> sList, int choose,
+                                String[][] combinations)
+  {
+    List<List<String>> sCombiList = new ArrayList<List<String>>();
+    for(int i=0; i<combinations.length; i++)
+    {
+      sCombiList.add(Arrays.asList(combinations[i]));
+    }
+    CombinationIterator<String> ci
+        = new CombinationIterator<String>(sList,choose);
+    List<String> li;
+
+    while(ci.hasNext()) {
+      li = ci.next();
+      assertTrue(sCombiList.contains(li));
+      sCombiList.remove(li);
+    }
+    assertTrue(sCombiList.isEmpty());
+  }
+
+
+} 
---
+++ test/src/org/lockss/util/TestPowerSetIterator.java
@@ -0,0 +1,159 @@
+package org.lockss.util;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import org.lockss.test.*;
+
+import java.util.*;
+
+//~--- JDK imports ------------------------------------------------------------
+
+/**
+ * PowerSetIterator Tester.
+ *
+ * @author Claire Griffin
+ * @since <pre>Sep 14, 2012</pre>
+ * @version One.0
+ */
+public class TestPowerSetIterator extends LockssTestCase {
+    Integer[]   m_ints   = { 1, 2, 3 };
+    Integer[][] m_intSet = {
+        {}, { 1 }, { 2 }, { 3 }, { 2, 3 }, { 1, 2 }, { 1, 3 }, { 1, 2, 3 }
+    };
+    String[]    m_strs   = { "One", "Two", "Three" };
+    String[][]  m_strSet = {
+        {}, { "One" }, { "Two" }, { "Three" }, { "Two", "Three" }, { "One", "Two" }, { "One", "Three" },
+        { "One", "Two", "Three" }
+    };
+    List<Integer>       m_iSet;
+    List<String>        m_sSet;
+    List<List<Integer>> m_iPowerset;
+    List<List<String>>  m_sPowerset;
+
+    public void setUp() throws Exception {
+        m_iSet = new ArrayList<Integer>();
+
+        for (Integer i_val : m_ints) {
+            m_iSet.add(i_val);
+        }
+
+        m_iPowerset = new ArrayList<List<Integer>>();
+
+        for (Integer[] list : m_intSet) {
+            List il = new ArrayList<Integer>();
+
+            for (Integer i_val : list) {
+                il.add(i_val);
+            }
+
+            m_iPowerset.add(il);
+        }
+
+        m_sSet = new ArrayList<String>();
+
+        for (String s_val : m_strs) {
+            m_sSet.add(s_val);
+        }
+
+        m_sPowerset = new ArrayList<List<String>>();
+
+        for (String[] list : m_strSet) {
+            List sl = new ArrayList<String>();
+
+            for (String s_val : list) {
+                sl.add(s_val);
+            }
+
+            m_sPowerset.add(sl);
+        }
+    }
+
+    public void tearDown() throws Exception {}
+
+    /**
+     *
+     * Method: resultSize(final int N)
+     *
+     */
+    public void testResultSize() throws Exception {
+        long expected = m_iPowerset.size();
+
+        assertEquals(expected, PowerSetIterator.resultSize(m_ints.length));
+    }
+
+
+    /**
+     *
+     * Method: hasNext()
+     *
+     */
+    public void testHasNext() throws Exception {
+      PowerSetIterator<Integer> psi = new PowerSetIterator<Integer>(m_iSet);
+      assertTrue(psi.hasNext());
+      // now call next until for result set time
+      for(int i = 0; i < m_intSet.length; i++)
+      {
+        psi.next();
+      }
+      assertFalse(psi.hasNext());
+    }
+
+    /**
+     *
+     * Method: next()
+     *
+     */
+    public void testNext() throws Exception {
+     // test the int version
+      PowerSetIterator<Integer> psi = new PowerSetIterator<Integer>(m_iSet);
+      while(psi.hasNext())
+      {
+        List<Integer> li = psi.next();
+        assertTrue(m_iPowerset.contains(li));
+        m_iPowerset.remove(li);
+      }
+      assertTrue(m_iPowerset.isEmpty());
+
+     // test the string version
+      PowerSetIterator<String> pss = new PowerSetIterator<String>(m_sSet);
+      while(pss.hasNext())
+      {
+        List<String> ls = pss.next();
+        assertTrue(m_sPowerset.contains(ls));
+        m_sPowerset.remove(ls);
+      }
+      assertTrue(m_sPowerset.isEmpty());
+
+
+    }
+    public void testNull() throws Exception {
+            // test a null list
+      ArrayList<Integer> null_list = new ArrayList<Integer>();
+      try {
+        PowerSetIterator<Integer> psn =
+          new PowerSetIterator<Integer>(null_list);
+        assertTrue("Null set did not throw!", false);
+      }
+      catch(Exception ex)
+      {
+        assertTrue(ex instanceof IllegalArgumentException);
+      }
+
+    }
+    /**
+     *
+     * Method: remove()
+     *
+     */
+    public void testRemove() throws Exception {
+        PowerSetIterator<Integer> psi = new PowerSetIterator<Integer>(m_iSet);
+
+        try {
+            psi.remove();
+            assertTrue("Failed to throw exception", false);
+        } catch (Exception ex) {
+            assertTrue(ex instanceof UnsupportedOperationException);
+        }
+    }
+
+}
