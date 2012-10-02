0159a47ce7cbe0a5d294c6e12852422ecf798c1b
davidecorcoran <claire@lockss.org>
2012-10-02 10:17:09 +0000

    Initial commit
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@20981 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/usdocspln/gov/gpo/fdsys/GPOFDSysSitemapsArticleIteratorFactory.java
@@ -0,0 +1,123 @@
+/*
+ * $Id: GPOFDSysSitemapsArticleIteratorFactory.java,v 1.1 2012-10-02 10:17:09 davidecorcoran Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2011 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.usdocspln.gov.gpo.fdsys;
+
+import java.util.*;
+import java.util.regex.*;
+
+import org.lockss.util.*;
+import org.lockss.plugin.*;
+import org.lockss.extractor.*;
+import org.lockss.daemon.PluginException;
+
+public class GPOFDSysSitemapsArticleIteratorFactory
+  implements ArticleIteratorFactory,
+       ArticleMetadataExtractorFactory {
+  
+  protected static Logger log = 
+    Logger.getLogger("GPOFDSysSitemapsArticleIteratorFactory");
+  
+  protected static final String ROOT_TEMPLATE =
+    "\"%sfdsys/pkg/\", base_url";
+
+  protected static final String PATTERN_TEMPLATE =
+    "\"^%sfdsys/pkg/([^/]+)/(html|pdf)/([^/]+)\\.htm$\", base_url";
+  
+  public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au,
+                                                      MetadataTarget target)
+      throws PluginException {
+    return new 
+      GPOFDSysSitemapsArticleIterator(au,
+              new SubTreeArticleIterator.Spec()
+              .setTarget(target)
+              .setRootTemplate(ROOT_TEMPLATE)
+              .setPatternTemplate(PATTERN_TEMPLATE));
+  }
+  
+  protected static class GPOFDSysSitemapsArticleIterator
+    extends SubTreeArticleIterator {
+    
+    protected static Pattern HTML_PATTERN = 
+      Pattern.compile("fdsys/pkg/([^/]+)/html/([^/]+)\\.htm$",
+        Pattern.CASE_INSENSITIVE);
+    
+    protected static Pattern PDF_PATTERN = 
+      Pattern.compile("fdsys/pkg/([^/]+)/pdf/([^/]+)\\.pdf$",
+        Pattern.CASE_INSENSITIVE);
+    
+    public GPOFDSysSitemapsArticleIterator(ArchivalUnit au,
+                                  SubTreeArticleIterator.Spec spec) {
+      super(au, spec);
+    }
+    
+    @Override
+    protected ArticleFiles createArticleFiles(CachedUrl cu) {
+      String url = cu.getUrl();
+      Matcher mat;
+      
+      mat = HTML_PATTERN.matcher(url);
+      if (mat.find()) {
+        return processFullTextHtml(cu, mat);
+      }
+        
+      mat = PDF_PATTERN.matcher(url);
+      if (mat.find()) {
+        return processFullTextPdf(cu, mat);
+      }
+      
+      log.warning("Mismatch between article iterator factory and article iterator: " + url);
+      return null;
+    }
+    
+    protected ArticleFiles processFullTextHtml(CachedUrl htmlCu,
+        Matcher htmlMat) {
+      ArticleFiles af = new ArticleFiles();
+      af.setFullTextCu(htmlCu);
+      af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_HTML, htmlCu);
+      return af;
+    }
+
+    protected ArticleFiles processFullTextPdf(CachedUrl pdfCu,
+        Matcher pdfMat) {
+      ArticleFiles af = new ArticleFiles();
+      af.setFullTextCu(pdfCu);
+      af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_PDF, pdfCu);
+      return af;
+    }
+  }
+  
+  public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target)
+      throws PluginException {
+    return new BaseArticleMetadataExtractor(null);
+  }
+}
