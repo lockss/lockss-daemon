1e83b4c763f7f4ad7d804d510adc9a8e4adfd00f
thibgc <github@thib.org>
2012-10-23 23:26:30 +0000

    Cosmetic.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@21692 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/ubiquitypress/UbiquityPressUrlNormalizer.java
+++ plugins/src/org/lockss/plugin/ubiquitypress/UbiquityPressUrlNormalizer.java
@@ -1,85 +1,56 @@
 /*
- * $Id: UbiquityPressUrlNormalizer.java,v 1.3 2012-06-21 23:00:56 kendrayee Exp $
+ * $Id: UbiquityPressUrlNormalizer.java,v 1.4 2012-10-23 23:26:29 thib_gc Exp $
  */
 
 /*
 
- Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
- all rights reserved.
+Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
- IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
- Except as contained in this notice, the name of Stanford University shall not
- be used in advertising or otherwise to promote the sale, use or other dealings
- in this Software without prior written authorization from Stanford University.
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
 
- *//*
- * $Id: UbiquityPressUrlNormalizer.java,v 1.3 2012-06-21 23:00:56 kendrayee Exp $
- */
-
-/*
-
- Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
- all rights reserved.
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
- IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
- Except as contained in this notice, the name of Stanford University shall not
- be used in advertising or otherwise to promote the sale, use or other dealings
- in this Software without prior written authorization from Stanford University.
-
- */
+*/
 package org.lockss.plugin.ubiquitypress;
 
 import org.lockss.daemon.ConfigParamDescr;
 import org.lockss.daemon.PluginException;
 import org.lockss.plugin.ArchivalUnit;
 import org.lockss.plugin.UrlNormalizer;
-import org.lockss.util.Logger;
 import org.lockss.util.StringUtil;
 
 public class UbiquityPressUrlNormalizer implements UrlNormalizer {
-//Logger logger = Logger.getLogger("UbiquityPressUrlNormalizer");
-	
+
+  protected String baseUrl;
+  
+  protected String journalId;
+  
   @Override
    public String normalizeUrl(String url,ArchivalUnit au)
       throws PluginException {
-    String baseUrl = au.getConfiguration().get(ConfigParamDescr.BASE_URL
-                                          .getKey());
-    String journalCode = au.getConfiguration().get(ConfigParamDescr.JOURNAL_ID
-                                              .getKey());
-    String normalizedUrl = normalizeUrl(url,baseUrl,journalCode) ;
-    return normalizedUrl ;
+    if (this.baseUrl == null) {
+      this.baseUrl = au.getConfiguration().get(ConfigParamDescr.BASE_URL.getKey());
+      this.journalId = au.getConfiguration().get(ConfigParamDescr.JOURNAL_ID.getKey());
+    }
+    return doNormalize(url, baseUrl, journalId) ;
   }
   
   /**Defines the protected normalize method  which will normalize the url into 
@@ -87,29 +58,23 @@ public class UbiquityPressUrlNormalizer implements UrlNormalizer {
    * 
    * @param url
    * @param baseUrl
-   * @param journalCode
+   * @param journalId
    * @return normalized url with the appended index.php and journal code.
    */
-  protected String normalizeUrl(String url,String baseUrl, String journalCode) {
-    if (   StringUtil.isNullString(journalCode) 
+  protected String doNormalize(String url,
+                               String baseUrl,
+                               String journalId) {
+    if (   StringUtil.isNullString(journalId) 
         || StringUtil.isNullString(baseUrl)
         || !StringUtil.startsWithIgnoreCase(url, baseUrl)) {
-    	//logger.debug("returning orginal url: "+ url) ;
       return url;
     }
     
-     // for eg. url refers to http://www.presentpasts.info/article/view/pp.52/92 
-    // urlDetailInfo = article/view/pp.52/92 will be extracted from url
-    String urlDetailInfo = url.substring(baseUrl.length());
-    StringBuilder buffer = new StringBuilder();
-    buffer.append(baseUrl);
-    buffer.append("index.php");
-    buffer.append("/");
-    buffer.append(journalCode);
-    buffer.append("/");
-    buffer.append(urlDetailInfo);
-    String normalizedUrl = buffer.toString();
-    //logger.debug ("orginal url: " + url + " normalizedUrl: " + normalizedUrl) ;
-    return normalizedUrl ;
+    // e.g. url is "http://www.presentpasts.info/article/view/pp.52/92" 
+    // path is "article/view/pp.52/92"
+    return String.format("%sindex.php/%s/%s",
+                         baseUrl,
+                         journalId,
+                         url.substring(baseUrl.length()));
   }
 }
