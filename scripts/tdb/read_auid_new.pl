2b1f7b1fda584f91df8bd13789a8bf588cec33aa
mellen22 <maryellen@lockss.org>
2012-11-19 22:39:26 +0000

    add Copernicus
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@22598 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -632,6 +632,31 @@ while (my $line = <>) {
     }
     sleep(5);
                 
+  } elsif ($plugin eq "ClockssCopernicusPublicationsPlugin") {
+    $url = sprintf("%s%s/index.html", 
+      $param{base_url}, $param{volume_name});
+    $man_url = uri_unescape($url);
+    my $req = HTTP::Request->new(GET, $man_url);
+    my $resp = $ua->request($req);
+    if ($resp->is_success) {
+      my $man_contents = $resp->content;
+      if (defined($man_contents) && ($man_contents =~ m/$clockss_tag/)) {
+        if ($man_contents =~ m/farbe_auf_journalhintergrund.>\s*(.*)\s*<\/h1>/si) {
+          $vol_title = $1;
+          $vol_title =~ s/\s*\n\s*/ /g;
+          if (($vol_title =~ m/</) || ($vol_title =~ m/>/)) {
+            $vol_title = "\"" . $vol_title . "\"";
+          }
+        } 
+        $result = "Manifest"
+      } else {
+        $result = "--NO_TAG--"
+      }
+    } else {
+      $result = "--REQ_FAIL--"
+    }
+    sleep(5);
+                
   } 
   if ($result eq "Plugin Unknown") {
     printf("*PLUGIN UNKNOWN* %s, %s, %s, %s\n",$result,$vol_title,$auid,$man_url);
