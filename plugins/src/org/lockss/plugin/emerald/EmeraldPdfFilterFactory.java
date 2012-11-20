6aa82393ec2543d5ae35211f8f9b760c86588c96
thibgc <github@thib.org>
2012-11-20 01:01:24 +0000

    Removing old-style PDF filter.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@22614 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/emerald/EmeraldPdfFilterFactory.java
+++ plugins/src/org/lockss/plugin/emerald/EmeraldPdfFilterFactory.java
@@ -1,6 +1,10 @@
 /*
+ * $Id: EmeraldPdfFilterFactory.java,v 1.3 2012-11-20 01:01:24 thib_gc Exp $
+ */
 
-Copyright (c) 2000-2007 Board of Trustees of Leland Stanford Jr. University,
+/*
+
+Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -28,33 +32,149 @@ in this Software without prior written authorization from Stanford University.
 
 package org.lockss.plugin.emerald;
 
-import java.io.InputStream;
+import java.io.*;
 
-import org.lockss.daemon.PluginException;
-import org.lockss.filter.pdf.OutputDocumentTransform;
-import org.lockss.plugin.ArchivalUnit;
-import org.lockss.plugin.FilterFactory;
+import org.lockss.filter.pdf.*;
+import org.lockss.pdf.*;
+import org.lockss.plugin.*;
 import org.lockss.util.Logger;
-import org.lockss.util.PdfUtil;
 
-@Deprecated
-public class EmeraldPdfFilterFactory implements FilterFactory {
-	static Logger log = Logger.getLogger("EmeraldPdfFilterFactory");
-	
-	public InputStream createFilteredInputStream(ArchivalUnit au,
-									             InputStream in,
-									             String encoding)
-									            		 throws PluginException {
-	    try {
-	      log.debug2("PDF filter factory for: " + au.getName());
-	      OutputDocumentTransform documentTransform = new EmeraldPdfTransform(au);
-	      in = PdfUtil.applyFromInputStream(new NormalizeMetadata(), in);
-	      return PdfUtil.applyFromInputStream(documentTransform, in);
-	    }
-	    catch (Exception exc) {
-	      log.error("Exception in PDF transform; unfiltered", exc);
-	      return in;
-	    }
-	}
+/**
+ * <p>
+ * A new-style PDF filter factory for Emerald Group Publishing
+ * content. 
+ * </p>
+ * <p>
+ * When synthetic front pages were introduced, the major PDF version
+ * of most documents was upped and most document-level metadata was
+ * stripped. The former makes it necessary to use a scraping filter
+ * instead of the older PDF-to-PDF filter.
+ * </p>
+ */
+public class EmeraldPdfFilterFactory extends ExtractingPdfFilterFactory {
+
+  protected static final Logger logger = Logger.getLogger(EmeraldPdfFilterFactory.class); 
+  
+  public static class FrontPageWorker extends PdfTokenStreamWorker {
+
+    protected boolean result;
+    
+    private int state;
+    
+    @Override
+    public void operatorCallback() throws PdfException {
+      switch (state) {
+
+        case 0: case 3: {
+          if (PdfOpcodes.SAVE_GRAPHICS_STATE.equals(getOpcode())) { ++state; }
+          else { stop(); }
+        } break;
+        
+        case 1: case 4: {
+          if (PdfOpcodes.RESTORE_GRAPHICS_STATE.equals(getOpcode())) { stop(); }
+          else if (PdfOpcodes.INVOKE_XOBJECT.equals(getOpcode())) { ++state; }
+        } break;
+        
+        case 2: case 5: {
+          if (PdfOpcodes.RESTORE_GRAPHICS_STATE.equals(getOpcode())) { ++state; }
+        } break;
+        
+        // case 3: see case 0
+        
+        // case 4: see case 1
+        
+        // case 5: see case 2
+        
+        case 6: {
+          if (PdfOpcodes.BEGIN_TEXT_OBJECT.equals(getOpcode())) { ++state; }
+        } break;
+        
+        case 7: {
+          if (PdfOpcodes.END_TEXT_OBJECT.equals(getOpcode())) { state = 6; }
+          if (   PdfOpcodes.SHOW_TEXT.equals(getOpcode())
+              && getTokens().get(getIndex() - 1).getString().startsWith("Downloaded on: ")) { ++state; }
+        } break;
+        
+        case 8: {
+          if (PdfOpcodes.END_TEXT_OBJECT.equals(getOpcode())) { ++state; }
+        } break;
+
+        case 9: {
+          if (PdfOpcodes.BEGIN_TEXT_OBJECT.equals(getOpcode())) { ++state; }
+        } break;
+        
+        case 10: {
+          if (PdfOpcodes.END_TEXT_OBJECT.equals(getOpcode())) { state = 9; }
+          if (   PdfOpcodes.SHOW_TEXT.equals(getOpcode())
+              && getTokens().get(getIndex() - 1).getString().startsWith("Access to this document was granted through an Emerald subscription provided by ")) { ++state; }
+        } break;
+        
+        case 11: {
+          if (PdfOpcodes.END_TEXT_OBJECT.equals(getOpcode())) {
+            result = true;
+            stop();
+          }
+        } break;
+
+        default: {
+          throw new PdfException("Invalid state in " + getClass().getName() + ": " + state);
+        }
+      }
+    }
+    
+    @Override
+    public void setUp() throws PdfException {
+      super.setUp();
+      this.state = 0;
+      this.result = false;
+    }
+    
+  }
+  
+  public EmeraldPdfFilterFactory() {
+    super();
+  }
+  
+  @Override
+  public PdfTransform<PdfDocument> getDocumentTransform(ArchivalUnit au,
+                                                        OutputStream os) {
+    // Override the document-level transform to skip all document-level strings
+    return new BaseDocumentExtractingTransform(os) {
+      @Override
+      public void transform(ArchivalUnit au, PdfDocument pdfDocument) throws PdfException {
+        this.au = au;
+        this.pdfDocument = pdfDocument;
+        for (PdfPage pdfPage : pdfDocument.getPages()) {
+          outputPage(pdfPage);
+        }
+      }
+      @Override
+      public void outputPage(PdfPage pdfPage) throws PdfException {
+        try {
+          super.outputPage(pdfPage);
+        } catch (java.lang.Error error) {
+          if (error.getMessage().startsWith("TIFFFaxDecoder")) {
+            // In PDFBox 1.6.0, TIFFFaxDecoder throws java.lang.Error
+            // in various unimplemented cases. I'm not kidding.
+            logger.debug("TIFFFaxDecoder threw java.lang.Error, skipping page", error);
+          }
+          else {
+            throw error;
+          }
+        }
+      }
+    };
+  }
+
+  @Override
+  public void transform(ArchivalUnit au,
+                        PdfDocument pdfDocument)
+      throws PdfException {
+    FrontPageWorker frontPageWorker = new FrontPageWorker();
+    frontPageWorker.process(pdfDocument.getPage(0).getPageTokenStream());
+    if (frontPageWorker.result) {
+      pdfDocument.removePage(0);
+    }
+  }
   
 }
