4e1942ba6443854c32f8a5dc1a96e38e68ed4f70
alexandraohlson <alexandra@slgroup.com>
2012-11-20 22:37:33 +0000

    remove 2nd argument in ReaderInputStream
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@22636 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/copernicus/CopernicusHtmlFilterFactory.java
+++ plugins/src/org/lockss/plugin/copernicus/CopernicusHtmlFilterFactory.java
@@ -1,5 +1,5 @@
 /*
-* $Id: CopernicusHtmlFilterFactory.java,v 1.2 2012-11-19 21:03:18 alexandraohlson Exp $
+* $Id: CopernicusHtmlFilterFactory.java,v 1.3 2012-11-20 22:37:33 alexandraohlson Exp $
 */
 
 /*
@@ -88,7 +88,7 @@ public class CopernicusHtmlFilterFactory implements FilterFactory {
 
     Reader reader = FilterUtil.getReader(htmlFilter, encoding);
     Reader filtReader = makeFilteredReader(reader);
-    return new ReaderInputStream(filtReader,encoding);
+    return new ReaderInputStream(filtReader);
   }
 
   static Reader makeFilteredReader(Reader reader) {
