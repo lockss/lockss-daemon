c9afc3bec2572f25300801edacb4953eff7b1440
pgust <claire@lockss.org>
2013-01-05 20:21:23 +0000

    Fixed code for emitting authors and keywords to output multiple values rather than all values in a semicolon separated string. Also fixed Dylan's weird formatting.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@24029 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/elsevier/ElsevierTocMetadataExtractorFactory.java
+++ plugins/src/org/lockss/plugin/elsevier/ElsevierTocMetadataExtractorFactory.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ElsevierTocMetadataExtractorFactory.java,v 1.8 2012-07-27 17:44:45 pgust Exp $
+ * $Id: ElsevierTocMetadataExtractorFactory.java,v 1.9 2013-01-05 20:21:23 pgust Exp $
  */
 
 /*
@@ -52,131 +52,131 @@ import org.lockss.plugin.*;
  *
  */
 public class ElsevierTocMetadataExtractorFactory
-  implements FileMetadataExtractorFactory {
+implements FileMetadataExtractorFactory {
   static Logger log = Logger.getLogger("ElsevierTocMetadataExtractorFactory");
-  
+
   public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
-							   String contentType)
-      throws PluginException {
+      String contentType)
+          throws PluginException {
     return new ElsevierTocMetadataExtractor();
   }
 
   public static class ElsevierTocMetadataExtractor 
-    implements FileMetadataExtractor {
-    
-	  private final int DOI_LEN = 6;
-	  
+  implements FileMetadataExtractor {
+
+    private final int DOI_LEN = 6;
+
     private final int ISSN_INDEX = 0;
     private final int DATE_INDEX = 5;
-	  private final int FILE_NAME_INDEX = 6;
-	  private final int DOI_INDEX = 7;
+    private final int FILE_NAME_INDEX = 6;
+    private final int DOI_INDEX = 7;
     private final int AUTHOR_INDEX = 10;
-	  private final int PAGE_INDEX = 13;
-	  private final int KEYWORD_INDEX = 12;
-	  
-	  private final int INVALID_TAG = -1;
-	  private final int REPEATED_TAG = -3;
-	  private final int ARTICLE_COMPLETE = -4;
+    private final int PAGE_INDEX = 13;
+    private final int KEYWORD_INDEX = 12;
 
-	  private List<String> articleTags = Arrays.asList(new String[]{
-	    "_t1",
-	    "_vl",
-		  "_jn", 
-		  "_cr", 
-		  "_is",
-		  "_dt",
-		  "_t3", 
-		  "_ii",
-		  "_la",
-		  "_ti",
-		  "_au",
-		  "_ab", 
-		  "_kw", 
-		  "_pg",
-      "_pg",
-	  });
-	  
-	  private final String END_ARTICLE_METADATA = "_mf";
-	  
-	  private String[] articleValues = new String[articleTags.size()];
-	  
-	  private MetadataField[] metadataFields = {
-	    MetadataField.FIELD_ISSN, 
+    private final int INVALID_TAG = -1;
+    private final int REPEATED_TAG = -3;
+    private final int ARTICLE_COMPLETE = -4;
+
+    private List<String> articleTags = Arrays.asList(new String[]{
+        "_t1",
+        "_vl",
+        "_jn", 
+        "_cr", 
+        "_is",
+        "_dt",
+        "_t3", 
+        "_ii",
+        "_la",
+        "_ti",
+        "_au",
+        "_ab", 
+        "_kw", 
+        "_pg",
+        "_pg",
+    });
+
+    private final String END_ARTICLE_METADATA = "_mf";
+
+    private String[] articleValues = new String[articleTags.size()];
+
+    private MetadataField[] metadataFields = {
+        MetadataField.FIELD_ISSN, 
         MetadataField.FIELD_VOLUME, 
-  		MetadataField.FIELD_JOURNAL_TITLE, 
-  		MetadataField.DC_FIELD_RIGHTS,
-  		MetadataField.FIELD_ISSUE,
-  	        MetadataField.FIELD_DATE,
-  		MetadataField.FIELD_ACCESS_URL, 
-  		MetadataField.FIELD_DOI, 
-  		MetadataField.DC_FIELD_LANGUAGE,
-  		MetadataField.FIELD_ARTICLE_TITLE, 
-  		MetadataField.FIELD_AUTHOR, 
-  		MetadataField.DC_FIELD_DESCRIPTION,
-  		MetadataField.FIELD_KEYWORDS, 
-  		MetadataField.FIELD_START_PAGE,
-      MetadataField.FIELD_END_PAGE,
-	  };
-	 
-	  private int lastTag = INVALID_TAG;
-	  private String base_url, year;
-	  
+        MetadataField.FIELD_JOURNAL_TITLE, 
+        MetadataField.DC_FIELD_RIGHTS,
+        MetadataField.FIELD_ISSUE,
+        MetadataField.FIELD_DATE,
+        MetadataField.FIELD_ACCESS_URL, 
+        MetadataField.FIELD_DOI, 
+        MetadataField.DC_FIELD_LANGUAGE,
+        MetadataField.FIELD_ARTICLE_TITLE, 
+        MetadataField.FIELD_AUTHOR, 
+        MetadataField.DC_FIELD_DESCRIPTION,
+        MetadataField.FIELD_KEYWORDS, 
+        MetadataField.FIELD_START_PAGE,
+        MetadataField.FIELD_END_PAGE,
+    };
+
+    private int lastTag = INVALID_TAG;
+    private String base_url, year;
+
     /**
      * Use SimpleHtmlMetaTagMetadataExtractor to extract raw metadata, map
      * to cooked fields, then extract extra tags by reading the file.
      */
-		@Override
-		public void extract(MetadataTarget target, CachedUrl cu, Emitter emitter)
-				throws IOException {
-			CachedUrl metadata = cu.getArchivalUnit().makeCachedUrl(getToc(cu.getUrl()));
-			
-			if(metadata.getUrl().equals(cu.getUrl())) {
-				metadata = cu;
-			}
-			if (metadata == null || !metadata.hasContent()) {
-				log.error("The metadata file does not exist or is not readable.");
-				return;
-			}
-			
-			base_url = cu.getArchivalUnit().getConfiguration().get("base_url");
-			year = cu.getArchivalUnit().getConfiguration().get("year");
+    @Override
+    public void extract(MetadataTarget target, CachedUrl cu, Emitter emitter)
+        throws IOException {
+      CachedUrl metadata = cu.getArchivalUnit().makeCachedUrl(getToc(cu.getUrl()));
 
-			BufferedReader bReader = new BufferedReader(metadata.openForReading());
-			try {
-				for (String line = bReader.readLine(); line != null; line = bReader.readLine()) { 
-					line = line.trim();
+      if (metadata.getUrl().equals(cu.getUrl())) {
+        metadata = cu;
+      }
+      if (metadata == null || !metadata.hasContent()) {
+        log.error("The metadata file does not exist or is not readable.");
+        return;
+      }
 
-					if (extractFrom(line) && articleValues[FILE_NAME_INDEX] != null) {
-						CachedUrl container = cu.getArchivalUnit().makeCachedUrl(getUrlFrom(articleValues[FILE_NAME_INDEX]));
-						log.debug3("Emitting metadata for url: " + container.getUrl());
-						articleValues[FILE_NAME_INDEX] = container.getUrl();
-						
-						ArticleMetadata am = new ArticleMetadata();
-						putMetadataIn(am);
-						emitter.emitMetadata(container, am);
-						
-						clear(articleValues);
-					}
-				}
-			} finally {
-				IOUtil.safeClose(bReader);
-			}
-		}
-    
-		private String getIssnFrom(String line) {
-      if(line.length() < 4) {
+      base_url = cu.getArchivalUnit().getConfiguration().get("base_url");
+      year = cu.getArchivalUnit().getConfiguration().get("year");
+
+      BufferedReader bReader = new BufferedReader(metadata.openForReading());
+      try {
+        for (String line = bReader.readLine(); line != null; line = bReader.readLine()) { 
+          line = line.trim();
+
+          if (extractFrom(line) && articleValues[FILE_NAME_INDEX] != null) {
+            CachedUrl container = cu.getArchivalUnit().makeCachedUrl(getUrlFrom(articleValues[FILE_NAME_INDEX]));
+            log.debug3("Emitting metadata for url: " + container.getUrl());
+            articleValues[FILE_NAME_INDEX] = container.getUrl();
+
+            ArticleMetadata am = new ArticleMetadata();
+            putMetadataIn(am);
+            emitter.emitMetadata(container, am);
+
+            clear(articleValues);
+          }
+        }
+      } finally {
+        IOUtil.safeClose(bReader);
+      }
+    }
+
+    private String getIssnFrom(String line) {
+      if (line.length() < 4) {
         return "";
       }
-		  int i = line.lastIndexOf(' ');
-		  if (i > 0) {
-		    return MetadataUtil.formatIssn(line.substring(i+1));
-		  }
-		  return line;
-		}
-		
+      int i = line.lastIndexOf(' ');
+      if (i > 0) {
+        return MetadataUtil.formatIssn(line.substring(i+1));
+      }
+      return line;
+    }
+
     private String getStartPageFrom(String line) {
       // skip past "_pg " prefix
-      if(line.length() < 4) {
+      if (line.length() < 4) {
         return "";
       }
       int i = line.indexOf(' ');
@@ -191,7 +191,7 @@ public class ElsevierTocMetadataExtractorFactory
     }
 
     private String getEndPageFrom(String line) {
-      if(line.length() < 4) {
+      if (line.length() < 4) {
         return "";
       }
       int i = line.indexOf(' ');
@@ -206,14 +206,14 @@ public class ElsevierTocMetadataExtractorFactory
       return line;
     }
 
-    
+
     Pattern datepat = Pattern.compile("([\\d]{4})([\\d]{2})?([\\d]{2})?");
 
     private String getDateFrom(String line) {
-      if(line.length() < 4) {
+      if (line.length() < 4) {
         return "";
       }
-      
+
       int i = line.indexOf(' ');
       if (i >= 3) {
         Matcher m = datepat.matcher(line.substring(i+1));
@@ -224,36 +224,36 @@ public class ElsevierTocMetadataExtractorFactory
             if (m.group(3) != null) {
               date += "-" + m.group(3);
             }
-         }
+          }
           return date;
         }
       }
       return line;
     }
-    
+
     private String getDoiFrom(String line)
     {
-    	if(line.contains("[DOI] ")) {
-    		return line.substring(line.indexOf("[DOI] ")+DOI_LEN);
-    	}
-    	return line;
+      if (line.contains("[DOI] ")) {
+        return line.substring(line.indexOf("[DOI] ")+DOI_LEN);
+      }
+      return line;
     }
-    
+
     private void clear(String[] values)
     {
-    	for(int i = FILE_NAME_INDEX; i < values.length; ++i) {
-    		values[i] = null;
-    	}
+      for(int i = FILE_NAME_INDEX; i < values.length; ++i) {
+        values[i] = null;
+      }
     }
-    
+
     private String getMetadataFrom(String line)
     {
-    	if(line.length() < 4) {
-    		return "";
-    	}
-    	return line.substring(4); //substring after '_xx' tag
+      if (line.length() < 4) {
+        return "";
+      }
+      return line.substring(4); //substring after '_xx' tag
     }
-    
+
     /**
      * Returns the index of the current line's tag in the tag List or
      * some other value if the current tag is special in some way
@@ -262,60 +262,60 @@ public class ElsevierTocMetadataExtractorFactory
      */
     private int getTagIndex(String line)
     {
-    	if(line.indexOf("_") < 0) {
-    		return REPEATED_TAG;
-    	}
-    	String tag = line.substring(0,3);
-    	
-    	if(tag.equals(END_ARTICLE_METADATA)) {
-    		return ARTICLE_COMPLETE;
-    	} else if(articleTags.contains(tag)) {
-    		return articleTags.indexOf(tag);
-    	} else {
-    		return INVALID_TAG;
-    	}
+      if (line.indexOf("_") < 0) {
+        return REPEATED_TAG;
+      }
+      String tag = line.substring(0,3);
+
+      if (tag.equals(END_ARTICLE_METADATA)) {
+        return ARTICLE_COMPLETE;
+      } else if (articleTags.contains(tag)) {
+        return articleTags.indexOf(tag);
+      } else {
+        return INVALID_TAG;
+      }
     }
-    
+
     private boolean extractFrom(String line)
     {
-    	int tag = getTagIndex(line);
-    	
-        if(tag == ARTICLE_COMPLETE) {
-          return true;
+      int tag = getTagIndex(line);
+
+      if (tag == ARTICLE_COMPLETE) {
+        return true;
+      }
+      if (tag == INVALID_TAG) {
+        lastTag = tag;
+      } else if (tag == REPEATED_TAG) {
+        if (lastTag != INVALID_TAG) {
+          articleValues[lastTag] += " "+line;
         }
-    	if(tag == INVALID_TAG) {
-    		lastTag = tag;
-    	} else if(tag == REPEATED_TAG) {
-    		if (lastTag != INVALID_TAG) {
-    	          articleValues[lastTag] += " "+line;
-    		}
-    	} else {
-    		if(tag == AUTHOR_INDEX || tag == KEYWORD_INDEX) {
-    			if(articleValues[tag] == null) {
-    				articleValues[tag] = getMetadataFrom(line);
-    			} else {
-    				articleValues[tag] += "; "+getMetadataFrom(line);
-    			}
-    		} else if(tag == DOI_INDEX) {
-    			articleValues[tag] = getDoiFrom(line);
-    		} else if(tag == ISSN_INDEX) {
+      } else {
+        if (tag == AUTHOR_INDEX || tag == KEYWORD_INDEX) {
+          if (articleValues[tag] == null) {
+            articleValues[tag] = getMetadataFrom(line);
+          } else {
+            articleValues[tag] += "; "+getMetadataFrom(line);
+          }
+        } else if (tag == DOI_INDEX) {
+          articleValues[tag] = getDoiFrom(line);
+        } else if (tag == ISSN_INDEX) {
           articleValues[tag] = getIssnFrom(line);
-        } else if(tag == PAGE_INDEX) {
+        } else if (tag == PAGE_INDEX) {
           articleValues[tag] = getStartPageFrom(line);
           articleValues[tag+1] = getEndPageFrom(line);
         } else if (tag == DATE_INDEX) {
           articleValues[tag] = getDateFrom(line);
         }
-    		else {
-    			articleValues[tag] = getMetadataFrom(line);
-    		}
-    		
-    		lastTag = tag;
-    	}
-    		
-    	return false;
+        else {
+          articleValues[tag] = getMetadataFrom(line);
+        }
+
+        lastTag = tag;
+      }
+
+      return false;
     }
-    
+
     /**
      * Stores the gathered MetadataField values in the ArticleMetadata
      * so it can be emitted
@@ -323,14 +323,24 @@ public class ElsevierTocMetadataExtractorFactory
      */
     private void putMetadataIn(ArticleMetadata am)
     {   
-        for(int i = 0; i < articleTags.size(); ++i) {
-        	am.put(metadataFields[i],articleValues[i]);
-        	if (log.isDebug3()) {
-        	  log.debug(metadataFields[i].getKey() + ": " +  articleValues[i]);
-        	}
+      for (int i = 0; i < articleTags.size(); ++i) {
+        if (articleValues[i] != null) {
+          if ((i == AUTHOR_INDEX) || (i == KEYWORD_INDEX)) {
+            // store individual values of a multi-valued field
+            String[] values = articleValues[i].split(";");
+            for (String value : values) {
+              am.put(metadataFields[i], value.trim());
+            }
+          } else {
+            am.put(metadataFields[i],articleValues[i]);
+          }
+          if (log.isDebug3()) {
+            log.debug(metadataFields[i].getKey() + ": " +  articleValues[i]);
+          }
         }
+      }
     }
-    
+
     /**
      * Uses a url of an article to construct its metadata file's url
      * @param url - address of an article file
@@ -338,16 +348,16 @@ public class ElsevierTocMetadataExtractorFactory
      */
     protected String getToc(String url)
     {
-        Pattern pattern = Pattern.compile("(.*/[^/]+/)[\\d]+[^/]+/[\\d]+/[\\d]+/main.pdf$",Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(url);
-        return matcher.replaceFirst("$1dataset.toc");
+      Pattern pattern = Pattern.compile("(.*/[^/]+/)[\\d]+[^/]+/[\\d]+/[\\d]+/main.pdf$",Pattern.CASE_INSENSITIVE);
+      Matcher matcher = pattern.matcher(url);
+      return matcher.replaceFirst("$1dataset.toc");
     }
-    
+
     protected String getUrlFrom(String identifier)
     {
-    	Pattern pattern = Pattern.compile("([^/]+)( )([^/]+)( )([^/]+)( )([^/]+)");
-        Matcher matcher = pattern.matcher(identifier);
-        return matcher.replaceFirst(base_url+year+"/$1/$3.tar!/$5/$7/main.pdf");
+      Pattern pattern = Pattern.compile("([^/]+)( )([^/]+)( )([^/]+)( )([^/]+)");
+      Matcher matcher = pattern.matcher(identifier);
+      return matcher.replaceFirst(base_url+year+"/$1/$3.tar!/$5/$7/main.pdf");
     }
   }
 }
