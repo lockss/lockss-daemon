1afad8413ab1aca5da3936fa19797e4da4fc5941
aishizaki <aishizaki@lockss.org>
2013-01-30 00:02:02 +0000

    added IUMJHtmlHashFilterFactory.java to filter out a timestamp in the footer updated IUMJPlugin to use the above updated ClockssIUMJPlugin.xml to be a child plugin of IUMJPlugin.xml added TestIUMJHtmlHashFilterFactory.java
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@24819 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/iumj/IUMJHtmlHashFilterFactory.java
@@ -0,0 +1,73 @@
+/* $Id: IUMJHtmlHashFilterFactory.java,v 1.1 2013-01-30 00:02:02 aishizaki Exp $ */
+/*
+
+Copyright (c) 2000-2011 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, tMassachusettsMedicalSocietyHtmlFilterFactoryhe name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.iumj;
+
+import java.io.*;
+
+import org.htmlparser.*;
+import org.htmlparser.filters.*;
+import org.lockss.daemon.PluginException;
+import org.lockss.filter.html.*;
+import org.lockss.plugin.*;
+import org.lockss.util.Logger;
+
+public class IUMJHtmlHashFilterFactory implements FilterFactory {
+
+  Logger log = Logger.getLogger("IUMJHtmlFilterFactory");
+        
+  public static class FilteringException extends PluginException {
+    public FilteringException() { super(); }
+    public FilteringException(String msg, Throwable cause) { super(msg, cause); }
+    public FilteringException(String msg) { super(msg); }
+    public FilteringException(Throwable cause) { super(cause); }
+  }
+  
+  @Override
+  public InputStream createFilteredInputStream(ArchivalUnit au,
+                                               InputStream in,
+                                               String encoding)
+                                               throws PluginException {
+          
+    NodeFilter[] filters = new NodeFilter[] {
+      // filters out the footer tag with a changing date/time    
+      HtmlNodeFilters.tagWithAttribute("div", "id", "footer")
+    };
+    
+    OrFilter oFilter = new OrFilter(filters);
+    // filters out the footer tag with a changing date/time   
+    HtmlTransform transform = 
+      HtmlNodeFilterTransform.exclude(oFilter);
+    
+    return new HtmlFilterInputStream(in,
+                                     encoding,
+                                     transform);
+  }
+  
+}
---
+++ plugins/test/src/org/lockss/plugin/iumj/TestIUMJHtmlHashFilterFactory.java
@@ -0,0 +1,100 @@
+/*
+ * $Id: TestIUMJHtmlHashFilterFactory.java,v 1.1 2013-01-30 00:02:02 aishizaki Exp $
+ */
+
+/*
+
+ Copyright (c) 2000-2006 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.iumj;
+
+import java.io.*;
+
+import org.lockss.util.*;
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.MetadataField;
+import org.lockss.test.*;
+
+public class TestIUMJHtmlHashFilterFactory extends LockssTestCase {
+  static String ENC = Constants.DEFAULT_ENCODING;
+
+  private IUMJHtmlHashFilterFactory fact;
+  private MockArchivalUnit mau;
+
+  public void setUp() throws Exception {
+    super.setUp();
+    fact = new IUMJHtmlHashFilterFactory();
+    mau = new MockArchivalUnit();
+  }
+ 
+  // timestampHtmlHashA should be filtered to become timestampHashAFiltered
+  private static final String timestampHtmlHashA =
+    "<div id=\"container\">" +
+    "<div id=\"footer\">" +
+    "<p>Hello World Journal - 2013-01-23 18:57:08</p>" +
+    "</div>" +
+    "</div>";
+ 
+  private static final String timestampHtmlHashAFiltered =
+    "<div id=\"container\"></div>";
+  
+  // timestampHtmlHashB should be filtered same as 'A'
+  private static final String timestampHtmlHashB =
+    "<div id=\"container\">" +
+    "<div id=\"footer\">" +
+    "<p>Hello World - 2013-01-23 18:57:08</p>" +
+    "</div>" +
+    "</div>";
+  
+  // timestampHtmlHashC should not get filtered at all (stays the same)
+  private static final String timestampHtmlHashC =
+    "<div id=\"container\">" +
+    "<div id=\"header\">" +
+    "<p>Hello World Journal - 2013-01-23 18:57:08</p>" +
+    "</div>" +
+    "</div>";
+ 
+  public void testFiltering() throws Exception {
+    InputStream inA, inB, inC;
+
+    /* timestamp test */
+    inA = fact.createFilteredInputStream(mau, 
+          new StringInputStream(timestampHtmlHashA), ENC);
+    String filtStrA = StringUtil.fromInputStream(inA);
+    assertEquals(timestampHtmlHashAFiltered, filtStrA);
+    
+    inB = fact.createFilteredInputStream(mau, 
+        new StringInputStream(timestampHtmlHashB), ENC);
+    String filtStrB = StringUtil.fromInputStream(inB);
+    assertNotEquals(timestampHtmlHashB, filtStrB);
+
+    inC = fact.createFilteredInputStream(mau, 
+        new StringInputStream(timestampHtmlHashC), ENC);
+    String filtStrC = StringUtil.fromInputStream(inC);
+    assertEquals(timestampHtmlHashC, filtStrC);    
+  }
+}
