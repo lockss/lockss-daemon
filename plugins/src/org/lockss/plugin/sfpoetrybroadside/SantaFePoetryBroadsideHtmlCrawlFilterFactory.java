7470e2f6cad46c795b60400fa6adc682ab45188d
alexandraohlson <alexandra@slgroup.com>
2013-02-20 01:59:51 +0000

    Crawl filter for Santa Fe to keep from going to other issues/volumes off the bio pages.  Also a test to check the crawl filter. No bump to the version number because this will piggyback off the last checkin which hasn't been pushed yet.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@25352 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/sfpoetrybroadside/SantaFePoetryBroadsideHtmlCrawlFilterFactory.java
@@ -0,0 +1,98 @@
+/*
+ * $Id: SantaFePoetryBroadsideHtmlCrawlFilterFactory.java,v 1.1 2013-02-20 01:59:51 alexandraohlson Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.sfpoetrybroadside;
+
+import java.io.InputStream;
+
+import org.htmlparser.Node;
+import org.htmlparser.NodeFilter;
+import org.htmlparser.Tag;
+import org.htmlparser.filters.*;
+import org.htmlparser.nodes.TextNode;
+import org.htmlparser.tags.BodyTag;
+import org.htmlparser.tags.CompositeTag;
+import org.htmlparser.tags.Div;
+import org.htmlparser.tags.HeadTag;
+import org.htmlparser.tags.HeadingTag;
+import org.htmlparser.tags.LinkTag;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.SimpleNodeIterator;
+import org.lockss.daemon.PluginException;
+import org.lockss.filter.html.*;
+import org.lockss.plugin.*;
+
+public class SantaFePoetryBroadsideHtmlCrawlFilterFactory implements FilterFactory {
+
+  public InputStream createFilteredInputStream(ArchivalUnit au,
+                                               InputStream in,
+                                               String encoding)
+      throws PluginException {
+    NodeFilter[] filters = new NodeFilter[] {
+        /*
+         * In early issues (up to #35) no clear div for bio chunks with links
+         * but all bio pages (except for issue2 which has no outside issue links) have
+         * the same page title so crawl filter out the body section of bio pages
+         */
+        new NodeFilter() {
+          @Override public boolean accept(Node node) {
+            if (!(node instanceof BodyTag)) return false;
+            Node prevNode = node.getPreviousSibling();
+            while (prevNode != null && !(prevNode instanceof HeadTag)) {
+              prevNode = prevNode.getPreviousSibling();
+            }
+            if (prevNode != null && prevNode instanceof HeadTag) {
+              for (SimpleNodeIterator iter = ((CompositeTag)prevNode).elements() ; iter.hasMoreNodes() ; ) {
+                Node n = iter.nextNode();
+                if (n instanceof TextNode) { continue; }
+                String tagName = ((Tag)n).getTagName().toLowerCase();
+                if ( ("title".equals(tagName))  && (((Tag)n).toPlainTextString()).contains("Bionotes")) {
+                  return true; 
+                }
+              }
+            }
+            return false;
+          }
+        },
+        /*
+         * issue #35 and later there is no <html> start tag on pages so the DOM parsing no longer works, 
+         * but the bio pages begin to use a <div class="bio2"> structure around bio page links and we can 
+         * remove those. Do it in this order so we don't waste time if we're just ripping out the whole body section
+         */
+        HtmlNodeFilters.tagWithAttribute("div", "class", "bio2"),
+    };
+    return new HtmlFilterInputStream(in,
+        encoding,
+        HtmlNodeFilterTransform.exclude(new OrFilter(filters)));
+  }
+
+}
---
+++ plugins/test/src/org/lockss/plugin/sfpoetrybroadside/TestSantaFePoetryBroadsideHtmlCrawlFilterFactory.java
@@ -0,0 +1,197 @@
+/*
+ * $Id: TestSantaFePoetryBroadsideHtmlCrawlFilterFactory.java,v 1.1 2013-02-20 01:59:51 alexandraohlson Exp $
+ */
+package org.lockss.plugin.sfpoetrybroadside;
+
+import java.io.*;
+import org.lockss.util.*;
+import org.lockss.test.*;
+
+public class TestSantaFePoetryBroadsideHtmlCrawlFilterFactory extends LockssTestCase {
+  static String ENC = Constants.DEFAULT_ENCODING;
+
+  private SantaFePoetryBroadsideHtmlCrawlFilterFactory fact;
+  private MockArchivalUnit mau;
+
+  public void setUp() throws Exception {
+    super.setUp();
+    fact = new SantaFePoetryBroadsideHtmlCrawlFilterFactory();
+    mau = new MockArchivalUnit();
+  }
+
+  private static final String bioPage = 
+      "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" " +
+       "   \"http://www.w3.org/TR/REC-html40/loose.dtd\"> " +                                                                                                                     
+    "<html>" +                                                                                                                                                                   
+    "<head>" +                                                                                                                                                                   
+    "<title>Santa Fe Poetry Broadside... Bionotes, Issue #1</title>" +                                                                                                           
+    "</head>" +                                                                                                                                                                  
+    "<body bgcolor=\"ffffff\">" +                                                                                                                                                
+    "<a href=\"broadside.html\" title=\"Index of all issues\"><img alt=\"Santa Fe Poetry Broadside\"" +                                                                          
+    "src=\"smbanner.gif\"></a> " +                                                                                                                                               
+    "<center>" +                                                                                                                                                                 
+    "<h2>About the Poets</h2>" +                                                                                                                                                 
+    "</center>" +                                                                                                                                                                
+    "<dl>" +                                                                                                                                                                     
+    "<dt>" +                                                                                                                                                                     
+    "<a name=\"chavezc\"><b>Margo Chavez-Charles</b></a>" +                                                                                                                      
+    "<dd>Sam Q Author teaches" +                                                                                                                                                 
+    "<dd><small><b>poems in the Broadside, this issue...</b><a href=\"author.html\"><i>BOO</i></a></small>" +                                                                    
+    "<dd><small><b>poem in the Broadside, issue #36... </b><a href=\"author3.html\"><i>BOO2</i></a></small>" +                                                                   
+    "<p>" +                                                                                                                                                                      
+    "<dt>" +                                                                                                                                                                     
+    "<br>" +                                                                                                                                                                     
+    "</dl>" +                                                                                                                                                                    
+    "</body>" +                                                                                                                                                                  
+    "</html>";           
+
+  private static final String bioPageFiltered = 
+      "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" " +
+          "   \"http://www.w3.org/TR/REC-html40/loose.dtd\"> " +                                                                                                                     
+       "<html>" +                                                                                                                                                                   
+       "<head>" +                                                                                                                                                                   
+       "<title>Santa Fe Poetry Broadside... Bionotes, Issue #1</title>" +                                                                                                           
+       "</head>" + 
+       "</html>";           
+  
+  private static final String notBioPage = 
+      "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" " +
+          "     \"http://www.w3.org/TR/REC-html4/loose.dtd\">  " +
+          "<html>" +
+          "<head>  " +
+          "<title>Santa Fe Poetry Broadside... Poets of the Region and Beyond</title>  " +
+          "<meta http-equiv=\"content-type\" content=\"text/html; charset=iso-8859-1\">  " +
+          "<meta name=\"Description\" content=\"Santa Fe Poetry Broadside... Poets of the Region and Beyond\">  " +
+          "<link rel=\"stylesheet\" href=\"broadside.css\" type=\"text/css\">  " +
+          "</head> " +
+          "<body>  " +
+          "<div id=\"header\">  " +
+          "<img alt=\"Santa Fe Poetry Broadside... Poets of the Region and Beyond\"  " +
+          "src=\"banner.gif\"  width=\"681\" height=\"112\" />  " +
+          "<div id=\"navmenu\">  " +
+          "<ul>  " +
+          "<li><a href=\"broadside.html\" title=\"Index of all issues\">All Issues</a></li>  " +
+          "<li><a href=\"about.html\">About the <em>Broadside</em></a></li>  " +
+          "<li><a href=\"bio46.html\">Bionotes</a></li> <li><a href=\"contact.html\">How to reach us</a></li>  " +
+          "<li><a href=\"links.html\">Links</a></li> <li><a href=\"about.html#copy\">Copyright</a></li>  " +
+          "</ul>  " +
+          "</div>  " +
+          "<hr width=\"70%\">   " +
+          "Issue #46, June, 2006 :  " +
+          "</div> " +
+          "</body> " +
+          "</html>  ";
+  
+  private static final String bioPageNoHtml =
+      "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"" +
+          "      \"http://www.w3.org/TR/REC-html4/loose.dtd\">" +
+          "<head>" +
+          "<title>Santa Fe Poetry Broadside... Bionotes</title>" +
+          "<meta http-equiv=\"content-type\" content=\"text/html; charset=iso-8859-1\">" +
+          "<meta name=\"Description\" content=\"Santa Fe Poetry Broadside... Poets of the Region and Beyond\">  " +
+          "<link rel=\"stylesheet\" href=\"broadside.css\" type=\"text/css\">" +
+          "</head>" +
+          "<body>" +
+          "<div id=\"header\">" +
+          "<hr width=\"70%\">" +
+          "<h4>" +                                                                                                                                                                     
+          "Santa Fe Poetry Broadside... Issue #47, August, 2006 :</h4>" +
+          "</div>" +
+          "" +
+          "<a name=\"logghe\"></a><br />" +
+          "<span class=\"author2\">Joan Logghe</span><br />" +
+          "<div class=\"indent2\">" +
+          "Joan Logghe of Espanola is a master performer and teacher of poetry. She has many books " +
+          "and publications to her credit, including <span class=\"italic\">Blessed Resistance</span>, and " +
+          "her latest, <span class=\"italic\">Rice</span> from Tres Chicas Books." +
+          "<div class=\"bio2\">" +
+          "Poem in the Broadside, this issue... <span class=\"italic\"><a href=\"whatis.html\">What Is</a></span><br />" +
+          "Poems in the Broadside, issue #17: <a href=\"sept00.html\">After Horses</a> (twelve poems)<br />" +
+          "Poems in the Broadside, issue #1... <a href=\"logghe1.html\"><i>from \"The Rice Sonnets\"</i></a>" +
+          "-- <a href=\"logghe2.html\"><i>Velvet</i></a>" +
+          "</div>" +
+          "</div>" +
+          "</body>" +
+          "</html>";
+
+  
+  private static final String bioPageNoHtmlFiltered =
+      "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"" +
+          "      \"http://www.w3.org/TR/REC-html4/loose.dtd\">" +
+          "<head>" +
+          "<title>Santa Fe Poetry Broadside... Bionotes</title>" +
+          "<meta http-equiv=\"content-type\" content=\"text/html; charset=iso-8859-1\">" +
+          "<meta name=\"Description\" content=\"Santa Fe Poetry Broadside... Poets of the Region and Beyond\">  " +
+          "<link rel=\"stylesheet\" href=\"broadside.css\" type=\"text/css\">" +
+          "</head>" +
+          "<body>" +
+          "<div id=\"header\">" +
+          "<hr width=\"70%\">" +
+          "<h4>" +                                                                                                                                                                     
+          "Santa Fe Poetry Broadside... Issue #47, August, 2006 :</h4>" +
+          "</div>" +
+          "" +
+          "<a name=\"logghe\"></a><br />" +
+          "<span class=\"author2\">Joan Logghe</span><br />" +
+          "<div class=\"indent2\">" +
+          "Joan Logghe of Espanola is a master performer and teacher of poetry. She has many books " +
+          "and publications to her credit, including <span class=\"italic\">Blessed Resistance</span>, and " +
+          "her latest, <span class=\"italic\">Rice</span> from Tres Chicas Books." +
+          "</div>" +
+          "</body>" +
+          "</html>";
+
+ 
+  
+  private static final String notBioPageNoHtml =
+      "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" " +
+          "     \"http://www.w3.org/TR/REC-html4/loose.dtd\">  " +
+          "<head>  " +
+          "<title>Santa Fe Poetry Broadside... Poets of the Region and Beyond</title>  " +
+          "<meta http-equiv=\"content-type\" content=\"text/html; charset=iso-8859-1\">  " +
+          "<meta name=\"Description\" content=\"Santa Fe Poetry Broadside... Poets of the Region and Beyond\">  " +
+          "<link rel=\"stylesheet\" href=\"broadside.css\" type=\"text/css\">  " +
+          "</head> " +
+          "<body>  " +
+          "<div id=\"header\">  " +
+          "<img alt=\"Santa Fe Poetry Broadside... Poets of the Region and Beyond\"  " +
+          "src=\"banner.gif\"  width=\"681\" height=\"112\" />  " +
+          "<div id=\"navmenu\">  " +
+          "<ul>  " +
+          "<li><a href=\"broadside.html\" title=\"Index of all issues\">All Issues</a></li>  " +
+          "<li><a href=\"about.html\">About the <em>Broadside</em></a></li>  " +
+          "<li><a href=\"bio46.html\">Bionotes</a></li> <li><a href=\"contact.html\">How to reach us</a></li>  " +
+          "<li><a href=\"links.html\">Links</a></li> <li><a href=\"about.html#copy\">Copyright</a></li>  " +
+          "</ul>  " +
+          "</div>  " +
+          "<hr width=\"70%\">   " +
+          "Issue #46, June, 2006 :  " +
+          "</div> " +
+          "</body> " +
+          "</html>  ";
+  
+
+  public void testBioPageFiltering() throws Exception {
+    InputStream actIn = fact.createFilteredInputStream(mau, new StringInputStream(bioPage),
+        Constants.DEFAULT_ENCODING);
+    assertEquals(bioPageFiltered, StringUtil.fromInputStream(actIn));
+    
+    actIn = fact.createFilteredInputStream(mau, new StringInputStream(notBioPage),
+        Constants.DEFAULT_ENCODING);
+    /* this shouldn't change at all */
+    assertEquals(notBioPage, StringUtil.fromInputStream(actIn));
+    
+  }
+  
+  public void testLateIssueBioPageFiltering() throws Exception {
+    InputStream actIn = fact.createFilteredInputStream(mau, new StringInputStream(bioPageNoHtml),
+        Constants.DEFAULT_ENCODING);
+    assertEquals(bioPageNoHtmlFiltered, StringUtil.fromInputStream(actIn));
+    
+    actIn = fact.createFilteredInputStream(mau, new StringInputStream(notBioPageNoHtml),
+        Constants.DEFAULT_ENCODING);
+    /* this shouldn't change at all */
+    assertEquals(notBioPageNoHtml, StringUtil.fromInputStream(actIn));
+  }
+
+}
