21618ba0878ea19e29494bbeb6d87b31ee0c5992
mellen22 <maryellen@lockss.org>
2013-02-20 18:32:51 +0000

    Add feature to find clockss new manifests for HighWire
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@25363 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/propose_new_aus.pl
+++ scripts/tdb/propose_new_aus.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# $Id: propose_new_aus.pl,v 1.2 2012-06-22 06:25:35 mellen22 Exp $
+# $Id: propose_new_aus.pl,v 1.3 2013-02-20 18:32:51 mellen22 Exp $
 #
 # Read in a list of AUs defined with the HighWire plugins.
 # Propose new AUs, either before or after the range provided
@@ -10,14 +10,17 @@ use Getopt::Long;
 
 my $opt_pre = 0;
 my $opt_post = 1;
+my $clockss = 0;
+my $plugin = "";
 my %au_volume = ();
 
 my @Usage = ("$0 [-h] [--pre=<num1>] [--post=<num2>] auid_file\n",
     "--pre=<num1>  Print <num1> earlier AUs (default $opt_pre)\n",
     "--post=<num2> Print <num2> newer AUs (default $opt_post)\n",
+    "-c            Use ClockssHighWirePressH20Plugin (default HighWirePressH20Plugin)\n",
     "-h            Print this help message.");
 sub usage {
-    print '$Revision: 1.2 $' . "\n";
+    print '$Revision: 1.3 $' . "\n";
     print "Usage:\n @Usage\n";
     exit(1);
 }
@@ -27,7 +30,13 @@ my $opt_debug = 0;
 my $ret = GetOptions('help|h' => \$opt_help,
     'pre=i' => \$opt_pre,
     'post=i' => \$opt_post,
+    'c' => \$clockss,
     'debug' => \$opt_debug);
+if ($clockss) {
+   $plugin = "ClockssHighWirePressH20Plugin";
+   } else {
+   $plugin = "HighWirePressH20Plugin";
+   }
 if ($ret != 1 || $opt_help || (int(@ARGV) < 1)) {
     &usage;
 }
@@ -35,7 +44,7 @@ if ($ret != 1 || $opt_help || (int(@ARGV) < 1)) {
 while (my $line = <>) {
     chomp($line);
     # Check only for HighWire plugins.
-    if ($line =~ m/\|(HighWireStrVolPlugin|HighWirePressPlugin|HighWirePressH20Plugin)/i) {
+    if ($line =~ m/(HighWireStrVolPlugin|HighWirePressPlugin|HighWirePressH20Plugin)/i) {
 	if ($line =~ m/\&base_url~(\S+)\&volume_name~(\d+)/) {
 	    my $base_url = $1;
 	    my $vol_num  = $2;
@@ -56,28 +65,19 @@ while (my $line = <>) {
 
 foreach my $base_url (sort(keys(%au_volume))) {
     for (my $x = $au_volume{$base_url}{min} - $opt_pre; $x < $au_volume{$base_url}{min}; ++$x) {
-	&print_au($base_url, $x) if ($x > 0);
+	&print_au($plugin, $base_url, $x) if ($x > 0);
     }
     for (my $x = $au_volume{$base_url}{max} + 1; $x <= $au_volume{$base_url}{max} + $opt_post; ++$x) {
-	&print_au($base_url, $x) if ($x > 0);
+	&print_au($plugin, $base_url, $x) if ($x > 0);
     }
 }
-
-#printf("org|lockss|plugin|highwire|HighWirePressH20Plugin&base_url~http%3A%2F%2Fapplij%2Eoxfordjournals%2Eorg%2F&volume_name~3");
-#printf("org|lockss|plugin|highwire|HighWirePressH20Plugin&base_url~http%3A%2F%2Fbfgp%2Eoxfordjournals%2Eorg%2F&volume_name~6");
-#printf("org|lockss|plugin|highwire|HighWirePressH20Plugin&base_url~http%3A%2F%2Fbfgp%2Eoxfordjournals%2Eorg%2F&volume_name~6");
-#printf("org|lockss|plugin|highwire|HighWirePressH20Plugin&base_url~http%3A%2F%2Fbfgp%2Eoxfordjournals%2Eorg%2F&volume_name~10");
-#printf("org|lockss|plugin|highwire|HighWirePressH20Plugin&base_url~http%3A%2F%2Fbjps%2Eoxfordjournals%2Eorg%2F&volume_name~16");
-#printf("org|lockss|plugin|highwire|HighWirePressH20Plugin&base_url~http%3A%2F%2Fcamqtly%2Eoxfordjournals%2Eorg%2F&volume_name~29");
-#printf("org|lockss|plugin|highwire|HighWirePressH20Plugin&base_url~http%3A%2F%2Fehr%2Eoxfordjournals%2Eorg%2F&volume_name~112");
-#printf("org|lockss|plugin|highwire|HighWirePressH20Plugin&base_url~http%3A%2F%2Fehr%2Eoxfordjournals%2Eorg%2F&volume_name~121");
 
 exit(0);
 
 sub print_au {
-    my ($base_url, $vol_num) = @_;
+    my ($plugin, $base_url, $vol_num) = @_;
     printf("%s|%s|%s|%s|%s\n", "org", "lockss", "plugin", "highwire",
-	"HighWirePressH20Plugin\&base_url~${base_url}\&volume_name~${vol_num}");
+	"${plugin}\&base_url~${base_url}\&volume_name~${vol_num}");
     return(1);
 }
 
