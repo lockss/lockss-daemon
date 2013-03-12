45b6cc500e507ca21a206d98781ff748c38c6e13
thibgc <github@thib.org>
2013-03-12 22:12:47 +0000

    Cosmetic.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@25683 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/emerald/EmeraldSubstancePredicate.java
+++ plugins/src/org/lockss/plugin/emerald/EmeraldSubstancePredicate.java
@@ -1,9 +1,10 @@
-/**
- * $Id:
+/*
+ * $Id: EmeraldSubstancePredicate.java,v 1.2 2013-03-12 22:12:47 thib_gc Exp $
  */
+
 /*
 
-Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
