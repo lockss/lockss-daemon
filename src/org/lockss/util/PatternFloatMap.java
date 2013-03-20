e5ba8750bb9fe12dda7b7729259d5fd59869d04e
tlipkis <tal@pss.com>
2013-03-20 03:23:46 +0000

    Maps first regexp match to float.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@25855 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/util/PatternFloatMap.java
@@ -0,0 +1,150 @@
+/*
+ * $Id: PatternFloatMap.java,v 1.1 2013-03-20 03:23:46 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+
+import java.util.*;
+import java.text.*;
+import org.apache.commons.collections.map.*;
+import org.apache.oro.text.regex.*;
+
+/** "Map" strings to floats, where the keys are patterns against which the
+ * strings are matched.  The patterns are ordered; the value associated
+ * with the first one that matches is returned.  */
+
+// Should genericize (along with PatternIntMap) but need to find a way to
+// do type-depending string parsing.
+
+public class PatternFloatMap {
+  static Logger log = Logger.getLogger("PatternFloatMap");
+
+  /** An empty PatternFloatMap, which always returns the default value. */
+  public final static PatternFloatMap EMPTY =
+    new PatternFloatMap(Collections.EMPTY_LIST);
+
+  private Map<Pattern,Float> patternMap;
+
+  /** Create a PatternFloatMap from a list of strings of the form
+   * <code><i>RE</i>,<i>int</i></code> */
+  public PatternFloatMap(List<String> patternPairs)
+      throws IllegalArgumentException {
+    makePatternMap(patternPairs);
+  }
+
+  /** Create a PatternFloatMap from a string of the form
+   * <code><i>RE</i>,<i>int</i>[;<i>RE</i>,<i>int</i> ...]</code> */
+  public PatternFloatMap(String spec)
+      throws IllegalArgumentException {
+    makePatternMap(StringUtil.breakAt(spec, ";", -1, true, true));
+  }
+
+  private void makePatternMap(List<String> patternPairs)
+      throws IllegalArgumentException {
+    if (patternPairs != null) {
+      patternMap = new LinkedMap();
+      for (String pair : patternPairs) {
+	// Find the last occurrence of comma to avoid regexp quoting
+	int pos = pair.lastIndexOf(',');
+	if (pos < 0) {
+	  throw new IllegalArgumentException("Marformed pattern,int pair; no comma: "
+					     + pair);
+	}
+	String regexp = pair.substring(0, pos);
+	String pristr = pair.substring(pos + 1);
+	float pri;
+	Pattern pat;
+	try {
+	  pri = Float.parseFloat(pristr);
+	  int flags = Perl5Compiler.READ_ONLY_MASK;
+	  pat = RegexpUtil.getCompiler().compile(regexp, flags);
+	  patternMap.put(pat, pri);
+	} catch (NumberFormatException e) {
+	  throw new IllegalArgumentException("Illegal priority: " + pristr);
+	} catch (MalformedPatternException e) {
+	  throw new IllegalArgumentException("Illegal regexp: " + regexp);
+	}
+      }
+    }
+  }
+
+  /** Return the value associated with the first pattern that the string
+   * matches, or zero if none */
+  public float getMatch(String str) {
+    return getMatch(str, 0);
+  }
+
+  /** Return the value associated with the first pattern that the string
+   * matches, or the specified default value if none */
+  public float getMatch(String str, float dfault) {
+    return getMatch(str, dfault, Float.MAX_VALUE);
+  }
+
+  /** Return the value associated with the first pattern that the string
+   * matches, or the specified default value if none, considering only
+   * patterns whose associated value is less than or equal to maxPri. */
+  public float getMatch(String str, float dfault, float maxPri) {
+    Perl5Matcher matcher = RegexpUtil.getMatcher();
+    for (Map.Entry<Pattern,Float> ent : patternMap.entrySet()) {
+      if (ent.getValue() <= maxPri) {
+	Pattern pat = ent.getKey();
+	if (matcher.contains(str, pat)) {
+	  log.debug2("getMatch(" + str + "): " + ent.getValue());
+	  return ent.getValue();
+	}
+      }
+    }
+    log.debug2("getMatch(" + str + "): default: " + dfault);
+    return dfault;
+  }
+
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("[pm: ");
+    if (patternMap.isEmpty()) {
+      sb.append("EMPTY");
+    } else {
+      for (Iterator<Map.Entry<Pattern,Float>> iter = patternMap.entrySet().iterator(); iter.hasNext(); ) {
+	Map.Entry<Pattern,Float> ent = iter.next();
+	sb.append("[");
+	sb.append(ent.getKey().getPattern());
+	sb.append(": ");
+	sb.append(ent.getValue());
+	sb.append("]");
+	if (iter.hasNext()) {
+	  sb.append(", ");
+	}
+      }
+    }
+    sb.append("]");
+    return sb.toString();
+  }
+}
---
+++ test/src/org/lockss/util/TestPatternFloatMap.java
@@ -0,0 +1,97 @@
+/*
+ * $Id: TestPatternFloatMap.java,v 1.1 2013-03-20 03:23:46 tlipkis Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+
+import java.util.*;
+import org.lockss.util.*;
+import org.lockss.test.*;
+
+public class TestPatternFloatMap extends LockssTestCase {
+
+  public void testToString() {
+    PatternFloatMap ppm1 = new PatternFloatMap("a.*b,2;ccc,4.3");
+    assertEquals("[pm: [a.*b: 2.0], [ccc: 4.3]]", ppm1.toString());
+
+    PatternFloatMap ppm2 =
+      new PatternFloatMap(ListUtil.list("xx,1", "yy,2"));
+    assertEquals("[pm: [xx: 1.0], [yy: 2.0]]", ppm2.toString());
+  }
+
+  public void testGetMatch() {
+    PatternFloatMap ppm1 = new PatternFloatMap("a.*b,2.5;ccc,-4;ddd,5e2");
+    assertEquals(0.0F, ppm1.getMatch("a123c"));
+    assertEquals(123.0F, ppm1.getMatch("a123c", 123));
+    assertEquals(2.5F, ppm1.getMatch("a123b"));
+    assertEquals(2.5F, ppm1.getMatch("accccb"));
+    assertEquals(-4.0F, ppm1.getMatch("bccccb"));
+    assertEquals(-4.0F, ppm1.getMatch("bccccb", 111));
+    assertEquals(-4F, ppm1.getMatch("bccccb", 111, 10));
+    assertEquals(111F, ppm1.getMatch("bccccb", 111, -10));
+    assertEquals(500F, ppm1.getMatch("bdddb"));
+  }
+
+  public void testEmpty() {
+    PatternFloatMap ppm = PatternFloatMap.EMPTY;
+    assertEquals(0F, ppm.getMatch("a"));
+    assertEquals(42.5F, ppm.getMatch("a", 42.5F));
+    assertEquals("[pm: EMPTY]", ppm.toString());
+  }
+
+  public void testIll() {
+    try {
+      PatternFloatMap ppm1 = new PatternFloatMap("a[)2");
+      fail("Should throw: Malformed");
+    } catch (IllegalArgumentException e) {
+      assertMatchesRE("no comma", e.getMessage());
+    }
+    try {
+      PatternFloatMap ppm1 = new PatternFloatMap("a,1;bbb");
+      fail("Should throw: Malformed");
+    } catch (IllegalArgumentException e) {
+      assertMatchesRE("no comma", e.getMessage());
+    }
+    try {
+      PatternFloatMap ppm1 = new PatternFloatMap("a.*b,2;ccc,xyz");
+      fail("Should throw: illegal priority");
+    } catch (IllegalArgumentException e) {
+      assertMatchesRE("Illegal priority", e.getMessage());
+    }
+    try {
+      PatternFloatMap ppm1 = new PatternFloatMap("a[),2");
+      fail("Should throw: illegal regexp");
+    } catch (IllegalArgumentException e) {
+      assertMatchesRE("Illegal regexp", e.getMessage());
+    }
+  }
+
+}
