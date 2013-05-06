838d4f0aea40d8a4a977048c7d60be8a8dc4bffa
lytdoan <ldoan@stanford.edu>
2013-05-06 17:01:16 +0000

    New plugin with sitemap as start url
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@27180 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/medknow/MedknowArticleIteratorFactory.java
@@ -0,0 +1,217 @@
+/**
+ * $Id: MedknowArticleIteratorFactory.java,v 1.1 2013-05-06 17:01:16 ldoan Exp $
+ */
+
+/**
+
+Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.medknow;
+
+import java.util.Iterator;
+import java.util.regex.*;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.ArticleMetadataExtractor;
+import org.lockss.extractor.ArticleMetadataExtractorFactory;
+import org.lockss.extractor.BaseArticleMetadataExtractor;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.*;
+import org.lockss.util.Logger;
+
+/**
+ * Medknow's Abstract page has links to Full-Text HTML and PDF, and also
+ * contains metatdata. Medknow's file type:
+ *      type=0  - Abstract
+ *      no type - Full-Text HTML
+ *      type=2  - Full-text PDF
+ *      type=3  - Full-Text Mobile
+ *      type=4  - EPUB 
+ *      
+ * also contains citation URLs:
+ * "http://www.afrjpaedsurg.org/citation.asp?issn=0189-6725;year=2012;volume=9;
+ * issue=1;spage=13;epage=16;aulast=Kothari;
+ * aid=AfrJPaediatrSurg_2012_9_1_13_93295"
+ *      
+ * full-text HTML URL:
+ * "http://www.afrjpaedsurg.org/article.asp?issn=0189-6725;year=2012;volume=9;
+ *  issue=1;spage=1;epage=2;aulast=Rutz"
+ */
+public class MedknowArticleIteratorFactory
+  implements ArticleIteratorFactory, ArticleMetadataExtractorFactory {
+
+  protected static Logger log = Logger.getLogger(MedknowArticleIteratorFactory.class);
+  
+  /** ROOT_TEMPLATE AND PATTERN_TEMPLATE required by SubTreeArticleIterator.
+    SubTreeArticleIterator returns only the URLs under ROOT_TEMPLATE, that
+    match PATTERN_TEMPLATE.  In this case, only URLs ending with 'type=0'
+    are returned. 'type=0' is the abstract which also contains medatata. */
+  protected static final String ROOT_TEMPLATE = "\"%s\", base_url";
+  protected static final String PATTERN_TEMPLATE = "\"^%sarticle\\.asp\\?issn=%s;year=%d;volume=%s;.*;type=0$\", base_url, journal_issn, year, volume_name";
+  
+  /**
+   * Create MedknowArticleIterator with the new object of 
+   * SubTreeArticleIterator ROOT_TEMPLATE and PATTERN_TEMPLATE already set.
+   */
+  @Override
+  public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au,
+                                                      MetadataTarget target)
+                                                          throws PluginException {
+      
+    return new MedknowArticleIterator(au,
+                                      new SubTreeArticleIterator.Spec()
+                                          .setTarget(target)
+                                          .setRootTemplate(ROOT_TEMPLATE)
+                                          .setPatternTemplate(PATTERN_TEMPLATE,
+                                              Pattern.CASE_INSENSITIVE)
+                                      );
+  } /** end createArticleIterator */     
+  
+  
+  /**
+   * Create article files.  Use abstract url as the base, and get other file
+   * types from it, full-text html, pdf, epub, mobile.
+   */
+  protected static class MedknowArticleIterator extends SubTreeArticleIterator {
+ 
+    /** this pattern is derived from PATTERN_TEMPLATE,
+      to create regex 'capturing groups', used for guessing other file types. */
+    protected Pattern ABSTRACT_PATTERN = Pattern.compile("/(article\\.asp)(\\?.*);type=0$", Pattern.CASE_INSENSITIVE);
+
+    protected MedknowArticleIterator(ArchivalUnit au,
+                                     SubTreeArticleIterator.Spec spec) {
+        super(au, spec);
+    }
+    
+    /**
+     * Start to match abstract url, the guess other file types from it.
+     * Abstrqct url: "http://www.afrjpaedsurg.org/article.asp?issn=0189-6725;year=2012;
+     *  volume=9;issue=1;spage=1;epage=2;aulast=Rutz;tyep=0"
+     */
+    @Override
+    protected ArticleFiles createArticleFiles(CachedUrl cu) {
+
+      String url = cu.getUrl();
+      log.debug("createArticleFiles: " + url);
+
+      Matcher abstractMat = ABSTRACT_PATTERN.matcher(url);
+      if (abstractMat.find()) {
+        return processAbstract(cu, abstractMat);
+      }
+      
+      /** Incompatible between ArticleIteratorFactor and ArtileIterator objects
+        Should not happen. */
+      log.warning("Mismatch between article iterator factory and article iterator: " + url);
+
+      return null;
+
+    } /** end createArticleFiles */
+    
+    protected ArticleFiles processAbstract(CachedUrl abstractCu, Matcher absMat) {
+
+      ArticleFiles af = new ArticleFiles();
+      
+      af.setRoleCu(ArticleFiles.ROLE_ABSTRACT, abstractCu);
+      af.setRoleCu(ArticleFiles.ROLE_ARTICLE_METADATA, abstractCu);
+      guessAdditionalFiles(af, absMat);
+
+      return af;
+      
+    } /** end processAbstract */
+
+    protected ArticleFiles guessAdditionalFiles(ArticleFiles af, Matcher absMat) {
+      
+      CachedUrl htmlCu = au.makeCachedUrl(absMat.replaceFirst("/$1$2"));
+      //af.setFullTextCu(htmlCu);
+      setArticleRole(af, htmlCu, ArticleFiles.ROLE_FULL_TEXT_HTML);
+      
+      CachedUrl pdfCu = au.makeCachedUrl(absMat.replaceFirst("/$1$2;type=2"));
+      setArticleRole(af, pdfCu, ArticleFiles.ROLE_FULL_TEXT_PDF);
+
+      CachedUrl mobileCu = au.makeCachedUrl(absMat.replaceFirst("/$1$2;type=3"));
+      setArticleRole(af, mobileCu, ArticleFiles.ROLE_FULL_TEXT_MOBILE);
+
+      CachedUrl epubCu = au.makeCachedUrl(absMat.replaceFirst("/$1$2;type=4"));
+      setArticleRole(af, epubCu, ArticleFiles.ROLE_FULL_TEXT_EPUB);                              
+    
+      CachedUrl citationCu = au.makeCachedUrl(absMat.replaceFirst("/citation.asp$2"));
+      setArticleRole(af, citationCu, ArticleFiles.ROLE_CITATION);
+
+      chooseFullTextCu(af);
+      
+      return af;
+      
+    } /** end guessAdditionalFiles */
+    
+    protected ArticleFiles chooseFullTextCu(ArticleFiles af) {
+      
+      final String[] ORDER = new String[] {
+          ArticleFiles.ROLE_FULL_TEXT_HTML,
+          ArticleFiles.ROLE_FULL_TEXT_PDF,
+          ArticleFiles.ROLE_FULL_TEXT_MOBILE,
+          ArticleFiles.ROLE_FULL_TEXT_EPUB
+      };
+      
+      for (String role : ORDER) {
+        CachedUrl cu = af.getRoleCu(role);
+        if (cu != null) {
+          af.setFullTextCu(cu);
+          return af;
+        }
+      }
+      
+      log.debug2("No full-text CU");
+      return af; // af probably has other data in it, don't want to return null
+      
+    } /** end chooseFullTextCu */
+   
+    
+    /**
+     * Set article role for each file type. Also set ROLE_ARTICLE_METADATA
+     * for Abstract HTML file. Param role2 is ArticleFiles.ROLE_ARTICLE_METADATA.
+     */
+    private void setArticleRole(ArticleFiles af, CachedUrl cu, String role) {
+
+      if ((cu != null) && (cu.hasContent())) {
+        if (role != null) {
+          af.setRoleCu(role, cu);
+        }
+      }
+      AuUtil.safeRelease(cu);
+
+    } /** end setArticleRole */
+      
+  } /** end class MedknowArticleIterator */
+   
+  
+  @Override
+  public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target)
+      throws PluginException {
+    return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
+  }
+
+} /** end class MedknowArticleIteratorFactory */
---
+++ plugins/src/org/lockss/plugin/medknow/MedknowHtmlMetadataExtractorFactory.java
@@ -0,0 +1,120 @@
+/**
+ * $Id: MedknowHtmlMetadataExtractorFactory.java,v 1.1 2013-05-06 17:01:16 ldoan Exp $
+ */
+
+/**
+
+ Copyright (c) 2000-2009 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.medknow;
+
+import java.io.*;
+import java.util.*;
+import org.apache.commons.collections.MultiMap;
+import org.apache.commons.collections.map.MultiValueMap;
+import org.lockss.util.*;
+import org.lockss.daemon.*;
+import org.lockss.extractor.*;
+import org.lockss.plugin.*;
+
+/**
+ * MedknowHtmlMetadataExtractorFactory extracts metadata from each article.
+ */
+public class MedknowHtmlMetadataExtractorFactory implements
+    FileMetadataExtractorFactory {
+  
+  static Logger log = Logger.getLogger(MedknowHtmlMetadataExtractorFactory.class);
+
+  public FileMetadataExtractor createFileMetadataExtractor(
+      MetadataTarget target, String contentType) throws PluginException {
+    
+    return new MedknowHtmlMetadataExtractor();
+    
+  } // createFileMetadataExtractor
+
+  public static class MedknowHtmlMetadataExtractor 
+    extends SimpleHtmlMetaTagMetadataExtractor {
+
+    // Map Medknow-specific HTML meta tag names to cooked metadata fields
+    private static MultiMap tagMap = new MultiValueMap();
+    
+    static {
+      
+      tagMap.put("citation_journal_title", MetadataField.FIELD_JOURNAL_TITLE);
+      tagMap.put("citation_title", MetadataField.FIELD_ARTICLE_TITLE);
+      tagMap.put("citation_date", MetadataField.FIELD_DATE);
+      tagMap.put("citation_author", MetadataField.FIELD_AUTHOR);
+      tagMap.put("citation_authors",
+          new MetadataField(MetadataField.FIELD_AUTHOR,
+              MetadataField.splitAt(";")));
+      tagMap.put("citation_publisher", MetadataField.FIELD_PUBLISHER);
+      tagMap.put("citation_issn", MetadataField.FIELD_ISSN);
+      tagMap.put("citation_volume", MetadataField.FIELD_VOLUME);
+      tagMap.put("citation_issue", MetadataField.FIELD_ISSUE);
+      tagMap.put("citation_firstpage", MetadataField.FIELD_START_PAGE);
+      tagMap.put("citation_lastpage", MetadataField.FIELD_END_PAGE);
+      tagMap.put("citation_doi", MetadataField.FIELD_DOI);
+      tagMap.put("citation_public_url", MetadataField.FIELD_ACCESS_URL);
+      tagMap.put("keywords",
+          new MetadataField(MetadataField.FIELD_KEYWORDS,
+              MetadataField.splitAt(",")));
+      tagMap.put("DC.Format", MetadataField.DC_FIELD_FORMAT);
+      tagMap.put("DC.Identifier", MetadataField.DC_FIELD_IDENTIFIER);
+      tagMap.put("DC.Subject", MetadataField.DC_FIELD_SUBJECT);
+      tagMap.put("DC.Description", MetadataField.DC_FIELD_DESCRIPTION);
+            
+    } // static
+
+    @Override
+    public ArticleMetadata extract(MetadataTarget target, CachedUrl cu)
+      throws IOException {
+      
+      log.debug3("Metadata - cachedurl cu:" + cu.getUrl());
+      
+      ArticleMetadata am = super.extract(target, cu);
+      am.cook(tagMap);
+      
+      /** raw metadata:
+        dc.identifier: [http://www.afrjpaedsurg.org/article.asp?issn=0189-6725;year=2010;volume=7;issue=2;spage=61;epage=65;aulast=Gupta;type=0; 0189-6725]
+        extract the muti-value for this field, check if one of the values
+        is valid issn, then set the issn metadata in cooked metadata list. */
+      List<String> idList = am.getRawList("DC.Identifier");
+      for (String id : idList) {
+        if (MetadataUtil.isIssn(id)) {
+          am.put(MetadataField.FIELD_ISSN, id);
+          break;
+        }
+      }
+      
+      return am;
+      
+    } /** end extract */
+    
+  } /** end class MedknowHtmlMetadataExtractor */
+  
+} /** end class MedknowHtmlMetadataExtractorFactory */
+ 
