d99e06ac2e41f11a8a413fcd5fa149eb5e2dfc70
bhayes <claire@lockss.org>
2013-06-04 17:50:31 +0000

    Add a wrapper to check the order of VoteBlocks of a VoteBlocksIterator.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@27935 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/protocol/OrderedVoteBlocksIterator.java
@@ -0,0 +1,113 @@
+/*
+ * $Id: OrderedVoteBlocksIterator.java,v 1.1 2013-06-04 17:50:31 barry409 Exp $
+ */
+
+/*
+
+Copyright (c) 2013 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.protocol;
+
+import java.io.IOException;
+
+/**
+ * Provide a wrapper for a {@link VoteBlocksIterator} which will check
+ * that the blocks are in the canonical order.
+ */
+public class OrderedVoteBlocksIterator implements VoteBlocksIterator {
+  private final VoteBlocksIterator iterator;
+  private VoteBlock prevBlock = null;
+
+  public static class OrderException extends IllegalStateException {
+    public OrderException(String msg) {
+      super(msg);
+    }
+  }
+
+  /**
+   * @param iterator An instance to wrap.
+   */
+  public OrderedVoteBlocksIterator(VoteBlocksIterator iterator) {
+    if (iterator == null) {
+      throw new IllegalArgumentException("wrapped iterator must be non-null");
+    }
+    this.iterator = iterator;
+  }
+
+  private void check() throws IOException {
+    if (prevBlock != null && iterator.peek() != null) {
+      if (prevBlock.compareTo(iterator.peek()) >= 0) {
+	throw new OrderException("VoteBlocks misordered.");
+      }
+    }
+  }
+
+  /**
+   * Return true if the iteration has more VoteBlock objects.
+   *  
+   * @return true if the iteration has more elements.
+   * @throws OrderException if blocks are detected to be out of order.
+   */
+  @Override public boolean hasNext() throws IOException { 
+    check();
+    return iterator.hasNext(); 
+  }
+
+  /**
+   * Returns the next VoteBlock in the iteration.
+   * 
+   * @return The next VoteBlock in the iteration
+   * VoteBlocks
+   * @throws IOException
+   * @throws NoSuchElementException
+   * @throws OrderException if blocks are detected to be out of order.
+   */
+  @Override public VoteBlock next() throws IOException { 
+    check();
+    prevBlock = iterator.next();
+    return prevBlock;
+  }
+
+  /**
+   * Returns the VoteBlock that next() would return, or null if next()
+   * would throw NoSuchElementException.  Does not advance the iterator
+   * cursor forward.
+   * 
+   * @return The next VoteBlock in the iteration, or null if no more
+   * VoteBlocks.
+   * @throws OrderException if blocks are detected to be out of order.
+   */
+  @Override public VoteBlock peek() throws IOException { 
+    check();
+    return iterator.peek(); 
+  }
+
+  /** Release any resources held by the iterator */
+  @Override public void release() {
+    iterator.release(); 
+  }
+}
---
+++ test/src/org/lockss/protocol/TestOrderedVoteBlocksIterator.java
@@ -0,0 +1,158 @@
+/*
+ * $Id: TestOrderedVoteBlocksIterator.java,v 1.1 2013-06-04 17:50:31 barry409 Exp $
+ */
+
+/*
+
+Copyright (c) 2013 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.protocol;
+
+import java.io.*;
+import java.util.*;
+
+import org.lockss.test.LockssTestCase;
+
+
+public class TestOrderedVoteBlocksIterator extends LockssTestCase {
+
+  private class ListVoteBlocksIterator implements VoteBlocksIterator {
+    private final List<VoteBlock> voteBlocks;
+    private int cursor = 0;
+
+    ListVoteBlocksIterator(List<VoteBlock> voteBlocks) {
+      this.voteBlocks = voteBlocks;
+    }
+
+    @Override public boolean hasNext() {
+      return (cursor < voteBlocks.size());
+    }
+
+    @Override public VoteBlock next() {
+      if (hasNext()) {
+	return voteBlocks.get(cursor++);
+      } else {
+	throw new NoSuchElementException();
+      }
+    }
+
+    @Override public VoteBlock peek() {
+      if (hasNext()) {
+	return voteBlocks.get(cursor);
+      } else {
+	return null;
+      }
+    }
+
+    @Override public void release() {}
+  }
+
+  private void sizedTest(int size) throws Exception {
+    List<VoteBlock> voteBlockList = V3TestUtils.makeVoteBlockList(size);
+    VoteBlocksIterator wrapped = new ListVoteBlocksIterator(voteBlockList);
+    VoteBlocksIterator iterator = new OrderedVoteBlocksIterator(wrapped);
+    for (int i = 0; i < voteBlockList.size(); i++) {
+      assertTrue(iterator.hasNext());
+      assertEquals(voteBlockList.get(i), iterator.peek());
+      assertEquals(voteBlockList.get(i), iterator.next());
+    }
+    assertFalse(iterator.hasNext());
+    try {
+      iterator.next();
+      fail("Expected Exception not thrown.");
+    } catch (NoSuchElementException ex) {
+      // expected
+    }
+    assertNull(iterator.peek());
+  }
+
+  private void sizedMisorderedTest(int size, int i1, int i2) throws Exception {
+    assertTrue("Test not set up correctly.", i1 < i2);
+    
+    List<VoteBlock> voteBlockList = V3TestUtils.makeVoteBlockList(size);
+    swap(voteBlockList, i1, i2);
+    VoteBlocksIterator wrapped = new ListVoteBlocksIterator(voteBlockList);
+    VoteBlocksIterator iterator = new OrderedVoteBlocksIterator(wrapped);
+
+    // The block swapped into i1 is in order with the one before; it's
+    // the one after i1 that's out of order with the one previously at i2.
+    for (int i = 0; i < i1+1; i++) {
+      assertTrue(iterator.hasNext());
+      assertEquals(voteBlockList.get(i), iterator.peek());
+      assertEquals(voteBlockList.get(i), iterator.next());
+    }
+
+    // Everything throws.
+    try {
+      iterator.hasNext();
+      fail("Expected Exception not thrown.");
+    } catch (OrderedVoteBlocksIterator.OrderException ex) {
+      // expected
+    }
+    try {
+      iterator.peek();
+      fail("Expected Exception not thrown.");
+    } catch (OrderedVoteBlocksIterator.OrderException ex) {
+      // expected
+    }
+    try {
+      iterator.next();
+      fail("Expected Exception not thrown.");
+    } catch (OrderedVoteBlocksIterator.OrderException ex) {
+      // expected
+    }
+  }
+
+  private void swap(List<VoteBlock> voteBlockList, int i, int j) {
+    VoteBlock temp = voteBlockList.get(i);
+    voteBlockList.set(i, voteBlockList.get(j));
+    voteBlockList.set(j, temp);
+  }
+
+  public void testNullCreate() throws Exception {
+    try {
+      new OrderedVoteBlocksIterator(null);
+      fail("Illegal create failed to throw.");
+    } catch (IllegalArgumentException ex) {
+      // expected
+    }
+  }
+
+  // Test that misorder is detected
+  public void testOrdered() throws Exception {
+    sizedTest(0);
+    sizedTest(1);
+    sizedTest(2);
+    sizedTest(10);
+  }
+
+  // Test that misorder is detected
+  public void testMisordered() throws Exception {
+    sizedMisorderedTest(2, 0, 1);
+    sizedMisorderedTest(10, 3, 5);
+  }
+}
