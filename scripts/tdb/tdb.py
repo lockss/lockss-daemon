4e5798aa1b77a864e7bc1d021b0893b98474bab6
thibgc <github@thib.org>
2013-06-11 19:09:31 +0000

    Typo.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@28134 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/tdb.py
+++ scripts/tdb/tdb.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-# $Id: tdb.py,v 1.21 2013-04-01 13:01:24 pgust Exp $
+# $Id: tdb.py,v 1.22 2013-06-11 19:09:31 thib_gc Exp $
 
 __copyright__ = '''\
-Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -28,7 +28,7 @@ be used in advertising or otherwise to promote the sale, use or other dealings
 in this Software without prior written authorization from Stanford University.
 '''
 
-__version__ = '0.4.3'
+__version__ = '0.4.4'
 
 import re
 
@@ -126,7 +126,7 @@ class Title(Map):
         # Fixed
         self.__publisher = None
         # Memoized
-        self.__eibsn = Undef
+        self.__eisbn = Undef
         self.__eissn = Undef
         self.__isbn = Undef
         self.__issn = Undef
