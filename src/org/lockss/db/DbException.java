713e6078952f3804eb4f98997f9465c9009ee14a
fergaloy <fergaloy@stanford.edu>
2013-06-19 22:35:40 +0000

    Initial version.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@28268 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/db/DbException.java
@@ -0,0 +1,58 @@
+/*
+ * $Id: DbException.java,v 1.1 2013-06-19 22:35:40 fergaloy-sf Exp $
+ */
+
+/*
+
+ Copyright (c) 2013 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+/**
+ * Exception specific to database processing.
+ * 
+ * @version 1.0
+ */
+package org.lockss.db;
+
+@SuppressWarnings("serial")
+public class DbException extends Exception {
+
+  public DbException() {
+    super();
+  }
+
+  public DbException(String message) {
+    super(message);
+  }
+
+  public DbException(Throwable cause) {
+    super(cause);
+  }
+
+  public DbException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
