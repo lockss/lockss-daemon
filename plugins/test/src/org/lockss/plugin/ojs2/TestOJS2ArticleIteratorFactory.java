2a30c5769a0b973ca68b7341f37f05f69a897aaa
lytdoan <ldoan@stanford.edu>
2013-06-20 01:12:14 +0000

    Modifed to test abstract with frame src containging metadata. Issue3907
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@28290 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/test/src/org/lockss/plugin/ojs2/TestOJS2ArticleIteratorFactory.java
+++ plugins/test/src/org/lockss/plugin/ojs2/TestOJS2ArticleIteratorFactory.java
@@ -1,8 +1,4 @@
-/*
- * $Id: TestOJS2ArticleIteratorFactory.java,v 1.2 2012-12-24 00:31:56 ldoan Exp $
- */
-
-/*
+/* $Id: TestOJS2ArticleIteratorFactory.java,v 1.3 2013-06-20 01:12:14 ldoan Exp $
 
 Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
@@ -40,30 +36,21 @@ import java.util.Iterator;
 
 import org.lockss.config.ConfigManager;
 import org.lockss.config.Configuration;
-import org.lockss.daemon.ConfigParamDescr;
-import org.lockss.extractor.ArticleMetadataExtractor;
-import org.lockss.extractor.FileMetadataExtractor;
-import org.lockss.extractor.MetadataTarget;
 import org.lockss.plugin.*;
 import org.lockss.test.*;
 import org.lockss.util.*;
 
 /* 
- * TestOJS2ArticleIteratorFactory stores sample article URLs with different
- * format: abstract, full text HTML and full text PDF.  The input sample TOC file
- * is called text_toc_junit.html. It then invokes OJS2ArticleIterator which
- * will process the TOC file.
- * 
- * Table of content:
+ * Processes the table of content:
  * http://www.ojs2articleiteratortest.com/index.php/lq/issue/view/478
- * 
- * Articles:
- * Abstract - http://www.ojs2articleiteratortest.com/index.php/lq/article/view/8110
- * Full Text HTML - http://www.ojs2articleiteratortest.com/lq/article/view/8110/8514
- * Full Text PDF - http://www.ojs2articleiteratortest.com/index.php/lq/article/view/8110/8601
- * 
- * The label for the full text HTML is typically 'HTML', but sometimes 'Full Text'.
- * An example is http://www.ancient-asia-journal.com/issue/view/2 (Ubiquity Press).
+ *
+ * Stores sample article URLs with different article files aspects:
+ * Abstract - http://www.ojs2articleiteratortest.com/index.php/lq/article
+ *                      /view/8110
+ * Full Text HTML - http://www.ojs2articleiteratortest.com/lq/article
+ *                      /view/8110/8514
+ * Full Text PDF - http://www.ojs2articleiteratortest.com/index.php/lq/article
+ *                      /view/8110/8601
  */
 
 public class TestOJS2ArticleIteratorFactory extends ArticleIteratorTestCase {
@@ -71,22 +58,44 @@ public class TestOJS2ArticleIteratorFactory extends ArticleIteratorTestCase {
   static Logger log = Logger.getLogger(TestOJS2ArticleIteratorFactory.class);
 
   private final String PLUGIN_NAME = "org.lockss.plugin.ojs2.OJS2Plugin";
-  static final String BASE_URL_KEY = ConfigParamDescr.BASE_URL.getKey();
   private final String BASE_URL = "http://www.ojs2articleiteratortest.com/";
-  static final String JOURNAL_ID_KEY = ConfigParamDescr.JOURNAL_ID.getKey();
   private final String JOURNAL_ID = "lq";
-  static final String YEAR_KEY = ConfigParamDescr.YEAR.getKey();
   private final String YEAR = "2012";
   
-  private final String TOC_URL =
-        "http://www.ojs2articleiteratortest.com/index.php/lq/issue/view/478";
+  // expected urls for artile 1
+  private final String EXPECTED_ABS_URL_1 =
+    "http://www.ojs2articleiteratortest.com/index.php/lq/article/view/8110";
+  private final String EXPECTED_ARTICLE_METADATA_URL_1 =
+    "http://www.ojs2articleiteratortest.com/index.php/lq/article/view/8110";
+  private final String EXPECTED_FULL_TEXT_HTML_URL_1 = 
+    "http://www.ojs2articleiteratortest.com/index.php/lq/article/view/8110/8514";
+  private final String EXPECTED_PDF_URL_1 = 
+    "http://www.ojs2articleiteratortest.com/index.php/lq/article/view/8110/8601";
+  private final String EXPECTED_FULL_TEXT_URL_1 = EXPECTED_FULL_TEXT_HTML_URL_1;
 
-  protected String cuRole = null;
-  ArticleMetadataExtractor.Emitter emitter;
-  protected boolean emitDefaultIfNone = false;
-  FileMetadataExtractor me = null; 
-  MetadataTarget target;
+  String [] expectedUrls1 = { EXPECTED_ABS_URL_1,
+                              EXPECTED_ARTICLE_METADATA_URL_1,
+                              EXPECTED_FULL_TEXT_URL_1,
+                              EXPECTED_FULL_TEXT_HTML_URL_1,
+                              EXPECTED_PDF_URL_1 };
 
+  // expected urls for article 2
+  private final String EXPECTED_ABS_URL_2 =
+    "http://www.ojs2articleiteratortest.com/index.php/lq/article/view/8120";
+  private final String EXPECTED_ARTICLE_METADATA_URL_2 =
+    "http://www.ojs2articleiteratortest.com/index.php/lq/article/viewArticle/8120";
+  private final String EXPECTED_FULL_TEXT_HTML_URL_2 = 
+    "http://www.ojs2articleiteratortest.com/index.php/lq/article/view/8120/8515";
+  private final String EXPECTED_PDF_URL_2 = 
+    "http://www.ojs2articleiteratortest.com/index.php/lq/article/view/8120/8602";
+  private final String EXPECTED_FULL_TEXT_URL_2 = EXPECTED_FULL_TEXT_HTML_URL_2;
+  
+  String [] expectedUrls2 = { EXPECTED_ABS_URL_2,
+                              EXPECTED_ARTICLE_METADATA_URL_2,
+                              EXPECTED_FULL_TEXT_URL_2,
+                              EXPECTED_FULL_TEXT_HTML_URL_2,
+                              EXPECTED_PDF_URL_2 };
+  
   public void setUp() throws Exception {
     super.setUp();
     au = createAu();
@@ -101,152 +110,155 @@ public class TestOJS2ArticleIteratorFactory extends ArticleIteratorTestCase {
         PluginTestUtil.createAndStartAu(PLUGIN_NAME, OJS2AuConfig());
   }     
 
-  // Set configuration attributes to create plugin AU (archival unit)
   Configuration OJS2AuConfig() {
-    
     Configuration conf = ConfigManager.newConfiguration();
     conf.put("base_url", BASE_URL);
     conf.put("journal_id", JOURNAL_ID);
     conf.put("year", YEAR);
     return conf;
-    
-  } // OJS2AuConfig
+  }
   
-  // These URLs are the simulated content URLs stored in the UrlCacher object.
+  // Simulated content URLs stored in the UrlCacher object.
   public void storeTestContent(String url, UrlCacher urlca) throws Exception {
-    
-    if (url.contains("478")) { // issue table of contents
-      urlca.storeContent(getTestTocInputStream(TOC_URL), getAbsProperties());
-    } else {
-      // only needs nodes without contents since we don't need to
-      // look in the article content.
-      //urlca.storeContent(getTestTocInputStream(null), getAbsProperties());
-      urlca.storeContent(new StringInputStream("<html></html>"), getAbsProperties());
+    log.info("storeTestContent() url: " + url);
+    // issue table of content
+    if (url.contains("478")) { 
+      urlca.storeContent(getTestTocInputStream(), getHtmlProperties());
+      return;
+    } 
+    // abs/full-text html
+    if (url.endsWith("8110") || url.endsWith("8120")
+        || url.endsWith("8514") || url.endsWith("8515")) { 
+      urlca.storeContent(new StringInputStream("<html></html>"), 
+                         getHtmlProperties());
+      return;
     }
- 
-    return;
-    
-  } // storeTestContent
+    // pdf
+    if (url.endsWith("8601") || url.endsWith("8602")) { 
+      urlca.storeContent(new StringInputStream(""), getPdfProperties());
+      return;
+    }
+  }  
   
-  
-  // Use Assert statements in class ArticleTester 
-  public void passOrFailArticle(ArticleFiles af) {
-    
-    ArticleTester testArticleObj = new ArticleTester(af);
-    
-    // example: http://www.ojs2articleiteratortest.com/index.php/lq/article/view/8110
-    if (testArticleObj.testAbstract("8110") != null) {
-      log.info("testAbstract(\"8110\") passed");
-    } else {
-        if (testArticleObj.testAbstract("8220") != null) {
-          log.info("testAbstract(\"8220\") passed");
-        }
-    }
-    
-    // example: http://www.ojs2articleiteratortest.com/index.php/lq/article/view/8110/8514
-    if (testArticleObj.testFullTextHtml("8110/8514") != null) {
-      log.info("testFullTextHtml(\"8110/8514\") passed");
-    } else {
-        // http://www.ojs2articleiteratortest.com/index.php/lq/article/view/8220/8390
-        if (testArticleObj.testFullTextHtml("8220/8390") != null) {
-          log.info("testFullTextHtml(\"8220/8390\") passed");
-        }
-    }
-    
-    // The article iterator lists full text HTML, but not PDFs
-    // so the following tests will have no results.
-    if (testArticleObj.testFullTextPdf("8110/8601") != null) {
-      log.info("testFullTextPdf(\"8110/8601\") passed");
-    } else {
-        // example: http://www.ojs2articleiteratortest.com/index.php/lq/article/view/8220/8391
-        if (testArticleObj.testFullTextPdf("8220/8391") != null) {
-          log.info("testFullTextPdf(\"8220/8391\") passed");
-        }
-    }
-    
-    return;
-    
-  } // passOrFailArticle
-  
-  // Access OJS2ArticleIterator
-  public void accessOJS2ArticleIterator(ArchivalUnit au) {
-
-    Iterator<ArticleFiles> it = au.getArticleIterator();
-    while (it.hasNext()) {
-      
-      ArticleFiles af = it.next();
-      log.info("article file af: " + af.toString());
-
-      // Pass or fail articles
-      passOrFailArticle(af);
-
-    } // while
-
-  } // accessOJS2ArticleIterator  
- 
-  // Create simulated article files for testing
-  // Store them in UrlCacher object
-  // Access OJS2ArticleItrerator to process the table of contents
-  // page (TOC). The simulated TOC file being read in is test_toc_junit.html
+  // Create simulated article files for testing. Store them in UrlCacher object
+  // Access OJS2ArticleItrerator to process the table of content page (TOC).
+  // The simulated TOC file being read in is test_toc_junit.html
   // which can be found in the same directory as this TestOJS2ArticleIterator.
   public void testCreateArticleFiles() throws Exception {
      
     ArrayList<String> articleUrls = new ArrayList<String>();
-    articleUrls.add(String.format("%sindex.php/%s/issue/view/478", BASE_URL, JOURNAL_ID)); // table of contents
+    articleUrls.add(String.format("%sindex.php/%s/issue/view/478",
+                                  BASE_URL, JOURNAL_ID)); // table of contents
+    articleUrls.add(String.format("%sindex.php/%s/article/view/8110",
+                                  BASE_URL, JOURNAL_ID)); // abstract
+    articleUrls.add(String.format("%sindex.php/%s/article/view/8110/8514",
+                                  BASE_URL, JOURNAL_ID)); // full-text html
+    articleUrls.add(String.format("%sindex.php/%s/article/view/8110/8601",
+                                  BASE_URL, JOURNAL_ID)); // pdf
+    articleUrls.add(String.format("%sindex.php/%s/article/view/8120",
+                                  BASE_URL, JOURNAL_ID)); // abstract
+    articleUrls.add(String.format("%sindex.php/%s/article/viewArticle/8120",
+                                  BASE_URL, JOURNAL_ID)); // abstract
+    articleUrls.add(String.format("%sindex.php/%s/article/view/8120/8515",
+                                  BASE_URL, JOURNAL_ID)); // full-text html
+    articleUrls.add(String.format("%sindex.php/%s/article/view/8120/8602",
+                                  BASE_URL, JOURNAL_ID)); // pdf
     
-    articleUrls.add(String.format("%sindex.php/%s/article/view/8110", BASE_URL, JOURNAL_ID));
-    articleUrls.add(String.format("%sindex.php/%s/article/view/8110/8514", BASE_URL, JOURNAL_ID));
-    articleUrls.add(String.format("%sindex.php/%s/article/view/8110/8601", BASE_URL, JOURNAL_ID));
-    
-    articleUrls.add(String.format("%sindex.php/%s/article/view/8220", BASE_URL, JOURNAL_ID));
-    articleUrls.add(String.format("%sindex.php/%s/article/view/8220/8390", BASE_URL, JOURNAL_ID));
-    articleUrls.add(String.format("%sindex.php/%s/article/view/8220/8391", BASE_URL, JOURNAL_ID));
-    
-     // Store test cases - articleUrls
+    // Store test cases - articleUrls
     Iterator<String> itr = articleUrls.iterator();
     while (itr.hasNext()) {
       String url = itr.next();
+      log.info("testCreateArticleFiles() url: " + url);
       UrlCacher urlca = au.makeUrlCacher(url);
       storeTestContent(url, urlca);
-    } // while
+    }
     
-    // Access OJS2ArticleItrerator
-    accessOJS2ArticleIterator(au);
-       
-  } // testCreateArticleFiles
+    // access OJS2ArticleItrerator
+    Iterator<ArticleFiles> it = au.getArticleIterator();
+    while (it.hasNext()) {
+      ArticleFiles af1 = it.next();
+      log.info("article file af1: " + af1.toString());
+      
+      // assert article 1
+      String[] actualUrls1 = { af1.getRoleUrl(ArticleFiles.ROLE_ABSTRACT),
+                               af1.getRoleUrl(ArticleFiles.ROLE_ARTICLE_METADATA),
+                               af1.getFullTextUrl(),                                                                      
+                               af1.getRoleUrl(ArticleFiles.ROLE_FULL_TEXT_HTML),
+                               af1.getRoleUrl(ArticleFiles.ROLE_FULL_TEXT_PDF) };
+
+      for (int i = 0;i< actualUrls1.length; i++) {
+        log.info("expected url1: " + expectedUrls1[i]);
+        log.info("actual url1: " + actualUrls1[i]);
+        assertEquals(expectedUrls1[i], actualUrls1[i]);
+      }
+      
+      // assert article 2 - with frame src
+      if (it.hasNext()) {
+        ArticleFiles af2 = it.next();
+        log.info("article file af2: " + af2.toString());
+
+        String[] actualUrls2 = { af2.getRoleUrl(ArticleFiles.ROLE_ABSTRACT),
+                                 af2.getRoleUrl(ArticleFiles.ROLE_ARTICLE_METADATA),
+                                 af2.getFullTextUrl(),                                                                      
+                                 af2.getRoleUrl(ArticleFiles.ROLE_FULL_TEXT_HTML),
+                                 af2.getRoleUrl(ArticleFiles.ROLE_FULL_TEXT_PDF) };
+
+        for (int i = 0;i< actualUrls2.length; i++) {
+          log.info("expected url2: " + expectedUrls2[i]);
+          log.info("actual url2: " + actualUrls2[i]);
+          assertEquals(expectedUrls2[i], actualUrls2[i]);
+        }
+      }
+    }
+
+  }
     
-  // Response header to be stored with the simulated content URLs
+  // Response header to be stored with the simulated HTML content URLs
   // Note the X-Locks-content-type is necessary (not Content-type)
   // in order to be found by the LOCKSS Daemon. 
-  private CIProperties getAbsProperties() {
-    
-    CIProperties absProps = new CIProperties();
-    absProps.put("RESPONSE","HTTP/1.0 200 OK");
-    absProps.put("Date", "Fri, 06 Apr 2012 18:22:49 GMT");
-    absProps.put("Server", "Apache/2.2.3 (CentOS)");
-    absProps.put("X-Powered-By", "PHP/5.2.17");
-    absProps.put("Expires", "Thu, 19 Nov 1981 08:52:00 GMT");
-    absProps.put("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
-    absProps.put("Pragma", "no-cache");
-    absProps.put("X-Lockss-content-type", "text/html; charset=UTF-8");
-    absProps.put("X-Cache", "MISS from lockss.org");
-    absProps.put("X-Cache-Lookup", "MISS from lockss.org:8888");
-    absProps.put("Via", "1.1 lockss.org:8888 (squid/2.7.STABLE7)");
-    absProps.put("Connection", "close");
-    
-    return absProps;
-    
-  } // getAbsProperties
+  private CIProperties getHtmlProperties() {
+    CIProperties htmlProps = new CIProperties();
+    htmlProps.put("RESPONSE","HTTP/1.0 200 OK");
+    htmlProps.put("Date", "Fri, 06 Apr 2012 18:22:49 GMT");
+    htmlProps.put("Server", "Apache/2.2.3 (CentOS)");
+    htmlProps.put("X-Powered-By", "PHP/5.2.17");
+    htmlProps.put("Expires", "Thu, 19 Nov 1981 08:52:00 GMT");
+    htmlProps.put("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
+    htmlProps.put("Pragma", "no-cache");
+    htmlProps.put("X-Lockss-content-type", "text/html; charset=UTF-8");
+    htmlProps.put("X-Cache", "MISS from lockss.org");
+    htmlProps.put("X-Cache-Lookup", "MISS from lockss.org:8888");
+    htmlProps.put("Via", "1.1 lockss.org:8888 (squid/2.7.STABLE7)");
+    htmlProps.put("Connection", "close");
+    return htmlProps;
+  }
+  
+  // Response header to be stored with the simulated PDF content URLs
+  // Note the X-Locks-content-type is necessary (not Content-type)
+  // in order to be found by the LOCKSS Daemon. 
+  private CIProperties getPdfProperties() {
+    CIProperties pdfProps = new CIProperties();
+    pdfProps.put("RESPONSE","HTTP/1.0 200 OK");
+    pdfProps.put("Date", "Fri, 06 Apr 2012 18:22:49 GMT");
+    pdfProps.put("Server", "Apache/2.2.3 (CentOS)");
+    pdfProps.put("X-Powered-By", "PHP/5.2.17");
+    pdfProps.put("Expires", "Thu, 19 Nov 1981 08:52:00 GMT");
+    pdfProps.put("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
+    pdfProps.put("Pragma", "no-cache");
+    pdfProps.put("X-Lockss-content-type", "application/pdf; charset=UTF-8");
+    pdfProps.put("X-Cache", "MISS from lockss.org");
+    pdfProps.put("X-Cache-Lookup", "MISS from lockss.org:8888");
+    pdfProps.put("Via", "1.1 lockss.org:8888 (squid/2.7.STABLE7)");
+    pdfProps.put("Connection", "close");
+    return pdfProps;
+  }
 
   // Read the test TOC file test_toc_junit.html from current directory
   // Prepare input stream for UrlCacher storeContent() method
-    private InputStream getTestTocInputStream(String url) throws IOException {
-  
+  private InputStream getTestTocInputStream() throws IOException {
     InputStream htmlIn = getClass().getResourceAsStream("test_toc_junit.html");
     String absHtml = StringUtil.fromInputStream(htmlIn);
-        
     return IOUtils.toInputStream(absHtml);
-        
-  } // getTestTocInputStream
+  }
    
-} // TestOJS2ArticleIteratorFactory
+}
