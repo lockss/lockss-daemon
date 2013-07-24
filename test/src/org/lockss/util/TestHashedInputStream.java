fba4c5b57a9885d355c4d09b1e07af5d479b6521
tlipkis <tal@pss.com>
2013-07-24 19:02:56 +0000

    Removed embedded BufferedInputStream. Mark hash valid iff all bytes have been read from underlying stream. HashedInputStream.Hasher encapsulates MessageDigest and valid flag,  replaces MessageDigest arg to CachedUrl, BlockHasher, HashedInputStream. Aded tests for isValid(), removed tests for BufferedInputStream.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@28995 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/util/TestHashedInputStream.java
+++ test/src/org/lockss/util/TestHashedInputStream.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestHashedInputStream.java,v 1.2 2013-07-22 18:07:40 dshr Exp $
+ * $Id: TestHashedInputStream.java,v 1.3 2013-07-24 19:02:56 tlipkis Exp $
  */
 
 /*
@@ -33,15 +33,11 @@ in this Software without prior written authorization from Stanford University.
 package org.lockss.util;
 import java.io.*;
 import java.security.*;
+import org.apache.commons.lang.*;
 import org.lockss.test.*;
 
 public class TestHashedInputStream extends LockssTestCase {
 
-  /*
-   * NB - the specification of HashedInputStream is that it
-   * hashes the entire content of the unerlying stream whether
-   * or not the client reads it.
-   */
   private byte[] emptyStream = new byte[0];
   private byte[] fullStream = new byte[77];
 
@@ -62,15 +58,16 @@ public class TestHashedInputStream extends LockssTestCase {
 
   public void testNullArgumentsToConstructor() {
     MessageDigest md = makeMessageDigest();
+    HashedInputStream.Hasher h = new HashedInputStream.Hasher(md);
     InputStream inp = makeEmptyStream();
     try {
-      HashedInputStream is = new HashedInputStream(null, md);
+      HashedInputStream is = new HashedInputStream(null, h);
       fail("Calling the constructor with a null InputStream should have thrown");
     } catch (IllegalArgumentException e) {
     }
     try {
       HashedInputStream is = new HashedInputStream(inp, null);
-      fail("Calling the constructor with a null InputStream should have thrown");
+      fail("Calling the constructor with a null Hasher should have thrown");
     } catch (IllegalArgumentException e) {
     }
   }
@@ -78,81 +75,85 @@ public class TestHashedInputStream extends LockssTestCase {
   public void testEmptyStream() throws IOException {
     InputStream inp = makeEmptyStream();
     MockMessageDigest md = makeMessageDigest();
-    HashedInputStream is = new HashedInputStream(inp, md);
+    HashedInputStream.Hasher hasher = new HashedInputStream.Hasher(md);
+    HashedInputStream is = new HashedInputStream(inp, hasher);
+    assertFalse(hasher.isValid());
     assertEquals(-1, is.read());
+    assertEquals(0, md.getUpdatedBytes().length);
+    assertTrue(hasher.isValid());
     is.close();
     assertEquals(0, md.getUpdatedBytes().length);
+    assertTrue(hasher.isValid());
   }
 
   public void testFullStreamReadOne() throws IOException {
     InputStream inp = makeFullStream();
     MockMessageDigest md = makeMessageDigest();
-    HashedInputStream is = new HashedInputStream(inp, md);
+    HashedInputStream.Hasher hasher = new HashedInputStream.Hasher(md);
+    HashedInputStream is = new HashedInputStream(inp, hasher);
+    assertFalse(hasher.isValid());
     assertEquals(fullStream[0], is.read());
+    assertFalse(hasher.isValid());
     byte[] t = new byte[1];
     t[0] = fullStream[0];
+    assertEquals(t, md.getUpdatedBytes());
+    assertFalse(hasher.isValid());
     is.close();
-    assertEquals(fullStream, md.getUpdatedBytes());
+    assertFalse(hasher.isValid());
   }
 
   public void testFullStreamReadBuff() throws IOException {
     InputStream inp = makeFullStream();
     MockMessageDigest md = makeMessageDigest();
-    HashedInputStream is = new HashedInputStream(inp, md);
+    HashedInputStream.Hasher hasher = new HashedInputStream.Hasher(md);
+    HashedInputStream is = new HashedInputStream(inp, hasher);
     byte[] t = new byte[fullStream.length];
-    assertEquals(fullStream.length, is.read(t));
+    assertFalse(hasher.isValid());
+    assertEquals(fullStream.length, StreamUtil.readBytes(is, t, t.length));
     assertEquals(fullStream, t);
+    assertEquals(t, md.getUpdatedBytes());
     is.close();
-    assertEquals(fullStream, md.getUpdatedBytes());
+    // have read up to but not including signaling EOF.  close() should
+    // figure out that whole stream was read
+    assertTrue(hasher.isValid());
+  }
+
+  public void testReadToEof() throws IOException {
+    InputStream inp = makeFullStream();
+    MockMessageDigest md = makeMessageDigest();
+    HashedInputStream.Hasher hasher = new HashedInputStream.Hasher(md);
+    HashedInputStream is = new HashedInputStream(inp, hasher);
+    byte[] t = new byte[fullStream.length + 10];
+    assertFalse(hasher.isValid());
+    int n = StreamUtil.readBytes(is, t, t.length);
+    assertEquals(fullStream.length, n);
+    assertNotEquals(t.length, n);
+    assertTrue(hasher.isValid());
+    byte[] subt = ArrayUtils.subarray(t, 0, fullStream.length);
+    assertEquals(fullStream, subt);
+    assertEquals(subt, md.getUpdatedBytes());
+    is.close();
+    assertTrue(hasher.isValid());
   }
 
   public void testFullStreamReadOffset() throws IOException {
     InputStream inp = makeFullStream();
     MockMessageDigest md = makeMessageDigest();
-    HashedInputStream is = new HashedInputStream(inp, md);
+    HashedInputStream.Hasher hasher = new HashedInputStream.Hasher(md);
+    HashedInputStream is = new HashedInputStream(inp, hasher);
     byte[] t = new byte[fullStream.length];
     int off = 11;
     int len = 33;
     assertEquals(len, is.read(t, off, len));
+    assertFalse(hasher.isValid());
     byte[] p = new byte[len];
     for (int i = 0; i < len; i++) {
       assertEquals(t[i+off], fullStream[i]);
       p[i] = t[i + off];
     }
+    assertFalse(hasher.isValid());
+    assertEquals(p, md.getUpdatedBytes());
     is.close();
-    assertEquals(fullStream, md.getUpdatedBytes());
-  }
-
-  public void testMarkReset() throws IOException {
-    InputStream inp = makeFullStream();
-    MockMessageDigest md = makeMessageDigest();
-    HashedInputStream is = new HashedInputStream(inp, md);
-    byte[] t = new byte[11];
-    int len = t.length;
-    byte[] p = new byte[len*3];
-    for (int i = 0; i < len; i++) {
-      // We read len bytes, then call mark()
-      p[i] = fullStream[i];
-      // We read len bytes, then call reset()
-      p[i+len] = fullStream[i+len];
-      // We read len bytes
-      p[i+2*len] = fullStream[i+len];
-    }
-    assertEquals(len, is.read(t));
-    for (int i = 0; i < len; i++) {
-      assertEquals(t[i], p[i]);
-    }
-    is.mark(2*len);
-    assertEquals(len, is.read(t));
-    for (int i = 0; i < len; i++) {
-      assertEquals(t[i], p[i+len]);
-    }
-    is.reset();
-    assertEquals(len, is.read(t));
-    for (int i = 0; i < len; i++) {
-      assertEquals(t[i], p[i+2*len]);
-    }
-    is.close();
-    assertEquals(fullStream, md.getUpdatedBytes());
+    assertFalse(hasher.isValid());
   }
 }
