a2bdc056394c9259b29e9c234d59f1c50857c81f
alexandraohlson <alexandra@slgroup.com>
2013-09-11 15:51:22 +0000

    Add url normalizer as fix for RU#4408 (Jira PD-56) removing "?message=remove" suffix. Bump version/poll. Add test
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@29779 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/nature/NaturePublishingGroupUrlNormalizer.java
@@ -0,0 +1,53 @@
+/*
+ * $Id: NaturePublishingGroupUrlNormalizer.java,v 1.1 2013-09-11 15:51:22 alexandraohlson Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.nature;
+
+import org.apache.commons.lang.StringUtils;
+import org.lockss.daemon.PluginException;
+import org.lockss.plugin.*;
+import org.lockss.util.Logger;
+
+
+public class NaturePublishingGroupUrlNormalizer implements UrlNormalizer {
+  protected static Logger log = 
+      Logger.getLogger("NaturePublishingGroupUrlNormalizer");  
+  
+  private static final String MESSAGE_ENDING = "?message=remove";  
+
+  public String normalizeUrl(String url,
+                             ArchivalUnit au)
+      throws PluginException {
+    return StringUtils.chomp(url, MESSAGE_ENDING);
+  }
+
+}
