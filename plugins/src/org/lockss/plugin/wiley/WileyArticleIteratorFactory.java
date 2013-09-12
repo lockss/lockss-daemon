0afdb65d1b228f44769cca440c0826f3ba2b049e
lytdoan <ldoan@stanford.edu>
2013-09-12 20:01:36 +0000

    Made changes after code review. Removed ROOT_TEMPLATE since for all content falls under <baes_url>/<year>. Moved setVisitArchiveMembers to be in WileyArticleIterator argument list. Modified processFullText() and guessAdditionalFiles to handle inconsistent content file names. Related issue4873
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@29805 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/wiley/WileyArticleIteratorFactory.java
+++ plugins/src/org/lockss/plugin/wiley/WileyArticleIteratorFactory.java
@@ -43,76 +43,122 @@ import org.lockss.extractor.MetadataTarget;
 import org.lockss.plugin.*;
 import org.lockss.util.Logger;
 
+/*
+ * Iterates article files.  Archived source content zip files include files
+ * with mime-type pdf and xml. Content has inconstitent file name formats.
+ * 
+ * Full-text:
+ * http://clockss-ingest.lockss.org/sourcefiles/wiley-released
+ *                      /2012/A/AAB102.1.zip!/j.1744-7348.1983.tb02660.x.pdf
+ * http://clockss-ingest.lockss.org/sourcefiles/wiley-released
+ *                      /2011/A/ADMA23.16!/1810_ftp.pdf
+ * 
+ * Cover image/ Abstract:
+ * http://clockss-ingest.lockss.org/sourcefiles/wiley-released
+ *                      /2011/A/1803_ftp.pdf
+ * http://clockss-ingest.lockss.org/sourcefiles/wiley-released
+ *                      /2011/A/1803_hdp.wml.xml
+ */
 public class WileyArticleIteratorFactory 
   implements ArticleIteratorFactory, ArticleMetadataExtractorFactory {
 
-  protected static Logger log = Logger.getLogger("WileyArticleIteratorFactory");
+  protected static Logger log = 
+                          Logger.getLogger("WileyArticleIteratorFactory");
   
-  protected static final String ROOT_TEMPLATE = "\"%s%d\",base_url,year";
-  
-  protected static final String PATTERN_TEMPLATE = "\"%s%d/[A-Z0-9]/[^/]+\\.zip!/.*\\.pdf$\",base_url,year";
-  
+  // no need to set ROOT_TEMPLATE since all content is under <base_url>/<year>
+  protected static final String PATTERN_TEMPLATE = 
+      "\"%s%d/[A-Z0-9]/[^/]+\\.zip!/.*\\.pdf$\",base_url,year";
+     
   @Override
   public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au,
                                                       MetadataTarget target)
       throws PluginException {
-    return new WileyArticleIterator(
-        au, new SubTreeArticleIterator.Spec()
-              .setTarget(target)
-              .setRootTemplate(ROOT_TEMPLATE)
-              .setPatternTemplate(PATTERN_TEMPLATE, Pattern.CASE_INSENSITIVE));
+    
+    return new WileyArticleIterator(au, 
+                                    new SubTreeArticleIterator.Spec()
+                                        .setTarget(target)
+                                        .setVisitArchiveMembers(true)
+                                        .setPatternTemplate(PATTERN_TEMPLATE, 
+                                                      Pattern.CASE_INSENSITIVE)
+                                    );
   }
   
   protected static class WileyArticleIterator extends SubTreeArticleIterator {
 	 
-    protected final static Pattern PATTERN = 
+    protected final static Pattern PDF_PATTERN = 
       Pattern.compile("(/[^/]+\\.zip!/.*)(\\.pdf)$", Pattern.CASE_INSENSITIVE);
     
+    protected final static Pattern FTP_PDF_PATTERN = 
+      Pattern.compile("(.*)_ftp\\.pdf$", Pattern.CASE_INSENSITIVE);
+    
     protected WileyArticleIterator(ArchivalUnit au,
-                                  SubTreeArticleIterator.Spec spec) {
+                                   SubTreeArticleIterator.Spec spec) {
       super(au, spec);
-      spec.setVisitArchiveMembers(true);
     }
     
     @Override
     protected ArticleFiles createArticleFiles(CachedUrl cu) {
       String url = cu.getUrl();
-      
-      Matcher mat = PATTERN.matcher(url);
-      if (mat.find()) {
-        return processFullText(cu, mat);
+      Matcher matPdf = PDF_PATTERN.matcher(url);
+      if (matPdf.find()) {
+        return processFullText(cu, matPdf);
       }
-      log.warning("Mismatch between article iterator factory and article iterator: " + url);
+      log.warning("Url does not match PDF_PATTERN: " + url);
       return null;
     }
-
-    protected ArticleFiles processFullText(CachedUrl cu, Matcher mat) {
-      ArticleFiles af = new ArticleFiles();
-      af.setFullTextCu(cu);
+    
+    // if found pdfs with file name containing "*_ftp.pdf",
+    // then make cached url for its corresponding abstract "*_hdp.wml.xml"
+    private CachedUrl getHdpXmlCu(Matcher matFtpPdf) {
+       String hdpXml = matFtpPdf.replaceFirst("$1_hdp.wml.xml");
+       CachedUrl hdpXmlCu = au.makeCachedUrl(hdpXml);
+       return (hdpXmlCu);
+    }
+    
+    private void setRoleFullText(ArticleFiles af, CachedUrl cu) {
       af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_PDF, cu);
-      
-      if(spec.getTarget() != MetadataTarget.Article) {
-        guessAdditionalFiles(af, mat);
+      af.setFullTextCu(cu);
+    }
+
+    // pdfs are a mix of full-text (most) and cover image and/or abstract
+    // images. Abstract xml is recognized by its file name containing '_hdp'
+    // coupling with its *_ftp.pdf file.
+    protected ArticleFiles processFullText(CachedUrl cu, Matcher matPdf) {
+      ArticleFiles af = new ArticleFiles();
+      Matcher matFtpPdf = FTP_PDF_PATTERN.matcher(cu.getUrl());
+      if (!matFtpPdf.find()) {
+        setRoleFullText(af, cu);
+      } else {
+        CachedUrl hdpXmlCu = getHdpXmlCu(matFtpPdf);
+        // if hdp xml exists, then the pdf is a cover image
+        // and the hdp xml is an abstract and contains not <body> tag
+        if (hdpXmlCu.hasContent()) {
+          af.setRoleCu("Cover image", cu); // cover image pdf
+          af.setRoleCu(ArticleFiles.ROLE_ABSTRACT, hdpXmlCu);
+          af.setRoleCu(ArticleFiles.ROLE_ARTICLE_METADATA, hdpXmlCu);
+        } else {
+          setRoleFullText(af, cu);
+        }
+      } 
+      if (spec.getTarget() != MetadataTarget.Article()) {
+        guessAdditionalFiles(af, matPdf);
       }
-        
       return af;
     }
     
-    protected void guessAdditionalFiles(ArticleFiles af, Matcher mat) {
-      CachedUrl xmlCu = au.makeCachedUrl(mat.replaceFirst("$1.wml.xml"));
-      
-      if (xmlCu != null && xmlCu.hasContent()) {
-    	  af.setRoleCu(ArticleFiles.ROLE_ARTICLE_METADATA, xmlCu);
-      }
-      
-      if (xmlCu != null && xmlCu.hasContent()) {
-    	  af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_HTML, xmlCu);
+    // metadata found in all xmls (full-text and abstract)
+    protected void guessAdditionalFiles(ArticleFiles af, Matcher matPdf) {
+      CachedUrl xmlCu = au.makeCachedUrl(matPdf.replaceFirst("$1.wml.xml"));
+      if (xmlCu.hasContent()) {
+        af.setRoleCu(ArticleFiles.ROLE_ARTICLE_METADATA, xmlCu);
       }
     }
+    
   }
   
-  public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target)
-	      throws PluginException {
-	    return new BaseArticleMetadataExtractor(null);
+  public ArticleMetadataExtractor createArticleMetadataExtractor(
+                                                         MetadataTarget target)
+        throws PluginException {
+    return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
   }
 }
