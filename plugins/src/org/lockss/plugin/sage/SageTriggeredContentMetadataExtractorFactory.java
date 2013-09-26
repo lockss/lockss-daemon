09c138634e085520bc99dee597ee48641ce54696
etenbrink <etenbrink@yahoo.com>
2013-09-26 23:18:12 +0000

    Correct indentation, prior to making additional changes
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@30069 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/sage/SageTriggeredContentMetadataExtractorFactory.java
+++ plugins/src/org/lockss/plugin/sage/SageTriggeredContentMetadataExtractorFactory.java
@@ -1,5 +1,5 @@
 /*
- * $Id: SageTriggeredContentMetadataExtractorFactory.java,v 1.1 2012-04-27 21:49:31 dylanrhodes Exp $
+ * $Id: SageTriggeredContentMetadataExtractorFactory.java,v 1.2 2013-09-26 23:18:12 etenbrink Exp $
  */
 
 /*
@@ -55,136 +55,136 @@ import javax.xml.xpath.XPathExpressionException;
  */
 
 public class SageTriggeredContentMetadataExtractorFactory
-  implements FileMetadataExtractorFactory {
+implements FileMetadataExtractorFactory {
   static Logger log = Logger.getLogger("SageTriggeredContentMetadataExtractorFactory");
-
+  
   public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
-							   String contentType)
-      throws PluginException {
+      String contentType)
+          throws PluginException {
     return new SageXmlMetadataExtractor();
   }
-
+  
   public static class SageXmlMetadataExtractor 
-    implements FileMetadataExtractor {
-	  
-	  /** NodeValue for creating value of subfields from author tag */
-	    static private final NodeValue AUTHOR_VALUE = new NodeValue() {
-	      @Override
-	      public String getValue(Node node) {
-	        if (node == null) {
-	          return null;
-	        }
-	        
-	        NodeList nameNodes = node.getChildNodes();
-	        String fname = null, mname = null, sname = null;
-	        for (int k = 0; k < nameNodes.getLength(); k++) {
-	          Node nameNode = nameNodes.item(k);
-	          if (nameNode.getNodeName().equals("fn")) {
-	            fname = nameNode.getTextContent();
-	          } else if (nameNode.getNodeName().equals("ln")) {
-	            sname = nameNode.getTextContent();
-	          } else if (nameNode.getNodeName().equals("mn")) {
-	            mname = nameNode.getTextContent();
-	          }
-	        }
-	        // return name as [surname], [firstname] [middlename]
-	        return sname + ", " + fname + ((mname == null) ? "" : " " + mname);
-	      }
-	    };
-	    
-	    /** NodeValue for creating value of subfields from date tag */
-	    static private final NodeValue DATE_VALUE = new NodeValue() {
-	      @Override
-	      public String getValue(Node node) {
-	        if (node == null) {
-	          return null;
-	        }
-	        
-	        NodeList dateNodes = node.getChildNodes();
-	        String year = "", month = null;
-	        for (int k = 0; k < dateNodes.getLength(); k++) {
-	          Node dateNode = dateNodes.item(k);
-	          if (dateNode.getNodeName().equals("yy")) {
-	            year = dateNode.getTextContent();
-	          } else if (dateNode.getNodeName().equals("mm")) {
-	            month = dateNode.getTextContent();
-	          }
-	        }
-	        // return name as [month]/[year]
-	        return ((month == null) ? "" : month + "/") + year;
-	      }
-	    };
-	    
-	    /** Map of raw xpath key to node value function */
-	    static private final Map<String,XPathValue> nodeMap = 
-	        new HashMap<String,XPathValue>();
-	    static {
-	      // normal journal article schema
-	      nodeMap.put("/SAGEmeta/@doi", XmlDomMetadataExtractor.TEXT_VALUE);
-	      nodeMap.put("/SAGEmeta/header/jrn_info/jrn_title", XmlDomMetadataExtractor.TEXT_VALUE);
-	      nodeMap.put("/SAGEmeta/header/jrn_info/ISSN", XmlDomMetadataExtractor.TEXT_VALUE);
-	      nodeMap.put("/SAGEmeta/header/jrn_info/vol", XmlDomMetadataExtractor.TEXT_VALUE);
-	      nodeMap.put("/SAGEmeta/header/jrn_info/iss", XmlDomMetadataExtractor.TEXT_VALUE);
-	      nodeMap.put("/SAGEmeta/header/jrn_info/date", DATE_VALUE);
-	      nodeMap.put("/SAGEmeta/header/jrn_info/pub_info/pub_name", XmlDomMetadataExtractor.TEXT_VALUE);
-	      nodeMap.put("/SAGEmeta/header/art_info/art_title", XmlDomMetadataExtractor.TEXT_VALUE);
-	      nodeMap.put("/SAGEmeta/header/art_info/art_author/per_aut", AUTHOR_VALUE);
-	      nodeMap.put("/SAGEmeta/header/art_info/spn", XmlDomMetadataExtractor.TEXT_VALUE);
-	      nodeMap.put("/SAGEmeta/header/art_info/epn", XmlDomMetadataExtractor.TEXT_VALUE);
-	    }
-
-	    /** Map of raw xpath key to cooked MetadataField */
-	    static private final MultiValueMap xpathMap = new MultiValueMap();
-	    static {
-	      // normal journal article schema
-	      xpathMap.put("/SAGEmeta/@doi", MetadataField.FIELD_DOI);
-		  xpathMap.put("/SAGEmeta/header/jrn_info/jrn_title", MetadataField.FIELD_JOURNAL_TITLE);
-		  xpathMap.put("/SAGEmeta/header/jrn_info/ISSN", MetadataField.FIELD_ISSN);
-		  xpathMap.put("/SAGEmeta/header/jrn_info/vol", MetadataField.FIELD_VOLUME);
-		  xpathMap.put("/SAGEmeta/header/jrn_info/iss", MetadataField.FIELD_ISSUE);
-		  xpathMap.put("/SAGEmeta/header/jrn_info/date", MetadataField.FIELD_DATE);
-		  xpathMap.put("/SAGEmeta/header/jrn_info/pub_info/pub_name", MetadataField.FIELD_PUBLISHER);
-		  xpathMap.put("/SAGEmeta/header/art_info/art_title", MetadataField.FIELD_ARTICLE_TITLE);
-		  xpathMap.put("/SAGEmeta/header/art_info/art_author/per_aut", MetadataField.FIELD_AUTHOR);
-		  xpathMap.put("/SAGEmeta/header/art_info/spn", MetadataField.FIELD_START_PAGE);
-		  xpathMap.put("/SAGEmeta/header/art_info/epn", MetadataField.FIELD_END_PAGE);
-	    }
-
-	    /**
-	     * Use XmlMetadataExtractor to extract raw metadata, map
-	     * to cooked fields, then extract extra tags by reading the file.
-	     * 
-	     * @param target the MetadataTarget
-	     * @param cu the CachedUrl from which to read input
-	     * @param emitter the emiter to output the resulting ArticleMetadata
-	     */
-	    @Override
-	    public void extract(MetadataTarget target, CachedUrl cu, Emitter emitter)
-	        throws IOException, PluginException {
-	      ArticleMetadata am = do_extract(target, cu, emitter);
-	      emitter.emitMetadata(cu,  am);
-	    }
-
-	    /**
-	       * Use XmlMetadataExtractor to extract raw metadata, map
-	       * to cooked fields, then extract extra tags by reading the file.
-	       * 
-	       * @param target the MetadataTarget
-	       * @param in the Xml input stream to parse
-	       * @param emitter the emitter to output the resulting ArticleMetadata
-	       */
-	      public ArticleMetadata do_extract(MetadataTarget target, CachedUrl cu, Emitter emit)
-	          throws IOException, PluginException {
-	        try {
-	          ArticleMetadata am = 
-	            new XmlDomMetadataExtractor(nodeMap).extract(target, cu);
-	          am.cook(xpathMap);
-	          return am;
-	        } catch (XPathExpressionException ex) {
-	          PluginException ex2 = new PluginException("Error parsing XPaths");
-	          ex2.initCause(ex);
-	          throw ex2;
-	        }
-	      }
-	  }
+  implements FileMetadataExtractor {
+    
+    /** NodeValue for creating value of subfields from author tag */
+    static private final NodeValue AUTHOR_VALUE = new NodeValue() {
+      @Override
+      public String getValue(Node node) {
+        if (node == null) {
+          return null;
+        }
+        
+        NodeList nameNodes = node.getChildNodes();
+        String fname = null, mname = null, sname = null;
+        for (int k = 0; k < nameNodes.getLength(); k++) {
+          Node nameNode = nameNodes.item(k);
+          if (nameNode.getNodeName().equals("fn")) {
+            fname = nameNode.getTextContent();
+          } else if (nameNode.getNodeName().equals("ln")) {
+            sname = nameNode.getTextContent();
+          } else if (nameNode.getNodeName().equals("mn")) {
+            mname = nameNode.getTextContent();
+          }
+        }
+        // return name as [surname], [firstname] [middlename]
+        return sname + ", " + fname + ((mname == null) ? "" : " " + mname);
+      }
+    };
+    
+    /** NodeValue for creating value of subfields from date tag */
+    static private final NodeValue DATE_VALUE = new NodeValue() {
+      @Override
+      public String getValue(Node node) {
+        if (node == null) {
+          return null;
+        }
+        
+        NodeList dateNodes = node.getChildNodes();
+        String year = "", month = null;
+        for (int k = 0; k < dateNodes.getLength(); k++) {
+          Node dateNode = dateNodes.item(k);
+          if (dateNode.getNodeName().equals("yy")) {
+            year = dateNode.getTextContent();
+          } else if (dateNode.getNodeName().equals("mm")) {
+            month = dateNode.getTextContent();
+          }
+        }
+        // return name as [month]/[year]
+        return ((month == null) ? "" : month + "/") + year;
+      }
+    };
+    
+    /** Map of raw xpath key to node value function */
+    static private final Map<String,XPathValue> nodeMap = 
+        new HashMap<String,XPathValue>();
+    static {
+      // normal journal article schema
+      nodeMap.put("/SAGEmeta/@doi", XmlDomMetadataExtractor.TEXT_VALUE);
+      nodeMap.put("/SAGEmeta/header/jrn_info/jrn_title", XmlDomMetadataExtractor.TEXT_VALUE);
+      nodeMap.put("/SAGEmeta/header/jrn_info/ISSN", XmlDomMetadataExtractor.TEXT_VALUE);
+      nodeMap.put("/SAGEmeta/header/jrn_info/vol", XmlDomMetadataExtractor.TEXT_VALUE);
+      nodeMap.put("/SAGEmeta/header/jrn_info/iss", XmlDomMetadataExtractor.TEXT_VALUE);
+      nodeMap.put("/SAGEmeta/header/jrn_info/date", DATE_VALUE);
+      nodeMap.put("/SAGEmeta/header/jrn_info/pub_info/pub_name", XmlDomMetadataExtractor.TEXT_VALUE);
+      nodeMap.put("/SAGEmeta/header/art_info/art_title", XmlDomMetadataExtractor.TEXT_VALUE);
+      nodeMap.put("/SAGEmeta/header/art_info/art_author/per_aut", AUTHOR_VALUE);
+      nodeMap.put("/SAGEmeta/header/art_info/spn", XmlDomMetadataExtractor.TEXT_VALUE);
+      nodeMap.put("/SAGEmeta/header/art_info/epn", XmlDomMetadataExtractor.TEXT_VALUE);
+    }
+    
+    /** Map of raw xpath key to cooked MetadataField */
+    static private final MultiValueMap xpathMap = new MultiValueMap();
+    static {
+      // normal journal article schema
+      xpathMap.put("/SAGEmeta/@doi", MetadataField.FIELD_DOI);
+      xpathMap.put("/SAGEmeta/header/jrn_info/jrn_title", MetadataField.FIELD_JOURNAL_TITLE);
+      xpathMap.put("/SAGEmeta/header/jrn_info/ISSN", MetadataField.FIELD_ISSN);
+      xpathMap.put("/SAGEmeta/header/jrn_info/vol", MetadataField.FIELD_VOLUME);
+      xpathMap.put("/SAGEmeta/header/jrn_info/iss", MetadataField.FIELD_ISSUE);
+      xpathMap.put("/SAGEmeta/header/jrn_info/date", MetadataField.FIELD_DATE);
+      xpathMap.put("/SAGEmeta/header/jrn_info/pub_info/pub_name", MetadataField.FIELD_PUBLISHER);
+      xpathMap.put("/SAGEmeta/header/art_info/art_title", MetadataField.FIELD_ARTICLE_TITLE);
+      xpathMap.put("/SAGEmeta/header/art_info/art_author/per_aut", MetadataField.FIELD_AUTHOR);
+      xpathMap.put("/SAGEmeta/header/art_info/spn", MetadataField.FIELD_START_PAGE);
+      xpathMap.put("/SAGEmeta/header/art_info/epn", MetadataField.FIELD_END_PAGE);
+    }
+    
+    /**
+     * Use XmlMetadataExtractor to extract raw metadata, map
+     * to cooked fields, then extract extra tags by reading the file.
+     * 
+     * @param target the MetadataTarget
+     * @param cu the CachedUrl from which to read input
+     * @param emitter the emiter to output the resulting ArticleMetadata
+     */
+    @Override
+    public void extract(MetadataTarget target, CachedUrl cu, Emitter emitter)
+        throws IOException, PluginException {
+      ArticleMetadata am = do_extract(target, cu, emitter);
+      emitter.emitMetadata(cu,  am);
+    }
+    
+    /**
+     * Use XmlMetadataExtractor to extract raw metadata, map
+     * to cooked fields, then extract extra tags by reading the file.
+     * 
+     * @param target the MetadataTarget
+     * @param in the Xml input stream to parse
+     * @param emitter the emitter to output the resulting ArticleMetadata
+     */
+    public ArticleMetadata do_extract(MetadataTarget target, CachedUrl cu, Emitter emit)
+        throws IOException, PluginException {
+      try {
+        ArticleMetadata am = 
+            new XmlDomMetadataExtractor(nodeMap).extract(target, cu);
+        am.cook(xpathMap);
+        return am;
+      } catch (XPathExpressionException ex) {
+        PluginException ex2 = new PluginException("Error parsing XPaths");
+        ex2.initCause(ex);
+        throw ex2;
+      }
+    }
+  }
 }
