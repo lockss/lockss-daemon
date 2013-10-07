92c2ddc101e2d8983312d779ed22576f82b9d56b
etenbrink <etenbrink@yahoo.com>
2013-10-07 18:05:04 +0000

    Correct indentation
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@30272 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/test/src/org/lockss/plugin/iumj/TestIUMJArticleIteratorFactory.java
+++ plugins/test/src/org/lockss/plugin/iumj/TestIUMJArticleIteratorFactory.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestIUMJArticleIteratorFactory.java,v 1.2 2013-10-03 17:37:21 etenbrink Exp $
+ * $Id: TestIUMJArticleIteratorFactory.java,v 1.3 2013-10-07 18:05:04 etenbrink Exp $
  */
 
 /*
@@ -47,65 +47,65 @@ import org.lockss.test.*;
 import org.lockss.util.*;
 
 public class TestIUMJArticleIteratorFactory extends ArticleIteratorTestCase {
-        
-        private SimulatedArchivalUnit sau;      // Simulated AU to generate content
-        
-        private final String PLUGIN_NAME = "org.lockss.plugin.iumj.IUMJPlugin";
-        static final String BASE_URL_KEY = ConfigParamDescr.BASE_URL.getKey();
-        static final String YEAR_KEY = ConfigParamDescr.YEAR.getKey();
-        static final String VOLUME_NAME_KEY = ConfigParamDescr.VOLUME_NAME.getKey();
-        private final String BASE_URL = "http://www.example.net/";
-        private final String VOLUME_NAME = "55";
-        private final String YEAR = "2006";
-        private final String CRAWLRULE0 = String.format("IUMJ/(YEAR|ABS|FULLTEXT|FTDLOAD)/%s/", YEAR, VOLUME_NAME);
-        private final String CRAWLRULE1 = String.format("IUMJ/FTDLOAD/%s/%s/([^/])+/pdf", YEAR, VOLUME_NAME);
-        private final Configuration AU_CONFIG = ConfigurationUtil.fromArgs(
-                                                BASE_URL_KEY, BASE_URL,
-                                                VOLUME_NAME_KEY, VOLUME_NAME,
-                                                YEAR_KEY, YEAR);
-        private static final int DEFAULT_FILESIZE = 3000;
-
-        protected String cuRole = null;
-        ArticleMetadataExtractor.Emitter emitter;
-        protected boolean emitDefaultIfNone = false;
-        FileMetadataExtractor me = null; 
-        MetadataTarget target;
-        private static Pattern regexpattern;
-
+  
+  private SimulatedArchivalUnit sau;      // Simulated AU to generate content
+  
+  private final String PLUGIN_NAME = "org.lockss.plugin.iumj.IUMJPlugin";
+  static final String BASE_URL_KEY = ConfigParamDescr.BASE_URL.getKey();
+  static final String YEAR_KEY = ConfigParamDescr.YEAR.getKey();
+  static final String VOLUME_NAME_KEY = ConfigParamDescr.VOLUME_NAME.getKey();
+  private final String BASE_URL = "http://www.example.net/";
+  private final String VOLUME_NAME = "55";
+  private final String YEAR = "2006";
+  private final String CRAWLRULE0 = String.format("IUMJ/(YEAR|ABS|FULLTEXT|FTDLOAD)/%s/", YEAR, VOLUME_NAME);
+  private final String CRAWLRULE1 = String.format("IUMJ/FTDLOAD/%s/%s/([^/])+/pdf", YEAR, VOLUME_NAME);
+  private final Configuration AU_CONFIG = ConfigurationUtil.fromArgs(
+      BASE_URL_KEY, BASE_URL,
+      VOLUME_NAME_KEY, VOLUME_NAME,
+      YEAR_KEY, YEAR);
+  private static final int DEFAULT_FILESIZE = 3000;
+  
+  protected String cuRole = null;
+  ArticleMetadataExtractor.Emitter emitter;
+  protected boolean emitDefaultIfNone = false;
+  FileMetadataExtractor me = null; 
+  MetadataTarget target;
+  private static Pattern regexpattern;
+  
   public void setUp() throws Exception {
     super.setUp();
     String tempDirPath = setUpDiskSpace();
-
+    
     au = createAu();
     sau = PluginTestUtil.createAndStartSimAu(simAuConfig(tempDirPath));
   }
   
   public void tearDown() throws Exception {
-            sau.deleteContentTree();
-            super.tearDown();
-          }
-
+    sau.deleteContentTree();
+    super.tearDown();
+  }
+  
   protected ArchivalUnit createAu() throws ArchivalUnit.ConfigurationException {
     return
       PluginTestUtil.createAndStartAu(PLUGIN_NAME,  AU_CONFIG);
   }
   
   Configuration simAuConfig(String rootPath) {
-            Configuration conf = ConfigManager.newConfiguration();
-            conf.put("root", rootPath);
-            conf.put(BASE_URL_KEY, BASE_URL);
-            conf.put("depth", "1");
-            conf.put("branch", "4");
-            conf.put("numFiles", "5");
-            conf.put("fileTypes",
-                     "" + (SimulatedContentGenerator.FILE_TYPE_HTML |
-                         SimulatedContentGenerator.FILE_TYPE_PDF | 
-                         SimulatedContentGenerator.FILE_TYPE_TXT));
-            conf.put("binFileSize", ""+DEFAULT_FILESIZE);
-            return conf;
-          }
+    Configuration conf = ConfigManager.newConfiguration();
+    conf.put("root", rootPath);
+    conf.put(BASE_URL_KEY, BASE_URL);
+    conf.put("depth", "1");
+    conf.put("branch", "4");
+    conf.put("numFiles", "5");
+    conf.put("fileTypes", "" + (
+        SimulatedContentGenerator.FILE_TYPE_HTML |
+        SimulatedContentGenerator.FILE_TYPE_PDF | 
+        SimulatedContentGenerator.FILE_TYPE_TXT));
+    conf.put("binFileSize", ""+DEFAULT_FILESIZE);
+    return conf;
+  }
   
-
+  
   public void testRoots() throws Exception {      
     SubTreeArticleIterator artIter = createSubTreeIter();
     //http://www.example.net/IUMJ/FTDLOAD/2006/
@@ -124,7 +124,7 @@ public class TestIUMJArticleIteratorFactory extends ArticleIteratorTestCase {
     assertNotMatchesRE(pat, "http://www.wrong.com/doi/abs/10.2446/01.02.03.PR0.108.4.567-589");
     assertNotMatchesRE(pat, BASE_URL + "IUMJK/ABS/2006/2675");
     assertNotMatchesRE(pat, BASE_URL + "IUMJ/FULLTEXT/2012/55/2579");
-
+    
     assertMatchesRE(pat, BASE_URL + "IUMJ/ABS/2006/2675");
     assertMatchesRE(pat, BASE_URL + "IUMJ/FTDLOAD/2006/55/2558/pdf");
     assertMatchesRE(pat, BASE_URL + "IUMJ/FULLTEXT/2006/55/2579");
@@ -133,10 +133,10 @@ public class TestIUMJArticleIteratorFactory extends ArticleIteratorTestCase {
     re_pattern =BASE_URL+String.format(CRAWLRULE1);    
     regexpattern = Pattern.compile(re_pattern);
     pat = regexpattern;
-
+    
     assertMatchesRE(pat, BASE_URL + "IUMJ/FTDLOAD/2006/55/2558/pdf");
     assertNotMatchesRE(pat, BASE_URL + "IUMJ/FTDLOAD/2006/55/2558/213/pdf");
-
+    
   }
 
 }
