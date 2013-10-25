b77bd0fca130ce2dd4d5350def2b059df678389e
fergaloy <fergaloy@stanford.edu>
2013-10-25 19:57:25 +0000

    Forced it to exit with a zero exit code.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@30700 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- tools/src/org/lockss/db/SchemaDocDbCreator.java
+++ tools/src/org/lockss/db/SchemaDocDbCreator.java
@@ -1,3 +1,34 @@
+/*
+ * $Id: SchemaDocDbCreator.java,v 1.2 2013-10-25 19:57:25 fergaloy-sf Exp $
+ */
+
+/*
+
+ Copyright (c) 2013 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
 package org.lockss.db;
 
 import java.io.File;
@@ -10,6 +41,9 @@ import org.lockss.test.MockLockssDaemon;
 import org.lockss.util.Logger;
 import org.lockss.util.PlatformUtil;
 
+/**
+ * Creates a database for schemaSpy to document.
+ */
 public class SchemaDocDbCreator {
 
   /**
@@ -46,5 +80,6 @@ public class SchemaDocDbCreator {
     DbManager dbManager = daemon.getDbManager();
     dbManager.startService();
     dbManager.stopService();
+    System.exit(0);
   }
 }
