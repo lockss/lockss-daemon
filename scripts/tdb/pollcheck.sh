a389bb214925d8bb4ca65e90c24a63d2560105a4
mellen22 <maryellen@lockss.org>
2013-12-02 16:48:18 +0000

    Account for errors.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@31350 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/pollcheck.sh
+++ scripts/tdb/pollcheck.sh
@@ -8,7 +8,7 @@ export no_proxy=localhost,127.0.0.1
 for i in ingest1 ingest2 ingest3 ingest4 
 do
 echo "$i:"
-wget -o /dev/null -O - --tries=1 --timeout=4 --http-user=lockss-u --http-password=lockss-p http://${i}.clockss.org:8081/DaemonStatus?table=V3PollerTable 2>&1 | grep -A 1 "<b>Polls</b>:" | tail -1 | sed s/.....// | sed s/....$// | perl -ne 'print $_; $t=0; if (m/(\d+) started, (\d+) Complete/) {$t=$1-$2; if (m/(\d+) No Quorum/) {$t=$t-$1}; print "Polls in progress: $t\n"}'
+wget -o /dev/null -O - --tries=1 --timeout=4 --http-user=lockss-u --http-password=lockss-p http://${i}.clockss.org:8081/DaemonStatus?table=V3PollerTable 2>&1 | grep -A 1 "<b>Polls</b>:" | tail -1 | sed s/.....// | sed s/....$// | perl -ne 'print $_; $t=0; if (m/(\d+) started, (\d+) Complete/) {$t=$1-$2; if (m/(\d+) No Quorum/) {$t=$t-$1}; if (m/(\d+) Error/) {$t=$t-$1}; print "Polls in progress: $t\n"}'
 
 done
 
