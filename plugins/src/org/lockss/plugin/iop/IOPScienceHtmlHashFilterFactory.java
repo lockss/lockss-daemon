9292ab51e456069fb77459f8477d0017e664f1c0
thibgc <github@thib.org>
2013-12-05 00:40:59 +0000

    HTML filtering
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@31425 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/iop/IOPScienceHtmlHashFilterFactory.java
+++ plugins/src/org/lockss/plugin/iop/IOPScienceHtmlHashFilterFactory.java
@@ -1,10 +1,10 @@
 /*
- * $Id: IOPScienceHtmlHashFilterFactory.java,v 1.7 2013-10-28 21:17:41 etenbrink Exp $
+ * $Id: IOPScienceHtmlHashFilterFactory.java,v 1.8 2013-12-05 00:40:59 thib_gc Exp $
  */
 
 /*
 
-Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -32,9 +32,9 @@ in this Software without prior written authorization from Stanford University.
 
 package org.lockss.plugin.iop;
 
-import java.io.InputStream;
-import java.io.Reader;
+import java.io.*;
 
+import org.apache.commons.compress.utils.IOUtils;
 import org.htmlparser.NodeFilter;
 import org.htmlparser.filters.OrFilter;
 import org.htmlparser.filters.TagNameFilter;
@@ -55,28 +55,42 @@ public class IOPScienceHtmlHashFilterFactory implements FilterFactory {
                                                String encoding)
       throws PluginException {
     NodeFilter[] filters = new NodeFilter[] {
-        // Contains the search box, which changes over time
-        HtmlNodeFilters.tagWithAttribute("div", "id", "header-content"),
-        // Contains variable links to other content ("users also read", "related review articles", etc.)
-        HtmlNodeFilters.tagWithAttributeRegex("div", "class", "alsoRead"),
-        // The right column is an accordion that contains toc anchor links, related articles, related review articles, etc
+        /*
+         * Broad area filtering
+         */
+        // Scripts
+        new TagNameFilter("script"),
+        // Document header
+        HtmlNodeFilters.tagWithAttribute("link", "type", "text/css"),
+        // Header
+        HtmlNodeFilters.tagWithAttribute("div", "id", "cookieBanner"),
+        /* <header> -- see string filter below FIXME HTML5 */
+        HtmlNodeFilters.tagWithAttribute("div", "id", "jnl-head-band"),
+        HtmlNodeFilters.tagWithAttribute("div", "id", "hdr"), // (old)
+        HtmlNodeFilters.tagWithAttribute("div", "id", "nav"), // (old)
+        HtmlNodeFilters.tagWithAttribute("div", "id", "header-content"), // (old)
+        HtmlNodeFilters.tagWithAttribute("div", "id", "banner"), // (old)
+        // Right column
         HtmlNodeFilters.tagWithAttribute("div",  "id", "rightCol"),
+        HtmlNodeFilters.tagWithAttributeRegex("div", "class", "alsoRead"), // (now within)
+        HtmlNodeFilters.tagWithAttribute("div", "id", "tacticalBanners"), // (now within)
+        // Footer
+        /* <footer> -- see string filter below FIXME HTML5 */
+        HtmlNodeFilters.tagWithAttribute("div", "id", "footer"), // (old)
+
+        /*
+         * Main content area 
+         */
+        // Last 10 articles viewed
+        HtmlNodeFilters.tagWithAttribute("div", "class", "tabs javascripted"),
+
+        /*
+         * Other
+         */
         // may not be an issue, but concerned that mathjax exposure will change
         HtmlNodeFilters.tagWithAttribute("div",  "class", "mathJaxControls"),
-        // Last 10 articles viewed; not the best characterization but unique enough
-        HtmlNodeFilters.tagWithAttribute("div", "class", "tabs javascripted"),
-        // Contains the institution name and/or banner
-        HtmlNodeFilters.tagWithAttribute("div", "id", "banner"),
-        // Contains this year in the copyright notice
-        HtmlNodeFilters.tagWithAttribute("div", "id", "footer"),
-        // Contains style sheet
-        HtmlNodeFilters.tagWithAttribute("link", "type", "text/css"),
-        // Contains script
-        new TagNameFilter("script"),
         // Contains a jsessionid
         HtmlNodeFilters.tagWithAttributeRegex("form", "action", "jsessionid"),
-        // Contains variable ads, promos, etc.
-        HtmlNodeFilters.tagWithAttribute("div", "id", "tacticalBanners"),
     };
     
     InputStream filtered = new HtmlFilterInputStream(in,
@@ -86,10 +100,18 @@ public class IOPScienceHtmlHashFilterFactory implements FilterFactory {
     Reader filteredReader = FilterUtil.getReader(filtered, encoding);
     Reader tagFilter = HtmlTagFilter.makeNestedFilter(filteredReader,
         ListUtil.list(
-            new TagPair("<header>", "</header>"),
-            new TagPair("<footer>", "</footer>")
-            ));
+            new TagPair("<header>", "</header>"), // FIXME HTML5
+            new TagPair("<footer>", "</footer>") // FIXME HTML5
+        ));
     return new ReaderInputStream(new WhiteSpaceFilter(tagFilter));
   }
 
+  public static void main(String[] args) throws Exception {
+    String file = "/tmp/HashCUSA3a";
+    IOUtils.copy(new IOPScienceHtmlHashFilterFactory().createFilteredInputStream(null,
+                                                                                 new FileInputStream(file),
+                                                                                 null),
+                 new FileOutputStream(file + ".out"));
+  }
+  
 }
