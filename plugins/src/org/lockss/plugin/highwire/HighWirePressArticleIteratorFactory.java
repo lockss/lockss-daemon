2d4ea25e6b4c4d40ad4e227529b93f16c91d03e9
etenbrink <etenbrink@yahoo.com>
2014-01-06 19:22:57 +0000

    Use SubTreeArticleIteratorBuilder, replace all guess role code PD-444
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@31915 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/highwire/HighWirePressArticleIteratorFactory.java
+++ plugins/src/org/lockss/plugin/highwire/HighWirePressArticleIteratorFactory.java
@@ -1,10 +1,10 @@
 /*
- * $Id: HighWirePressArticleIteratorFactory.java,v 1.13 2013-06-30 18:40:17 pgust Exp $
+ * $Id: HighWirePressArticleIteratorFactory.java,v 1.14 2014-01-06 19:22:57 etenbrink Exp $
  */
 
 /*
 
-Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -43,10 +43,10 @@ import org.lockss.util.*;
 public class HighWirePressArticleIteratorFactory
     implements ArticleIteratorFactory,
                ArticleMetadataExtractorFactory {
-
+  
   protected static Logger log =
-    Logger.getLogger("HighWirePressArticleIteratorFactory");
-
+    Logger.getLogger(HighWirePressArticleIteratorFactory.class);
+  
   protected static final String ROOT_TEMPLATE_HTML =
     "\"%scgi/content/full/%s/\", base_url, volume_name";
   
@@ -60,220 +60,115 @@ public class HighWirePressArticleIteratorFactory
     "\"%scgi/reprint/%d/\", base_url, volume";
   
   protected static final String PATTERN_TEMPLATE =
-    "\"^%scgi/(content/full/([^/]+;)?%s/[^/]+/[^/]+|reprint/([^/]+;)?%s/[^/]+/[^/]+(\\.pdf)?)$\", base_url, volume_name, volume_name";
-
+    "\"^%scgi/(?:content/full/(?:[^/]+;)?%s/[^/]+/[^/]+|reprint/(?:[^/]+;)?%s/[^/]+/[^/]+" +
+    "[.]pdf)$\", base_url, volume_name, volume_name";
+  
   protected static final String OLD_PATTERN_TEMPLATE =
-    "\"^%scgi/(content/full/([^/]+;)?%d/[^/]+/[^/]+|reprint/([^/]+;)?%d/[^/]+/[^/]+(\\.pdf)?)$\", base_url, volume, volume";
-
+    "\"^%scgi/(content/full/([^/]+;)?%d/[^/]+/[^/]+|reprint/([^/]+;)?%d/[^/]+/[^/]+" +
+    "([.]pdf)?)$\", base_url, volume, volume";
+  
+  protected static final Pattern HTML_PATTERN = Pattern.compile(
+      "/cgi/content/full/([^/]+;)?([^/]+/[^/]+/[^/]+)$",
+      Pattern.CASE_INSENSITIVE);
+  
+  protected static Pattern PDF_PATTERN = Pattern.compile(
+      "/cgi/reprint/([^/]+;)?([^/]+/[^/]+/[^/]+)[.]pdf$",
+      Pattern.CASE_INSENSITIVE);
+  
+  protected static Pattern PDF_LANDING_PATTERN = Pattern.compile(
+      "/cgi/(?:reprintframed|framedreprint|reprint)/([^/]+;)?([^/]+/[^/]+/[^/]+)$",
+      Pattern.CASE_INSENSITIVE);
+  
+  // how to change from one form (aspect) of article to another
+  protected static final String HTML_REPLACEMENT1 = "/cgi/content/full/$2";
+  protected static final String HTML_REPLACEMENT2 = "/cgi/content/full/$1$2";
+  protected static final String PDF_REPLACEMENT1 = "/cgi/reprint/$2.pdf";
+  protected static final String PDF_REPLACEMENT2 = "/cgi/reprint/$1$2.pdf";
+  protected static final String PDF_LANDING_REPLACEMENT1 = "/cgi/reprintframed/$2";
+  protected static final String PDF_LANDING_REPLACEMENT2 = "/cgi/reprintframed/$1$2";
+  protected static final String PDF_LANDING_REPLACEMENT3 = "/cgi/framedreprint/$2";
+  protected static final String PDF_LANDING_REPLACEMENT4 = "/cgi/framedreprint/$1$2";
+  protected static final String PDF_LANDING_REPLACEMENT5 = "/cgi/reprint/$2";
+  protected static final String PDF_LANDING_REPLACEMENT6 = "/cgi/reprint/$1$2";
+  protected static final String ABSTRACT_REPLACEMENT1 = "/cgi/content/abstract/$2";
+  protected static final String ABSTRACT_REPLACEMENT2 = "/cgi/content/abstract/$1$2";
+  protected static final String SUMMARY_REPLACEMENT = "/cgi/content/summary/$2";
+  protected static final String REFERENCES_REPLACEMENT = "/cgi/content/refs/$2";
+  
   public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au,
                                                       MetadataTarget target)
       throws PluginException {
+    
     List<String> rootTemplates = new ArrayList<String>(2);
     String pluginId = au.getPluginId();
     if ("org.lockss.plugin.highwire.HighWirePlugin".equals(pluginId)) {
       // H10a plugin uses integer volume
       rootTemplates.add(OLD_ROOT_TEMPLATE_HTML);
       rootTemplates.add(OLD_ROOT_TEMPLATE_PDF);
-      return
-	new HighWirePressArticleIterator(au,
-					 new SubTreeArticleIterator.Spec()
-					 .setTarget(target)
-					 .setRootTemplates(rootTemplates)
-					 .setPatternTemplate(OLD_PATTERN_TEMPLATE));
     }
-    rootTemplates.add(ROOT_TEMPLATE_HTML);
-    rootTemplates.add(ROOT_TEMPLATE_PDF);
-    return new
-      HighWirePressArticleIterator(au,
-				   new SubTreeArticleIterator.Spec()
-				   .setTarget(target)
-				   .setRootTemplates(rootTemplates)
-				   .setPatternTemplate(PATTERN_TEMPLATE));
+    else {
+      rootTemplates.add(ROOT_TEMPLATE_HTML);
+      rootTemplates.add(ROOT_TEMPLATE_PDF);
+    }
+    
+    SubTreeArticleIteratorBuilder builder = new SubTreeArticleIteratorBuilder(au);
+    
+    builder.setSpec(target,
+        rootTemplates, PATTERN_TEMPLATE, Pattern.CASE_INSENSITIVE);
+    
+    // set up html or pdf to be an aspect that will trigger an ArticleFiles
+    // NOTE - for the moment this also means full is considered a FULL_TEXT_CU 
+    // until this is deprecated
+    builder.addAspect(
+        HTML_PATTERN, Arrays.asList(HTML_REPLACEMENT1, HTML_REPLACEMENT2),
+        ArticleFiles.ROLE_FULL_TEXT_HTML);
+    
+    builder.addAspect(
+        PDF_PATTERN, Arrays.asList(PDF_REPLACEMENT1, PDF_REPLACEMENT2),
+        ArticleFiles.ROLE_FULL_TEXT_PDF);
+    
+    // set up abstract/summary to be an aspect
+    builder.addAspect(
+        Arrays.asList(ABSTRACT_REPLACEMENT1, ABSTRACT_REPLACEMENT2, SUMMARY_REPLACEMENT),
+        ArticleFiles.ROLE_ABSTRACT,
+        ArticleFiles.ROLE_ARTICLE_METADATA);
+    
+    // set up pdf landing page to be an aspect
+    builder.addAspect(
+        PDF_LANDING_PATTERN, Arrays.asList(
+        PDF_LANDING_REPLACEMENT1,
+        PDF_LANDING_REPLACEMENT2,
+        PDF_LANDING_REPLACEMENT3,
+        PDF_LANDING_REPLACEMENT4,
+        PDF_LANDING_REPLACEMENT5,
+        PDF_LANDING_REPLACEMENT6),
+        ArticleFiles.ROLE_FULL_TEXT_PDF_LANDING_PAGE,
+        ArticleFiles.ROLE_ARTICLE_METADATA);
+    
+    // add aspect for html to have metadata role
+    builder.addAspect(
+        Arrays.asList(HTML_REPLACEMENT1, HTML_REPLACEMENT2),
+        ArticleFiles.ROLE_ARTICLE_METADATA);
+    
+    // set up references to be an aspect
+    builder.addAspect(REFERENCES_REPLACEMENT,
+        ArticleFiles.ROLE_REFERENCES);
+    
+    // The order in which we want to define full_text_cu.
+    // First one that exists will get the job
+    builder.setFullTextFromRoles(
+        ArticleFiles.ROLE_FULL_TEXT_HTML, 
+        ArticleFiles.ROLE_FULL_TEXT_PDF, 
+        ArticleFiles.ROLE_FULL_TEXT_PDF_LANDING_PAGE,
+        ArticleFiles.ROLE_ABSTRACT);
+    
+    return builder.getSubTreeArticleIterator();
   }
-
+  
   public ArticleMetadataExtractor
     createArticleMetadataExtractor(MetadataTarget target)
       throws PluginException {
     return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
   }
   
-  protected static class HighWirePressArticleIterator
-    extends SubTreeArticleIterator {
-    
-    protected static Pattern HTML_PATTERN =
-      Pattern.compile("/cgi/content/full/([^/]+;)?([^/]+/[^/]+/[^/]+)$",Pattern.CASE_INSENSITIVE);
-    
-    protected static Pattern PDF_PATTERN =
-      Pattern.compile("/cgi/reprint/([^/]+;)?([^/]+/[^/]+/[^/]+)\\.pdf$",Pattern.CASE_INSENSITIVE);
-    
-    public HighWirePressArticleIterator(ArchivalUnit au,
-                                        SubTreeArticleIterator.Spec spec) {
-      super(au, spec);
-    }
-    
-    @Override
-    protected ArticleFiles createArticleFiles(CachedUrl cu) {
-      String url = cu.getUrl();
-      Matcher mat;
-      
-      mat = HTML_PATTERN.matcher(url);
-      if (mat.find()) {
-        if (log.isDebug3()) {
-          log.debug3("HTML match: " + url);
-        }    
-        return processFullTextHtml(cu, mat);
-      }
-        
-      mat = PDF_PATTERN.matcher(url);
-      if (mat.find()) {
-        if (log.isDebug3()) {
-          log.debug3("PDF match: " + url);
-        }
-        return processFullTextPdf(cu, mat);
-      }
-      
-      if (log.isDebug3()) log.debug3("Skipping: " + url);
-      return null;
-    }
-
-    protected ArticleFiles processFullTextHtml(CachedUrl htmlCu,
-					       Matcher htmlMat) {
-      CachedUrl altCu = guessDifferentFrom(htmlCu, htmlMat,
-                                           "/cgi/content/full/$2");
-
-      if (altCu != null) {
-        if (log.isDebug3()) {
-          log.debug2("Skipping " + htmlCu.getUrl()
-		     + " because of " + altCu.getUrl());
-        }
-        return null;
-      }
-
-      ArticleFiles af = new ArticleFiles();
-      af.setFullTextCu(htmlCu);
-      af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_HTML, htmlCu);
-      if (af.getRoleCu(ArticleFiles.ROLE_ARTICLE_METADATA) == null) {
-        af.setRoleCu(ArticleFiles.ROLE_ARTICLE_METADATA, htmlCu);
-      }
-      return af;
-    }
-    
-    protected ArticleFiles processFullTextPdf(CachedUrl pdfCu, Matcher pdfMat) {
-      CachedUrl altCu = guessDifferentFrom(pdfCu, pdfMat,
-                                           "/cgi/reprint/$2.pdf",
-                                           "/cgi/content/full/$1$2",
-                                           "/cgi/content/full/$2");
-      if (altCu != null) {
-        if (log.isDebug3()) {
-          log.debug3("Skipping " + pdfCu.getUrl()
-		     + " because of " + altCu.getUrl());
-        }
-        return null;
-      }
-
-      ArticleFiles af = new ArticleFiles();
-      af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_PDF, pdfCu);
-      // must be first to get metadata from abstract if it exists
-      guessAbstract(af, pdfMat);
-      guessPdfLandingPage(af, pdfMat);
-      af.setFullTextCu(af.getRoleCu(ArticleFiles.ROLE_FULL_TEXT_PDF_LANDING_PAGE) != null
-                       ? af.getRoleCu(ArticleFiles.ROLE_FULL_TEXT_PDF_LANDING_PAGE)
-                       : pdfCu);
-//      guessOtherParts(af, pdfMat);
-      return af;
-    }
-    
-
-    protected void guessPdfLandingPage(ArticleFiles af, Matcher mat) {
-      CachedUrl pdfLandCu = guess(mat,
-                                  "/cgi/reprint/$1$2",
-                                  "/cgi/reprint/$2",
-                                  "/cgi/reprintframed/$1$2",
-                                  "/cgi/reprintframed/$2",
-                                  "/cgi/framedreprint/$1$2",
-                                  "/cgi/framedreprint/$2");
-      if (pdfLandCu != null) {
-        af.setRoleCu(ArticleFiles.ROLE_FULL_TEXT_PDF_LANDING_PAGE, pdfLandCu);
-        if (af.getRoleCu(ArticleFiles.ROLE_ARTICLE_METADATA) == null) {
-          af.setRoleCu(ArticleFiles.ROLE_ARTICLE_METADATA, pdfLandCu);
-        }
-      }
-    }
-    
-    protected void guessAbstract(ArticleFiles af, Matcher mat) {
-    CachedUrl absCu = guess(mat,
-                            "/cgi/content/abstract/$1$2",
-                            "/cgi/content/abstract/$2");
-    if (absCu != null) {
-      af.setRoleCu(ArticleFiles.ROLE_ABSTRACT, absCu);
-      if (af.getRoleCu(ArticleFiles.ROLE_ARTICLE_METADATA) == null) {
-        af.setRoleCu(ArticleFiles.ROLE_ARTICLE_METADATA, absCu);
-        }
-      }
-    }
-    
-  
-    /**
-     * <p>Tries various URLs in this AU similar to the one in the
-     * given matcher, using each one of the given matcher replacement
-     * patterns in turn, until one is found that exists and has
-     * content.</p>
-     * <p>The replacement patterns are applied to the matcher using
-     * {@link Matcher#replaceFirst(String)}.</p>
-     * <p>This method is a candidate to be refactored into a utility
-     * framework for article iterators.</p>
-     * @param mat      A matcher encapsulating a previously-matched
-     *                 URL in this AU.
-     * @param replPats Any number of replacement patterns consistent
-     *                 with the matcher, to try in sequence.
-     * @return A {@link CachedUrl} if one of the replacement patterns
-     *         produces a URL in this AU that exists and has content,
-     *         or <code>null</code> otherwise.
-     * @see Matcher#replaceFirst(String)
-     * @see CachedUrl#hasContent()
-     */
-    protected CachedUrl guess(Matcher mat,
-                              String... replPats) {
-      for (String replPat : replPats) {
-        CachedUrl guessCu = au.makeCachedUrl(mat.replaceFirst(replPat));
-        if (guessCu != null && guessCu.hasContent()) {
-          return guessCu;
-        }
-      }
-      return null;
-    }
-    
-    /**
-     * <p>Does the same thing as
-     * {@link #guess(Matcher, String...)}, with the
-     * further guarantee that the returned variant URL does not
-     * represent the same URL as the given URL.</p>
-     * <p>This method is a candidate to be refactored into a utility
-     * framework for article iterators.</p>
-     * @param original The original URL from this AU.
-     * @param mat      A matcher encapsulating a previously-matched
-     *                 URL in this AU.
-     * @param replPats Any number of replacement patterns consistent
-     *                 with the matcher, to try in sequence.
-     * @return A {@link CachedUrl} if one of the replacement patterns
-     *         produces a URL in this AU that exists, does not
-     *         represent the same URL as the original, and has
-     *         content, or <code>null</code> otherwise.
-     * @see #guess(Matcher, String...)
-     */
-    protected CachedUrl guessDifferentFrom(CachedUrl original,
-                                           Matcher mat,
-                                           String... replPats) {
-      for (String replPat : replPats) {
-        CachedUrl guessCu = au.makeCachedUrl(mat.replaceFirst(replPat));
-        if (guessCu != null
-	    && guessCu.hasContent()
-	    && !guessCu.getUrl().equals(original.getUrl())) {
-          return guessCu;
-        }
-      }
-      return null;
-    }
-   
-  }
 }
