846405ad5c9692391e51e141b00fb9927770b92b
lytdoan <ldoan@stanford.edu>
2014-01-22 22:08:30 +0000

    Comments
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@32193 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/businesssystemslaboratory/BusinessSystemsLaboratoryMetadataExtractorFactory.java
+++ plugins/src/org/lockss/plugin/businesssystemslaboratory/BusinessSystemsLaboratoryMetadataExtractorFactory.java
@@ -1,11 +1,8 @@
-/*
-
- * $Id: BusinessSystemsLaboratoryMetadataExtractorFactory.java,v 1.2 2014-01-22 22:05:55 ldoan Exp $
- */
+/* $Id: BusinessSystemsLaboratoryMetadataExtractorFactory.java,v 1.3 2014-01-22 22:08:30 ldoan Exp $ */
 
 /*
 
- Copyright (c) 2000-2009 Board of Trustees of Leland Stanford Jr. University,
+ Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
  all rights reserved.
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
