8295358cad28a6179d4afb7d25be0346e865bea8
alexandraohlson <alexandra@slgroup.com>
2014-01-23 21:07:27 +0000

    post review changes - legibility and improving read in to array; improve test framework
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@32213 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/clockss/XmlFilteringInputStream.java
+++ plugins/src/org/lockss/plugin/clockss/XmlFilteringInputStream.java
@@ -1,5 +1,5 @@
 /*
- * $Id: XmlFilteringInputStream.java,v 1.1 2014-01-16 22:17:59 alexandraohlson Exp $
+ * $Id: XmlFilteringInputStream.java,v 1.2 2014-01-23 21:07:27 alexandraohlson Exp $
  */
 
 /*
@@ -49,11 +49,18 @@ import org.lockss.util.Logger;
  * XML specification
  *  Unicode character, excluding the surrogate blocks, FFFE, and FFFF. 
  * Char    ::=          #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | 
- * [#x10000-#x10FFFF]  
+ * [#x10000-#x10FFFF]
+ * 
+ * NOTE: Should we need to expand this to handle other charsets, 
+ * enhance the filter to create a reader with the initial charset, 
+ * read the first line and match against a regex to extract the charset, 
+ * reset and create the correct reader, then filter disallowed chars < 32.  
+ * This is much easier than html because the xml declaration must be on the 
+ * first line.
  */
 public class XmlFilteringInputStream extends FilterInputStream {
   static Logger log = Logger.getLogger(XmlFilteringInputStream.class);
-  
+
   private static int READ_AHEAD = 2; // for mark/reset
   // make a quick way to assert that a character is < 32 but not 9, 10, or 13
   // in Java an integer is always 32 bits
@@ -62,70 +69,84 @@ public class XmlFilteringInputStream extends FilterInputStream {
   protected XmlFilteringInputStream(InputStream in) {
     super(in);
   }
-  
-  /* ISO-8859-1 is a single byte charset */
+
+  /* ISO-8859-1 is a single byte charset and this depends on that*/
+
+  int AMPERSAND_INT = '&'; //38
+  int WS_INT = ' '; //32
+  int QUEST_INT = '?'; //63
   public int read() throws IOException {
-    
+
     int b = in.read();
     if (b == -1) return b;
-    // check for unprotected & (followed by a space) useful, but not perfect
-    if (b == 38) {
+    // check for unprotected & (followed by a space) useful
+    // This is a bit of a hack, but works for our limited purposes
+    if (b == AMPERSAND_INT) { 
       in.mark(READ_AHEAD);
       int nextb = in.read();
       in.reset();
-      if (nextb < 0 || nextb == 32 ||
-            // mask to see if a legal WS character
-            (nextb < 32 && ((INVALID_LOW_CHAR_MASK & (1 << nextb)) == 0))) {
-        return '?'; //instead of unprotected &
+      // If & followed by end of file, WS or a legal WS ascii char
+      if (nextb < 0 || nextb == WS_INT ||
+          legal_lowspace_char(nextb)) { 
+        return QUEST_INT;  //instead of unprotected &
       }
     }
     // all other printing, ASCII characters
     if (b >= 32) return b;
     // carriage return, linefeed, tab, and end of file are allowed
-    //else if (b == 10 || b == 13 || b == 9 || b == -1) return b;
-    else if ( ((INVALID_LOW_CHAR_MASK & (1 << b)) == 0 )) {
+    else if (legal_lowspace_char(b)) {
       return b;
     }
     // non-printing characters
-    return '?';
+    return QUEST_INT;
+  }
+
+  // tab, line feed, carriage return
+  // this also returns FALSE if >= 32, so use carefully
+  private boolean legal_lowspace_char(int b) {
+    return (b < WS_INT &&
+        (b == 9 || b == 10 || b == 13));
   }
 
   /*
    * (non-Javadoc)
    * @see java.io.FilterInputStream#read(byte[], int, int)
-   * Implement using the read() method so that the filtering is consolidated
-   * in one method
+   * 
    */
   public int read(byte[] data, int offset, int length) throws IOException {
 
-    if (data == null) {
-      throw new NullPointerException();
-    } else if (offset < 0 || length < 0 || length > data.length - offset) {
-      throw new IndexOutOfBoundsException();
-    } else if (length == 0) {
-      return 0;
-    }
+    int result = in.read(data, offset, length);
+    // result = -1 drops past for loop and returns directly
 
-    int c = read(); 
-    if (c == -1) {
-      return -1;
-    }
-    data[offset] = (byte)c;
-
-    int i = 1;
-    try {
-      for (; i < length ; i++) {
-        c = read(); 
-        if (c == -1) {
-          break;
+    // loop over returned results changing invalid chars to ?
+    for (int i = offset; i < offset+result; i++) {
+      // if low, check validity
+      if (data[i] < WS_INT) {
+        if (data[i] > 0 && !(legal_lowspace_char(data[i]))) {
+          data[i] = (byte)QUEST_INT;
         }
-        data[offset + i] = (byte)c;
+      } else {
+        //in this range we only handle '&'
+        if (data[i] == AMPERSAND_INT) {
+          //perform lookahead
+          int nextb;
+          if (i+1 < offset+result) {
+            // we're in luck and still have data!
+            nextb = data[i+1];
+          } else {
+            // drat. we'll have to read one more byte
+            in.mark(READ_AHEAD);
+            nextb = in.read();
+            in.reset();
+          }
+          // If & followed by end of file, WS or a legal WS ascii char
+          if (nextb < 0 || nextb == WS_INT || legal_lowspace_char(nextb)) {
+            data[i] = (byte)QUEST_INT;
+          }
+        }
       }
-    } catch (IOException ee) {
     }
-    return i;
+    return result;
   }
-
-
 
 }
--- plugins/test/src/org/lockss/plugin/clockss/TestXmlFilteringInputStream.java
+++ plugins/test/src/org/lockss/plugin/clockss/TestXmlFilteringInputStream.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestXmlFilteringInputStream.java,v 1.1 2014-01-17 18:05:32 alexandraohlson Exp $
+ * $Id: TestXmlFilteringInputStream.java,v 1.2 2014-01-23 21:07:27 alexandraohlson Exp $
  */
 
 /*
@@ -45,67 +45,75 @@ public class TestXmlFilteringInputStream extends LockssTestCase {
 
   private static final String BASIC_STRING = "123";
   private static final String BASIC_XML = "<?xml version=\"1.0\"?><foo><bar>a\u00e9b a &amp; b</bar></foo>";
+  private static final String EMPTY_STRING = "            ";
 
   private static final String BAD_XML = "<foo><![CDATA[" + "\u001c" + "]]></foo>"; //15th char is 0x1c
   private static final String UNPROTECTED_XML = "<foo>a\u00e9b a & b</foo>"; //12th char is unprotected &
   private static final String EDGE_STRING1 = "&";//should replace - followed by end of buffer
   private static final String EDGE_STRING2 = "&blah;& "; //should replace last one, followed by space
   private static final String EDGE_STRING3 = "& &amp; &\u0009";  // should replace first and last &
- 
+
+  private static final String FIXED_BAD_XML = "<foo><![CDATA[" + "?" + "]]></foo>"; //15th char is 0x1c
+  private static final String FIXED_UNPROTECTED_XML = "<foo>a\u00e9b a ? b</foo>"; //12th char is unprotected &
+  private static final String FIXED_EDGE_STRING1 = "?";//should replace - followed by end of buffer
+  private static final String FIXED_EDGE_STRING2 = "&blah;? "; //should replace last one, followed by space
+  private static final String FIXED_EDGE_STRING3 = "? &amp; ?\u0009";  // should replace first and last &
 
   // compare reading of string input  - these strings shouldn't change
   public void testRead() {
-    testCharacterReplacement(BASIC_STRING, null); // no characters should replace
-    testCharacterReplacement(BASIC_XML, null); // no characters should replace
+    testReadArrayReplacement(BASIC_STRING, BASIC_STRING); // no changes expected
+    testSingleReadReplacement(BASIC_STRING, BASIC_STRING); // no changes expected
+    testReadArrayReplacement(BASIC_XML, BASIC_XML); // no changes expected
+    testSingleReadReplacement(BASIC_XML, BASIC_XML); // no changes expected
+    testReadArrayReplacement(EMPTY_STRING, EMPTY_STRING); // no changes expected
+    testSingleReadReplacement(EMPTY_STRING, EMPTY_STRING); // no changes expected
   }
 
   public void testReplacement() {
-    testCharacterReplacement(UNPROTECTED_XML, Arrays.asList(12));
-    testCharacterReplacement(BAD_XML, Arrays.asList(15));
-    testCharacterReplacement(EDGE_STRING1, Arrays.asList(1));
-    testCharacterReplacement(EDGE_STRING2, Arrays.asList(7));
-    testCharacterReplacement(EDGE_STRING3, Arrays.asList(1,9));
-
+    testReadArrayReplacement(UNPROTECTED_XML, FIXED_UNPROTECTED_XML);
+    testSingleReadReplacement(UNPROTECTED_XML, FIXED_UNPROTECTED_XML);
+    testReadArrayReplacement(BAD_XML, FIXED_BAD_XML);
+    testSingleReadReplacement(BAD_XML, FIXED_BAD_XML);
+    testReadArrayReplacement(EDGE_STRING1, FIXED_EDGE_STRING1);
+    testSingleReadReplacement(EDGE_STRING1, FIXED_EDGE_STRING1);
+    testReadArrayReplacement(EDGE_STRING2, FIXED_EDGE_STRING2);
+    testSingleReadReplacement(EDGE_STRING2, FIXED_EDGE_STRING2);
+    testReadArrayReplacement(EDGE_STRING3, FIXED_EDGE_STRING3);
+    testSingleReadReplacement(EDGE_STRING3, FIXED_EDGE_STRING3);
   }
-  
+
   public void testDifficultChars() {
 
     //Couldn't set these in unicode notation as a string - eclipse was unhappy
     // should replace 2,3,6,7,9 and leave the others
     String LOTS_OF_ODD_CHARS = new String(new byte[] { 0x58, 0x01, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x1c, 0x58});
+    String FIXED_LOTS_OF_ODD_CHARS = new String(new byte[] { 0x58, 0x3F, 0x3F, 0x09, 0x0A, 0x3F, 0x3F, 0x0D, 0x3F, 0x58});
     // replace none of them
     String LOTS_OF_GOOD_CHARS = new String(new byte[] { 0x58, 0x3c, 0x20, 0x0A, 0x0D, 0x58 });
 
-    testCharacterReplacement(LOTS_OF_ODD_CHARS, Arrays.asList(2,3,6,7,9));
-    testCharacterReplacement(LOTS_OF_GOOD_CHARS,null);
+    testReadArrayReplacement(LOTS_OF_ODD_CHARS, FIXED_LOTS_OF_ODD_CHARS);
+    testSingleReadReplacement(LOTS_OF_ODD_CHARS, FIXED_LOTS_OF_ODD_CHARS);
+    testReadArrayReplacement(LOTS_OF_GOOD_CHARS,LOTS_OF_GOOD_CHARS);
+    testSingleReadReplacement(LOTS_OF_GOOD_CHARS,LOTS_OF_GOOD_CHARS);
   }
 
-  // filter a string and if the correct characters have been turned in to '?'
-  // the list of integers indicate which position in the string should have been
-  // altered. For other slots, compare against and unfiltered version of the string.
-  // POSITION in string starts counting at ONE (eg. 1st char or 2nd char) not from zero
-  private void testCharacterReplacement(String testString, List<Integer> changedSlots) {
 
-    XmlFilteringInputStream filtIn = (XmlFilteringInputStream) createInputStreamFromString(testString, true);
-    InputStream unFiltIn = createInputStreamFromString(testString,false);
+  /*
+   * Take in an original string that may or may not get changed with filtering
+   * use read() to force filtering
+   * compare the result against the expected string
+   */
 
-    int strLen = testString.length();
-    byte fchars[] = new byte[strLen+1];//offset so count starts at 1
-    fchars[0] = 0;
-    byte ufchars[] = new byte[strLen+1];
-    ufchars[0] = 0;
+  private void testSingleReadReplacement(String origString, String expectedString) {
+    XmlFilteringInputStream filtIn = (XmlFilteringInputStream) createInputStreamFromString(origString, true);
+
+    int strLen = origString.length();
+    int nextbyte;
     try {
-      filtIn.read(fchars, 1, strLen);
-      unFiltIn.read(ufchars, 1, strLen);
-
-      for (int i=1; i < strLen+1; i ++) {
-        log.debug3("Filtered char at " + i + " is " + (byte)fchars[i]);
-        log.debug3("Original char at " + i + " is " + (byte)ufchars[i]);
-        if ((changedSlots != null) && changedSlots.contains(i)) {
-          assertEquals(fchars[i], '?');
-        } else {
-          assertEquals(ufchars[i],fchars[i]);
-        }
+      byte exchars[] = expectedString.getBytes(Constants.ENCODING_ISO_8859_1);
+      for (int i = 0; i < strLen; i++) {
+        nextbyte = filtIn.read();
+        assertEquals(exchars[i], (byte)nextbyte);
       }
     } catch (IOException e) {
       // TODO Auto-generated catch block
@@ -113,8 +121,47 @@ public class TestXmlFilteringInputStream extends LockssTestCase {
     } finally {
       IOUtil.safeClose(filtIn);
     }
-
   }
+
+
+  // Take in an original string that may or may not get changed with filtering
+  // use read(char, offset, int) to force filtering
+  // compare the result against the expected string
+  // Try this two ways - read in as one big bytearray and as well
+  // read in as a bunch of reads to a single byte bytearray to test edge cases
+  private void testReadArrayReplacement(String origString, String expectedString) {
+
+    /* make 2 copies - one for each reading test */
+    XmlFilteringInputStream filtIn = (XmlFilteringInputStream) createInputStreamFromString(origString, true);
+    XmlFilteringInputStream filtIn2 = (XmlFilteringInputStream) createInputStreamFromString(origString, true);
+    int strLen = origString.length();
+
+    try {
+      byte exchars[] = expectedString.getBytes(Constants.ENCODING_ISO_8859_1);
+
+      // Read string in to array of full length and check result against expected
+      byte fchars[] = new byte[strLen];
+      filtIn.read(fchars, 0, strLen);
+      assertEquals(exchars,fchars);
+      
+      //Read string in to array ONE char at a time (to force lookahead) and check against expected
+      byte onechar[] = new byte[1];
+      byte allchars[] = new byte[strLen];
+      for (int i = 0; i< strLen; i++) {
+        filtIn2.read(onechar, 0, 1);
+        allchars[i] = onechar[0]; // add to complete array
+      }
+      // now compare the result array to the expected array
+      assertEquals(exchars, allchars);
+    } catch  (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } finally {
+      IOUtil.safeClose(filtIn);
+      IOUtil.safeClose(filtIn2);
+    }
+  }
+
 
   // Set up stream to get bytes encoded for ISO_8859
   private InputStream createInputStreamFromString(String s, boolean filter) {
@@ -132,3 +179,4 @@ public class TestXmlFilteringInputStream extends LockssTestCase {
   }
 
 }
+
