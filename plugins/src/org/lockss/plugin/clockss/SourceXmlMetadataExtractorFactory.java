0dde15e54777cbc5e16f6dbc75d89e6eefa576b6
alexandraohlson <alexandra@slgroup.com>
2014-01-28 22:06:39 +0000

    Missed two files - 1st stage of rewack of XML bulk source framework
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@32280 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/clockss/SourceXmlMetadataExtractorFactory.java
+++ plugins/src/org/lockss/plugin/clockss/SourceXmlMetadataExtractorFactory.java
@@ -1,5 +1,5 @@
 /*
- * $Id: SourceXmlMetadataExtractorFactory.java,v 1.13 2014-01-23 22:31:38 alexandraohlson Exp $
+ * $Id: SourceXmlMetadataExtractorFactory.java,v 1.14 2014-01-28 22:06:39 alexandraohlson Exp $
  */
 
 /*
@@ -32,22 +32,18 @@
 
 package org.lockss.plugin.clockss;
 
-import org.apache.commons.collections.map.LRUMap;
-import org.apache.commons.collections.map.MultiValueMap;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.cxf.common.util.StringUtils;
 import org.lockss.util.*;
 import org.lockss.daemon.*;
 import org.lockss.extractor.*;
-import org.lockss.extractor.XmlDomMetadataExtractor.XPathValue;
-
 import java.io.IOException;
 import java.util.*;
 import javax.xml.xpath.XPathExpressionException;
 
 import org.lockss.plugin.ArchivalUnit;
-import org.lockss.plugin.AuUtil;
 import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
 import org.lockss.plugin.clockss.XPathXmlMetadataParser;
 import org.xml.sax.SAXException;
 
@@ -56,136 +52,21 @@ import org.xml.sax.SAXException;
  *  A factory to create an generic SourceXmlMetadataExtractor
  *  @author alexohlson
  */
-public class SourceXmlMetadataExtractorFactory
+public abstract class SourceXmlMetadataExtractorFactory
 implements FileMetadataExtractorFactory {
   static Logger log = Logger.getLogger(SourceXmlMetadataExtractorFactory.class);
-
-  public static final String PLUGIN_SOURCE_XML_HELPER_CLASS_KEY = "plugin_source_xml_metadata_extractor_helper";
-  private static LRUMap schemaHelperMap = new LRUMap(10); /*<helperClassName, helperClassInstance> */ 
-
-  @Override
-  public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
-      String contentType)
-          throws PluginException {
-    return new SourceXmlMetadataExtractor();
-  }
-
-  /** Class to set up specific schema information for the
-   * SourceXmlMetadataExtractro. A helper class defines the schema
-   * and provides the information to the extractor via the get methods.
-   */
-  public interface SourceXmlMetadataExtractorHelper {
-
-    /**
-     * Return the map for metadata that carries across all records in this XML
-     * schema. It can be null. <br/>
-     * If null, only article level information is retrieved.
-     * @return
-     */
-    public Map<String,XPathValue> getGlobalMetaMap();
-
-    /**
-     * Return the map for metadata that is specific to each "article" record.
-     * @return
-     */
-    public Map<String,XPathValue> getArticleMetaMap();
-
-    /**
-     * Return the xPath string which defines one "article" record
-     * It can be null.<br/>
-     * If null, xPath matching starts at the root of the document.
-     * @return
-     */
-    public String getArticleNode();
-
-    /**
-     * Return the map to translate from raw metadata to cooked metadata
-     * This must be set or no metadata gets emitted
-     * @return
-     */
-    public MultiValueMap getCookMap();
-
-
-    /**
-     * Return the xPath key for an item in the record that identifies a 
-     * unique article, even if multiple records provide information about 
-     * different manifestations of that article. For example, an ISBN13 will
-     * be consistent, even if there are different records for each book 
-     * type (eg. pdf, epub, etc)
-     * The key will be used to combine metadata from multiple records in to
-     * one ArticleMetadata for all associated records. 
-     * It can be null - no record consolidation will happen.
-     * @return
-     */
-    public String getDeDuplicationXPathKey();
-
-    /**
-     * Used only in consolidateRecords() 
-     * Return the xPath key for the item in the record that should be combined
-     * when multiple records are consolidated because the refer to the same
-     * item. For example - when different formats of a book are described in
-     * separate records, the format description would be the item to 
-     * consolidate when deduping.
-     * It can be null. Consolidation may occur, but the record field will
-     * not be combined.
-     * @return
-     */
-    public String getConsolidationXPathKey();
-
-    /**
-     * Used only in preEmitCheck() which may be overridden by a child
-     * Return the filename prefix to be used when building up a filename to determine
-     * if a file exists before emitting its metadata. It can also contain
-     * path information (relative to current directory for XML file) if needed. 
-     * It can be null if not needed.
-     * @return
-     */
-    public String getFilenamePrefix();
-
-    /**
-     * Used only in preEmitCheck() which may be overridden by a child
-     * Return a list of suffixes for the filenames to be looked for when 
-     * checking for existence before emitting. It could be just the filetype
-     * (eg. [.pdf][.epub]) or it could include additional text if needed
-     * The preEmitCheck will check the suffixes in order until it finds one.
-     * It can be null. 
-     * @return
-     */
-    public ArrayList<String> getFilenameSuffixList();
-
-    /**
-     * Used only in preEmitCheck() which may be overridden by a child
-     * Return the xPath key to use an item from the raw metadata to build the
-     * filename for a preEmitCheck. For example if the ISBN13 value is used
-     * as the filename.
-     * It can be null if the filename doesn't include metadata information.
-     * @return
-     */
-    public String getFilenameXPathKey();
-    
-    /**
-     * Indicates whether the XML should be filtered as an InputStream to 
-     * remove XML illegal characters that might be there.  This will only work
-     * for single byte char encodings (such as ISO8859) so check before 
-     * setting.
-     * If you don't need it, best to set it to false.  
-     */
-    public boolean getDoXmlFiltering();
-  }
 
 
   /**
    * The generic class to handle metadata extraction for source content
-   * that uses XML with a set schema for metadata.
-   * The class gets its schema definition from an implemenation of a
-   * SourceXmlMetadataExtractorHelper 
-   * which must be provided and is defined in a plugin using the key
-   * plugin_source_xml_metadata_extractor_helper
-   * 
+   * that uses XML with a set schema.
+   * A specific plugin must subclass this. While it might inherit and use
+   * the vast majority, at a minimum it must define the schema by overriding
+   * the "setUpSchema()" method 
    * @author alexohlson
    *
    */
-  public static class SourceXmlMetadataExtractor 
+  public static abstract class SourceXmlMetadataExtractor 
   implements FileMetadataExtractor {
 
 
@@ -204,10 +85,10 @@ implements FileMetadataExtractorFactory {
         throws IOException, PluginException {
       try {
 
-        SourceXmlMetadataExtractorHelper schemaHelper;
+        SourceXmlSchemaHelper schemaHelper;
         // 1. figure out which XmlMetadataExtractorHelper class to use to get
         // the schema specific information
-        if ((schemaHelper = setUpSchema(cu)) == null) {
+        if ((schemaHelper = setUpSchema()) == null) {
           log.debug("Unable to set up XML schema. Cannot extract from XML");
           throw new PluginException("XML schema not set up for " + cu.getUrl());
         }     
@@ -220,7 +101,7 @@ implements FileMetadataExtractorFactory {
                 schemaHelper.getArticleMetaMap(),
                 schemaHelper.getDoXmlFiltering()).extractMetadata(target, cu);
 
-        
+
         Collection<ArticleMetadata> AMCollection = amList; //if deduping, this will change
 
         // 3. Consolidate identical records based on DeDuplicationXPathKey
@@ -265,7 +146,7 @@ implements FileMetadataExtractorFactory {
     protected void handleIOException(CachedUrl cu, IOException ex) {
       // Add an alert or more significant warning here
       log.debug3("IO exception loading XML file", ex);
-      
+
     }
 
     // Overrideable method for plugins that want to catch and handle
@@ -273,7 +154,7 @@ implements FileMetadataExtractorFactory {
     protected void handleSAXException(CachedUrl cu, SAXException ex) {
       // Add an alert or more significant warning here
       log.debug3("SAX exception loading XML file", ex);
-      
+
     }
 
     /**
@@ -294,7 +175,7 @@ implements FileMetadataExtractorFactory {
      * the dedup'd records
      * @param thisAM - determine if this AM is unique or a duplicate
      */
-    protected void updateRecordMap(SourceXmlMetadataExtractorHelper schemaHelper,
+    protected void updateRecordMap(SourceXmlSchemaHelper schemaHelper,
         String deDupRawKey,
         Map<String, ArticleMetadata> uniqueRecordMap,
         ArticleMetadata thisAM) {
@@ -322,7 +203,7 @@ implements FileMetadataExtractorFactory {
      * This method could be overridden for a child plugin that needs to 
      * consolidate records in a different way.
      */
-    protected void consolidateRecords(SourceXmlMetadataExtractorHelper schemaHelper,
+    protected void consolidateRecords(SourceXmlSchemaHelper schemaHelper,
         ArticleMetadata intoAM, ArticleMetadata fromAM) {
 
       String consolidationXPathKey = schemaHelper.getConsolidationXPathKey();
@@ -349,7 +230,7 @@ implements FileMetadataExtractorFactory {
      * @param thisAM
      * @return
      */
-    protected boolean preEmitCheck(SourceXmlMetadataExtractorHelper schemaHelper, 
+    protected boolean preEmitCheck(SourceXmlSchemaHelper schemaHelper, 
         CachedUrl cu, ArticleMetadata thisAM) {
 
       log.debug3("in SourceXmlMetadataExtractor preEmitCheck");
@@ -404,50 +285,20 @@ implements FileMetadataExtractorFactory {
       log.debug3(filename + " does not exist in this AU");
       return false; //No files found that match this record
     }
-    
+
     // a routine to allow a child to add in some post-cook processing for each
     // AM record (eg. "putifbetter"
     // Default is to do nothing
-    protected void postCookProcess(SourceXmlMetadataExtractorHelper schemaHelper, 
+    protected void postCookProcess(SourceXmlSchemaHelper schemaHelper, 
         CachedUrl cu, ArticleMetadata thisAM) {
 
       log.debug3("in SourceXmlMetadataExtractor postEmitProcess");
     }
 
     /* 
-     * Use the helper class defined in the plugin to set up the necessary
-     * information for extraction. If this doesn't happen sucessfully, no
-     * metadata is emitted. 
-     * Validate the return values where necessary.
+     * A particular XML extractor might inherit the rest of the base methods
+     * but it MUST implement a definition for a specific schema
      */
-    private SourceXmlMetadataExtractorHelper setUpSchema(CachedUrl cu) {
-      ArchivalUnit au = cu.getArchivalUnit();
-
-      TypedEntryMap auDefMap = AuUtil.getPluginDefinition(au);
-      String helperName = null;
-      // must check for key first or getString would throw exception
-      if(auDefMap.containsKey(PLUGIN_SOURCE_XML_HELPER_CLASS_KEY)){
-        helperName = auDefMap.getString(PLUGIN_SOURCE_XML_HELPER_CLASS_KEY);
-      } else {
-        return null;
-      }
-
-      SourceXmlMetadataExtractorHelper helper = (SourceXmlMetadataExtractorHelper)schemaHelperMap.get(helperName);
-      if (helper != null) {
-        return helper;
-      }
-
-      try {
-        helper = 
-            (SourceXmlMetadataExtractorHelper)au.getPlugin().newAuxClass(helperName, 
-                SourceXmlMetadataExtractorHelper.class);
-        schemaHelperMap.put(helperName, helper);
-        return helper;
-      } catch (PluginException.InvalidDefinition ide) {
-        log.error("Couldn't load xml definition helper " + helperName +
-            ": ", ide);
-        return null;
-      }
-    }
+    protected abstract SourceXmlSchemaHelper setUpSchema();
   }
 }
---
+++ plugins/src/org/lockss/plugin/clockss/SourceXmlSchemaHelper.java
@@ -0,0 +1,143 @@
+/*
+ * $Id: SourceXmlSchemaHelper.java,v 1.1 2014-01-28 22:06:38 alexandraohlson Exp $
+ */
+
+/*
+
+ Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.clockss;
+
+import org.apache.commons.collections.map.MultiValueMap;
+import org.lockss.extractor.XmlDomMetadataExtractor.XPathValue;
+
+import java.util.*;
+
+
+/** Class to set up specific schema information for the
+ * SourceXmlMetadataExtractor. A helper class defines the schema
+ * and provides the information to the extractor via the get methods.
+ */
+
+public interface SourceXmlSchemaHelper {
+
+  /**
+   * Return the map for metadata that carries across all records in this XML
+   * schema. It can be null. <br/>
+   * If null, only article level information is retrieved.
+   * @return
+   */
+  public Map<String,XPathValue> getGlobalMetaMap();
+
+  /**
+   * Return the map for metadata that is specific to each "article" record.
+   * @return
+   */
+  public Map<String,XPathValue> getArticleMetaMap();
+
+  /**
+   * Return the xPath string which defines one "article" record
+   * It can be null.<br/>
+   * If null, xPath matching starts at the root of the document.
+   * @return
+   */
+  public String getArticleNode();
+
+  /**
+   * Return the map to translate from raw metadata to cooked metadata
+   * This must be set or no metadata gets emitted
+   * @return
+   */
+  public MultiValueMap getCookMap();
+
+
+  /**
+   * Return the xPath key for an item in the record that identifies a 
+   * unique article, even if multiple records provide information about 
+   * different manifestations of that article. For example, an ISBN13 will
+   * be consistent, even if there are different records for each book 
+   * type (eg. pdf, epub, etc)
+   * The key will be used to combine metadata from multiple records in to
+   * one ArticleMetadata for all associated records. 
+   * It can be null - no record consolidation will happen.
+   * @return
+   */
+  public String getDeDuplicationXPathKey();
+
+  /**
+   * Used only in consolidateRecords() 
+   * Return the xPath key for the item in the record that should be combined
+   * when multiple records are consolidated because the refer to the same
+   * item. For example - when different formats of a book are described in
+   * separate records, the format description would be the item to 
+   * consolidate when deduping.
+   * It can be null. Consolidation may occur, but the record field will
+   * not be combined.
+   * @return
+   */
+  public String getConsolidationXPathKey();
+
+  /**
+   * Used only in preEmitCheck() which may be overridden by a child
+   * Return the filename prefix to be used when building up a filename to determine
+   * if a file exists before emitting its metadata. It can also contain
+   * path information (relative to current directory for XML file) if needed. 
+   * It can be null if not needed.
+   * @return
+   */
+  public String getFilenamePrefix();
+
+  /**
+   * Used only in preEmitCheck() which may be overridden by a child
+   * Return a list of suffixes for the filenames to be looked for when 
+   * checking for existence before emitting. It could be just the filetype
+   * (eg. [.pdf][.epub]) or it could include additional text if needed
+   * The preEmitCheck will check the suffixes in order until it finds one.
+   * It can be null. 
+   * @return
+   */
+  public ArrayList<String> getFilenameSuffixList();
+
+  /**
+   * Used only in preEmitCheck() which may be overridden by a child
+   * Return the xPath key to use an item from the raw metadata to build the
+   * filename for a preEmitCheck. For example if the ISBN13 value is used
+   * as the filename.
+   * It can be null if the filename doesn't include metadata information.
+   * @return
+   */
+  public String getFilenameXPathKey();
+
+  /**
+   * Indicates whether the XML should be filtered as an InputStream to 
+   * remove XML illegal characters that might be there.  This will only work
+   * for single byte char encodings (such as ISO8859) so check before 
+   * setting.
+   * If you don't need it, best to set it to false.  
+   */
+  public boolean getDoXmlFiltering();
+}
