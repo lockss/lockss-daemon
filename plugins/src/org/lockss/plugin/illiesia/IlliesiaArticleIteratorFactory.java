4c95192a556a55817845bdae925f22467d640137
lytdoan <ldoan@stanford.edu>
2014-02-01 19:06:44 +0000

    New plugin
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@32400 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/illiesia/IlliesiaArticleIteratorFactory.java
@@ -0,0 +1,92 @@
+/*
+ * $Id: IlliesiaArticleIteratorFactory.java,v 1.1 2014-02-01 19:06:44 ldoan Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.illiesia;
+
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
+import org.lockss.daemon.*;
+import org.lockss.extractor.ArticleMetadataExtractor;
+import org.lockss.extractor.ArticleMetadataExtractorFactory;
+import org.lockss.extractor.BaseArticleMetadataExtractor;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.*;
+import org.lockss.util.Logger;
+
+/*
+ * Each article consists of a full text pdf and nothing else.  All abstracts
+ * are listed in one page. ROLE_ARTICLE_METADATA can not be set.
+ * BaseArticleMetadataExtractor provides default metadata from tdb file.
+ *   Issue toc containing abstracts - <illiesiabase>/html/2013.html
+ *   pdf - <illiesiabase>/papers/Illiesia09-01.pdf
+ */
+public class IlliesiaArticleIteratorFactory
+  implements ArticleIteratorFactory, ArticleMetadataExtractorFactory {
+
+  protected static Logger log = 
+      Logger.getLogger(IlliesiaArticleIteratorFactory.class);
+
+  protected static final String ROOT_TEMPLATE = "\"%s\", base_url";
+  
+  protected static final String PATTERN_TEMPLATE =
+      "\"^%spapers/Illiesia[0-9]+-[0-9]+\\.pdf$\", base_url";
+  
+  private Pattern PDF_PATTERN = 
+      Pattern.compile("(.+\\.pdf)$", Pattern.CASE_INSENSITIVE);
+
+  private static String PDF_REPLACEMENT = "$1";
+
+  @Override
+  public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au, 
+      MetadataTarget target) throws PluginException {
+    
+    SubTreeArticleIteratorBuilder builder = 
+                                        new SubTreeArticleIteratorBuilder(au);    
+    
+    builder.setSpec(target,
+        ROOT_TEMPLATE, PATTERN_TEMPLATE, Pattern.CASE_INSENSITIVE);
+ 
+    // full text pdf - aspect that will trigger an ArticleFiles
+    builder.addAspect(
+        PDF_PATTERN, PDF_REPLACEMENT, ArticleFiles.ROLE_FULL_TEXT_PDF);
+                            
+    return builder.getSubTreeArticleIterator();
+  }  
+  
+  @Override
+  public ArticleMetadataExtractor createArticleMetadataExtractor(
+      MetadataTarget target) throws PluginException {
+    return new BaseArticleMetadataExtractor(null);
+  }
+
+}
---
+++ plugins/src/org/lockss/plugin/illiesia/IlliesiaMetadataExtractorFactory.java
@@ -0,0 +1,106 @@
+/*
+
+ * $Id: IlliesiaMetadataExtractorFactory.java,v 1.1 2014-02-01 19:06:44 ldoan Exp $
+ */
+
+/*
+
+ Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.illiesia;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.ArticleMetadata;
+import org.lockss.extractor.FileMetadataExtractor;
+import org.lockss.extractor.FileMetadataExtractorFactory;
+import org.lockss.extractor.MetadataField;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.util.Logger;
+
+/*
+ * Each article consists of a full text pdf and nothing else.  All abstracts
+ * are listed in one page. ROLE_ARTICLE_METADATA can not be set.
+ * BaseArticleMetadataExtractor provides default metadata from tdb file.
+ *   Issue toc containing abstracts - <illiesiabase>/html/2013.html
+ *   pdf - <illiesiabase>/papers/Illiesia09-01.pdf
+ * The cached url is pdf.
+ * 
+ * A sample metadata from daemon:
+ * ArticleFiles
+ *  Full text CU: <illiesiabase>/papers/Illiesia08-01.pdf
+ *  FullTextPdfFile: <illiesiabase>/papers/Illiesia08-01.pdf
+ * Metadata
+ *  access.url: <illiesiabase>/papers/Illiesia08-01.pdf
+ *  date: 2012
+ *  eissn: 1854-0392
+ *  issn: 1855-5810
+ *  journal.title: Illesia International Journal of Stonefly Research
+ *  publisher: Illiesia
+ * Raw Metadata (empty)
+ */
+public class IlliesiaMetadataExtractorFactory 
+  implements FileMetadataExtractorFactory {
+  
+  static Logger log = Logger.getLogger(IlliesiaMetadataExtractorFactory.class);
+  
+  public FileMetadataExtractor createFileMetadataExtractor(
+      MetadataTarget target, String contentType) throws PluginException {
+    return new IlliesiaMetadataExtractor();
+  }
+
+  // Gets default metadata from tdb: date, journal.title, publisher, 
+  public static class IlliesiaMetadataExtractor 
+    implements FileMetadataExtractor {
+
+    // get article number from pdf url
+    private Pattern ARTICLE_NUM_PATTERN = 
+      Pattern.compile("/papers/Illiesia[0-9]+-([0-9]+).pdf$");      
+
+    @Override
+    public void extract(MetadataTarget target, 
+        CachedUrl pdfCu, Emitter emitter) throws IOException {
+      
+      log.debug3("Metadata - cachedurl pdf cu:" + pdfCu.getUrl());
+            
+      ArticleMetadata am = new ArticleMetadata();
+      Matcher mat = ARTICLE_NUM_PATTERN.matcher(pdfCu.getUrl());
+      if (mat.find()) {
+        String articleNum = mat.group(1);
+        am.put(MetadataField.FIELD_ITEM_NUMBER, articleNum);
+      }
+
+      emitter.emitMetadata(pdfCu, am);
+    }
+  }
+  
+}
+ 
