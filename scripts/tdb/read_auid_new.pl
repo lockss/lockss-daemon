c9116b9a2994ab5b9eb5b82a72ed68e14acf45af
mellen22 <maryellen@lockss.org>
2014-02-26 01:22:46 +0000

    Add Thieme
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@32877 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -280,9 +280,9 @@ while (my $line = <>) {
         my $resp = $ua->request($req);
         if ($resp->is_success) {
             my $man_contents = $resp->content;
-            if (defined($man_contents) && ($man_contents =~ m/$clockss_tag/)) {
+            if (defined($man_contents) && ($man_contents =~ m/$clockss_tag/) && ($man_contents =~ m/Year $param{volume_name}/)) {
                 if ($man_contents =~ m/<h1>(.*)<\/h1>/si) {
-                    $vol_title = $1 #. "Volume " . $param{year};
+                    $vol_title = uri_unescape($1)
                 }
                 $result = "Manifest"
             } else {
