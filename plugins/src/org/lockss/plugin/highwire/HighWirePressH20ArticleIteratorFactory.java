d9b9317709f36d865e0604e2631185b914c5fd0a
etenbrink <etenbrink@yahoo.com>
2014-03-03 21:47:41 +0000

    Move final Strings out of method; set ROLE_ARTICLE_METADATA with setRoleFromOtherRoles
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@32980 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/highwire/HighWirePressH20ArticleIteratorFactory.java
+++ plugins/src/org/lockss/plugin/highwire/HighWirePressH20ArticleIteratorFactory.java
@@ -1,10 +1,10 @@
 /*
- * $Id: HighWirePressH20ArticleIteratorFactory.java,v 1.12 2013-09-16 22:10:00 etenbrink Exp $
+ * $Id: HighWirePressH20ArticleIteratorFactory.java,v 1.13 2014-03-03 21:47:41 etenbrink Exp $
  */
 
 /*
 
-Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -44,91 +44,94 @@ import org.lockss.util.Logger;
 public class HighWirePressH20ArticleIteratorFactory
     implements ArticleIteratorFactory,
                ArticleMetadataExtractorFactory {
-
+  
   protected static Logger log =
-    Logger.getLogger("HighWirePressH20ArticleIteratorFactory");
-
+    Logger.getLogger(HighWirePressH20ArticleIteratorFactory.class);
+  
   protected static final String ROOT_TEMPLATE =
     "\"%scontent/%s/\", base_url, volume_name";
   
   protected static final String PATTERN_TEMPLATE =
     "\"^%scontent/%s/((?:[^/]+/)?[^/]+)(?:.*[.]body|[.]full(?:[.]pdf)?)$\", " +
     "base_url, volume_name";
-
+  
+  // various aspects of an article
+  // http://bjo.bmj.com/content/96/1/1.full
+  // http://bjo.bmj.com/content/96/1/1.full.pdf
+  // http://bjo.bmj.com/content/96/1/1.full.pdf+html
+  // http://bjo.bmj.com/content/96/1/1.extract
+  // http://bjo.bmj.com/content/96/1/1.short
+  // http://bjo.bmj.com/content/96/1/1.citation (usually no links)
+  // and 
+  // http://aapredbook.aappublications.org/content/1/SEC70/SEC72/SEC74.body
+  
+  protected static final Pattern BODY_PATTERN = Pattern.compile(
+      "/([^/]+)[.]body$", Pattern.CASE_INSENSITIVE);
+  
+  protected static final Pattern HTML_PATTERN = Pattern.compile(
+      "/([^/]+)[.]full$", Pattern.CASE_INSENSITIVE);
+  
+  protected static final Pattern PDF_PATTERN = Pattern.compile(
+      "/([^/]+)[.]full[.]pdf$", Pattern.CASE_INSENSITIVE);
+  
+  // how to change from one form (aspect) of article to another
+  protected static final String HTML_REPLACEMENT = "/$1.full";
+  protected static final String PDF_REPLACEMENT = "/$1.full.pdf";
+  protected static final String PDF_LANDING_REPLACEMENT = "/$1.full.pdf+html";
+  protected static final String ABSTRACT_REPLACEMENT = "/$1.abstract";
+  protected static final String EXTRACT_REPLACEMENT = "/$1.extract";
+  protected static final String BODY_REPLACEMENT = "/$1.body";
+  protected static final String FIGURES_REPLACEMENT = "/$1.figures-only";
+  protected static final String SUPPL_REPLACEMENT = "/$1/suppl/DC1";
+  
+  
   public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au,
                                                       MetadataTarget target)
       throws PluginException {
     SubTreeArticleIteratorBuilder builder = new SubTreeArticleIteratorBuilder(au);
     
-    // various aspects of an article
-    // http://bjo.bmj.com/content/96/1/1.full
-    // http://bjo.bmj.com/content/96/1/1.full.pdf
-    // http://bjo.bmj.com/content/96/1/1.full.pdf+html
-    // http://bjo.bmj.com/content/96/1/1.extract
-    // http://bjo.bmj.com/content/96/1/1.short
-    // http://bjo.bmj.com/content/96/1/1.citation (usually no links)
-    // and 
-    // http://aapredbook.aappublications.org/content/1/SEC70/SEC72/SEC74.body
-    
-    final Pattern BODY_PATTERN = Pattern.compile(
-        "/([^/]+)[.]body$", Pattern.CASE_INSENSITIVE);
-    
-    final Pattern HTML_PATTERN = Pattern.compile(
-        "/([^/]+)[.]full$", Pattern.CASE_INSENSITIVE);
-    
-    final Pattern PDF_PATTERN = Pattern.compile(
-        "/([^/]+)[.]full[.]pdf$", Pattern.CASE_INSENSITIVE);
-    
-    // how to change from one form (aspect) of article to another
-    final String HTML_REPLACEMENT = "/$1.full";
-    final String PDF_REPLACEMENT = "/$1.full.pdf";
-    final String PDF_LANDING_REPLACEMENT = "/$1.full.pdf+html";
-    final String ABSTRACT_REPLACEMENT = "/$1.abstract";
-    final String EXTRACT_REPLACEMENT = "/$1.extract";
-    final String BODY_REPLACEMENT = "/$1.body";
-    final String FIGURES_REPLACEMENT = "/$1.figures-only";
-    final String SUPPL_REPLACEMENT = "/$1/suppl/DC1";
-    
     builder.setSpec(target,
         ROOT_TEMPLATE, PATTERN_TEMPLATE, Pattern.CASE_INSENSITIVE);
-
+    
     // set up full or full.pdf to be an aspect that will trigger an ArticleFiles
     // NOTE - for the moment this also means full is considered a FULL_TEXT_CU 
     // until this is deprecated
     builder.addAspect(
         BODY_PATTERN, BODY_REPLACEMENT,
-        ArticleFiles.ROLE_FULL_TEXT_HTML,
-        ArticleFiles.ROLE_ARTICLE_METADATA);
-
+        ArticleFiles.ROLE_FULL_TEXT_HTML);
+    
     builder.addAspect(
         HTML_PATTERN, HTML_REPLACEMENT,
-        ArticleFiles.ROLE_FULL_TEXT_HTML,
-        ArticleFiles.ROLE_ARTICLE_METADATA);
-
+        ArticleFiles.ROLE_FULL_TEXT_HTML);
+    
     builder.addAspect(
         PDF_PATTERN, PDF_REPLACEMENT,
         ArticleFiles.ROLE_FULL_TEXT_PDF);
-
+    
     // set up pdf landing page to be an aspect
     builder.addAspect(
         PDF_LANDING_REPLACEMENT,
-        ArticleFiles.ROLE_FULL_TEXT_PDF_LANDING_PAGE,
-        ArticleFiles.ROLE_ARTICLE_METADATA);
-
+        ArticleFiles.ROLE_FULL_TEXT_PDF_LANDING_PAGE);
+    
     // set up abstract/extract to be an aspect
     builder.addAspect(Arrays.asList(
         ABSTRACT_REPLACEMENT, EXTRACT_REPLACEMENT),
-        ArticleFiles.ROLE_ABSTRACT,
-        ArticleFiles.ROLE_ARTICLE_METADATA);
-
+        ArticleFiles.ROLE_ABSTRACT);
+    
     // set up figures-only to be an aspect
     builder.addAspect(FIGURES_REPLACEMENT,
         ArticleFiles.ROLE_FIGURES_TABLES);
-
+    
     // set up suppl to be an aspect
     builder.addAspect(SUPPL_REPLACEMENT,
         ArticleFiles.ROLE_SUPPLEMENTARY_MATERIALS);
-
+    
+    // add metadata role from abstract, html, or pdf (NOTE: pdf metadata gets DOI from filename)
+    builder.setRoleFromOtherRoles(ArticleFiles.ROLE_ARTICLE_METADATA, Arrays.asList(
+        ArticleFiles.ROLE_ABSTRACT,
+        ArticleFiles.ROLE_FULL_TEXT_PDF_LANDING_PAGE,
+        ArticleFiles.ROLE_FULL_TEXT_HTML));
+    
     // The order in which we want to define full_text_cu.
     // First one that exists will get the job
     builder.setFullTextFromRoles(
@@ -144,5 +147,5 @@ public class HighWirePressH20ArticleIteratorFactory
       throws PluginException {
     return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
   }
-
+  
 }
