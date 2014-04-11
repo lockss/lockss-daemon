49241f1a64d366e226774e36fb6e00f36a50d1fe
etenbrink <etenbrink@yahoo.com>
2014-04-11 18:27:09 +0000

    Cosmetic, add cvs id
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@33702 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/highwire/HighWirePressH20HtmlMetadataExtractorFactory.java
+++ plugins/src/org/lockss/plugin/highwire/HighWirePressH20HtmlMetadataExtractorFactory.java
@@ -1,6 +1,10 @@
 /*
+ * $Id: HighWirePressH20HtmlMetadataExtractorFactory.java,v 1.5 2014-04-11 18:27:09 etenbrink Exp $
+ */
 
-Copyright (c) 2000-2011 Board of Trustees of Leland Stanford Jr. University,
+/*
+
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
