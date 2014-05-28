80dcfd8b40c4c6c4fa7b7b35ae0ed652ff6ad739
tlipkis <tal@pss.com>
2014-05-28 00:16:27 +0000

    Added internMapValue(key, value), interns values of selected keys,  configurable with org.lockss.stringPool.<name>.mapKeys. Added default keys whose values to intern in several pools.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/branches/branch_1-65@34662 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/util/StringPool.java
+++ src/org/lockss/util/StringPool.java
@@ -1,10 +1,10 @@
 /*
- * $Id: StringPool.java,v 1.3 2013-08-08 06:01:27 tlipkis Exp $
+ * $Id: StringPool.java,v 1.3.8.1 2014-05-28 00:13:09 tlipkis Exp $
  */
 
 /*
 
-Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -32,7 +32,7 @@ in this Software without prior written authorization from Stanford University.
 
 package org.lockss.util;
 import java.util.*;
-
+import org.lockss.config.*;
 
 /**
  * Named intern() pools for Strings.  Similer to String.intern(), but use
@@ -41,14 +41,44 @@ import java.util.*;
  */
 public class StringPool {
 
-  public static StringPool AU_CONFIG_PROPS = new StringPool("AU config props");
+  static final String PREFIX = Configuration.PREFIX + "stringPool.";
+
+  /** List of mep keys whose value should be interned in the named pool.
+   * Defaults are pool-specific; See {@link org.lockss.StringPool} static
+   * fields. */
+  static final String PARAM_MAP_KEYS = PREFIX + "<poolname>.mapKeys";
+  static final String SUFFIX_MAP_KEYS = "mapKeys";
+
+
+  /** Pool for AU config property names. */
+  public static StringPool AU_CONFIG_PROPS =
+    new StringPool("AU config props").setMapKeys(ListUtil.list("year"));
+  /** Pool for TdbAu props. */
+  public static StringPool TDBAU_PROPS =
+    new StringPool("TdbAu props").setMapKeys(ListUtil.list("type", "issn",
+							   "issn1"));
+  /** Pool for TdbAu attrs. */
+  public static StringPool TDBAU_ATTRS =
+    new StringPool("TdbAu attrs").setMapKeys(ListUtil.list("publisher",
+							   "au_feature_key",
+							   "year",
+							   "rights"));
+  /** Pool for HTTP header names. */
   public static StringPool HTTP_HEADERS = new StringPool("HTTP headers");
+  /** Pool for plugin IDs. */
   public static StringPool PLUGIN_IDS = new StringPool("Plugin IDs");
-  public static StringPool FEATURE_VERSIONS = new StringPool("Feature Versions");
+  /** Pool for feature version strings. */
+  public static StringPool FEATURE_VERSIONS = new StringPool("Feature versions");
+  /** Pool for PropertyTree keys and subkeys. */
+  public static StringPool PROPERTY_TREE = new StringPool("Property trees");
+
+  private static Map<String,StringPool> pools;
 
   private String name;
   private Map<String,String> map;
   private boolean sealed = false;
+  private Set mapKeys = Collections.EMPTY_SET;
+  private int hits = 0;
 
   public StringPool(String name) {
     this(name, 20);
@@ -58,27 +88,85 @@ public class StringPool {
   public StringPool(String name, int initialSize) {
     this.name = name;
     map = new HashMap<String,String>(initialSize);
+    registerPool(name, this);
+  }
+
+  private static void registerPool(String name, StringPool pool) {
+    if (pools == null) {
+      pools = new HashMap<String,StringPool>();
+    }
+    pools.put(name, pool);
+    Configuration poolConf =
+      ConfigManager.getCurrentConfig().getConfigTree(PREFIX + "." + name);
+    if (poolConf != null) {
+      pool.setPoolConfig(poolConf);
+    }
+  }
+
+  /** Called by org.lockss.config.MiscConfig
+   */
+  public static void setConfig(Configuration config,
+                               Configuration oldConfig,
+                               Configuration.Differences diffs) {
+    if (diffs.contains(PREFIX)) {
+      Configuration allPools = config.getConfigTree(PREFIX);
+      for (Iterator<String> iter = allPools.nodeIterator(); iter.hasNext(); ) {
+	String poolName = iter.next();
+	StringPool pool = getPool(poolName);
+	if (pool != null) {
+	  Configuration poolConf = allPools.getConfigTree(poolName);
+	  if (poolConf != null) {
+	    pool.setPoolConfig(poolConf);
+	  }
+	}
+      }
+    }
+  }
+
+  public void setPoolConfig(Configuration poolConfig) {
+    setMapKeys(poolConfig.getList(SUFFIX_MAP_KEYS, Collections.EMPTY_LIST));
+  }    
+
+  public static StringPool getPool(String name) {
+    return pools.get(name);
+  }
+
+  private StringPool setMapKeys(Collection<String> keys) {
+    mapKeys = SetUtil.theSet(keys);
+    return this;
   }
 
   /** Return the instance of the string already in the pool, if any, else
    * add this instance and return it.
    * @param str the String to be interned.  If null, null is returned. */
   public synchronized String intern(String str) {
-    if (true) {
-      if (str == null) {
-	return str;
-      }
-      String res = map.get(str);
-      if (res == null) {
-	if (sealed) {
-	  return str;
-	}
-	map.put(str, str);
-	res = str;
-      }
-      return res;
-    } else {
+    if (str == null) {
       return str;
+    }
+    String res = map.get(str);
+    if (res != null) {
+      hits++;
+      return res;
+    }
+    if (sealed) {
+      return str;
+    }
+    map.put(str, str);
+    return str;
+  }
+
+  /** Intern the value iff the key is a member of this StringPool's set of
+   * map keys whose values should be interned.
+   * @param key the map key
+   * @param val the String to be stored in the map.
+   * @return the interned value if the key is contained in the set of map
+   * keys whose values should be interned, else the original value.
+   */
+  public synchronized String internMapValue(String key, String val) {
+    if (mapKeys.contains(key)) {
+      return intern(val);
+    } else {
+      return val;
     }
   }
 
@@ -106,7 +194,17 @@ public class StringPool {
 
   public String toStats() {
     return "[StringPool " + name + ", " + map.size() + " entries, " +
+      hits + " hits, " +
       sumStringChars() + " total chars]";
+  }
+
+  public static String allStats() {
+    StringBuilder sb = new StringBuilder();
+    for (StringPool pool : pools.values()) {
+      sb.append(pool.toStats());
+      sb.append("\n");
+    }
+    return sb.toString();
   }
 
 }
