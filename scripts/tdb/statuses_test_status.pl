1311761c1779d60a044e8b406abf5ba24686102e
mellen22 <maryellen@lockss.org>
2014-07-01 04:11:45 +0000

    Script that will replace the plugin with plugin+status in test reports.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@35452 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ scripts/tdb/statuses_test_status.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+#
+# Filter a table and perform text substitutions on the 2nd field of the line.
+# Used for replacing the plugin with plugin+status in test reports.
+
+my %new_text = ();
+
+my $sub_fname = shift(@ARGV);
+open(IFILE, "<$sub_fname") ||
+    die("Could not open substitution file '$sub_fname'!\n");
+while (my $line = <IFILE>) {
+    if ($line =~ m/(\S+)\t+(\S+)/) {
+        $new_text{$1} = $2;
+    }
+}
+
+while (my $line = <>) {
+    chomp($line);
+    my @F = split(/\t+/, $line);
+    my @G = ();
+    for (my $x = 0; $x < @F; ++$x) {
+        push(@G, $F[$x]);
+        if ($x == 1) {
+            my $new_field = "unknown";
+            if (exists($new_text{$F[1]})) {
+                $new_field = $new_text{$F[$x]};
+            }
+            push(@G, $new_field);
+        }
+    };
+    print join("\t", @G) . "\n";
+}
+
+exit(0);
