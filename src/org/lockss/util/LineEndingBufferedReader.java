a75a41c3e97d26cec67daece48eea883b31a9a42
thibgc <github@thib.org>
2014-07-22 02:07:28 +0000

    A BufferedReader/LineNumberReader look-alike that retains line endings (\r\n, \r or \n).
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@35867 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/util/LineEndingBufferedReader.java
@@ -0,0 +1,376 @@
+/*
+ * $Id: LineEndingBufferedReader.java,v 1.1 2014-07-22 02:07:28 thib_gc Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+
+import java.io.*;
+
+/**
+ * <p>
+ * A buffered {@link Reader} that also gives a way to consume input line by
+ * line like {@link BufferedReader} but with the line endings preserved.
+ * </p>
+ * <p>
+ * Line endings are defined as <code>"\r\n"</code> (Windows-style), or
+ * <code>"\r"</code> not followed by <code>"\n"</code> (Macintosh-style), or
+ * <code>"\n"</code> not preceded by <code>"\r"</code> (Unix-style). The last
+ * line returned will not have a line ending if the input does not end with one.
+ * </p>
+ * <p>
+ * This class also gives a way to count lines as it goes, similarly to
+ * {@link LineNumberReader}. The documentation of
+ * {@link LineNumberReader#getLineNumber()} does not make it perfectly clear
+ * that the line number being returned is that of the line most recently read,
+ * but it is, and this is what this class does, via
+ * {@link #getLineCount()}. That number is zero before any input is read and
+ * remains the same after end of file as just before end of file, and in this
+ * class comes from {@link #getLineCount()}. There is no equivalent to
+ * {@link LineNumberReader#setLineNumber(int)} but it could be achieved with
+ * a subclass.
+ * </p>
+ * 
+ * @author Thib Guicherd-Callin
+ * @since 1.66
+ * @see BufferedReader
+ * @see LineNumberReader
+ */
+public class LineEndingBufferedReader extends Reader {
+
+  /**
+   * <p>
+   * The underlying {@link Reader} this class consumes.
+   * </p>
+   * 
+   * @since 1.66
+   */
+  protected Reader underlyingReader;
+  
+  /**
+   * <p>
+   * This class' internal buffer.
+   * </p>
+   * 
+   * @since 1.66
+   */
+  protected char[] myBuffer;
+  
+  /**
+   * <p>
+   * The current index into the internal buffer, {@link #myBuffer}.
+   * </p>
+   * 
+   * @since 1.66
+   */
+  protected int myIndex;
+  
+  /**
+   * <p>
+   * A flag set when end of file has been reached in the underlying
+   * {@link Reader}.
+   * </p>
+   * 
+   * @since 1.66
+   * @see #isEof()
+   */
+  protected boolean seenEof;
+  
+  /**
+   * <p>
+   * A flag set when this instance has been closed.
+   * </p>
+   * 
+   * @since 1.66
+   * @see #close()
+   */
+  protected boolean isClosed;
+  
+  /**
+   * <p>
+   * The length of {@link #myBuffer}, except when the underlying {@link Reader}
+   * reaches end of file and there is not enough input left to fill the
+   * internal buffer, in which case it is the maximum index into the internal
+   * buffer.
+   * </p>
+   * 
+   * @since 1.66
+   * @see #isEof()
+   */
+  protected int maxBufferIndex;
+  
+  /**
+   * <p>
+   * The total number of lines read so far by {@link #readLine()}.
+   * </p>
+   * 
+   * @see #getLineCount()
+   */
+  protected int lineCount;
+  
+  /**
+   * <p>
+   * The default internal buffer size ({@value #BUFFER_SIZE_DEFAULT} bytes).
+   * </p>
+   * 
+   * @since 1.66
+   */
+  public static final int BUFFER_SIZE_DEFAULT = 8192;
+  
+  /**
+   * <p>
+   * Makes a new instance with a default internal buffer size.
+   * </p>
+   * 
+   * @param underlyingReader
+   *          The {@link Reader} being consumed.
+   * @since 1.66
+   * @see #BUFFER_SIZE_DEFAULT
+   */
+  public LineEndingBufferedReader(Reader underlyingReader) {
+    this(underlyingReader, BUFFER_SIZE_DEFAULT);
+  }
+  
+  /**
+   * <p>
+   * Makes a new instance with the specified internal buffer size.
+   * </p>
+   * 
+   * @param underlyingReader
+   *          The {@link Reader} being consumed.
+   * @param bufferSize
+   *          The desired internal buffer size.
+   * @since 1.66
+   */
+  public LineEndingBufferedReader(Reader underlyingReader,
+                                  int bufferSize) {
+    this.underlyingReader = underlyingReader;
+    this.myBuffer = new char[bufferSize];
+    this.myIndex = myBuffer.length;
+    this.maxBufferIndex = myBuffer.length;
+    this.seenEof = false;
+    this.isClosed = false;
+    this.lineCount = 0;
+  }
+  
+  @Override
+  public void close() throws IOException {
+    if (isClosed) {
+      return;
+    }
+    isClosed = true;
+    underlyingReader.close();
+  }
+
+  @Override
+  public int read(char[] buf, int off, int len) throws IOException {
+    throwIfClosed();
+    if (isEof()) {
+      return -1;
+    }
+    refill();
+    int charsAvailable = maxBufferIndex - myIndex;
+    int charsRequested = Math.min(buf.length - off, len);
+    int charsToProcess = Math.min(charsAvailable, charsRequested);
+    System.arraycopy(myBuffer, myIndex, buf, off, charsToProcess);
+    myIndex += charsToProcess;
+    return charsToProcess;
+  }
+  
+  /**
+   * <p>
+   * Read one line of input until a line ending (or end of file), incrementing
+   * the total line count returned by {@link #getLineCount()} in the process.
+   * </p>
+   * <p>
+   * Line endings are defined as <code>"\r\n"</code> (Windows-style), or
+   * <code>"\r"</code> not followed by <code>"\n"</code> (Macintosh-style), or
+   * <code>"\n"</code> not preceded by <code>"\r"</code> (Unix-style). The last
+   * line returned will not have a line ending if the input does not end with
+   * one. When end of file has been reached, this method returns
+   * <code>null</code>.
+   * </p>
+   * 
+   * @return A line of text including its line ending (or not, if it is the last
+   *         line in the input and the input does not end with a line ending),
+   *         or <code>null</code> if end of file has been reached.
+   * @throws IOException
+   *           if the underlying {@link Reader} encounters an I/O exception.
+   * @since 1.66
+   */
+  public String readLine() throws IOException {
+    throwIfClosed();
+    StringBuilder sb = new StringBuilder(myBuffer.length);
+    boolean carriageReturnAcrossRefill = false;
+    int endIndex; // not inclusive, like in substring() and similar
+
+    /*
+     * There are basically five cases.
+     * (1) "\n" (at the end of the buffer or not)
+     * (2) "\r" not at the end of the buffer, followed by "\n"
+     * (3) "\r" not at the end of the buffer, not followed by "\n" 
+     * (4) "\r" at the end of the buffer, followed by "\n" after a refill
+     * (5) "\r" at the end of the buffer, not followed by "\n" after a refill 
+     */
+    
+    _read_loop: while (true) {
+      // Step 1: refill buffer
+      refill();
+      if (isEof()) {
+        if (sb.length() == 0) {
+          // At EOF
+          return null;
+        }
+        else {
+          ++lineCount;
+          return sb.toString();
+        }
+      }
+      
+      // Step 2: look for a line ending
+      for (int i = myIndex ; i < maxBufferIndex ; ++i) {
+        char ch = myBuffer[i];
+        if (ch == '\n') {
+          // Case (1) or (4)
+          endIndex = i + 1;
+          break _read_loop;
+        }
+        else if (!carriageReturnAcrossRefill && ch == '\r') {
+          if (i + 1 < maxBufferIndex) {
+            // Next character readily available; simply check it
+            if (myBuffer[i + 1] == '\n') {
+              endIndex = i + 2; // Case (2)
+            }
+            else {
+              endIndex = i + 1; // Case (3)
+            }
+            break _read_loop;
+          }
+          else {
+            // Need to refill buffer and check one character
+            carriageReturnAcrossRefill = true;
+          }
+        }
+        else if (carriageReturnAcrossRefill) {
+          // Case (5)
+          endIndex = i;
+          break _read_loop;
+        }
+      }
+      
+      // Step 3: still in _read_loop; copy into result
+      sb.append(myBuffer, myIndex, maxBufferIndex - myIndex);
+      myIndex = maxBufferIndex;
+    }
+    
+    // Broke out of _read_loop; endIndex is set
+    sb.append(myBuffer, myIndex, endIndex - myIndex);
+    myIndex = endIndex;
+    ++lineCount;
+    return sb.toString();
+  }
+
+  /**
+   * <p>
+   * Returns the number of lines returned by {@link #readLine()} so far, zero
+   * before any input is read, and the same number after end of file is reached
+   * as just before end of file.
+   * </p>
+   * <p>
+   * Calling {@link #read(char[], int, int)} (et al.) some of the time and
+   * {@link #readLine()} some of the time render the results of this method
+   * meaningless.
+   * </p>
+   * 
+   * @return The total line count so far.
+   * @since 1.66
+   */
+  public int getLineCount() {
+    return lineCount;
+  }
+  
+  /**
+   * <p>
+   * Refills the internal buffer if necessary.
+   * <p>
+   * 
+   * @throws IOException
+   *           if the underlying {@link Reader} encounters an I/O exception
+   * @since 1.66
+   */
+  protected void refill() throws IOException {
+    if (myIndex == maxBufferIndex) {
+      myIndex = 0;
+      int totalRead = 0;
+      int singleRead;
+      do {
+        singleRead = underlyingReader.read(myBuffer, totalRead, myBuffer.length - totalRead);
+        if (singleRead == -1) {
+          seenEof = true;
+        }
+        else {
+          totalRead = totalRead + singleRead;
+        }
+      } while (singleRead != -1 && totalRead < myBuffer.length);
+      maxBufferIndex = totalRead;
+    }
+  }
+  
+  /**
+   * <p>
+   * Throws if the instance is already closed.
+   * </p>
+   * 
+   * @throws IOException
+   *           if the instance is already closed.
+   * @since 1.66
+   * @see #isClosed
+   */
+  protected void throwIfClosed() throws IOException {
+    if (isClosed) {
+      throw new IOException("stream closed");
+    }
+  }
+
+  /**
+   * <p>
+   * Determines if this instance has reached end of file. This is when the
+   * underlying {@link Reader} has reached end of file and the index into the
+   * this instance's internal buffer has reached the maximum number of
+   * characters still available.
+   * </p>
+   * 
+   * @return whether this instance has reached end of file
+   */
+  protected boolean isEof() {
+    return seenEof && myIndex == maxBufferIndex;
+  }
+  
+}
---
+++ test/src/org/lockss/util/TestLineEndingBufferedReader.java
@@ -0,0 +1,228 @@
+/*
+ * $Id: TestLineEndingBufferedReader.java,v 1.1 2014-07-22 02:07:28 thib_gc Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.util;
+
+import java.io.*;
+
+import org.lockss.test.LockssTestCase;
+
+public class TestLineEndingBufferedReader extends LockssTestCase {
+
+  public void testEmptyInput() throws Exception {
+    LineEndingBufferedReader r = new LineEndingBufferedReader(new StringReader(""));
+    assertNull(r.readLine());
+    assertNull(r.readLine());
+  }
+  
+  public void testReadLineAcrossRefills() throws Exception {
+    LineEndingBufferedReader r = new LineEndingBufferedReader(new StringReader("aaaabbbbcc"), 4);
+    assertEquals(0, r.getLineCount());
+    assertEquals("aaaabbbbcc", r.readLine());
+    assertEquals(1, r.getLineCount());
+    assertNull(r.readLine());
+    assertNull(r.readLine());
+    assertEquals(1, r.getLineCount());
+  }
+  
+  public void testNewline() throws Exception {
+    LineEndingBufferedReader r = new LineEndingBufferedReader(new StringReader("aaa\n" + "bb\nc" + "cccc" + "\nd\n"), 4);
+    assertEquals(0, r.getLineCount());
+    assertEquals("aaa\n", r.readLine()); // line ending at end of buffer
+    assertEquals(1, r.getLineCount());
+    assertEquals("bb\n", r.readLine()); // line ending not at end of buffer
+    assertEquals(2, r.getLineCount());
+    assertEquals("ccccc\n", r.readLine()); // line ending across refills
+    assertEquals(3, r.getLineCount());
+    assertEquals("d\n", r.readLine()); // line ending followed by EOF
+    assertEquals(4, r.getLineCount());
+    assertNull(r.readLine());
+    assertNull(r.readLine());
+    assertEquals(4, r.getLineCount());
+  }
+  
+  public void testCarriageReturn() throws Exception {
+    LineEndingBufferedReader r = new LineEndingBufferedReader(new StringReader("aaa\r" + "bb\rc" + "cccc" + "\rd\r"), 4);
+    assertEquals(0, r.getLineCount());
+    assertEquals("aaa\r", r.readLine()); // line ending at end of buffer
+    assertEquals(1, r.getLineCount());
+    assertEquals("bb\r", r.readLine()); // line ending not at end of buffer
+    assertEquals(2, r.getLineCount());
+    assertEquals("ccccc\r", r.readLine()); // line ending across refills
+    assertEquals(3, r.getLineCount());
+    assertEquals("d\r", r.readLine()); // line ending followed by EOF
+    assertEquals(4, r.getLineCount());
+    assertNull(r.readLine());
+    assertNull(r.readLine());
+    assertEquals(4, r.getLineCount());
+  }
+  
+  public void testCarriageReturnNewline() throws Exception {
+    LineEndingBufferedReader r = new LineEndingBufferedReader(new StringReader("aa\r\n" + "b\r\nc" + "ccc\r" + "\nddd" + "dddd" + "\r\nee" + "e\r\n"), 4);
+    assertEquals(0, r.getLineCount());
+    assertEquals("aa\r\n", r.readLine()); // line ending at end of buffer
+    assertEquals(1, r.getLineCount());
+    assertEquals("b\r\n", r.readLine()); // line ending not at end of buffer
+    assertEquals(2, r.getLineCount());
+    assertEquals("cccc\r\n", r.readLine()); // line ending across buffer refill
+    assertEquals(3, r.getLineCount());
+    assertEquals("ddddddd\r\n", r.readLine()); // line ending across refills
+    assertEquals(4, r.getLineCount());
+    assertEquals("eee\r\n", r.readLine()); // line ending followed by EOF
+    assertEquals(5, r.getLineCount());
+    assertNull(r.readLine());
+    assertNull(r.readLine());
+    assertEquals(5, r.getLineCount());
+  }
+  
+  public void testConsecutiveLineEndings() throws Exception {
+    LineEndingBufferedReader r = new LineEndingBufferedReader(new StringReader("aa\r\rbb\n\n" + "cc\r\n\r\n"), 8);
+    assertEquals(0, r.getLineCount());
+    assertEquals("aa\r", r.readLine()); // "\r" followed by "\r"
+    assertEquals(1, r.getLineCount());
+    assertEquals("\r", r.readLine());
+    assertEquals(2, r.getLineCount());
+    assertEquals("bb\n", r.readLine()); // "\n" followed by "\n"
+    assertEquals(3, r.getLineCount());
+    assertEquals("\n", r.readLine());
+    assertEquals(4, r.getLineCount());
+    assertEquals("cc\r\n", r.readLine()); // "\r\n" followed by "\r\n"
+    assertEquals(5, r.getLineCount());
+    assertEquals("\r\n", r.readLine());
+    assertEquals(6, r.getLineCount());
+    assertNull(r.readLine());
+    assertNull(r.readLine());
+    assertEquals(6, r.getLineCount());
+  }
+  
+  public void testNormalRead() throws Exception {
+    LineEndingBufferedReader r = new LineEndingBufferedReader(new StringReader("0123456789"), 4);
+    char[] ch3 = new char[3];
+    // Try to get 3 characters
+    assertEquals(3, r.read(ch3)); // consumes 3 characters, leaves 1 in buffer
+    assertEquals("012", new String(ch3));
+    // Try to get 3 characters
+    assertEquals(1, r.read(ch3)); // consumes last character, doesn't attempt more
+    assertEquals("3", new String(ch3, 0, 1));
+    assertEquals(2, r.read(ch3, 1, 2)); // refills, consumes 2 characters, leaves 2 in buffer
+    assertEquals("45", new String(ch3, 1, 2));
+    // Try to get 3 characters
+    assertEquals(2, r.read(ch3)); // consumes last 2 characters, doesn't attempt more
+    assertEquals("67", new String(ch3, 0, 2));
+    assertEquals(1, r.read(ch3, 2, 1)); // refills, consumes 1 character, leaves 1 in buffer before EOF
+    assertEquals("8", new String(ch3, 2, 1));
+    // Try to get 3 characters
+    assertEquals(1, r.read(ch3)); // consumes last character, reaches EOF
+    assertEquals("9", new String(ch3, 0, 1));
+    assertEquals(-1, r.read(ch3)); // EOF
+    assertEquals(-1, r.read(ch3)); // EOF again
+  }
+  
+  public void testClose() throws Exception {
+    LineEndingBufferedReader r = new LineEndingBufferedReader(new StringReader("aaa\nbbb\n"));
+    assertEquals("aaa\n", r.readLine());
+    r.close();
+    try {
+      r.readLine();
+      fail("Should have thrown \"stream closed\"");
+    }
+    catch (IOException expected) {
+      assertEquals("stream closed", expected.getMessage());
+    }
+    try {
+      char[] ch4 = new char[4];
+      r.read(ch4);
+      fail("Should have thrown \"stream closed\"");
+    }
+    catch (IOException expected) {
+      assertEquals("stream closed", expected.getMessage());
+    }
+    r.close(); // shouldn't throw
+  }
+  
+  /**
+   * <p>
+   * The documentation of {@link LineNumberReader} (a subclass of
+   * {@link BufferedReader}) that counts lines as it goes) is unclear about
+   * whether {@link LineNumberReader#getLineNumber()} means the number of the
+   * line that is about to be read or the number of the line that was most
+   * recently read. It turns out it is the latter. In addition to documenting
+   * {@link LineEndingBufferedReader} as such, the corresponding method is named
+   * {@link LineEndingBufferedReader#getLineCount()} to make it a little
+   * clearer.
+   * </p>
+   * 
+   * @throws Exception
+   *           if any exception occurs
+   * @since 1.66
+   */
+  public void testAssumptionsAboutLineNumberReader() throws Exception {
+    java.io.LineNumberReader lnr = null;
+
+    // With final line ending
+    lnr = new LineNumberReader(new StringReader("aaa\n"));
+    assertEquals(0, lnr.getLineNumber());
+    assertEquals("aaa", lnr.readLine());
+    assertEquals(1, lnr.getLineNumber());
+    assertEquals(null, lnr.readLine());
+    assertEquals(1, lnr.getLineNumber());
+
+    // With no final line ending
+    lnr = new LineNumberReader(new StringReader("aaa"));
+    assertEquals(0, lnr.getLineNumber());
+    assertEquals("aaa", lnr.readLine());
+    assertEquals(1, lnr.getLineNumber());
+    assertEquals(null, lnr.readLine());
+    assertEquals(1, lnr.getLineNumber());
+  }
+  
+  public void testGetLineCount() throws Exception {
+    LineEndingBufferedReader r = null;
+
+    // With final line ending
+    r = new LineEndingBufferedReader(new StringReader("aaa\n"));
+    assertEquals(0, r.getLineCount());
+    assertEquals("aaa\n", r.readLine());
+    assertEquals(1, r.getLineCount());
+    assertEquals(null, r.readLine());
+    assertEquals(1, r.getLineCount());
+
+    // With no final line ending
+    r = new LineEndingBufferedReader(new StringReader("aaa"));
+    assertEquals(0, r.getLineCount());
+    assertEquals("aaa", r.readLine());
+    assertEquals(1, r.getLineCount());
+    assertEquals(null, r.readLine());
+    assertEquals(1, r.getLineCount());
+  }
+
+}
