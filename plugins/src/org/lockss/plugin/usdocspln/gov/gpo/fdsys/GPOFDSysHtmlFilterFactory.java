1a09483ea4a0b14252056e5b9300027ccb4597ff
thibgc <github@thib.org>
2014-08-08 22:09:02 +0000

    HTML filtering
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@36323 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/usdocspln/gov/gpo/fdsys/GPOFDSysHtmlFilterFactory.java
+++ plugins/src/org/lockss/plugin/usdocspln/gov/gpo/fdsys/GPOFDSysHtmlFilterFactory.java
@@ -1,10 +1,10 @@
 /*
- * $Id: GPOFDSysHtmlFilterFactory.java,v 1.6 2012-07-31 21:42:56 thib_gc Exp $
+ * $Id: GPOFDSysHtmlFilterFactory.java,v 1.7 2014-08-08 22:09:02 thib_gc Exp $
  */
 
 /*
 
-Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -35,8 +35,10 @@ package org.lockss.plugin.usdocspln.gov.gpo.fdsys;
 import java.io.*;
 import java.util.List;
 
-import org.htmlparser.NodeFilter;
+import org.htmlparser.*;
 import org.htmlparser.filters.*;
+import org.htmlparser.util.*;
+import org.htmlparser.visitors.NodeVisitor;
 import org.lockss.daemon.PluginException;
 import org.lockss.filter.*;
 import org.lockss.filter.HtmlTagFilter.TagPair;
@@ -52,12 +54,33 @@ public class GPOFDSysHtmlFilterFactory implements FilterFactory {
       throws PluginException {
 	
     NodeFilter[] filters = new NodeFilter[] {
+        /*
+         * Broad area filtering 
+         */
+        /* Document header */
         // Differences in the presence and order of <meta> tags and spacing of the <title> tag
         new TagNameFilter("head"),
-        // Variability of scripts
+        /* Scripts */
         new TagNameFilter("script"),
-        // Some URL targets changed over time
+        new TagNameFilter("noscript"),
+        /* Header */
         HtmlNodeFilters.tagWithAttributeRegex("div", "id", "top-menu-one"),
+        HtmlNodeFilters.tagWithAttributeRegex("div", "id", "top-banner-inside"),
+        HtmlNodeFilters.tagWithAttributeRegex("div", "id", "top-menu-two"),
+        /* Left column */
+        HtmlNodeFilters.tagWithAttributeRegex("div", "id", "page-details-left-mask"),
+        /* Footer */
+        HtmlNodeFilters.tagWithAttributeRegex("div", "id", "footer"),
+        /*
+         * Main area
+         */
+        // Seen in the field: "null" in <a href="/fdsys/search/pagedetails.action?null&amp;bread=true">More Information</a>
+        HtmlNodeFilters.tagWithAttributeRegex("span", "id", "breadcrumbs"),
+        // Whitespace differences inside <div id="page-details-form-mask">
+        HtmlNodeFilters.tagWithAttributeRegex("div", "id", "page-details-form-mask"),
+        /*
+         * Other
+         */
         // Filters the "Email a link to this page" link
         HtmlNodeFilters.tagWithAttributeRegex("a", "href", "^search/notificationPage\\.action\\?emailBody="),
         // Filters session ID from search results
@@ -66,17 +89,40 @@ public class GPOFDSysHtmlFilterFactory implements FilterFactory {
         HtmlNodeFilters.tagWithAttributeRegex("a", "href", "^delivery/getpackage\\.action\\?packageId="),
     };
   
-    OrFilter combinedFilter = new OrFilter(filters);
-    HtmlNodeFilterTransform transform = HtmlNodeFilterTransform.exclude(combinedFilter);
-    InputStream prefilteredStream = new HtmlFilterInputStream(in, encoding, transform);
+    HtmlTransform xform = new HtmlTransform() {
+      @Override
+      public NodeList transform(NodeList nodeList) throws IOException {
+        try {
+          nodeList.visitAllNodesWith(new NodeVisitor() {
+            @Override
+            public void visitTag(Tag tag) {
+              String tagName = tag.getTagName().toLowerCase();
+              if ("a".equals(tagName)) {
+                tag.removeAttribute("onclick");
+              }
+            }
+          });
+          return nodeList;
+        }
+        catch (ParserException pe) {
+          throw new IOException("ParserException inside HtmlTransform", pe);
+        }
+      }
+    };
+    
+    InputStream prefilteredStream =
+        new HtmlFilterInputStream(in,
+                                  encoding,
+                                  new HtmlCompoundTransform(HtmlNodeFilterTransform.exclude(new OrFilter(filters)),
+                                                            xform));
   	    
     List pairs = ListUtil.list(
-    // May contain a session token in a comment
-    new TagPair("<!--<input type=\"hidden\" name=\"struts.token.name\" value=\"struts.token\" />",
-                "\" />-->"),
-    // ...Or not in a comment, just two <input> tags in a row
-    new TagPair("<input type=\"hidden\" name=\"struts.token.name\" value=\"struts.token\" />",
-                "\" />")
+      // May contain a session token in a comment
+      new TagPair("<!--<input type=\"hidden\" name=\"struts.token.name\" value=\"struts.token\" />",
+                  "\" />-->"),
+      // ...Or not in a comment, just two <input> tags in a row
+      new TagPair("<input type=\"hidden\" name=\"struts.token.name\" value=\"struts.token\" />",
+                  "\" />")
     );
   	      
     try {
