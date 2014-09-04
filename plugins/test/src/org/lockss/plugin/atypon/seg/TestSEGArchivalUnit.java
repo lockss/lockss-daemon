5a732dd72354b2cd1e0dc0d643bdc12b9491d600
lytdoan <ldoan@stanford.edu>
2014-09-04 03:16:36 +0000

    Junit tests for new atypon child plugin
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@36815 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/test/src/org/lockss/plugin/atypon/seg/TestSEGArchivalUnit.java
@@ -0,0 +1,210 @@
+/*
+ * $Id: TestSEGArchivalUnit.java,v 1.1 2014-09-04 03:16:35 ldoan Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.atypon.seg;
+
+import java.net.URL;
+import java.util.Properties;
+
+import org.lockss.config.Configuration;
+import org.lockss.daemon.ConfigParamDescr;
+import org.lockss.daemon.RangeCachedUrlSetSpec;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.plugin.CachedUrlSet;
+import org.lockss.plugin.UrlCacher;
+import org.lockss.plugin.base.BaseCachedUrlSet;
+import org.lockss.plugin.definable.DefinableArchivalUnit;
+import org.lockss.plugin.definable.DefinablePlugin;
+import org.lockss.state.AuState;
+import org.lockss.test.ConfigurationUtil;
+import org.lockss.test.LockssTestCase;
+import org.lockss.test.MockAuState;
+import org.lockss.test.MockLockssDaemon;
+import org.lockss.util.ListUtil;
+import org.lockss.util.Logger;
+import org.lockss.util.TimeBase;
+
+/* 
+ * Tests archival unit for Society Of Exploration Geophysicists.
+ */
+
+public class TestSEGArchivalUnit 
+  extends LockssTestCase {
+  
+  private static Logger log = Logger.getLogger(
+      TestSEGArchivalUnit.class);
+  
+  private MockLockssDaemon theDaemon;
+  private DefinableArchivalUnit segAu;
+  private URL baseUrl;
+
+  private static final String PLUGIN_ID = 
+      "org.lockss.plugin.atypon.seg.ClockssSEGPlugin";
+  
+  private static final String PLUGIN_NAME = 
+                "Society Of Exploration Geophysicists (CLOCKSS)";
+
+  private static final String BASE_URL_KEY = 
+      ConfigParamDescr.BASE_URL.getKey();
+  private static final String JOURNAL_ID_KEY = 
+      ConfigParamDescr.JOURNAL_ID.getKey();
+  private static final String VOL_KEY = 
+      ConfigParamDescr.VOLUME_NAME.getKey();
+    
+  private static final String ROOT_URL = "http://www.example.com/";
+  private static final String JOURNAL_ID = "xxxjid";  
+  private static final String VOLUME_NAME = "78";  
+  
+
+  public void setUp() throws Exception {
+    super.setUp();
+    setUpDiskSpace();
+    theDaemon = getMockLockssDaemon();
+    theDaemon.getHashService();
+
+    baseUrl = new URL(ROOT_URL);
+    segAu = makeAu(baseUrl, JOURNAL_ID, VOLUME_NAME);
+  }
+
+  public void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  private DefinableArchivalUnit makeAu(URL url, 
+                                       String journalId, String volumeName) 
+      throws Exception {
+ 
+    Properties props = new Properties();
+    props.setProperty(JOURNAL_ID_KEY, journalId);
+    props.setProperty(VOL_KEY, volumeName);
+    
+    if (url != null) {
+      props.setProperty(BASE_URL_KEY, url.toString());
+    }
+    Configuration config = ConfigurationUtil.fromProps(props);
+    
+    DefinablePlugin segDefinablePlugin = new DefinablePlugin();
+    segDefinablePlugin.initPlugin(getMockLockssDaemon(), PLUGIN_ID);
+    DefinableArchivalUnit au = 
+        (DefinableArchivalUnit)segDefinablePlugin.createAu(config);
+    return au;
+  }
+
+  public void testConstructNullUrl() throws Exception {
+    try {
+      makeAu(null, "yyyjid", "99");
+      fail("Should have thrown ArchivalUnit.ConfigurationException");
+    } catch (ArchivalUnit.ConfigurationException e) {
+    }
+  }
+  
+  // Test the crawl rules for Society Of Exploration Geophysicists
+  public void testShouldCacheProperPages() throws Exception {
+    theDaemon.getLockssRepository(segAu);
+    theDaemon.getNodeManager(segAu);
+    BaseCachedUrlSet cus = new BaseCachedUrlSet(segAu,
+                             new RangeCachedUrlSetSpec(baseUrl.toString()));
+    // Test for pages that should get crawled
+    // permission page/start url
+    // <segbase>/clockss/jid/1/index.html
+    shouldCacheTest(ROOT_URL + "clockss/" + JOURNAL_ID + "/" + VOLUME_NAME 
+                    + "/index.html", true, segAu, cus);  
+    // toc page for an issue - <segbase>/toc/jid/2013/2
+    shouldCacheTest(ROOT_URL + "toc/" + JOURNAL_ID +"/" + VOLUME_NAME + "/2",
+                    true, segAu, cus);
+    // article files
+    // abstract  - <segbase>/doi/abs/11.1111/XXX-2013-0041.1
+    // full text - <segbase>/doi/full/11.1111/XXX-2013-0041.1
+    // pdf       - <segbase>/doi/pdf/11.1111/XXX-2013-0041.1
+    // pdfplus   - <segbase>/doi/pdfplus/11.1111/XXX-2013-0041.1
+
+    shouldCacheTest(ROOT_URL + "doi/abs/11.1111/XXX-2013-0041.1", 
+                    true, segAu, cus);
+    shouldCacheTest(ROOT_URL + "doi/full/11.1111/XXX-2013-0041.1", 
+                    true, segAu, cus);
+    shouldCacheTest(ROOT_URL + "doi/pdf/11.1111/XXX-2013-0041.1",
+                    true, segAu, cus);
+    shouldCacheTest(ROOT_URL + "doi/pdfplus/11.1111/XXX-2013-0041.1",
+                    true, segAu, cus);
+    
+    // should not get crawled - missing doi
+    shouldCacheTest(ROOT_URL + "/full/XXX-2013-0041.1", false, segAu, cus);  
+    // should not get crawled - LOCKSS
+    shouldCacheTest("http://lockss.stanford.edu", false, segAu, cus);
+  }
+  
+  private void shouldCacheTest(String url, boolean shouldCache,
+                               ArchivalUnit au, CachedUrlSet cus) {
+    log.info ("shouldCacheTest url: " + url);
+
+    UrlCacher uc = au.makeUrlCacher(url);
+    assertEquals(shouldCache, uc.shouldBeCached());
+  }
+  
+  public void testStartUrlConstruction() throws Exception {
+    String expected = ROOT_URL + "clockss/" + JOURNAL_ID + "/" 
+                      + VOLUME_NAME + "/index.html";
+    assertEquals(ListUtil.list(expected), 
+                 segAu.getNewContentCrawlUrls());
+  }
+  
+  public void testShouldNotCachePageFromOtherSite() throws Exception {
+    theDaemon.getLockssRepository(segAu);
+    theDaemon.getNodeManager(segAu);
+    UrlCacher uc = segAu.makeUrlCacher(
+        "http://shadow2.stanford.edu/toc/segj/2013/2");
+    assertFalse(uc.shouldBeCached());
+  }
+
+  public void testShouldDoNewContentCrawlTooEarly() throws Exception {
+    AuState aus = new MockAuState(null, TimeBase.nowMs(), -1, -1, null);
+    assertFalse(segAu.shouldCrawlForNewContent(aus));
+  }
+
+  public void testShouldDoNewContentCrawlFor0() throws Exception {
+    AuState aus = new MockAuState(null, 0, -1, -1, null);
+    assertTrue(segAu.shouldCrawlForNewContent(aus));
+  }
+
+  public void testgetName() throws Exception {
+    DefinableArchivalUnit au1 =
+      makeAu(new URL("http://www.example1.com/"), "aaajid", "25");
+    assertEquals(PLUGIN_NAME + ", Base URL http://www.example1.com/,"
+                 + " Journal ID aaajid, Volume 25", au1.getName());
+    DefinableArchivalUnit au2 =
+      makeAu(new URL("http://www.example2.com/"), "bbbjid", "44");
+    assertEquals(PLUGIN_NAME + ", Base URL http://www.example2.com/,"
+                 + " Journal ID bbbjid, Volume 44", au2.getName());
+  }
+
+}
+
---
+++ plugins/test/src/org/lockss/plugin/atypon/seg/TestSEGHtmlFilterFactory.java
@@ -0,0 +1,345 @@
+/*
+ * $Id: TestSEGHtmlFilterFactory.java,v 1.1 2014-09-04 03:16:36 ldoan Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the \"Software\"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.atypon.seg;
+
+import java.io.*;
+
+import junit.framework.Test;
+
+import org.lockss.util.*;
+import org.lockss.config.ConfigManager;
+import org.lockss.config.Configuration;
+import org.lockss.daemon.PluginException;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.plugin.FilterFactory;
+import org.lockss.plugin.PluginManager;
+import org.lockss.plugin.PluginTestUtil;
+import org.lockss.test.*;
+
+public class TestSEGHtmlFilterFactory
+  extends LockssTestCase {
+  
+  FilterFactory variantFact;
+  ArchivalUnit variantAu;
+  String tempDirPath;
+  MockLockssDaemon daemon;
+  PluginManager pluginMgr;
+        
+  private static final String PLUGIN_NAME = 
+      "org.lockss.plugin.atypon.seg.ClockssSEGPlugin";
+  
+  private static final String filteredStr = 
+      "<div class=\"block\"></div>";
+
+  // For crawl filtering
+  // top right of issue toc - links to previous or next issue
+  private static final String withPrevNextNav = 
+      "<div class=\"block\">" +
+          "<div id=\"prevNextNav\">" +
+          "<div id=\"issueSearch\">" +
+          "<form method=\"get\" action=\"/action/doSearch\">" +
+          "<input type=\"text\" size=\"17\" value=\"\" name=\"AllField\">" +
+          "<input type=\"hidden\" value=\"4\" name=\"issue\">" +
+          "<input type=\"hidden\" value=\"xxxxx\" name=\"jcode\">" +
+          "</div><a href=\"javascript:toggleSlide('issueSearch')\">" +
+          "Search Issue</a> |" +
+          "<img src=\"/templates/images/aa32.png\"> " +
+          "<br>" +
+          "<a href=\"/toc/xxxxxx/1/3\">Previous Issue</a>" +
+          "<a href=\"/toc/xxxxxx/2/1\"> Next Issue</a>" +
+          "</div>" +
+          "</div>";
+  
+  // top right of article - links to previous or next article
+  private static final String withArticleToolsNav = 
+      "<div class=\"block\">" +
+          "<div class=\"stackContents\" id=\"articleToolsNav\">" +
+          "<div class=\"stacked\" id=\"articleToolsPrev\">&lt;" +
+          "<a class=\"articleToolsNav\" "+
+          "href=\"/doi/abs/xx.xxxx/xxxx.185\">Previous Article</a>" +
+          "</div>" +
+          "<div class=\"stackedReverse\" id=\"articleToolsNext\">" +
+          "<a class=\"articleToolsNav\" "+
+          "href=\"/doi/abs/xx.xxxx/xxxx.215\">Next Article</a>&gt;" +
+          "</div>" +
+          "<div class=\"groupInfo\">" +
+          "Volume a, Issue b (2012)" +
+          "</div>" +
+          "</div>" +
+          "</div>";
+  
+  // left column of an article - all except Download Citations
+  // <div class="yui3-u yui3-u-1-4 leftColumn">
+  private static final String withLeftColumnExceptDownloadCitation = 
+      "<div class=\"block\">" +
+          "<div class=\"yui3-u yui3-u-1-4 leftColumn\">" +
+          "<div class=\"panel\"><h3>Article Tools</h3></div>" +
+          "<div class=\"box-inner\">" +
+          "<div><a href=\"/action/addFavoritePublication" +
+          "?doi=99.9999%2FJid99.9.999\">Add to my favorites</a></div>" +
+          "<div><a href=\"/action/showCitFormats" +
+          "?doi=99.9999%2FJid99.9.999\">Download Citations</a></div>" +
+          "<div><a href=\"/action/addCitationAlert" +
+          "?doi=99.9999%2FJid99.9.999\">Track Citations</a></div>" +
+          "</div>" +
+          "<div class=\"panel\"><h3>Recommend &amp; Share</h3></div>" +
+          "<div class=\"box-inner\">" +
+          "<div class=\"social\"><a href=\"http://www.facebook.com/sh.php" +
+          "?u=http%3a%2f%2fsegbase%2fdoi%2fabs%2f99.9999%2fJid99.9.999" +
+          "&amp;t=blah+of+blah+blah+for+blah+blah\">Facebook</a></div>" +
+          "</div>" +
+          "<div id=\"sessionHistory\" class=\"panel panel_476\">" +
+          "<div class=\"header\"><h3>Session History</h3></div>" +
+          "<div class=\"sessionViewed\">" +
+          "<div class=\"label\">Recently Viewed</div>" +
+          "<ul class=\"sessionHistory\">" +
+          "<li><a href=\"/doi/abs/99.9999/gabc2012-9999.9\">" +
+          "The cow jumps over the moon</a></li>" +
+          "</ul>" +
+          "</div>" +
+          "</div>" +
+          "</div>" +
+          "</div>";
+  
+  private static final String withoutLeftColumnExceptDownloadCitation = 
+      "<div class=\"block\">" +
+          "<div class=\"yui3-u yui3-u-1-4 leftColumn\">" +
+          "<div class=\"box-inner\">" +
+          "<div><a href=\"/action/showCitFormats" +
+          "?doi=99.9999%2FJid99.9.999\">Download Citations</a></div>" +
+          "</div>" +
+          "</div>" +
+          "</div>";
+  
+  // Errata section
+  private static final String withSecErrata = 
+      "<div class=\"block\">" +
+          "<div id=\"sec_Errata\" class=\"articleGrouping\">" +
+          "<div class=\"header innerSubject\"><input type=\"checkbox\" " +
+          "onclick=\"markid('Errata');\" id=\"markallErrata\" " +
+          "class=\"marksec\"><h3><div class=\"subject\">Errata</div></h3>" +
+          "<a href=\"#top\" class=\"gooo\">" +
+          "<img src=\"/imagessrc/booo.gif\"></a></div><div id=\"Errata\">  " +
+          "<div class=\"nono\" id=\"erratum1\">" +
+          "<table class=\"articleEntry\">" +
+          "<tbody><tr><td width=\"18\" valign=\"top\" align=\"left\">" +
+          "<input type=\"chb\" onclick=\"chacha('erratum1', 'ss', 'nn')\" " +
+          "value=\"99.9999/2013-1009-ERRATUM.1\" " +
+          "name=\"doi\" id=\"erratum1\"><br>" +
+          "<img src=\"/imagessrc/access_full.gif\" " +
+          "alt=\"full access\" title=\"full access\" " +
+          "class=\"accessIcon\"></td><td class=\"toggle\"></td>" +
+          "<td valign=\"top\"><a href=\"/doi/abs/99.9999/erratum.1\" " +
+          "class=\"ref nowrap\"></a>" +
+          "<div class=\"date\">pub date: 31 Oct 2013</div>" +
+          "<a href=\"/doi/abs/99.9999/erratum.1\">Citation</a> | " +
+          "<a href=\"/doi/full/99.9999/erratum.1\">Full Text</a> | " +
+          "<a href=\"/doi/pdf/99.9999/erratum.1\" title=\"tittle open\" " +
+          "pdf\">PDF (26 KB)</a> " +
+          "| <a href=\"/doi/pdfplus/99.9999/erratum.1\" title=\"title open\"" +
+          "pdfplus\">PDF w/Links (28 KB)</a>&nbsp;</td></tr>" +
+          "</tbody></table></div> </div></div>" +
+          "</div>";
+  
+  // external links within References section
+  private static final String withAbstractReferences = 
+      "<div class=\"block\">" +
+          "<div class=\"abstractReferences\">" +
+          "<div class=\"sectionHeadingContainer\">" +
+          "<ol>" +
+          "<li class=\"reference oList\">Smith, and Maxwell, 2001. " +
+          "Rewiew X, 22 pp. </li>" +
+          "<li class=\"reference oList\">" +
+          "<a href=\"www.estcp.org\">www.estcp.org</a>" +
+          ", 101 pp." +
+          "</li>" +
+          "</ol>" +
+          "</div>" +
+          "</div>" +
+          "</div>";
+  
+  // external links with short-legend of a figure
+  private static final String withShortLegend =
+      "<div class=\"block\">" +
+          "<div class=\"short-legend\">" +
+          "<p class=\"first last\">" +
+          "<span class=\"captionLabel\">Figure 6.</span>" +
+          "<a href=\"http://library.example.org/external_link\">extlink</a>" +
+          "</p" +
+          "</div>" +
+          "</div>";         
+  
+  // external links within Acknowledgements and Case Studies sections
+  private static final String withExtLink =
+      "<div class=\"block\">" +      
+          "<a class=\"ext-link\" title=\"External link, opens new window\" " +
+          "target=\"_blank\" href=\"xxx/yyy\">xxx/yyy</a>" +
+          "</div>";
+  
+  // previous/next links from showFullPopup page, generated by 
+  // javascript:popFullRef
+  private static final String withShowFullPopupPrevNext =
+      "<div class=\"block\">" +  
+          "<span id=\"prevnext\">" +
+          "<a href=\"/action/showFullPopup?doi=99.9999%2Fgabc2012-9999.9" +
+          "&amp;id=fig1\">[previous]</a>" +
+          "href=\"/action/showFullPopup?doi=99.9999%2Fgabc012-9999.9" +
+          "&amp;id=fig3\">[next]</a></span>" +
+          "</div>";
+  
+  // For hash filtering
+  private static final String withMainAd =
+      "<div class=\"block\">" +
+          "<div class=\"mainAd\">" +
+          "<div><!-- placeholder id=null, description=Site Ad 1 -->" +
+          "<div style=\"float:right;\">" +
+          "<!-- load the list of ads -->" +
+          "<script src=\"http://segads/ox/xxx.php?zone=29\" " +
+          "type=\"text/javascript\"></script>" +
+          "</div>" +
+          "</div>" +
+          "</div>" +
+          "</div>";
+  
+   //// ?? might need:
+   //// some issue toc page has 'ContribStored', other 'ContribAuthorStored'
+   //private static final String withEntryAuthor =
+   //    "<div class=\"block\">" +
+   //    "<a class=\"entryAuthor\" href=\"/xxx/doSearch?display=true
+  //     "&ContribAuthorStored=MrXyz\">Mr. Xyz</a>" +
+   //    "</div>"; 
+
+  //// ?? might need:
+  //// from an article
+  //// some issue toc page has 'ContribStored', other 'ContribAuthorStored'
+  //private static final String withArtAuthors =
+  //    "<div class=\"block\">" +
+  //    "<div class=\"artAuthors\"><a href=\"/xxx/doSearch
+  //    "?ContribStored=MrXyz\">Mr. Xyz</a>" +
+  //    "</div>" +
+  //    "</div>";
+
+  protected ArchivalUnit createAu()
+      throws ArchivalUnit.ConfigurationException {
+    return PluginTestUtil.createAndStartAu(PLUGIN_NAME,  segAuConfig());
+  }
+  
+  private Configuration segAuConfig() {
+    Configuration conf = ConfigManager.newConfiguration();
+    conf.put("base_url", "http://www.example.com/");
+    conf.put("journal_id", "abc");
+    conf.put("volume_name", "99");
+    return conf;
+  }
+  
+  private static void doFilterTest(ArchivalUnit au, 
+      FilterFactory fact, String nameToHash, String expectedStr) 
+          throws PluginException, IOException {
+    InputStream actIn; 
+    actIn = fact.createFilteredInputStream(au, 
+        new StringInputStream(nameToHash), Constants.DEFAULT_ENCODING);
+    
+    // debug
+    String hashedStr = StringUtil.fromInputStream(actIn);
+    assertEquals(expectedStr, hashedStr);
+    // not sure why the below line would not pass the test
+    //assertEquals(afterFilteringStr, StringUtil.fromInputStream(actIn));
+  }
+  
+  public void startMockDaemon() {
+    daemon = getMockLockssDaemon();
+    pluginMgr = daemon.getPluginManager();
+    pluginMgr.setLoadablePluginsReady(true);
+    daemon.setDaemonInited(true);
+    pluginMgr.startService();
+    daemon.getAlertManager();
+    daemon.getCrawlManager();
+  }
+          		
+  // Variant to test with Crawl Filter
+  public static class TestCrawl
+    extends TestSEGHtmlFilterFactory {
+    public void setUp() throws Exception {
+      super.setUp();
+      tempDirPath = setUpDiskSpace();
+      startMockDaemon();
+      variantFact = new SEGHtmlCrawlFilterFactory();
+      variantAu = createAu();
+    }    
+    public void testFiltering() throws Exception {
+      doFilterTest(variantAu, variantFact, withPrevNextNav, filteredStr);
+      doFilterTest(variantAu, variantFact, withArticleToolsNav, filteredStr);
+      doFilterTest(variantAu, variantFact, 
+          withLeftColumnExceptDownloadCitation, 
+          withoutLeftColumnExceptDownloadCitation);
+      doFilterTest(variantAu, variantFact, withSecErrata, filteredStr);
+      doFilterTest(variantAu, variantFact, 
+          withAbstractReferences, filteredStr);
+      doFilterTest(variantAu, variantFact, withExtLink, filteredStr);
+      doFilterTest(variantAu, variantFact, withShortLegend, filteredStr);
+      doFilterTest(variantAu, variantFact, 
+          withShowFullPopupPrevNext, filteredStr);
+    }    
+  }
+
+  // Variant to test with Hash Filter
+   public static class TestHash 
+    extends TestSEGHtmlFilterFactory {   
+    public void setUp() throws Exception {
+      super.setUp();
+      tempDirPath = setUpDiskSpace();
+      startMockDaemon();
+      variantFact = new SEGHtmlHashFilterFactory();
+      variantAu = createAu();
+    }
+    public void testFiltering() throws Exception {
+      doFilterTest(variantAu, variantFact, withPrevNextNav, filteredStr);
+      doFilterTest(variantAu, variantFact, withArticleToolsNav, filteredStr);
+      doFilterTest(variantAu, variantFact, 
+          withLeftColumnExceptDownloadCitation, 
+          withoutLeftColumnExceptDownloadCitation);
+      doFilterTest(variantAu, variantFact, withMainAd, filteredStr);
+      //doFilterTest(variantAu, variantFact, withEntryAuthor, filteredStr);
+      //doFilterTest(variantAu, variantFact, withArtAuthors, filteredStr);
+    }
+  }
+  
+  public static Test suite() {
+    return variantSuites(new Class[] {
+        TestCrawl.class,
+        TestHash.class
+      });
+  }
+  
+}
+
