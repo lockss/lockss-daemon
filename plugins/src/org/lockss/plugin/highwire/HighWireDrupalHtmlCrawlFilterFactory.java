5a7911a9b088f8d2b647f219c134d96eb7f85f41
etenbrink <etenbrink@yahoo.com>
2014-10-06 23:09:50 +0000

    Add filters for BMJ and Royal Society
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@37432 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/highwire/HighWireDrupalHtmlCrawlFilterFactory.java
+++ plugins/src/org/lockss/plugin/highwire/HighWireDrupalHtmlCrawlFilterFactory.java
@@ -1,5 +1,5 @@
 /*
- * $Id: HighWireDrupalHtmlCrawlFilterFactory.java,v 1.3 2014-07-30 15:56:39 etenbrink Exp $
+ * $Id: HighWireDrupalHtmlCrawlFilterFactory.java,v 1.4 2014-10-06 23:09:50 etenbrink Exp $
  */
 
 /*
@@ -51,10 +51,14 @@ public class HighWireDrupalHtmlCrawlFilterFactory implements FilterFactory {
         // Do not crawl header or footer for links 
         new TagNameFilter("header"),
         new TagNameFilter("footer"),
-        // Do not crawl reference section, right-sidebar, or prev/next pager for links 
+        // Do not crawl reference section, right-sidebar, or prev/next pager for links from APS
         HtmlNodeFilters.tagWithAttribute("div", "class", "section ref-list"),
         HtmlNodeFilters.tagWithAttributeRegex("div", "class", "sidebar-right-wrapper"),
         HtmlNodeFilters.tagWithAttributeRegex("div", "class", "pane-highwire-node-pager"),
+        // Do not crawl for links from BMJ
+        new TagNameFilter("aside"),
+        // Do not crawl for links from Royal Society <div class="panel-pane pane-highwire-article-comments-form">
+        HtmlNodeFilters.tagWithAttributeRegex("div", "class", "comments-form"),
     };
     InputStream filtered = new HtmlFilterInputStream(in, encoding,
         HtmlNodeFilterTransform.exclude(new OrFilter(filters)));
