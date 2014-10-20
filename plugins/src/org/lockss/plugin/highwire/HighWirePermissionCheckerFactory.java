eb98118fed8d24e1ff9f0b79841d572abcc4e5b3
etenbrink <etenbrink@yahoo.com>
2014-10-20 22:44:41 +0000

    Fix bug in Matcher; remove mostly unused code and definitions; now dependent on version 1.66
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@37648 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/highwire/HighWirePermissionCheckerFactory.java
+++ plugins/src/org/lockss/plugin/highwire/HighWirePermissionCheckerFactory.java
@@ -1,5 +1,5 @@
 /*
- * $Id: HighWirePermissionCheckerFactory.java,v 1.5 2014-06-05 20:16:01 tlipkis Exp $
+ * $Id: HighWirePermissionCheckerFactory.java,v 1.6 2014-10-20 22:44:41 etenbrink Exp $
  */
 
 /*
@@ -32,25 +32,15 @@ in this Software without prior written authorization from Stanford University.
 
 package org.lockss.plugin.highwire;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
 import java.io.Reader;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.lockss.config.ConfigManager;
-import org.lockss.config.Configuration;
-import org.lockss.crawler.BaseCrawler;
-import org.lockss.crawler.BaseCrawler.StorePermissionScheme;
 import org.lockss.daemon.*;
 import org.lockss.daemon.Crawler.PermissionHelper;
-import org.lockss.extractor.GoslingHtmlLinkExtractor;
-import org.lockss.extractor.LinkExtractor;
 import org.lockss.plugin.*;
-import org.lockss.util.IOUtil;
 import org.lockss.util.Logger;
-import org.lockss.util.ReaderInputStream;
 
 public class HighWirePermissionCheckerFactory
   implements PermissionCheckerFactory {
@@ -60,20 +50,17 @@ public class HighWirePermissionCheckerFactory
   private class H10ProbePermissionChecker extends ProbePermissionChecker {
     
     private final Logger logger = Logger.getLogger(H10ProbePermissionChecker.class);
-    private ArchivalUnit au;
-    private String probeUrl;
     
     @Override
     public boolean checkPermission(PermissionHelper pHelper, Reader inputReader,
         String permissionUrl) {
       
-      // FIXME super_checkPermission should be super.checkPermission when 
-      // probeUrl and au are visible to child class
+      // XXX probeUrl and au are visible to child class, since 1.66
       boolean ret = super.checkPermission(pHelper, inputReader, permissionUrl);
       logger.debug3("returned: " + ret + " probeUrl:" + probeUrl);
       if (ret && probeUrl != null) {
         Matcher urlMat = H10_PATTERN.matcher(probeUrl);
-        if (!urlMat.matches()) {
+        if (!urlMat.find()) {
           logger.siteError(" ");
           logger.siteError("       ===============        ");
           logger.siteError(" ");
@@ -91,99 +78,8 @@ public class HighWirePermissionCheckerFactory
     
     public H10ProbePermissionChecker(ArchivalUnit au) {
       super(au);
-      this.au = au;
     }
     
-    // FIXME remove super_checkPermission when probeUrl and au are visible to child class
-    private boolean super_checkPermission(Crawler.PermissionHelper pHelper,
-        Reader inputReader, String permissionUrl) {
-      probeUrl = null;
-      CustomHtmlLinkExtractor extractor = new CustomHtmlLinkExtractor();
-      logger.debug3("Checking permission on "+permissionUrl);
-      try {
-        extractor.extractUrls(au, new ReaderInputStream(inputReader), null,
-            permissionUrl, new MyLinkExtractorCallback());
-      } catch (IOException ex) {
-        logger.error("Exception trying to parse permission url "+permissionUrl,
-            ex);
-        return false;
-      }
-      if (probeUrl != null) {
-        logger.debug3("Found probeUrl "+probeUrl);
-        BufferedInputStream is = null;
-        try {
-          UrlCacher uc = pHelper.makePermissionUrlCacher(probeUrl);
-          is = new BufferedInputStream(uc.getUncachedInputStream());
-          logger.debug3("Non-login page: " + probeUrl);
-          
-          // Retain compatibility with legacy behavior of not storing probe
-          // permission pages.
-          Configuration config = ConfigManager.getCurrentConfig();
-          StorePermissionScheme sps =
-              (StorePermissionScheme)config.getEnum(StorePermissionScheme.class,
-                  BaseCrawler.PARAM_STORE_PERMISSION_SCHEME,
-                  BaseCrawler.DEFAULT_STORE_PERMISSION_SCHEME);
-          if (StorePermissionScheme.Legacy != sps) {
-            pHelper.storePermissionPage(uc, is);
-          }
-          return true;
-        } catch (org.lockss.util.urlconn.CacheException.PermissionException ex) {
-          logger.debug3("Found a login page");
-          return false;
-        } catch (IOException ex) {
-          logger.error("Exception trying to check for login page "+probeUrl, ex);
-          return false;
-        } finally {
-          IOUtil.safeClose(is);
-        } 
-      } else {
-        logger.warning("Didn't find a probe URL on "+permissionUrl);
-      }
-      return false;
-    }
-    
-    // FIXME remove CustomHtmlLinkExtractor when probeUrl and au are visible to child class
-    private class CustomHtmlLinkExtractor
-    extends GoslingHtmlLinkExtractor {
-      
-      private static final String LOCKSSPROBE = "lockss-probe";
-      
-      protected String extractLinkFromTag(StringBuffer link, ArchivalUnit au,
-          LinkExtractor.Callback cb) {
-        String returnStr = null;
-        
-        switch (link.charAt(0)) {
-          case 'l': //<link href=blah.css>
-          case 'L':
-            logger.debug3("Looking for probe in "+link);
-            if (beginsWithTag(link, LINKTAG)) {
-              returnStr = getAttributeValue(HREF, link);
-              String probeStr = getAttributeValue(LOCKSSPROBE, link);
-              if (!"true".equalsIgnoreCase(probeStr)) {
-                returnStr = null;
-              }
-            }
-            break;
-          default:
-            return null;
-        }
-        return returnStr;
-      }
-    }
-    
-    // FIXME remove MyLinkExtractorCallback when probeUrl and au are visible to child class
-    private class MyLinkExtractorCallback implements LinkExtractor.Callback {
-      public MyLinkExtractorCallback() {
-      }
-      
-      public void foundLink(String url) {
-        if (probeUrl != null) {
-          logger.warning("Multiple probe URLs found on manifest page.  " +
-              "Old: "+probeUrl+" New: "+url);
-        }
-        probeUrl = url;
-      }
-    }
   }
   
   public List<?> createPermissionCheckers(ArchivalUnit au) {
