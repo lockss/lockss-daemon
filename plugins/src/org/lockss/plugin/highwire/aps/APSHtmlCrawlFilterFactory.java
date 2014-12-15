5b7c5449d1e0f27874b1ae04d23803930d3e6aaf
etenbrink <etenbrink@yahoo.com>
2014-12-15 20:33:12 +0000

    Initial HighWire APS Drupal child plugin
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@38513 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/highwire/aps/APSHtmlCrawlFilterFactory.java
@@ -0,0 +1,66 @@
+/*
+ * $Id: APSHtmlCrawlFilterFactory.java,v 1.1 2014-12-15 20:33:12 etenbrink Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.highwire.aps;
+
+import java.io.InputStream;
+
+import org.htmlparser.NodeFilter;
+import org.lockss.daemon.PluginException;
+import org.lockss.filter.html.*;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.plugin.highwire.HighWireDrupalHtmlCrawlFilterFactory;
+import org.lockss.util.Logger;
+
+public class APSHtmlCrawlFilterFactory extends HighWireDrupalHtmlCrawlFilterFactory {
+  
+  Logger log = Logger.getLogger(APSHtmlCrawlFilterFactory.class);
+  
+  public InputStream createFilteredInputStream(ArchivalUnit au,
+                                               InputStream in,
+                                               String encoding)
+      throws PluginException {
+    NodeFilter[] filters = new NodeFilter[] {
+        // Do not crawl reference section, right-sidebar 
+        HtmlNodeFilters.tagWithAttribute("div", "class", "section ref-list"),
+        HtmlNodeFilters.tagWithAttributeRegex("div", "class", "sidebar-right-wrapper"),
+        // author tool-tips changed for http://ajpheart.physiology.org/content/306/11/H1594.figures-only
+        HtmlNodeFilters.tagWithAttributeRegex("div", "class", "^author-tooltip"),
+        // APS articles sometimes had view links, but not always
+        HtmlNodeFilters.tagWithAttributeRegex("a", "class", "hw-link"),
+    };
+    
+    InputStream filtered = super.createFilteredInputStream(au, in, encoding, filters);
+    
+    return filtered;
+  }
+}
