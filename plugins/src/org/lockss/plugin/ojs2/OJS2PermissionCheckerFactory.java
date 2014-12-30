866a79c898587912a06d8724ba61b33cb41f35bc
etenbrink <etenbrink@yahoo.com>
2014-12-30 19:41:10 +0000

    Update OJS2PermissionChecker for changes to ProbePermissionChecker; make string static final
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@38752 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/ojs2/OJS2PermissionCheckerFactory.java
+++ plugins/src/org/lockss/plugin/ojs2/OJS2PermissionCheckerFactory.java
@@ -1,5 +1,5 @@
 /*
- * $Id: OJS2PermissionCheckerFactory.java,v 1.6 2014-12-08 21:40:16 alexandraohlson Exp $
+ * $Id: OJS2PermissionCheckerFactory.java,v 1.7 2014-12-30 19:41:10 etenbrink Exp $
  */
 
 /*
@@ -39,6 +39,7 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.lockss.daemon.Crawler.CrawlerFacade;
 import org.lockss.daemon.Crawler.PermissionHelper;
 import org.lockss.daemon.*;
 import org.lockss.plugin.*;
@@ -59,13 +60,19 @@ public class OJS2PermissionCheckerFactory
     protected Pattern au_year_paren;
     protected Pattern au_year_colon;
     
-    @Override
-    public boolean checkPermission(PermissionHelper pHelper, Reader inputReader,
-        String permissionUrl) {
-      
+    private static final String CLOCKSS_FRAG = "about/editorialPolicies";
+    
+    public OJS2PermissionChecker(ArchivalUnit au) {
+      super(au);
+      au_year = au.getConfiguration().get(ConfigParamDescr.YEAR.getKey());
+      au_year_paren = Pattern.compile("[(]" + au_year + "[)]");
+      au_year_colon = Pattern.compile(":\\s+" + au_year + "[^0-9]");
+    }
+    
+    private boolean checkPermission0(Reader inputReader, String permissionUrl) {
       // if the permissionUrl is for CLOCKSS, then just return True
       // XXX FIXME replace the entire PremissionChecker with CrawlSeed?
-      if (permissionUrl.contains("about/editorialPolicies")) {
+      if (permissionUrl.contains(CLOCKSS_FRAG)) {
         return true;
       }
       
@@ -99,13 +106,20 @@ public class OJS2PermissionCheckerFactory
       return ret;
     }
     
-    
-    public OJS2PermissionChecker(ArchivalUnit au) {
-      super(au);
-      au_year = au.getConfiguration().get(ConfigParamDescr.YEAR.getKey());
-      au_year_paren = Pattern.compile("[(]" + au_year + "[)]");
-      au_year_colon = Pattern.compile(":\\s+" + au_year + "[^0-9]");
+    @Override
+    public boolean checkPermission(CrawlerFacade crawlFacade,
+        Reader inputReader, String permissionUrl) {
+      
+      return checkPermission0(inputReader, permissionUrl);
     }
+    
+    @Override
+    public boolean checkPermission(PermissionHelper pHelper, Reader inputReader,
+        String permissionUrl) {
+      
+      return checkPermission0(inputReader, permissionUrl);
+    }
+    
   }
   
   public List<OJS2PermissionChecker> createPermissionCheckers(ArchivalUnit au) {
