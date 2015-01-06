0eb9eae3777bec7debacb38c6e8da426b7c57ef8
thibgc <github@thib.org>
2015-01-06 22:43:55 +0000

    Better attempt at skipping the filtering of a few specific URLs.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@38876 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/usdocspln/gov/gpo/fdsys/GPOFDSysHtmlFilterFactory.java
+++ plugins/src/org/lockss/plugin/usdocspln/gov/gpo/fdsys/GPOFDSysHtmlFilterFactory.java
@@ -1,10 +1,10 @@
 /*
- * $Id: GPOFDSysHtmlFilterFactory.java,v 1.9 2015-01-06 02:46:38 thib_gc Exp $
+ * $Id: GPOFDSysHtmlFilterFactory.java,v 1.10 2015-01-06 22:43:55 thib_gc Exp $
  */
 
 /*
 
-Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -33,7 +33,9 @@ in this Software without prior written authorization from Stanford University.
 package org.lockss.plugin.usdocspln.gov.gpo.fdsys;
 
 import java.io.*;
+import java.util.regex.*;
 
+import org.apache.commons.io.input.BoundedInputStream;
 import org.htmlparser.*;
 import org.htmlparser.filters.*;
 import org.htmlparser.util.*;
@@ -46,11 +48,68 @@ import org.lockss.util.*;
 
 public class GPOFDSysHtmlFilterFactory implements FilterFactory {
 
+  private static final Logger logger = Logger.getLogger(GPOFDSysHtmlFilterFactory.class);
+
+  private static final Pattern titlePat =
+      Pattern.compile("<title>U.S.C. Title 42 - THE PUBLIC HEALTH AND WELFARE",
+                      Pattern.CASE_INSENSITIVE); 
+  
+  private static final Pattern chapPat =
+      Pattern.compile("<span( [^>]*)?>CHAPTER",
+                      Pattern.CASE_INSENSITIVE); 
+  
+  public boolean shouldFilter(InputStream is, String encoding) throws IOException {
+    logger.setLevel(Logger.LEVEL_DEBUG3);
+    BufferedReader br = new BufferedReader(new InputStreamReader(new BoundedInputStream(is, 2000), encoding));
+    boolean title42 = false;
+    for (String line = br.readLine() ; line != null ; line = br.readLine()) {
+      if (logger.isDebug3()) {
+        logger.debug3("line: " + line);
+      }
+      if (title42) {
+        Matcher chapMat = chapPat.matcher(line);
+        if (chapMat.find()) {
+          logger.debug2("Chapter of Title 42");
+          return true;
+        }
+      }
+      else {
+        if (line.contains("<title>")) {
+          logger.debug2("<title> line");
+          Matcher titleMat = titlePat.matcher(line);
+          if (titleMat.find()) {
+            logger.debug2("Title 42");
+            title42 = true;
+          }
+          else {
+            logger.debug2("Not Title 42");
+            return true;
+          }
+        }
+      }
+    }
+    // Filter iff inside Title 42 but not a chapter thereof
+    logger.debug2(title42 ? "Title 42 but not a chapter thereof" : "Not Title 42 (final)");
+    return !title42;
+  }
+  
   public InputStream createFilteredInputStream(ArchivalUnit au,
                                                InputStream in,
                                                String encoding)
       throws PluginException {
-	
+    BufferedInputStream bis = new BufferedInputStream(in, 2048);
+    bis.mark(2048);
+    try {
+      boolean filter = shouldFilter(bis, encoding);
+      bis.reset();
+      if (!filter) {
+        return bis;
+      }
+    }
+    catch (IOException ioe) {
+      logger.debug("IOException while inspecting document to skip filtering", ioe);
+    }
+    
     NodeFilter[] filters = new NodeFilter[] {
         /*
          * Broad area filtering 
@@ -114,29 +173,10 @@ public class GPOFDSysHtmlFilterFactory implements FilterFactory {
     };
     
     InputStream prefilteredStream =
-        new HtmlFilterInputStream(in,
+        new HtmlFilterInputStream(bis, // NOTE: this is 'bis', not 'in'
                                   encoding,
                                   new HtmlCompoundTransform(HtmlNodeFilterTransform.exclude(new OrFilter(filters)),
-                                                            xform)) {
-      // Title 42 has a 50+ MB HTML file that does not require this filter.
-      // Many daemons get an OOME applying this filter, caught by BlockHasher,
-      // and the daemon exits (e.g. Stanford on the order of once a day).
-      // In the future, the interface will have the CU or URL, but for now,
-      // just ignore OOME by rethrowing an IOException.
-      // HtmlFilterInputStream's parse() or getOut() are not visible.
-      @Override public int read() throws IOException {
-        try { return super.read(); }
-        catch (OutOfMemoryError oome) { throw new IOException(oome); }
-      }
-      @Override public int read(byte[] buf) throws IOException {
-        try { return super.read(buf); }
-        catch (OutOfMemoryError oome) { throw new IOException(oome); }
-      }
-      @Override public int read(byte[] buf, int off, int len) throws IOException {
-        try { return super.read(buf, off, len); }
-        catch (OutOfMemoryError oome) { throw new IOException(oome); }
-      }
-    };
+                                                            xform));
 
     try {
       Reader filteredReader = new InputStreamReader(prefilteredStream, encoding);
--- plugins/test/src/org/lockss/plugin/usdocspln/gov/gpo/fdsys/TestGPOFDSysHtmlFilterFactory.java
+++ plugins/test/src/org/lockss/plugin/usdocspln/gov/gpo/fdsys/TestGPOFDSysHtmlFilterFactory.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestGPOFDSysHtmlFilterFactory.java,v 1.6 2014-08-12 21:42:43 thib_gc Exp $
+ * $Id: TestGPOFDSysHtmlFilterFactory.java,v 1.7 2015-01-06 22:43:55 thib_gc Exp $
  */
 
 /*
@@ -40,7 +40,7 @@ public class TestGPOFDSysHtmlFilterFactory extends LockssTestCase {
   
   private static final String ENC = Constants.DEFAULT_ENCODING;
 
-  private FilterFactory fact;
+  private GPOFDSysHtmlFilterFactory fact;
 
   public void setUp() throws Exception {
     super.setUp();
@@ -230,6 +230,196 @@ public class TestGPOFDSysHtmlFilterFactory extends LockssTestCase {
     doTestString(" ",
                  "<input type=\"hidden\" name=\"struts.token.name\" value=\"struts.token\" />\n" +
                  "<input type=\"hidden\" name=\"struts.token\" value=\"H34QZK9TF4N77V7LRD94SBULW6JZ9DPB\" />\n");
+  }
+  
+  public void testShouldFilter() throws Exception {
+    // From http://www.gpo.gov/fdsys/pkg/USCODE-2006-title42/html/USCODE-2006-title42-chap1-subchapI.htm
+    assertTrue(fact.shouldFilter(new StringInputStream(
+"<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"usc.css\"/><title>U.S.C. Title 42 - THE PUBLIC HEALTH AND WELFARE</title></html></head>\n" +
+"<span style=\"font-weight:bold;font-size:12pt;\">42 U.S.C. </span><br/>\n" +
+"<span style=\"font-size:10pt\">United States Code, 2006 Edition</span><br/>\n" +
+"<span style=\"font-size:10pt\">Title 42 - THE PUBLIC HEALTH AND WELFARE</span><br/>\n" +
+"<span style=\"font-size:10pt\">CHAPTER 1 - THE PUBLIC HEALTH SERVICE</span><br/>\n" +
+"<span style=\"font-size:10pt\">SUBCHAPTER I - GENERALLY</span><br/>\n" +
+"<span style=\"font-size:10pt\">From the U.S. Government Printing Office, <a href=\"http://www.gpo.gov\">www.gpo.gov</a></span><br/><br/>\n" +
+"<!-- documentid:42_-ch1-scI currentthrough:20070103 documentPDFPage:3 -->\n" +
+"<!-- itempath:/420/CHAPTER 1/SUBCHAPTER I -->\n" +
+"<!-- itemsortkey:420AAAC -->\n" +
+"<!-- expcite:TITLE 42-THE PUBLIC HEALTH AND WELFARE!@!CHAPTER 1-THE PUBLIC HEALTH SERVICE!@!SUBCHAPTER I-GENERALLY -->\n" +
+"<!-- field-start:structuralhead -->\n" +
+"<h3 class=\"subchapter-head\">SUBCHAPTER I&mdash;GENERALLY</h3>\n" +
+"<!-- field-end:structuralhead -->\n" +
+"\n" +
+"<!-- documentid:42_1_to_1j  usckey:420000000000100000000000000000000 currentthrough:20070103 documentPDFPage:3 -->\n" +
+"<!-- itempath:/420/CHAPTER 1/SUBCHAPTER I/Secs. 1 to 1j -->\n" +
+"<!-- itemsortkey:420AAAD -->\n" +
+"<!-- expcite:TITLE 42-THE PUBLIC HEALTH AND WELFARE!@!CHAPTER 1-THE PUBLIC HEALTH SERVICE!@!SUBCHAPTER I-GENERALLY!@!Secs. 1 to 1j -->\n" +
+"<!-- field-start:repealedhead -->\n" +
+"<h3 class=\"section-head\">&sect;&sect;1 to 1j. Repealed. July 1, 1944, ch. 373, title XIII, &sect;1313, 58 Stat. 714</h3>\n" +
+"<!-- field-end:repealedhead -->\n" +
+"<!-- field-start:repealsummary -->\n" +
+"<p class=\"note-body\">Section 1, acts July 1, 1902, ch. 1370, &sect;1, 32 Stat. 712; Aug. 14, 1912, ch. 288, &sect;1, 37 Stat. 309, provided that Public Health and Marine Hospital Service should be known as the Public Health Service. See section 202 of this title.</p>\n" +
+"<p class=\"note-body\">Section 1a, act Nov. 11, 1943, ch. 298, &sect;1, 57 Stat. 587, provided for organization and function of Public Health Service. See section 203 of this title.</p>\n" +
+"<p class=\"note-body\">Section 1b, act Nov. 11, 1943, ch. 298, &sect;2, 57 Stat. 587, provided for appointment of Assistant Surgeons General, their grade, pay, and allowances. See sections 206, 207, and 210 of this title.</p>\n" +
+"<p class=\"note-body\">Section 1c, act Nov. 11, 1943, ch. 298, &sect;3, 57 Stat. 587, provided for chiefs of divisions, their grade, pay and allowances, and creation of a Dental Division and a Sanitary Engineering Division. See sections 206, 207, and 210 of this title.</p>\n"
+             ), "UTF-8"));
+    // From http://www.gpo.gov/fdsys/search/pagedetails.action?collectionCode=USCODE&searchPath=Title+42%2FChapter+1%2FSUBCHAPTER+I&granuleId=&packageId=USCODE-2006-title42&oldPath=Title+42%2FCHAPTER+1&fromPageDetails=true&collapse=true&ycord=114
+    assertTrue(fact.shouldFilter(new StringInputStream(
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"       \n" +
+"\n" +
+"                         \n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"       \n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n" +
+"<head profile=\"http://www.w3.org/2005/10/profile\">\n" +
+"       <base href=\"http://www.gpo.gov/fdsys/\" />\n" +
+"       \n" +
+"       <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n" +
+"       <meta http-equiv=\"Content-Script-Type\" content=\"text/javascript\" />        \n" +
+"       <meta http-equiv=\"pragma\" content=\"no-cache\" />\n" +
+"       <meta http-equiv=\"cache-control\" content=\"no-cache\" />\n" +
+"       <meta http-equiv=\"expires\" content=\"0\" />     \n" +
+"       <meta http-equiv=\"keywords\" content=\"\n" +
+"               42 U.S.C.  - THE PUBLIC HEALTH AND WELFARE\n" +
+"       \" />\n" +
+"       <meta http-equiv=\"description\" content=\"\n" +
+"               42 U.S.C.  - THE PUBLIC HEALTH AND WELFARE\n" +
+"       \" /> \n" +
+"       \n" +
+"       <script type=\"text/javascript\">\n" +
+"       \n" +
+"       var WT_SERVER = '162.140.239.17';\n" +
+"       var WT_DCS_ID = 'dcsjsw8h600000gotf0vyrmly_2j7v';\n" +
+"       var WT_HOST_SERVER_NAME = 'http://www.gpo.gov/fdsys';\n" +
+"       \n" +
+"       var BASE_PRESENTATION_PATH = 'http://www.gpo.gov/fdsys/';\n" +
+"       var BASE_USER_HELP = 'http://www.gpo.gov/help/';        \n" +
+"       \n" +
+"               \n" +
+"                var SEARCHWEBAPP_BUILD = 'DEV_INT_R3_15';\n" +
+"                               \n" +
+"                               var coopValue='';\n" +
+"\n" +
+"    </script>\n" +
+"       <script type=\"text/javascript\" src=\"http://www.gpo.gov/fdsysgpopages/scripts/gpo.js\"></script>      \n" +
+"       <script type=\"text/javascript\" src=\"scripts/fdsys.js\"></script>             \n" +
+"       <script type=\"text/javascript\" src=\"scripts/mktree.js\"></script>\n" +
+"       <title>\n" +
+"               42 U.S.C.  - THE PUBLIC HEALTH AND WELFARE\n" +
+"       </title>\n" +
+"       \n" +
+"               <style type=\"text/css\">\n" +
+"                       #left-menu-sub-search {\n" +
+"                               color: #000000;\n" +
+"                       }\n" +
+"               </style>                \n" +
+"       \n" +
+"\n" +
+"       <style type=\"text/css\">\n" +
+"               @import url(http://www.gpo.gov/fdsysgpopages/styles/fdsyspages.css);\n" +
+"               @import url(http://www.gpo.gov/fdsysgpopages/styles/gpo.css);\n" +
+"       </style>        \n" +
+"       <link rel=\"icon\" type=\"image/gif\" href=\"images/gpo_favicon.gif\" />\n" +
+"</head>\n" +
+"\n" +
+"\n" +
+"<body onload=\"setFocusTextField(); setScrollXY();\">\n" +
+"       <script type=\"text/javascript\" src=\"scripts/wz_tooltip.js\"></script>        \n" +
+"       \n" +
+"            \n" +
+"       <div id=\"wrapper\">\n" +
+"\n" +
+"               \n" +
+"               \n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"<!--<input type=\"hidden\" name=\"struts.token.name\" value=\"struts.token\" />\n" +
+"<input type=\"hidden\" name=\"struts.token\" value=\"1V1X534Y9933UICYEHEEVP7HPIKML821\" />-->\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"<a href=\"\" id=\"skip-anchor\" >\n" +
+"       <img src=\"/images/spacer.gif\" alt=\"Skip to content.\" class=\"skip-anchor-gif\" />\n" +
+"</a>\n" +
+"<script type=\"text/javascript\">createSkipLink();</script>\n"
+        ), "UTF-8"));
+    // From http://www.gpo.gov/fdsys/pkg/USCODE-2006-title42/html/USCODE-2006-title42.htm
+    // (one that should not be filtered)
+    assertFalse(fact.shouldFilter(new StringInputStream(
+"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n" +
+"\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
+"<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
+" <head>\n" +
+"  <link rel=\"stylesheet\" type=\"text/css\" href=\"usc.css\"  />\n" +
+"  <title>U.S.C. Title 42 - THE PUBLIC HEALTH AND WELFARE\n" +
+"<!-- AUTHORITIES-PUBLICATION-NAME:2006 Edition -->\n" +
+"<!-- AUTHORITIES-PUBLICATION-ID:2006MNED006 -->\n" +
+"<!-- AUTHORITIES-PUBLICATION-YEAR:2006 -->\n" +
+"<!-- AUTHORITIES-LAWS-ENACTED-THROUGH-DATE:20070103 -->\n" +
+"<!-- SEARCHABLE-LAWS-ENACTED-THROUGH-DATE:January 3rd, 2007 -->\n" +
+"<!-- AUTHORITIES-USC-TITLE-NAME:TITLE 42 - THE PUBLIC HEALTH AND WELFARE -->\n" +
+"<!-- AUTHORITIES-USC-TITLE-ENUM:42 -->\n" +
+"<!-- AUTHORITIES-USC-TITLE-STATUS:editorial -->\n" +
+"\n" +
+"<!-- CONVERSION-PROGRAM:xy2html.pm-0.329-20100225 -->\n" +
+"<!-- CONVERSION-DATETIME:20100225153648 -->\n" +
+"\n" +
+"  </title>\n" +
+" </head>\n" +
+" <body>\n" +
+"<span style=\"font-weight:bold;font-size:12pt;\">42 U.S.C. </span><br/>\n" +
+"<span style=\"font-size:10pt\">United States Code, 2006 Edition</span><br/>\n" +
+"<span style=\"font-size:10pt\">Title 42 - THE PUBLIC HEALTH AND WELFARE</span><br/>\n" +
+"<span style=\"font-size:10pt\">From the U.S. Government Printing Office, <a href=\"http://www.gpo.gov\">www.gpo.gov</a></span><br/><br/>\n" +
+"\n" +
+"  <div width=\"700\">\n" +
+"\n" +
+"<!-- PDFPage:1 -->\n" +
+"<!-- documentid:42_ currentthrough:20070103 documentPDFPage:-1 -->\n" +
+"<!-- itempath:/420 -->\n" +
+"<!-- itemsortkey:420AAAA -->\n" +
+"<!-- expcite:TITLE 42-THE PUBLIC HEALTH AND WELFARE -->\n" +
+"<!-- field-start:titlehead -->\n" +
+"<h1 class=\"usc-title-head\">TITLE 42&mdash;THE PUBLIC HEALTH AND WELFARE</h1>\n" +
+"<!-- field-end:titlehead -->\n" +
+"<!-- field-start:analysis -->\n" +
+"<div class=\"analysis\">\n" +
+"<div><div class=\"analysis-head-left\">Chap.</div><div class=\"analysis-head-right\">Sec.</div></div>\n" +
+"<div><div class=\"three-column-analysis-style-content-left\">1.</div><div class=\"three-column-analysis-style-content-center\" id=\"wide\">The Public Health Service [Repealed or Omitted, See Chapter 6A]</div><div class=\"three-column-analysis-style-content-right\">1</div></div>\n" +
+"<div><div class=\"three-column-analysis-style-content-left\">1A.</div><div class=\"three-column-analysis-style-content-center\" id=\"wide\">The Public Health Service; Supplemental Provisions [Transferred or Omitted]</div><div class=\"three-column-analysis-style-content-right\">71</div></div>\n"
+        ), "UTF-8"));
   }
   
   private void doTestString(String expectedOutput,
