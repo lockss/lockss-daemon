1cb357f64485b9f50ae669627a6d1488e40c0a18
aishizaki <aishizaki@lockss.org>
2015-01-14 17:31:13 +0000

    PalgraveBookPdfFilterFactory.java:  now filters access date from rhs of ALL pages  now uses the pdf filter state machine (requires 1.67) ClockssPalgraveBookPlugin.xml:  requires 1.67, new version = 8
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@39070 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/palgrave/PalgraveBookPdfFilterFactory.java
+++ plugins/src/org/lockss/plugin/palgrave/PalgraveBookPdfFilterFactory.java
@@ -1,10 +1,10 @@
 /*
- * $Id: PalgraveBookPdfFilterFactory.java,v 1.4 2015-01-12 17:53:56 aishizaki Exp $
+ * $Id: PalgraveBookPdfFilterFactory.java,v 1.5 2015-01-14 17:31:13 aishizaki Exp $
  */
 
 /*
 
-Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -33,112 +33,92 @@ in this Software without prior written authorization from Stanford University.
 package org.lockss.plugin.palgrave;
 
 import java.util.List;
-
 import org.lockss.filter.pdf.SimplePdfFilterFactory;
 import org.lockss.pdf.*;
 import org.lockss.plugin.ArchivalUnit;
 import org.lockss.util.Logger;
+import java.util.regex.Pattern;
 
 public class PalgraveBookPdfFilterFactory extends SimplePdfFilterFactory {
-
   private static final Logger logger = Logger.getLogger(PalgraveBookPdfFilterFactory.class);
-
-  protected static class Worker extends PdfTokenStreamWorker {
-    
-    protected boolean result;
-
-    protected int beginIndex;
-    
-    protected int endIndex;
-    
-    protected int state;
-
-    public Worker() {
-      super(Direction.BACKWARD);
-    }
-    
-    @Override
-    public void setUp() throws PdfException {
-      super.setUp();
-      this.state = 0;
-      this.result = false;
-      this.beginIndex = -1;
-      this.endIndex = -1;
-    }
-
-    @Override
-    public void operatorCallback() throws PdfException {
-      if (logger.isDebug3()) {
-        logger.debug3("Worker: initial: " + state);
-        logger.debug3("Worker: index: " + getIndex());
-        logger.debug3("Worker: operator: " + getOpcode());
-      }
-      
-      switch (state) {
-        
-        case 0: {
-          if (isEndTextObject()) {
-            endIndex = getIndex();
-            ++state;
-          }
-        } break;
-        
-        case 1: {
-          if (isShowTextGlyphPositioningStartsWith("Copyright material from www.palgraveconnect.com")) {
-            ++state;
-          }
-          else if (isBeginTextObject()) {
-            stop();
-          }
-        } break;
-        
-        case 2: {
-          if (isBeginTextObject()) {
-            result = true;
-            beginIndex = getIndex();
-            stop();
-          }
-        } break;
-        
-        default: {
-          throw new PdfException("Invalid state in Worker: " + state);
-        }
-        
-      }
-      
-      if (logger.isDebug3()) {
-        logger.debug3("Worker: final: " + state);
-        logger.debug3("Worker: result: " + result);
-      }
-      
-    }
-
-  }
+  private static final String searchString = "^Copyright material from www.palgraveconnect.com";
   
   @Override
   public void transform(ArchivalUnit au, PdfDocument pdfDocument) throws PdfException {
+    doBaseTransforms(pdfDocument);    
+    removeDownloadStrip(pdfDocument);
+  }
+
+  /* and set the correct string to use for this publisher */
+  public Pattern getDownloadStripPattern() {
+    return Pattern.compile(searchString);
+  }
+
+  public static void doBaseTransforms(PdfDocument pdfDocument) throws PdfException {
     pdfDocument.unsetModificationDate();
     pdfDocument.unsetCreationDate();
     pdfDocument.unsetMetadata();
     PdfUtil.normalizeTrailerId(pdfDocument);
-    
-    Worker worker = new Worker();
-    for (PdfPage pdfPage : pdfDocument.getPages()) {
-      // Pages seem to be made of concatenated token streams, and while most
-      // target personalization is at the end (previously filtered the last
-      // token stream), some books (9781137023803 - on pages with "Left Intentianally
-      // Blank") had customization elsewhere in the token stream, so now checking all
-      List<PdfTokenStream> pdfTokenstreams = pdfPage.getAllTokenStreams();
-      for (int i = 0; i < pdfTokenstreams.size(); i++){
-        PdfTokenStream pdfTokenStream = pdfTokenstreams.get(i);
-        worker.process(pdfTokenStream);
-        if (worker.result) {
-          List<PdfToken> pdfTokens = pdfTokenStream.getTokens();
-          pdfTokens.subList(worker.beginIndex, worker.endIndex + 1).clear();
-          pdfTokenStream.setTokens(pdfTokens);
-        }
-      }
-    }
   }
   
+  private void removeDownloadStrip(PdfDocument pdfDocument) throws PdfException {
+    // using StateMachine which requires 1.67
+   PalgraveDownloadedFromStateMachine worker = new PalgraveDownloadedFromStateMachine(getDownloadStripPattern());
+
+   // check each page for an access date set on the rhs of the page
+   for (PdfPage pdfPage : pdfDocument.getPages()) {
+     // Pages seem to be made of concatenated token streams, and while most
+     // target personalization is at the end (previously filtered the last
+     // token stream), some books (9781137023803 - on pages with "Left Intentianally
+     // Blank") had customization elsewhere in the token stream, so now checking all
+     List<PdfTokenStream> pdfTokenstreams = pdfPage.getAllTokenStreams();
+     for (PdfTokenStream pdfTokenStream : pdfTokenstreams) {
+       worker.process(pdfTokenStream);
+       if (worker.getResult()) {    // found it, fixing it
+         List<PdfToken> pdfTokens = pdfTokenStream.getTokens();
+         pdfTokens.subList(worker.getBegin(), worker.getEnd() + 1).clear();
+         pdfTokenStream.setTokens(pdfTokens);
+         break;        // now go check the next page
+       }
+     }
+   }
+  }
+  
+  public static class PalgraveDownloadedFromStateMachine extends PdfTokenStreamStateMachine {
+    /* set when this worker is created */
+    public static Pattern DOWNLOADED_FROM_PATTERN;
+
+    // The footer is close to the bottom of each page
+    public PalgraveDownloadedFromStateMachine(Pattern downloadPattern) {
+      super(Direction.BACKWARD);
+      DOWNLOADED_FROM_PATTERN = downloadPattern;
+    }
+
+    @Override
+    public void state0() throws PdfException {
+      if (isEndTextObject()) {
+        setEnd(getIndex());
+        setState(getState() + 1);
+      }
+    } 
+
+    @Override
+    public void state1() throws PdfException {
+      if (isShowTextGlyphPositioningFind(DOWNLOADED_FROM_PATTERN))  {
+        setState(getState() + 1);
+      } else if (isBeginTextObject()) { // not the BT-ET we were looking for
+        //COULD STOP HERE IF assuming only one bt/et in the stream we want
+        stop();
+      }
+    }
+
+    @Override
+    public void state2() throws PdfException {
+      if (isBeginTextObject()) {  
+          setResult(true);
+          setBegin(getIndex());
+          stop(); // found what we needed, stop processing this page
+      }  
+    }
+  }
 }
