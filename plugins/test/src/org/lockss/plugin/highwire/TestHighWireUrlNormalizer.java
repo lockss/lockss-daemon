a4f2ed3ffba5ab44a746f366454b084fdd9a561b
etenbrink <etenbrink@yahoo.com>
2015-01-15 03:04:48 +0000

    Add test cases for UrlNormalizer
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@39106 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/test/src/org/lockss/plugin/highwire/TestHighWireUrlNormalizer.java
@@ -0,0 +1,79 @@
+/*
+ * $Id: TestHighWireUrlNormalizer.java,v 1.1 2015-01-15 03:04:48 etenbrink Exp $
+ */
+
+/*
+
+Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.highwire;
+
+import java.util.Properties;
+
+import org.lockss.config.Configuration;
+import org.lockss.daemon.ConfigParamDescr;
+import org.lockss.plugin.UrlNormalizer;
+import org.lockss.plugin.definable.DefinablePlugin;
+import org.lockss.test.ConfigurationUtil;
+import org.lockss.test.LockssTestCase;
+/*
+ * UrlNormalizer
+ * map H20 links on manifest pages to H10 links
+ * <base_url>content/168/1.toc to <base_url>content/vol168/issue1/
+ */
+
+public class TestHighWireUrlNormalizer extends LockssTestCase {
+  
+  static final String BASE_URL_KEY = ConfigParamDescr.BASE_URL.getKey();
+  static final String VOL_KEY = ConfigParamDescr.VOLUME_NAME.getKey();
+  private DefinablePlugin plugin;
+  
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    plugin = new DefinablePlugin();
+    plugin.initPlugin(getMockLockssDaemon(),
+        "org.lockss.plugin.highwire.HighWirePressPlugin");
+    Properties props = new Properties();
+    props.setProperty(VOL_KEY, "303");
+    props.setProperty(BASE_URL_KEY, "http://www.example.com/");
+    
+    Configuration config = ConfigurationUtil.fromProps(props);
+    plugin.configureAu(config, null);
+    }
+  
+  public void testUrlNormalizer() throws Exception {
+    UrlNormalizer normalizer = new HighWireUrlNormalizer();
+    
+    assertEquals("http://ajpcell.physiology.org/content/vol303/issue1/",
+        normalizer.normalizeUrl("http://ajpcell.physiology.org/content/303/1.toc", null));
+    
+    assertEquals("http://ajpcell.physiology.org/content/303/1/",
+        normalizer.normalizeUrl("http://ajpcell.physiology.org/content/303/1/", null));
+    
+  }
+}
