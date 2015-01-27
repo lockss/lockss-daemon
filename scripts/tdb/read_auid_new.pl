a6fd1cfb0659d194fee771069c9ade03c47d6422
mellen22 <maryellen@lockss.org>
2015-01-27 22:44:10 +0000

    Add other Drupal plugins.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@39358 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -132,7 +132,7 @@ while (my $line = <>) {
         }
         sleep(4);
 
-  } elsif ($plugin eq "HighWireDrupalPlugin") {
+  } elsif ($plugin eq "HighWireDrupalPlugin" || $plugin eq "OUPDrupalPlugin" || $plugin eq "APSDrupalPlugin") {
         $url = sprintf("%slockss-manifest/vol_%s_manifest.html",
             $param{base_url}, $param{volume_name});
         $man_url = uri_unescape($url);
