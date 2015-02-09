d5a53688e766effc0fd7c538136a37489ce96d52
tlipkis <tal@pss.com>
2015-02-09 05:37:02 +0000

    Logging.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/branches/branch_1-67@39649 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/poller/PollManager.java
+++ src/org/lockss/poller/PollManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: PollManager.java,v 1.271.2.6 2014-12-28 08:37:57 tlipkis Exp $
+ * $Id: PollManager.java,v 1.271.2.7 2015-02-09 05:37:02 tlipkis Exp $
  */
 
 /*
@@ -2462,7 +2462,7 @@ public class PollManager
 	handleIncomingMessage(msg);
       }
       catch (ProtocolException ex) {
-	theLog.error("handleIncomingMessage() threw", ex);
+	theLog.error("handleIncomingMessage() threw " + ex);
       }
       catch (IOException ex) {
 	theLog.error("handleIncomingMessage() threw", ex);
