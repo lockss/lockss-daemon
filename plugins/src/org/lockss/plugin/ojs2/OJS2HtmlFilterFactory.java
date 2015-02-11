c5dde80bc01fc9a2c13dd5bee27125b3a942b16e
etenbrink <etenbrink@yahoo.com>
2015-02-11 07:56:07 +0000

    Refactor to allow child plugins to inherit from OJS2HtmlFilterFactory
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@39691 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/ojs2/OJS2HtmlFilterFactory.java
+++ plugins/src/org/lockss/plugin/ojs2/OJS2HtmlFilterFactory.java
@@ -1,5 +1,5 @@
 /*
- * $Id: OJS2HtmlFilterFactory.java,v 1.13 2014-07-11 19:01:04 etenbrink Exp $
+ * $Id: OJS2HtmlFilterFactory.java,v 1.14 2015-02-11 07:56:07 etenbrink Exp $
  */
 
 /*
@@ -33,10 +33,12 @@ in this Software without prior written authorization from Stanford University.
 package org.lockss.plugin.ojs2;
 
 import java.io.InputStream;
+import java.util.Arrays;
 
 import org.htmlparser.NodeFilter;
 import org.htmlparser.filters.*;
 import org.htmlparser.tags.CompositeTag;
+import org.lockss.daemon.PluginException;
 import org.lockss.filter.html.*;
 import org.lockss.plugin.*;
 
@@ -58,59 +60,100 @@ public class OJS2HtmlFilterFactory implements FilterFactory {
      * Return the set of names handled by this tag.
      * @return The names to be matched that create tags of this type.
      */
+    @Override
     public String[] getIds() {
       return mIds;
     }
     
   }
   
+  protected static NodeFilter[] baseFilters = new NodeFilter[] {
+    // No need to hash these tags in OJS sites, not content and things change
+    HtmlNodeFilters.tagWithAttribute("div", "id", "header"),
+    HtmlNodeFilters.tagWithAttribute("div", "id", "sidebar"),
+    HtmlNodeFilters.tagWithAttributeRegex("div", "id", "commentsOnArticle"),
+    HtmlNodeFilters.tagWithAttributeRegex("div", "id", "citation-block"),
+    // Some OJS sites have a tag cloud
+    HtmlNodeFilters.tagWithAttribute("div", "id", "sidebarKeywordCloud"),
+    // Some OJS sites have a subscription status area
+    HtmlNodeFilters.tagWithAttribute("div", "id", "sidebarSubscription"),
+    // Some OJS sites have a language switcher, which can change over time
+    HtmlNodeFilters.tagWithAttribute("div", "id", "sidebarLanguageToggle"),
+    // Top-level menu items sometimes change over time
+    HtmlNodeFilters.tagWithAttribute("div", "id", "navbar"),
+    // Popular location for sidebar customizations
+    HtmlNodeFilters.tagWithAttribute("div", "id", "custom"),
+    //  the order of <meta> tags changes capriciously, which slows down testing
+    new TagNameFilter("head"),
+    // Site customizations often involve Javascript (e.g. Google Analytics), which can change over time
+    new TagNameFilter("script"),
+    // Date accessed is a variable
+    HtmlNodeFilters.tagWithTextRegex("div", "Date accessed: "),
+    // The version of the OJS software, which can change over time, appears in a tag
+    HtmlNodeFilters.tagWithAttribute("meta", "name", "generator"),
+    // Header image with variable dimensions
+    HtmlNodeFilters.tagWithAttribute("div", "id", "headerTitle"),
+    // For Ubiquity Press
+    HtmlNodeFilters.tagWithAttribute("div", "id", "rightSidebar"),
+    // For JLIS.it: landing pages contain user view count
+    HtmlNodeFilters.tagWithAttribute("span", "class", "ArticleViews"),
+    // For ibictpln: PHP Query Profiler
+    // e.g. http://seer.bce.unb.br/index.php/Musica/article/view/915
+    HtmlNodeFilters.tagWithAttribute("div", "id", "pqp-container"),
+    // Total de acessos: keeps changing, there is no 'good' tag wrapped around text
+    // e.g. https://www.revistas.unijui.edu.br/index.php/desenvolvimentoemquestao/issue/view/18
+    HtmlNodeFilters.tagWithTextRegex("b", "^ *total de acesso( dos artigo)?s: +[0-9]+ *$", true),
+    // Footer contains changing non-content, debug etc.
+    // http://www.portalseer.ufba.br/index.php/cmbio/article/viewArticle/4093
+    HtmlNodeFilters.tagWithAttribute("div", "id", "footer"),
+    // For Librello: download and view counts
+    // e.g. http://www.librelloph.com/challengesinsustainability/issue/view/10
+    HtmlNodeFilters.tagWithTextRegex("a", "^(HTML|PDF|Views)$", true),
+    new AndFilter(
+        HtmlNodeFilters.tagWithAttribute("span", "class", "badge"),
+        HtmlNodeFilters.tagWithTextRegex("span", "^[0-9]*$")),
+  };
+  
+  @Override
   public InputStream createFilteredInputStream(ArchivalUnit au,
                                                InputStream in,
-                                               String encoding) {
-    NodeFilter[] filters = new NodeFilter[] {
-            // Some OJS sites have a tag cloud
-            HtmlNodeFilters.tagWithAttribute("div", "id", "sidebarKeywordCloud"),
-            // Some OJS sites have a subscription status area
-            HtmlNodeFilters.tagWithAttribute("div", "id", "sidebarSubscription"),
-            // Some OJS sites have a language switcher, which can change over time
-            HtmlNodeFilters.tagWithAttribute("div", "id", "sidebarLanguageToggle"),
-            // Top-level menu items sometimes change over time
-            HtmlNodeFilters.tagWithAttribute("div", "id", "navbar"),
-            // Popular location for sidebar customizations
-            HtmlNodeFilters.tagWithAttribute("div", "id", "custom"),
-            //  the order of <meta> tags changes capriciously, which slows down testing
-            new TagNameFilter("head"),
-            // Site customizations often involve Javascript (e.g. Google Analytics), which can change over time
-            new TagNameFilter("script"),
-            // Date accessed is a variable
-            HtmlNodeFilters.tagWithTextRegex("div", "Date accessed: "),
-            // The version of the OJS software, which can change over time, appears in a tag
-            HtmlNodeFilters.tagWithAttribute("meta", "name", "generator"),
-            // Header image with variable dimensions
-            HtmlNodeFilters.tagWithAttribute("div", "id", "headerTitle"),
-            // For Ubiquity Press
-            HtmlNodeFilters.tagWithAttribute("div", "id", "rightSidebar"),
-            // For JLIS.it: landing pages contain user view count
-            HtmlNodeFilters.tagWithAttribute("span", "class", "ArticleViews"),
-            // For ibictpln: PHP Query Profiler
-            // e.g. http://seer.bce.unb.br/index.php/Musica/article/view/915
-            HtmlNodeFilters.tagWithAttribute("div", "id", "pqp-container"),
-            // Total de acessos: keeps changing, there is no 'good' tag wrapped around text
-            // e.g. https://www.revistas.unijui.edu.br/index.php/desenvolvimentoemquestao/issue/view/18
-            HtmlNodeFilters.tagWithTextRegex("b", "^ *total de acesso( dos artigo)?s: +[0-9]+ *$", true),
-            // Footer contains changing non-content, debug etc.
-            // http://www.portalseer.ufba.br/index.php/cmbio/article/viewArticle/4093
-            HtmlNodeFilters.tagWithAttribute("div", "id", "footer"),
-            // For Librello: download and view counts
-            // e.g. http://www.librelloph.com/challengesinsustainability/issue/view/10
-            HtmlNodeFilters.tagWithTextRegex("a", "^(HTML|PDF|Views)$", true),
-            new AndFilter(
-                HtmlNodeFilters.tagWithAttribute("span", "class", "badge"),
-                HtmlNodeFilters.tagWithTextRegex("span", "^[0-9]*$")),
-    };
-    return new HtmlFilterInputStream(in, encoding,
-          HtmlNodeFilterTransform.exclude(new OrFilter(filters))).
-          registerTag(new bTag());
+                                               String encoding)
+      throws PluginException {
+     
+    return doFiltering(in, encoding, null);
+  }
+  
+  public InputStream createFilteredInputStream(ArchivalUnit au,
+                                               InputStream in,
+                                               String encoding,
+                                               NodeFilter[] moreNodes)
+      throws PluginException {
+    
+    return doFiltering(in, encoding, moreNodes);
+  }
+  
+  /* the shared portion of the filtering
+   * pick up the extra nodes from the child if there are any
+   */
+  protected InputStream doFiltering(InputStream in, String encoding, NodeFilter[] moreNodes) {
+    NodeFilter[] filters = baseFilters;
+    if (moreNodes != null) {
+      filters = addTo(moreNodes);
+    }
+    
+    HtmlFilterInputStream filteredStream = new HtmlFilterInputStream(in, encoding,
+        HtmlNodeFilterTransform.exclude(new OrFilter(filters)));
+    filteredStream.registerTag(new bTag());
+    return filteredStream;
+  }
+  
+  /** Create an array of NodeFilters that combines the parent with the given array
+   *  @param nodes The array of NodeFilters to add
+   */
+  protected NodeFilter[] addTo(NodeFilter[] nodes) {
+    NodeFilter[] result  = Arrays.copyOf(baseFilters, baseFilters.length + nodes.length);
+    System.arraycopy(nodes, 0, result, baseFilters.length, nodes.length);
+    return result;
   }
   
 }
