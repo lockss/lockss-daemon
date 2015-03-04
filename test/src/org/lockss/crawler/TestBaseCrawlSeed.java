0e08c4ce8497b33254e9433c5b8dd3db2d10b01f
thibgc <github@thib.org>
2015-03-04 08:06:28 +0000

    Added CrawlerFacade constructor that calls the ArchivalUnit constructor (1.67.5).
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@40194 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/crawler/TestBaseCrawlSeed.java
+++ test/src/org/lockss/crawler/TestBaseCrawlSeed.java
@@ -1,3 +1,35 @@
+/*
+ * $Id$
+ */
+
+/*
+
+Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
 package org.lockss.crawler;
 
 import java.util.List;
@@ -5,6 +37,7 @@ import java.util.List;
 import org.apache.commons.collections.ListUtils;
 import org.lockss.daemon.PluginException;
 import org.lockss.daemon.PluginException.InvalidDefinition;
+import org.lockss.plugin.*;
 import org.lockss.plugin.ArchivalUnit.ConfigurationException;
 import org.lockss.test.LockssTestCase;
 import org.lockss.test.MockArchivalUnit;
@@ -48,7 +81,7 @@ public class TestBaseCrawlSeed extends LockssTestCase {
   
   public void testNullAu() {
     try {
-      bcs = new BaseCrawlSeed(null);
+      bcs = new BaseCrawlSeed((ArchivalUnit)null);
     } catch(Exception e) {
       fail("Should not throw it is conceivable"
           + " for a crawlseed to not need the au");
