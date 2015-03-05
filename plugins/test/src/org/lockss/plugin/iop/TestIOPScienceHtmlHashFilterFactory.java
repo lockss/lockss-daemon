8d70646e9ae1f3f9984457c37c20233a96ecb6d6
etenbrink <etenbrink@yahoo.com>
2015-03-05 22:19:33 +0000

    Modify test for added filtering
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@40248 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/test/src/org/lockss/plugin/iop/TestIOPScienceHtmlHashFilterFactory.java
+++ plugins/test/src/org/lockss/plugin/iop/TestIOPScienceHtmlHashFilterFactory.java
@@ -4,7 +4,7 @@
 
 /*
 
-Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -192,6 +192,9 @@ public class TestIOPScienceHtmlHashFilterFactory extends LockssTestCase {
   // test removal of sideTabBar, viewingLinks, metrics-panel, 
   private static final String miscHtml =
       "\n<body>stuff" +
+      "<div id=\"sideTabBar\">\n" + 
+      "  <input type=\"hidden\" value=\"0268-1242/27/1/015002\" name=\"articleId\">\n" + 
+      "\n" + 
       "<div class=\"sideTabBar\">\n" + 
       "  <input type=\"hidden\" value=\"0268-1242/27/1/015002\" name=\"articleId\">\n" + 
       "\n" + 
