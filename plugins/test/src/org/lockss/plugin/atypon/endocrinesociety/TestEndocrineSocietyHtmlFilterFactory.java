586c7708cf4378dc8f72fc71236468f05f40143b
lytdoan <ldoan@stanford.edu>
2015-03-10 21:10:50 +0000

    Plugin new version 4, parent 33 - updated crawl and hash filters to accomodate publishers' html changes
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@40393 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/test/src/org/lockss/plugin/atypon/endocrinesociety/TestEndocrineSocietyHtmlFilterFactory.java
+++ plugins/test/src/org/lockss/plugin/atypon/endocrinesociety/TestEndocrineSocietyHtmlFilterFactory.java
@@ -70,7 +70,7 @@ public class TestEndocrineSocietyHtmlFilterFactory
   // top right of issue toc - links to previous or next issue
   private static final String withNavJournal = 
       "<div class=\"block\">" +
-          "<section class=\"widget general-html none nav-journal " +
+          "<div class=\"widget general-html none nav-journal " +
           "widget-none  widget-compact-all\" id=\"d97a\">  " +
           "<div class=\"widget-body body body-none  body-compact-all\">" +
           "<div class=\"nav-journal\">" +
@@ -81,38 +81,89 @@ public class TestEndocrineSocietyHtmlFilterFactory
           "<li><a href=\"/page/jid/about\">About</a></li>" +
           "</ul>" +
           "</div></div> +" +
-          "</section>" +
+          "</div>" +
           "</div>";
   
+  private static final String withLiteratumBreadcrumbs =
+      "<div class=\"block\">" +
+          "<div id=\"de5\" class=\"widget literatumBreadcrumbs none " +
+          "breadcrumbs widget-none widget-compact-all\">" +
+          "<div class=\"wrapped \">" +
+          "<div class=\"widget-body body body-none body-compact-all\">" +
+          "<nav>" +
+          "<ul class=\"breadcrumbs\">" +
+          "<li class=\"\">" +
+          "<a href=\"/\">Home</a>" +
+          "<span class=\"divider\">></span>" +
+          "</li>" +
+          "<li class=\"\">" +
+          "<a href=\"/journal/jid\">Endocrine Reviews</a>" +
+          "<span class=\"divider\">></span>" +
+          "</li>" +
+          "<li class=\"\">" +
+          "<a href=\"/loi/jid\">List of Issues</a>" +
+          "<span class=\"divider\">></span>" +
+          "</li>" +
+          "<li class=\"\"> Volume 999, Issue 999 </li>" +
+          "</ul>" +
+          "</nav>" +
+          "</div>" +
+          "</div>" +
+          "</div>" +
+          "</div>";
+ 
   // from toc - access icon container 
   private static final String withAccessIconContainer =  
       "<div class=\"block\">" +
         "<td class=\"accessIconContainer\"><div></div></td>" +
       "</div>"; 
   
+  private static final String withMathJaxMessage =
+      "<div class=\"block\">" +  
+          "<div id=\"MathJax_Message\" style=\"display: none;\"></div>" +
+          "</div>";
+  
+  private static final String withSectionJumpTo =
+      "<div class=\"block\">" +        
+          "<div class=\"sectionJumpTo\">" +
+          "<div class=\"sectionLabel\">Section:</div>" +
+          "<form style=\"margin-bottom:0\">" +
+          "<select class=\"full\" onchange=\"GoTo(this, 's')\" name=\"s23\">" +
+          "<option selected=\"#\" value=\"#\">Choose</option>" +
+          "<option value=\"\">Abstract <<</option>" +
+          "<option value=\"#_i1\">I. intro</option>" +
+          "<option value=\"#_i2\">II. abc ...</option>" +
+          "<option value=\"#_i30\">XII. dfe</option>" +
+          "<option value=\"#_i33\">ref</option>" +
+          "<option value=\"#citart1\">CITING ARTICLES</option>" +
+          "</select>" +
+          "</form>" +
+          "</div>" +
+          "</div>" ;
+  
   // from toc - top panel with 'subscribe'
   private static final String withGutterless =  
       "<div class=\"block\">" +
           "<div class=\"pb-columns row-fluid gutterless\">" +
-          "<section class=\"widget general-image none  widget-none  " +
+          "<div class=\"widget general-image none  widget-none  " +
           "widget-compact-all\" id=\"5bd\">" +
           "<div class=\"widget-body body body-none  body-compact-all\">" +
           "<a href=\"/journal/jid\"><img src=\"/imagesrc/header.jpg\"></a>" +
           "</div>" +
-          "</section>" +
-          "<section class=\"widget general-image alignRight " +
+          "</div>" +
+          "<div class=\"widget general-image alignRight " +
           "subsButtonHead widget-none  widget-compact-all\" id=\"7ae\">" +
           "<div class=\"widget-body body body-none  body-compact-all\">" +
           "<a href=\"http://www.example.com/e781\">" +
           "<img src=\"/imagesrc/subscribe.png\"></a></div>" +
-          "</section>" +
+          "</div>" +
           "</div>" +
           "</div>";
           
   // top right of issue toc - links to previous or next issue
   private static final String withLiteratumBookIssueNavigation = 
       "<div class=\"block\">" +
-          "<section class=\"widget literatumBookIssueNavigation none  " +
+          "<div class=\"widget literatumBookIssueNavigation none  " +
           "widget-none\" id=\"xxx\">" +
           "<div class=\"wrapped \">" +
           "<div class=\"widget-body body body-none \">" +
@@ -127,20 +178,36 @@ public class TestEndocrineSocietyHtmlFilterFactory
           "</tbody>" +
           "</table>" +
           "</div></div></div>" +
-          "</section>" +
+          "</div>" +
           "</div>";
+  
+  private static final String withFreeGif =
+      "<div class=\"block\">" +
+          "<img src=\"/imagessrc/free.gif\">" +
+          "</div>";
+  
+  private static final String withCitedby =
+      "<div class=\"block\">" + 
+          "<ul><li>" +
+          "<a href=\"/doi/citedby/11.1111/jid.20129999\"> Cited by </a>" +
+          "</li></ul>" +
+          "</div>"; 
+  private static final String withoutCitedby =
+      "<div class=\"block\">" + 
+          "<ul></ul>" +
+          "</div>";   
   
   // right column of an article - all except Download Citations
   private static final String withRightColumnExceptDownloadCitation = 
       "<div class=\"block\">" +
-          "<section class=\"widget literatumRightSidebar none right-sidebar " +
+          "<div class=\"widget literatumRightSidebar none right-sidebar " +
           "widget-none\" id=\"pageRightSidebar\">" +
           "<div class=\"sidebar sidebar-right\">" +
           "<div data-pb-dropzone=\"main\">" +
-          "<section class=\"widget general-widget\" id=\"xxx\">" +
+          "<div class=\"widget general-widget\" id=\"xxx\">" +
           "<div class=\"widget-body\"><a href=\"/act/s\">Authen</a></div>" +
-          "</section>" +
-          "<section class=\"widget-box\" id=\"yyy\">" +
+          "</div>" +
+          "<div class=\"widget-box\" id=\"yyy\">" +
           "<div class=\"body-box \">" +
           "<div class=\"artTools\">" +
           "<ul class=\"linkList\">" +
@@ -153,31 +220,27 @@ public class TestEndocrineSocietyHtmlFilterFactory
           "<a href=\"/action/addCitAlert?doi=11.1111%2Fen.2012-1111\">Ta</a>" +
           "</li>" +
           "</ul>" +
-          "</div></div>" +
-          "</section>" +
-          "</div>" +
-          "</div>" +
-          "</section>" +
+          "</div></div></div></div></div></div>" +
           "</div>";
   
-  private static final String RightColumnFiltered = 
+  // 'id' attributes also removed
+  private static final String rightColumnFiltered = 
       "<div class=\"block\">" +
-          "<section class=\"widget literatumRightSidebar none right-sidebar " +
-          "widget-none\" id=\"pageRightSidebar\">" +
-          "<div class=\"sidebar sidebar-right\">" +
-          "<div data-pb-dropzone=\"main\">" +
-          "<section class=\"widget-box\" id=\"yyy\">" +
-          "<div class=\"body-box \">" +
-          "<div class=\"artTools\">" +
-          "<ul class=\"linkList\">" +
-          "<li class=\"downloadCitations\">" +
-          "<a href=\"/action/showCitFormats?doi=11.1111%2Fen.2012-1111\">" +
-          "Download Citation</a>" +
-          "</li>" +
-          "</ul>" +
-          "</div></div></section></div></div>" +
-          "</section>" +
-          "</div>";
+      "<div class=\"widget literatumRightSidebar none right-sidebar " +
+      "widget-none\" >" +
+      "<div class=\"sidebar sidebar-right\">" +
+      "<div data-pb-dropzone=\"main\">" +
+      "<div class=\"widget-box\" >" +
+      "<div class=\"body-box \">" +
+      "<div class=\"artTools\">" +
+      "<ul class=\"linkList\">" +
+      "<li class=\"downloadCitations\">" +
+      "<a href=\"/action/showCitFormats?doi=11.1111%2Fen.2012-1111\">" +
+      "Download Citation</a>" +
+      "</li>" +
+      "</ul>" +
+      "</div></div></div></div></div></div>" +
+      "</div>";
   
   // related content near Erratum
   // http://press.endocrine.org/toc/endo/154/10
@@ -216,29 +279,49 @@ public class TestEndocrineSocietyHtmlFilterFactory
   // pageheader
   private static final String withPageHeader =  
       "<div class=\"block\">" +
-          "<section class=\"widget pageHeader none header widget-none  " +
+          "<div class=\"widget pageHeader none header widget-none  " +
           "widget-compact-all\" id=\"pageHeader\">" +
           "<div class=\"widget-body body body-none  body-compact-all\">" +
           "<div class=\"page-header\">" +
-          "<section class=\"widget doubleClickAd alignCenter leaderBoard " +
+          "<div class=\"widget doubleClickAd alignCenter leaderBoard " +
           "widget-none  widget-compact-all\" id=\"1e3\">" +
-          "</section>" +
+          "</div>" +
           "</div></div>" +
-          "</section>" +
+          "</div>" +
           "</div>";        
   
   // pageFooter
   private static final String withPageFooter =  
       "<div class=\"block\">" +
-          "<section class=\"widget pageFooter none container-footer " +
+          "<div class=\"widget pageFooter none container-footer " +
           "widget-none  widget-compact-all\" id=\"pageFooter\">" +
           "<div class=\"widget-body body body-none  body-compact-all\">" +
           "<div class=\"page-footer\">" +
           "<div class=\"copyright\">" +
           "<p>&copy; 2014 Endocrine Society</p>" +
           "</div></div></div>" +
-          "</section>" +
-          "</div>";           
+          "</div>" +
+          "</div>"; 
+  
+  // showCitFormats - Support and Help block
+  // http://press.endocrine.org/action/showCitFormats?doi=10.1210%2Fjc.2013-1811
+  private static final String withTwoColumnRightDropZoneColor =  
+      "<div class=\"block\">" +
+          "<div class=\"twoColumnRight twoColumnRightDropZoneColor \">" +
+          "<div data-pb-dropzone=\"right\">" +
+          "<div id=\"pageRightSidebar\" class=\"wid literatumRightSidebar " +
+          "none right-sidebar widget-none\">" +
+          "<div class=\"widget-body body-none body-separator body-shadow " +
+          "body-border-toggle body-rounded body-alternate\">" +
+          "<ul>" +
+          "<li><a href=\"http://example.com/page/firefox-pdf-issues\">" +
+          "Opening PDFs with Firefox</a></li>" +
+          "<li><a href=\"http://example.com/page/login-issues\">" +
+          "Sign In and Authentication</a></li>" +
+          "</ul>" +
+          "</div></div></div></div>" +
+          "</div>";
+               
   
   protected ArchivalUnit createAu()
       throws ArchivalUnit.ConfigurationException {
@@ -299,9 +382,18 @@ public class TestEndocrineSocietyHtmlFilterFactory
       doFilterTest(esau, variantFact, withNavJournal, filteredStr);
       doFilterTest(esau, variantFact, withAccessIconContainer, filteredStr);
       doFilterTest(esau, variantFact, withGutterless, filteredStr);
-      doFilterTest(esau, variantFact, 
-          withRightColumnExceptDownloadCitation, RightColumnFiltered);
+      doFilterTest(esau, variantFact, withRightColumnExceptDownloadCitation, 
+                   rightColumnFiltered);
       doFilterTest(esau, variantFact, withPageFooter, filteredStr);
+      doFilterTest(esau, variantFact, withLiteratumBreadcrumbs, filteredStr);
+      doFilterTest(esau, variantFact, withLiteratumBookIssueNavigation, 
+                   filteredStr);
+      doFilterTest(esau, variantFact, withFreeGif, filteredStr);
+      doFilterTest(esau, variantFact, withMathJaxMessage, filteredStr);
+      doFilterTest(esau, variantFact, withCitedby, withoutCitedby); 
+      doFilterTest(esau, variantFact, withSectionJumpTo, filteredStr);
+      doFilterTest(esau, variantFact, withTwoColumnRightDropZoneColor, 
+                   filteredStr);         
     }
   }
   
