6114be71c746c4d6296b2938936647055ba02471
tlipkis <tal@pss.com>
2015-05-11 06:32:18 +0000

    Delay incorrect password response by .25 second.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@41976 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- src/org/lockss/jetty/LockssUserRealm.java
+++ src/org/lockss/jetty/LockssUserRealm.java
@@ -4,7 +4,7 @@
 
 /*
 
-Copyright (c) 2000-2009 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -238,6 +238,14 @@ public class LockssUserRealm implements UserRealm {
 	res = false;
       } else {
 	res = acct.check(credentials);
+	if (!res) {
+	  // Delay failing password response a little
+	  try {
+	    Deadline.in(250).sleep();
+	  } catch (InterruptedException e) {
+	    // ignore
+	  }
+	}
       }
       if (res) {
 	if (acct.isPasswordExpired()) {
