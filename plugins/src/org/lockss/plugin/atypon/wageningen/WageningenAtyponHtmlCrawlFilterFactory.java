c3e231666ce4f575f0433b940157fea914702aed
lytdoan <ldoan@stanford.edu>
2015-05-13 04:40:59 +0000

    New plugin preliminary checkin.  Development will resume when manifest pages are up.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@42026 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/atypon/wageningen/WageningenAtyponHtmlCrawlFilterFactory.java
@@ -0,0 +1,73 @@
+/*
+ * $Id$
+ */
+
+/*
+
+Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.atypon.wageningen;
+
+import java.io.InputStream;
+
+import org.htmlparser.NodeFilter;
+import org.lockss.daemon.PluginException;
+import org.lockss.filter.html.*;
+import org.lockss.plugin.*;
+import org.lockss.plugin.atypon.BaseAtyponHtmlCrawlFilterFactory;
+
+public class WageningenAtyponHtmlCrawlFilterFactory 
+  extends BaseAtyponHtmlCrawlFilterFactory {
+  
+  static NodeFilter[] filters = new NodeFilter[] {
+    // handled by parent:
+    // previous/next article - <td class="journalNavRightTd">
+    
+    // toc, abs, ref - breadcrumbs
+    // http://www.wageningenacademic.com/toc/bm/5/1
+    // http://www.wageningenacademic.com/doi/abs/10.3920/BM2012.0069
+    HtmlNodeFilters.tagWithAttribute("ul", "class", "breadcrumbs"),    
+    // toc, abs, ref, suppl - right column most read/most cited
+    HtmlNodeFilters.tagWithAttribute("div", "aria-relevant", "additions"),
+    // abs, ref - below <div class="response">, or after the main content
+    HtmlNodeFilters.tagWithAttribute("div", "id", "relatedContent"),
+    // abs, ref, suppl - all right column except Citation Mgr
+    // http://www.wageningenacademic.com/doi/abs/10.3920/BM2012.0069
+    HtmlNodeFilters.allExceptSubtree(
+        HtmlNodeFilters.tagWithAttributeRegex("div", "class", "articleTools"),
+          HtmlNodeFilters.tagWithAttributeRegex(
+                 "a", "href", "/action/showCitFormats\\?")) 
+
+  };
+
+  @Override
+  public InputStream createFilteredInputStream(ArchivalUnit au,
+      InputStream in, String encoding) throws PluginException{
+    return super.createFilteredInputStream(au, in, encoding, filters);
+  }
+  
+}
