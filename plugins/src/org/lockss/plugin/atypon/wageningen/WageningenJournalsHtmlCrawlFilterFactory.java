6a14ad4f83b7a79112bffe39314568f00a756890
lytdoan <ldoan@stanford.edu>
2015-05-18 19:53:13 +0000

    Changed plugin name.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@42132 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/atypon/wageningen/WageningenJournalsHtmlCrawlFilterFactory.java
+++ plugins/src/org/lockss/plugin/atypon/wageningen/WageningenJournalsHtmlCrawlFilterFactory.java
@@ -40,7 +40,7 @@ import org.lockss.filter.html.*;
 import org.lockss.plugin.*;
 import org.lockss.plugin.atypon.BaseAtyponHtmlCrawlFilterFactory;
 
-public class WageningenAtyponHtmlCrawlFilterFactory 
+public class WageningenJournalsHtmlCrawlFilterFactory 
   extends BaseAtyponHtmlCrawlFilterFactory {
   
   static NodeFilter[] filters = new NodeFilter[] {
--- plugins/src/org/lockss/plugin/atypon/wageningen/WageningenJournalsHtmlHashFilterFactory.java
+++ plugins/src/org/lockss/plugin/atypon/wageningen/WageningenJournalsHtmlHashFilterFactory.java
@@ -43,7 +43,7 @@ import org.lockss.plugin.atypon.BaseAtyponHtmlHashFilterFactory;
 
 // Keeps contents only (includeNodes), then hashes out unwanted nodes 
 // within the content (excludeNodes).
-public class WageningenAtyponHtmlHashFilterFactory 
+public class WageningenJournalsHtmlHashFilterFactory 
   extends BaseAtyponHtmlHashFilterFactory  {
      
   @Override
