03dd2f9fd260ac8d6bd4d29c3dcf9afc344f7723
mellen22 <maryellen@lockss.org>
2015-06-17 20:27:49 +0000

    Find title of journal.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@42592 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -1250,6 +1250,9 @@ while (my $line = <>) {
       if (defined($man_contents) && ($man_contents =~ m/$lockss_tag/) && ($man_contents =~ m/\/publications\/$param{journal_id}\/tocs\/$param{volume_name}\//)) {
         if ($man_contents =~ m/<title>(.*)<\/title>/si) {
           $vol_title = $1;
+          $vol_title =~ s/ | Digital Library//;
+          $vol_title =~ s/ - / /;  &amp;
+          $vol_title =~ s/&amp\;/&/;  
           }
         $result = "Manifest"
       } else {
@@ -1271,6 +1274,9 @@ while (my $line = <>) {
       if (defined($man_contents) && ($man_contents =~ m/$clockss_tag/) && ($man_contents =~ m/\/publications\/$param{journal_id}\/tocs\/$param{volume_name}\//)) {
         if ($man_contents =~ m/<title>(.*)<\/title>/si) {
           $vol_title = $1;
+          $vol_title =~ s/ | Digital Library//;
+          $vol_title =~ s/ - / /;
+          $vol_title =~ s/&amp\;/&/;  
           }
         $result = "Manifest"
       } else {
