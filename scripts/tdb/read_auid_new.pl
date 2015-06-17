7bc13e573cc3506423e0ca264626d43e1e0923b1
mellen22 <maryellen@lockss.org>
2015-06-17 20:21:19 +0000

    Find title of journal.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@42591 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -1248,6 +1248,9 @@ while (my $line = <>) {
     if ($resp->is_success) {
       my $man_contents = $resp->content;
       if (defined($man_contents) && ($man_contents =~ m/$lockss_tag/) && ($man_contents =~ m/\/publications\/$param{journal_id}\/tocs\/$param{volume_name}\//)) {
+        if ($man_contents =~ m/<title>(.*)<\/title>/si) {
+          $vol_title = $1;
+          }
         $result = "Manifest"
       } else {
         $result = "--NO_TAG--"
@@ -1266,6 +1269,9 @@ while (my $line = <>) {
     if ($resp->is_success) {
       my $man_contents = $resp->content;
       if (defined($man_contents) && ($man_contents =~ m/$clockss_tag/) && ($man_contents =~ m/\/publications\/$param{journal_id}\/tocs\/$param{volume_name}\//)) {
+        if ($man_contents =~ m/<title>(.*)<\/title>/si) {
+          $vol_title = $1;
+          }
         $result = "Manifest"
       } else {
         $result = "--NO_TAG--"
