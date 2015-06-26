7bcdab0dcedfe61ec95c4ba2ee082d2cace066c2
etenbrink <etenbrink@yahoo.com>
2015-06-26 04:49:37 +0000

    Bump plugin version; add ArticleIterator and Substance pattern
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@42825 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/edu/princeton/plugin/bmcr/BMCRArticleIteratorFactory.java
@@ -0,0 +1,93 @@
+/*
+ * $Id: HighWireDrupalArticleIteratorFactory.java 39864 2015-02-18 09:10:24Z thib_gc $
+ */
+
+/*
+
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package edu.princeton.plugin.bmcr;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.regex.*;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.*;
+import org.lockss.plugin.*;
+import org.lockss.util.Logger;
+
+public class BMCRArticleIteratorFactory
+    implements ArticleIteratorFactory,
+               ArticleMetadataExtractorFactory {
+  
+  private static final Logger log = Logger.getLogger(BMCRArticleIteratorFactory.class);
+  
+  protected static final String ROOT_TEMPLATE =
+    "\"%s%d/\", base_url, year";
+  /*
+   */
+  // <base_url>/<year>/<pg>.html
+  protected static final String PATTERN_TEMPLATE =
+    "\"^%s%d/(?:%d-[^./?&]+).html$\", base_url, year, year";
+  
+  // various aspects of an article ;)
+  // http://bmcr.brynmawr.edu/2005/2005-01-01.html
+  
+  // these kinds of urls are not used as part of the AI
+  // http://bmcr.brynmawr.edu/2005
+  
+  protected static final Pattern HTML_PATTERN = Pattern.compile(
+      "/([^./?&]+).html$", Pattern.CASE_INSENSITIVE);
+  
+  // how to change from one form (aspect) of article to another
+  protected static final String HTML_REPLACEMENT = "/$1.html";
+  
+  public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au,
+                                                      MetadataTarget target)
+      throws PluginException {
+    SubTreeArticleIteratorBuilder builder = new SubTreeArticleIteratorBuilder(au);
+    
+    builder.setSpec(target,
+        ROOT_TEMPLATE, PATTERN_TEMPLATE, Pattern.CASE_INSENSITIVE);
+    
+    builder.addAspect(
+        HTML_PATTERN, HTML_REPLACEMENT,
+        ArticleFiles.ROLE_FULL_TEXT_HTML,
+        ArticleFiles.ROLE_ARTICLE_METADATA);
+    
+    return builder.getSubTreeArticleIterator();
+  }
+  
+  // getting metadata from the tdb - BaseArticleMetadataExtractor does that for us!
+  @Override
+  public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target)
+      throws PluginException {
+    return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
+  }
+
+}
