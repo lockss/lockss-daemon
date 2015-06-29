0b6f12959dcfd08a62d88400dbf4a1ecee083da7
etenbrink <etenbrink@yahoo.com>
2015-06-29 23:37:29 +0000

    Initial tests
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@42847 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/test/src/edu/princeton/plugin/bmcr/TestBMCRPlugin.java
@@ -0,0 +1,163 @@
+/*
+ * $Id: .java 42654 2015-06-19 10:40:23Z etenbrink $
+ */
+
+/*
+
+Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package edu.princeton.plugin.bmcr;
+
+import java.net.MalformedURLException;
+import java.util.Properties;
+
+import org.lockss.config.Configuration;
+import org.lockss.daemon.*;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.plugin.ArchivalUnit.ConfigurationException;
+import org.lockss.plugin.definable.*;
+import org.lockss.test.*;
+import org.lockss.util.ListUtil;
+import org.lockss.util.urlconn.CacheException;
+import org.lockss.util.urlconn.HttpResultMap;
+
+public class TestBMCRPlugin extends LockssTestCase {
+  
+  static final String BASE_URL_KEY = ConfigParamDescr.BASE_URL.getKey();
+  static final String YEAR_KEY = ConfigParamDescr.YEAR.getKey();
+  
+  private MockLockssDaemon theDaemon;
+  private DefinablePlugin plugin;
+  
+  public TestBMCRPlugin(String msg) {
+    super(msg);
+  }
+  
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    setUpDiskSpace();
+    theDaemon = getMockLockssDaemon();
+    plugin = new DefinablePlugin();
+    plugin.initPlugin(getMockLockssDaemon(),
+        "edu.princeton.plugin.bmcr.BMCRPlugin");
+    
+  }
+  
+  public void testGetAuNullConfig()
+      throws ArchivalUnit.ConfigurationException {
+    try {
+      plugin.configureAu(null, null);
+      fail("Didn't throw ArchivalUnit.ConfigurationException");
+    } catch (ArchivalUnit.ConfigurationException e) {
+    }
+  }
+  public void testCreateAu() throws ConfigurationException {
+    Properties props = new Properties();
+    props.setProperty(BASE_URL_KEY, "http://www.example.com/");
+    props.setProperty(YEAR_KEY, "2012");
+    makeAuFromProps(props);
+  }
+  
+  private DefinableArchivalUnit makeAuFromProps(Properties props)
+      throws ArchivalUnit.ConfigurationException {
+    Configuration config = ConfigurationUtil.fromProps(props);
+    return (DefinableArchivalUnit)plugin.configureAu(config, null);
+  }
+  
+  public void testGetAuConstructsProperAu()
+      throws ArchivalUnit.ConfigurationException, MalformedURLException {
+    Properties props = new Properties();
+    props.setProperty(YEAR_KEY, "2003");
+    props.setProperty(BASE_URL_KEY, "http://www.example.com/");
+    
+    String starturl =
+        "http://www.example.com/archive.html";
+    DefinableArchivalUnit au = makeAuFromProps(props);
+    assertEquals("BMCR Plugin, Base URL http://www.example.com/, Year 2003",
+        au.getName());
+    assertEquals(ListUtil.list(starturl), au.getStartUrls());
+  }
+  
+  public void testGetPluginId() {
+    assertEquals("edu.princeton.plugin.bmcr.BMCRPlugin",
+        plugin.getPluginId());
+  }
+  
+  public void testGetAuConfigProperties() {
+    assertEquals(ListUtil.list(ConfigParamDescr.BASE_URL,
+        ConfigParamDescr.YEAR),
+        plugin.getLocalAuConfigDescrs());
+  }
+  
+  // Test the crawl rules for eLife
+  public void testShouldCacheProperPages() throws Exception {
+    String ROOT_URL = "http://highwire.org/";
+    Properties props = new Properties();
+    props.setProperty(BASE_URL_KEY, ROOT_URL);
+    props.setProperty(YEAR_KEY, "2015");
+    DefinableArchivalUnit au = null;
+    try {
+      au = makeAuFromProps(props);
+    }
+    catch (ConfigurationException ex) {
+    }
+    theDaemon.getLockssRepository(au);
+    
+    // Test for pages that should get crawled or not
+    // permission page/start url manifest1990.html
+    shouldCacheTest(ROOT_URL + "archive.html", true, au);
+    shouldCacheTest(ROOT_URL + "manifest1990.html", true, au);
+    shouldCacheTest(ROOT_URL + "manifest2015.html", true, au);
+    shouldCacheTest(ROOT_URL + "manifest2014.html", false, au);
+    shouldCacheTest(ROOT_URL + "manifest/year=2015", false, au);
+    // toc page for a volume, issue
+    shouldCacheTest(ROOT_URL + "2015", true, au);
+    shouldCacheTest(ROOT_URL + "2015/", true, au);
+    shouldCacheTest(ROOT_URL + "2014", false, au);
+    shouldCacheTest(ROOT_URL + "1990", false, au);
+    shouldCacheTest(ROOT_URL + "content/2015", false, au);
+    // article files
+    shouldCacheTest(ROOT_URL + "2015/2015-01-01.html", true, au);
+    
+    shouldCacheTest(ROOT_URL + "2015/2015-01-01.01", false, au);
+    shouldCacheTest(ROOT_URL + "2015/2015-01-01.abstract", false, au);
+    shouldCacheTest(ROOT_URL + "2015/2015-01-01", false, au);
+    
+    shouldCacheTest("", false, au);
+    
+    // should not get crawled - LOCKSS
+    shouldCacheTest("http://lockss.stanford.edu", false, au);
+    
+  }
+  
+  private void shouldCacheTest(String url, boolean shouldCache, ArchivalUnit au) {
+    log.info ("shouldCacheTest url: " + url);
+    assertEquals(shouldCache, au.shouldBeCached(url));
+  }
+  
+}
