38fd455456b4649bea25f8d8425c10e1f04f1bf1
thibgc <github@thib.org>
2015-07-31 21:19:47 +0000

    Don't crawl filter article toolbox in sidebar. Exclude duplicate cookies URLs in crawl rules. Fetch rate.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@43340 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/ojs2/OJS2HtmlCrawlFilterFactory.java
+++ plugins/src/org/lockss/plugin/ojs2/OJS2HtmlCrawlFilterFactory.java
@@ -4,7 +4,7 @@
 
 /*
 
-Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -52,9 +52,10 @@ public class OJS2HtmlCrawlFilterFactory implements FilterFactory {
         
         // Some sidebars contain links to all other issue TOCs
         // e.g. http://ojs.statsbiblioteket.dk/index.php/bras/issue/view/1049
-        // Keep sidebar images for now (probably important logos)
+        // Keep sidebar images (important logos) and article toolbox (printer-friendly link, etc.)
         HtmlNodeFilters.allExceptSubtree(HtmlNodeFilters.tagWithAttribute("div", "id", "sidebar"),
-                                         HtmlNodeFilters.tag("img")),
+                                         new OrFilter(HtmlNodeFilters.tag("img"),
+                                                      HtmlNodeFilters.tagWithAttribute("div", "id", "sidebarRTArticleTools"))),
         
         // do not get links from navbar
         HtmlNodeFilters.tagWithAttribute("div", "id", "navbar"),
