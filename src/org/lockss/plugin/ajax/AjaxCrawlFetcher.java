48a89f24b16532a06f709c5092cb2c32343b5a8e
clairegriffin <clairetg@stanford.edu>
2015-08-03 16:01:13 +0000

    add ajax AU support
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@43355 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ src/org/lockss/plugin/ajax/AjaxCrawlFetcher.java
@@ -0,0 +1,299 @@
+/*
+ * $Id: AJaxCrawlFetcher.java 39864 2015-02-18 09:10:24Z thib_gc $
+ */
+
+/*
+
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+package org.lockss.plugin.ajax;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.lockss.config.Configuration;
+import org.lockss.config.CurrentConfig;
+import org.lockss.crawler.CrawlUrlData;
+import org.lockss.crawljax.AjaxRequestResponse;
+import org.lockss.crawljax.AjaxRequestResponse.Header;
+import org.lockss.crawljax.AjaxRequestResponse.IndexEntry;
+import org.lockss.crawljax.AjaxRequestResponse.Response;
+import org.lockss.daemon.Crawler;
+import org.lockss.daemon.LockssWatchdog;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.FetchedUrlData;
+import org.lockss.plugin.base.BaseUrlFetcher;
+import org.lockss.util.CIProperties;
+import org.lockss.util.HeaderUtil;
+import org.lockss.util.Logger;
+import org.lockss.util.StringUtil;
+import org.lockss.util.urlconn.CacheException;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AjaxCrawlFetcher  extends BaseUrlFetcher {
+  // The Parameters for use by plugins
+
+  public static final String PREFIX =
+    Configuration.PREFIX + "extractor.crawljax.";
+  public static final String PARAM_DEBUG = PREFIX + "debug";
+  public static final String PARAM_OUTPUT_DIR = PREFIX + "outDir";
+  public static final String PARAM_CLASSPATH = PREFIX + "classpath";
+  public static final String PARAM_CONFIG_FILE = PREFIX + "configFile";
+  public static final String LINK_FILE_NAME = "links.txt";
+
+  static final Logger theLogger = Logger.getLogger(AjaxCrawlFetcher.class);
+
+
+  /**
+   * the au for this ajax extractor
+   */
+  private ArchivalUnit m_au;
+
+  /**
+   * the classpath to send when invoking the jar file
+   */
+  private String m_classpath;
+
+  /**
+   * the base command line as configured during construction
+   */
+  private ArrayList<String> m_baseCommand;
+
+   /* required arguments */
+  /**
+   * the directory to use for files.
+   */
+  private String m_outdir;
+
+  /** the configuration file for the crawl */
+  private String m_configFile;
+
+ /* optional arguments: all string arguments are set to null, all numbers are -1 */
+  /**
+   * set true if we should crawl nonvisible links
+   */
+  private boolean m_debug = false;
+
+  protected LockssWatchdog wdog;
+
+  public AjaxCrawlFetcher(Crawler.CrawlerFacade crawlFacade, String url) {
+    super(crawlFacade, url);
+    m_classpath = CurrentConfig.getParam(PARAM_CLASSPATH);
+    m_outdir = makeCrawlDir(CurrentConfig.getParam(PARAM_OUTPUT_DIR));
+    m_configFile = CurrentConfig.getParam(PARAM_CONFIG_FILE);
+    m_debug = CurrentConfig.getBooleanParam(PARAM_DEBUG, false);
+    m_baseCommand = makeBaseCommand();
+  }
+
+
+  public FetchResult fetch() {
+    int exitValue = -1;
+    List<String> command = new ArrayList<String>();
+    command.addAll(m_baseCommand);
+    command.add(fetchUrl);
+    command.add(m_outdir);
+    try {
+      ProcessBuilder pbuilder = new ProcessBuilder(command);
+      pbuilder.redirectErrorStream(true);
+      Process process = pbuilder.start();
+      InputStreamReader isr = new  InputStreamReader(process.getInputStream());
+      BufferedReader br = new BufferedReader(isr);
+
+      if(theLogger.isDebug())
+        theLogger.debug("Running command:" + pbuilder.command() +"...");
+      String line;
+      while ((line = br.readLine()) != null) {
+        // todo: modify this to add a heartbeat...
+        if(theLogger.isDebug())
+          theLogger.debug(line);
+      }
+      exitValue = process.waitFor();
+      if(theLogger.isDebug()) {
+        theLogger.debug("Ajax Crawl for " + fetchUrl + " exit: " + exitValue);
+      }
+      // process
+      File outDir = new File(m_outdir);
+      // process the index file
+      File idxFile = new File(outDir,AjaxRequestResponse.INDEX_FILE_NAME);
+      processIndexFile(idxFile);
+    } catch (IOException ioe) {
+      theLogger.warning("Ajax Crawl Failed.", ioe);
+    } catch (InterruptedException e) {
+      theLogger.warning("Ajax Crawl Interrupted:" + exitValue, e);
+    }
+    return null;
+  }
+
+
+  /**
+   * Get a unique output directory name
+   * @return the absolute path to the newly created directory.
+   */
+  protected String makeCrawlDir(String outdir)
+  {
+    String fdir = String.valueOf(System.currentTimeMillis());
+    File file = new File(outdir, m_au.getAuId() + "_" + fdir);
+    file.mkdirs();
+    return file.getAbsolutePath();
+  }
+
+  /**
+   * Process the index file.  This is the most reliable way to process
+   * the output. It contains a list of request url to Request-Response output
+   * file names.
+   * @param indexFile the file containing our json file
+   * @throws IOException
+   */
+  protected void processIndexFile(File indexFile) throws IOException {
+    if(indexFile.exists() && indexFile.canRead()) {
+      // we load in the json file and read it.
+      ObjectMapper mapper = new ObjectMapper();
+      List<IndexEntry> entries = mapper.readValue(indexFile,
+                                                  new TypeReference<List<IndexEntry>>(){});
+      // now we have the urls and their corresponding result files
+      String reqUrl;
+      String filePath;
+      for(IndexEntry entry : entries) {
+        reqUrl = entry.getUrl();
+        filePath = entry.getFile();
+        processOneFile(reqUrl, reqUrl, filePath);
+      }
+    }
+  }
+
+  /**
+   * process a AjaxRequestResponse file we fetched
+   * @param reqUrl the url that was sent to the server.
+   * @param filePath the location of the file returned.
+   * @throws IOException
+   */
+  private FetchResult processOneFile(String reqUrl, String fetchUrl,
+                                String filePath) throws IOException{
+    File rrFile = new File(filePath);
+    ObjectMapper mapper = new ObjectMapper();
+    AjaxRequestResponse rr = mapper.readValue(rrFile, AjaxRequestResponse.class);
+    Response response = rr.getResponse();
+    CIProperties headers = makeCIProperties(response.getHeaders());
+    String ctype = headers.getProperty("Content-Type");
+    // todo change the url generate CrawlUrlData for later usage
+
+    if (!StringUtil.isNullString(ctype)) {
+      headers.setProperty(CachedUrl.PROPERTY_CONTENT_TYPE, ctype);
+    }
+    ByteArrayInputStream input = new ByteArrayInputStream(response.getContent());
+
+    FetchedUrlData fud = new FetchedUrlData(reqUrl, fetchUrl,
+                                            input, headers,
+                                            redirectUrls, this);
+    CrawlUrlData cud = new CrawlUrlData(reqUrl, curl.getDepth());
+
+    try {
+      fud.setStoreRedirects(redirectScheme
+                              .isRedirectOption(RedirectScheme.REDIRECT_OPTION_STORE_ALL));
+      fud.setFetchFlags(fetchFlags);
+      getUrlConsumerFactory().createUrlConsumer(crawlFacade, fud).consume();
+      updateCacheStats(FetchResult.FETCHED, cud );
+      crawlFacade.addToParseQueue(cud);
+    } catch (CacheException ex) {
+      crawlStatus.signalErrorForUrl(reqUrl, ex);
+      crawlStatus.setCrawlStatus(Crawler.STATUS_FETCH_ERROR);
+      return FetchResult.NOT_FETCHED;
+    }
+    return FetchResult.FETCHED;
+
+  }
+
+  /**
+   * update the cache stats for each fetched url which is not
+   * @param res  the result to use for setting the specific crawl result
+   * @param curl the CrawlUrlData used to crawl the ajax content
+   */
+  protected void updateCacheStats(FetchResult res, CrawlUrlData curl) {
+    switch (res) {
+      case FETCHED:
+        crawlStatus.signalUrlFetched(curl.getUrl());
+        curl.setFetched(true);
+        CachedUrl cu = au.makeCachedUrl(curl.getUrl());
+
+        if (cu.hasContent()) {
+          String conType = cu.getContentType();
+          if (conType != null) {
+            String mimeType = HeaderUtil.getMimeTypeFromContentType(conType);
+            crawlStatus.signalMimeTypeOfUrl(mimeType, cu.getUrl());
+          }
+          crawlStatus.addContentBytesFetched(cu.getContentSize());
+          previousContentType = cu.getContentType();
+        }
+        cu.release();
+      break;
+      case NOT_FETCHED:
+        curl.setFetched(false);
+        break;
+    }
+  }
+
+  /**
+   * make the command line based on the values from the context.  This
+   * excludes the outdir and url which are added dynamically.
+   * "java -jar lockss-crawljax.jar configFile outDir url";
+   * @return  the command line as an array
+   */
+  protected ArrayList<String> makeBaseCommand()
+  {
+    ArrayList<String> params = new ArrayList<String>();
+    params.add("java");
+    if(m_classpath != null) {
+      params.add("-cp");
+      params.add(System.getProperty("java.class.path").concat(";").concat(m_classpath));
+    }
+    params.add("-jar");
+    params.add("lockss-crawljax.jar");
+    if(m_configFile != null)
+      params.add(m_configFile);
+    return params;
+  }
+
+  protected CIProperties makeCIProperties(List<Header> headers)
+    throws IOException {
+    CIProperties ret = new CIProperties();
+    for(Header header : headers) {
+      String key = header.getName();
+      String value = header.getValue();
+      if(value == null) {
+        theLogger.warning("Ignoring null value for key '" + key + "'.");
+      }
+      else {
+        theLogger.debug3(key + ": " + value);
+        ret.put(key, value);
+      }
+    }
+    return (ret);
+  }
+
+}
---
+++ src/org/lockss/plugin/ajax/AjaxWarcExploder.java
@@ -0,0 +1,222 @@
+/*
+ * $Id: AjaxWarcExploder.java 39864 2015-02-18 09:10:24Z thib_gc $
+ */
+
+/*
+
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+package org.lockss.plugin.ajax;
+
+import org.archive.io.ArchiveReader;
+import org.archive.io.ArchiveRecord;
+import org.archive.io.ArchiveRecordHeader;
+import org.archive.util.ArchiveUtils;
+import org.lockss.config.Configuration;
+import org.lockss.config.CurrentConfig;
+import org.lockss.crawler.WarcExploder;
+import org.lockss.daemon.ArchiveEntry;
+import org.lockss.daemon.ConfigParamDescr;
+import org.lockss.daemon.Crawler.CrawlerFacade;
+import org.lockss.daemon.LockssWatchdog;
+import org.lockss.daemon.PluginException;
+import org.lockss.plugin.*;
+import org.lockss.plugin.base.BaseExploderHelper;
+import org.lockss.util.*;
+import org.lockss.util.urlconn.CacheException;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Iterator;
+
+/**
+ * A modified version of the warc exploder which does not assume that the
+ * warc contains a complete au and will therefore not terminate the  crawl.
+ */
+public class AjaxWarcExploder extends WarcExploder {
+  private static Logger logger = Logger.getLogger("AjaxWarcExploder");
+
+  /**
+   * Constructor
+   * @param toExplode  url data of for the archive to explode
+   * @param crawlFacade  facade for crawler performing crawl
+   * @param helper helper for exploding archive
+   */
+  public AjaxWarcExploder(final FetchedUrlData toExplode,
+                          final CrawlerFacade crawlFacade,
+                          final ExploderHelper helper) {
+    super(toExplode, crawlFacade, helper);
+  }
+
+  /**
+   * Explode the archive into its constituent elements
+   */
+  public void explode() throws CacheException {
+    int goodEntries = 0;
+    int badEntries = 0;
+    int entriesBetweenSleep = 0;
+    ArchiveReader arcReader = null;
+
+    logger.info((storeArchive ? "Storing" : "Fetching") + " WARC file: " +
+                origUrl + " will explode");
+    try {
+      // Wrap it in an ArchiveReader
+      logger.debug3("About to wrap stream");
+      arcReader = wrapStream(fetchUrl, arcStream);
+      logger.debug3("wrapStream() returns " +
+                    (arcReader == null ? "null" : "non-null"));
+      // Explode it
+      if (arcReader == null) {
+        throw new CacheException.ExploderException("no WarcReader for " +
+                                                   origUrl);
+      }
+      ArchivalUnit au = crawlFacade.getAu();
+      logger.debug("Exploding " + fetchUrl);
+      // Iterate through the elements in the WARC file, except the first
+      Iterator<ArchiveRecord>  iter = arcReader.iterator();
+      // Skip first record
+      if(iter.hasNext()) iter.next();
+      while(iter.hasNext()) {
+        helper.pokeWDog();
+        // check need to pause
+        handlePause(++entriesBetweenSleep);
+        // handle each element in the archive
+        ArchiveRecord element = iter.next();
+        // Each element is a URL to be cached in our AU
+        ArchiveRecordHeader elementHeader = element.getHeader();
+        String elementUrl = elementHeader.getUrl();
+        String elementMimeType = elementHeader.getMimetype();
+        long elementLength = elementHeader.getLength();
+        long elementDate;
+        try {
+          elementDate = ArchiveUtils.parse14DigitDate(elementHeader.getDate())
+                                  .getTime();
+        }
+        catch (ParseException e) {
+          elementDate = 0;
+        }
+        logger.debug2("WARC url " + elementUrl + " mime " + elementMimeType);
+        // add check to determine if this is a url which should be cached
+        if (au.shouldBeCached(elementUrl) && elementUrl.startsWith("http:")) {
+          ArchiveEntry ae =
+            new ArchiveEntry(elementUrl,
+                             elementLength,
+                             elementDate,
+                             element, // ArchiveRecord extends InputStream
+                             this,
+                             fetchUrl);
+          ae.setHeaderFields(makeCIProperties(elementHeader));
+          long bytesStored = elementLength;
+          logger.debug3("ArchiveEntry: " + ae.getName() + " bytes " +
+                        bytesStored);
+          try {
+            helper.process(ae);
+          }
+          catch (PluginException ex) {
+            throw new CacheException.ExploderException("helper.process() threw",
+                                                       ex);
+          }
+          if (ae.getBaseUrl() != null) {
+            if (ae.getRestOfUrl() != null &&
+                ae.getHeaderFields() != null) {
+              storeEntry(ae);
+              handleAddText(ae);
+              goodEntries++;
+//              crawlFacade.addToParseQueue();
+              crawlFacade.getCrawlerStatus()
+                         .addContentBytesFetched(bytesStored);
+            }
+          }
+          else {
+            badEntries++;
+            logger.debug2("Can't map " + elementUrl + " from " + archiveUrl);
+          }
+        }
+      }
+    }
+    catch (IOException ex) {
+      throw new CacheException.ExploderException(ex);
+    }
+    finally {
+      if (arcReader != null) {
+        try {
+          arcReader.close();
+        }
+        catch (IOException ex) {
+          throw new CacheException.ExploderException(ex);
+        }
+      }
+      IOUtil.safeClose(arcStream);
+    }
+    // report failed fetches
+    if (badEntries != 0) {
+      String msg = archiveUrl + ": " + badEntries + "/" +
+                   goodEntries + " bad entries";
+      throw new CacheException.UnretryableException(msg);
+    }
+  }
+
+  public class AjaxExploderHelper extends BaseExploderHelper {
+    private CrawlerFacade crawlFacade;
+    protected LockssWatchdog wdog;
+
+    public AjaxExploderHelper(CrawlerFacade crawlFacade) {
+      this.crawlFacade = crawlFacade;
+    }
+
+    public void process(ArchiveEntry ae) {
+      // By default the files have to go in the crawler's AU
+      ArchivalUnit au = crawlFacade.getAu();
+      // By default the path should start at the AU's base url.
+      Configuration config = au.getConfiguration();
+      String url = config.get(ConfigParamDescr.BASE_URL.getKey());
+      ae.setBaseUrl(url);
+      ae.setRestOfUrl(ae.getName());
+      CIProperties cip = new CIProperties();
+
+      ae.setHeaderFields(cip);
+    }
+  }
+
+  private void handlePause(int entriesBetweenSleep) {
+    if ((entriesBetweenSleep % sleepAfter) == 0) {
+      long pauseTime =
+        CurrentConfig.getTimeIntervalParam(PARAM_RETRY_PAUSE,
+                                           DEFAULT_RETRY_PAUSE);
+      Deadline pause = Deadline.in(pauseTime);
+      logger.debug3("Sleeping for " +
+                    StringUtil.timeIntervalToString(pauseTime));
+      while (!pause.expired()) {
+        try {
+          pause.sleep();
+        }
+        catch (InterruptedException ie) {
+          // no action
+        }
+      }
+    }
+  }
+}
