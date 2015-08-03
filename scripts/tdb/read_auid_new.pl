b5f74e00c61bbd2a64b3b0c92d8a35dfd57cdcd2
mellen22 <maryellen@lockss.org>
2015-08-03 22:51:33 +0000

    Add plugin.
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@43372 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -1092,6 +1092,31 @@ while (my $line = <>) {
     }
     sleep(4);
                 
+  } elsif ($plugin eq "ClockssRSC2014Plugin") {
+    $url = sprintf("%sen/journals/lockss?journalcode=%s&amp;volume=%s&amp;year=%d", 
+      $param{base_url}, $param{journal_code}, $param{volume_name}, $param{year});
+    $man_url = uri_unescape($url);
+    my $req = HTTP::Request->new(GET, $man_url);
+    my $resp = $ua->request($req);
+    if ($resp->is_success) {
+      my $man_contents = $resp->content;
+      if (defined($man_contents) && ($man_contents =~ m/$clockss_tag/)) {
+        if ($man_contents =~ m/<title>\s*(.*)\s*<\/title>/si) {
+          $vol_title = $1;
+          $vol_title =~ s/\s*\n\s*/ /g;
+          if (($vol_title =~ m/</) || ($vol_title =~ m/>/)) {
+            $vol_title = "\"" . $vol_title . "\"";
+          }
+        } 
+        $result = "Manifest"
+      } else {
+        $result = "--NO_TAG--"
+      }
+    } else {
+      $result = "--REQ_FAIL--"
+    }
+    sleep(4);
+                
   } elsif ($plugin eq "ClockssCopernicusPublicationsPlugin") {
     $url = sprintf("%s%s/index.html", 
       $param{base_url}, $param{volume_name});
