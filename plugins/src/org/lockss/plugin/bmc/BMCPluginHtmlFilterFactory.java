0b8754b0ab0647f61ccafe99436640f20f4b4546
etenbrink <etenbrink@yahoo.com>
2015-08-20 23:32:49 +0000

    Reorder & align
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@43713 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/src/org/lockss/plugin/bmc/BMCPluginHtmlFilterFactory.java
+++ plugins/src/org/lockss/plugin/bmc/BMCPluginHtmlFilterFactory.java
@@ -4,31 +4,31 @@
 
 /*
 
- Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
- all rights reserved.
+Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
- IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
- Except as contained in this notice, the name of Stanford University shall not
- be used in advertising or otherwise to promote the sale, use or other dealings
- in this Software without prior written authorization from Stanford University.
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
 
- */
+*/
 
 package org.lockss.plugin.bmc;
 
@@ -44,8 +44,10 @@ import org.lockss.util.ReaderInputStream;
 
 public class BMCPluginHtmlFilterFactory implements FilterFactory {
 
-  public InputStream createFilteredInputStream(ArchivalUnit au, InputStream in,
-      String encoding) throws PluginException {
+  public InputStream createFilteredInputStream(ArchivalUnit au,
+                                               InputStream in,
+                                               String encoding)
+      throws PluginException {
     NodeFilter[] filters = new NodeFilter[] {
         // Contains variable code
         HtmlNodeFilters.tag("script"),
@@ -71,18 +73,18 @@ public class BMCPluginHtmlFilterFactory implements FilterFactory {
         HtmlNodeFilters.tagWithAttribute("a", "name"),
         // Links to one-time names inside the page
         HtmlNodeFilters.tagWithAttributeRegex("a", "href", "^#"),
+        // Institution-dependent greeting
+        HtmlNodeFilters.tagWithAttribute("li", "class", "greeting"),
+        
         // Contains the menu  <ul class="primary-nav">
         HtmlNodeFilters.tagWithAttribute("ul", "class", "primary-nav"),
-        // Contains advertising
-        HtmlNodeFilters.tagWithAttribute("dl", "class", "google-ad"),
-        // Contains advertising  <dl class="google-ad wide ">
-        HtmlNodeFilters.tagWithAttribute("dl", "class", "google-ad wide "),
+        // remove footer
         //Contains the terms and conditions,copyright year & links to springer
         HtmlNodeFilters.tagWithAttribute("div", "id", "footer"),
         // Contains university name: <ul id="login"
-        HtmlNodeFilters.tagWithAttribute("ul", "id", "login"),        
-        // Contains university name
-        HtmlNodeFilters.tagWithAttribute("li", "class", "greeting"),
+        HtmlNodeFilters.tagWithAttribute("ul", "id", "login"),
+        // Contains advertising
+        HtmlNodeFilters.tagWithAttributeRegex("dl", "class", "google-ad"),
         // Social networking links (have counters)
         HtmlNodeFilters.tagWithAttribute("ul", "id", "social-networking-links"),
         // A usage counter/glif that gets updated over time
@@ -92,36 +94,37 @@ public class BMCPluginHtmlFilterFactory implements FilterFactory {
         // Contains adverstising <a class="skyscraper-ad" 
         HtmlNodeFilters.tagWithAttribute("a", "class", "skyscraper-ad"),
         // An open access link/glyph that may get added
-        HtmlNodeFilters.tagWithAttributeRegex("a", "href", ".*/about/access"),
+        HtmlNodeFilters.tagWithAttributeRegex("a", "href", "/about/access"),
         // A highly accessed link/glyph that may get added
-        HtmlNodeFilters.tagWithAttributeRegex("a", "href", ".*/about/mostviewed"),
+        HtmlNodeFilters.tagWithAttributeRegex("a", "href", "/about/mostviewed"),
         // Institution-dependent image
-        HtmlNodeFilters.tagWithAttributeRegex("img", "src", "^/sfx_links\\?.*"),
+        HtmlNodeFilters.tagWithAttributeRegex("img", "src", "^/sfx_links\\?"),
         // Institution-dependent link resolvers  v2 - added
-        HtmlNodeFilters.tagWithAttributeRegex("a", "href", "^/sfx_links\\?.*"),
+        HtmlNodeFilters.tagWithAttributeRegex("a", "href", "^/sfx_links\\?"),
         // Institution-dependent link resolvers   v1
-        HtmlNodeFilters.tagWithAttributeRegex("a", "href", "^/sfx_links\\.asp.*"),
+        HtmlNodeFilters.tagWithAttributeRegex("a", "href", "^/sfx_links\\.asp"),
         // Springer branding below the footer
         HtmlNodeFilters.tagWithAttribute("div", "class", "springer"),
+        
         // The text of this link changed from "About this article" to "Article metrics"
         HtmlNodeFilters.tagWithAttributeRegex("a", "href", "/about$"),
         // removes mathml inline wierdnesses
         HtmlNodeFilters.tagWithAttribute("p", "class", "inlinenumber"),
-        HtmlNodeFilters.tagWithAttributeRegex("div", "style", ".*display:inline$"),
+        HtmlNodeFilters.tagWithAttributeRegex("div", "style", "display:inline$"),
         HtmlNodeFilters.tagWithAttribute("span", "class", "mathjax"),
         HtmlNodeFilters.tagWithAttribute("span", "class", "inline-math"),
         HtmlNodeFilters.tagWithAttribute("span", "class", "inlinenumber"),
         
         // floating bottom banner announcing access to beta version of new site
         HtmlNodeFilters.tagWithAttributeRegex("div", "class",  "^banner-footer"),
-
+        
     };
+    
     InputStream filtered =  new HtmlFilterInputStream(in, encoding, 
         HtmlNodeFilterTransform.exclude(new OrFilter(filters)));
-    // added whitespace filter
     Reader filteredReader = FilterUtil.getReader(filtered, encoding);
+    // added whitespace filter
     return new ReaderInputStream(new WhiteSpaceFilter(filteredReader));
-
   }
 
 }
