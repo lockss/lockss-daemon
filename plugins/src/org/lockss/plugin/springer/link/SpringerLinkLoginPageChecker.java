3621907205afc63fd268a36b9ec778a638f7ede0
wendellwilson <wendell.wilson@gmail.com>
2015-08-26 17:39:27 +0000

    Login page checker
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@43797 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/springer/link/SpringerLinkLoginPageChecker.java
@@ -0,0 +1,65 @@
+/*
+
+Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.springer.link;
+
+import java.io.*;
+import java.util.Properties;
+
+import org.lockss.daemon.*;
+import org.lockss.util.*;
+
+public class SpringerLinkLoginPageChecker implements LoginPageChecker {
+
+  /**
+   * <p>A recognizable string found on login pages.</p>
+   */
+  protected static final String LOGIN_STRINGS[] = new String[] {
+      "class=\"document no-access\"",
+      "<a class=\"access-link\" href=\"/accesspage/"
+  };
+  
+  @Override
+  public boolean isLoginPage(Properties props,
+                             Reader reader)
+      throws IOException,
+             PluginException {
+    String contentType =
+       HeaderUtil.getMimeTypeFromContentType(props.getProperty("Content-Type"));
+    if ("text/html".equalsIgnoreCase(contentType)) {
+      String fromReader = StringUtil.fromReader(reader);
+      for (String loginString : LOGIN_STRINGS) {
+        if (fromReader.contains(loginString)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+}
