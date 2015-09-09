079409744debd00d045dfe9aa30f420c687cb3e1
etenbrink <etenbrink@yahoo.com>
2015-09-09 20:54:45 +0000

    Add declaration of artTag & registerTag
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@43999 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- plugins/test/src/org/lockss/plugin/highwire/bmj/TestBMJDrupalHtmlHashFilterFactory.java
+++ plugins/test/src/org/lockss/plugin/highwire/bmj/TestBMJDrupalHtmlHashFilterFactory.java
@@ -4,7 +4,7 @@
 
 /*
 
- Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+ Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
  all rights reserved.
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
