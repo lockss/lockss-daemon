dd21b7625efce1207ff3f472d6df9c11873af07c
alexandraohlson <alexandra@slgroup.com>
2015-09-14 19:47:50 +0000

    Add a login page checker in addition to the au_redirect_to_login_url_pattern to handle those cases where a PDF url instead leads to an html page with a subscribe/download button
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@44063 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/medknow/MedknowLoginPageChecker.java
@@ -0,0 +1,66 @@
+/*
+ * $Id:$
+ */
+
+/*
+
+Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.medknow;
+
+import java.io.*;
+import java.util.Properties;
+
+import org.lockss.daemon.*;
+import org.lockss.util.*;
+
+/**
+ * <p>Detects login pages in Medknow journals.
+ * The plugin also uses an au_redirect_to_login_url_pattern of base_url/subscriberlogin.asp
+ * but some journals don't redirect, instead providing a download page using the PDF url  
+ * </p>
+ * @author Alexandra R Ohlson
+ */
+public class MedknowLoginPageChecker implements LoginPageChecker {
+
+  /**
+   * <p>When the pdf url returns a page allowing for download, the title of 
+   * the document is the title of the article with ": Download PDF" at the end.
+   */
+  protected static final String DOWNLOAD_TITLE_END = "Download PDF</title>";
+  
+  public boolean isLoginPage(Properties props,
+                             Reader reader)
+      throws IOException,
+             PluginException {
+    if ("text/html".equalsIgnoreCase(HeaderUtil.getMimeTypeFromContentType(props.getProperty("Content-Type")))) {
+      return StringUtil.containsString(reader, DOWNLOAD_TITLE_END);
+    }
+    return false;
+  }
+
+}
