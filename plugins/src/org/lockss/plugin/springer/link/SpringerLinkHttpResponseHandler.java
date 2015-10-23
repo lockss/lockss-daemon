3c228e9850521e427f3229af4c8f12d3ee2a47fa
wendellwilson <wendell.wilson@gmail.com>
2015-10-23 17:16:50 +0000

    Retry bad links
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@44646 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/springer/link/SpringerLinkHttpResponseHandler.java
@@ -0,0 +1,78 @@
+/*
+ * $Id:$
+ */
+
+/*
+
+Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.springer.link;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.util.Logger;
+import org.lockss.util.urlconn.*;
+
+public class SpringerLinkHttpResponseHandler implements CacheResultHandler {
+
+  private static final Logger logger = Logger.getLogger(SpringerLinkHttpResponseHandler.class);
+
+  @Override
+  public void init(CacheResultMap crmap) {
+    logger.warning("Unexpected call to init()");
+    throw new UnsupportedOperationException("Unexpected call to HighWirePressHttpResponseHandler.init()");
+  }
+
+  @Override
+  public CacheException handleResult(ArchivalUnit au,
+                                     String url,
+                                     int responseCode)
+      throws PluginException {
+    logger.debug2(url);
+    switch (responseCode) {
+      case 404:
+        logger.debug3("404");
+        return new CacheException.RetryDeadLinkException("404 Not Found");
+      case 500:
+        logger.debug2("500");
+        return new CacheException.RetrySameUrlException("500 Internal Server Error");
+      default:
+        logger.warning("Unexpected responseCode (" + responseCode + ") in handleResult(): AU " + au.getName() + "; URL " + url);
+        throw new UnsupportedOperationException("Unexpected responseCode (" + responseCode + ")");
+    }
+  }
+
+  @Override
+  public CacheException handleResult(ArchivalUnit au,
+                                     String url,
+                                     Exception ex)
+      throws PluginException {
+    logger.warning("Unexpected call to handleResult(): AU " + au.getName() + "; URL " + url, ex);
+    throw new UnsupportedOperationException("Unexpected call to handleResult(): AU " + au.getName() + "; URL " + url, ex);
+  }
+
+}
