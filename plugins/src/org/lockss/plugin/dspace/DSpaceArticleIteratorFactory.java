872e1265d74c5305637c3118804dc94af58631bb
wendellwilson <wendell.wilson@gmail.com>
2015-11-18 18:33:59 +0000

    DSpace metadata extraction
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@44935 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/dspace/DSpaceArticleIteratorFactory.java
@@ -0,0 +1,98 @@
+/*
+ * $Id: DryadArticleIteratorFactory.java 39864 2015-02-18 09:10:24Z thib_gc $
+ */
+
+/*
+
+Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.dspace;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
+import org.lockss.daemon.*;
+import org.lockss.extractor.ArticleMetadataExtractor;
+import org.lockss.extractor.ArticleMetadataExtractorFactory;
+import org.lockss.extractor.BaseArticleMetadataExtractor;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.*;
+import org.lockss.util.Logger;
+
+public class DSpaceArticleIteratorFactory
+implements ArticleIteratorFactory,
+           ArticleMetadataExtractorFactory {
+
+  protected static Logger log = 
+      Logger.getLogger(DSpaceArticleIteratorFactory.class);
+
+  // params from tdb file corresponding to AU
+  protected static final String ROOT_TEMPLATE = "\"%s\", base_url"; 
+
+  protected static final String PATTERN_TEMPLATE =
+      "\"^%s(xmlui/)?(bitstream|handle)/([0-9]+/[0-9]+)(.*)\", base_url";
+  
+  @Override
+  public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au, MetadataTarget target) 
+      throws PluginException {
+    SubTreeArticleIteratorBuilder builder = new SubTreeArticleIteratorBuilder(au);
+
+    final Pattern CONTENT_PATTERN = Pattern.compile(
+       "bitstream/([0-9]+/[0-9]+)(/.*\\.pdf)$",
+       Pattern.CASE_INSENSITIVE);
+    
+    final String ABSTRACT_REPLACEMENT = "handle/$1";
+    final String CONTENT_REPLACEMENT = "bitstream/$1$2";
+
+    builder.setSpec(target,
+        ROOT_TEMPLATE, PATTERN_TEMPLATE, Pattern.CASE_INSENSITIVE);
+    
+    builder.addAspect(
+    		CONTENT_PATTERN,
+            CONTENT_REPLACEMENT,
+            ArticleFiles.ROLE_FULL_TEXT_PDF);
+
+    builder.addAspect(
+        ABSTRACT_REPLACEMENT,
+        ArticleFiles.ROLE_ABSTRACT, 
+        ArticleFiles.ROLE_ARTICLE_METADATA);
+
+    builder.setFullTextFromRoles(
+        ArticleFiles.ROLE_FULL_TEXT_PDF,
+        ArticleFiles.ROLE_ABSTRACT);
+
+    return builder.getSubTreeArticleIterator();
+  }
+  
+  @Override
+  public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target)
+    throws PluginException {
+    return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
+  }
+
+}
