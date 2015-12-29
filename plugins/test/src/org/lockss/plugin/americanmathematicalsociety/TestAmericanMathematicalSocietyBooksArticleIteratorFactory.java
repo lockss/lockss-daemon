a0b35d2417742e2b8eff85d4957da51cf2d466c8
etenbrink <etenbrink@yahoo.com>
2015-12-29 20:12:21 +0000

    Initial commit tests
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@45454 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/test/src/org/lockss/plugin/americanmathematicalsociety/TestAmericanMathematicalSocietyBooksArticleIteratorFactory.java
@@ -0,0 +1,211 @@
+/*
+ * $Id: TestAmericanMathematicalSocietyArticleIteratorFactory.java 39864 2015-02-18 09:10:24Z thib_gc $
+ */
+
+/*
+
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.americanmathematicalsociety;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
+import org.lockss.config.*;
+import org.lockss.daemon.*;
+import org.lockss.extractor.*;
+import org.lockss.plugin.*;
+import org.lockss.plugin.simulated.*;
+import org.lockss.state.NodeManager;
+import org.lockss.test.*;
+import org.lockss.util.ListUtil;
+
+public class TestAmericanMathematicalSocietyBooksArticleIteratorFactory extends ArticleIteratorTestCase {
+  
+  private SimulatedArchivalUnit sau;	// Simulated AU to generate content
+  
+  private final String PLUGIN_NAME =
+      "org.lockss.plugin.americanmathematicalsociety.ClockssAmericanMathematicalSocietyBooksPlugin";
+  static final String BASE_URL_KEY = ConfigParamDescr.BASE_URL.getKey();
+  static final String JOURNAL_ID_KEY = ConfigParamDescr.JOURNAL_ID.getKey();
+  static final String YEAR_KEY = ConfigParamDescr.YEAR.getKey();
+  private final String BASE_URL = "http://www.ams.org/";
+  private final String JOURNAL_ID = "jid";
+  private final String YEAR = "2008";
+  private final Configuration AU_CONFIG = ConfigurationUtil.fromArgs(
+      BASE_URL_KEY, BASE_URL,
+      JOURNAL_ID_KEY, JOURNAL_ID,
+      YEAR_KEY, YEAR);
+  private static final int DEFAULT_FILESIZE = 3000;
+  
+  protected String cuRole = null;
+  ArticleMetadataExtractor.Emitter emitter;
+  protected boolean emitDefaultIfNone = false;
+  FileMetadataExtractor me = null;
+  MetadataTarget target;
+  
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    String tempDirPath = setUpDiskSpace();
+    
+    au = createAu();
+    sau = PluginTestUtil.createAndStartSimAu(simAuConfig(tempDirPath));
+  }
+  
+  @Override
+  public void tearDown() throws Exception {
+    sau.deleteContentTree();
+    super.tearDown();
+  }
+  
+  protected ArchivalUnit createAu() throws ArchivalUnit.ConfigurationException {
+    return
+        PluginTestUtil.createAndStartAu(PLUGIN_NAME, AU_CONFIG);
+  }
+  
+  Configuration simAuConfig(String rootPath) {
+    Configuration conf = ConfigManager.newConfiguration();
+    conf.put("root", rootPath);
+    conf.put(BASE_URL_KEY, BASE_URL);
+    conf.put(JOURNAL_ID_KEY, "jid");
+    conf.put(YEAR_KEY, "2008");
+    conf.put("depth", "1");
+    conf.put("branch", "0");
+    conf.put("numFiles", "4");
+    conf.put("fileTypes",
+        "" + (SimulatedContentGenerator.FILE_TYPE_HTML |
+            SimulatedContentGenerator.FILE_TYPE_PDF));
+    conf.put("binFileSize", ""+DEFAULT_FILESIZE);
+    return conf;
+  }
+  
+  public void testRoots() throws Exception {
+    SubTreeArticleIterator artIter = createSubTreeIter();
+    assertEquals(ListUtil.list(BASE_URL + "books/jid/"),
+        getRootUrls(artIter));
+  }
+  
+  //
+  // We are set up to match any of <journal_id>/<year>/p.*
+  //
+  
+  public void testUrls() throws Exception {
+    SubTreeArticleIterator artIter = createSubTreeIter();
+    Pattern pat = getPattern(artIter);
+    
+    // we match to "^%sbooks/(%s)/([0-9]{2,5})(/\\1\\2\\.pdf)?$", base_url, journal_i
+    assertMatchesRE(pat, "http://www.ams.org/books/jid/200");
+    assertMatchesRE(pat, "http://www.ams.org/books/jid/200/jid200.pdf");
+    
+    // but not to pdf or image http://www.ams.org/books/conm/629/conm629-endmatter.pdf
+    assertNotMatchesRE(pat, "http://www.ams.org/jid/200/jid200-endmatter.pdf");
+    assertNotMatchesRE(pat, "http://www.ams.org/images/remote-access-icon.png");
+    
+    // wrong base url
+    assertNotMatchesRE(pat, "http://ametsoc.org/jid/2008/3-1/p01.xhtml");
+  }
+  
+  //
+  // simAU was created with only one depth
+  // 2 filetypes (html & pdf) and 4 files of each type
+  // So the total number of files of all types is 8 (4*2)
+  // simAU file structures looks like this 01file.html or 08file.pdf, etc
+  //
+  public void testCreateArticleFiles() throws Exception {
+    PluginTestUtil.crawlSimAu(sau);
+    
+    /*
+     *  Go through the simulated content you just crawled and modify the results to emulate
+     *  what you would find in a "real" crawl with AmericanMathematicalSociety:
+     *  <base_url>/books/<journal_id>/<year>-.*
+     */
+    
+    String pat1 = "(\\d+)file[.]html";
+    // turn xxfile.xhtml into abstracts
+    String repHtml = "books/" + JOURNAL_ID + "/" + "$1";
+    PluginTestUtil.copyAu(sau, au, ".*[.]html$", pat1, repHtml);
+    // turn xxfile.pdf into fulltext pdfs
+    String pat2 = "(\\d+)file[.]pdf";
+    String repPdf = "books/" + JOURNAL_ID + "/" +
+        "$1/" + JOURNAL_ID + "$1.pdf";
+    PluginTestUtil.copyAu(sau, au, ".*[.]pdf$", pat2, repPdf);
+    
+    // At this point we have 10 sets of 2 types of articles
+    // Remove some of the URLs just created to make test more robust
+    
+    int deleted = 0;
+    for (CachedUrl cu : AuUtil.getCuIterable(au)) {
+        String url = cu.getUrl();
+        if (url.contains("003.pdf")) {
+          deleteBlock(cu);
+          ++deleted;
+        }
+    }
+    assertEquals(1, deleted);
+    
+    Iterator<ArticleFiles> it = au.getArticleIterator(MetadataTarget.Any());
+    int count = 0;
+    int countFullTextPdf= 0;
+    int countMetadata = 0;
+    while (it.hasNext()) {
+      ArticleFiles af = it.next();
+      count ++;
+      //log.info(af.toString());
+      CachedUrl cu = af.getRoleCu(ArticleFiles.ROLE_FULL_TEXT_PDF);
+      if ( cu != null) {
+        ++countFullTextPdf;
+      }
+      cu = af.getRoleCu(ArticleFiles.ROLE_ARTICLE_METADATA);
+      if (cu != null) {
+        ++countMetadata;
+      }
+    }
+    // potential article count is 4
+    // subtract the one where we removed pdf
+    int expCount = 4;
+    
+    log.debug3("Article count is " + count);
+    assertEquals(expCount, count);
+    
+    // you will get a full text for ALL articles
+    assertEquals(expCount-1, countFullTextPdf);
+    
+    // you will get metadata for ALL articles
+    assertEquals(expCount, countMetadata);
+  }
+  
+  private void deleteBlock(CachedUrl cu) throws IOException {
+    //log.info("deleting " + cu.getUrl());
+    CachedUrlSetSpec cuss = new SingleNodeCachedUrlSetSpec(cu.getUrl());
+    ArchivalUnit au = cu.getArchivalUnit();
+    CachedUrlSet cus = au.makeCachedUrlSet(cuss);
+    NodeManager nm = au.getPlugin().getDaemon().getNodeManager(au);
+    nm.deleteNode(cus);
+  }
+}
---
+++ plugins/test/src/org/lockss/plugin/americanmathematicalsociety/TestAmericanMathematicalSocietyBooksPlugin.java
@@ -0,0 +1,213 @@
+/*
+ * $Id: TestAmericanMathematicalSocietyPlugin.java 39864 2015-02-18 09:10:24Z thib_gc $
+ */
+
+/*
+
+Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.americanmathematicalsociety;
+
+import java.net.*;
+import java.util.*;
+
+import org.lockss.test.*;
+import org.lockss.util.ListUtil;
+import org.lockss.plugin.*;
+import org.lockss.config.Configuration;
+import org.lockss.daemon.*;
+import org.lockss.extractor.*;
+import org.lockss.plugin.ArchivalUnit.*;
+import org.lockss.plugin.definable.*;
+import org.lockss.plugin.wrapper.WrapperUtil;
+
+public class TestAmericanMathematicalSocietyBooksPlugin extends LockssTestCase {
+  static final String BASE_URL_KEY = ConfigParamDescr.BASE_URL.getKey();
+  static final String JOURNAL_ID_KEY = ConfigParamDescr.JOURNAL_ID.getKey();
+  static final String YEAR_KEY = ConfigParamDescr.YEAR.getKey();
+  
+  private MockLockssDaemon theDaemon;
+  private DefinablePlugin plugin;
+  
+  public TestAmericanMathematicalSocietyBooksPlugin(String msg) {
+    super(msg);
+  }
+  
+  public void setUp() throws Exception {
+    super.setUp();
+    setUpDiskSpace();
+    theDaemon = getMockLockssDaemon();
+    plugin = new DefinablePlugin();
+    plugin.initPlugin(theDaemon,
+        "org.lockss.plugin.americanmathematicalsociety." +
+        "ClockssAmericanMathematicalSocietyBooksPlugin");
+  }
+  
+  public void testGetAuNullConfig()
+      throws ArchivalUnit.ConfigurationException {
+    try {
+      plugin.configureAu(null, null);
+      fail("Didn't throw ArchivalUnit.ConfigurationException");
+    } catch (ArchivalUnit.ConfigurationException e) {
+    }
+  }
+  
+  public void testCreateAu() {
+    Properties props = new Properties();
+    props.setProperty(BASE_URL_KEY, "http://www.example.com/");
+    props.setProperty(JOURNAL_ID_KEY, "j_id");
+    props.setProperty(YEAR_KEY, "2004");
+    DefinableArchivalUnit au = null;
+    try {
+      au = makeAuFromProps(props);
+    }
+    catch (ConfigurationException ex) {
+    }
+    au.getName();
+  }
+  
+  private DefinableArchivalUnit makeAuFromProps(Properties props)
+      throws ArchivalUnit.ConfigurationException {
+    Configuration config = ConfigurationUtil.fromProps(props);
+    return (DefinableArchivalUnit)plugin.configureAu(config, null);
+  }
+  
+  public void testGetAuHandlesBadUrl()
+      throws ArchivalUnit.ConfigurationException, MalformedURLException {
+    Properties props = new Properties();
+    props.setProperty(BASE_URL_KEY, "blah");
+    props.setProperty(JOURNAL_ID_KEY, "jams");
+    props.setProperty(YEAR_KEY, "2001");
+    
+    try {
+      makeAuFromProps(props);
+      fail ("Didn't throw InstantiationException when given a bad url");
+    } catch (ArchivalUnit.ConfigurationException auie) {
+      assertNotNull(auie.getCause());
+    }
+  }
+  
+  public void testGetAuConstructsProperAu()
+      throws ArchivalUnit.ConfigurationException, MalformedURLException {
+    Properties props = new Properties();
+    props.setProperty(BASE_URL_KEY, "http://www.example.com/");
+    props.setProperty(JOURNAL_ID_KEY, "j_id");
+    props.setProperty(YEAR_KEY, "2004");
+    
+    DefinableArchivalUnit au = makeAuFromProps(props);
+    assertEquals("American Mathematical Society Books Plugin (CLOCKSS), " +
+        "Base URL http://www.example.com/, " +
+        "Book ID j_id, Year 2004", au.getName());
+  }
+  
+  public void testGetPluginId() {
+    assertEquals("org.lockss.plugin.americanmathematicalsociety." +
+        "ClockssAmericanMathematicalSocietyBooksPlugin",
+        plugin.getPluginId());
+  }
+  
+  public void testGetAuConfigProperties() {
+    assertEquals(ListUtil.list(ConfigParamDescr.BASE_URL,
+        ConfigParamDescr.JOURNAL_ID,
+        ConfigParamDescr.YEAR),
+        plugin.getLocalAuConfigDescrs());
+  }
+  
+  public void testGetArticleMetadataExtractor() {
+    Properties props = new Properties();
+    props.setProperty(BASE_URL_KEY, "http://www.example.com/");
+    props.setProperty(JOURNAL_ID_KEY, "asdf");
+    props.setProperty(YEAR_KEY, "2004");
+    DefinableArchivalUnit au = null;
+    try {
+      au = makeAuFromProps(props);
+    }
+    catch (ConfigurationException ex) {
+    }
+    assertTrue(""+plugin.getArticleMetadataExtractor(MetadataTarget.Any(), au),
+        plugin.getArticleMetadataExtractor(null, au) instanceof ArticleMetadataExtractor);
+    assertTrue(""+plugin.getFileMetadataExtractor(MetadataTarget.Any(), "text/html", au),
+        plugin.getFileMetadataExtractor(MetadataTarget.Any(), "text/html", au) instanceof
+        FileMetadataExtractor
+        );
+  }
+  
+  public void testGetHashFilterFactory() {
+    assertNull(plugin.getHashFilterFactory("BogusFilterFactory"));
+    assertNull(plugin.getHashFilterFactory("application/pdf"));
+    assertNotNull(plugin.getHashFilterFactory("text/html"));
+  }
+  public void testGetArticleIteratorFactory() {
+    assertTrue(WrapperUtil.unwrap(plugin.getArticleIteratorFactory())
+        instanceof org.lockss.plugin.americanmathematicalsociety.
+        AmericanMathematicalSocietyBooksArticleIteratorFactory);
+  }
+  
+  // Test the crawl rules for AmericanMathematicalSocietyPlugin
+  public void testShouldCacheProperPages() throws Exception {
+    String ROOT_URL = "http://www.example.com/";
+    Properties props = new Properties();
+    props.setProperty(BASE_URL_KEY, ROOT_URL);
+    props.setProperty(JOURNAL_ID_KEY, "asdf");
+    props.setProperty(YEAR_KEY, "2004");
+    DefinableArchivalUnit au = null;
+    try {
+      au = makeAuFromProps(props);
+    }
+    catch (ConfigurationException ex) {
+    }
+    theDaemon.getLockssRepository(au);
+    
+    // Test for pages that should get crawled
+    // permission page/start url
+    shouldCacheTest(ROOT_URL + "clockssdata?p=asdf", true, au);
+    shouldCacheTest(ROOT_URL + "lockssdata?p=asdf", false, au);
+    shouldCacheTest(ROOT_URL + "books/asdf/year/2004", true, au);
+    
+    // toc page for a book http://www.ams.org/books/conm/630
+    shouldCacheTest(ROOT_URL + "books/asdf/200/", true, au);
+    shouldCacheTest(ROOT_URL + "books/asdf/200", true, au);
+    shouldCacheTest(ROOT_URL + "books/asdf/9", false, au);
+    
+    // chapter files
+    shouldCacheTest(ROOT_URL + "books/asdf/200/123456", true, au);
+    shouldCacheTest(ROOT_URL + "books/asdf/200/asdf200.pdf", true, au);
+    shouldCacheTest(ROOT_URL + "books/asdf/200/asdf2000000.pdf", true, au);
+    shouldCacheTest(ROOT_URL + "books/asdf/200/123456/asdf200-123456.pdf", true, au);
+    
+    // should not get crawled - wrong journal
+    shouldCacheTest(ROOT_URL + "clockssdata/?p=ecgd", false, au);
+    // should not get crawled - LOCKSS
+    shouldCacheTest("http://lockss.stanford.edu", false, au);
+  }
+  
+  private void shouldCacheTest(String url, boolean shouldCache, ArchivalUnit au) {
+    log.info ("shouldCacheTest url: " + url);
+    assertEquals(shouldCache, au.shouldBeCached(url));
+  }
+  
+}
