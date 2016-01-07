ab4e2bf8d37339f025a5300f4d917c24bd68645c
clairegriffin <clairetg@stanford.edu>
2016-01-07 02:24:29 +0000

    - change CacheException.WrongSize to Retryable Exception - bump versions
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@45589 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/silverchair/ScHtmlHttpResponseHandler.java
@@ -0,0 +1,97 @@
+/*
+ *
+ *  * $Id: Template.jav,v 1.2 2005/10/07 23:46:50 clairetg Exp $
+ *
+ *
+ *
+ * Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
+ * all rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of Stanford University shall not
+ * be used in advertising or otherwise to promote the sale, use or other dealings
+ * in this Software without prior written authorization from Stanford University.
+ *
+ * /
+ */
+
+package org.lockss.plugin.silverchair;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.util.Constants;
+import org.lockss.util.Logger;
+
+import org.lockss.util.urlconn.CacheException;
+import org.lockss.util.urlconn.CacheResultHandler;
+import org.lockss.util.urlconn.CacheResultMap;
+
+public class ScHtmlHttpResponseHandler implements CacheResultHandler {
+  private static final Logger logger = Logger.getLogger(ScHtmlHttpResponseHandler.class);
+
+  @Override
+  public void init(final CacheResultMap map) throws PluginException {
+    logger.warning("Unexpected call to init()");
+    throw new UnsupportedOperationException("Unexpected call to ScHttpResponseHandler.init()");
+
+  }
+
+  @Override
+  public CacheException handleResult(final ArchivalUnit au, final String url, final int code) throws PluginException {
+    logger.debug(code + ": " + url);
+    switch (code) {
+      case 405:
+          return new CacheException.RetryableNetworkException_3_10S("405 Method Not Allowed");
+      default:
+        logger.warning("Unexpected responseCode (" + code + ") in handleResult(): AU " + au.getName() + "; URL " + url);
+        throw new UnsupportedOperationException("Unexpected responseCode (" + code + ")");
+    }
+  }
+
+  @Override
+  public CacheException handleResult(final ArchivalUnit au, final String url, final Exception ex)
+    throws PluginException {
+    logger.debug(ex.getMessage() + ": " + url);
+    return new ScRetryableNetworkException(ex);
+  }
+
+  public static class ScRetryableNetworkException extends CacheException.RetryableNetworkException_3 {
+    public ScRetryableNetworkException() {
+      super();
+      attributeBits.clear(ATTRIBUTE_FAIL);
+    }
+
+    public ScRetryableNetworkException(String message) {
+      super(message);
+      attributeBits.clear(ATTRIBUTE_FAIL);
+    }
+
+    /** Create this if details of causal exception are more relevant. */
+    public ScRetryableNetworkException(Exception e) {
+      super(e);
+      attributeBits.clear(ATTRIBUTE_FAIL);
+    }
+
+    public long getRetryDelay() {
+      return 3 * Constants.MINUTE;
+    }
+
+  }
+
+}
