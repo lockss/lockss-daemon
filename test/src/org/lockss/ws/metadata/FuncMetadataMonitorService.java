75b4ef36b811e1c9a1d0807d696040317fa5c52e
fergaloy <fergaloy@stanford.edu>
2016-01-08 21:26:08 +0000

    Roundup issue 5229 (https://roundup.lockss.org/lockss/issue5229): Display metadata items that have no DOI.
    Added a MetadataMonitor function to list non-parent metadata items that have no DOI with enough ancillary information to determine where to look for the root cause.
    
    
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/branches/branch_1-69@45604 4f837ed2-42f5-46e7-a7a5-fa17313484d4

--- test/src/org/lockss/ws/metadata/FuncMetadataMonitorService.java
+++ test/src/org/lockss/ws/metadata/FuncMetadataMonitorService.java
@@ -4,7 +4,7 @@
 
 /*
 
- Copyright (c) 2015 Board of Trustees of Leland Stanford Jr. University,
+ Copyright (c) 2015-2016 Board of Trustees of Leland Stanford Jr. University,
  all rights reserved.
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -235,5 +235,6 @@ public class FuncMetadataMonitorService extends LockssTestCase {
     assertNull(proxy.getAuNamesWithMultiplePublishers());
     assertNull(proxy.getUnnamedItems());
     assertNull(proxy.getPublicationsWithMultiplePids());
+    assertNull(proxy.getNoDoiItems());
   }
 }
