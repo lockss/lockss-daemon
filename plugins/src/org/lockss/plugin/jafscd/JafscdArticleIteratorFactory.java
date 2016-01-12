164f1d6ced4c2fc79d36bd5d27892f3fc7af7f10
wendellwilson <wendell.wilson@gmail.com>
2016-01-12 21:36:49 +0000

    First pass at JAFSCD Plugin
    git-svn-id: svn://svn.code.sf.net/p/lockss/svn/lockss-daemon/trunk@45660 4f837ed2-42f5-46e7-a7a5-fa17313484d4

---
+++ plugins/src/org/lockss/plugin/jafscd/JafscdArticleIteratorFactory.java
@@ -0,0 +1,82 @@
+/*
+ * $Id: JstorArticleIteratorFactory.java 39864 2015-02-18 09:10:24Z thib_gc $
+ */
+
+/*
+
+Copyright (c) 2000-2011 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.jafscd;
+
+import java.util.Iterator;
+import java.util.regex.*;
+
+import org.lockss.daemon.*;
+import org.lockss.extractor.*;
+import org.lockss.plugin.*;
+import org.lockss.util.Logger;
+
+public class JafscdArticleIteratorFactory implements ArticleIteratorFactory, ArticleMetadataExtractorFactory {
+
+  protected static Logger log = Logger.getLogger(JafscdArticleIteratorFactory.class);
+
+  protected static final String ROOT_TEMPLATE = "\"%s\", base_url";
+
+  protected static final String PATTERN_TEMPLATE = "\"^%svolum?e-%s-issue-[0-9]+/[0-9]+[^/]+\\.html\", base_url, volume_name";
+
+  private final Pattern ABSTRACT_PATTERN = Pattern.compile("(volum?e-[0-9]+-issue-[0-9]+/[0-9]+.+\\.html.+)", Pattern.CASE_INSENSITIVE);
+  private final String ABSTRACT_REPLACEMENT = "$1"; //no replacement...just the one
+
+  @Override
+  public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au, MetadataTarget target) throws PluginException {
+    SubTreeArticleIteratorBuilder builder = localBuilderCreator(au);
+
+    builder.setSpec(new SubTreeArticleIterator.Spec()
+    .setTarget(target)
+    .setRootTemplate(ROOT_TEMPLATE)
+    .setPatternTemplate(PATTERN_TEMPLATE, Pattern.CASE_INSENSITIVE));
+
+    builder.addAspect(ABSTRACT_PATTERN,
+        ABSTRACT_REPLACEMENT,
+        ArticleFiles.ROLE_ARTICLE_METADATA);
+
+    return builder.getSubTreeArticleIterator();
+  }
+
+  // Enclose the method that creates the builder to allow a child to do additional processing
+  protected SubTreeArticleIteratorBuilder localBuilderCreator(ArchivalUnit au) { 
+    return new SubTreeArticleIteratorBuilder(au);
+  }
+
+
+  @Override
+  public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target)
+      throws PluginException {
+    return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
+  }
+}
+
