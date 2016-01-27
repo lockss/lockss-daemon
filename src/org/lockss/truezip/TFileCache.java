2ec9f94a9ea37cba22903fe9490d5a36c51da267
Tom Lipkis <tal@pss.xom>
2016-01-26 16:33:09 -0800

    Cosmetic.

--- src/org/lockss/truezip/TFileCache.java
+++ src/org/lockss/truezip/TFileCache.java
@@ -4,7 +4,7 @@
 
 /*
 
-Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2016 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -178,6 +178,7 @@ public class TFileCache {
   void fillTFile(Entry ent, CachedUrl cu) {
     TFile tf = ent.ctf;
     log.debug2("filling " + tf + " from " + cu);
+    // XXXX
     InputStream is = cu.getUnfilteredInputStream();
     try {
       tf.input(is);
