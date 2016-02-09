cc896c89510f950d40563133fc0576a460de6ffa
Mary-Ellen <maryellen@lockss.org>
2016-02-09 14:02:56 -0800

    Add GPOFDSysSitemapsPlugin to manifest page checker.

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -260,6 +260,24 @@ while (my $line = <>) {
   }
         sleep(4);
 
+  } elsif ($plugin eq "GPOFDSysSitemapsPlugin") {
+      $url = sprintf("%ssmap/fdsys/sitemap_%d/%d_%s_sitemap.xml", 
+      $param{base_url}, $param{year}, $param{year}, $param{collection_id});
+      $man_url = uri_unescape($url);
+      my $req = HTTP::Request->new(GET, $man_url);
+      my $resp = $ua->request($req);
+      if ($resp->is_success) {
+          my $man_contents = $resp->content;
+          if (defined($man_contents) && (($man_contents =~ m/$lockss_tag/) || ($man_contents =~ m/$oa_tag/))) {
+              $result = "Manifest"
+          } else {
+              $result = "--"
+          }
+      } else {
+          $result = "--"
+      }
+      sleep(4);
+
   } elsif ($plugin eq "BePressPlugin") {
         $url = sprintf("%s%s/lockss-volume%d.html", 
             $param{base_url}, $param{journal_abbr}, $param{volume});
