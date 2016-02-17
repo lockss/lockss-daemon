98942e29fdccbb962b5027592785ecd4caec9b3a
Alexandra Ohlson <aohlson@lockss.org>
2016-02-16 16:39:36 -0800

    prototype of Bradypus Source plugin

---
+++ plugins/src/org/lockss/plugin/clockss/bradypus/BradypusOnix2XmlMetadataExtractorFactory.java
@@ -0,0 +1,87 @@
+/*
+ * $Id$
+ */
+
+/*
+
+ Copyright (c) 2000-2016 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.clockss.bradypus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lockss.util.*;
+import org.lockss.daemon.*;
+import org.lockss.extractor.*;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.clockss.Onix2BooksSchemaHelper;
+import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
+import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
+
+
+public class BradypusOnix2XmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
+  private static final Logger log = Logger.getLogger(BradypusOnix2XmlMetadataExtractorFactory.class);
+
+  private static SourceXmlSchemaHelper Onix2Helper = null;
+
+  @Override
+  public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
+      String contentType)
+          throws PluginException {
+    return new BradypusOnix2XmlMetadataExtractor();
+  }
+
+  public class BradypusOnix2XmlMetadataExtractor extends SourceXmlMetadataExtractor {
+
+    @Override
+    protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu) {
+      // Once you have it, just keep returning the same one. It won't change.
+      if (Onix2Helper == null) {
+        Onix2Helper = new Onix2BooksSchemaHelper();
+      }
+      return Onix2Helper;
+    }
+
+
+    /* In this case, build up the filename the XML filename with .pdf
+     * Although this plugin could use the RecordReference string - this is easier
+     */
+    @Override
+    protected List<String> getFilenamesAssociatedWithRecord(SourceXmlSchemaHelper helper, CachedUrl cu,
+        ArticleMetadata oneAM) {
+
+  
+      String xmlFilename = cu.getUrl();
+      String filenameValue = xmlFilename.substring(0,xmlFilename.length() - 3) + "pdf";
+      List<String> returnList = new ArrayList<String>();
+      returnList.add(filenameValue);
+      return returnList;
+    }
+
+  }
+}
