36e213c78d7f7e0ebec27176d186fde6f0dc8183
Alexandra Ohlson <aohlson@lockss.org>
2016-02-24 12:45:18 -0800

    allow cover.gif through the crawl filter

--- plugins/src/org/lockss/plugin/atypon/practicalaction/PracticalActionJournalsHtmlCrawlFilterFactory.java
+++ plugins/src/org/lockss/plugin/atypon/practicalaction/PracticalActionJournalsHtmlCrawlFilterFactory.java
@@ -35,6 +35,7 @@ package org.lockss.plugin.atypon.practicalaction;
 import java.io.InputStream;
 
 import org.htmlparser.NodeFilter;
+import org.htmlparser.filters.OrFilter;
 import org.lockss.daemon.PluginException;
 import org.lockss.filter.html.*;
 import org.lockss.plugin.*;
@@ -54,7 +55,12 @@ public class PracticalActionJournalsHtmlCrawlFilterFactory
     // Current Issue, or right sidebar top block of abstract, full text and ref
     // http://www.developmentbookshelf.com/toc/edm/25/1
     // http://www.developmentbookshelf.com/doi/abs/10.3362/1755-1986.2014.004
-    HtmlNodeFilters.tagWithAttributeRegex("div", "class", "body-emphasis"),
+    // but we want to get the cover.gif which is also in the panel
+    //HtmlNodeFilters.tagWithAttributeRegex("div", "class", "body-emphasis"),
+    HtmlNodeFilters.allExceptSubtree(
+        HtmlNodeFilters.tagWithAttributeRegex("div", "class", "body-emphasis"),
+        HtmlNodeFilters.tagWithAttributeRegex(
+            "img", "alt", "Publication Cover")),
     // toc, abs, ref - right column most read/most cited
     HtmlNodeFilters.tagWithAttribute("div", "aria-relevant", "additions"),
     // abs, ref - below <div class="response">, or after the main content
