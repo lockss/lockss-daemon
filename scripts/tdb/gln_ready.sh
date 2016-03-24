bad1cfcede0322084c63916e1a2a476e0e4ac8e3
Mary-Ellen <maryellen@lockss.org>
2016-03-23 17:31:23 -0700

    Updates. Increase number. Sort before comm.

--- scripts/tdb/gln_ready.sh
+++ scripts/tdb/gln_ready.sh
@@ -20,8 +20,8 @@ tpath="/home/$LOGNAME/tmp"
    cat $tpath/gr_gln_m.txt | sed -e 's/HighWirePressH20Plugin/ClockssHighWirePressH20Plugin/' > $tpath/gr_gln_mc.txt
    # Find common items on the clockss list and the clockss-formatted gln list
    comm -12 $tpath/gr_clockss_c.txt $tpath/gr_gln_mc.txt > $tpath/gr_common.txt
-   # Select a random collection of 100 clockss AUids
-   shuf $tpath/gr_common.txt | head -100 > $tpath/gr_common_shuf.txt
+   # Select a random collection of 300 clockss AUids
+   shuf $tpath/gr_common.txt | head -300 > $tpath/gr_common_shuf.txt
 
 
 # Does AU have a clockss and gln manifest page?
@@ -32,9 +32,9 @@ tpath="/home/$LOGNAME/tmp"
    # Look for lockss manifest pages for AUids that have clockss manifest pages.
    ./scripts/tdb/read_auid_new.pl $tpath/gr_common_shuf_g.txt  | grep "*M" | sed -e 's/.*, \(org|lockss|plugin|highwire|[^,]*\), .*/\1/' > $tpath/gr_common_shuf_c.txt
    # Convert the list from gln to clockss
-   cat $tpath/gr_common_shuf_c.txt | sed -e 's/HighWirePressH20Plugin/ClockssHighWirePressH20Plugin/' > $tpath/gr_common_manifest.txt
+   cat $tpath/gr_common_shuf_c.txt | sed -e 's/HighWirePressH20Plugin/ClockssHighWirePressH20Plugin/' | sort > $tpath/gr_common_manifest.txt
    # Find common items on the list of AUs with manifest pages, and the list of healthy AUs on the ingest machines.
-   comm -12 $tpath/gr_ingest_healthy.txt $tpath/gr_man_gln.txt
+   comm -12 $tpath/gr_ingest_healthy.txt $tpath/gr_common_manifest.txt
 
 exit 0
 
