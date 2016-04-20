df8b3ed99b08b597a7975d13ac3a4e1e43401cd3
Tom Lipkis <tal@pss.com>
2016-04-20 01:55:28 -0700

    First iteration of sample plugin, with ContentValidator example.

---
+++ plugins/src/org/lockss/plugin/sample/PdfContentValidator.java
@@ -0,0 +1,87 @@
+/*
+ * $Id$
+ */
+
+/*
+
+Copyright (c) 2016 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.sample;
+
+import java.io.*;
+
+import org.lockss.daemon.*;
+import org.lockss.plugin.*;
+import org.lockss.util.*;
+import org.lockss.util.urlconn.*;
+
+public class PdfContentValidator implements ContentValidator {
+
+  public void validate(CachedUrl cu)
+      throws ContentValidationException, PluginException, IOException {
+    CIProperties headers = cu.getProperties();
+    String some_val = headers.getProperty("some_prop");
+    if (some_val != "some_string") {
+      throw new InvalidReponseHeaderException("msg");
+    }
+    // may also read the content
+    if (isTruncatedPdf(cu.getUnfilteredInputStream())) {
+      throw new TruncatedPdfException("msg");
+    }      
+  }
+
+  boolean isTruncatedPdf(InputStream ins) {
+    return false;
+  }
+
+  public static class Factory implements ContentValidatorFactory {
+    public ContentValidator createContentValidator(ArchivalUnit au,
+						   String contentType) {
+      // content type available but need not be used.
+      return new PdfContentValidator();
+    }
+  }
+
+  public static class InvalidReponseHeaderException
+    extends ContentValidationException {
+
+    public InvalidReponseHeaderException(String msg) {
+      super(msg);
+    }
+  }
+
+  public static class TruncatedPdfException
+    extends ContentValidationException {
+
+    public TruncatedPdfException(String msg) {
+      super(msg);
+    }
+  }
+
+
+}
+    
