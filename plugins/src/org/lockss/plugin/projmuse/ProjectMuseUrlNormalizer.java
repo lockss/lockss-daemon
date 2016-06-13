b5e6900c70087dab99cff2cdb6c831145df8b227
Thib Guicherd-Callin <github@thib.org>
2016-06-13 07:11:41 -0700

    Rework Muse plugins to have knowledge of new start URLs and the mapping from old URLs to new, opaque URLs (to prevent content preserved at old URLs from being orphaned). Allow dat and .dat.gz files to be included in the plugins tree/JAR. Fix a few Muse book AUs that have disappeared/been retracted.

--- plugins/src/org/lockss/plugin/projmuse/ProjectMuseUrlNormalizer.java
+++ plugins/src/org/lockss/plugin/projmuse/ProjectMuseUrlNormalizer.java
@@ -32,20 +32,77 @@ in this Software without prior written authorization from Stanford University.
 
 package org.lockss.plugin.projmuse;
 
+import java.io.*;
+import java.util.*;
+import java.util.zip.GZIPInputStream;
+
 import org.apache.commons.lang3.StringUtils;
 import org.lockss.daemon.PluginException;
 import org.lockss.plugin.*;
+import org.lockss.util.*;
 
 /**
  * @since 1.67.5
  */
 public class ProjectMuseUrlNormalizer extends HttpToHttpsUtil.BaseUrlHttpHttpsUrlNormalizer {
 
+  private static final Logger log = Logger.getLogger(ProjectMuseUrlNormalizer.class);
+  
   protected static final String VERSION_SUFFIX = "?v=";
 
+  protected static Map<String, String> paths;
+  
+  /*
+   * STATIC INITIALIZER
+   */
+  static {
+    InputStream is = null;
+    LineNumberReader lnr = null;
+    try {
+      is = ProjectMuseUrlNormalizer.class.getResourceAsStream("url_normalizer.dat.gz");
+      if (is == null) {
+        throw new ExceptionInInitializerError("Mapping file not found");
+      }
+      Map<String, String> map = new HashMap<String, String>();
+      lnr = new LineNumberReader(new InputStreamReader(new GZIPInputStream(is), Constants.ENCODING_US_ASCII));
+      for (String line = lnr.readLine() ; line != null ; line = lnr.readLine()) {
+        int tab = line.indexOf('\t');
+        if (tab < 0) {
+          throw new IOException("Invalid mapping file format at line " + lnr.getLineNumber());
+        }
+        map.put(line.substring(0, tab), line.substring(tab + 1));
+      }
+      paths = map;
+    }
+    catch (IOException ioe) {
+      throw new ExceptionInInitializerError(ioe);
+    }
+    finally {
+      IOUtil.safeClose(lnr);
+    }
+  }
+  
   @Override
   public String additionalNormalization(String url, ArchivalUnit au) throws PluginException {
+    log.debug3("in: " + url);
+    
     url = StringUtils.substringBeforeLast(url, VERSION_SUFFIX);
+    
+    final String musehost = "://muse.jhu.edu/";
+    int ix = url.indexOf(musehost);
+    if (ix >= 0) {
+      int slash = ix + musehost.length() - 1;
+      String path = url.substring(slash);
+      if (path.endsWith("/")) {
+        path = path.substring(0, path.length() - 1);
+      }
+      String oldPath = paths.get(path);
+      if (oldPath != null) {
+        url = url.substring(0, slash) + oldPath;
+      }
+    }
+
+    log.debug3("out: " + url);
     return url;
   }
 
