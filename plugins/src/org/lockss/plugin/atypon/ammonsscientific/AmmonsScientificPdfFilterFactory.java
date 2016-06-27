80f4a0d30fd320bfac12b6097568fff91d467f0a
Alexandra Ohlson <aohlson@lockss.org>
2016-06-27 16:34:44 -0400

    Make Ammons PDF filter more robust

---
+++ plugins/src/org/lockss/plugin/atypon/ammonsscientific/AmmonsScientificPdfFilterFactory.java
@@ -0,0 +1,59 @@
+/*
+ * $Id$
+ */
+
+/*
+
+ Copyright (c) 2000-2016 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.atypon.ammonsscientific;
+
+import org.lockss.plugin.atypon.BaseAtyponScrapingPdfFilterFactory;
+
+/*
+ * The PDF plus offer a growing list of Cited by items. Remove them
+ * Also remove the information at the front of the PDF file about how it was produced
+ */
+public class AmmonsScientificPdfFilterFactory extends BaseAtyponScrapingPdfFilterFactory {
+    
+
+  // The PDF Producer added in new information - remove it all
+  @Override
+  public boolean doRemoveAllDocumentInfo() {
+    return true;
+  }
+
+  /* 
+   * Turn on removal of "This article cited by:" pages - the default string is correct
+   */
+  @Override
+  public boolean doRemoveCitedByPage() {
+    return true;    
+  }  
+
+  
+}
