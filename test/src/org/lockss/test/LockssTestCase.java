ea295c6cfeb669ae0a82236fc6362503a58d0234
Tom Lipkis <tal@pss.com>
2016-06-30 09:18:54 -0700

    Cosmetic

--- test/src/org/lockss/test/LockssTestCase.java
+++ test/src/org/lockss/test/LockssTestCase.java
@@ -4,7 +4,7 @@
 
 /*
 
-Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2016 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
