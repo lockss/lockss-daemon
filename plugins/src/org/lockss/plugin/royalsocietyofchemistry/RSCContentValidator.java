6b42cb53a3d89a7f052b8e2db428f4dbdd1927c6
E Ten Brink <etenbrink@yahoo.com>
2016-09-08 17:23:35 -0700

    Add content validator for PDF

---
+++ plugins/src/org/lockss/plugin/royalsocietyofchemistry/RSCContentValidator.java
@@ -0,0 +1,73 @@
+/*
+ * $Id$
+ */
+
+/*
+
+Copyright (c) 2016 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.royalsocietyofchemistry;
+
+import java.io.*;
+
+import org.lockss.daemon.*;
+import org.lockss.plugin.*;
+import org.lockss.util.StringUtil;
+import org.lockss.util.HeaderUtil;
+
+public class RSCContentValidator {
+  
+  protected static final String PDF_1 = "/articlepdf/";
+  protected static final String PDF_2 = "/chapterpdf/";
+  
+  public static class TextTypeValidator implements ContentValidator {
+    
+    public void validate(CachedUrl cu)
+        throws ContentValidationException, PluginException, IOException {
+      // validate based on pdf type url (ie .pdf or .jpg)
+      String url = cu.getUrl();
+      if ((StringUtil.indexOfIgnoreCase(url, PDF_1) > 0) ||
+          (StringUtil.indexOfIgnoreCase(url, PDF_2) > 0)) {
+        throw new ContentValidationException("URL MIME type mismatch");
+      }
+    }
+  }
+  
+  public static class Factory implements ContentValidatorFactory {
+    public ContentValidator createContentValidator(ArchivalUnit au, String contentType) {
+      switch (HeaderUtil.getMimeTypeFromContentType(contentType)) {
+      case "text/html":
+      case "text/*":
+        return new TextTypeValidator();
+      default:
+        return null;
+      }
+    }
+  }
+  
+}
+
