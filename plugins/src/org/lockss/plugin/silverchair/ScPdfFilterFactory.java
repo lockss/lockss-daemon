9c4af6876acdb77288d72b8c3d1bc1b36dcdcb31
Wendell Wilson <wendell.wilson@gmail.com>
2016-10-10 14:03:20 -0400

    AMA PDF filter

---
+++ plugins/src/org/lockss/plugin/silverchair/ScPdfFilterFactory.java
@@ -0,0 +1,68 @@
+/*
+ * $Id$
+ */
+
+/*
+
+Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.silverchair;
+
+import java.io.InputStream;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.filter.pdf.*;
+import org.lockss.pdf.*;
+import org.lockss.plugin.*;
+
+/**
+ * Returns the input stream unless its JAMA which gets scraped
+ */
+public class ScPdfFilterFactory implements FilterFactory{
+
+  public ScPdfFilterFactory() {
+  }
+  
+  public InputStream createFilteredInputStream(ArchivalUnit au,
+                                               InputStream in,
+                                               String encoding)
+      throws PluginException {
+	  if(au.getProperties().getString("base_url").contains("jamanetwork")) {
+		    FilterFactory AmaPdfFilter = new AmaPdfFilterFactory();
+		    return AmaPdfFilter.createFilteredInputStream(au, in, encoding);
+	  }
+	  return in;
+  }
+  
+  public class AmaPdfFilterFactory extends ExtractingPdfFilterFactory {
+
+	@Override
+	public void transform(ArchivalUnit au, PdfDocument anObject) throws PdfException {
+		// Just scrape
+	}
+  }
+}
