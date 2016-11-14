8dcf153641fd7916fb712852db5b068c743796a1
Mary-Ellen <maryellen@lockss.org>
2016-11-14 11:57:50 -0800

    Prep for move to Silverchair.

---
+++ scripts/tdb/report_aus_to_test.sh
@@ -0,0 +1,49 @@
+###!/bin/bash
+###
+### Read in tester number and output auids to test.
+###
+##set -x
+##tester=$1
+##current_year=2016
+##contract=2008 #includes all possible contract years
+##published_start=2014
+##until [ $contract -gt $current_year ]; do
+##    echo The contract is $contract
+##    published=$contract
+##    until [ $published -gt $current_year ] ; do
+##        echo   The published year is $published
+##        #echo ./scripts/tdb/tdbout -a -Q 'year ~ "'$published'$" and publisher:info[tester] is "'$tester'" and publisher:info[contract] is "'$contract'"' tdb/clockssingest/amer*.tdb | sort -u > ~/tmp/rb1.txt
+##        ./scripts/tdb/tdbout -a -Q 'year ~ "'$published'$" and publisher:info[tester] is "'$tester'" and publisher:info[contract] is "'$contract'"' tdb/clockssingest/*.tdb | sort -u > ~/tmp/rb1.txt
+##        let published=published+1
+##    done
+##    let contract=contract+1
+##done
+##set +x
+##exit 0
+##
+
+# Read in tester number and output auids to test.
+#
+tester=$1
+current_year=2015
+published=2013
+contract=2008 #includes all possible contract years
+
+until [ $published -gt $current_year ] ; do
+    echo   The published year is $published
+
+    contract=2008 #includes all possible contract years
+    until [ $contract -gt $published ]; do
+        echo The contract is $contract
+#set -x
+            ./scripts/tdb/tdbout -a -M -Q 'year ~ "'$published'$" and publisher:info[tester] is "'$tester'" and publisher:info[contract] is "'$contract'"' tdb/clockssingest/*.tdb
+#set +x
+        let contract=contract+1
+    done
+    
+    let published=published+1
+done
+
+
+exit 0
+
