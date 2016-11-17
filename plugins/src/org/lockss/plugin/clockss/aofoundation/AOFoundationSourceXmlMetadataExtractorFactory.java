fcf3f9bf1a03854ec0ad67c6a34126451cdeaa64
Alexandra Ohlson <aohlson@lockss.org>
2016-11-16 23:08:09 -0800

    Prototype for new file-transfer for AO Foundation

---
+++ plugins/src/org/lockss/plugin/clockss/aofoundation/AOFoundationSourceXmlMetadataExtractorFactory.java
@@ -0,0 +1,96 @@
+/*
+ * $Id$
+ */
+
+/*
+
+ Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.clockss.aofoundation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.StringUtils;
+import org.lockss.util.*;
+import org.lockss.daemon.*;
+import org.lockss.extractor.*;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
+import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
+
+
+public class AOFoundationSourceXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
+  private static final Logger log = Logger.getLogger(AOFoundationSourceXmlMetadataExtractorFactory.class);
+  
+  private static SourceXmlSchemaHelper PubmedHelper = null;
+  
+  @Override
+  public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
+      String contentType)
+          throws PluginException {
+    return new AOFoundationSourceXmlMetadataExtractor();
+  }
+
+  public class AOFoundationSourceXmlMetadataExtractor extends SourceXmlMetadataExtractor {
+
+    @Override
+    protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu) {
+      // Once you have it, just keep returning the same one. It won't change.
+      if (PubmedHelper != null) {
+        return PubmedHelper;
+      }
+      PubmedHelper = new PubmedArticleXmlSchemaHelper();
+      return PubmedHelper;
+    }
+
+    
+    // TODO - if we get full text XML without a matching pdf we must still emit
+    @Override
+    protected List<String> getFilenamesAssociatedWithRecord(SourceXmlSchemaHelper helper, CachedUrl cu,
+        ArticleMetadata oneAM) {
+
+      // filename is just the same a the XML filename but with
+      String cuBase = FilenameUtils.getFullPath(cu.getUrl());
+      String pii = oneAM.getRaw(helper.getFilenameXPathKey());
+      String piibase = StringUtils.substringAfter(pii,"vol");
+      String pdfName = cuBase + "v" + piibase + ".pdf";
+      log.info("pdfName is " + pdfName);
+      List<String> returnList = new ArrayList<String>();
+      returnList.add(pdfName);
+      return returnList;
+    }
+    
+    @Override
+    protected void postCookProcess(SourceXmlSchemaHelper schemaHelper, 
+        CachedUrl cu, ArticleMetadata thisAM) {
+      log.debug("in AOFoundation postcook");
+    }
+
+  }
+}
