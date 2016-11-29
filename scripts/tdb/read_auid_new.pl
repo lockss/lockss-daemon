8f3abafb4c92320263c0f3310b8116cb19000dbb
Mary-Ellen <maryellen@lockss.org>
2016-11-29 15:35:50 -0800

    Add Wageningen Journals.

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -629,7 +629,8 @@ while (my $line = <>) {
            ($plugin eq "MarkAllenPlugin") ||
            ($plugin eq "MultiSciencePlugin") ||
            ($plugin eq "MassachusettsMedicalSocietyPlugin") ||
-           ($plugin eq "SiamPlugin")) {
+           ($plugin eq "SiamPlugin") ||
+           ($plugin eq "WageningenJournalsPlugin")) {
       $url = sprintf("%slockss/%s/%s/index.html",
       $param{base_url}, $param{journal_id}, $param{volume_name});
       $man_url = uri_unescape($url);
