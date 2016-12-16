43f6aac521dbfd6a46e09cf4f6285edcc9cfee3a
Alexandra Ohlson <aohlson@lockss.org>
2016-12-16 15:08:54 -0800

    add iterator/extractor to BK triggered for ServeAu

---
+++ plugins/src/org/lockss/plugin/clockss/nature/BkTriggeredArticleIteratorFactory.java
@@ -0,0 +1,106 @@
+/*
+
+Copyright (c) 2000-2017 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.clockss.nature;
+
+import java.util.Iterator;
+import java.util.regex.*;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.ArticleMetadataExtractor;
+import org.lockss.extractor.ArticleMetadataExtractorFactory;
+import org.lockss.extractor.BaseArticleMetadataExtractor;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.*;
+import org.lockss.util.Logger;
+
+/*
+ *  Although this uses the SourceXmlMetadataExtractor framework, it is a harvest plugin,
+ *  so the ArticleIterator will find the ".html" files if they exist
+ *  Not all articles have .pdf
+ */
+public class BkTriggeredArticleIteratorFactory implements ArticleIteratorFactory, ArticleMetadataExtractorFactory {
+
+  protected static final Logger log = Logger.getLogger(BkTriggeredArticleIteratorFactory.class);
+  
+  public static final Pattern XML_PATTERN = Pattern.compile("/(.*)\\.xml$", Pattern.CASE_INSENSITIVE);
+  public static final String XML_REPLACEMENT = "/$1.xml";
+  public static final Pattern PDF_PATTERN = Pattern.compile("/(.*)\\.pdf$", Pattern.CASE_INSENSITIVE);
+  public static final String PDF_REPLACEMENT = "/$1.pdf";
+  public static final String ABS_REPLACEMENT = "/$1.html";
+  
+  // all files found under article subdirectories that contain bonekeyreports_<year> in the name
+  protected static final String PATTERN_TEMPLATE = "\"%s%d/bonekeyreports_%d_[^/]+/.*\\.(xml|pdf)$\",base_url,year,year";
+  
+  @Override
+  public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au,
+                                                      MetadataTarget target)
+      throws PluginException {
+    SubTreeArticleIteratorBuilder builder = new SubTreeArticleIteratorBuilder(au);
+    
+    // no need to limit to ROOT_TEMPLATE
+    builder.setSpec(builder.newSpec()
+                    .setTarget(target)
+                    .setPatternTemplate(PATTERN_TEMPLATE, Pattern.CASE_INSENSITIVE));
+    
+    // Because we created the interim website, we KNOW that there will always
+    // be an XML and an HTML landing page for each article. There will sometimes
+    // be a PDF, unless one was never provided to us.
+    
+    // Not all volumes have PDF, but for those that do
+    builder.addAspect(PDF_PATTERN,
+                      PDF_REPLACEMENT,
+                      ArticleFiles.ROLE_FULL_TEXT_PDF);
+    // set up XML to be an aspect that will trigger an ArticleFiles and feed the metadata extractor
+    builder.addAspect(XML_PATTERN,
+                      XML_REPLACEMENT,
+                      ArticleFiles.ROLE_FULL_TEXT_XML,
+                      ArticleFiles.ROLE_ARTICLE_METADATA);
+
+    // set up ABSTRACT to be a secondary aspect - otherwise you'd try to create
+    // an article out of the issue TOC
+    builder.addAspect(ABS_REPLACEMENT,
+                      ArticleFiles.ROLE_ABSTRACT);
+    
+    
+    // The order in which we want to define full_text_cu.  
+    // First one that exists will get the job
+    // When there is no PDF, the "abstract" is actually complete content
+    builder.setFullTextFromRoles(ArticleFiles.ROLE_FULL_TEXT_PDF,
+        ArticleFiles.ROLE_ABSTRACT,
+        ArticleFiles.ROLE_FULL_TEXT_XML);  
+
+    return builder.getSubTreeArticleIterator();
+  }
+  
+  @Override
+  public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target)
+      throws PluginException {
+    return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
+  }
+}
