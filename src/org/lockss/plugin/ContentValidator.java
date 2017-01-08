56ae59f948648990f523666d502eb0c7a5095716
Tom Lipkis <tal@pss.com>
2017-01-07 18:26:05 -0800

    Added CacheException.NoStoreWarningOnly.

--- src/org/lockss/plugin/ContentValidator.java
+++ src/org/lockss/plugin/ContentValidator.java
@@ -36,11 +36,14 @@ import java.io.*;
 import org.lockss.daemon.*;
 
 /** Validate a received file; determine whether to accept or reject it.  To
- * accept, just return.  To reject, throw a ContentValidationException or
- * subclass, the response to which is determined by the plugin's (and the
- * default) mapping of exceptions to CacheExceptions.  If the validator
- * throws an exception (that is mapped to CacheException with either
- * ATTRIBUTE_FAIL or ATTRIBUTE_FATAL set) then the content is not stored.
+ * accept, just return.  To reject or issue a warning, throw a
+ * ContentValidationException or subclass.  The CacheException to which
+ * this exception is mapped, by default or by plugin-specified mapping,
+ * determines the action taken.  If the CacheException's ATTRIBUTE_FAIL or
+ * ATTRIBUTE_FATAL is set, the file will not be stored and the crawl will
+ * fail (when it completes, or, for ATTRIBUTE_FATAL, immediately).  If
+ * neither attribute is set a warning will be issued; if ATTRIBUTE_NO_STORE
+ * is set the file will not be stored, otherwise it will be stored.
  */
 public interface ContentValidator {
 
@@ -48,7 +51,7 @@ public interface ContentValidator {
    * CachedUrl and return if acceptable or throw a
    * ContentValidationException or subclass if not.  The CachedUrl need not
    * be released - the caller does so.<br>The action taken depends on the
-   * attributes of the {@link org.lockss.util.urlconn#CacheException} to
+   * attributes of the {@link org.lockss.util.urlconn.CacheException} to
    * which the thrown ContentValidationException is mapped.  The default
    * mappings are:<ul>
    *
@@ -67,7 +70,14 @@ public interface ContentValidator {
    * subclasses of ContentValidationException and map them to
    * CacheExceptions, using <tt>plugin_cache_result_list</tt>.
    *
-   * @param cu the CachedUrl to validate
+   * @param cu the CachedUrl to validate.  If a redirect occurred, the
+   * CachedUrl argument reflects the original URL, the response headers are
+   * those from the final, non-redirect response, and the list of
+   * redirected-to URLs (not including the original URL) is in the
+   * CachedUrl.PROPERTY_VALIDATOR_REDIRECT_URLS ({@value
+   * org.lockss.plugin.CachedUrl#PROPERTY_VALIDATOR_REDIRECT_URLS})
+   * property.  (This is a List, which must be retrieved using Map.get(),
+   * not Properties.getProperty()).
    * @throws ContentValidationException 
    * @throws IOException propagated from reading cu, etc.
    * @throws PluginException 
