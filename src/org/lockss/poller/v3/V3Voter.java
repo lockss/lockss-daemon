0ce7c77a8b958aeb3e774de8771e874fec9ae568
Tom Lipkis <tal@pss.com>
2017-01-16 22:19:35 -0800

    Disable poll checkpointing by default

--- src/org/lockss/poller/v3/V3Voter.java
+++ src/org/lockss/poller/v3/V3Voter.java
@@ -4,7 +4,7 @@
 
 /*
 
-Copyright (c) 2000-2008 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2017 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -289,8 +289,7 @@ public class V3Voter extends BasePoll {
     }
 
     pollSerializer = new V3VoterSerializer(theDaemon);
-    pollSerializer.enable(!c.getBoolean(PARAM_CHECKPOINT_VOTES, DEFAULT_CHECKPOINT_VOTES));
-
+    pollSerializer.enable(c.getBoolean(PARAM_CHECKPOINT_VOTES, DEFAULT_CHECKPOINT_VOTES));
     
     stateDir = PollUtil.ensurePollStateRoot();
 
