3ab1b9f643324ca16976bff7f3229d2971ad7e1a
E Ten Brink <etenbrink@yahoo.com>
2017-01-23 08:58:26 -0800

    Check-point work

---
+++ plugins/src/org/lockss/plugin/silverchair/ama/ScAMAHtmlCrawlFilterFactory.java
@@ -0,0 +1,137 @@
+/*
+ * $Id$
+ */
+
+/*
+
+Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.silverchair.ama;
+
+import java.io.InputStream;
+
+import org.htmlparser.NodeFilter;
+import org.htmlparser.filters.OrFilter;
+import org.lockss.daemon.PluginException;
+import org.lockss.filter.html.*;
+import org.lockss.plugin.*;
+
+public class ScAMAHtmlCrawlFilterFactory implements FilterFactory {
+
+  /*
+   * AMA = American Medical Association (http://jamanetwork.com/)
+   * Tabs 20151025
+   * 1=extract/abstract/article
+   * 2=discussion (w/i framework of article contents)
+   * 3=figures
+   * 4=tables
+   * 5=video
+   * 6=references
+   * 7=letters
+   * 8=cme
+   * 9=citing
+   * 10=comments
+   * 12=supplemental
+   * SPIE = SPIE (http://spiedigitallibrary.org/)
+   * Tabs 20151025
+   * 1= extract/abstract/article
+   * 2=figures
+   * 3=tables
+   * 5=multimedia
+   * 6=references
+   *
+   */
+
+  @Override
+  public InputStream createFilteredInputStream(ArchivalUnit au,
+                                               InputStream in,
+                                               String encoding)
+      throws PluginException {
+    return new HtmlFilterInputStream(
+      in,
+      encoding,
+      HtmlNodeFilterTransform.exclude(new OrFilter(new NodeFilter[] {
+          // DROP right column: related content, etc.
+          // KEEP links to article views, citation files, etc.
+          
+//          HtmlNodeFilters.allExceptSubtree(HtmlNodeFilters.tagWithAttributeRegex("div", "class", "portletColumn"),
+//                                           HtmlNodeFilters.tagWithAttribute("div", "id", "scm6MainContent_ToolBox")),
+          
+          HtmlNodeFilters.tagWithAttributeRegex("section", "class", "master-(header|footer)"),
+          HtmlNodeFilters.tagWithAttributeRegex("nav", "class", "issue-browse"),
+          HtmlNodeFilters.tagWithAttribute("div", "class", "(reveal|artmet)-modal"),
+          
+/*          
+<section class="master-header">
+<section id="secFooterControl" class="master-footer">
+<nav class="issue-browse">
+<div id="revealModal" class="reveal-modal" data-reveal="">
+<div id="MetricsModal" class="artmet-modal">
+<div id="no-access-reveal" class="no-access reveal-modal login-modal medium modal" data-reveal="">
+<div id="no-access-reveal-learning" class="no-access reveal-modal login-modal medium modal" data-reveal="">
+<div id="register-free-pdf-reveal" class="reveal-modal modal register-free-pdf medium" data-reveal="">
+          
+*/          
+          // DROP link to previous/next article [AMA]
+//          HtmlNodeFilters.tagWithAttributeRegex("a", "class", "(prev|next)"),
+          // DROP link back to issue TOC (prune impact of overcrawl) [AMA]
+//          HtmlNodeFilters.tagWithAttribute("a", "id", "scm6MainContent_lnkFullIssueName"),
+          // DROP first page preview: sometimes appears, sometimes not [AMA article]
+//          HtmlNodeFilters.tagWithAttribute("div", "id", "divFirstPagePreview"),
+          // DROP corrections link: creates two-way link between articles [AMA article]
+//          HtmlNodeFilters.tagWithAttribute("div", "id", "scm6MainContent_divCorrections"), // e.g. http://jama.jamanetwork.com/article.aspx?articleid=1456081 [AMA article]
+//          HtmlNodeFilters.tagWithAttribute("div", "id", "scm6MainContent_divCorrectionLinkToParent"), // e.g. http://jama.jamanetwork.com/article.aspx?articleid=1487482 [AMA article]
+          // DROP letters pane (7): creates two-way link between letter and reply [AMA article]
+          // e.g. http://jama.jamanetwork.com/article.aspx?articleid=1487491 and http://jama.jamanetwork.com/article.aspx?articleid=1487492
+          // [SPIE]: unknown as of yet
+//          HtmlNodeFilters.tagWithAttribute("div", "id", "tab7"),
+          
+          //Prev Next
+//          HtmlNodeFilters.tagWithAttribute("header", "class", "article-nav"),
+          //Issues
+//          HtmlNodeFilters.tagWithAttribute("section", "id", "IssueInfo"),
+          //references
+//          HtmlNodeFilters.tagWithAttributeRegex("div", "class", "refRow"),
+          
+          // Unvetted from prior version:
+
+          // Some references can be article links (especially in hidden tab) (AMA)
+//          HtmlNodeFilters.tagWithAttributeRegex("div", "class", "refContainer"),
+          // Comments may contain article links? (not in above commentFormContainer div) (AMA)
+//          HtmlNodeFilters.tagWithAttribute("div", "class", "commentBody"),
+          // Cross links: "This article/letter/etc. relates to...", "This erratum
+          // concerns...", "See also...", [ACCP, ACP]
+//          HtmlNodeFilters.tagWithAttribute("div", "class", "linkType"),
+          // Author disclosures box tends to reference older policy/guidelines documents
+//          HtmlNodeFilters.tagWithAttribute("div", "id", "scm6MainContent_divDisclosures"),
+          // Erratum link from old to new article [APA]
+//          HtmlNodeFilters.tagWithAttribute("div", "id", "scm6MainContent_divErratum"),
+      }))
+    );
+  }
+
+}
---
+++ plugins/src/org/lockss/plugin/silverchair/ama/ScAMAHtmlLinkExtractorFactory.java
@@ -0,0 +1,183 @@
+/*
+ * $Id$
+ */
+
+/*
+
+Copyright (c) 2017 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.silverchair.ama;
+
+import org.jsoup.nodes.Node;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.*;
+import org.lockss.extractor.JsoupHtmlLinkExtractor.SimpleTagLinkExtractor;
+import org.lockss.extractor.LinkExtractor.Callback;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.util.Logger;
+import org.lockss.util.StringUtil;
+
+//import java.io.UnsupportedEncodingException;
+//import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ScAMAHtmlLinkExtractorFactory implements LinkExtractorFactory {
+
+  private static final Logger logger = Logger.getLogger(ScAMAHtmlLinkExtractorFactory.class);
+
+  private static final String ANCHOR_TAG = "a";
+//  public static final String IMG_TAG = "img";
+
+  protected static final Pattern PATTERN_ARTICLE_PDF =
+      Pattern.compile("/journals/([^/]+)/data/journals/([^/]+/[^/]+/[^/.]+[.]pdf)$",
+          Pattern.CASE_INSENSITIVE);
+  protected static final String ACCESS_ARTICLE_PDF =
+      "/pdfaccess.ashx[?]url=/data/journals/%s&routename=%s";
+  
+  // http://jamanetwork.com/journals/jamainternalmedicine/data/journals/intemed/934800/iii160001.pdf
+  // http://jamanetwork.com/pdfaccess.ashx?url=/data/journals/intemed/934800/iii160001.pdf&routename=jamainternalmedicine
+//  protected static final Pattern PATTERN_ARTICLE =
+//    Pattern.compile("/(article|proceeding)\\.aspx\\?(articleid=[^&]+)$",
+//                    Pattern.CASE_INSENSITIVE);
+//
+//  protected static final Pattern PATTERN_CITATION =
+//    Pattern.compile("/downloadCitation\\.aspx\\?(format=[^&]+)?$",
+//                    Pattern.CASE_INSENSITIVE);
+//
+//  protected static final Pattern PATTERN_DOWNLOAD_FILE =
+//    Pattern.compile("javascript:downloadFile\\('([^']+)'\\)",
+//                    Pattern.CASE_INSENSITIVE);
+//
+//  protected static final Pattern PATTERN_OPENPDF =
+//    Pattern.compile("openPDFWindow\\('([^']+)'\\)",
+//                    Pattern.CASE_INSENSITIVE);
+
+  @Override
+  public LinkExtractor createLinkExtractor(String mimeType) throws PluginException {
+    JsoupHtmlLinkExtractor extractor = new JsoupHtmlLinkExtractor(false, false, null, null);
+    registerExtractors(extractor);
+    return extractor;
+  }
+
+  /*
+ *  For when it is insufficient to simply use a different link tag or script
+ *  tag link extractor class, a child plugin can override this and register
+ *  additional or alternate extractors
+ */
+  protected void registerExtractors(JsoupHtmlLinkExtractor extractor) {
+
+    extractor.registerTagExtractor(ANCHOR_TAG,
+                                   new ScAMAAnchorTagExtractor(new String[]{
+                                       "href",
+                                       "data-article-url",
+//                                       "onclick",
+//                                       "download"
+                                       }));
+//    extractor.registerTagExtractor(IMG_TAG,
+//                                   new SimpleTagLinkExtractor(new String[]{"src", "longdesc",
+//                                                                           "data-original"
+//                                   }));
+
+  }
+
+  public static class ScAMAAnchorTagExtractor extends SimpleTagLinkExtractor {
+
+    public ScAMAAnchorTagExtractor(final String[] attrs) {
+      super(attrs);
+  }
+
+    /**
+     * Extract link(s) from this tag for attributes href, onclick, download.
+     * We process each of the attributes in turn since more than one may be present.
+     *
+     * @param node the node containing the link
+     * @param au Current archival unit to which this html document belongs.
+     * @param cb A callback to record extracted links.
+     */
+    public void tagBegin(Node node, ArchivalUnit au, Callback cb) {
+      String srcUrl = node.baseUri();
+
+      //the <a href attribute handler
+      if (node.hasAttr("href")) {
+//        String href = node.attr("href");
+//        Matcher hrefMat;
+//        // we look for the citation and derive the article id and generate a url
+//        hrefMat = PATTERN_CITATION.matcher(href);
+//        if (hrefMat.find()) {
+//          logger.debug3("Found target citation URL");
+//          // Derive citation format; can be null
+//          String formatPair = hrefMat.group(1);
+//          Matcher srcUrlMat = PATTERN_ARTICLE.matcher(srcUrl);
+//          if (srcUrlMat.find()) {
+//            // Derive article ID
+//            String articleIdPair = srcUrlMat.group(2);
+//            // Generate correct citation URL
+//            String url;
+//            if (formatPair == null) {
+//              url = String.format("/downloadCitation.aspx?%s", articleIdPair);
+//            }
+//            else {
+//              url = String.format("/downloadCitation.aspx?%s&%s", formatPair,
+//                                  articleIdPair);
+//            }
+//            logger.debug3(String.format("Generated %s", url));
+//            if (!StringUtil.isNullString(url)) {
+//              cb.foundLink(url);
+//            }
+//          }
+//        }
+//        else {
+          // the standard method for <a href>
+          JsoupHtmlLinkExtractor.checkLink(node, cb, "href");
+//        }
+      }   // end <a href
+      
+      if (node.hasAttr("data-article-url")) {
+        String url = node.attr("data-article-url");
+        if (!StringUtil.isNullString(url)) {
+          // we take the url and derive the pdfaccess url
+          Matcher urlMat;
+          urlMat = PATTERN_ARTICLE_PDF.matcher(url);
+          if (urlMat.find()) {
+            logger.debug3("Found target URL: " + url);
+            String part1 = urlMat.group(1);
+            String part2 = urlMat.group(2);
+            String accessUrl = String.format(ACCESS_ARTICLE_PDF, part2, part1);
+            cb.foundLink(accessUrl);
+          }
+        }
+      }
+      
+      // the 'download' attribute handler - the standard method for <a download
+      if (node.hasAttr("download")) {
+        JsoupHtmlLinkExtractor.checkLink(node, cb, "download");
+      }
+    }
+
+} }
