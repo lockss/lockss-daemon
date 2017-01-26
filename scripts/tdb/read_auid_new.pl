14817b8be5135992319b72b062e2638b968f1822
Mary-Ellen <maryellen@lockss.org>
2017-01-25 17:03:33 -0800

    Correct doi.

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -573,12 +573,13 @@ while (my $line = <>) {
         $url = sprintf("%sejournals/issues/%s/%s",
           $param{base_url}, $param{journal_id}, $param{volume_name});
         $man_url = uri_unescape($url);
+        my $doi = uri_unescape($param{journal_id});
         my $req = HTTP::Request->new(GET, $man_url);
         my $resp = $ua->request($req);
         if ($resp->is_success) {
             my $man_contents = $resp->content;
             #no lockss permission statement on start page. Permission statement is here: https://www.thieme-connect.de/lockss.txt
-            if (defined($man_contents) && ($man_contents =~ m/Year $param{volume_name}/) && ($man_contents =~ m/DOI: $param{journal_id}/)) {
+            if (defined($man_contents) && ($man_contents =~ m/Year $param{volume_name}/) && ($man_contents =~ m/DOI: $doi/)) {
                 if ($man_contents =~ m/<h1>(.*)<\/h1>/si) {
                     $vol_title = $1
                 }
@@ -597,11 +598,12 @@ while (my $line = <>) {
         $url = sprintf("%sejournals/issues/%s/%s",
           $param{base_url}, $param{journal_id}, $param{volume_name});
         $man_url = uri_unescape($url);
+        my $doi = uri_unescape($param{journal_id});
         my $req = HTTP::Request->new(GET, $man_url);
         my $resp = $ua->request($req);
         if ($resp->is_success) {
             my $man_contents = $resp->content;
-            if (defined($man_contents) && ($man_contents =~ m/$clockss_tag/) && ($man_contents =~ m/Year $param{volume_name}/) && ($man_contents =~ m/DOI: $param{journal_id}/)) {
+            if (defined($man_contents) && ($man_contents =~ m/$clockss_tag/) && ($man_contents =~ m/Year $param{volume_name}/) && ($man_contents =~ m/DOI: $doi/)) {
                 if ($man_contents =~ m/<h1>(.*)<\/h1>/si) {
                     $vol_title = $1
                 }
