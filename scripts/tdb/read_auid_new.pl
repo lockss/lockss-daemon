b5d146df744ff66e5b406c22de789d8cf116a3da
Mary-Ellen <maryellen@lockss.org>
2017-03-13 13:17:11 -0700

    Add manifest page checker.

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -1853,7 +1853,7 @@ while (my $line = <>) {
     my $resp = $ua->request($req);
     if ($resp->is_success) {
       my $man_contents = $resp->content;
-      if (defined($man_contents) && ($man_contents =~ m/$lockss_tag/) && (man_contents =~ m/issn=$param{journal_issn}\&vol=$param{volume_name}/)) {
+      if (defined($man_contents) && ($man_contents =~ m/$lockss_tag/) && (man_contents =~ m/p.issn=$param{journal_issn}.vol=$param{volume_name}/)) {
         $result = "Manifest"
       } else {
         $result = "--NO_TAG--"
@@ -1871,7 +1871,7 @@ while (my $line = <>) {
     my $resp = $ua->request($req);
     if ($resp->is_success) {
       my $man_contents = $resp->content;
-      if (defined($man_contents) && ($man_contents =~ m/$clockss_tag/) && (man_contents =~ m/issn=$param{journal_issn}&vol=$param{volume_name}/)) {
+      if (defined($man_contents) && ($man_contents =~ m/$clockss_tag/) && (man_contents =~ m/p.issn=$param{journal_issn}.vol=$param{volume_name}/)) {
         $result = "Manifest"
       } else {
         $result = "--NO_TAG--"
