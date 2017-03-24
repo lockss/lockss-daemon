6b1eb2c1158b020ab41a55ba2c261be4b9e2e96b
wkwilson <wkwilson@lockss.org>
2017-03-24 10:37:03 -0400

    CourseSource Source

---
+++ plugins/src/org/lockss/plugin/clockss/coursesource/CsXmlMetadataExtractorFactory.java
@@ -0,0 +1,93 @@
+/*
+ * $Id$
+ */
+
+/*
+
+ Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.clockss.coursesource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lockss.util.*;
+import org.lockss.daemon.*;
+import org.lockss.extractor.*;
+
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.clockss.JatsPublishingSchemaHelper;
+import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
+import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
+
+
+/*
+ * Use the common Jats schema
+ * The pdf filename is the same as the XML filename but with a .pdf suffix
+ */
+
+public class CsXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
+  private static final Logger log = Logger.getLogger(CsXmlMetadataExtractorFactory.class);
+
+  private static SourceXmlSchemaHelper JatsPublishingHelper = null;
+
+  @Override
+  public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
+      String contentType)
+          throws PluginException {
+    return new CsXmlMetadataExtractor();
+  }
+
+  public class CsXmlMetadataExtractor extends SourceXmlMetadataExtractor {
+
+    @Override
+    protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu) {
+      // Once you have it, just keep returning the same one. It won't change.
+      if (JatsPublishingHelper != null) {
+        return JatsPublishingHelper;
+      }
+      JatsPublishingHelper = new JatsPublishingSchemaHelper();
+      return JatsPublishingHelper;
+    }
+
+
+    /* In this case, the filename is the same as the xml filename
+     */
+    @Override
+    protected List<String> getFilenamesAssociatedWithRecord(SourceXmlSchemaHelper helper, CachedUrl cu,
+        ArticleMetadata oneAM) {
+
+      String url_string = cu.getUrl();
+      String pdfName = url_string.substring(0,url_string.length() - 3) + "pdf";
+      log.debug3("pdfName is " + pdfName);
+      List<String> returnList = new ArrayList<String>();
+      returnList.add(pdfName);
+      return returnList;
+    }
+
+  }
+}
---
+++ plugins/test/src/org/lockss/plugin/clockss/coursesource/TestCsXmlMetadataExtractor.java
@@ -0,0 +1,172 @@
+/*
+ * $Id$
+ */
+/*
+
+/*
+
+ Copyright (c) 2000-2015 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of his software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.clockss.coursesource;
+import java.io.InputStream;
+import java.util.*;
+
+import org.lockss.test.*;
+import org.lockss.util.*;
+import org.lockss.config.*;
+import org.lockss.extractor.*;
+import org.lockss.plugin.*;
+import org.lockss.plugin.clockss.schweizerbart.SchweizerbartXmlMetadataExtractorFactory;
+
+
+public class TestCsXmlMetadataExtractor extends LockssTestCase {
+
+  private static final Logger log = Logger.getLogger(TestCsXmlMetadataExtractor.class);
+
+  private MockLockssDaemon theDaemon;
+  private MockArchivalUnit mau;
+
+  private static String PLUGIN_NAME = "org.lockss.plugin.clockss.coursesource.ClockssCourseSourceSourcePlugin";
+  private static String BASE_URL = "http://www.source.org/";
+
+  public void setUp() throws Exception {
+    super.setUp();
+    setUpDiskSpace(); // you need this to have startService work properly...
+
+    theDaemon = getMockLockssDaemon();
+    mau = new MockArchivalUnit();
+
+    theDaemon.getAlertManager();
+    theDaemon.getPluginManager().setLoadablePluginsReady(true);
+    theDaemon.setDaemonInited(true);
+    theDaemon.getPluginManager().startService();
+    theDaemon.getCrawlManager();
+    mau.setConfiguration(auConfig());
+
+  }
+
+  public void tearDown() throws Exception {
+    theDaemon.stopDaemon();
+    super.tearDown();
+  }
+
+  /**
+   * Configuration method. 
+   * @return
+   */
+  Configuration auConfig() {
+    Configuration conf = ConfigManager.newConfiguration();
+    conf.put("base_url", BASE_URL);
+    conf.put("year", "2014");
+    return conf;
+  }
+  
+
+
+ private static final String realXMLFile = "test_coursesource.xml";
+
+  public void testFromJatsPublishingXMLFile() throws Exception {
+    InputStream file_input = null;
+    try {
+      file_input = getResourceAsStream(realXMLFile);
+      String string_input = StringUtil.fromInputStream(file_input);
+      IOUtil.safeClose(file_input);
+
+      CIProperties xmlHeader = new CIProperties();    
+      String xml_url = "http://www.source.com/TestXML.xml";
+      xmlHeader.put(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+      MockCachedUrl mcu = mau.addUrl(xml_url, true, true, xmlHeader);
+      // Now add all the pdf files in our AU since we check for them before emitting
+      mau.addUrl(pdfUrl1, true, true, xmlHeader);
+      
+      mcu.setContent(string_input);
+      mcu.setContentSize(string_input.length());
+      mcu.setProperty(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+
+    FileMetadataExtractor me = new  CsXmlMetadataExtractorFactory().createFileMetadataExtractor(MetadataTarget.Any(), "text/xml");
+      FileMetadataListExtractor mle =
+          new FileMetadataListExtractor(me);
+      List<ArticleMetadata> mdlist = mle.extract(MetadataTarget.Any(), mcu);
+      assertNotEmpty(mdlist);
+      assertEquals(1, mdlist.size());
+
+      // check each returned md against expected values
+      Iterator<ArticleMetadata> mdIt = mdlist.iterator();
+      ArticleMetadata mdRecord = null;
+      while (mdIt.hasNext()) {
+        mdRecord = (ArticleMetadata) mdIt.next();
+        compareMetadata(mdRecord);
+      }
+    }finally {
+      IOUtil.safeClose(file_input);
+    }
+
+  }
+ 
+  private static final int ISSN_INDEX = 0;
+  private static final int DOI_INDEX = 1;
+  private static final int AUTHOR_INDEX = 2;
+  private static final int JOURNAL_TITLE = 3;
+  private static final int ARTICLE_DATE = 4;
+  private static final int ARTICLE_TITLE = 5;
+  private static final int VOLUME = 6;
+  private static final int ISSUE = 7;
+  private static final int SPAGE = 8;
+  private static final int EPAGE = 9;
+
+
+  
+  private static final String pdfUrl1 = "http://www.source.com/TestXML.pdf";
+  private static final ArrayList md1 = (ArrayList) ListUtil.list(
+      "1111-1111",
+      "",
+      "[Foo, Bar M]",
+      "CourseSource",
+      "2016",
+      "A Name Subtitle",
+      "172",
+      "2-3",
+      "309",
+      "323");
+
+  // Find the matching expected data by matching against ISSN
+  private void compareMetadata(ArticleMetadata AM) {
+    String issn = AM.get(MetadataField.FIELD_ISSN);
+    ArrayList expected = md1;
+    assertNotNull(expected);
+    //assertEquals(expected.get(DOI_INDEX), AM.get(MetadataField.FIELD_DOI));
+    assertEquals(expected.get(AUTHOR_INDEX), AM.getList(MetadataField.FIELD_AUTHOR).toString());
+    assertEquals(expected.get(JOURNAL_TITLE), AM.get(MetadataField.FIELD_PUBLICATION_TITLE));
+    assertEquals(expected.get(ARTICLE_DATE), AM.get(MetadataField.FIELD_DATE));
+    assertEquals(expected.get(ARTICLE_TITLE), AM.get(MetadataField.FIELD_ARTICLE_TITLE));
+    //assertEquals(expected.get(VOLUME), AM.get(MetadataField.FIELD_VOLUME));
+    //assertEquals(expected.get(ISSUE), AM.get(MetadataField.FIELD_ISSUE));
+    //assertEquals(expected.get(SPAGE), AM.get(MetadataField.FIELD_START_PAGE));
+    //assertEquals(expected.get(EPAGE), AM.get(MetadataField.FIELD_END_PAGE));
+    
+  }
+}
