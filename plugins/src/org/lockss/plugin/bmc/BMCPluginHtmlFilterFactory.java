9d19ee0f766244b60c6526521018611218dfe6f0
E Ten Brink <etenbrink@yahoo.com>
2017-04-24 15:08:18 -0700

    Add hashing for notes

--- plugins/src/org/lockss/plugin/bmc/BMCPluginHtmlFilterFactory.java
+++ plugins/src/org/lockss/plugin/bmc/BMCPluginHtmlFilterFactory.java
@@ -91,6 +91,8 @@ public class BMCPluginHtmlFilterFactory implements FilterFactory {
           HtmlNodeFilters.tagWithAttributeRegex("a", "class",
               "(abstract|fulltext|pdf.*)-link")),
       HtmlNodeFilters.tagWithAttribute("a", "id", "comments"),
+      // Extreme filtering
+      HtmlNodeFilters.tagWithAttribute("div", "id", "article-note"),
       
       // Contains one-time names inside the page
       HtmlNodeFilters.tagWithAttribute("a", "name"),
