ee14137bdadfcd732170ee5df20be09d44cf21ae
Mary-Ellen <maryellen@lockss.org>
2017-05-05 10:29:28 -0700

    Update scripts with notes on usage.

--- scripts/tdb/report_buckets_clockss.sh
+++ scripts/tdb/report_buckets_clockss.sh
@@ -3,11 +3,39 @@
 # Read in two reports of AUids and status. Generate a table
 # of category crossings.
 #
-# To create a report, comparing clockss status1 and status2
+# To create a report, comparing clockss status1 and status2, right now.
 # ./scripts/tdb/tdbout -t auid,status tdb/clockssingest/*.tdb | sort -u > ../SageEdits/file1.txt
 # ./scripts/tdb/tdbout -t auid,status2 tdb/clockssingest/*.tdb | sort -u > ../SageEdits/file2.txt
 # ./scripts/tdb/report_buckets.pl ../SageEdits/file1.txt ../SageEdits/file2.txt > ../SageEdits/buckets_today.tsv
+#
+# This example is for creating a table for the GLN over time.
+#git checkout master
+#git checkout `git rev-list -n 1 --before="2016-04-01 00:00" master`
+#ant jar-lockss
+#./scripts/tdb/tdbout -t auid,status tdb/prod/*.tdb | sort -u > ../tmp/file1.txt
+#git checkout master
+#git checkout `git rev-list -n 1 --before="2017-04-01 00:00" master`
+#ant jar-lockss
+#./scripts/tdb/tdbout -t auid,status tdb/prod/*.tdb | sort -u > ../tmp/file2.txt
+#git checkout master
+#git pull origin master
+#ant jar-lockss
+#./scripts/tdb/report_buckets.pl ../tmp/file1.txt ../tmp/file2.txt
+#exit 0
+#
+# This example is for creating a table for clockss at one specific time.
+#git checkout master
+#git checkout `git rev-list -n 1 --before="2017-05-01 00:00" master`
+#ant jar-lockss
+#./scripts/tdb/tdbout -t auid,status tdb/clockssingest/*.tdb | sort -u > ../tmp/file1.txt
+#./scripts/tdb/tdbout -t auid,status2 tdb/clockssingest/*.tdb | sort -u > ../tmp/file2.txt
+#git checkout master
+#git pull origin master
+#ant jar-lockss
+#./scripts/tdb/report_buckets.pl ../tmp/file1.txt ../tmp/file2.txt
+#exit 0
 
+# This active script is for creating a table for clockss, for a specific publication year, at the current time.
 year=$1
 ./scripts/tdb/tdbout -t auid,status -Q 'year ~ "'$year'"' tdb/clockssingest/*.tdb | sort -u > ~/tmp/rb1.txt
 #./scripts/tdb/tdbout -t auid,status -Q 'year ~ "'$year'"' tdb/clockssingest/*.tdb | grep -v ClockssTaylorAndFrancisPlugin | sort -u > ~/tmp/rb1.txt
