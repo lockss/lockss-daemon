e6ad16a2fec094fa6c493531c9ac41bba50256f2
Alexandra Ohlson <aohlson@lockss.org>
2017-06-08 14:53:05 -0700

    Prototype for ICE Publishing file-transfer

---
+++ plugins/src/org/lockss/plugin/clockss/ice/IceJatsXmlMetadataExtractorFactory.java
@@ -0,0 +1,106 @@
+/*
+ * $Id:$
+ */
+
+/*
+
+ Copyright (c) 2000-2017 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.clockss.ice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.lockss.util.*;
+import org.lockss.daemon.*;
+import org.lockss.extractor.*;
+
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.clockss.JatsPublishingSchemaHelper;
+import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
+import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
+
+
+
+public class IceJatsXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
+  private static final Logger log = Logger.getLogger(IceJatsXmlMetadataExtractorFactory.class);
+
+  private static SourceXmlSchemaHelper JatsPublishingHelper = null;
+
+  @Override
+  public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
+      String contentType)
+          throws PluginException {
+    return new JatsPublishingSourceXmlMetadataExtractor();
+  }
+
+  public class JatsPublishingSourceXmlMetadataExtractor extends SourceXmlMetadataExtractor {
+
+    @Override
+    protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu) {
+      // Once you have it, just keep returning the same one. It won't change.
+      if (JatsPublishingHelper == null) {
+        JatsPublishingHelper = new JatsPublishingSchemaHelper();
+      }
+      return JatsPublishingHelper;
+    }
+    
+
+
+    /* In this case, the filename is the same as the xml filename
+     */
+    @Override
+    protected List<String> getFilenamesAssociatedWithRecord(SourceXmlSchemaHelper helper, CachedUrl cu,
+        ArticleMetadata oneAM) {
+
+      String url_string = cu.getUrl();
+      String pdfName = url_string.substring(0,url_string.length() - 3) + "pdf";
+      log.debug3("pdfName is " + pdfName);
+      List<String> returnList = new ArrayList<String>();
+      returnList.add(pdfName);
+      return returnList;
+    }
+    
+    @Override
+    protected void postCookProcess(SourceXmlSchemaHelper schemaHelper, 
+        CachedUrl cu, ArticleMetadata thisAM) {
+
+      log.debug3("in ICE postCookProcess");
+      //If we didn't get a valid date value, use the copyright year if it's there
+      if (thisAM.get(MetadataField.FIELD_DATE) == null) {
+        if (thisAM.getRaw(JatsPublishingSchemaHelper.JATS_date) != null) {
+          thisAM.put(MetadataField.FIELD_DATE, thisAM.getRaw(JatsPublishingSchemaHelper.JATS_date));
+        } else {// last chance
+          thisAM.put(MetadataField.FIELD_DATE, thisAM.getRaw(JatsPublishingSchemaHelper.JATS_edate));
+        }
+      }
+    }
+
+  }
+}
