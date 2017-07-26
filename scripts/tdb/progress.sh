95c11b489de18374bd1a83a649535f97fbe5ab30
Mary-Ellen <maryellen@lockss.org>
2017-07-25 19:27:59 -0700

    Allow script to work in varied environments.

--- scripts/tdb/progress.sh
+++ scripts/tdb/progress.sh
@@ -8,12 +8,16 @@ countother=`cat $tpath/allstatus | wc -l`
 OutputX=`date +%F`
 for j in doesNotExist expected exists manifest doNotProcess testing notReady ready released down superseded
 do
-count=`grep $j $tpath/allstatus | wc -l`
-countother=$(($countother - $count))
-OutputX=$OutputX,$count
-#echo $OutputX
+  count=`grep $j $tpath/allstatus | wc -l`
+  countother=$(($countother - $count))
+  OutputX=$OutputX,$count
+  #echo $OutputX
 done
 OutputX=$OutputX,$countother
+if [ ! -f /scripts/tdb/progress_out.txt ]
+then
+  echo "date,doesNotExist,expected,exists,manifest,doNotProcess,testing,notReady,ready,released,down,superseded,other" > ./scripts/tdb/progress_out.txt
+fi
 echo $OutputX >> ./scripts/tdb/progress_out.txt
 cat ./scripts/tdb/progress_out.txt
 exit 0
