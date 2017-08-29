cfffd78b2802a13b4a46ba905d83f1284300ad86
Alexandra Ohlson <aohlson@lockss.org>
2017-08-29 15:05:07 -0700

    prototype for AIAA meeting paper file-trs plugin

---
+++ plugins/src/org/lockss/plugin/clockss/aiaa/AIAAXmlMetadataExtractorFactory.java
@@ -0,0 +1,142 @@
+/*
+ * $Id:$
+ */
+
+/*
+
+ Copyright (c) 2000-2017 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.clockss.aiaa;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.ArticleMetadata;
+import org.lockss.extractor.FileMetadataExtractor;
+import org.lockss.extractor.MetadataField;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
+import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
+import org.lockss.util.Logger;
+
+
+
+public class AIAAXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
+  private static final Logger log = Logger.getLogger(AIAAXmlMetadataExtractorFactory.class);
+  //example: .....foo.zip!/miecec13/6.2013-4027/6.2013-4027.pdf
+  private static final Pattern DATEPAT = Pattern.compile("/[^/.]+\\.([0-9]{4})-[^/.-]+\\.pdf$", Pattern.CASE_INSENSITIVE);
+
+
+  // currently only for meeting papers which is book format
+  private static SourceXmlSchemaHelper AtyponBookHelper = null;
+
+  @Override
+  public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
+      String contentType)
+          throws PluginException {
+    return new AIAASourceXmlMetadataExtractor();
+  }
+
+  public class AIAASourceXmlMetadataExtractor extends SourceXmlMetadataExtractor {
+
+    @Override
+    protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu) {
+      // Once you have it, just keep returning the same one. It won't change.
+      if (AtyponBookHelper == null) {
+        AtyponBookHelper = new AtyponBooksXmlSchemaHelper();
+      }
+      return AtyponBookHelper;
+    }
+    
+
+
+    /* 
+     * There are two types of XML files 
+     * Each meeting has a top level XML that encompasses the entire conference. 
+     * This is the XML file with the publisher, ISBN and date if either are available - publication level
+     * The other XML files are per-article, equivalent of chapter level with author information
+     * We process both types but only check for content.pdf file when looking at chapter info
+     * In this case, the filename is the same as the xml filename
+     */
+    @Override
+    protected List<String> getFilenamesAssociatedWithRecord(SourceXmlSchemaHelper helper, CachedUrl cu,
+        ArticleMetadata oneAM) {
+
+      String url_string = cu.getUrl();
+      List<String> returnList = new ArrayList<String>();
+      // hard to really know...could check pattern of url path... for now do this
+      if( (oneAM.getRaw(AtyponBooksXmlSchemaHelper.chapter_pdf) == null )
+        && (oneAM.getRaw(AtyponBooksXmlSchemaHelper.chapter_title) == null)){
+        returnList.add(url_string); // this xml is sufficient
+      } else {
+        // look for the pdf 
+        String pdfName = url_string.substring(0,url_string.length() - 3) + "pdf";
+        log.debug3("pdfName is " + pdfName);
+        returnList.add(pdfName); // must have the pdf
+      }   
+      return returnList;
+    }
+    
+    @Override
+    protected void postCookProcess(SourceXmlSchemaHelper schemaHelper, 
+        CachedUrl cu, ArticleMetadata thisAM) {
+
+      log.debug3("in AIAA postCookProcess");
+      //If we didn't get a valid date value, use the copyright year if it's there
+      if (thisAM.get(MetadataField.FIELD_DATE) == null) {
+        if (thisAM.getRaw(AtyponBooksXmlSchemaHelper.chapter_copyright_year) != null) {
+          thisAM.put(MetadataField.FIELD_DATE, thisAM.getRaw(AtyponBooksXmlSchemaHelper.chapter_copyright_year));
+        } else {
+          // fallback if there was a PDF - extract the year from the pdf name
+          Matcher dateMat = DATEPAT.matcher(thisAM.get(MetadataField.FIELD_ACCESS_URL));
+          if (dateMat.find()) {
+            thisAM.put(MetadataField.FIELD_DATE, dateMat.group(1));
+          }
+        }
+      }
+      // this is definnitely a book
+      thisAM.put(MetadataField.FIELD_PUBLICATION_TYPE,MetadataField.PUBLICATION_TYPE_BOOK);
+      if (thisAM.get(MetadataField.FIELD_ACCESS_URL).endsWith(".pdf")) {
+        // but only a chapter if we have content associated with it
+        thisAM.put(MetadataField.FIELD_ARTICLE_TYPE,MetadataField.ARTICLE_TYPE_BOOKCHAPTER);
+      } else {
+        // this is publication level information only
+        //only set the book-level doi if we're emitting for the book volume and not a chapter
+        thisAM.put(MetadataField.FIELD_ARTICLE_TYPE,MetadataField.ARTICLE_TYPE_BOOKVOLUME);
+        if (thisAM.getRaw(AtyponBooksXmlSchemaHelper.book_doi) != null) {
+          thisAM.put(MetadataField.FIELD_DOI,thisAM.getRaw(AtyponBooksXmlSchemaHelper.book_doi));
+        }
+      }
+    
+    }
+
+  }
+}
---
+++ plugins/test/src/org/lockss/plugin/clockss/aiaa/TestAIAAXmlMetadataExtractor.java
@@ -0,0 +1,181 @@
+/*
+ * $Id:$
+ */
+/*
+
+/*
+
+ Copyright (c) 2000-2017 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of his software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.clockss.aiaa;
+import java.io.InputStream;
+import java.util.*;
+
+import org.lockss.test.*;
+import org.lockss.util.*;
+import org.lockss.config.*;
+import org.lockss.extractor.*;
+import org.lockss.plugin.*;
+
+
+public class TestAIAAXmlMetadataExtractor extends LockssTestCase {
+
+  private static final Logger log = Logger.getLogger(TestAIAAXmlMetadataExtractor.class);
+
+  private MockLockssDaemon theDaemon;
+  private MockArchivalUnit mau;
+
+  private static String BASE_URL = "http://www.source.org/";
+  private static final String xml_article_url = BASE_URL + "2017/foo.zip!/miecec03/6.2003-6092/6.2003-6092.xml";
+  private static final String xml_index_url = BASE_URL + "2017/foo.zip!/miecec03/miecec03/miecec03.xml";
+  private static final String pdf_url = BASE_URL + "2017/foo.zip!/miecec03/6.2003-6092/6.2003-6092.pdf";
+
+  public void setUp() throws Exception {
+    super.setUp();
+    setUpDiskSpace(); // you need this to have startService work properly...
+
+    theDaemon = getMockLockssDaemon();
+    mau = new MockArchivalUnit();
+
+    theDaemon.getAlertManager();
+    theDaemon.getPluginManager().setLoadablePluginsReady(true);
+    theDaemon.setDaemonInited(true);
+    theDaemon.getPluginManager().startService();
+    theDaemon.getCrawlManager();
+    mau.setConfiguration(auConfig());
+
+  }
+
+  public void tearDown() throws Exception {
+    theDaemon.stopDaemon();
+    super.tearDown();
+  }
+
+  /**
+   * Configuration method. 
+   * @return
+   */
+  Configuration auConfig() {
+    Configuration conf = ConfigManager.newConfiguration();
+    conf.put("base_url", BASE_URL);
+    conf.put("year", "2017");
+    return conf;
+  }
+
+
+  
+  private static final String realXMLFile1 = "AIAAArticleSourceTest.xml";
+  private static final String realXMLFile2 = "AIAAIndexSourceTest.xml";
+
+  public void testFromArticleXMLFile() throws Exception {
+    InputStream file_input = null;
+    try {
+      file_input = getResourceAsStream(realXMLFile1);
+      String string_input = StringUtil.fromInputStream(file_input);
+      IOUtil.safeClose(file_input);
+
+      CIProperties xmlHeader = new CIProperties();    
+      xmlHeader.put(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+      MockCachedUrl mcu = mau.addUrl(xml_article_url, true, true, xmlHeader);
+      // Now add all the pdf files in our AU since we check for them before emitting
+      mau.addUrl(pdf_url, true, true, xmlHeader);
+
+      mcu.setContent(string_input);
+      mcu.setContentSize(string_input.length());
+      mcu.setProperty(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+
+      FileMetadataExtractor me = new  AIAAXmlMetadataExtractorFactory().createFileMetadataExtractor(MetadataTarget.Any(), "text/xml");
+      FileMetadataListExtractor mle =
+          new FileMetadataListExtractor(me);
+      List<ArticleMetadata> mdlist = mle.extract(MetadataTarget.Any(), mcu);
+      assertNotEmpty(mdlist);
+      assertEquals(1, mdlist.size());
+
+      // check each returned md against expected values
+      Iterator<ArticleMetadata> mdIt = mdlist.iterator();
+      ArticleMetadata mdRecord = null;
+      while (mdIt.hasNext()) {
+        mdRecord = (ArticleMetadata) mdIt.next();
+        //log.info(mdRecord.ppString(2));
+        assertEquals(mdRecord.get(MetadataField.FIELD_PUBLICATION_TITLE),"International Conference");
+        assertEquals(mdRecord.get(MetadataField.FIELD_ARTICLE_TITLE),"Presentation Title");
+        assertEquals(mdRecord.get(MetadataField.FIELD_DOI),"10.2514/6.2003-chapter");
+        assertEquals(mdRecord.get(MetadataField.FIELD_DATE),"2003"); //pulled from pdf name
+        assertEquals(mdRecord.get(MetadataField.FIELD_ARTICLE_TYPE),MetadataField.ARTICLE_TYPE_BOOKCHAPTER);
+        assertEquals(mdRecord.get(MetadataField.FIELD_PUBLICATION_TYPE),MetadataField.PUBLICATION_TYPE_BOOK);
+        assertEquals(mdRecord.get(MetadataField.FIELD_ACCESS_URL).endsWith(".pdf"),true);
+      }
+    }finally {
+      IOUtil.safeClose(file_input);
+    }
+
+  }
+  
+  public void testFromVolumeXMLFile() throws Exception {
+    InputStream file_input = null;
+    try {
+      file_input = getResourceAsStream(realXMLFile2);
+      String string_input = StringUtil.fromInputStream(file_input);
+      IOUtil.safeClose(file_input);
+
+      CIProperties xmlHeader = new CIProperties();    
+      xmlHeader.put(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+      MockCachedUrl mcu = mau.addUrl(xml_index_url, true, true, xmlHeader);
+      // no pdf file for this one
+      
+      mcu.setContent(string_input);
+      mcu.setContentSize(string_input.length());
+      mcu.setProperty(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+
+      FileMetadataExtractor me = new  AIAAXmlMetadataExtractorFactory().createFileMetadataExtractor(MetadataTarget.Any(), "text/xml");
+      FileMetadataListExtractor mle =
+          new FileMetadataListExtractor(me);
+      List<ArticleMetadata> mdlist = mle.extract(MetadataTarget.Any(), mcu);
+      assertNotEmpty(mdlist);
+      assertEquals(1, mdlist.size());
+
+      // check each returned md against expected values
+      Iterator<ArticleMetadata> mdIt = mdlist.iterator();
+      ArticleMetadata mdRecord = null;
+      while (mdIt.hasNext()) {
+        mdRecord = (ArticleMetadata) mdIt.next();
+        //log.info(mdRecord.ppString(2));
+        assertEquals(mdRecord.get(MetadataField.FIELD_PUBLICATION_TITLE),"International Conference");
+        assertEquals(mdRecord.get(MetadataField.FIELD_ISBN),"978-1-11111-111-1");
+        assertEquals(mdRecord.get(MetadataField.FIELD_DOI),"10.2514/BOOK");
+        assertEquals(mdRecord.get(MetadataField.FIELD_DATE),"January 9, 2017");
+        assertEquals(mdRecord.get(MetadataField.FIELD_ARTICLE_TYPE),MetadataField.ARTICLE_TYPE_BOOKVOLUME);
+        assertEquals(mdRecord.get(MetadataField.FIELD_PUBLICATION_TYPE),MetadataField.PUBLICATION_TYPE_BOOK);
+        assertEquals(mdRecord.get(MetadataField.FIELD_ACCESS_URL).endsWith(".pdf"),false);
+      }
+    }finally {
+      IOUtil.safeClose(file_input);
+    }
+
+  }
+
+}
