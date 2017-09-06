16050a89e1196c2061e0dbd17a9885931c979f70
Alexandra Ohlson <aohlson@lockss.org>
2017-09-06 16:54:00 -0700

    update to correctly hash book manifest pages

--- plugins/src/org/lockss/plugin/atypon/wageningen/WageningenJournalsHtmlHashFilterFactory.java
+++ plugins/src/org/lockss/plugin/atypon/wageningen/WageningenJournalsHtmlHashFilterFactory.java
@@ -72,6 +72,14 @@ public class WageningenJournalsHtmlHashFilterFactory
                   }
                 }
               }
+            } else if (HtmlNodeFilters.tagWithAttributeRegex("a", "href", 
+                "/doi/book/").accept(node)) {
+             // book manifest page has single doi/book ref whose parent is just the <body> element
+             // http://emeraldinsight.com/clockss/eisbn/9780080549910
+               Node liParent = node.getParent();
+               if (liParent instanceof BodyTag) {
+                 return true;
+               }  
             }
             return false;
           }
