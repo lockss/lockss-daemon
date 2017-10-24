a95d3caf4376b5fb8918bd0cbae0993130b0a09a
Alexandra Ohlson <aohlson@lockss.org>
2017-10-24 11:34:07 -0700

    normalize non-ascii url chars

--- plugins/src/org/lockss/plugin/maffey/MaffeyUrlNormalizer.java
+++ plugins/src/org/lockss/plugin/maffey/MaffeyUrlNormalizer.java
@@ -34,14 +34,27 @@ package org.lockss.plugin.maffey;
 
 import org.lockss.daemon.PluginException;
 import org.lockss.plugin.*;
+import org.lockss.util.Constants;
+import org.lockss.util.Logger;
+import org.lockss.util.StringUtil;
+import org.lockss.util.UrlUtil;
 
 public class MaffeyUrlNormalizer implements UrlNormalizer {
+  private static final Logger log = Logger.getLogger(MaffeyUrlNormalizer.class);
 
   public String normalizeUrl(String url,
                              ArchivalUnit au)
       throws PluginException {
+    
 	  url = url.replaceFirst("\\.css\\?[0-9]+", ".css");
 	  url = url.replaceFirst("&nocount=1", "");
+	  // now change any non-ascii chars to a safe value
+	  // without this, unicode chars in a url could cause 404 at servecontent time 
+	  if(!StringUtil.isAscii(url)) {
+	    if(log.isDebug2()) log.debug2("in:" + url);
+	    url = UrlUtil.encodeUri(url, Constants.ENCODING_UTF_8);
+	    if(log.isDebug2()) log.debug2("out:" + url);
+	  }
 	  return url;
   }
 
