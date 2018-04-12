6a2c100a92feca1202e7b9e63008f984564e4b45
E Ten Brink <etenbrink@yahoo.com>
2018-04-12 11:45:48 -0700

    Add case for UrlConsumer

--- plugins/src/org/lockss/plugin/highwire/ash/ASHDrupalUrlConsumerFactory.java
+++ plugins/src/org/lockss/plugin/highwire/ash/ASHDrupalUrlConsumerFactory.java
@@ -32,6 +32,9 @@ in this Software without prior written authorization from Stanford University.
 
 package org.lockss.plugin.highwire.ash;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.lockss.daemon.Crawler.CrawlerFacade;
 import org.lockss.plugin.*;
 import org.lockss.plugin.highwire.HighWireDrupalSigninUrlConsumerFactory;
@@ -42,8 +45,31 @@ import org.lockss.util.Logger;
  */
 public class ASHDrupalUrlConsumerFactory extends HighWireDrupalSigninUrlConsumerFactory {
   private static final Logger log = Logger.getLogger(ASHDrupalUrlConsumerFactory.class);
+  //For a search redirect 
   private static final String SEARCH_STR = "/search/volume";
   private static final String CHECKED_STR = "sso-checked=true";
+  
+  //For a supplement redirect - it could be multiple hops, eg 
+  //   http://www.bloodjournal.org/highwire/filestream/318501/field_highwire_adjunct_files/0/blood-2012-10-455055-1.pdf
+  //   http://www.bloodjournal.org/highwire/filestream/318501/field_highwire_adjunct_files/0/blood-2012-10-455055-1.full.pdf
+  //   https://signin.hematology.org/Login.aspx?vi=9&vt=...&DPLF=Y
+  //   http://www.bloodjournal.org/highwire/filestream/318501/field_highwire_adjunct_files/0/blood-2012-10-455055-1.full.pdf?sso-checked=true
+  //   http://www.bloodjournal.org/content/bloodjournal/suppl/2012/12/18/blood-2012-10-455055.DC1/blood-2012-10-455055-1.pdf?sso-checked=true
+  // so allow for more than 2 hops and don't worry about the addition of a jid in the url pattern
+  private static final String ORIG_FILE_STRING = "(/[^/?]+)$";
+  // Same as the original but with optional CHECKED_STR
+  private static final String DEST_FILE_STRING = "(/[^/?]+)(\\?sso-checked=true)?$";
+  
+  private static final Pattern origPat = Pattern.compile(ORIG_FILE_STRING, Pattern.CASE_INSENSITIVE);
+  private static final Pattern destPat = Pattern.compile(DEST_FILE_STRING, Pattern.CASE_INSENSITIVE);
+  
+  // for testing
+  public static Pattern getOrigPattern() {
+    return origPat;
+  }
+  public static Pattern getDestPattern() {
+    return destPat;
+  }
   
   @Override
   public UrlConsumer createUrlConsumer(CrawlerFacade facade, FetchedUrlData fud) {
@@ -84,8 +110,16 @@ public class ASHDrupalUrlConsumerFactory extends HighWireDrupalSigninUrlConsumer
         if (fud.origUrl.contains(SEARCH_STR) &&
             fud.fetchUrl.contains(SEARCH_STR) && fud.fetchUrl.endsWith(CHECKED_STR)) {
           should = true;
+        } else {
+          Matcher destMat = destPat.matcher(fud.fetchUrl);
+          Matcher origMat = origPat.matcher(fud.origUrl);
+          if (destMat.find() && origMat.find()) {
+            should =  origMat.group(1).equals(destMat.group(1));
+          }
         }
-        log.warning("SSO redirect: " + should + ": " + fud.redirectUrls.size()  + ": " + fud.origUrl + ": " + fud.redirectUrls.toString());
+        if (!should) {
+          log.warning("SSO redirect: " + should + ": " + fud.redirectUrls.size()  + ": " + fud.origUrl + ": " + fud.redirectUrls.toString());
+        }
       }
       return should;
     }
---
+++ plugins/test/src/org/lockss/plugin/highwire/ash/TestASHUrlConsumer.java
@@ -0,0 +1,65 @@
+/*
+ * $Id:$
+ */
+
+/*
+
+Copyright (c) 2018 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.highwire.ash;
+
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import org.lockss.test.LockssTestCase;
+import org.lockss.util.SetUtil;
+
+public class TestASHUrlConsumer extends LockssTestCase {
+  Set<String> originatingUrls = SetUtil.set(
+      "http://www.bloodjournal.org/highwire/filestream/318501/field_highwire_adjunct_files/0/blood-2012-10-455055-1.pdf"
+      );
+  
+  Set<String> destinationUrls = SetUtil.set(
+      "http://www.bloodjournal.org/content/bloodjournal/suppl/2012/12/18/blood-2012-10-455055.DC1/blood-2012-10-455055-1.pdf?sso-checked=true"
+      );
+  
+  public void testOrigPdfPattern() throws Exception {
+    Pattern origFullTextPat = ASHDrupalUrlConsumerFactory.getOrigPattern();
+    for (String url : originatingUrls) {
+      assertMatchesRE(origFullTextPat, url);
+    }
+  }
+  
+  public void testDestPdfPattern() throws Exception {
+    Pattern destFullTextPat = ASHDrupalUrlConsumerFactory.getDestPattern();
+    for (String url : destinationUrls) {
+      assertMatchesRE(destFullTextPat, url);
+    }
+    
+  }
+  
+}
