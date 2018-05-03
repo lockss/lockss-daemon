919a9468462c407d781b147e0b3fb68a703f9afd
E Ten Brink <etenbrink@yahoo.com>
2018-05-03 11:26:59 -0700

    Update hash filter to exclude bibl as new links added

--- plugins/src/org/lockss/plugin/internationalunionofcrystallography/oai/IUCrOaiHtmlHashFilterFactory.java
+++ plugins/src/org/lockss/plugin/internationalunionofcrystallography/oai/IUCrOaiHtmlHashFilterFactory.java
@@ -4,7 +4,7 @@
 
 /*
 
-Copyright (c) 2000-2012 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2018 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -55,6 +55,7 @@ public class IUCrOaiHtmlHashFilterFactory implements FilterFactory {
      HtmlNodeFilters.tagWithAttribute("div", "id", "footer"),
      HtmlNodeFilters.tagWithAttribute("div", "id", "footersearch"),
      HtmlNodeFilters.tagWithAttribute("div", "id", "journalsocialmedia"),
+     HtmlNodeFilters.tagWithAttribute("div", "id", "bibl"),
     };
     return new HtmlFilterInputStream(in,
                                      encoding,
