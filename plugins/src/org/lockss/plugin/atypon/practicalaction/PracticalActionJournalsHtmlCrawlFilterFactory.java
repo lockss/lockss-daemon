f95db0da3910150a24bc4a3f928281245de0f4ec
Alexandra Ohlson <aohlson@lockss.org>
2018-05-25 11:32:00 -0700

    Update crawl filter and plugin versions

--- plugins/src/org/lockss/plugin/atypon/practicalaction/PracticalActionJournalsHtmlCrawlFilterFactory.java
+++ plugins/src/org/lockss/plugin/atypon/practicalaction/PracticalActionJournalsHtmlCrawlFilterFactory.java
@@ -66,7 +66,9 @@ public class PracticalActionJournalsHtmlCrawlFilterFactory
     // toc, abs, ref - right column most read/most cited
     // too restrictive - it relates to any tabbed content, which could be main
     // TODO - look for a better solution
-    HtmlNodeFilters.tagWithAttribute("div", "aria-relevant", "additions"),                
+    HtmlNodeFilters.tagWithAttribute("div", "aria-relevant", "additions"),
+    // in-line links to other chapters, etc
+    HtmlNodeFilters.tagWithAttribute("a", "class", "ext-link"),
 
 
   };
