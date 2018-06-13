ad1feb7c6d26037dd8c8e1fe92b2392468eeb9b3
Alexandra Ohlson <aohlson@lockss.org>
2018-06-13 15:38:17 -0700

    add in a useful test for validating new warc projects manually

--- plugins/test/src/org/lockss/plugin/clockss/warc/TestWarcXmlMetadataExtractor.java
+++ plugins/test/src/org/lockss/plugin/clockss/warc/TestWarcXmlMetadataExtractor.java
@@ -178,6 +178,37 @@ public class TestWarcXmlMetadataExtractor extends LockssTestCase {
     
   }
  
+    /*
+  private static final String testXMLFile = "enternamehere.xml";
+
+    public void testNewWarcJatsXMLFile() throws Exception {
+      InputStream file_input = null;
+      try {
+        file_input = getResourceAsStream(testXMLFile);
+        String string_input = StringUtil.fromInputStream(file_input);
+        IOUtil.safeClose(file_input);
+
+        CIProperties xmlHeader = new CIProperties();   
+        // mock up coming from a zip arhive
+        String xml_url = BASE_URL + YEAR + "TestXML.xml";
+        xmlHeader.put(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+        MockCachedUrl mcu = mau.addUrl(xml_url, true, true, xmlHeader);
+        mcu.setContent(string_input);
+        mcu.setContentSize(string_input.length());
+        mcu.setProperty(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+
+      FileMetadataExtractor me = new WarcXmlMetadataExtractorFactory().createFileMetadataExtractor(
+          MetadataTarget.Any(), "text/xml");
+        FileMetadataListExtractor mle =
+            new FileMetadataListExtractor(me);
+        List<ArticleMetadata> mdlist = mle.extract(MetadataTarget.Any(), mcu);
+        assertNotEmpty(mdlist);
+        debugPrintMdListToFile(mdlist);
+      }finally {
+        IOUtil.safeClose(file_input);
+      }
+    }
+    */
   
   private void compareMetadata(ArticleMetadata AM) {
 
