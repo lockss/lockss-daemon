79ae68c9984a4f27f65dc7a0ca354b76f04a8449
Alexandra Ohlson <aohlson@lockss.org>
2018-08-01 12:29:26 -0400

    Fast track - new file-transfer publisher to testing

---
+++ plugins/src/org/lockss/plugin/clockss/emhswiss/EmhSwissXmlMetadataExtractorFactory.java
@@ -0,0 +1,118 @@
+/*
+ * $Id:$
+ */
+
+/*
+
+ Copyright (c) 2000-2018 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.clockss.emhswiss;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lockss.util.*;
+import org.lockss.daemon.*;
+import org.lockss.extractor.*;
+
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.clockss.JatsPublishingSchemaHelper;
+import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
+import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
+
+
+/*
+ * If the xml is at smw-2018-14582.xml
+ * then the pdf is smw-2018-14582.pdf
+ */
+
+public class EmhSwissXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
+  private static final Logger log = Logger.getLogger(EmhSwissXmlMetadataExtractorFactory.class);
+
+  private static SourceXmlSchemaHelper JatsPublishingHelper = null;
+
+  @Override
+  public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
+      String contentType)
+          throws PluginException {
+    return new JatsPublishingSourceXmlMetadataExtractor();
+  }
+
+  public class JatsPublishingSourceXmlMetadataExtractor extends SourceXmlMetadataExtractor {
+
+    @Override
+    protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu) {
+      // Once you have it, just keep returning the same one. It won't change.
+      if (JatsPublishingHelper == null) {
+        JatsPublishingHelper = new JatsPublishingSchemaHelper();
+      }
+      return JatsPublishingHelper;
+    }
+
+
+    /* 
+     * filename is the same as the xml, just change the suffix 
+     */
+    @Override
+    protected List<String> getFilenamesAssociatedWithRecord(SourceXmlSchemaHelper helper, CachedUrl cu,
+        ArticleMetadata oneAM) {
+
+      // filename is just the same a the XML filename but with .pdf 
+      // instead of .xml
+      String url_string = cu.getUrl();
+      String pdfName = url_string.substring(0,url_string.length() - 3) + "pdf";
+      log.debug3("pdfName is " + pdfName);
+      List<String> returnList = new ArrayList<String>();
+      returnList.add(pdfName);
+      return returnList;
+    }
+    
+    private static final String EMH_PUBLISHER_NAME = "EMH Swiss Medical Publishers Ltd";
+    @Override
+    protected void postCookProcess(SourceXmlSchemaHelper schemaHelper, 
+        CachedUrl cu, ArticleMetadata thisAM) {
+
+      log.debug3("in EMH postCookProcess");
+      // we get an issn but no publication information. This is protocols.io
+      if (thisAM.get(MetadataField.FIELD_PUBLICATION_TITLE) == null) {
+        if(thisAM.getRaw(JatsPublishingSchemaHelper.JATS_jid_pubmed) != null) {
+          thisAM.put(MetadataField.FIELD_PUBLICATION_TITLE, thisAM.getRaw(JatsPublishingSchemaHelper.JATS_jid_pubmed));
+        }
+      }
+      //If we didn't get a valid date value, use the copyright year if it's there
+      if (thisAM.get(MetadataField.FIELD_DATE) == null) {
+        if (thisAM.getRaw(JatsPublishingSchemaHelper.JATS_date) != null) {
+          thisAM.put(MetadataField.FIELD_DATE, thisAM.getRaw(JatsPublishingSchemaHelper.JATS_date));
+        } else {// last chance
+          thisAM.put(MetadataField.FIELD_DATE, thisAM.getRaw(JatsPublishingSchemaHelper.JATS_edate));
+        }
+      }
+      thisAM.put(MetadataField.FIELD_PUBLISHER,EMH_PUBLISHER_NAME);
+    }
+    
+  }
+}
