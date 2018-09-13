4601c1167c58e25d9a386d0a40ee7ce2548d14e9
Mary-Ellen <maryellen@lockss.org>
2018-09-13 12:20:58 -0700

    Typo. Must clear file first.

--- scripts/tdb/automate_ingest.sh
+++ scripts/tdb/automate_ingest.sh
@@ -8,7 +8,7 @@ echo "###Ready highwire" > $t/tmp_HW  #clear out file.
 
 #Atypon (not T&F), in order by year
 #Ready
-#echo "###Ready Atypon 2019" > $t/tmp_Atypon  #clear out file
+echo "###Ready Atypon 2019" > $t/tmp_Atypon  #clear out file
 #./scripts/tdb/tdbout -Y -a -Q 'plugin ~ "typon" and year is "2019" and (au:hidden[proxy] is not set or au:hidden[proxy] is "")' tdb/clockssingest/*.tdb | shuf >> $t/tmp_Atypon
 echo "###Ready Atypon not 2019" >> $t/tmp_Atypon
 ./scripts/tdb/tdbout -Y -a -Q 'plugin ~ "typon" and year is not "2019" and (au:hidden[proxy] is not set or au:hidden[proxy] is "")' tdb/clockssingest/*.tdb | shuf >> $t/tmp_Atypon
