949e5fabc8eacbb81606b2711b8449fba5b41595
Alexandra Ohlson <aohlson@lockss.org>
2018-10-24 23:20:19 -0700

    Fix missing publication date in sgm extractor

---
+++ plugins/test/src/org/lockss/plugin/clockss/cambridge/TestCambridgeMetadataExtractor.java
@@ -0,0 +1,171 @@
+/*
+ * $Id$
+ */
+/*
+
+/*
+
+ Copyright (c) 2000-2018 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of his software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.clockss.cambridge;
+import java.io.InputStream;
+import java.util.*;
+
+import org.lockss.test.*;
+import org.lockss.util.*;
+import org.lockss.config.*;
+import org.lockss.extractor.*;
+import org.lockss.plugin.*;
+import org.lockss.plugin.clockss.schweizerbart.SchweizerbartXmlMetadataExtractorFactory;
+
+
+public class TestCambridgeMetadataExtractor extends LockssTestCase {
+
+  private static final Logger log = Logger.getLogger(TestCambridgeMetadataExtractor.class);
+
+  private MockLockssDaemon theDaemon;
+  private MockArchivalUnit mau;
+
+  private static String PLUGIN_NAME = "org.lockss.plugin.clockss.cambridge.ClockssCambridgeUniversityPressSourcePlugin";
+  private static String BASE_URL = "http://www.source.org/";
+
+  public void setUp() throws Exception {
+    super.setUp();
+    setUpDiskSpace(); // you need this to have startService work properly...
+
+    theDaemon = getMockLockssDaemon();
+    mau = new MockArchivalUnit();
+
+    theDaemon.getAlertManager();
+    theDaemon.getPluginManager().setLoadablePluginsReady(true);
+    theDaemon.setDaemonInited(true);
+    theDaemon.getPluginManager().startService();
+    theDaemon.getCrawlManager();
+    mau.setConfiguration(auConfig());
+    //log.setLevel("debug3");
+  }
+
+  public void tearDown() throws Exception {
+    theDaemon.stopDaemon();
+    super.tearDown();
+  }
+
+  /**
+   * Configuration method. 
+   * @return
+   */
+  Configuration auConfig() {
+    Configuration conf = ConfigManager.newConfiguration();
+    conf.put("base_url", BASE_URL);
+    conf.put("year", "2016");
+    return conf;
+  }
+  
+
+
+  private static final String realXMLFile = "JatsXmlTest.xml";
+  private static final String realSgmFile = "SgmTest.xml";
+
+  public void testFromJatsPublishingXMLFile() throws Exception {
+    InputStream file_input = null;
+    try {
+      file_input = getResourceAsStream(realXMLFile);
+      String string_input = StringUtil.fromInputStream(file_input);
+      IOUtil.safeClose(file_input);
+
+      CIProperties xmlHeader = new CIProperties();    
+      String xml_url = "http://www.source.com/TestXMLh.xml";
+      String pdfUrl1 = "http://www.source.com/TestXMLa.pdf";
+      xmlHeader.put(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+      MockCachedUrl mcu = mau.addUrl(xml_url, true, true, xmlHeader);
+      // Now add all the pdf files in our AU since we check for them before emitting
+      mau.addUrl(pdfUrl1, true, true, xmlHeader);
+      
+      mcu.setContent(string_input);
+      mcu.setContentSize(string_input.length());
+      mcu.setProperty(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+
+    FileMetadataExtractor me = new  CambridgeXmlMetadataExtractorFactory().createFileMetadataExtractor(MetadataTarget.Any(), "text/xml");
+      FileMetadataListExtractor mle =
+          new FileMetadataListExtractor(me);
+      List<ArticleMetadata> mdlist = mle.extract(MetadataTarget.Any(), mcu);
+      assertNotEmpty(mdlist);
+      assertEquals(1, mdlist.size());
+      
+      // check each returned md against expected values
+      Iterator<ArticleMetadata> mdIt = mdlist.iterator();
+      ArticleMetadata mdRecord = null;
+      while (mdIt.hasNext()) {
+        mdRecord = (ArticleMetadata) mdIt.next();
+        log.debug3(mdRecord.ppString(2));
+      }
+    }finally {
+      IOUtil.safeClose(file_input);
+    }
+
+  }
+
+  public void testFromSgmFile() throws Exception {
+    InputStream file_input = null;
+    try {
+      file_input = getResourceAsStream(realSgmFile);
+      String string_input = StringUtil.fromInputStream(file_input);
+      IOUtil.safeClose(file_input);
+
+      CIProperties xmlHeader = new CIProperties();    
+      String xml_url = "http://www.source.com/TestXMLh.sgm";
+      String pdfUrl1 = "http://www.source.com/TestXMLa.pdf";
+      xmlHeader.put(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+      MockCachedUrl mcu = mau.addUrl(xml_url, true, true, xmlHeader);
+      // Now add all the pdf files in our AU since we check for them before emitting
+      mau.addUrl(pdfUrl1, true, true, xmlHeader);
+      
+      mcu.setContent(string_input);
+      mcu.setContentSize(string_input.length());
+      mcu.setProperty(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+
+    FileMetadataExtractor me = new  CambridgeXmlMetadataExtractorFactory().createFileMetadataExtractor(MetadataTarget.Any(), "text/xml");
+      FileMetadataListExtractor mle =
+          new FileMetadataListExtractor(me);
+      List<ArticleMetadata> mdlist = mle.extract(MetadataTarget.Any(), mcu);
+      assertNotEmpty(mdlist);
+      assertEquals(1, mdlist.size());
+
+      // check each returned md against expected values
+      Iterator<ArticleMetadata> mdIt = mdlist.iterator();
+      ArticleMetadata mdRecord = null;
+      while (mdIt.hasNext()) {
+        mdRecord = (ArticleMetadata) mdIt.next();
+        log.debug3(mdRecord.ppString(2));
+      }
+    }finally {
+      IOUtil.safeClose(file_input);
+    }
+
+  }  
+  
+}
