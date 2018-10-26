cedd8cdd7fa37da682d2d0a4e6764990494f82bd
E Ten Brink <etenbrink@yahoo.com>
2018-10-26 12:10:09 -0700

    Cleanup WS

--- plugins/src/org/lockss/plugin/atypon/BaseAtyponHtmlHashFilterFactory.java
+++ plugins/src/org/lockss/plugin/atypon/BaseAtyponHtmlHashFilterFactory.java
@@ -78,7 +78,7 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
   // usually "PDF Plus (527 KB)" or similar (PDFPlus, PDF-Plus)
   // (?i) makes it case insensitive
   private static final String SIZE_REGEX = "PDF(\\s|-)?(Plus)?\\s?\\(\\s?[0-9]+";
-  private static final Pattern SIZE_PATTERN = Pattern.compile(SIZE_REGEX, Pattern.CASE_INSENSITIVE);  
+  private static final Pattern SIZE_PATTERN = Pattern.compile(SIZE_REGEX, Pattern.CASE_INSENSITIVE);
   protected static final Pattern CITED_BY_PATTERN =
       Pattern.compile("Cited by", Pattern.CASE_INSENSITIVE);
   
@@ -97,12 +97,12 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
     HtmlNodeFilters.tagWithAttribute("div", "id", "header"),
     HtmlNodeFilters.tagWithAttribute("div", "id", "footer"),
     
-    // sections that may show up with this skin - from CRAWL filter   
+    // sections that may show up with this skin - from CRAWL filter
     HtmlNodeFilters.tagWithAttributeRegex("div", "class", "literatumBookIssueNavigation"),
     // http://www.birpublications.org/toc/bjr/88/1052
-    HtmlNodeFilters.tagWithAttributeRegex("div", "class", "literatumMostReadWidget"),    
+    HtmlNodeFilters.tagWithAttributeRegex("div", "class", "literatumMostReadWidget"),
     HtmlNodeFilters.tagWithAttributeRegex("div", "class", "literatumMostCitedWidget"),
-    HtmlNodeFilters.tagWithAttributeRegex("div",  "class","literatumMostRecentWidget"),                                      
+    HtmlNodeFilters.tagWithAttributeRegex("div",  "class","literatumMostRecentWidget"),
     HtmlNodeFilters.tagWithAttributeRegex("div", "class", "literatumListOfIssuesWidget"),
     HtmlNodeFilters.tagWithAttributeRegex("div", "class", "literatumBreadcrumbs"),
     //seen in TandF but likely to spread
@@ -110,8 +110,8 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
     // additional just for hashing - may or may not be necessary
     HtmlNodeFilters.tagWithAttributeRegex("div", "class", "literatumAd"),
     //http://press.endocrine.org/doi/full/10.1210/en.2013-1159
-    HtmlNodeFilters.tagWithAttributeRegex("div", "class", "doubleClickAdWidget"),    
-    HtmlNodeFilters.tagWithAttributeRegex("div", "class", "literatumInstitutionBanner"),    
+    HtmlNodeFilters.tagWithAttributeRegex("div", "class", "doubleClickAdWidget"),
+    HtmlNodeFilters.tagWithAttributeRegex("div", "class", "literatumInstitutionBanner"),
     
     // crossref to site library
     HtmlNodeFilters.tagWithAttribute("a", "class", "sfxLink"),
@@ -155,13 +155,13 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
             } else if (tag instanceof Html && tag.getAttribute("data-request-id") != null) {
               //changeable html attribute data-request-id first seen in BiR, see all html
               tag.removeAttribute("data-request-id");
-            } else if 
+            } else if
             (tag instanceof LinkTag && ((CompositeTag) tag).getChildCount() == 1 &&
             ((CompositeTag) tag).getFirstChild() instanceof Text) {
               if (SIZE_PATTERN.matcher(((CompositeTag) tag).getStringText()).find()) {
                 log.debug3("removing link child text : " + ((CompositeTag) tag).getStringText());
                 ((CompositeTag) tag).removeChild(0);
-              } 
+              }
             }
           }
         });
@@ -172,11 +172,11 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
       }
       return nodeList;
     }
-  };   
+  };
 
   /*
-   * Removes all "id' attributes and/or white space if activated by 
-   * child plugins.  The "id" attribute of various tags <span>, <section> .. 
+   * Removes all "id' attributes and/or white space if activated by
+   * child plugins.  The "id" attribute of various tags <span>, <section> ..
    * can have a gensym. Also removes pdf(plus) file sizes.
    */
   //And while we're visiting the tag, also remove data-request-id from html tag
@@ -191,7 +191,7 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
               tag.removeAttribute("id");
             }
             //changeable html attribute data-request-id first seen in BiR, see all html
-            //<html lang="en" class="pb-page" data-request-id="cc2ac1e1-80e0-472e-acc8-2e97853c3c01" >            
+            //<html lang="en" class="pb-page" data-request-id="cc2ac1e1-80e0-472e-acc8-2e97853c3c01" >
             if (tag instanceof Html && tag.getAttribute("data-request-id") != null) {
               tag.removeAttribute("data-request-id");
             } else if (tag instanceof LinkTag && ((CompositeTag) tag).getChildCount() == 1 &&
@@ -199,7 +199,7 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
               if (SIZE_PATTERN.matcher(((CompositeTag) tag).getStringText()).find()) {
                 log.debug3("removing link child text : " + ((CompositeTag) tag).getStringText());
                 ((CompositeTag) tag).removeChild(0);
-              } 
+              }
             }
           }
         });
@@ -239,7 +239,7 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
    * @param in  Incoming input stream
    * @param encoding  The encoding
    * @param moreNodes An array of NodeFilters to be excluded with atyponBaseFilters
-   */ 
+   */
   public InputStream createFilteredInputStream(ArchivalUnit au,
       InputStream in, String encoding, NodeFilter[] moreNodes) {
 
@@ -249,7 +249,7 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
   /* the shared portion of the filtering
    * pick up the extra nodes from the child if there are any
    * Use the correct xform, depending on the return value of the getter.
-   *  
+   *
    */
   private InputStream doFiltering(InputStream in, String encoding, NodeFilter[] moreNodes, boolean doWS) {
     NodeFilter[] bothFilters = baseAtyponFilters;
@@ -259,8 +259,8 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
 
     // Remove script tags that can confuse the parser with " and ' out of match order (even if escaped)
     in = new BufferedInputStream(new ReaderInputStream(
-        new HtmlTagFilter(FilterUtil.getReader(in, encoding), new TagPair("<script","</script>"))));    
-    
+        new HtmlTagFilter(FilterUtil.getReader(in, encoding), new TagPair("<script","</script>"))));
+
     InputStream combinedFiltered;
     if (doTagIDFiltering()) {
       combinedFiltered = new HtmlFilterInputStream(in, encoding,
@@ -269,22 +269,22 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
       combinedFiltered = new HtmlFilterInputStream(in, encoding,
           new HtmlCompoundTransform(HtmlNodeFilterTransform.exclude(new OrFilter(bothFilters)), xform_spanID));
     }
-    
+
     return commonFiltering(combinedFiltered, encoding, doWS);
   }
 
   private InputStream commonFiltering(InputStream combinedFiltered, String encoding, boolean doWS) {
-    
+
     // a little inefficient but pull out comments after the child nodes are applied
     combinedFiltered = filterComments(combinedFiltered,encoding);
-    
+
     if (!doTagRemovalFiltering() && !doWS && !doHttpsConversion()) {
       // already done, return without converting back to a reader
       return combinedFiltered;
     }
-    
-    /* 
-     * optional additional processing - 
+
+    /*
+     * optional additional processing -
      *    removal of all tags and/or removal of WS & https conversion
      */
     Reader tagFilter;
@@ -300,34 +300,34 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
       tagFilter = new HtmlTagFilter(tagFilter, new TagPair("<", ">"));
     }
     
-    // as Atypon publishers move to https this will support them. 
+    // as Atypon publishers move to https this will support them.
     // It doesn't matter if it changes http to http unnecessarily for hash purposes
     if (doHttpsConversion()) {
       tagFilter = new StringFilter(tagFilter, "http:", "https:");
     }
     
     if (doWS) {
-      // first substitute plain white space for &nbsp;   
+      // first substitute plain white space for &nbsp;
       // add spaces before all "<"
       // consolidate spaces down to 1
       // If the tags were removed above (with space added) it will not find tags
       // to add the space before. This is fine.
       String[][] unifySpaces = new String[][] {
-          // inconsistent use of nbsp v empty space - do this replacement first                                                                        
+          // inconsistent use of nbsp v empty space - do this replacement first
           {"&nbsp;", " "},
           {"<", " <"},
       };
       Reader NBSPFilter = StringFilter.makeNestedFilter(tagFilter,
           unifySpaces, false);
-      return new ReaderInputStream(new WhiteSpaceFilter(NBSPFilter)); 
-    } else { 
-      return new ReaderInputStream(tagFilter); 
+      return new ReaderInputStream(new WhiteSpaceFilter(NBSPFilter));
+    } else {
+      return new ReaderInputStream(tagFilter);
     }
   }
   
   /*
-   * Takes include and exclude nodes as input. Removes all "id' attributes 
-   * and/or white space if activated by child plugins.  The "id" attribute of 
+   * Takes include and exclude nodes as input. Removes all "id' attributes
+   * and/or white space if activated by child plugins.  The "id" attribute of
    * various tags <span>, <section> .. can have a gensym.
    * Also removes pdf(plus) file sizes.
    */
@@ -336,10 +336,10 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
     NodeFilter[] allExcludeNodes = baseAtyponFilters;
     if (excludeNodes == null) {
       throw new NullPointerException("excludeNodes array is null");
-    }  
+    }
     if (includeNodes == null) {
       throw new NullPointerException("includeNodes array is null!");
-    }   
+    }
     // combine baseAtyponFilters and excludeNodes
     allExcludeNodes = addTo(excludeNodes);
     
@@ -354,7 +354,7 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
       combinedFiltered = new HtmlFilterInputStream(in, encoding,
         new HtmlCompoundTransform(
             HtmlNodeFilterTransform.include(new OrFilter(includeNodes)),
-            HtmlNodeFilterTransform.exclude(new OrFilter(allExcludeNodes)), 
+            HtmlNodeFilterTransform.exclude(new OrFilter(allExcludeNodes)),
             xform_allIDs)
         );
     } else {
@@ -439,12 +439,9 @@ public class BaseAtyponHtmlHashFilterFactory implements FilterFactory {
     return false;
   }
   
- 
   public InputStream filterComments(InputStream in,  String encoding) {
     return new HtmlFilterInputStream(in, encoding,
         HtmlNodeFilterTransform.exclude(HtmlNodeFilters.comment()));
-
   }
-
 
 }
