c067c9161666152b43d3e5ce7d54f8785d31eb19
Alexandra Ohlson <aohlson@lockss.org>
2018-12-12 13:15:40 -0800

    prototype placeholder until prerender available in early 2019

---
+++ plugins/src/org/lockss/plugin/usdocspln/gov/govinfo/GovInfoSitemapsArticleIteratorFactory.java
@@ -0,0 +1,125 @@
+/*
+ * $Id$
+ */
+
+/*
+
+Copyright (c) 2000-2016 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.usdocspln.gov.govinfo;
+
+import java.util.*;
+import java.util.regex.*;
+
+import org.lockss.util.*;
+import org.lockss.plugin.*;
+import org.lockss.extractor.*;
+import org.lockss.daemon.PluginException;
+
+public class GovInfoSitemapsArticleIteratorFactory
+  implements ArticleIteratorFactory,
+             ArticleMetadataExtractorFactory {
+  
+  protected static Logger log = 
+    Logger.getLogger(GovInfoSitemapsArticleIteratorFactory.class);
+  
+  /*
+     https://www.govinfo.gov/app/details/DCPD-201800002
+     htps://www.govinfo.gov/content/pkg/DCPD-201800002/html/DCPD-201800002.htm
+     htps://www.govinfo.gov/content/pkg/DCPD-201800002/pdf/DCPD-201800002.pdf
+*/
+  protected static final String ROOT_TEMPLATE =
+    "\"%scontent/pkg/\", base_url";
+
+  protected static final String PATTERN_TEMPLATE =
+    "\"^%scontent/pkg/([^/]+)/(html?|mp3|pdf|xml)/([^/]+)\\.(html?|mp3|pdf|xml)$\", base_url";
+  
+  protected static final Pattern HTML_PATTERN = 
+      Pattern.compile("([^/]+)/html/([^/]+)\\.htm$",
+          Pattern.CASE_INSENSITIVE);
+  
+  protected static final Pattern MP3_PATTERN = 
+      Pattern.compile("([^/]+)/mp3/([^/]+)\\.mp3$",
+          Pattern.CASE_INSENSITIVE);
+  
+  protected static final Pattern PDF_PATTERN = 
+      Pattern.compile("([^/]+)/pdf/([^/]+)\\.pdf$",
+          Pattern.CASE_INSENSITIVE);
+  
+  protected static final Pattern XML_PATTERN = 
+      Pattern.compile("([^/]+)/xml/([^/]+)\\.xml$",
+          Pattern.CASE_INSENSITIVE);
+  
+  protected static final String HTML_REPLACEMENT1 = "$1/html/$2.htm";
+  protected static final String HTML_REPLACEMENT2 = "$1/html/$2.html";
+  protected static final String MP3_REPLACEMENT = "$1/mp3/$2.mp3";
+  protected static final String PDF_REPLACEMENT = "$1/pdf/$2.pdf";
+  protected static final String XML_REPLACEMENT = "$1/xml/$2.xml";
+  
+  protected static final String ROLE_AUDIO_FILE = "AudioFile";
+  
+  public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au,
+                                                      MetadataTarget target)
+      throws PluginException {
+    
+    SubTreeArticleIteratorBuilder builder = new SubTreeArticleIteratorBuilder(au);
+    
+    builder.setSpec(target, ROOT_TEMPLATE, PATTERN_TEMPLATE, Pattern.CASE_INSENSITIVE);
+    
+    // set up html, pdf, xml to be an aspects that will trigger an ArticleFiles
+    builder.addAspect(
+        HTML_PATTERN, Arrays.asList(HTML_REPLACEMENT1, HTML_REPLACEMENT2),
+        ArticleFiles.ROLE_FULL_TEXT_HTML);
+    
+    builder.addAspect(
+        PDF_PATTERN, PDF_REPLACEMENT, 
+        ArticleFiles.ROLE_FULL_TEXT_PDF);
+    
+    builder.addAspect(
+        XML_PATTERN, XML_REPLACEMENT, 
+        ArticleFiles.ROLE_FULL_TEXT_XML);
+    
+    builder.addAspect(
+        MP3_PATTERN, MP3_REPLACEMENT, 
+        ROLE_AUDIO_FILE);
+    
+    // add metadata role from html, xml, or pdf (NOTE: pdf metadata is the access url)
+    builder.setRoleFromOtherRoles(ArticleFiles.ROLE_ARTICLE_METADATA, Arrays.asList(
+        ArticleFiles.ROLE_FULL_TEXT_HTML,
+        ArticleFiles.ROLE_FULL_TEXT_PDF,
+        ArticleFiles.ROLE_FULL_TEXT_XML,
+        ROLE_AUDIO_FILE));
+    
+    return builder.getSubTreeArticleIterator();
+  }
+  
+  
+  public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target)
+      throws PluginException {
+    return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
+  }
+}
---
+++ plugins/src/org/lockss/plugin/usdocspln/gov/govinfo/GovInfoSitemapsLinkExtractorFactory.java
@@ -0,0 +1,203 @@
+/*
+ * $Id$
+ */
+
+/*
+
+Copyright (c) 2000-2016 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.usdocspln.gov.govinfo;
+
+import java.io.*;
+import java.net.*;
+
+import javax.xml.parsers.*;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.*;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.util.*;
+import org.xml.sax.*;
+
+public class GovInfoSitemapsLinkExtractorFactory implements LinkExtractorFactory {
+
+  protected static Logger log = Logger.getLogger(GovInfoSitemapsLinkExtractorFactory.class);
+  
+  protected static class NoNeedToContinueException extends SAXException {
+    
+    public NoNeedToContinueException() {
+      super();
+    }
+    
+  }
+  
+  @Override
+  public LinkExtractor createLinkExtractor(String mimeType) throws PluginException {
+    return new GPOFDSysSitemapsLinkExtractor();
+  }
+  
+  protected static class GPOFDSysSitemapsLinkExtractor implements LinkExtractor {
+    
+    /**
+     * <p>Not thread-safe.</p>
+     */
+    protected static class SitemapsHandler extends org.xml.sax.helpers.DefaultHandler {
+      
+      public InputSource resolveEntity(String publicID, String systemID) 
+        throws SAXException {
+        return new InputSource(new StringReader(""));
+      }
+      
+      protected Callback callback;
+      
+      protected URL baseUrl;
+      
+      protected StringBuilder locBuilder;
+      
+      protected boolean hasStarted;
+      
+      public SitemapsHandler(URL baseUrl,
+                             Callback cb) {
+        this.baseUrl = baseUrl;
+        this.callback = cb;
+        this.locBuilder = null;
+        this.hasStarted = false;
+      }
+      
+      @Override
+      public void characters(char[] ch,
+                             int start,
+                             int length)
+          throws SAXException {
+        super.characters(ch, start, length);
+        if (locBuilder != null) {
+          locBuilder.append(ch, start, length);
+        }
+      }
+      
+      @Override
+      public void startElement(String uri,
+                               String localName,
+                               String qName,
+                               Attributes attributes)
+          throws SAXException {
+        super.startElement(uri, localName, qName, attributes);
+        if (hasStarted) {
+          // Not the top-level element and is a sitemap: look for <loc>
+          if (!"loc".equalsIgnoreCase(qName)) {
+            locBuilder = new StringBuilder();
+          }
+        }
+        else {
+          // Top-level element: bail if not a sitemap
+          if ("urlset".equalsIgnoreCase(qName)) {
+            hasStarted = true;
+          }
+          else {
+            hasStarted = false;
+            throw new NoNeedToContinueException();
+          }
+        }
+      }
+      
+      @Override
+      public void endElement(String uri,
+                             String localName,
+                             String qName)
+          throws SAXException {
+        super.endElement(uri, localName, qName);
+        if ("loc".equalsIgnoreCase(qName)) {
+          String url = locBuilder.toString();
+          locBuilder = null;
+          if (log.isDebug2()) {
+            log.debug2("Found at " + baseUrl.toString() + ": " + url);
+          }
+          try {
+            String resolved = UrlUtil.resolveUri(baseUrl, url);
+            callback.foundLink(resolved);
+          }
+          catch (MalformedURLException mue) {
+            log.warning("Malformed URL at " + baseUrl.toString() + ": " + url, mue);
+          }
+        }
+      }
+      
+      @Override
+      public void endDocument() throws SAXException {
+        super.endDocument();
+        hasStarted = false;
+      }
+      
+    }
+    
+    @Override
+    public void extractUrls(final ArchivalUnit au,
+                            InputStream in,
+                            String encoding,
+                            final String srcUrl,
+                            final Callback cb)
+        throws IOException, PluginException {
+      // Create SAX parser
+      SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+      saxParserFactory.setValidating(false);
+      InputSource inputSource = new InputSource(in);
+      inputSource.setEncoding(encoding);
+      inputSource.setPublicId(srcUrl);
+      SAXParser saxParser = null;
+      try {
+        saxParser = saxParserFactory.newSAXParser();
+      }
+      catch (ParserConfigurationException pce) {
+        throw new IOException("Error setting up SAX parser", pce);
+      }
+      catch (SAXException se) {
+        throw new IOException("Error while parsing", se);
+      }
+      
+      // Parse document; may bail early with custom NoNeedToContinueException
+      try {
+        saxParser.parse(inputSource, new SitemapsHandler(new URL(srcUrl), 
+            new Callback() {
+          @Override
+          public void foundLink(String url) {
+            cb.foundLink(url);
+          }
+      }));
+      }
+      catch (NoNeedToContinueException nntce) {
+        if (log.isDebug2()) {
+          log.debug2("No need to parse " + srcUrl);
+        }
+      }
+      catch (SAXException se) {
+        throw new IOException("Error while parsing", se);
+      }
+    }
+
+  }
+
+}
