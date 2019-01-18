af438fd113f0e0f8eb84c755de7ec3ffed30a91d
Alexandra Ohlson <aohlson@lockss.org>
2019-01-17 17:44:43 -0800

    Update ISSN map with values from IOP

--- plugins/src/org/lockss/plugin/clockss/iop/IopArticleXmlMetadataExtractorFactory.java
+++ plugins/src/org/lockss/plugin/clockss/iop/IopArticleXmlMetadataExtractorFactory.java
@@ -65,69 +65,93 @@ public class IopArticleXmlMetadataExtractorFactory extends SourceXmlMetadataExtr
   private static SourceXmlSchemaHelper ArticlePublishingHelper = null;
   private static final Map<String,String> IOPIssnTitleMap = new HashMap<String,String>();
   static {
-	  IOPIssnTitleMap.put("0295-5075","Europhysics Letters");
-	  IOPIssnTitleMap.put("0021-4922","Japanese Journal of Applied Physics");
-	  IOPIssnTitleMap.put("0264-9381","Classical and Quantum Gravity");
-	  IOPIssnTitleMap.put("0143-0807","European Journal of Physics");
-	  IOPIssnTitleMap.put("0266-5611","Inverse Problems");
-	  IOPIssnTitleMap.put("0960-1317","Journal of Micromechanics and Microengineering");
-	  IOPIssnTitleMap.put("0953-4075","Journal of Physics B: Atomic, Molecular and Optical Physics");
-	  IOPIssnTitleMap.put("0022-3727","Journal of Physics D: Applied Physics");
-	  IOPIssnTitleMap.put("0954-3899","Journal of Physics G: Nuclear and Particle Physics");
-	  IOPIssnTitleMap.put("0953-8984","Journal of Physics: Condensed Matter");
-	  IOPIssnTitleMap.put("0952-4746","Journal of Radiological Protection");
-	  IOPIssnTitleMap.put("0957-0233","Measurement Science and Technology");
-	  IOPIssnTitleMap.put("0965-0393","Modelling and Simulation in Materials Science and Engineering");
-	  IOPIssnTitleMap.put("0957-4484","Nanotechnology");
-	  IOPIssnTitleMap.put("0951-7715","Nonlinearity");
-	  IOPIssnTitleMap.put("0031-9120","Physics Education");
-	  IOPIssnTitleMap.put("0031-9155","Physics in Medicine & Biology");
-	  IOPIssnTitleMap.put("0967-3334","Physiological Measurement");
-	  IOPIssnTitleMap.put("0741-3335","Plasma Physics and Controlled Fusion");
-	  IOPIssnTitleMap.put("0963-0252","Plasma Sources Science and Technology");
-	  IOPIssnTitleMap.put("0034-4885","Reports on Progress in Physics");
-	  IOPIssnTitleMap.put("0268-1242","Semiconductor Science and Technology");
-	  IOPIssnTitleMap.put("0964-1726","Smart Materials and Structures");
-	  IOPIssnTitleMap.put("0953-2048","Superconductor Science and Technology");
-	  IOPIssnTitleMap.put("1478-3975","Physical Biology");
-	  IOPIssnTitleMap.put("1054-660X","Laser Physics");
-	  IOPIssnTitleMap.put("1612-2011","Laser Physics Letters");
-	  IOPIssnTitleMap.put("0026-1394","Metrologia");
-	  IOPIssnTitleMap.put("1741-2560","Journal of Neural Engineering");
-	  IOPIssnTitleMap.put("0256-307X","Chinese Physics Letters");
-	  IOPIssnTitleMap.put("0029-5515","Nuclear Fusion");
-	  IOPIssnTitleMap.put("1742-2132","Journal of Geophysics and Engineering");
-	  IOPIssnTitleMap.put("1742-6588","Journal of Physics: Conference Series");
-	  IOPIssnTitleMap.put("1748-3182","Bioinspiration & Biomimetics");
-	  IOPIssnTitleMap.put("1748-6041","Biomedical Materials");
-	  IOPIssnTitleMap.put("1749-4680","Computational Science & Discovery");
-	  IOPIssnTitleMap.put("1751-8113","Journal of Physics A: Mathematical and Theoretical");
-	  IOPIssnTitleMap.put("1752-7155","Journal of Breath Research");
-	  IOPIssnTitleMap.put("1755-1307","IOP Conference Series: Earth and Environmental Science");
-	  IOPIssnTitleMap.put("1757-8981","IOP Conference Series: Materials Science and Engineering");
-	  IOPIssnTitleMap.put("1758-5082","Biofabrication");
-	  IOPIssnTitleMap.put("0169-5983","Fluid Dynamics Research");
-	  IOPIssnTitleMap.put("1882-0778","Applied Physics Express");
-	  IOPIssnTitleMap.put("2040-8978","Journal of Optics");
-	  IOPIssnTitleMap.put("2043-6262","Advances in Natural Sciences: Nanoscience and Nanotechnology");
-	  IOPIssnTitleMap.put("0031-8949","Physica Scripta");
-	  IOPIssnTitleMap.put("0253-6102","Communications in Theoretical Physics");
-	  IOPIssnTitleMap.put("1009-0630","Plasma Science and Technology");
-	  IOPIssnTitleMap.put("1674-1056","Chinese Physics B");
-	  IOPIssnTitleMap.put("1674-1137","Chinese Physics C");
-	  IOPIssnTitleMap.put("1674-4527","Research in Astronomy and Astrophysics");
-	  IOPIssnTitleMap.put("1674-4926","Journal of Semiconductors");
-	  //using eissn for these ones; issn is the same
-	  IOPIssnTitleMap.put("1367-2630","New Journal of Physics");
-	  IOPIssnTitleMap.put("2050-6120","Methods and Applications in Fluorescence");
-	  IOPIssnTitleMap.put("2051-672X","Surface Topography: Metrology and Properties");
-	  IOPIssnTitleMap.put("2053-1583","2D Materials");
-	  IOPIssnTitleMap.put("2053-1591","Materials Research Express");
-	  IOPIssnTitleMap.put("2053-1613","Translational Materials Research");
-	  IOPIssnTitleMap.put("2057-1739","Convergent Science Physical Oncology");
-	  IOPIssnTitleMap.put("2057-1976","Biomedical Physics & Engineering Express");
-	  IOPIssnTitleMap.put("1748-9326","Environmental Research Letters");
-	  IOPIssnTitleMap.put("1742-5468","Journal of Statistical Mechanics: Theory and Experiment");
+	     IOPIssnTitleMap.put("0004-6256","The Astronomical Journal");
+	     IOPIssnTitleMap.put("0004-6280","Publications of the Astronomical Society of the Pacific");
+	     IOPIssnTitleMap.put("0004-637X","The Astrophysical Journal");
+	     IOPIssnTitleMap.put("0021-4922","Japanese Journal of Applied Physics");
+	     IOPIssnTitleMap.put("0022-3727","Journal of Physics D: Applied Physics");
+	     IOPIssnTitleMap.put("0026-1394","Metrologia ");
+	     IOPIssnTitleMap.put("0029-5515","Nuclear Fusion ");
+	     IOPIssnTitleMap.put("0031-8949","Physica Scripta ");
+	     IOPIssnTitleMap.put("0031-9120","Physics Education");
+	     IOPIssnTitleMap.put("0031-9155","Physics in Medicine & Biology ");
+	     IOPIssnTitleMap.put("0034-4885","Reports on Progress in Physics");
+	     IOPIssnTitleMap.put("0036-021X","Russian Chemical Reviews");
+	     IOPIssnTitleMap.put("0036-0279","Russian Mathematical Surveys ");
+	     IOPIssnTitleMap.put("0067-0049","The Astrophysical Journal Supplement Series");
+	     IOPIssnTitleMap.put("0143-0807","European Journal of Physics ");
+	     IOPIssnTitleMap.put("0169-5983","Fluid Dynamics Research ");
+	     IOPIssnTitleMap.put("0253-6102","Communications in Theoretical Physics ");
+	     IOPIssnTitleMap.put("0256-307X","Chinese Physics Letters ");
+	     IOPIssnTitleMap.put("0264-9381","Classical and Quantum Gravity");
+	     IOPIssnTitleMap.put("0266-5611","Inverse Problems");
+	     IOPIssnTitleMap.put("0268-1242","Semiconductor Science and Technology");
+	     IOPIssnTitleMap.put("0295-5075","Europhysics Letters ");
+	     IOPIssnTitleMap.put("0741-3335","Plasma Physics and Controlled Fusion");
+	     IOPIssnTitleMap.put("0951-7715","Nonlinearity ");
+	     IOPIssnTitleMap.put("0952-4746","Journal of Radiological Protection ");
+	     IOPIssnTitleMap.put("0953-2048","Superconductor Science and Technology");
+	     IOPIssnTitleMap.put("0953-4075","Journal of Physics B: Atomic, Molecular and Optical Physics");
+	     IOPIssnTitleMap.put("0953-8984","Journal of Physics: Condensed Matter");
+	     IOPIssnTitleMap.put("0954-3899","Journal of Physics G: Nuclear and Particle Physics");
+	     IOPIssnTitleMap.put("0957-0233","Measurement Science and Technology");
+	     IOPIssnTitleMap.put("0957-4484","Nanotechnology");
+	     IOPIssnTitleMap.put("0960-1317","Journal of Micromechanics and Microengineering");
+	     IOPIssnTitleMap.put("0963-0252","Plasma Sources Science and Technology");
+	     IOPIssnTitleMap.put("0964-1726","Smart Materials and Structures");
+	     IOPIssnTitleMap.put("0965-0393","Modelling and Simulation in Materials Science and Engineering");
+	     IOPIssnTitleMap.put("0967-3334","Physiological Measurement ");
+	     IOPIssnTitleMap.put("1009-0630","Plasma Science and Technology ");
+	     IOPIssnTitleMap.put("1054-660X","Laser Physics ");
+	     IOPIssnTitleMap.put("1063-7818","Quantum Electronics");
+	     IOPIssnTitleMap.put("1063-7869","Physics-Uspekhi");
+	     IOPIssnTitleMap.put("1064-5616","Sbornik: Mathematics");
+	     IOPIssnTitleMap.put("1064-5632","Izvestiya: Mathematics");
+	     IOPIssnTitleMap.put("1478-3975","Physical Biology");
+	     IOPIssnTitleMap.put("1612-2011","Laser Physics Letters ");
+	     IOPIssnTitleMap.put("1674-1056","Chinese Physics B ");
+	     IOPIssnTitleMap.put("1674-1137","Chinese Physics C ");
+	     IOPIssnTitleMap.put("1674-4527","Research in Astronomy and Astrophysics ");
+	     IOPIssnTitleMap.put("1674-4926","Journal of Semiconductors ");
+	     IOPIssnTitleMap.put("1741-2560","Journal of Neural Engineering");
+	     IOPIssnTitleMap.put("1742-2132","Journal of Geophysics and Engineering ");
+	     IOPIssnTitleMap.put("1742-6588","Journal of Physics: Conference Series");
+	     IOPIssnTitleMap.put("1748-3182","Bioinspiration & Biomimetics");
+	     IOPIssnTitleMap.put("1748-6041","Biomedical Materials");
+	     IOPIssnTitleMap.put("1751-8113","Journal of Physics A: Mathematical and Theoretical");
+	     IOPIssnTitleMap.put("1752-7155","Journal of Breath Research");
+	     IOPIssnTitleMap.put("1755-1307","IOP Conference Series: Earth and Environmental Science");
+	     IOPIssnTitleMap.put("1757-8981","IOP Conference Series: Materials Science and Engineering");
+	     IOPIssnTitleMap.put("1758-5082","Biofabrication");
+	     IOPIssnTitleMap.put("1882-0778","Applied Physics Express");
+	     IOPIssnTitleMap.put("2040-8978","Journal of Optics ");
+	     IOPIssnTitleMap.put("2041-8205","The Astrophysical Journal Letters");
+	     IOPIssnTitleMap.put("2043-6262","Advances in Natural Sciences: Nanoscience and Nanotechnology ");
+	     //EISSN from here down; no print ISSN for title
+	     IOPIssnTitleMap.put("2053-1583","2D Materials");
+	     IOPIssnTitleMap.put("2057-1976","Biomedical Physics & Engineering Express");
+	     IOPIssnTitleMap.put("2057-1739","Convergent Science Physical Oncology");
+	     IOPIssnTitleMap.put("1748-9326","Environmental Research Letters");
+	     IOPIssnTitleMap.put("2516-1075","Electronic Structure");
+	     IOPIssnTitleMap.put("2058-8585","Flexible and Printed Electronics");
+	     IOPIssnTitleMap.put("1475-7516","Journal of Cosmology and Astroparticle Physics ");
+	     IOPIssnTitleMap.put("1748-0221","Journal of Instrumentation ");
+	     IOPIssnTitleMap.put("2399-6528","Journal of Physics Communications");
+	     IOPIssnTitleMap.put("2515-7655","Journal of Physics: Energy");
+	     IOPIssnTitleMap.put("2515-7639","Journal of Physics: Materials");
+	     IOPIssnTitleMap.put("2515-7647","Journal of Physics: Photonics");
+	     IOPIssnTitleMap.put("1742-5468","Journal of Statistical Mechanics: Theory and Experiment ");
+	     IOPIssnTitleMap.put("2050-6120","Methods and Applications in Fluorescence");
+	     IOPIssnTitleMap.put("2399-7532","Multifunctional Materials");
+	     IOPIssnTitleMap.put("2053-1591","Materials Research Express");
+	     IOPIssnTitleMap.put("2399-1984","Nano Futures");
+	     IOPIssnTitleMap.put("1367-2630","New Journal of Physics ");
+	     IOPIssnTitleMap.put("2516-1067","Plasma Research Express");
+	     IOPIssnTitleMap.put("2058-9565","Quantum Science & Technology");
+	     IOPIssnTitleMap.put("2515-5172","AAS Research Notes");
+	     IOPIssnTitleMap.put("2051-672X","Surface Topography: Metrology and Properties");
+	     IOPIssnTitleMap.put("2053-1613","Translational Materials Research");
+	     IOPIssnTitleMap.put("2515-7620","Environmental Research Communications");
   }  
 
 
@@ -203,11 +227,12 @@ public class IopArticleXmlMetadataExtractorFactory extends SourceXmlMetadataExtr
     		if (thisAM.get(MetadataField.FIELD_PUBLICATION_TITLE) == null) {
     			// try to set a publication.title from the IOP issn-title map
     			String mapTitle = IOPIssnTitleMap.get(thisAM.get(MetadataField.FIELD_ISSN)); 
+    			if (mapTitle == null) {
+    				mapTitle = IOPIssnTitleMap.get(thisAM.get(MetadataField.FIELD_EISSN));
+    			}
     			if (mapTitle != null) {
     				thisAM.put(MetadataField.FIELD_PUBLICATION_TITLE, mapTitle);
     			}
-    					
-    					
     		}
     	}
     }
