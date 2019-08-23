b6b1e99473f5e2e071772318ce4b7d13c7d4f0d6
Mary-Ellen <maryellen@lockss.org>
2019-08-22 22:04:33 -0700

    Update for multiple files.

--- scripts/tdb/content_check.sh
+++ scripts/tdb/content_check.sh
@@ -9,13 +9,13 @@ date
 echo "---------------------"
 echo "---------------------"
 echo "*Status typos gln: "
-scripts/tdb/tdbout -t status tdb/prod/*.tdb | sort | uniq -c | grep -vw manifest | grep -vw released | grep -vw expected | grep -vw exists | grep -vw testing | grep -vw wanted | grep -vw ready | grep -vw down | grep -vw superseded | grep -vw doNotProcess | grep -vw notReady | grep -vw doesNotExist
+scripts/tdb/tdbout -t status tdb/prod/ | sort | uniq -c | grep -vw manifest | grep -vw released | grep -vw expected | grep -vw exists | grep -vw testing | grep -vw wanted | grep -vw ready | grep -vw down | grep -vw superseded | grep -vw doNotProcess | grep -vw notReady | grep -vw doesNotExist
 echo "*Status variations clockssingest: "
-scripts/tdb/tdbout -c status,status2 tdb/clockssingest/*.tdb | sort | uniq -c | sort -n | grep -vw "manifest,exists" | grep -vw "crawling,exists" | grep -vw "finished,crawling" | grep -vw "exists,exists" | grep -vw "down,crawling" | grep -vw "doNotProcess,doNotProcess" | grep -vw "expected,exists" | grep -vw "testing,exists" | grep -vw "notReady,exists" | grep -vw "ingNotReady,exists" | grep -vw "zapped,finished" | grep -vw "doesNotExist,doesNotExist"
+scripts/tdb/tdbout -c status,status2 tdb/clockssingest/ | sort | uniq -c | sort -n | grep -vw "manifest,exists" | grep -vw "crawling,exists" | grep -vw "finished,crawling" | grep -vw "exists,exists" | grep -vw "down,crawling" | grep -vw "doNotProcess,doNotProcess" | grep -vw "expected,exists" | grep -vw "testing,exists" | grep -vw "notReady,exists" | grep -vw "ingNotReady,exists" | grep -vw "zapped,finished" | grep -vw "doesNotExist,doesNotExist"
 #echo "*Status typos ibictpln: "
-#scripts/tdb/tdbout -t status tdb/ibictpln/*.tdb | grep -vx manifest | grep -vx released | grep -vx expected | grep -vx exists | grep -vx testing | grep -vx wanted | grep -vx ready | grep -vx down | grep -vx superseded | grep -vx doNotProcess | grep -vx notReady | grep -vx doesNotExist
+#scripts/tdb/tdbout -t status tdb/ibictpln/ | grep -vx manifest | grep -vx released | grep -vx expected | grep -vx exists | grep -vx testing | grep -vx wanted | grep -vx ready | grep -vx down | grep -vx superseded | grep -vx doNotProcess | grep -vx notReady | grep -vx doesNotExist
 #echo "*Status typos coppulpln: "
-#scripts/tdb/tdbout -t status tdb/coppulpln/*.tdb | grep -vx manifest | grep -vx released | grep -vx expected | grep -vx exists | grep -vx testing | grep -vx wanted | grep -vx ready | grep -vx down | grep -vx superseded | grep -vx doNotProcess | grep -vx notReady | grep -vx doesNotExist
+#scripts/tdb/tdbout -t status tdb/coppulpln/ | grep -vx manifest | grep -vx released | grep -vx expected | grep -vx exists | grep -vx testing | grep -vx wanted | grep -vx ready | grep -vx down | grep -vx superseded | grep -vx doNotProcess | grep -vx notReady | grep -vx doesNotExist
 #
 # Find plugins listed in tdb files, that don't exist
 echo "---------------------"
@@ -24,7 +24,7 @@ echo "---------------------"
 # These items should be run from lockss-daemon/plugins/src
 echo "*Plugins that don't exist, but are listed in tdb files: "
 ( cd plugins/src && grep -rl --include "*.xml" "plugin_identifier" * | sed 's/\(.*\).xml/\1/' | sort -u ) > $tpath/ab.txt
-scripts/tdb/tdbout -t plugin tdb/*/*.tdb | sort -u | sed 's/\./\//g' > $tpath/ac.txt
+scripts/tdb/tdbout -t plugin tdb/*/ | sort -u | sed 's/\./\//g' > $tpath/ac.txt
 #plugins that have no AUs.
 #diff $tpath/ab.txt $tpath/ac.txt | grep "^< "     
 #plugins that don't exist, but are listed in tdb files
@@ -35,15 +35,15 @@ echo " "
 echo "---------------------"
 echo "---------------------"
 echo "reingest: 1:8082, 2:8085, 3:8083, 4:8082, 5:8082"
-scripts/tdb/tdbout -F -t "au:hidden[proxy]" -Q 'status2 is "manifest"' tdb/clockssingest/*.tdb | sort | uniq -c
+scripts/tdb/tdbout -F -t "au:hidden[proxy]" -Q 'status2 is "manifest"' tdb/clockssingest/ | sort | uniq -c
 echo "No reingest set."
-scripts/tdb/tdbout -F -t "publisher,title" -Q 'status2 is "manifest" and au:hidden[proxy] is ""' tdb/clockssingest/*.tdb | sort | uniq -c
+scripts/tdb/tdbout -F -t "publisher,title" -Q 'status2 is "manifest" and au:hidden[proxy] is ""' tdb/clockssingest/ | sort | uniq -c
 echo " "
 #
 # Find duplicate auids in the gln title database
 echo "---------------------"
 echo "---------------------"
-scripts/tdb/tdbout -AXEa tdb/prod/*.tdb | sort > $tpath/allAUs
+scripts/tdb/tdbout -AXEa tdb/prod/ | sort > $tpath/allAUs
 uniq $tpath/allAUs > $tpath/dedupedAUs
 allAUs=`cat $tpath/allAUs | wc -l`
 uniqAUs=`cat $tpath/dedupedAUs | wc -l`
@@ -53,7 +53,7 @@ diff $tpath/allAUs $tpath/dedupedAUs | grep "<" | sed s/..//
 #
 # Find duplicate name/plugin pairs in the gln title database
 echo "---------------------"
-scripts/tdb/tdbout -AXE -c plugin,name tdb/prod/*.tdb | sort > $tpath/allAUs
+scripts/tdb/tdbout -AXE -c plugin,name tdb/prod/ | sort > $tpath/allAUs
 uniq $tpath/allAUs > $tpath/dedupedAUs
 allAUs=`cat $tpath/allAUs | wc -l`
 uniqAUs=`cat $tpath/dedupedAUs | wc -l`
@@ -64,7 +64,7 @@ diff $tpath/allAUs $tpath/dedupedAUs | grep "<" | sed s/..//
 # Find duplicate released names in the gln title database
 echo "---------------------"
 echo "GLN. Duplicate Released Names. Commented out."
-#scripts/tdb/tdbout -P -c name tdb/prod/*.tdb | sort > $tpath/allAUs
+#scripts/tdb/tdbout -P -c name tdb/prod/ | sort > $tpath/allAUs
 #uniq $tpath/allAUs > $tpath/dedupedAUs
 #allAUs=`cat $tpath/allAUs | wc -l`
 #uniqAUs=`cat $tpath/dedupedAUs | wc -l`
@@ -74,13 +74,13 @@ echo "GLN. Duplicate Released Names. Commented out."
 #
 # Find number of AUs ready for release in the prod title database
 echo "----------------------"
-./scripts/tdb/tdbout -Y -t status tdb/prod/*.tdb | sort | uniq -c
+./scripts/tdb/tdbout -Y -t status tdb/prod/ | sort | uniq -c
 echo " "
 #
 # Find duplicate auids in the clockss title database
 echo "---------------------"
 echo "---------------------"
-scripts/tdb/tdbout -AXEa tdb/clockssingest/*.tdb | sort > $tpath/allAUs
+scripts/tdb/tdbout -AXEa tdb/clockssingest/ | sort > $tpath/allAUs
 uniq $tpath/allAUs > $tpath/dedupedAUs
 allAUs=`cat $tpath/allAUs | wc -l`
 uniqAUs=`cat $tpath/dedupedAUs | wc -l`
@@ -90,7 +90,7 @@ diff $tpath/allAUs $tpath/dedupedAUs | grep "<" | sed s/..//
 #
 # Find duplicate name/plugin pairs in the clockss title database
 echo "---------------------"
-scripts/tdb/tdbout -AXE -c plugin,name tdb/clockssingest/*.tdb | sort > $tpath/allAUs
+scripts/tdb/tdbout -AXE -c plugin,name tdb/clockssingest/ | sort > $tpath/allAUs
 uniq $tpath/allAUs > $tpath/dedupedAUs
 allAUs=`cat $tpath/allAUs | wc -l`
 uniqAUs=`cat $tpath/dedupedAUs | wc -l`
@@ -101,7 +101,7 @@ diff $tpath/allAUs $tpath/dedupedAUs | grep "<" | sed s/..//
 # Find duplicate released names in the clockss title database
 echo "---------------------"
 echo "Clockss. Duplicate Released Names. Commented out."
-#scripts/tdb/tdbout -PCZI -c name tdb/clockssingest/*.tdb | sort > $tpath/allAUs
+#scripts/tdb/tdbout -PCZI -c name tdb/clockssingest/ | sort > $tpath/allAUs
 #uniq $tpath/allAUs > $tpath/dedupedAUs
 #allAUs=`cat $tpath/allAUs | wc -l`
 #uniqAUs=`cat $tpath/dedupedAUs | wc -l`
@@ -111,13 +111,13 @@ echo "Clockss. Duplicate Released Names. Commented out."
 #
 # Find number of AUs ready for release in the clockssingest title database
 echo "----------------------"
-./scripts/tdb/tdbout -Y -t status tdb/clockssingest/*.tdb | sort | uniq -c
+./scripts/tdb/tdbout -Y -t status tdb/clockssingest/ | sort | uniq -c
 echo " "
 #
 # Find duplicate auids in the ibictpln title database
 #echo "---------------------"
 #echo "---------------------"
-#scripts/tdb/tdbout -AXEa tdb/ibictpln/*.tdb | sort > $tpath/allAUs
+#scripts/tdb/tdbout -AXEa tdb/ibictpln/ | sort > $tpath/allAUs
 #uniq $tpath/allAUs > $tpath/dedupedAUs
 #allAUs=`cat $tpath/allAUs | wc -l`
 #uniqAUs=`cat $tpath/dedupedAUs | wc -l`
@@ -127,7 +127,7 @@ echo " "
 #
 # Find duplicate name/plugin pairs in the ibictpln title database
 #echo "---------------------"
-#scripts/tdb/tdbout -AXE -c plugin,name tdb/ibictpln/*.tdb | sort > $tpath/allAUs
+#scripts/tdb/tdbout -AXE -c plugin,name tdb/ibictpln/ | sort > $tpath/allAUs
 #uniq $tpath/allAUs > $tpath/dedupedAUs
 #allAUs=`cat $tpath/allAUs | wc -l`
 #uniqAUs=`cat $tpath/dedupedAUs | wc -l`
@@ -137,13 +137,13 @@ echo " "
 #
 # Find number of AUs ready for release in the ibictpln title database
 #echo "----------------------"
-#./scripts/tdb/tdbout -Y -t status tdb/ibictpln/*.tdb | sort | uniq -c
+#./scripts/tdb/tdbout -Y -t status tdb/ibictpln/ | sort | uniq -c
 #echo " "
 #
 # Find duplicate auids in the whole database. Not exists or expected
 #echo "---------------------"
 #echo "---------------------"
-#scripts/tdb/tdbout -Aa tdb/*/*.tdb | sort > $tpath/allAUs
+#scripts/tdb/tdbout -Aa tdb/*/ | sort > $tpath/allAUs
 #uniq $tpath/allAUs > $tpath/dedupedAUs
 #allAUs=`cat $tpath/allAUs | wc -l`
 #uniqAUs=`cat $tpath/dedupedAUs | wc -l`
@@ -157,8 +157,8 @@ echo " "
 #echo "---------------------"
 #echo "---------------------"
 #echo "GLN. HighWire Dupe AUs across plugins"
-#scripts/tdb/tdbout -Q 'plugin ~ "highwire"' -URD -t param[base_url],param[volume_name],param[volume],status,publisher tdb/prod/*.tdb | perl -pe 's/\t+/ /g' | sort -k 1,2 > $tpath/HW_g_all
-#scripts/tdb/tdbout -Q 'plugin ~ "highwire"' -URD -t param[base_url],param[volume_name],param[volume],status,publisher tdb/prod/*.tdb | perl -pe 's/\t+/ /g' | sort -k 1,2 -u > $tpath/HW_g_dedupe
+#scripts/tdb/tdbout -Q 'plugin ~ "highwire"' -URD -t param[base_url],param[volume_name],param[volume],status,publisher tdb/prod/ | perl -pe 's/\t+/ /g' | sort -k 1,2 > $tpath/HW_g_all
+#scripts/tdb/tdbout -Q 'plugin ~ "highwire"' -URD -t param[base_url],param[volume_name],param[volume],status,publisher tdb/prod/ | perl -pe 's/\t+/ /g' | sort -k 1,2 -u > $tpath/HW_g_dedupe
 #cat $tpath/HW_g_all | wc -l
 #cat $tpath/HW_g_dedupe | wc -l
 #diff $tpath/HW_g_all $tpath/HW_g_dedupe
@@ -168,8 +168,8 @@ echo " "
 #echo "---------------------"
 #echo "---------------------"
 #echo "CLOCKSS. HighWire Dupe AUs across plugins"
-#scripts/tdb/tdbout -Q 'plugin ~ "highwire"' -URD -t param[base_url],param[volume_name],param[volume],status,publisher tdb/clockssingest/*.tdb | perl -pe 's/\t+/ /g' | sort -k 1,2 > $tpath/HW_c_all
-#scripts/tdb/tdbout -Q 'plugin ~ "highwire"' -URD -t param[base_url],param[volume_name],param[volume],status,publisher tdb/clockssingest/*.tdb | perl -pe 's/\t+/ /g' | sort -k 1,2 -u > $tpath/HW_c_dedupe
+#scripts/tdb/tdbout -Q 'plugin ~ "highwire"' -URD -t param[base_url],param[volume_name],param[volume],status,publisher tdb/clockssingest/ | perl -pe 's/\t+/ /g' | sort -k 1,2 > $tpath/HW_c_all
+#scripts/tdb/tdbout -Q 'plugin ~ "highwire"' -URD -t param[base_url],param[volume_name],param[volume],status,publisher tdb/clockssingest/ | perl -pe 's/\t+/ /g' | sort -k 1,2 -u > $tpath/HW_c_dedupe
 #diff $tpath/HW_c_all $tpath/HW_c_dedupe | grep "< " | sort
 #diff $tpath/HW_c_all $tpath/HW_c_dedupe | grep "< " | wc -l
 #echo "expect 89"
@@ -179,35 +179,35 @@ echo " "
 echo "---------------------"
 echo "---------------------"
 echo "GLN. ISSN issues"
-scripts/tdb/tdbout -t publisher,title,issn,eissn tdb/prod/*.tdb | sed 's/\t\(.*\) & /\t\1 and /' | sed 's/\tThe /\t/' | sort -u > $tpath/issn
+scripts/tdb/tdbout -t publisher,title,issn,eissn tdb/prod/ | sed 's/\t\(.*\) & /\t\1 and /' | sed 's/\tThe /\t/' | sort -u > $tpath/issn
 scripts/tdb/scrub_table.pl $tpath/issn
 #
 # Find issn problems in clockss title database
 echo "---------------------"
 echo "---------------------"
 echo "CLOCKSS. ISSN issues"
-scripts/tdb/tdbout -t publisher,title,issn,eissn tdb/clockssingest/*.tdb | sed 's/\t\(.*\) & /\t\1 and /' | sed 's/\tThe /\t/' | sort -u > $tpath/issn
+scripts/tdb/tdbout -t publisher,title,issn,eissn tdb/clockssingest/ | sed 's/\t\(.*\) & /\t\1 and /' | sed 's/\tThe /\t/' | sort -u > $tpath/issn
 scripts/tdb/scrub_table.pl $tpath/issn
 #
 # Find issn problems in ibictpln title database
 #echo "---------------------"
 #echo "---------------------"
 #echo "IBICT. ISSN issues"
-#scripts/tdb/tdbout -t publisher,title,issn,eissn tdb/ibictpln/*.tdb | sort -u > $tpath/issn
+#scripts/tdb/tdbout -t publisher,title,issn,eissn tdb/ibictpln/ | sort -u > $tpath/issn
 #scripts/tdb/scrub_table.pl $tpath/issn
 #
 # Find Muse titles that don't have attr[journal_id]
 #echo "---------------------"
 #echo "---------------------"
 #echo "GLN. Muse. Titles missing journal_id"
-#scripts/tdb/tdbout -MTNYP -t publisher,param[journal_dir] -Q '(plugin ~ "ProjectMusePlugin" and (attr[journal_id] is not set or attr[journal_id] is ""))' tdb/prod/*.tdb | sort -u
+#scripts/tdb/tdbout -MTNYP -t publisher,param[journal_dir] -Q '(plugin ~ "ProjectMusePlugin" and (attr[journal_id] is not set or attr[journal_id] is ""))' tdb/prod/ | sort -u
 #
 #### Find Titles that don't have AUs
 ###echo "---------------------"
 ###echo "---------------------"
 ###echo "GLN. Titles with no AUs"
-###scripts/tdb/tdbout -j tdb/prod/*.tdb | sort -u > $tpath/AllTitles.txt
-###scripts/tdb/tdbout --any-and-all -c publisher,title,issn,eissn tdb/prod/*.tdb | sort -u > $tpath/TitlesWAUs.txt
+###scripts/tdb/tdbout -j tdb/prod/ | sort -u > $tpath/AllTitles.txt
+###scripts/tdb/tdbout --any-and-all -c publisher,title,issn,eissn tdb/prod/ | sort -u > $tpath/TitlesWAUs.txt
 ###echo "Total Num Titles with no AUs"
 ###diff $tpath/AllTitles.txt $tpath/TitlesWAUs.txt | grep "< " | wc -l
 ###echo "All titles"
@@ -218,8 +218,8 @@ scripts/tdb/scrub_table.pl $tpath/issn
 ###echo "---------------------"
 ###echo "---------------------"
 ###echo "CLOCKSS. Titles with no AUs"
-###scripts/tdb/tdbout -j tdb/clockssingest/*.tdb | sort -u > $tpath/AllTitlesC.txt
-###scripts/tdb/tdbout --any-and-all -c publisher,title,issn,eissn tdb/clockssingest/*.tdb | sort -u > $tpath/TitlesWAUsC.txt
+###scripts/tdb/tdbout -j tdb/clockssingest/ | sort -u > $tpath/AllTitlesC.txt
+###scripts/tdb/tdbout --any-and-all -c publisher,title,issn,eissn tdb/clockssingest/ | sort -u > $tpath/TitlesWAUsC.txt
 ###echo "Total Num Titles with no AUs"
 ###   #diff $tpath/AllTitlesC.txt $tpath/TitlesWAUsC.txt | grep "< " | wc -l
 ###   #echo "Not incl Springer SBM, AIAA, Annual Reviews, or Medknow"
@@ -230,7 +230,7 @@ scripts/tdb/scrub_table.pl $tpath/issn
 ###echo "---------------------"
 echo "---------------------"
 echo "Missing Slashes"
-grep "param\[base_url\]" tdb/*/*.tdb | grep "http.*://" | grep -v "/\s*$" | grep -v ":\s*#" | grep -v "\/\s*#"
+grep "param\[base_url\]" tdb/*/ | grep "http.*://" | grep -v "/\s*$" | grep -v ":\s*#" | grep -v "\/\s*#"
 echo "---------------------"
 echo "---------------------"
 
