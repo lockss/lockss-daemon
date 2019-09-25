327b8d1dbf131684157a42046ba5d5ab68d46cb6
feili18 <feili18@stanford.edu>
2019-09-25 14:24:59 -0700

    initial commit

---
+++ plugins/src/org/lockss/plugin/innovativemedicalresearch/InnovativeMedicalResearchJatsSourceZipXmlArticleIteratorFactory.java
@@ -0,0 +1,84 @@
+package org.lockss.plugin.innovativemedicalresearch;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.ArticleMetadataExtractor;
+import org.lockss.extractor.ArticleMetadataExtractorFactory;
+import org.lockss.extractor.BaseArticleMetadataExtractor;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.plugin.ArticleFiles;
+import org.lockss.plugin.ArticleIteratorFactory;
+import org.lockss.plugin.SubTreeArticleIteratorBuilder;
+import org.lockss.util.Logger;
+
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
+public class InnovativeMedicalResearchJatsSourceZipXmlArticleIteratorFactory implements ArticleIteratorFactory, ArticleMetadataExtractorFactory {
+
+    protected static Logger log = Logger.getLogger(InnovativeMedicalResearchJatsSourceZipXmlArticleIteratorFactory.class);
+
+    protected static final String ALL_ZIP_XML_PATTERN_TEMPLATE =
+            "\"%s[^/]+/.*\\.zip!/.*\\.xml$\", base_url";
+
+    // Be sure to exclude all nested archives in case supplemental data is provided this way
+    protected static final Pattern SUB_NESTED_ARCHIVE_PATTERN =
+            Pattern.compile(".*/[^/]+\\.zip!/.+\\.(zip|tar|gz|tgz|tar\\.gz)$",
+                    Pattern.CASE_INSENSITIVE);
+
+    protected Pattern getExcludeSubTreePattern() {
+        return SUB_NESTED_ARCHIVE_PATTERN;
+    }
+
+    protected String getIncludePatternTemplate() {
+        return ALL_ZIP_XML_PATTERN_TEMPLATE;
+    }
+
+    //Their folder name combines uppercase and lowercase
+    protected static final Pattern PDF_PATTERN = Pattern.compile("/([^/]+)\\.pdf$");
+    protected static final String PDF_REPLACEMENT = "/$1.pdf";
+
+    protected static final Pattern XML_PATTERN = Pattern.compile("/([^/]+)\\.xml$");
+    protected static final String XML_REPLACEMENT = "/$1.xml";
+
+
+    @Override
+    public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au,
+                                                        MetadataTarget target)
+            throws PluginException {
+        SubTreeArticleIteratorBuilder builder = new SubTreeArticleIteratorBuilder(au);
+
+        builder.setSpec(builder.newSpec()
+                .setTarget(target)
+                .setPatternTemplate(getIncludePatternTemplate())
+                .setExcludeSubTreePattern(getExcludeSubTreePattern())
+                .setVisitArchiveMembers(getIsArchive()));
+
+        builder.addAspect(PDF_PATTERN,
+                PDF_REPLACEMENT,
+                ArticleFiles.ROLE_FULL_TEXT_PDF);
+
+
+        builder.addAspect(XML_PATTERN,
+                XML_REPLACEMENT,
+                ArticleFiles.ROLE_ARTICLE_METADATA);
+
+        builder.setFullTextFromRoles(ArticleFiles.ROLE_FULL_TEXT_PDF);
+        builder.setFullTextFromRoles(ArticleFiles.ROLE_ARTICLE_METADATA, ArticleFiles.ROLE_ARTICLE_METADATA);
+
+        return builder.getSubTreeArticleIterator();
+    }
+
+    // NOTE - for a child to create their own version of this
+    // indicates if the iterator should descend in to archives (for tar/zip deliveries)
+    protected boolean getIsArchive() {
+        return true;
+    }
+
+    @Override
+    public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target)
+            throws PluginException {
+        return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
+    }
+
+}
