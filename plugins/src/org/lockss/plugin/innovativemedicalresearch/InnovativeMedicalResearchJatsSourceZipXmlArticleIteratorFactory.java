f82bf2d33a87157cff8e106ee3b93c654dc9dfd9
feili18 <feili18@stanford.edu>
2019-09-26 09:29:49 -0700

    use common pattern to catch both pdf and xml string

--- plugins/src/org/lockss/plugin/innovativemedicalresearch/InnovativeMedicalResearchJatsSourceZipXmlArticleIteratorFactory.java
+++ plugins/src/org/lockss/plugin/innovativemedicalresearch/InnovativeMedicalResearchJatsSourceZipXmlArticleIteratorFactory.java
@@ -9,6 +9,7 @@ import org.lockss.plugin.ArchivalUnit;
 import org.lockss.plugin.ArticleFiles;
 import org.lockss.plugin.ArticleIteratorFactory;
 import org.lockss.plugin.SubTreeArticleIteratorBuilder;
+import org.lockss.plugin.clockss.JatsPublishingSchemaHelper;
 import org.lockss.util.Logger;
 
 import java.util.Iterator;
@@ -18,28 +19,22 @@ public class InnovativeMedicalResearchJatsSourceZipXmlArticleIteratorFactory imp
 
     protected static Logger log = Logger.getLogger(InnovativeMedicalResearchJatsSourceZipXmlArticleIteratorFactory.class);
 
+    //http://content5.lockss.org/sourcefiles/imrpress-released/2019/JMCM2018-Volume%201%20Issue%202.zip!/JMCM2018-Volume 1 Issue 2/2617-5282-2018-2/2617-5282-1-2-107.xml
+    //http://content5.lockss.org/sourcefiles/imrpress-released/2019/JMCM2018-Volume%201%20Issue%202.zip!/JMCM2018-Volume 1 Issue 2/2617-5282-2018-2/1545826577946-771105881.pdf
+    //Their folder name combines uppercase and lowercase
     protected static final String ALL_ZIP_XML_PATTERN_TEMPLATE =
-            "\"%s[^/]+/.*\\.zip!/.*\\.xml$\", base_url";
+            "\"%s%s/.*\\.zip!/([^/]+)/([^/]+)/([^/]+)$\", base_url, directory";
 
     // Be sure to exclude all nested archives in case supplemental data is provided this way
     protected static final Pattern SUB_NESTED_ARCHIVE_PATTERN =
             Pattern.compile(".*/[^/]+\\.zip!/.+\\.(zip|tar|gz|tgz|tar\\.gz)$",
                     Pattern.CASE_INSENSITIVE);
 
-    protected Pattern getExcludeSubTreePattern() {
-        return SUB_NESTED_ARCHIVE_PATTERN;
-    }
+    protected static final String COMMON_PATTERN_STRING = "/([^/]+)/([^/]+)/((\\d+-\\d+\\.pdf)|(\\d+\\-\\d+\\-[\\d\\-]+))\\.(?:xml|pdf)$";
+    protected static final Pattern COMMON_PATTERN = Pattern.compile(COMMON_PATTERN_STRING);
 
-    protected String getIncludePatternTemplate() {
-        return ALL_ZIP_XML_PATTERN_TEMPLATE;
-    }
-
-    //Their folder name combines uppercase and lowercase
-    protected static final Pattern PDF_PATTERN = Pattern.compile("/([^/]+)\\.pdf$");
-    protected static final String PDF_REPLACEMENT = "/$1.pdf";
-
-    protected static final Pattern XML_PATTERN = Pattern.compile("/([^/]+)\\.xml$");
-    protected static final String XML_REPLACEMENT = "/$1.xml";
+    protected static final String PDF_REPLACEMENT = "/$1/$2/$3.pdf";
+    protected static final String XML_REPLACEMENT = "/$1/$2/$3.xml";
 
 
     @Override
@@ -54,19 +49,26 @@ public class InnovativeMedicalResearchJatsSourceZipXmlArticleIteratorFactory imp
                 .setExcludeSubTreePattern(getExcludeSubTreePattern())
                 .setVisitArchiveMembers(getIsArchive()));
 
-        builder.addAspect(PDF_PATTERN,
+
+        builder.addAspect(COMMON_PATTERN,
                 PDF_REPLACEMENT,
                 ArticleFiles.ROLE_FULL_TEXT_PDF);
 
-
-        builder.addAspect(XML_PATTERN,
+        builder.addAspect(COMMON_PATTERN,
                 XML_REPLACEMENT,
                 ArticleFiles.ROLE_ARTICLE_METADATA);
 
         builder.setFullTextFromRoles(ArticleFiles.ROLE_FULL_TEXT_PDF);
-        builder.setFullTextFromRoles(ArticleFiles.ROLE_ARTICLE_METADATA, ArticleFiles.ROLE_ARTICLE_METADATA);
 
         return builder.getSubTreeArticleIterator();
+    }
+
+    protected Pattern getExcludeSubTreePattern() {
+        return SUB_NESTED_ARCHIVE_PATTERN;
+    }
+
+    protected String getIncludePatternTemplate() {
+        return ALL_ZIP_XML_PATTERN_TEMPLATE;
     }
 
     // NOTE - for a child to create their own version of this
