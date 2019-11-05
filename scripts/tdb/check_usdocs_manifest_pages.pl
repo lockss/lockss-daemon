9baab7fae8e733ce3ca59da75823093a050ec3a0
Mary-Ellen <maryellen@lockss.org>
2019-11-04 23:02:52 -0800

    Improve USDocs tdb files.

--- scripts/tdb/check_usdocs_manifest_pages.pl
+++ scripts/tdb/check_usdocs_manifest_pages.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+# This script scrapes the usdocs website to create a complete list of all the available AUs, not including the federal courts
 
 use URI::Escape;
 use Getopt::Long;
@@ -6,6 +7,7 @@ use LWP::UserAgent;
 use HTTP::Request;
 use HTTP::Cookies;
 use HTML::Entities;
+use LWP::Protocol::https;
 use utf8;
 #use encoding 'utf8';
 use Encode qw(decode encode);
@@ -19,7 +21,6 @@ my $ua = LWP::UserAgent->new;
 # Cookies. Don't save cookies from run to run
     $ua->cookie_jar(HTTP::Cookies->new());
 
-
 #tdbout -Dc param[collection_id],year tdb/usdocspln/united_states_government_printing_office.fdsys.tdb > tmp/list1a
 #tdbout -MTYRc param[collection_id],year tdb/usdocspln/united_states_government_printing_office.tdb >> tmp/list1a
 #cat tmp/list1a | sort  > tmp/list1b
@@ -32,58 +33,58 @@ my $ua = LWP::UserAgent->new;
 $url = sprintf("https://www.govinfo.gov/sitemaps");
 my @collection_list=();
 #$man_url=uri_unescape($url);
-printf("%s\n",$url);
+#printf("%s\n",$url);
 my $req = HTTP::Request->new(GET, $url);
 my $resp = $ua->request($req);
-    if ($resp->is_success) {
-    	printf("Success\n");
-      my $man_contents = $resp->content;
-      #printf("$man_contents\n");
-      if ($req->url ne $resp->request->uri) {
-        printf("Redirected from %s\n", $url);
-      } elsif (defined($man_contents) && ($man_contents =~ m/\/sitemap\/.*_sitemap_index.xml/)) {
-      	printf("Not redirected and has a match.\n");
-      	#while (my $line = <$man_contents>){
-      		#printf($line);
-      	#}
-        my $ln = 0;
-        foreach my $line (split(/\n/m, $man_contents)) {
-        	#++$ln;
-        	#print $ln . ": " . $line . "\n";
-          if ($line =~ m#sitemap/([^/]+)_sitemap_index\.xml#) {
-            print "$1\n";
-            push(@collection_list,$1);
-          } 
-        }   
-        foreach my $collection (@collection_list) {
-      		#fetch the associated sitemap which has all the years.
-      		#collect the list of all the urls, and collect all the years.
-      		#output collection_id,year
-      		$url2 = sprintf("https://www.govinfo.gov/sitemap/%s_sitemap_index.xml", $collection);
-      		my @year_list=();
-      		printf("%s\n",$url2);
-      		my $req2 = HTTP::Request->new(GET, $url2);
-      		my $resp2 = $ua->request($req2);
-          if ($resp2->is_success) {
-          	printf("Success2\n");
-          	my $man_contents = $resp2->request->uri) {
-          		printf("Redirected from %s\n", $url);
-          	} elseif (defined($man_contents) && ($man_contents =~ m//)){
-          		printf("Not redirected and has a match.\n");
-          		
-          	}
-          }      		
-        }
-#          #collect all collection names from that page that are in urls like: /sitemap/BILLS_sitemap_index.xml
+if ($resp->is_success) {
+  #printf("Success\n");
+  my $man_contents = $resp->content;
+  #printf("$man_contents\n");
+  if ($req->url ne $resp->request->uri) {
+    printf("Redirected from %s\n", $url);
+  } elsif (defined($man_contents) && ($man_contents =~ m/\/sitemap\/.*_sitemap_index.xml/)) {
+    #printf("Not redirected and has a match.\n");
+        #my $ln = 0;
+    foreach my $line (split(/\n/m, $man_contents)) {
+      #++$ln;
+      #print $ln . ": " . $line . "\n";
+      if ($line =~ m#sitemap/([^/]+)_sitemap_index\.xml#) {
+        #print "$1\n";
+        push(@collection_list,$1);
       }
-    } else {
-      printf("--REQ_FAIL--" . $resp->code() . " " . $resp->message());
+    } 
+  }   
+} else {
+  printf("--REQ_FAIL--" . $resp->code() . " " . $resp->message());
+}
+#exit(0)
+
+foreach my $collection (@collection_list) {
+  #fetch the associated sitemap which has all the years.
+  #collect the list of all the urls, and collect all the years.
+  #output collection_id,year
+  $url2 = sprintf("https://www.govinfo.gov/sitemap/%s_sitemap_index.xml", $collection);
+  #printf("$url2\n");
+  my @year_list=();
+  #printf("%s\n",$url2);
+  my $req2 = HTTP::Request->new(GET, $url2);
+  my $resp2 = $ua->request($req2);
+  if ($resp2->is_success) {
+    #printf("Success2\n");
+    my $man_contents2 = $resp2->content;
+    if ($req2->url ne $resp2->request->uri) {
+      printf("Redirected from %s\n", $url2);
+    } elsif (defined($man_contents2) && ($man_contents2 =~ m/a/)){
+      #printf("Not redirected and has a match.\n");
+      #https://www.govinfo.gov/sitemap/CRPT_2001_sitemap.xml
+      foreach my $line2 (split(/\n/m, $man_contents2)) {
+        if ($line2 =~ m#sitemap/${collection}_(\d+)_sitemap\.xml#) {
+          printf("%s,$1\n", $collection);
+        }
+      }
     }
-#
-#
-#
-#Then fetch all those pages and find all the urls that are like this:
-#https://www.govinfo.gov/sitemap/BILLS_2001_sitemap.xml
+  }      		
+}
 #
 #Then output the collection name (e.g. BILLS) and year (e.g. 2001), comma seperated
 #sort, output list2
---
+++ scripts/tdb/check_usdocs_manifest_pages.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# Script that creates a list of auids that are ready to be pushed to the gln, based on clockss test results
+#
+
+tpath="/home/$LOGNAME/tmp"
+#mkdir -p $tpath
+
+tdbout -Dc param[collection_id],param[year] tdb/usdocspln/united_states_government_printing_office.fdsys.tdb > $tpath/list1a
+tdbout -EMTYRc param[collection_id],param[year] tdb/usdocspln/united_states_government_printing_office.tdb >> $tpath/list1a
+tdbout -EMTYRc param[collection_id],param[year] tdb/usdocspln/united_states_government_printing_office.tdb | sort > $tpath/list1d
+cat $tpath/list1a | sort  > $tpath/list1b
+cat $tpath/list1b | uniq > $tpath/list1c
+echo "Duplicate AUs in previous and new tdb files."
+comm -23 $tpath/list1b $tpath/list1c
+./scripts/tdb/check_usdocs_manifest_pages.pl | sort > $tpath/list2a
+echo "AUs missing from the tdb files."
+comm -13 $tpath/list1c $tpath/list2a
+echo "Extra AUs in the tdb files."
+comm -23 $tpath/list1d $tpath/list2a
+exit 0
+
