a6e1d8ad3b8ca5591498b2afc6a164c5d6cb6466
Mary-Ellen <maryellen@lockss.org>
2019-12-02 14:12:34 -0800

    Update USDocs AUs and script

--- scripts/tdb/check_usdocs_manifest_pages.sh
+++ scripts/tdb/check_usdocs_manifest_pages.sh
@@ -12,9 +12,11 @@ cat $tpath/list1c | uniq > $tpath/list1d #combined list sorted and uniqed
 echo "Duplicate AUs in previous and new tdb files."
 comm -23 $tpath/list1c $tpath/list1d #output. duplicate aus in new and old tdb files?
 perl scripts/tdb/check_usdocs_manifest_pages.pl | sort | uniq > $tpath/list2a #list of AUs on website
-echo "AUs missing from the new tdb file."
+echo "AUs missing from the combined tdb files. Add to the new tdb file."
 comm -13 $tpath/list1d $tpath/list2a #output. AUs missing from the new tdb file.
-echo "Extra AUs in the tdb files."
+echo "Extra AUs in the new tdb file."
 comm -23 $tpath/list1b $tpath/list2a #output. AUs in the new tdb file, which are not on the website
+#echo "AUs missing from the old tdb file."
+#comm -13 $tpath/list1a $tpath/list2a #output. AUs missing from the old tdb file.
 exit 0
 
