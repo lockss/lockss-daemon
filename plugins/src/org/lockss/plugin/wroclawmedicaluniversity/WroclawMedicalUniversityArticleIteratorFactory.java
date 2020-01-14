3698074b3a3f273b563f50b81df2491a0be76b54
feili18 <feili18@stanford.edu>
2020-01-14 15:13:50 -0800

    add ArticleIterator and MetadataExtrator to WroclawMedicalUniversity

--- plugins/src/org/lockss/plugin/wroclawmedicaluniversity/WroclawMedicalUniversityArticleIteratorFactory.java
+++ plugins/src/org/lockss/plugin/wroclawmedicaluniversity/WroclawMedicalUniversityArticleIteratorFactory.java
@@ -20,22 +20,24 @@ public class WroclawMedicalUniversityArticleIteratorFactory implements ArticleIt
 
     protected static Logger log = Logger.getLogger(WroclawMedicalUniversityArticleIteratorFactory.class);
 
-    protected static final String ROOT_TEMPLATE = "\"^%s\", base_url";
+    protected static final String ROOT_TEMPLATE = "\"%s\", base_url";
     protected static final String PATTERN_TEMPLATE =
-            "\"^%s((en/article)|pdf)/%d/%s/\", base_url, year, volume_number";
-
+            "\"%s((en/article)|pdf)/%d/%s/\", base_url, year, volume_number";
 
     private static final Pattern HTML_PATTERN = Pattern.compile(
-            "en/article/([^/]+/[^/]+/[^/]+)/",
+            "en/article/([^/]+/[^/]+/[^/]+/[^/]+)",
             Pattern.CASE_INSENSITIVE);
+    private static final String HTML_REPLACEMENT = "en/article/$1";
+
     private static final Pattern PDF_PATTERN = Pattern.compile(
-            "pdf/(.*)/[^/]+.pdf$",
+            "pdf/([^/]+/[^/]+/[^/]+/[^/]+)\\.pdf",
             Pattern.CASE_INSENSITIVE);
+    private static final String PDF_REPLACEMENT = "pdf/$1.pdf";
 
 
     //It has only html and PDF, no separate pages other aspects, like  abstract/full_tex
     //http://www.dmp.umed.wroc.pl/en/article/2019/56/3/317
-    //http://www.dmp.umed.wroc.pl/pdf/2019/56/3/285.pdf
+    //http://www.dmp.umed.wroc.pl/pdf/2019/56/3/317.pdf
     @Override
     public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au, MetadataTarget target)
             throws PluginException {
@@ -47,16 +49,17 @@ public class WroclawMedicalUniversityArticleIteratorFactory implements ArticleIt
 
         builder.addAspect(
                 HTML_PATTERN,
-                ArticleFiles.ROLE_ABSTRACT,
+                HTML_REPLACEMENT,
+                ArticleFiles.ROLE_FULL_TEXT_HTML,
                 ArticleFiles.ROLE_ARTICLE_METADATA);
 
         builder.addAspect(
-                PDF_PATTERN,
+                PDF_REPLACEMENT,
                 ArticleFiles.ROLE_FULL_TEXT_PDF);
 
-
         builder.setFullTextFromRoles(
-                ArticleFiles.ROLE_ABSTRACT);
+                ArticleFiles.ROLE_ABSTRACT,
+                ArticleFiles.ROLE_FULL_TEXT_HTML);
 
         return builder.getSubTreeArticleIterator();
     }
--- plugins/src/org/lockss/plugin/wroclawmedicaluniversity/WroclawMedicalUniversityMetadataExtractorFactory.java
+++ plugins/src/org/lockss/plugin/wroclawmedicaluniversity/WroclawMedicalUniversityMetadataExtractorFactory.java
@@ -1,4 +1,46 @@
 package org.lockss.plugin.wroclawmedicaluniversity;
 
-public class WroclawMedicalUniversityMetadataExtractorFactory {
+import org.apache.commons.collections.MultiMap;
+import org.apache.commons.collections.map.MultiValueMap;
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.*;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.ans.AnsHtmlMetadataExtractorFactory;
+
+import java.io.IOException;
+
+public class WroclawMedicalUniversityMetadataExtractorFactory implements FileMetadataExtractorFactory {
+
+    @Override
+    public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
+                                                             String contentType)
+            throws PluginException {
+        return new WroclawMedicalUniversityHtmlMetadataExtractor();
+    }
+
+    public static class WroclawMedicalUniversityHtmlMetadataExtractor
+            extends SimpleHtmlMetaTagMetadataExtractor {
+        private static MultiMap tagMap = new MultiValueMap();
+        static {
+            tagMap.put("citation_author", MetadataField.FIELD_AUTHOR);
+            tagMap.put("citation_date", MetadataField.FIELD_DATE);
+            tagMap.put("citation_title", MetadataField.FIELD_ARTICLE_TITLE);
+            tagMap.put("citation_journal_title", MetadataField. FIELD_PUBLICATION_TITLE);
+            tagMap.put("citation_volume", MetadataField.FIELD_VOLUME);
+            tagMap.put("citation_issue", MetadataField.FIELD_ISSUE);
+            tagMap.put("citation_firstpage", MetadataField.FIELD_START_PAGE);
+            tagMap.put("citation_lastpage", MetadataField.FIELD_END_PAGE);
+            tagMap.put("citation_doi", MetadataField.FIELD_DOI);
+            tagMap.put("citation_issn", MetadataField.FIELD_ISSN);
+        }
+
+        @Override
+        public ArticleMetadata extract(MetadataTarget target, CachedUrl cu)
+                throws IOException {
+            ArticleMetadata am = super.extract(target, cu);
+            am.cook(tagMap);
+            return am;
+        }
+    }
 }
+
