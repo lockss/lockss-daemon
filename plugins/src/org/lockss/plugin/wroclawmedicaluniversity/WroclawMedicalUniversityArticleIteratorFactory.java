3b9ef6929ee8737e07c7f9047628612da12e7a75
feili18 <feili18@stanford.edu>
2020-01-15 09:29:07 -0800

    make ArticleIterator using PDF instead of html

--- plugins/src/org/lockss/plugin/wroclawmedicaluniversity/WroclawMedicalUniversityArticleIteratorFactory.java
+++ plugins/src/org/lockss/plugin/wroclawmedicaluniversity/WroclawMedicalUniversityArticleIteratorFactory.java
@@ -22,17 +22,17 @@ public class WroclawMedicalUniversityArticleIteratorFactory implements ArticleIt
 
     protected static final String ROOT_TEMPLATE = "\"%s\", base_url";
     protected static final String PATTERN_TEMPLATE =
-            "\"%s((en/article)|pdf)/%d/%s/\", base_url, year, volume_number";
+            "\"%s(en/article|pdf)/%d/%s/\", base_url, year, volume_number";
 
     private static final Pattern HTML_PATTERN = Pattern.compile(
-            "en/article/([^/]+/[^/]+/[^/]+/[^/]+)",
+            ".*/(en/article)/([^/]+/[^/]+/[^/]+/[^/]+)",
             Pattern.CASE_INSENSITIVE);
-    private static final String HTML_REPLACEMENT = "en/article/$1";
+    private static final String HTML_REPLACEMENT = "en/article/$2";
 
     private static final Pattern PDF_PATTERN = Pattern.compile(
-            "pdf/([^/]+/[^/]+/[^/]+/[^/]+)\\.pdf",
+            ".*/(pdf)/([^/]+/[^/]+/[^/]+/\\d+)\\.pdf",
             Pattern.CASE_INSENSITIVE);
-    private static final String PDF_REPLACEMENT = "pdf/$1.pdf";
+    private static final String PDF_REPLACEMENT = "pdf/$2.pdf";
 
 
     //It has only html and PDF, no separate pages other aspects, like  abstract/full_tex
@@ -48,18 +48,17 @@ public class WroclawMedicalUniversityArticleIteratorFactory implements ArticleIt
                 PATTERN_TEMPLATE, Pattern.CASE_INSENSITIVE);
 
         builder.addAspect(
+                PDF_PATTERN,
+                PDF_REPLACEMENT,
+                ArticleFiles.ROLE_FULL_TEXT_PDF);
+
+        builder.addAspect(
                 HTML_PATTERN,
                 HTML_REPLACEMENT,
                 ArticleFiles.ROLE_FULL_TEXT_HTML,
                 ArticleFiles.ROLE_ARTICLE_METADATA);
 
-        builder.addAspect(
-                PDF_REPLACEMENT,
-                ArticleFiles.ROLE_FULL_TEXT_PDF);
-
-        builder.setFullTextFromRoles(
-                ArticleFiles.ROLE_ABSTRACT,
-                ArticleFiles.ROLE_FULL_TEXT_HTML);
+        builder.setFullTextFromRoles(ArticleFiles.ROLE_FULL_TEXT_PDF);
 
         return builder.getSubTreeArticleIterator();
     }
