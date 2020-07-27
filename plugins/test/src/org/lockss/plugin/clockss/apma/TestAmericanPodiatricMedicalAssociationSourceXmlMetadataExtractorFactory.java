c76a91e8a84e4f1b42555e9c9528e00eb43d8ba1
feili18 <feili18@standford.edu>
2020-07-27 08:05:49 -0700

    make change to American Podiatric MedicalAssociation metadata extractor

---
+++ plugins/test/src/org/lockss/plugin/clockss/apma/TestAmericanPodiatricMedicalAssociationSourceXmlMetadataExtractorFactory.java
@@ -0,0 +1,110 @@
+/*
+ * $Id:$
+ */
+/*
+
+/*
+
+ Copyright (c) 2000-2016 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of his software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.clockss.apma;
+
+import org.apache.commons.io.FileUtils;
+import org.lockss.extractor.*;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.clockss.SourceXmlMetadataExtractorTest;
+import org.lockss.plugin.clockss.isecs.InternationalStructuralEngineeringConstructionSocietySourceXmlMetadataExtractorFactory;
+import org.lockss.test.MockArchivalUnit;
+import org.lockss.test.MockCachedUrl;
+import org.lockss.util.CIProperties;
+import org.lockss.util.Constants;
+import org.lockss.util.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+
+public class TestAmericanPodiatricMedicalAssociationSourceXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorTest {
+
+  private static final Logger log = Logger.getLogger(TestAmericanPodiatricMedicalAssociationSourceXmlMetadataExtractorFactory.class);
+
+  private static String BaseUrl = "http://source.host.org/sourcefiles/isecs/";
+  private static String Directory = "2019";
+
+  private static String getXmlFileContent(String fname) {
+    String xmlContent = "";
+    try {
+      String currentDirectory = System.getProperty("user.dir");
+      String pathname = currentDirectory +
+              "/plugins/test/src/org/lockss/plugin/clockss/apma/" + fname;
+      xmlContent = FileUtils.readFileToString(new File(pathname), Constants.DEFAULT_ENCODING);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return xmlContent;
+  }
+
+  public void testExtractArticleXmlSchema() throws Exception {
+
+    String fname = "sample_test_jats.xml";
+    String journalXml = getXmlFileContent(fname);
+
+    assertNotNull(journalXml);
+
+    String xml_url = BaseUrl + Directory + "/" + fname;
+    CIProperties xmlHeader = new CIProperties();
+    xmlHeader.put(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+
+    MockArchivalUnit mau = new MockArchivalUnit();
+    MockCachedUrl mcu = mau.addUrl(xml_url, true, true, xmlHeader);
+    mcu.setContent(journalXml);
+    mcu.setContentSize(journalXml.length());
+    mcu.setProperty(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+
+    FileMetadataExtractor me =  new AmericanPodiatricMedicalAssociationXmlMetadataExtractorFactory().
+            createFileMetadataExtractor(MetadataTarget.Any(), "text/xml");
+    FileMetadataListExtractor mle =
+            new FileMetadataListExtractor(me);
+    List<ArticleMetadata> mdlist = mle.extract(MetadataTarget.Any(), mcu);
+    assertNotEmpty(mdlist);
+    assertEquals(1, mdlist.size());
+
+    ArticleMetadata md = mdlist.get(0);
+    assertNotNull(md);
+
+    assertEquals("110", md.get(MetadataField.FIELD_VOLUME));
+    assertEquals("2", md.get(MetadataField.FIELD_ISSUE));
+    assertEquals("Procedure-Specific Hardware Removal After Evans Osteotomy", md.get(MetadataField.FIELD_ARTICLE_TITLE));
+    assertEquals("8750-7315", md.get(MetadataField.FIELD_ISSN));
+    assertEquals("DeHeer, Patrick A.", md.get(MetadataField.FIELD_AUTHOR));
+    assertEquals("Journal of the American Podiatric Medical Association", md.get(MetadataField.FIELD_PUBLICATION_TITLE));
+    assertEquals("American Podiatric Medical Association", md.get(MetadataField.FIELD_PUBLISHER));
+    assertEquals("2020", md.get(MetadataField.FIELD_DATE));
+  }
+}
+
