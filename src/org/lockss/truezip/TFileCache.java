567095d97ed651b25f868a4fb88438535dae70dc
Tom Lipkis <tal@pss.com>
2020-11-14 09:55:39 -0800

    Prevent NPE in test

--- src/org/lockss/truezip/TFileCache.java
+++ src/org/lockss/truezip/TFileCache.java
@@ -308,7 +308,7 @@ public class TFileCache {
       CachedUrl splitZipCu =
           au.makeCachedUrl(replaceZipExtension(cu.getUrl(), prefix, digits, i));
 
-      if (splitZipCu.hasContent()) {
+      if (splitZipCu != null && splitZipCu.hasContent()) {
         return splitZipCu;
       }
     }
