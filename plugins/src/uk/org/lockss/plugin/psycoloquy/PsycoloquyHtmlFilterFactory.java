7100476ab09f6bb31cfe2b2a60cb8e16f482f900
Thib Guicherd-Callin <thib@cs.stanford.edu>
2020-11-21 17:57:46 -0800

    Quickly clean up plugins signed by smorabito-lockss or adamr-lockss.

---
+++ plugins/src/uk/org/lockss/plugin/psycoloquy/PsycoloquyHtmlFilterFactory.java
@@ -0,0 +1,60 @@
+/*
+
+Copyright (c) 2000-2020, Board of Trustees of Leland Stanford Jr. University
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package uk.org.lockss.plugin.psycoloquy;
+
+import java.io.*;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.filter.*;
+import org.lockss.plugin.*;
+import org.lockss.util.ReaderInputStream;
+
+public class PsycoloquyHtmlFilterFactory implements FilterFactory {
+
+  private static final String START = "<p>This list was generated on <strong>";
+  private static final String END = "</strong>.</p>";
+
+  @Override
+  public InputStream createFilteredInputStream(ArchivalUnit au,
+                                               InputStream in,
+                                               String encoding)
+      throws PluginException {
+    return new ReaderInputStream(new HtmlTagFilter(FilterUtil.getReader(in, encoding),
+                                                   new HtmlTagFilter.TagPair(START,
+                                                                             END,
+                                                                             true)),
+                                 encoding);
+  }
+  
+}
