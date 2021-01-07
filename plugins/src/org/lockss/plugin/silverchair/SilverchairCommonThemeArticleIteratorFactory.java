350c78c082c8107548ab3b9dfa972da57378a268
feili18 <feili18@standford.edu>
2021-01-07 14:46:44 -0800

    modify the pattern and upgrade child version #

--- plugins/src/org/lockss/plugin/silverchair/SilverchairCommonThemeArticleIteratorFactory.java
+++ plugins/src/org/lockss/plugin/silverchair/SilverchairCommonThemeArticleIteratorFactory.java
@@ -10,7 +10,7 @@ public class SilverchairCommonThemeArticleIteratorFactory extends BaseScArticleI
     //https://academic.oup.com/psychsocgerontology/article/66B/1/109/580460
 
     private static String ROOT_TEMPLATE = "\"%s\", base_url";
-    private static String PATTERN_TEMPLATE =  "\"%s([^/]+)?/%s/article(-abstract)?\", base_url, journal_id";
+    private static String PATTERN_TEMPLATE =  "\"%s([^/]+/)?%s/article(-abstract)?\", base_url, journal_id";
 
     private static final Pattern HTML_PATTERN = Pattern.compile("/article/([0-9i]+)/(.*)$", Pattern.CASE_INSENSITIVE);
     private static final String HTML_REPLACEMENT = "/article/$1/$2";
