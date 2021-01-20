a8e5a51aefe422ba3218e4c00a1c0db97ca4d609
Mary-Ellen <maryellen@lockss.org>
2021-01-19 18:30:21 -0800

    Remove duplicate manifest page checker.

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -2947,32 +2947,32 @@ while (my $line = <>) {
     }
     sleep(4);
 
-  } elsif (($plugin eq "OupSilverchairPlugin") || 
-           ($plugin eq "DupSilverchairPlugin")) {
-    $url = sprintf("%s%s/list-of-issues/%d",
-      $param{base_url}, $param{journal_id}, $param{year});
-    $man_url = uri_unescape($url);
-    my $req = HTTP::Request->new(GET, $man_url);
-    my $resp = $ua->request($req);
-    if ($resp->is_success) {
-      my $man_contents = $resp->content;
-      if ($req->url ne $resp->request->uri) {
-              $vol_title = $resp->request->uri;
-              $result = "Redirected";
-      } elsif (defined($man_contents) && ($man_contents =~ m/$lockss_tag/)) {
-        if ($man_contents =~ m/<title>(.*) [|] \w* University Press<\/title>/si) {
-          $vol_title = $1;
-          $vol_title =~ s/\s*\n\s*/ /g;
-        }
-        $result = "Manifest"
-      } else {
-        $result = "--NO_TAG--"
-      }
-    } else {
-      $result = "--REQ_FAIL--" . $resp->code() . " " . $resp->message();
-    }
-    sleep(4);
-
+#  } elsif (($plugin eq "OupSilverchairPlugin") || 
+#           ($plugin eq "DupSilverchairPlugin")) {
+#    $url = sprintf("%s%s/list-of-issues/%d",
+#      $param{base_url}, $param{journal_id}, $param{year});
+#    $man_url = uri_unescape($url);
+#    my $req = HTTP::Request->new(GET, $man_url);
+#    my $resp = $ua->request($req);
+#    if ($resp->is_success) {
+#      my $man_contents = $resp->content;
+#      if ($req->url ne $resp->request->uri) {
+#              $vol_title = $resp->request->uri;
+#              $result = "Redirected";
+#      } elsif (defined($man_contents) && ($man_contents =~ m/$lockss_tag/)) {
+#        if ($man_contents =~ m/<title>(.*) [|] \w* University Press<\/title>/si) {
+#          $vol_title = $1;
+#          $vol_title =~ s/\s*\n\s*/ /g;
+#        }
+#        $result = "Manifest"
+#      } else {
+#        $result = "--NO_TAG--"
+#      }
+#    } else {
+#      $result = "--REQ_FAIL--" . $resp->code() . " " . $resp->message();
+#    }
+#    sleep(4);
+#
 #  } elsif (($plugin eq "ClockssOupSilverchairPlugin") || 
 #           ($plugin eq "ClockssDupSilverchairPlugin") || 
 #           ($plugin eq "ClockssGeoscienceWorldSilverchairPlugin")) {
