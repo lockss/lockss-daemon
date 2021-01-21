08ed5aba5b8e9dcfb306f609dc312ecd75ec968c
feili18 <feili18@standford.edu>
2021-01-21 13:32:36 -0800

    add more file related data to metadata

--- plugins/src/org/lockss/plugin/gigascience/GigaScienceAPIXmlMetadataExtractorFactory.java
+++ plugins/src/org/lockss/plugin/gigascience/GigaScienceAPIXmlMetadataExtractorFactory.java
@@ -1,11 +1,18 @@
 package org.lockss.plugin.gigascience;
 
 import org.lockss.daemon.PluginException;
-import org.lockss.extractor.*;
+import org.lockss.plugin.AuUtil;
 import org.lockss.plugin.CachedUrl;
 import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
 import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
 import org.lockss.util.Logger;
+import org.lockss.extractor.ArticleMetadata;
+import org.lockss.extractor.FileMetadataExtractor;
+import org.lockss.extractor.MetadataField;
+import org.lockss.extractor.MetadataTarget;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.io.FilenameUtils;
 
 public class GigaScienceAPIXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
     private static final Logger log = Logger.getLogger(GigaScienceAPIXmlMetadataExtractorFactory.class);
@@ -35,9 +42,44 @@ public class GigaScienceAPIXmlMetadataExtractorFactory extends SourceXmlMetadata
                                        CachedUrl cu, ArticleMetadata thisAM) {
 
             log.debug3("in GigaScienceAPI  postCookProcess");
+            
             thisAM.put(MetadataField.FIELD_ARTICLE_TYPE, MetadataField.ARTICLE_TYPE_FILE);
             thisAM.put(MetadataField.FIELD_PUBLICATION_TYPE, MetadataField.PUBLICATION_TYPE_FILE);
 
+            // Add a custom map to the generic am table 
+            // Allow a child to override FileType
+            Map<String, String> FILE_MAP = new HashMap<String,String>();
+
+            //default is "file"
+            FILE_MAP.put("FileType", MetadataField.ARTICLE_TYPE_FILE);
+            // default is base filename
+            FILE_MAP.put("FileIdentifier", getFileIdentifier(cu));
+            FILE_MAP.put("FileSizeBytes", getFileSize(cu));
+            FILE_MAP.put("FileMime", getFileMime(cu));
+            // default is no additional k-v pairs; child can add specific items
+            thisAM.putRaw(MetadataField.FIELD_MD_MAP.getKey(), FILE_MAP);
+
         }
+
+        protected String getFileIdentifier(CachedUrl cu) {
+            // we know cu isn't null
+            log.debug3("Fei - FileIdentifier : " + FilenameUtils.getBaseName(cu.getUrl());
+            return FilenameUtils.getBaseName(cu.getUrl());
+        }
+
+        protected String getFileSize(CachedUrl cu) {
+            long content_size = cu.getContentSize();
+            return Long.toString(content_size);
+
+        }
+        
+        protected String getFileMime(CachedUrl cu) {
+            String content_mime = cu.getContentType();
+            // needed after the getContentType
+            AuUtil.safeRelease(cu);
+            return content_mime;
+
+        }
+
     }
 }
