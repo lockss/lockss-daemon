2547265d275c4fe49aaf783d78ae139a385cef37
Mark McAdam <mol3earth@gmail.com>
2021-02-09 16:25:05 -0800

    made LoginPage Checker more robust

--- plugins/src/org/lockss/plugin/royalsocietyofchemistry/RSCLoginPageChecker.java
+++ plugins/src/org/lockss/plugin/royalsocietyofchemistry/RSCLoginPageChecker.java
@@ -1,5 +1,39 @@
+/*
+
+Copyright (c) 2000-2021, Board of Trustees of Leland Stanford Jr. University
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
 package org.lockss.plugin.royalsocietyofchemistry;
 
+import org.apache.commons.io.IOUtils;
 import org.lockss.daemon.LoginPageChecker;
 import org.lockss.daemon.PluginException;
 import org.lockss.util.HeaderUtil;
@@ -26,6 +60,14 @@ public class RSCLoginPageChecker implements LoginPageChecker {
   private static final Logger log = Logger.getLogger(RSCLoginPageChecker.class);
 
   /**
+   * These HTML docs are poorly formatted, so searching for the <title>Access Denied</title> as a regex incorporating
+   * much white space is probably the safest.
+   *
+   * Releavnt portion of roughly what is in the Page Source:
+   * <header>[HEADER STUFF]<title>
+   *    Access Denied
+   *   </title>
+   * </header>
    * <p>
    *  <body>
    *    <div>
@@ -35,24 +77,22 @@ public class RSCLoginPageChecker implements LoginPageChecker {
    * </p>
    */
 
-  ///protected static final String ACCESS_DENIED_SNIPPET = "<title>\\s*Access Denied\\s*</title>";
-  protected static final String ACCESS_DENIED_SNIPPET = "Access Denied";
+  protected static final String ACCESS_DENIED_SNIPPET = "<title>\\s*Access Denied\\s*</title>";
   protected static final Pattern ACCESS_DENIED_PATTERN = Pattern.compile(ACCESS_DENIED_SNIPPET, Pattern.CASE_INSENSITIVE);
 
   public boolean isLoginPage(Properties props,
                              Reader reader)
       throws IOException,
       PluginException {
-    //log.debug2(props.toString());
+
     boolean found = false;
     if ("text/html".equalsIgnoreCase(HeaderUtil.getMimeTypeFromContentType(props.getProperty("Content-Type")))) {
-      //String theContents = StringUtil.fromReader(reader);  // This returns an empty string most of the time.
-      //Matcher matcher = ACCESS_DENIED_PATTERN.matcher(theContents);
-      //log.debug(theContents);
-      //found = matcher.find();
-      found = StringUtil.containsString(reader, ACCESS_DENIED_SNIPPET, true);
+      String theContents = StringUtil.fromReader(reader);  // This returns an empty string most of the time.
+      Matcher matcher = ACCESS_DENIED_PATTERN.matcher(theContents);
+      //log.debug3( theContents.length() + "<<<" + theContents + ">>>" );
+      found = matcher.find();
       if (found) {
-        log.debug("found a match with: '" + ACCESS_DENIED_SNIPPET + "'");
+        log.debug3("found a match with: '" + ACCESS_DENIED_SNIPPET + "'");
         throw new CacheException.UnexpectedNoRetryFailException("Found an Access Denied page.");
       }
     }
