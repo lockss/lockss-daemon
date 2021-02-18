17e7874f924a33971f63baaeca6b187bf5a5f384
Mark McAdam <mol3earth@gmail.com>
2021-02-18 10:11:43 -0800

    copied Berghahn plugin into new pubfactory directory, readying new platform

---
+++ plugins/src/org/lockss/plugin/pubfactory/berghahn/BerghahnArticleIteratorFactory.java
@@ -0,0 +1,124 @@
+/*
+ * $Id$
+ */
+
+/*
+
+Copyright (c) 2000-2018 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.pubfactory.berghahn;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.ArticleMetadataExtractor;
+import org.lockss.extractor.ArticleMetadataExtractorFactory;
+import org.lockss.extractor.BaseArticleMetadataExtractor;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.*;
+import org.lockss.util.Logger;
+
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
+/*
+ * https://www.berghahnjournals.com/downloadpdf/journals/boyhood-studies/10/1/bhs100105.pdf
+ * https://www.berghahnjournals.com/downloadpdf/journals/boyhood-studies/10/1/bhs100105.xml
+ * https://www.berghahnjournals.com/view/journals/boyhood-studies/10/1/bhs100101.xml
+ * https://www.berghahnjournals.com/view/journals/boyhood-studies/10/1/bhs100101.xml?pdfVersion=true (frameset)
+ *
+ * Also - this look like an error. Ignore it. It's the link associated with the pdf tab when you're
+ * already on the html of the pdf tab active
+ * https://www.berghahnjournals.com/view/journals/boyhood-studies/10/1/bhs100101.xml?&pdfVersion=true (WHAAA?)
+ */
+
+public class BerghahnArticleIteratorFactory implements ArticleIteratorFactory, ArticleMetadataExtractorFactory {
+
+  private static final Logger log = Logger.getLogger(BerghahnArticleIteratorFactory.class);
+
+  // don't set the ROOT_TEMPLATE - it is just base_url
+
+  private static final String PATTERN_TEMPLATE = 
+      "\"^%s(downloadpdf|view)/journals/%s/%s/\", base_url, journal_id, volume_name";
+
+  // (?![^/]+issue[^/]+) is a negative lookahead to exclude issue TOC pages but to allow articles through
+  // it must come before the bit that picks up the filename when it's not an issue
+  // https://www.berghahnjournals.com/view/journals/boyhood-studies/10/1/boyhood-studies.10.issue-1.xml
+  private static final Pattern ART_LANDING_PATTERN = Pattern.compile("/view/(journals/.+/(?![^/]+issue[^/]+)[^/]+)\\.xml$", Pattern.CASE_INSENSITIVE);               
+  private static final Pattern ART_PDF_PATTERN = Pattern.compile("/downloadpdf/(journals/.+/[^/]+)\\.pdf$", Pattern.CASE_INSENSITIVE);               
+  private static final Pattern ART_PDF_XML_PATTERN = Pattern.compile("/downloadpdf/(journals/.+/[^/]+)\\.xml$", Pattern.CASE_INSENSITIVE);               
+  
+  // how to get from one of the above to the other
+  private final String PDF_REPLACEMENT = "/downloadpdf/$1.pdf"; 
+  private final String PDF_XML_REPLACEMENT = "/downloadpdf/$1.xml"; 
+  private final String PDF_FRAME_REPLACEMENT = "/view/$1.xml?pdfVersion=true"; //legacy
+  private final String LANDING_REPLACEMENT = "/view/$1.xml";
+
+  @Override
+  public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au, MetadataTarget target) throws PluginException {
+    SubTreeArticleIteratorBuilder builder = new SubTreeArticleIteratorBuilder(au);
+
+    builder.setSpec(new SubTreeArticleIterator.Spec()
+    .setTarget(target)
+    .setPatternTemplate(PATTERN_TEMPLATE, Pattern.CASE_INSENSITIVE));
+
+    builder.addAspect(ART_LANDING_PATTERN,
+        LANDING_REPLACEMENT,
+        ArticleFiles.ROLE_ABSTRACT,
+        ArticleFiles.ROLE_ARTICLE_METADATA);
+
+    // make this one primary by defining it first
+    builder.addAspect(ART_PDF_PATTERN,
+            PDF_REPLACEMENT,
+            ArticleFiles.ROLE_FULL_TEXT_PDF);
+
+    // another version
+    builder.addAspect(ART_PDF_XML_PATTERN,
+            PDF_XML_REPLACEMENT,
+            ArticleFiles.ROLE_FULL_TEXT_PDF);
+
+    builder.addAspect(
+        PDF_FRAME_REPLACEMENT,
+        ArticleFiles.ROLE_FULL_TEXT_PDF_LANDING_PAGE);
+
+
+    // The order in which we want to define full_text_cu.  
+    // First one that exists will get the job
+    // Leave the CITATION_RIS in because if just doing iterator, it's the only one set
+    builder.setFullTextFromRoles(ArticleFiles.ROLE_FULL_TEXT_PDF,
+        ArticleFiles.ROLE_ABSTRACT);
+
+    
+    return builder.getSubTreeArticleIterator();
+  }
+
+
+  @Override
+  public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target)
+      throws PluginException {
+    return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
+  }
+}
+
---
+++ plugins/src/org/lockss/plugin/pubfactory/berghahn/BerghahnUrlConsumerFactory.java
@@ -0,0 +1,97 @@
+/*
+ * $Id$
+ */
+
+/*
+
+Copyright (c) 2018 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Stanford University shall not
+be used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Stanford University.
+
+*/
+
+package org.lockss.plugin.pubfactory.berghahn;
+
+
+import org.lockss.daemon.Crawler.CrawlerFacade;
+import org.lockss.plugin.FetchedUrlData;
+import org.lockss.plugin.UrlConsumer;
+import org.lockss.plugin.UrlConsumerFactory;
+import org.lockss.plugin.base.SimpleUrlConsumer;
+import org.lockss.util.Logger;
+
+import java.io.IOException;
+
+/**
+ * UNUSED CURRENTLY
+ * Tho the links to the pdf are 
+ * blah/downloadpdf/x/y/z.xml
+ * which redirect to
+ * blah/downloadpdf/x/y/z.pdf
+ * after discussion we've decided to leave both versions in - so that if the pdf is ever linked to direclty, we have it
+ * 
+ */
+public class BerghahnUrlConsumerFactory implements UrlConsumerFactory {
+  private static final Logger log = Logger.getLogger(BerghahnUrlConsumerFactory.class);
+  
+  @Override
+  public UrlConsumer createUrlConsumer(CrawlerFacade crawlFacade,
+      FetchedUrlData fud) {
+    return new BerghahnUrlConsumer(crawlFacade, fud);
+  }
+  
+  public class BerghahnUrlConsumer extends SimpleUrlConsumer {
+    
+    public BerghahnUrlConsumer(CrawlerFacade facade, FetchedUrlData fud) {
+      super(facade, fud);
+    }
+    
+    @Override
+    public void consume() throws IOException {
+      if (shouldStoreAtOrigUrl()) {
+        storeAtOrigUrl();
+      }
+      super.consume();
+    }
+    
+    /**
+     * https://www.berghahnjournals.com/downloadpdf/journals/boyhood-studies/10/1/bhs100105.pdf
+     * https://www.berghahnjournals.com/downloadpdf/journals/boyhood-studies/10/1/bhs100105.xml (consumed in to above)
+     */
+    public boolean shouldStoreAtOrigUrl() throws IOException {
+    	boolean should = false;
+/*
+    	if (fud.redirectUrls != null && (fud.redirectUrls.size() > 0)) {
+    		//the fetched = original but for the terminating ".xml" and ".pdf"
+    		if (fud.fetchUrl.endsWith(".pdf") &&
+    				fud.origUrl.endsWith(".xml") &&
+    				fud.fetchUrl.substring(0,fud.fetchUrl.lastIndexOf(".pdf")).equals(fud.origUrl.substring(0,fud.origUrl.lastIndexOf(".xml")))) {
+    			should = true;
+    		}
+    		log.debug3("Berg redirect: " + fud.redirectUrls.size() + " " + fud.origUrl + " to " + fud.fetchUrl + " : " + should);
+    	}
+    	*/
+    	return should;
+    }
+  }
+}
