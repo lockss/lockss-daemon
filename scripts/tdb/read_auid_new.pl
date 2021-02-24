97f589d4aa16d2a94f979bed35f49145e0a09e7c
Mark McAdam <mol3earth@gmail.com>
2021-02-24 13:59:32 -0800

    renamed AMetSoc to AMetSoc2021 (hopefully everywhere needed)

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -997,7 +997,7 @@ while (my $line = <>) {
         sleep(4);
 
   } elsif (($plugin eq "BerghahnJournalsPlugin") ||
-           ($plugin eq "AMetSocPlugin")) {
+           ($plugin eq "AMetSoc2021Plugin")) {
       $url = sprintf("%slockss-manifest/journal/%s/volume/%s",
       $param{base_url}, $param{journal_id}, $param{volume_name});
       $man_url = uri_unescape($url);
@@ -1029,7 +1029,7 @@ while (my $line = <>) {
         sleep(4);
 
   } elsif (($plugin eq "ClockssBerghahnJournalsPlugin") ||
-           ($plugin eq "ClockssAMetSocPlugin")) {
+           ($plugin eq "ClockssAMetSoc2021Plugin")) {
       $url = sprintf("%slockss-manifest/journal/%s/volume/%s",
       $param{base_url}, $param{journal_id}, $param{volume_name});
       $man_url = uri_unescape($url);
