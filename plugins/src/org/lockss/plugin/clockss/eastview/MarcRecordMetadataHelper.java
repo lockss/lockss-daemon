ae8868bcdd093187ff1df5bfb81f5916b0536cad
feili18 <feili18@standford.edu>
2021-05-20 09:35:21 -0700

    turn off debug for quick metadata

--- plugins/src/org/lockss/plugin/clockss/eastview/MarcRecordMetadataHelper.java
+++ plugins/src/org/lockss/plugin/clockss/eastview/MarcRecordMetadataHelper.java
@@ -72,148 +72,148 @@ public class MarcRecordMetadataHelper implements FileMetadataExtractor {
     try {
 
       //Ignore the ".txt" file from metadata
-      if (cu.getUrl().contains(".txt")) {
+      if (!cu.getUrl().contains(".txt")) {
         log.debug(String.format("Url: %s will be avoid ", cu.getUrl()));
-        return; 
-      }
 
-      InputStream input = cu.getUnfilteredInputStream();
 
-      MarcReader reader = null;
+        InputStream input = cu.getUnfilteredInputStream();
 
-      if (cu.getUrl().contains(".xml")) {
-        reader = new MarcXmlReader(input, "UTF-8");
-      }
+        MarcReader reader = null;
 
-      if (cu.getUrl().contains(".mrc")) {
-        reader = new MarcStreamReader(input, "UTF-8");
-      }
+        if (cu.getUrl().contains(".xml")) {
+          reader = new MarcXmlReader(input, "UTF-8");
+        }
 
-      int recordCount = 0;
-      List<String> bookIDs = new ArrayList<String>();
+        if (cu.getUrl().contains(".mrc")) {
+          reader = new MarcStreamReader(input, "UTF-8");
+        }
 
-      while (reader.hasNext()) {
+        int recordCount = 0;
+        List<String> bookIDs = new ArrayList<String>();
 
-        ArticleMetadata am = new ArticleMetadata();
+        while (reader.hasNext()) {
 
-        Record record = reader.next();
-        recordCount++;
+          ArticleMetadata am = new ArticleMetadata();
 
-        // Get all the raw metadata and put it to raw
-        List<DataField> fields = record.getDataFields();
-        if (fields != null) {
-          for (DataField field : fields) {
-            String tag = field.getTag();
-            List<Subfield> subfields = field.getSubfields();
-            if (subfields != null) {
-              for (Subfield subfield : subfields) {
-                char subtag = subfield.getCode();
-                am.putRaw(String.format("%s_%c", tag, subtag), subfield.getData());
+          Record record = reader.next();
+          recordCount++;
+
+          // Get all the raw metadata and put it to raw
+          List<DataField> fields = record.getDataFields();
+          if (fields != null) {
+            for (DataField field : fields) {
+              String tag = field.getTag();
+              List<Subfield> subfields = field.getSubfields();
+              if (subfields != null) {
+                for (Subfield subfield : subfields) {
+                  char subtag = subfield.getCode();
+                  am.putRaw(String.format("%s_%c", tag, subtag), subfield.getData());
+                }
               }
             }
           }
-        }
 
-        String MARC_isbn = getMARCData(record, "020", 'a');
-        // This is only used in 2016 mrc record
-        String MARC_isbn_alt = getMARCData(record, "773", 'z');
-        String MARC_issn = getMARCData(record, "022", 'a');
-        // MARC_Title will be different for journal vs books
-        String MARC_title = getMARCData(record, "245", 'a');
-        String MARC_pub_date = getMARCData(record, "260", 'c');
-        String MARC_pub_date_alt = getMARCData(record, "264", 'c');
-        String MARC_publisher = getMARCData(record, "260", 'b');
-        String MARC_author = getMARCData(record, "100", 'a');
-        String MARC_author_alt = getMARCData(record, "700", 'a');
-        String MARC_doi =  getMARCData(record, "024", 'a');
-        String MARC_doi_alt =  getMARCData(record, "856", 'u');
-        String MARC_pdf = getMARCControlFieldData(record, "001");
-        // Add it to raw metadata
-        am.putRaw("mrc_controlfield_001", MARC_pdf);
+          String MARC_isbn = getMARCData(record, "020", 'a');
+          // This is only used in 2016 mrc record
+          String MARC_isbn_alt = getMARCData(record, "773", 'z');
+          String MARC_issn = getMARCData(record, "022", 'a');
+          // MARC_Title will be different for journal vs books
+          String MARC_title = getMARCData(record, "245", 'a');
+          String MARC_pub_date = getMARCData(record, "260", 'c');
+          String MARC_pub_date_alt = getMARCData(record, "264", 'c');
+          String MARC_publisher = getMARCData(record, "260", 'b');
+          String MARC_author = getMARCData(record, "100", 'a');
+          String MARC_author_alt = getMARCData(record, "700", 'a');
+          String MARC_doi = getMARCData(record, "024", 'a');
+          String MARC_doi_alt = getMARCData(record, "856", 'u');
+          String MARC_pdf = getMARCControlFieldData(record, "001");
+          // Add it to raw metadata
+          am.putRaw("mrc_controlfield_001", MARC_pdf);
 
-        //Set DOI
-        if (MARC_doi != null && isDoi(MARC_doi)) {
-          log.debug("MARC_doi:" + MARC_doi );
-          am.put(MetadataField.FIELD_DOI, MARC_doi);
-        } else if (MARC_doi_alt != null )  {
-          if (isDoi(MARC_doi_alt)) {
-            log.debug("MARC_doi_alt:" + MARC_doi_alt);
-            am.put(MetadataField.FIELD_DOI, MARC_doi_alt);
+          //Set DOI
+          if (MARC_doi != null && isDoi(MARC_doi)) {
+            log.debug3("MARC_doi:" + MARC_doi);
+            am.put(MetadataField.FIELD_DOI, MARC_doi);
+          } else if (MARC_doi_alt != null) {
+            if (isDoi(MARC_doi_alt)) {
+              log.debug3("MARC_doi_alt:" + MARC_doi_alt);
+              am.put(MetadataField.FIELD_DOI, MARC_doi_alt);
+            }
           }
-        }
 
-        // Set ISBN
-        if (MARC_isbn != null) {
-          log.debug("MARC_isbn:" + MARC_isbn);
-          am.put(MetadataField.FIELD_ISBN, MARC_isbn);
-        } else if (MARC_isbn_alt != null) {
-          log.debug("MARC_isbn_alt:" + MARC_isbn_alt);
-          am.put(MetadataField.FIELD_ISBN, MARC_isbn_alt);
-        }
+          // Set ISBN
+          if (MARC_isbn != null) {
+            log.debug3("MARC_isbn:" + MARC_isbn);
+            am.put(MetadataField.FIELD_ISBN, MARC_isbn);
+          } else if (MARC_isbn_alt != null) {
+            log.debug3("MARC_isbn_alt:" + MARC_isbn_alt);
+            am.put(MetadataField.FIELD_ISBN, MARC_isbn_alt);
+          }
 
-        // Set publiation date
-        if (MARC_pub_date != null) {
-          log.debug("MARC_pub_date:" + MARC_pub_date);
-          am.put(MetadataField.FIELD_DATE, MetadataStringHelperUtilities.cleanupPubDate(MARC_pub_date));
-        } else if (MARC_pub_date_alt != null) {
-          log.debug("MARC_pub_date_alt:" + MARC_pub_date_alt);
-          am.put(MetadataField.FIELD_DATE, MetadataStringHelperUtilities.cleanupPubDate(MARC_pub_date_alt));
-        }
+          // Set publiation date
+          if (MARC_pub_date != null) {
+            log.debug3("MARC_pub_date:" + MARC_pub_date);
+            am.put(MetadataField.FIELD_DATE, MetadataStringHelperUtilities.cleanupPubDate(MARC_pub_date));
+          } else if (MARC_pub_date_alt != null) {
+            log.debug3("MARC_pub_date_alt:" + MARC_pub_date_alt);
+            am.put(MetadataField.FIELD_DATE, MetadataStringHelperUtilities.cleanupPubDate(MARC_pub_date_alt));
+          }
 
-        // Set author
-        if (MARC_author_alt != null) {
-          log.debug("MARC_author_alt:" + MARC_author_alt);
-          am.put(MetadataField.FIELD_AUTHOR, MARC_author_alt.replace(".", ""));
-        } else if (MARC_author != null) {
-            log.debug("MARC_author:" + MARC_author);
+          // Set author
+          if (MARC_author_alt != null) {
+            log.debug3("MARC_author_alt:" + MARC_author_alt);
+            am.put(MetadataField.FIELD_AUTHOR, MARC_author_alt.replace(".", ""));
+          } else if (MARC_author != null) {
+            log.debug3("MARC_author:" + MARC_author);
             am.put(MetadataField.FIELD_AUTHOR, MARC_author.replace(".", ""));
+          }
+
+          // Set publisher name
+          String publisherName = "East View Information Services";
+
+          TdbAu tdbau = cu.getArchivalUnit().getTdbAu();
+          if (tdbau != null) {
+            publisherName = tdbau.getPublisherName();
+          }
+
+          am.put(MetadataField.FIELD_PUBLISHER, publisherName);
+
+          // Setup PDF
+          String zippedFolderName = EastviewMarcXmlSchemaHelper.getZippedFolderName();
+
+          String fileNum = MARC_pdf;
+          String cuBase = FilenameUtils.getFullPath(cu.getUrl());
+
+          String pdfFilePath = cuBase + zippedFolderName + ".zip!/" + fileNum + ".pdf";
+          log.debug3("pdfFilePath" + pdfFilePath);
+          am.put(MetadataField.FIELD_ACCESS_URL, pdfFilePath);
+
+          /*
+          Leader byte 07 “a” = Book (monographic component part)
+          Leader byte 07 “m” = Book
+          Leader byte 07 “s” = Journal
+          Leader byte 07 “b” = Journal (serial component part)
+          Leader leader = record.getLeader();
+
+          Since the raw data sent wrong publication type, use the pre defined one
+          */
+
+          am.put(MetadataField.FIELD_ARTICLE_TYPE, MetadataField.ARTICLE_TYPE_BOOKVOLUME);
+          am.put(MetadataField.FIELD_PUBLICATION_TYPE, MetadataField.PUBLICATION_TYPE_BOOK);
+
+          // Set publication title
+          if (MARC_title != null) {
+            String cleanedArticleTitle = MARC_title.replace(":", "").
+                    replace("/", "").
+                    replace("=", "").
+                    replace("\"", "").
+                    replace("...", "");
+            log.debug3(String.format("original artitle title = %s, cleaned title = %s", MARC_title, cleanedArticleTitle));
+            am.put(MetadataField.FIELD_PUBLICATION_TITLE, MARC_title);
+          }
+
+          emitter.emitMetadata(cu, am);
         }
-
-        // Set publisher name
-        String publisherName = "East View Information Services";
-
-        TdbAu tdbau = cu.getArchivalUnit().getTdbAu();
-        if (tdbau != null) {
-          publisherName = tdbau.getPublisherName();
-        }
-
-        am.put(MetadataField.FIELD_PUBLISHER, publisherName);
-
-        // Setup PDF 
-        String zippedFolderName = EastviewMarcXmlSchemaHelper.getZippedFolderName();
-
-        String fileNum = MARC_pdf;
-        String cuBase = FilenameUtils.getFullPath(cu.getUrl());
-
-        String pdfFilePath = cuBase + zippedFolderName + ".zip!/" + fileNum + ".pdf";
-        log.debug("pdfFilePath" + pdfFilePath );
-        am.put(MetadataField.FIELD_ACCESS_URL, pdfFilePath);
-
-        /*
-        Leader byte 07 “a” = Book (monographic component part)
-        Leader byte 07 “m” = Book
-        Leader byte 07 “s” = Journal
-        Leader byte 07 “b” = Journal (serial component part)
-        Leader leader = record.getLeader();
-
-        Since the raw data sent wrong publication type, use the pre defined one
-        */
-
-        am.put(MetadataField.FIELD_ARTICLE_TYPE, MetadataField.ARTICLE_TYPE_BOOKVOLUME);
-        am.put(MetadataField.FIELD_PUBLICATION_TYPE, MetadataField.PUBLICATION_TYPE_BOOK);
-
-        // Set publication title
-        if (MARC_title != null) {
-          String cleanedArticleTitle = MARC_title.replace(":", "").
-                  replace("/", "").
-                  replace("=", "").
-                  replace("\"", "").
-                  replace("...", "");
-          log.debug(String.format("original artitle title = %s, cleaned title = %s",MARC_title, cleanedArticleTitle));
-          am.put(MetadataField.FIELD_PUBLICATION_TITLE, MARC_title);
-        }
-
-        emitter.emitMetadata(cu, am);
       }
 
     } catch (NullPointerException exception) {
@@ -282,10 +282,10 @@ public class MarcRecordMetadataHelper implements FileMetadataExtractor {
 
     if (field != null) {
       String data = field.getData();
-      log.debug("Mrc Record getMARCControlFieldData: " + data);
+      log.debug3("Mrc Record getMARCControlFieldData: " + data);
       return data;
     } else {
-        log.debug("Mrc Record getMARCControlFieldData: " + dataFieldCode + " return null");
+        log.debug3("Mrc Record getMARCControlFieldData: " + dataFieldCode + " return null");
         return null;
     }
   }
