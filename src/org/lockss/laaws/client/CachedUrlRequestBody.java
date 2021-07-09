d13edce719b53629c16ca23ea300f4307abae83d
Claire Griffin <clairetg@stanford.edu>
2021-07-09 14:22:56 -0700

    Preliminary support for V1 to V2 AU Migration

---
+++ src/org/lockss/laaws/client/CachedUrlRequestBody.java
@@ -0,0 +1,179 @@
+package org.lockss.laaws.client;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.util.Map;
+import java.util.Set;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okhttp3.internal.Util;
+import okio.BufferedSink;
+import okio.Okio;
+import okio.Source;
+import org.apache.commons.io.output.UnsynchronizedByteArrayOutputStream;
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.StatusLine;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.impl.io.DefaultHttpResponseWriter;
+import org.apache.http.impl.io.HttpTransportMetricsImpl;
+import org.apache.http.impl.io.SessionOutputBufferImpl;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.message.BasicStatusLine;
+import org.lockss.plugin.AuUtil;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.util.CIProperties;
+import org.lockss.util.Logger;
+
+public class CachedUrlRequestBody extends RequestBody {
+
+  private static final Logger log = Logger.getLogger(CachedUrlRequestBody.class);
+  protected static StatusLine STATUS_LINE_OK =
+    new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), 200, "OK");
+
+  private final MediaType contentType;
+  private final CachedUrl artifactCu;
+
+  public CachedUrlRequestBody(MediaType contentType, CachedUrl cu) {
+    if (cu == null) {
+      throw new NullPointerException("cachedUrl == null");
+    }
+    this.contentType = contentType;
+    this.artifactCu = cu;
+  }
+
+  /**
+   * Adapter that takes an {@code CachedUrl} and returns an InputStream containing an HTTP response stream
+   * representation of the artifact.
+   *
+   * @return An {@code InputStream} containing an HTTP response stream representation of the artifact.
+   * @throws IOException
+   * @throws HttpException
+   */
+  public InputStream getHttpResponseStreamFromCachedUrl()
+    throws IOException {
+    InputStream httpResponse = getHttpResponseStreamFromHttpResponse(
+      getHttpResponseFromCachedUrl()
+    );
+    return httpResponse;
+  }
+
+  /**
+   * Adapts an {@code HttpResponse} object to an InputStream containing a HTTP response stream representation of the
+   * {@code HttpResponse} object.
+   *
+   * @param response A {@code HttpResponse} to adapt.
+   * @return An {@code InputStream} containing a HTTP response stream representation of this {@code HttpResponse}.
+   * @throws IOException
+   */
+  public InputStream getHttpResponseStreamFromHttpResponse(HttpResponse response)
+    throws IOException {
+    // Return the concatenation of the header and content streams
+    return new SequenceInputStream(
+      new ByteArrayInputStream(getHttpResponseHeader(response)),
+      response.getEntity().getContent()
+    );
+  }
+
+  /**
+   * Return a byte arroy representation of an HttpResponse
+   *
+   * @param response
+   * @return
+   * @throws IOException
+   */
+  public byte[] getHttpResponseHeader(HttpResponse response) throws IOException {
+    UnsynchronizedByteArrayOutputStream headerStream = new UnsynchronizedByteArrayOutputStream();
+
+    // Create a new SessionOutputBuffer from the OutputStream
+    SessionOutputBufferImpl outputBuffer = new SessionOutputBufferImpl(
+      new HttpTransportMetricsImpl(), 4096);
+    outputBuffer.bind(headerStream);
+
+    // Write the HTTP response header
+    writeHttpResponseHeader(response, outputBuffer);
+
+    // Flush anything remaining in the buffer
+    outputBuffer.flush();
+
+    return headerStream.toByteArray();
+  }
+
+  /**
+   * Writes a {@code HttpResponse} object's HTTP status and headers to an {@code OutputStream}.
+   *
+   * @param response     A {@code HttpResponse} whose HTTP status and headers will be written to the {@code OutputStream}.
+   * @param outputBuffer The {@code OutputStream} to write to.
+   * @throws IOException
+   */
+  private static void writeHttpResponseHeader(HttpResponse response,
+    SessionOutputBufferImpl outputBuffer) throws IOException {
+    try {
+      // Write the HTTP response header
+      DefaultHttpResponseWriter responseWriter = new DefaultHttpResponseWriter(outputBuffer);
+      responseWriter.write(response);
+    } catch (HttpException e) {
+      log.error(
+        "Caught HttpException while attempting to write the headers of an HttpResponse using DefaultHttpResponseWriter");
+      throw new IOException(e);
+    }
+  }
+
+  /**
+   * Adapter that takes an {@code CachedUrl} and returns an Apache {@code HttpResponse} object representation of
+   * the artifact.
+   *
+   * @return An {@code HttpResponse} object containing a representation of the artifact.
+   * @throws HttpException
+   * @throws IOException
+   */
+  public HttpResponse getHttpResponseFromCachedUrl() {
+    // Craft a new HTTP response object representation from the artifact
+    BasicHttpResponse response = new BasicHttpResponse(STATUS_LINE_OK);
+    // Create an InputStreamEntity from artifact InputStream
+    try {
+      InputStream is = artifactCu.getUnfilteredInputStream();
+      response.setEntity(new InputStreamEntity(artifactCu.getUnfilteredInputStream()));
+      // Add artifact headers into HTTP response
+      CIProperties props = artifactCu.getProperties();
+      if (props != null) {
+        ((Set<String>) ((Map) props).keySet()).forEach(
+          key -> response.addHeader(key, props.getProperty(key)));
+      }
+    }
+    catch (Exception ex) {
+      log.error("Unable to open input stream for " + artifactCu.getUrl(), ex);
+      AuUtil.safeRelease(artifactCu);
+    }
+    return response;
+  }
+
+  @Override
+  public MediaType contentType() {
+    return contentType;
+  }
+
+  @Override
+  public long contentLength() throws IOException {
+    //return inputStream.available() == 0 ? -1 : inputStream.available();
+    return -1L;
+  }
+
+  @Override
+  public void writeTo(BufferedSink sink) throws IOException {
+    Source source = null;
+    try {
+      InputStream inputStream = getHttpResponseStreamFromCachedUrl();
+      if (inputStream != null) {
+        source = Okio.source(inputStream);
+        sink.writeAll(source);
+      }
+    } finally {
+      Util.closeQuietly(source);
+      AuUtil.safeRelease(artifactCu);
+    }
+  }
+}
---
+++ test/src/org/lockss/laaws/TestV2AuMover.java
@@ -0,0 +1,272 @@
+package org.lockss.laaws;
+
+import static org.lockss.laaws.V2AuMover.compileRegexps;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.plugin.PluginManager;
+import org.lockss.test.ConfigurationUtil;
+import org.lockss.test.LockssTestCase;
+import org.lockss.test.MockArchivalUnit;
+import org.lockss.test.MockLockssDaemon;
+import org.lockss.test.MockPlugin;
+
+public class TestV2AuMover extends LockssTestCase {
+
+  V2AuMover auMover;
+  String tempDirPath;
+  String testUser = "tester";
+  String testPassword = "testPass";
+  String defaultCfgUrl = "http://"+ V2AuMover.DEFAULT_HOSTNAME + ":" + V2AuMover.DEFAULT_CFG_PORT;
+  String defaultRsUrl = "http://"+ V2AuMover.DEFAULT_HOSTNAME + ":" + V2AuMover.DEFAULT_RS_PORT;
+  private long numAus;
+  private long numUrls;
+  private long numArtifacts;
+  private long totTime;
+  private long numErrors;
+  private long numBytes;
+  private String aPluginRegex="(aplugin)";
+  private String bPluginRegex="(bplugin)";
+  private String cPluginRegex="(cplugin)";
+  private static int HEADER_LENGTH=4;
+  String[] reportLines = {
+    "Au:au1  urlsMoved: 10  artifactsMoved: 10  bytesMoved: 1000  errors: 0  totalRuntime: 300ms",
+    "",
+    "Au:au2  urlsMoved: 20  artifactsMoved: 33  bytesMoved: 3000  errors: 1  totalRuntime: 1500ms",
+    "cu2 Attempt to move artifact failed.",
+    "",
+    "Au:au3  urlsMoved: 4000  artifactsMoved: 4300  bytesMoved: 100000  errors: 3  totalRuntime: 20s",
+    "cu1: Attempt to move artifact failed.",
+    "cu5: Attempt to commit artifact failed.",
+    "cu80: Attempt to commit artifact failed.",
+    "",
+    "AusMoved: 3  urlsMoved: 4030  artifactsMoved: 4343  bytesMoved: 104000  errors: 4  totalRuntime: 21s"
+  };
+
+  public void setUp() throws Exception {
+    super.setUp();
+    tempDirPath = getTempDir().getAbsolutePath() + File.separator;
+    String reportFile = tempDirPath + "v2AuMigration.txt";
+    ConfigurationUtil.addFromArgs(V2AuMover.PARAM_REPORT_FILE,reportFile);
+    auMover = new V2AuMover();
+  }
+
+  public void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  public void testIsAvailable() {
+    MockArchivalUnit mau = new MockArchivalUnit();
+    assertTrue(auMover.isAvailable());
+    auMover.getAuMoveQueue().add(mau);
+    assertFalse(auMover.isAvailable());
+  }
+
+  public void testInitRequest() {
+    auMover = new V2AuMover();
+    // test file defaults
+    auMover.initRequest(null, testUser,testPassword);
+    // check the locals
+    assertEquals(defaultCfgUrl,auMover.getCfgAccessUrl());
+    assertEquals(defaultRsUrl,auMover.getRsAccessUrl());
+    assertEquals(testUser,auMover.getUserName());
+    assertEquals(testPassword, auMover.getUserPass());
+
+    // test a different host
+    String base="http://mockhost:";
+    String user="user1";
+    String passwd="passwd1";
+    auMover = new V2AuMover();
+    auMover.initRequest("mockhost", user,passwd);
+    assertEquals(base+24620,auMover.getCfgAccessUrl());
+    assertEquals(base+24610,auMover.getRsAccessUrl());
+    assertEquals(user,auMover.getUserName());
+    assertEquals(passwd, auMover.getUserPass());
+
+    // test config from configuration file
+    ConfigurationUtil.addFromArgs(
+      V2AuMover.PARAM_HOSTNAME,"test.com",
+      V2AuMover.PARAM_CFG_PORT, "25620",
+      V2AuMover.PARAM_RS_PORT, "25610");
+    base="http://test.com:";
+    user="user2";
+    passwd="passwd2";
+    auMover = new V2AuMover();
+    auMover.initRequest(null, user, passwd);
+    assertEquals(base+25620,auMover.getCfgAccessUrl());
+    assertEquals(base+25610,auMover.getRsAccessUrl());
+    assertEquals(user,auMover.getUserName());
+    assertEquals(passwd, auMover.getUserPass());
+
+    auMover = new V2AuMover();
+    try {
+      // test null entries
+      auMover.initRequest(null, null, null);
+      fail("null input should throw IllegalArgumentException");
+    }
+    catch(IllegalArgumentException iae) {
+
+    }
+
+  }
+
+  public void testReportFile() {
+    auMover.startReportFile();
+    numAus=0;
+    numUrls=0;
+    numArtifacts=0;
+    totTime=0;
+    numErrors=0;
+    numBytes=0;
+    auMover.setCurrentAu("au1");
+    List<String> errs = new ArrayList<>();
+    addAu("au1", 10,10,1000, 300, 0, errs);
+    auMover.updateReport();
+    errs.add("cu2 Attempt to move artifact failed.");
+    addAu("au2", 20,33, 3000, 1500,1, errs);
+    auMover.updateReport();
+    errs.clear();
+    errs.add("cu1: Attempt to move artifact failed.");
+    errs.add("cu5: Attempt to commit artifact failed.");
+    errs.add("cu80: Attempt to commit artifact failed.");
+    addAu("au3",4000,4300,100000,20000, 3, errs);
+    auMover.updateReport();
+    auMover.closeReport();
+    Path myPath = Paths.get(auMover.getReportFileName());
+    List< String > lines = null;
+    try {
+      lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
+    } catch (IOException ioe) {
+      fail("IOException thrown",ioe);
+    }
+    assertEquals(reportLines.length + HEADER_LENGTH, lines.size());
+    // we're only checking the report lines.
+    for(int i=0; i < reportLines.length; i++) {
+      assertEquals(reportLines[i],lines.get(i+HEADER_LENGTH));
+    }
+
+  }
+
+  public void testMoveOneAu() throws Exception {
+    MockArchivalUnit mau = new MockArchivalUnit("mockAuId");
+    MockV2AuMover mover = new MockV2AuMover();
+    mover.moveOneAu(null, testUser,testPassword, mau);
+    assertTrue(mover.getAuMoveQueue().isEmpty());
+    List<String> movedAus = mover.getMovedAus();
+    assertEquals(1, movedAus.size());
+    assertEquals("mockAuId",movedAus.get(0));
+  }
+
+  public void testMoveAllAus() throws Exception{
+    MockV2AuMover mover = new MockV2AuMover();
+    MockLockssDaemon daemon = getMockLockssDaemon();
+    MyMockPluginManager mpm = new MyMockPluginManager();
+    daemon.setPluginManager(mpm);
+    List<ArchivalUnit> aus = makeMockAuList();
+    mpm.setAllAus(aus);
+    mover.setPluginManager(mpm);
+    // move all unfiltered aus
+    mover.moveAllAus(null, testUser,testPassword, null);
+    assertTrue(mover.getAuMoveQueue().isEmpty());
+    List<String> movedAus = mover.getMovedAus();
+    assertEquals(aus.size(), movedAus.size());
+    for (ArchivalUnit au : aus) {
+      assertTrue(movedAus.contains(au.getAuId()));
+    }
+  }
+
+  public void testMoveAllAusWithFilter() throws Exception{
+    MockV2AuMover mover = new MockV2AuMover();
+    MockLockssDaemon daemon = getMockLockssDaemon();
+    MyMockPluginManager mpm = new MyMockPluginManager();
+    daemon.setPluginManager(mpm);
+    List<ArchivalUnit> aus = makeMockAuList();
+    mpm.setAllAus(aus);
+    mover.setPluginManager(mpm);
+    List<String> filters=Arrays.asList(aPluginRegex,bPluginRegex);
+    List<Pattern> selPatterns = compileRegexps(filters);
+    // move all unfiltered aus
+    mover.moveAllAus(null, testUser,testPassword, selPatterns);
+    assertTrue(mover.getAuMoveQueue().isEmpty());
+    List<String> movedAus = mover.getMovedAus();
+    assertNotEquals(aus.size(),movedAus.size());
+    assertEquals(5, movedAus.size());
+    assertDoesNotContain(movedAus,aus.get(5).getAuId());
+  }
+
+  private void addAu(String auName, long urls, long artifacts, long bytes, long runTime, long errors,List<String> errs){
+    numAus++;
+    numUrls+=urls;
+    numArtifacts+=artifacts;
+    numBytes+=bytes;
+    totTime+=runTime;
+    numErrors+=errors;
+    auMover.setCurrentAu(auName);
+    auMover.setAuCounters(urls, artifacts, bytes, runTime, errors,errs);
+    auMover.setTotalCounters(numAus, numUrls, numArtifacts, numBytes,totTime, numErrors);
+
+  }
+
+  List<ArchivalUnit> makeMockAuList() {
+    // Test with dummy AUs
+    MockPlugin plugin = new MockPlugin();
+    plugin.setPluginId("aplugin");
+    ArchivalUnit au1 = new MockArchivalUnit(plugin, "aplugin|auid1");
+    ArchivalUnit au2 = new MockArchivalUnit(plugin, "aplugin|auid2");
+    ArchivalUnit au3 = new MockArchivalUnit(plugin, "aplugin|auid3");
+    plugin = new MockPlugin();
+    plugin.setPluginId("bplugin");
+    ArchivalUnit au4 = new MockArchivalUnit(plugin, "bplugin|auid1");
+    ArchivalUnit au5 = new MockArchivalUnit(plugin, "bplugin|auid2");
+    plugin.setPluginId("cplugin");
+    ArchivalUnit au6 = new MockArchivalUnit(plugin, "cplugin|auid1");
+    return Arrays.asList(au1, au2, au3,au4, au5,au6);
+  }
+
+  class MockV2AuMover extends V2AuMover {
+    List<String> movedAus= new ArrayList<>();
+
+   public MockV2AuMover() {
+      super();
+    }
+
+    protected void moveAu(ArchivalUnit au) throws IOException {
+      movedAus.add(au.getAuId());
+      getAuMoveQueue().remove(au);
+      setCurrentAu(null);
+      moveNextAu();
+    }
+
+    protected List<String> getMovedAus() {
+      return movedAus;
+    }
+
+    protected void setPluginManager(PluginManager pmgr) {
+     super.pluginManager = pmgr;
+    }
+
+  }
+
+  class MyMockPluginManager extends PluginManager {
+    List<ArchivalUnit> allAus = new ArrayList<>();
+
+    void setAllAus(List<ArchivalUnit> allAus) {
+      this.allAus = allAus;
+    }
+
+    public List<ArchivalUnit> getAllAus() {
+      return allAus;
+    }
+
+  }
+
+}
