61f8836cb83df1789c02801f59294cf0baa13a0e
Mary-Ellen <maryellen@lockss.org>
2021-07-14 17:23:28 -0700

    Publisher moving to Wiley. Also, fix manifest page checker to fail when there are now issues.

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -2668,7 +2668,7 @@ while (my $line = <>) {
       if ($req->url ne $resp->request->uri) {
               $vol_title = $resp->request->uri;
               $result = "Redirected";
-      } elsif (defined($man_contents) && ($man_contents =~ m/$clockss_tag/)) {
+      } elsif (defined($man_contents) && ($man_contents =~ m/$clockss_tag/) && ($man_contents =~ m/\/content\/journals\/$param{journal_id}\/$param{volume_name}\//)) {
         $result = "Manifest"
       } else {
         $result = "--NO_TAG--"
