f7df63b03bcd9100bb4264e4b29f381b7f968cb4
Mark McAdam <mol3earth@gmail.com>
2021-07-19 16:37:12 -0700

    refactor

--- plugins/src/org/lockss/plugin/archiveit/ArchiveItApiCrawlSeedFactory.java
+++ plugins/src/org/lockss/plugin/archiveit/ArchiveItApiCrawlSeedFactory.java
@@ -4,11 +4,11 @@ import org.lockss.crawler.CrawlSeed;
 import org.lockss.crawler.CrawlSeedFactory;
 import org.lockss.daemon.Crawler;
 
-public class ArchiveItCrawlSeedFactory implements CrawlSeedFactory {
+public class ArchiveItApiCrawlSeedFactory implements CrawlSeedFactory {
 
   @Override
   public CrawlSeed createCrawlSeed(Crawler.CrawlerFacade facade) {
-    return new ArchiveItCrawlSeed(facade);
+    return new ArchiveItApiCrawlSeed(facade);
   }
 
 }
