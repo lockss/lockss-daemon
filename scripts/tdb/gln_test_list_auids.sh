a409e3c5588d3782584da0f796e2ef8818783a73
Mary-Ellen <maryellen@lockss.org>
2021-07-29 13:53:32 -0700

    Typo.

--- scripts/tdb/gln_test_list_auids.sh
+++ scripts/tdb/gln_test_list_auids.sh
@@ -9,8 +9,8 @@ mkdir -p $tpath
   echo "taylor_and_francis.tdb" > $tpath/glnfilter
   echo "sage_publications.tdb" >> $tpath/glnfilter
 
-  ls tdb/prod/{,_retired}/*.tdb > $tpath/glnlist
-  ls tdb/clockssingest/{,_retired}/*.tdb > $tpath/clocksslist
+  ls tdb/prod/{,_retired/}*.tdb > $tpath/glnlist
+  ls tdb/clockssingest/{,_retired/}*.tdb > $tpath/clocksslist
   #do not look at Springer AUids.
   diff $tpath/glnlist $tpath/clocksslist | grep "< " | sed 's/..//' | grep -v springer > $tpath/notclockss
   echo "american_medical_association.tdb" >> $tpath/notclockss
@@ -18,15 +18,13 @@ mkdir -p $tpath
   #echo "centro_de_filosofia_da_universidade_de_lisboa.tdb" >> $tpath/notclockss
   diff $tpath/glnlist $tpath/notclockss | grep "< " | sed 's/..//' > $tpath/glnAndclockss
 
-#Report any AU in the gln marked notReady, wanted, or testing.
-#scripts/tdb/tdbout -NWT -t auid,plugin,publisher:info[tester],status,year tdb/prod/*.tdb > $tpath/glntest_a
-echo "" > $tpath/glntest_a #clear the file.
+  echo "" > $tpath/glntest_a #clear the file.
 
-#Report any AU in the gln marked manifest that does not have a file in clockss.
-for file in `cat $tpath/notclockss`
-do
-  scripts/tdb/tdbout -M -t publisher,plugin,auid,publisher:info[tester],status,year tdb/prod/$file >> $tpath/glntest_a #don't clear the file in the loop
-done
+  #Report any AU in the gln marked manifest that does not have a file in clockss.
+  for file in `cat $tpath/notclockss`
+  do
+    scripts/tdb/tdbout -M -t publisher,plugin,auid,publisher:info[tester],status,year tdb/prod/$file >> $tpath/glntest_a #don't clear the file in the loop
+  done
 
 #Report some AUs in the gln marked manifest that do not have an equivalent in clockss.
 for file in `cat $tpath/glnAndclockss`
