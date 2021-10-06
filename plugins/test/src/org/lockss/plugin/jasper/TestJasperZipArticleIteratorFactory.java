765aba0e5851db2811830f8edd0941fb632a0508
Thib Guicherd-Callin <thib@cs.stanford.edu>
2021-10-06 13:44:40 -0700

    Prototype plugin for the Jasper project.
    Needs more work (e.g. crawl seed, crawl URL comparator, switch POST request to real URL fetcher) and benefits from upcoming developments (e.g. crawler facade state storage, convenience method to add body to POST request), but good enough to merge into the main branch.
    
    Squashed commit of the following:
    
    commit 4dcf8834520023dc2cf2bb1d9644567e94092c21
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Wed Oct 6 00:10:54 2021 -0700
    
        Return to normal.
    
    commit 185bb150528d3554911e408e6635f530fc634a24
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Wed Oct 6 00:10:17 2021 -0700
    
        URL consumer to do store-at-orig. Tweak article iterator/metadata
        extractor to reach JSON metadata.
    
    commit 7bcbeca6c92e9e86932c26e6e7f6b3e72b2a9b5a
    Author: Mark McAdam <mol3earth@gmail.com>
    Date:   Tue Oct 5 22:39:51 2021 -0700
    
        added Test for Jasper article iterator. slight mods to other classes.
    
    commit 8d07ff3193c8df5d002473ce1019eb4e5cf61ae0
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Tue Oct 5 18:43:47 2021 -0700
    
        Move logic that builds the custom Authorization: header out to a URL
        fetcher factory.
    
    commit 5eae1f9749093f92259951476ec13f17f76863af
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Tue Oct 5 18:42:57 2021 -0700
    
        Return this to normal.
    
    commit d60d9d216d33c8b18d5d3f63e0f4421e34f1da02
    Author: Mark McAdam <mol3earth@gmail.com>
    Date:   Tue Oct 5 17:04:39 2021 -0700
    
        added ArticleIterator, MetadataExtractor and test for Jasper
    
    commit acdd25b58b82ea62ab659fad032727ca2074d115
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Tue Oct 5 03:32:06 2021 -0700
    
        First attempt at Jasper.

---
+++ plugins/test/src/org/lockss/plugin/jasper/TestJasperZipArticleIteratorFactory.java
@@ -0,0 +1,160 @@
+package org.lockss.plugin.jasper;
+
+import org.lockss.config.ConfigManager;
+import org.lockss.config.Configuration;
+import org.lockss.daemon.CachedUrlSetSpec;
+import org.lockss.daemon.ConfigParamDescr;
+import org.lockss.daemon.SingleNodeCachedUrlSetSpec;
+import org.lockss.extractor.ArticleMetadataExtractor;
+import org.lockss.extractor.FileMetadataExtractor;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.*;
+import org.lockss.plugin.simulated.SimulatedArchivalUnit;
+import org.lockss.plugin.simulated.SimulatedContentGenerator;
+import org.lockss.state.NodeManager;
+import org.lockss.test.ArticleIteratorTestCase;
+import org.lockss.test.ConfigurationUtil;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
+public class TestJasperZipArticleIteratorFactory extends ArticleIteratorTestCase {
+
+  private SimulatedArchivalUnit sau;	// Simulated AU to generate content
+
+  private final String PLUGIN_NAME = "org.lockss.plugin.jasper.ClockssJasperPlugin";
+  static final String BASE_URL_KEY = ConfigParamDescr.BASE_URL.getKey();
+  static final String COLLECTION_KEY = ConfigParamDescr.COLLECTION.getKey();
+  private final String BASE_URL = "https://warcs.archive-it.org/";
+  private final String DOWNLOAD_URL = "https://archive.org/download/";
+  private final String COLLECTION = "111113";
+  private final Configuration AU_CONFIG = ConfigurationUtil.fromArgs(
+      BASE_URL_KEY, BASE_URL,
+      COLLECTION_KEY, COLLECTION,
+      "user_pass", "user:pass");
+  private static final int DEFAULT_FILESIZE = 3000;
+
+  protected String cuRole = null;
+  ArticleMetadataExtractor.Emitter emitter;
+  protected boolean emitDefaultIfNone = false;
+  FileMetadataExtractor me = null;
+  MetadataTarget target;
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    String tempDirPath = setUpDiskSpace();
+
+    au = createAu();
+    sau = PluginTestUtil.createAndStartSimAu(simAuConfig(tempDirPath));
+  }
+
+  @Override
+  public void tearDown() throws Exception {
+    sau.deleteContentTree();
+    super.tearDown();
+  }
+
+  protected ArchivalUnit createAu() throws ArchivalUnit.ConfigurationException {
+    return
+        PluginTestUtil.createAndStartAu(PLUGIN_NAME,  AU_CONFIG);
+  }
+
+  Configuration simAuConfig(String rootPath) {
+    Configuration conf = ConfigManager.newConfiguration();
+    conf.put("root", rootPath);
+    conf.put(BASE_URL_KEY, DOWNLOAD_URL);
+    conf.put("depth", "1");
+    conf.put("branch", "1");
+    conf.put("numFiles", "4");
+    conf.put("fileTypes",
+        "" + (SimulatedContentGenerator.FILE_TYPE_HTML |
+            SimulatedContentGenerator.FILE_TYPE_PDF ));
+    conf.put("binFileSize", ""+DEFAULT_FILESIZE);
+    return conf;
+  }
+
+  public void testUrlsWithPrefixes() throws Exception {
+    SubTreeArticleIterator artIter = createSubTreeIter();
+    Pattern pat = getPattern(artIter);
+    log.info(pat.toString());
+    String targzPath = "JournalofSuccessfulDataTransfers/rama-2021-06-25-11-25-53.tar.gz!";
+    String metadataPath = "/2051-5960/00003741594643f4996e2555a01e03c7/data/metadata.json";
+    assertMatchesRE(pat, DOWNLOAD_URL + targzPath + metadataPath);
+  }
+/*
+  //
+  // simAU was created with only one depth, but 5 branches
+  // 3 filetypes (html & pdf & txt) and 4 files of each type
+  // So the total number of files of all types is 40 (5 * (4*2)) + 4 RIS (handled independently)
+  // simAU file structures looks like this branch01/01file.html or branch04/08file.pdf, etc
+  //
+  public void testCreateArticleFiles() throws Exception {
+    PluginTestUtil.crawlSimAu(sau);
+
+    //  Go through the simulated content you just crawled and modify the results to emulate
+    //  what you would find in a "real" crawl with Atypon:
+    //  <base_url>/doi/{abs,pdf,full,pdfplus}/10.1137/X.XXXXXX
+    //  Currently this test doesn't create any "ref" or "suppl" files
+
+    // turn xxfile.html in to both abstracts and fulls
+    String pat1 = "branch(\\d+)/(\\d+)file\\.html";
+    String pdf = "JournalofSuccessfulDataTransfers/rama-2021-06-25-11-25-53\\.tar\\.gz!/$1/data/$2\\.pdf";
+    String json = "JournalofSuccessfulDataTransfers/rama-2021-06-25-11-25-53\\.tar\\.gz!/$1/data/metadata/metadata\\.json";
+    PluginTestUtil.copyAu(sau, au, ".*\\.html$", pat1, pdf);
+    PluginTestUtil.copyAu(sau, au, ".*\\.html$", pat1, json);
+
+    for (CachedUrl cu : AuUtil.getCuIterable(au)) {
+      String url = cu.getUrl();
+      log.info(url);
+    }
+
+    Iterator<ArticleFiles> it = au.getArticleIterator(MetadataTarget.Any());
+    int count = 0;
+    int countFullText= 0;
+    int countMetadata = 0;
+    while (it.hasNext()) {
+      ArticleFiles af = it.next();
+      log.info(af.toString());
+      count ++;
+      //log.info(af.toString());
+      CachedUrl cu = af.getFullTextCu();
+      if ( cu != null) {
+        ++countFullText;
+      }
+      cu = af.getRoleCu(ArticleFiles.ROLE_ARTICLE_METADATA);
+      if (cu != null) {
+        ++countMetadata; //could be ris file or abstract or full
+      }
+    }
+    // potential article count is 20 (5 branches * 4 files each branch) = 20 (4 RIS files don't count as articles)
+    // subtract the one where we removed everything
+    // subtract the one for abstract only. This would only count as an article
+    // in an "abstracts only AU
+    int expCount = 1;
+
+    log.debug3("Article count is " + count);
+    assertEquals(expCount, count);
+
+    // you will only get a full text for combos with pdf, pdfplus or full
+    // so there are only 2 cases - the one with nothing; the one with only abstract;
+    assertEquals(1, countFullText);
+
+    // you need to have either an abstract or a full text to have metadata 4 cases don't have either
+    // we're only going to get the right information if the TARGET is not NULL and set to other than isArticle()
+    //assertEquals(16, countMetadata);
+    //Now that we pick up downloaded RIS urls, we should always have this
+    // note that with only an abstract, we no longer create an artilefiles
+    assertEquals(expCount, countMetadata); //don't count the one where we removed everything
+  }
+*/
+  private void deleteBlock(CachedUrl cu) throws IOException {
+    //log.info("deleting " + cu.getUrl());
+    CachedUrlSetSpec cuss = new SingleNodeCachedUrlSetSpec(cu.getUrl());
+    ArchivalUnit au = cu.getArchivalUnit();
+    CachedUrlSet cus = au.makeCachedUrlSet(cuss);
+    NodeManager nm = au.getPlugin().getDaemon().getNodeManager(au);
+    nm.deleteNode(cus);
+  }
+}
