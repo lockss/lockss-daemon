b457e20f0d06d4cfdafb31156dbbc0e3ac00cf9a
Mark McAdam <mol3earth@gmail.com>
2022-01-14 13:23:14 -0800

    fixed Unit Test for inter-research  article iterator

--- plugins/test/src/org/lockss/plugin/interresearch/TestInterResearchArticleIterator.java
+++ plugins/test/src/org/lockss/plugin/interresearch/TestInterResearchArticleIterator.java
@@ -54,7 +54,7 @@ public class TestInterResearchArticleIterator extends LockssTestCase {
     conf.put("root", rootPath);
     conf.put("base_url", BASE_URL);
     conf.put("depth", "1");
-    conf.put("branch", "4");
+    conf.put("branch", "7");
     conf.put("numFiles", "7");
     conf.put("fileTypes",
         "" + (  SimulatedContentGenerator.FILE_TYPE_HTML
@@ -68,7 +68,7 @@ public class TestInterResearchArticleIterator extends LockssTestCase {
     conf.put("base_url", BASE_URL);
     conf.put("journal_id", "meps");
     conf.put("num_issue_range", "143-147");
-    conf.put("year", "2008");
+    conf.put("year", "2021");
     return conf;
   }
 
@@ -76,34 +76,48 @@ public class TestInterResearchArticleIterator extends LockssTestCase {
     log.setLevel("debug3");
     PluginTestUtil.crawlSimAu(sau);
     // https://www.int-res.com/abstracts/dao/v143/p205-226/
-    String pat1 = "branch(\\d+)/(\\d+)file\\.html";
-    String rep1 = "abstracts/meps/v$1/p$2-001.html";
-    PluginTestUtil.copyAu(sau, nau, ".*\\.html$", pat1, rep1);
+    String pat_abs00 = "branch(\\d+)/00(\\d)file\\.html";
+    String pat_abs0 = "branch(\\d+)/0([1-9]{2})file\\.html";
+    String pat_abs = "branch(\\d+)/([1-9]{3})file\\.html";
+    String rep1 = "abstracts/meps/v14$1/p$2-001";
+    PluginTestUtil.copyAu(sau, nau, ".*\\.html$", pat_abs00, rep1);
+    PluginTestUtil.copyAu(sau, nau, ".*\\.html$", pat_abs0, rep1);
+    PluginTestUtil.copyAu(sau, nau, ".*\\.html$", pat_abs, rep1);
     // https://www.int-res.com/articles/feature/d143p205.pdf
-    // https://www.int-res.com/articles/feature/d143p205.pdf
-    String pat2 = "branch(\\d+)/(\\d+)file\\.pdf";
-    String rep2 = "articles/feature/m$1p$2.pdf";
-    PluginTestUtil.copyAu(sau, nau, ".*\\.pdf$", pat2, rep2);
+    // https://www.int-res.com/articles/meps_oa/d143p005.pdf
+    // https://www.int-res.com/articles/meps2021/143/d143p005.pdf
+    String pdf_pat_feat = "branch([3-7])/00([2,7])file\\.pdf";
+    String pdf_rep_feat = "articles/feature/m14$1p00$2.pdf";
+    PluginTestUtil.copyAu(sau, nau, ".*\\.pdf$", pdf_pat_feat, pdf_rep_feat);
+    String pdf_pat_oa = "branch([3-7])/00([1])file\\.pdf";
+    String pdf_rep_oa = "articles/meps_oa/m14$1p00$2.pdf";
+    PluginTestUtil.copyAu(sau, nau, ".*\\.pdf$", pdf_pat_oa, pdf_rep_oa);
+    String pdf_pat_main = "branch([3-7])/(\\d\\d[0,3,4,5,6,8,9])file\\.pdf";
+    String pdf_rep_main = "articles/meps2021/14$1/m14$1p$2.pdf";
+    PluginTestUtil.copyAu(sau, nau, ".*\\.pdf$", pdf_pat_main, pdf_rep_main);
 
     // Remove some URLs
     int deleted = 0;
     for (CachedUrl cu : AuUtil.getCuIterable(nau)) {
       String url = cu.getUrl();
       log.info("url: " + url);
-      if (url.endsWith("v2/p001-001") || url.endsWith("m2p001.pdf")) {
+      // delete a few pdfs, and one html page
+      if (url.endsWith("v143/p1-001") || url.endsWith("m143p001.pdf") || url.endsWith("m147p006.pdf") || url.endsWith("m145p003.pdf")) {
         deleteBlock(cu);
         ++deleted;
       }
     }
     log.info("DELETED = " + deleted);
-    //assertEquals(1, deleted);
+    assertEquals(4, deleted);
 
     Iterator<ArticleFiles> it = nau.getArticleIterator();
+
     int count = 0;
     int countHtmlOnly = 0;
-    int countPdfOnly = 0;
+    int countPdf = 0;
     while (it.hasNext()) {
       ArticleFiles af = it.next();
+
       log.info(af.toString());
       CachedUrl ftcu = af.getFullTextCu();
       String fturl = ftcu.getUrl();
@@ -114,16 +128,16 @@ public class TestInterResearchArticleIterator extends LockssTestCase {
       // The full text CU will be the PDF unless one doesn't exist
       if (af.getRoleUrl(ArticleFiles.ROLE_FULL_TEXT_PDF) == null) {
         ++countHtmlOnly;
-        assertEquals(af.getRoleUrl(ArticleFiles.ROLE_FULL_TEXT_HTML), fturl);
+        assertEquals(af.getRoleUrl(ArticleFiles.ROLE_ABSTRACT), fturl);
       }
       if (af.getRoleUrl(ArticleFiles.ROLE_FULL_TEXT_PDF) == fturl) {
-        ++countPdfOnly;
+        ++countPdf;
       }
     }
     log.debug3("Article count is " + count);
-    //assertEquals(28, count);
-    //assertEquals(4, countHtmlOnly);
-    //assertEquals(24, countPdfOnly);
+    assertEquals(34, count);
+    //assertEquals(2, countHtmlOnly);
+    //assertEquals(32, countPdf);
   }
 
   private void deleteBlock(CachedUrl cu) throws IOException {
