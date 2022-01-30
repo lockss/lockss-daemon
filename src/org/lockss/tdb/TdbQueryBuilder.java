d7da47fa6cd36cf9311f51258d757cce4f1f231f
Thib Guicherd-Callin <thib@cs.stanford.edu>
2022-01-29 17:56:35 -0800

    Restore Environmental Health Perspectives Plugin and Environmental Health Perspectives (gpo-pilot).

--- src/org/lockss/tdb/TdbQueryBuilder.java
+++ src/org/lockss/tdb/TdbQueryBuilder.java
@@ -684,6 +684,7 @@ public class TdbQueryBuilder extends TdbQueryParserBaseListener {
                  "edu.yale.library.lockss.plugin.intermarium.IntermariumPlugin",
                  "edu.yale.library.lockss.plugin.mitejmes.MITEJMESPlugin",
                  "gov.gpo.access.permanent.plugin.amberwaves.Amber_WavesPlugin", // gpo-pilot
+                 "gov.gpo.access.permanent.plugin.Environmental_Health_PerspectivesPlugin", // gpo-pilot
                  "gov.loc.plugin.CJPentecostalCharismaticResearchPlugin",
                  "gov.loc.plugin.TESLEJPlugin",
                  "nz.ac.otago.plugin.scholia.ScholiaPlugin",
