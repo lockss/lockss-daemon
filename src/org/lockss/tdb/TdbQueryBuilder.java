9bcf0d1f5eee5f8d1289a969323cbd23de0da3a0
Thib Guicherd-Callin <thib@cs.stanford.edu>
2022-01-30 15:27:05 -0800

    Restore Monthly Energy Review Plugin and Monthly Energy Review (gpo-pilot).

--- src/org/lockss/tdb/TdbQueryBuilder.java
+++ src/org/lockss/tdb/TdbQueryBuilder.java
@@ -687,6 +687,7 @@ public class TdbQueryBuilder extends TdbQueryParserBaseListener {
                  "gov.gpo.access.permanent.plugin.Environmental_Health_PerspectivesPlugin", // gpo-pilot
                  "gov.gpo.access.permanent.plugin.fbilawenforcementbulletin.FBI_Law_Enforcement_BulletinPlugin", // gpo-pilot
                  "gov.gpo.access.permanent.plugin.humanities.HumanitiesPlugin", // gpo-pilot
+                 "gov.gpo.access.permanent.plugin.monthlyenergyreview.MonthlyEnergyReviewPlugin", // gpo-pilot
                  "gov.loc.plugin.CJPentecostalCharismaticResearchPlugin",
                  "gov.loc.plugin.TESLEJPlugin",
                  "nz.ac.otago.plugin.scholia.ScholiaPlugin",
