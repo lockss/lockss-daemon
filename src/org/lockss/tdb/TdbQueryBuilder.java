9f7406b85296891ed4690445d76320441a8065cb
Thib Guicherd-Callin <thib@cs.stanford.edu>
2022-01-30 14:58:08 -0800

    Resotre Humanities Plugin and Humanities: The Magazine of the National Endowment for the Humanities (gpo-pilot).

--- src/org/lockss/tdb/TdbQueryBuilder.java
+++ src/org/lockss/tdb/TdbQueryBuilder.java
@@ -686,6 +686,7 @@ public class TdbQueryBuilder extends TdbQueryParserBaseListener {
                  "gov.gpo.access.permanent.plugin.amberwaves.Amber_WavesPlugin", // gpo-pilot
                  "gov.gpo.access.permanent.plugin.Environmental_Health_PerspectivesPlugin", // gpo-pilot
                  "gov.gpo.access.permanent.plugin.fbilawenforcementbulletin.FBI_Law_Enforcement_BulletinPlugin", // gpo-pilot
+                 "gov.gpo.access.permanent.plugin.humanities.HumanitiesPlugin", // gpo-pilot
                  "gov.loc.plugin.CJPentecostalCharismaticResearchPlugin",
                  "gov.loc.plugin.TESLEJPlugin",
                  "nz.ac.otago.plugin.scholia.ScholiaPlugin",
