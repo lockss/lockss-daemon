6de7de6f44146bf6b53e23a9edcc6f634a2a0f63
Mark McAdam <mol3earth@gmail.com>
2022-02-01 16:18:59 -0800

    added custom ZipXmlArticleIteratorFactory with article_pattern to exclude MACOS

---
+++ plugins/src/org/lockss/plugin/clockss/hmp/HmpMediaZipXmlArticleIteratorFactory.java
@@ -0,0 +1,14 @@
+package org.lockss.plugin.clockss.hmp;
+
+import org.lockss.plugin.clockss.SourceZipXmlArticleIteratorFactory;
+
+public class HmpMediaZipXmlArticleIteratorFactory extends SourceZipXmlArticleIteratorFactory {
+
+  protected static final String ALL_BUT_MACOS_ZIP_XML_PATTERN_TEMPLATE =
+      "\"%s[^/]+/.*\\.zip!/(?!__MACOSX).*\\.xml$\", base_url";
+
+  protected String getIncludePatternTemplate() {
+    return ALL_BUT_MACOS_ZIP_XML_PATTERN_TEMPLATE;
+  }
+
+}
