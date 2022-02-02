81fd501b8167fa66aecd5d7ee24f9bceb19fdc9e
Thib Guicherd-Callin <thib@cs.stanford.edu>
2022-02-02 10:12:46 -0800

    Kim clarifies publisher name is HMP Global.

--- plugins/src/org/lockss/plugin/clockss/hmp/HmpGlobalZipXmlArticleIteratorFactory.java
+++ plugins/src/org/lockss/plugin/clockss/hmp/HmpGlobalZipXmlArticleIteratorFactory.java
@@ -2,7 +2,7 @@ package org.lockss.plugin.clockss.hmp;
 
 import org.lockss.plugin.clockss.SourceZipXmlArticleIteratorFactory;
 
-public class HmpMediaZipXmlArticleIteratorFactory extends SourceZipXmlArticleIteratorFactory {
+public class HmpGlobalZipXmlArticleIteratorFactory extends SourceZipXmlArticleIteratorFactory {
 
   protected static final String ALL_BUT_MACOS_ZIP_XML_PATTERN_TEMPLATE =
       "\"%s[^/]+/.*\\.zip!/(?!__MACOSX).*\\.xml$\", base_url";
