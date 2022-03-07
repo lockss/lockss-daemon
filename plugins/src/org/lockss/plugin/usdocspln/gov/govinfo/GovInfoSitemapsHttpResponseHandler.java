79cd6db18051f1352a6a3327c296da91a5a39350
Thib Guicherd-Callin <thib@cs.stanford.edu>
2022-03-07 11:21:15 -0800

    Retry more times and wait longer on HTTP 504 (common with this platform).

--- plugins/src/org/lockss/plugin/usdocspln/gov/govinfo/GovInfoSitemapsHttpResponseHandler.java
+++ plugins/src/org/lockss/plugin/usdocspln/gov/govinfo/GovInfoSitemapsHttpResponseHandler.java
@@ -1,9 +1,43 @@
+/*
+
+Copyright (c) 2000-2022, Board of Trustees of Leland Stanford Jr. University
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
 package org.lockss.plugin.usdocspln.gov.govinfo;
 
 import org.lockss.daemon.PluginException;
 import org.lockss.plugin.ArchivalUnit;
-import org.lockss.util.Logger;
+import org.lockss.util.*;
 import org.lockss.util.urlconn.CacheException;
+import org.lockss.util.urlconn.CacheException.RetryableException;
 import org.lockss.util.urlconn.CacheResultHandler;
 import org.lockss.util.urlconn.CacheResultMap;
 
@@ -50,7 +84,7 @@ public class GovInfoSitemapsHttpResponseHandler implements CacheResultHandler {
           if (fmat.find()) {
             return new CacheException.NoRetryDeadLinkException(responseCode + " Gateway Timeout (non-fatal)");
           }
-          return new CacheException.RetrySameUrlException(responseCode + " Gateway Timeout");
+          return new GovInfoRetryableException(responseCode + " Gateway Timeout");
 
         default:
           logger.warning("Unexpected responseCode (" + responseCode + ") in handleResult(): AU " + au.getName() + "; URL " + url);
@@ -64,4 +98,27 @@ public class GovInfoSitemapsHttpResponseHandler implements CacheResultHandler {
     logger.warning("Unexpected exception (" + ex + ") in handleResult(): AU " + au.getName() + "; URL " + url);
     throw new UnsupportedOperationException("Unexpected exception (" + ex + ")");
   }
+  
+  public static final class GovInfoRetryableException extends RetryableException {
+    
+    public GovInfoRetryableException() {
+      super();
+    }
+    
+    public GovInfoRetryableException(String msg) {
+      super(msg);
+    }
+    
+    @Override
+    public int getRetryCount() {
+      return 20;
+    }
+
+    @Override
+    public long getRetryDelay() {
+      return 30 * Constants.SECOND;
+    }
+    
+  }
+  
 }
