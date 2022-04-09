a6676ba152c192f7e7ca5455d1c6ef07002bd97b
Thib Guicherd-Callin <thib@cs.stanford.edu>
2022-04-08 17:08:02 -0700

    First attempt at fixing MDPI Keepers/KBART anomalies.

--- plugins/src/org/lockss/plugin/clockss/mdpi/MdpiJatsXmlMetadataExtractorFactory.java
+++ plugins/src/org/lockss/plugin/clockss/mdpi/MdpiJatsXmlMetadataExtractorFactory.java
@@ -1,41 +1,39 @@
 /*
- * $Id:$
- */
 
-/*
+Copyright (c) 2000-2022, Board of Trustees of Leland Stanford Jr. University
 
- Copyright (c) 2000-2016 Board of Trustees of Leland Stanford Jr. University,
- all rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
- IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
 
- Except as contained in this notice, the name of Stanford University shall not
- be used in advertising or otherwise to promote the sale, use or other dealings
- in this Software without prior written authorization from Stanford University.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
 
- */
+*/
 
 package org.lockss.plugin.clockss.mdpi;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.lockss.util.*;
 import org.lockss.daemon.*;
@@ -46,9 +44,8 @@ import org.lockss.plugin.clockss.JatsPublishingSchemaHelper;
 import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
 import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
 
-
-
 public class MdpiJatsXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
+  
   private static final Logger log = Logger.getLogger(MdpiJatsXmlMetadataExtractorFactory.class);
 
   private static SourceXmlSchemaHelper JatsPublishingHelper = null;
@@ -102,5 +99,38 @@ public class MdpiJatsXmlMetadataExtractorFactory extends SourceXmlMetadataExtrac
       }
     }
 
+    @Override
+    protected boolean preEmitCheck(SourceXmlSchemaHelper schemaHelper,
+                                   CachedUrl cu,
+                                   ArticleMetadata am) {
+      /*
+       * Some articles were originally delivered with key metadata sufficiently
+       * "off" to cause a starkly visible effect in Keepers/KBART reports, such
+       * as years or volumes that are clearly out of sequence/range. Corrected
+       * versions were subsequently delivered, but we still need to suppress the
+       * incorrect ones. 
+       */
+      String doi = am.get(MetadataField.FIELD_DOI);
+      if ("10.3390/rs61111444".equals(doi) && "61".equals(am.get(MetadataField.FIELD_VOLUME))) {
+        return false;
+      }
+      if ("10.3390/polym10030328".equals(doi) && "2079-6439".equals(am.get(MetadataField.FIELD_EISSN))) {
+        return false;
+      }
+      if ("10.3390/catal10070773".equals(doi) && "2411-5142".equals(am.get(MetadataField.FIELD_EISSN))) {
+        return false;
+      }
+      if ("10.3390/nu12051459".equals(doi) && "2075-1729".equals(am.get(MetadataField.FIELD_EISSN))) {
+        return false;
+      }
+      if ("10.3390/molecules25020281".equals(doi) && "1996-1944".equals(am.get(MetadataField.FIELD_EISSN))) {
+        return false;
+      }
+      if ("10.3390/biochem1010004".equals(doi) && "2020".equals(am.get(MetadataField.FIELD_DATE))) {
+        return false;
+      }
+      return super.preEmitCheck(schemaHelper, cu, am);
+    }
+    
   }
 }
