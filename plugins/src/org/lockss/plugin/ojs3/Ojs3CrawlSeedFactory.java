c3b7bdee7be008e0acdb0afc3c7807f4a4fbe114
Mark McAdam <mol3earth@gmail.com>
2022-04-27 13:24:22 -0700

    refactored duplicate addAStartStem methods in CrawlSeed and FeatureUrlHelper into Ojs3StartStemHelper.

--- plugins/src/org/lockss/plugin/ojs3/Ojs3CrawlSeedFactory.java
+++ plugins/src/org/lockss/plugin/ojs3/Ojs3CrawlSeedFactory.java
@@ -33,17 +33,12 @@
 package org.lockss.plugin.ojs3;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 
 import org.lockss.crawler.*;
 import org.lockss.daemon.Crawler.CrawlerFacade;
-import org.lockss.daemon.ConfigParamDescr;
 import org.lockss.daemon.PluginException;
-import org.lockss.plugin.AuUtil;
 import org.lockss.plugin.ArchivalUnit.ConfigurationException;
-import org.lockss.util.Logger;
 
 /**
  * <p>
@@ -59,10 +54,7 @@ import org.lockss.util.Logger;
  * @since 1.67.5
  */
 public class Ojs3CrawlSeedFactory implements CrawlSeedFactory {
-	private static final String START_STEM_ATTRIBUTE = "start_stem";
-  
-  private static final Logger log = Logger.getLogger(Ojs3CrawlSeedFactory.class);
-  
+
   public static class AddStemCrawlSeed extends BaseCrawlSeed {
     
     public AddStemCrawlSeed(CrawlerFacade crawlerFacade) {
@@ -72,112 +64,18 @@ public class Ojs3CrawlSeedFactory implements CrawlSeedFactory {
     @Override
     public Collection<String> doGetPermissionUrls() throws ConfigurationException,
         PluginException, IOException {
-      return addStartStem(super.doGetPermissionUrls());
+      return Ojs3StartStemHelper.addStartStem(au, super.doGetPermissionUrls());
     }    
 
 	@Override
     public Collection<String> doGetStartUrls() throws ConfigurationException,
         PluginException, IOException {
-      return addStartStem(super.doGetStartUrls());
+      return Ojs3StartStemHelper.addStartStem(au, super.doGetStartUrls());
     }
-
-	private Collection<String> addStartStem(Collection<String> origUrls) {
-       String start_stem = AuUtil.getTitleAttribute(super.au,START_STEM_ATTRIBUTE);
-       log.debug3("OJS3: In addStartStem. start_stem = " + start_stem);
-       if(start_stem == null) {
-    	   log.debug3("OJS3: No start_stem attributes, start_url is at represented in the plugin");
-    	   return origUrls;
-       }
-       String baseUrl = (super.au).getConfiguration().get(ConfigParamDescr.BASE_URL.getKey());
-       String journal_id = (super.au).getConfiguration().get(ConfigParamDescr.JOURNAL_ID.getKey());
-
-        String baseUrlProtocal = getProtocal(baseUrl);
-        
-       
-       int afterBaseUrl = baseUrl.length(); // will be first index after end of base url, this will change when "http" and "https" mixed in the page
-        
-        log.debug3("OJS3: baseUrl = " + baseUrl + ", journal_id = " + journal_id);
-
-       Collection<String> newUrls = new ArrayList<String>(origUrls.size());
-       for (Iterator<String> iter = origUrls.iterator(); iter.hasNext();) {
-         String url = iter.next();
-
-        String urlProtocal = getProtocal(url);
-        
-
-         String urlWithoutProtocal = url.replace(urlProtocal, "");
-         String baseurlWithoutProtocal =   baseUrl.replace(baseUrlProtocal, "");
-
-         log.debug3("OJS3: ------------url = " + url + ", urlWithoutProtocal = " + urlWithoutProtocal + ", base_url = "
-                   + baseUrl + ", baseurlWithoutProtocal = " + baseurlWithoutProtocal);
-            // insert start_stem between baseUrl and anything that follows
-            // There are cases "http" and "https" urls are embedded in the same html source
-           // url = https://journals.vgtu.lt/BME/gateway/lockss?year=2016, urlWithoutProtocal = journals.vgtu.lt/BME/gateway/lockss?year=2016, base_url=https://journals.vgtu.lt/, baseurlWithoutProtocal = journals.vgtu.lt/
-         if (urlWithoutProtocal.startsWith(baseurlWithoutProtocal)) {
-             String expected = getStartUrl(baseUrl, url, start_stem);
-             if (!newUrls.contains(expected)) {
-                 newUrls.add(expected);
-             }
-             log.debug3("OJS3: =========final sb = " + expected);
-         } else {
-        	 // if it doesn't start with start_url just leave it alone, not that this is currently happening
-             log.debug3("OJS3: .........adding other = " + url);
-             if (!newUrls.contains(url)) {
-                 newUrls.add(url);
-             }
-         }
-       }
-       if (log.isDebug3()) {
-    	   for(Iterator<String> debIt = newUrls.iterator(); debIt.hasNext();) {
-    		   String url = debIt.next();
-    		   log.debug3("OJS3: start url: " +  url);
-    	   }
-       }
-       return newUrls;
-	}
-
-	public String getProtocal(String url) {
-          String urlProtocal = "";
-
-          if (url.startsWith("https://")) {
-              urlProtocal = "https://";
-          }  else if (url.startsWith("http://")) {
-              urlProtocal = "http://";
-          }
-
-          return urlProtocal;
-      }
-
-      public String getStartUrl(String baseUrl, String url, String start_stem) {
-
-          String urlWithoutProtocal = url.replace(getProtocal(url), "");
-          String baseurlWithoutProtocal =  baseUrl.replace(getProtocal(baseUrl), "");
-
-          StringBuilder sb = new StringBuilder(baseUrl);
-
-          //log.debug3("OJS3: ------------url = " + url + ", urlWithoutProtocal = " + urlWithoutProtocal + ", base_url = "
-                  //+ baseUrl + ", baseurlWithoutProtocal = " + baseurlWithoutProtocal);
-
-          if (urlWithoutProtocal.startsWith(baseurlWithoutProtocal)) {
-              //log.debug3("OJS3: sb = " + sb.toString());
-              sb.append(start_stem);
-              //log.debug3("OJS3: sb append = " + sb.toString());
-              //log.debug3("OJS3: url substring = " + urlWithoutProtocal.substring(baseurlWithoutProtocal.length()));
-              //log.debug3("OJS3: url substring replace= " + urlWithoutProtocal.substring(baseurlWithoutProtocal.length()).replace(start_stem, ""));
-              sb.append(urlWithoutProtocal.substring(baseurlWithoutProtocal.length()).replace(start_stem, ""));
-              //log.debug3("OJS3: adding = " + urlWithoutProtocal.substring(baseurlWithoutProtocal.length()).replace(start_stem, ""));
-
-              //log.debug3("OJS3: =========final sb = " + sb.toString());
-          }
-
-          return sb.toString();
-      }
-
   }
   
   @Override
   public CrawlSeed createCrawlSeed(CrawlerFacade facade) {
 	  return new AddStemCrawlSeed(facade);
   }
-
 }
--- plugins/src/org/lockss/plugin/ojs3/Ojs3FeatureUrlHelperFactory.java
+++ plugins/src/org/lockss/plugin/ojs3/Ojs3FeatureUrlHelperFactory.java
@@ -32,22 +32,16 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package org.lockss.plugin.ojs3;
 
-import org.lockss.daemon.ConfigParamDescr;
 import org.lockss.daemon.OpenUrlResolver;
 import org.lockss.daemon.PluginException;
 import org.lockss.plugin.*;
-import org.lockss.util.Logger;
 import org.lockss.util.TypedEntryMap;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 public class Ojs3FeatureUrlHelperFactory implements FeatureUrlHelperFactory {
-  private static final Logger log = Logger.getLogger(Ojs3FeatureUrlHelperFactory.class);
-  private static final String START_STEM_ATTRIBUTE = "start_stem";
 
   @Override
   public FeatureUrlHelper createFeatureUrlHelper(Plugin plug) {
@@ -64,7 +58,7 @@ public class Ojs3FeatureUrlHelperFactory implements FeatureUrlHelperFactory {
         return null;
       }
       // return the proper start_url
-      return addStartStem(au);
+      return Ojs3StartStemHelper.addStartStem(au, au.getStartUrls());
     }
 
     @Override
@@ -77,90 +71,10 @@ public class Ojs3FeatureUrlHelperFactory implements FeatureUrlHelperFactory {
         return null;
       }
       if (itemType == OpenUrlResolver.OpenUrlInfo.ResolvedTo.VOLUME) {
-        return (List<String>) addStartStem(au);
+        return (List<String>) Ojs3StartStemHelper.addStartStem(au, au.getStartUrls());
       } else {
         return null;
       }
     }
   }
-
-  static Collection<String> addStartStem(ArchivalUnit au) {
-    Collection<String> origUrls = au.getStartUrls();
-    String start_stem = AuUtil.getTitleAttribute(au, START_STEM_ATTRIBUTE);
-    log.debug3("OJS3: In addStartStem. start_stem = " + start_stem);
-    if(start_stem == null) {
-      log.debug3("OJS3: No start_stem attributes, start_url is at represented in the plugin");
-      return origUrls;
-    }
-    String baseUrl = au.getConfiguration().get(ConfigParamDescr.BASE_URL.getKey());
-    String journal_id = au.getConfiguration().get(ConfigParamDescr.JOURNAL_ID.getKey());
-
-    String baseUrlProtocal = getProtocal(baseUrl);
-
-    int afterBaseUrl = baseUrl.length(); // will be first index after end of base url, this will change when "http" and "https" mixed in the page
-
-    log.debug3("OJS3: baseUrl = " + baseUrl + ", journal_id = " + journal_id);
-
-    Collection<String> newUrls = new ArrayList<>(origUrls.size());
-    for (String url : origUrls) {
-      String urlProtocal = getProtocal(url);
-
-
-      String urlWithoutProtocal = url.replace(urlProtocal, "");
-      String baseurlWithoutProtocal = baseUrl.replace(baseUrlProtocal, "");
-
-      log.debug3("OJS3: ------------url = " + url + ", urlWithoutProtocal = " + urlWithoutProtocal + ", base_url = "
-          + baseUrl + ", baseurlWithoutProtocal = " + baseurlWithoutProtocal);
-      // insert start_stem between baseUrl and anything that follows
-      // There are cases "http" and "https" urls are embedded in the same html source
-      // url = https://journals.vgtu.lt/BME/gateway/lockss?year=2016, urlWithoutProtocal = journals.vgtu.lt/BME/gateway/lockss?year=2016, base_url=https://journals.vgtu.lt/, baseurlWithoutProtocal = journals.vgtu.lt/
-      if (urlWithoutProtocal.startsWith(baseurlWithoutProtocal)) {
-        String expected = getStartUrl(baseUrl, url, start_stem);
-        if (!newUrls.contains(expected)) {
-          newUrls.add(expected);
-        }
-        log.debug3("OJS3: =========final sb = " + expected);
-      } else {
-        // if it doesn't start with start_url just leave it alone, not that this is currently happening
-        log.debug3("OJS3: .........adding other = " + url);
-        if (!newUrls.contains(url)) {
-          newUrls.add(url);
-        }
-      }
-    }
-    if (log.isDebug3()) {
-      for (String url : newUrls) {
-        log.debug3("OJS3: start url: " + url);
-      }
-    }
-    return newUrls;
-  }
-
-  public static String getProtocal(String url) {
-    String urlProtocal = "";
-
-    if (url.startsWith("https://")) {
-      urlProtocal = "https://";
-    }  else if (url.startsWith("http://")) {
-      urlProtocal = "http://";
-    }
-
-    return urlProtocal;
-  }
-
-  public static String getStartUrl(String baseUrl, String url, String start_stem) {
-
-    String urlWithoutProtocal = url.replace(getProtocal(url), "");
-    String baseurlWithoutProtocal =  baseUrl.replace(getProtocal(baseUrl), "");
-
-    StringBuilder sb = new StringBuilder(baseUrl);
-
-    if (urlWithoutProtocal.startsWith(baseurlWithoutProtocal)) {
-      sb.append(start_stem);
-      sb.append(urlWithoutProtocal.substring(baseurlWithoutProtocal.length()).replace(start_stem, ""));
-    }
-
-    return sb.toString();
-  }
-
 }
