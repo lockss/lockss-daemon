b37aa3eccf2b4d33624458c0bc0fb2b6338aa65c
Mark McAdam <mol3earth@gmail.com>
2022-06-09 15:25:49 -0700

    additional htmlhash filtering for Ojs3

--- plugins/src/org/lockss/plugin/ojs3/Ojs3HtmlHashFilterFactory.java
+++ plugins/src/org/lockss/plugin/ojs3/Ojs3HtmlHashFilterFactory.java
@@ -34,6 +34,7 @@ package org.lockss.plugin.ojs3;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -53,11 +54,13 @@ import org.htmlparser.tags.*;
 import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
 import org.htmlparser.visitors.NodeVisitor;
+import org.lockss.filter.WhiteSpaceFilter;
 import org.lockss.filter.html.*;
 import org.lockss.plugin.ArchivalUnit;
 import org.lockss.plugin.FilterFactory;
 import org.lockss.util.Constants;
 import org.lockss.util.Logger;
+import org.lockss.util.ReaderInputStream;
 
 /*
  * SO far we only have two publishers who have migrated to OJS3 and the html is basic and
@@ -72,16 +75,16 @@ public class Ojs3HtmlHashFilterFactory implements FilterFactory {
   private static final String CIT_RIGHT_INLINE_CLASS = "csl-right-inline";
   // [cited 2022May18]
   public static final Pattern VANCOUVER_CIT_PATTERN =
-      Pattern.compile("\\[cited \\d\\d\\d\\d[a-z]+\\d\\d?]", Pattern.CASE_INSENSITIVE);
+      Pattern.compile("\\[cited \\d\\d\\d\\d\\s?[a-z]+\\.?\\s?\\d\\d?\\s?]", Pattern.CASE_INSENSITIVE);
   // Accessed May 18, 2022.
   public static final Pattern TURABIAN_CIT_PATTERN =
-      Pattern.compile("Accessed [a-z]+ \\d\\d?, \\d\\d\\d\\d", Pattern.CASE_INSENSITIVE);
+      Pattern.compile("Accessed [a-z]+\\.? \\d\\d?, \\d\\d\\d\\d", Pattern.CASE_INSENSITIVE);
   //(Accessed: 20May2022)
   public static final Pattern HARVARD_CIT_PATTERN =
-      Pattern.compile("\\(Accessed: \\d\\d?[a-z]+\\d\\d\\d\\d\\)", Pattern.CASE_INSENSITIVE);
+      Pattern.compile("\\(Accessed: \\d\\d?\\s?[a-z]+\\.?\\s?\\d\\d\\d\\d\\s?\\)", Pattern.CASE_INSENSITIVE);
   // Acesso em: 19 may. 2022.
   public static final Pattern ASSOCIACAO_BRAZILEIRA_DE_NORMAS_TECNICAS_PATTERN =
-      Pattern.compile("Acesso em: \\d\\d? [a-z]+\\. \\d\\d\\d\\d", Pattern.CASE_INSENSITIVE);
+      Pattern.compile("Acesso em: \\d\\d?\\s?[a-z]+\\.?\\s?\\d\\d\\d\\d", Pattern.CASE_INSENSITIVE);
 
   private static final NodeFilter[] includeNodes = new NodeFilter[] {
     // manifest page
@@ -89,18 +92,19 @@ public class Ojs3HtmlHashFilterFactory implements FilterFactory {
     HtmlNodeFilters.tagWithAttributeRegex("div",  "class", "page lockss"),
     // toc - contents only
     HtmlNodeFilters.tagWithAttribute("div", "class", "issue-toc"),
+    HtmlNodeFilters.tagWithAttribute("div", "class", "obj_issue_toc"),
     // abstract landing page html
     HtmlNodeFilters.tagWithAttribute("article", "class", "article-details"),
     // pdf landing page - just get the header with title and link information
     HtmlNodeFilters.tagWithAttribute("header", "class", "header_view"),
     // article page, https://journals.vgtu.lt/index.php/BME/article/view/10292
-
     HtmlNodeFilters.tagWithAttribute("h2", "class", "headings"),
     HtmlNodeFilters.tagWithAttribute("div", "class", "authors"),
     HtmlNodeFilters.tagWithAttribute("div", "id", "article_tab"),
     // article page: https://www.clei.org/cleiej/index.php/cleiej/article/view/202
     HtmlNodeFilters.tagWithAttribute("div", "class", "main_entry"),
-
+    // https://mapress.com/jib/article/view/2018.07.1.1
+    HtmlNodeFilters.tagWithAttribute("div", "id", "jatsParserFullText"),
     // article page: 	https://haematologica.org/article/view/10276
     HtmlNodeFilters.tagWithAttributeRegex("div", "class", "article-container"),
     // issue page: https://haematologica.org/issue/view/377
@@ -158,8 +162,10 @@ public class Ojs3HtmlHashFilterFactory implements FilterFactory {
   private static final NodeFilter[] includeNodes2 = new NodeFilter[] {};
 
   private static final NodeFilter[] excludeNodes = new NodeFilter[] {
-        // on the article landing page - remove the bottom stuff
-        HtmlNodeFilters.tagWithAttribute("section","class","article-more-details"),
+      // on the article landing page - remove the bottom stuff
+      HtmlNodeFilters.tagWithAttribute("section","class","article-more-details"),
+      HtmlNodeFilters.tag("script"),
+      HtmlNodeFilters.tag("noscript"),
   };
 
   private static final NodeFilter[] excludeNodes2 = new NodeFilter[] {};
@@ -168,7 +174,7 @@ public class Ojs3HtmlHashFilterFactory implements FilterFactory {
   public InputStream createFilteredInputStream(ArchivalUnit au,
                                                InputStream in,
                                                String encoding) {
-    return new HtmlFilterInputStream(
+    InputStream htmlFiltered = new HtmlFilterInputStream(
       in,
       encoding,
       new HtmlCompoundTransform(
@@ -176,6 +182,8 @@ public class Ojs3HtmlHashFilterFactory implements FilterFactory {
         HtmlNodeFilterTransform.exclude(new OrFilter(excludeNodes))
       )
     );
+    // replace any multiple whitespaces with a single whitespace.
+    return new ReaderInputStream(new WhiteSpaceFilter(new InputStreamReader(htmlFiltered)));
   }
 
 }
--- plugins/test/src/org/lockss/plugin/ojs3/TestOjs3HtmlHashFilterFactory.java
+++ plugins/test/src/org/lockss/plugin/ojs3/TestOjs3HtmlHashFilterFactory.java
@@ -18,37 +18,57 @@ public class TestOjs3HtmlHashFilterFactory  extends LockssTestCase {
     super.setUp();
     fact = new Ojs3HtmlHashFilterFactory();
   }
-
-
   private static final String vancouver1 =
     "<div class=\"csl-entry\"><div class=\"csl-right-inline\">Dal PontGE. Lawyer Professionalism in the 21st Century. VULJ [Internet]. 2018Dec.31 [cited 2022May19];8(1):7\u001318. Available from: https://vulj.vu.edu.au/index.php/vulj/article/view/1132</div></div>";
 
   private static final String vancouver1Filt =
-    "<div class=\"csl-entry\"><div class=\"csl-right-inline\">Dal PontGE. Lawyer Professionalism in the 21st Century. VULJ [Internet]. 2018Dec.31 ;8(1):7\u001318. Available from: https://vulj.vu.edu.au/index.php/vulj/article/view/1132</div></div>";
+    "<div class=\"csl-entry\"><div class=\"csl-right-inline\">Dal PontGE. Lawyer Professionalism in the 21st Century. VULJ [Internet]. 2018Dec.31 ;8(1):7 18. Available from: https://vulj.vu.edu.au/index.php/vulj/article/view/1132</div></div>";
 
   private static final String vancouver2 =
-      "<div class=\"csl-entry\"><div class=\"csl-left-margin\">1. </div><div class=\"csl-right-inline\">Hebbar PA, Pashilkar AA, Biswas P. Using eye tracking system for aircraft design \u0013 a flight simulator study. Aviation [Internet]. 2022Mar.22 [cited 2022May21];26(1):11\u001321. Available from: https://journals.vilniustech.lt/index.php/Aviation/article/view/16398</div></div>";
+      "<div class=\"csl-entry\"><div class=\"csl-left-margin\">1. </div><div class=\"csl-right-inline\">Hebbar PA, Pashilkar AA, Biswas P. Using eye tracking system for aircraft design \u0013 a flight simulator study. Aviation [Internet]. 2022Mar.22 [cited 2022 May 20];26(1):11\u001321. Available from: https://journals.vilniustech.lt/index.php/Aviation/article/view/16398</div></div>";
 
   private static final String vancouver2Filt =
-      "<div class=\"csl-entry\"><div class=\"csl-left-margin\">1. </div><div class=\"csl-right-inline\">Hebbar PA, Pashilkar AA, Biswas P. Using eye tracking system for aircraft design \u0013 a flight simulator study. Aviation [Internet]. 2022Mar.22 ;26(1):11\u001321. Available from: https://journals.vilniustech.lt/index.php/Aviation/article/view/16398</div></div>";
+      "<div class=\"csl-entry\"><div class=\"csl-left-margin\">1. </div><div class=\"csl-right-inline\">Hebbar PA, Pashilkar AA, Biswas P. Using eye tracking system for aircraft design a flight simulator study. Aviation [Internet]. 2022Mar.22 ;26(1):11 21. Available from: https://journals.vilniustech.lt/index.php/Aviation/article/view/16398</div></div>";
+
+  private static final String vancouver3 =
+      "<div class=\"csl-bib-body\">\n" +
+          "  <div class=\"csl-entry\"><div class=\"csl-left-margin\">1.</div><div class=\"csl-right-inline\">Gross A. The Continuity of Scientific Discovery and Its Communication: The Example of Michael Faraday. Disc Collab [Internet]. 2009 Feb. 27 [cited 2022 Jun. 9];4:3. Available from: https://journals.uic.edu/ojs/index.php/jbdc/article/view/2444</div></div>\n" +
+          "</div>";
+
+  private static final String vancouver3Filt =
+      "<div class=\"csl-entry\"><div class=\"csl-left-margin\">1.</div><div class=\"csl-right-inline\">Gross A. The Continuity of Scientific Discovery and Its Communication: The Example of Michael Faraday. Disc Collab [Internet]. 2009 Feb. 27 ;4:3. Available from: https://journals.uic.edu/ojs/index.php/jbdc/article/view/2444</div></div>";
+
   private static final String tarubian =
       "<div class=\"csl-entry\">Zeller, Bruno, and Richard Lightfoot. \u001CGood Faith: An ICSID Convention Requirement?\u001D. <i>Victoria University Law and Justice Journal</i> 8, no. 1 (December 31, 2018): 19\u001332. Accessed May 19, 2022. https://vulj.vu.edu.au/index.php/vulj/article/view/1139.</div>";
 
   private static final String tarubianFilt =
-      "<div class=\"csl-entry\">Zeller, Bruno, and Richard Lightfoot. \u001CGood Faith: An ICSID Convention Requirement?\u001D. <i>Victoria University Law and Justice Journal</i> 8, no. 1 (December 31, 2018): 19\u001332. . https://vulj.vu.edu.au/index.php/vulj/article/view/1139.</div>";
+      "<div class=\"csl-entry\">Zeller, Bruno, and Richard Lightfoot. Good Faith: An ICSID Convention Requirement? . <i>Victoria University Law and Justice Journal</i> 8, no. 1 (December 31, 2018): 19 32. . https://vulj.vu.edu.au/index.php/vulj/article/view/1139.</div>";
 
   private static final String harvard =
       "<div class=\"csl-entry\">Mendoza, R. \u001CRuby\u001D . (2022) \u001CBook Review\u0014The Borders of AIDS: Race, Quarantine &amp; Resistance by Karma R. Chavez\u001D, <i>Literacy in Composition Studies</i>, 9(2), pp. 67-72. Available at: https://licsjournal.org/index.php/LiCS/article/view/2193 (Accessed: 21May2022).</div>";
 
   private static final String harvardFilt =
-      "<div class=\"csl-entry\">Mendoza, R. \u001CRuby\u001D . (2022) \u001CBook Review\u0014The Borders of AIDS: Race, Quarantine &amp; Resistance by Karma R. Chavez\u001D, <i>Literacy in Composition Studies</i>, 9(2), pp. 67-72. Available at: https://licsjournal.org/index.php/LiCS/article/view/2193 .</div>";
+      "<div class=\"csl-entry\">Mendoza, R. Ruby . (2022) Book Review The Borders of AIDS: Race, Quarantine &amp; Resistance by Karma R. Chavez , <i>Literacy in Composition Studies</i>, 9(2), pp. 67-72. Available at: https://licsjournal.org/index.php/LiCS/article/view/2193 .</div>";
 
   private static final String abdnt =
       "<div class=\"csl-entry\">DRERUP, C.; EVESLAGE, M.; SUNDERKTTER, C.; EHRCHEN, J. Diagnostic Value of Laboratory Parameters for Distinguishing Between Herpes Zoster and Bacterial Superficial Skin and Soft Tissue Infections. <b>Acta Dermato-Venereologica</b>, <i>[S. l.]</i>, v. 100, n. 1, p. 1\u00135, 2020. DOI: 10.2340/00015555-3357. Disponvel em: https://medicaljournalssweden.se/actadv/article/view/1631. Acesso em: 19 may. 2022.</div>";
 
   private static final String abdntFilt =
-      "<div class=\"csl-entry\">DRERUP, C.; EVESLAGE, M.; SUNDERKTTER, C.; EHRCHEN, J. Diagnostic Value of Laboratory Parameters for Distinguishing Between Herpes Zoster and Bacterial Superficial Skin and Soft Tissue Infections. <b>Acta Dermato-Venereologica</b>, <i>[S. l.]</i>, v. 100, n. 1, p. 1\u00135, 2020. DOI: 10.2340/00015555-3357. Disponvel em: https://medicaljournalssweden.se/actadv/article/view/1631. .</div>";
+      "<div class=\"csl-entry\">DRERUP, C.; EVESLAGE, M.; SUNDERKTTER, C.; EHRCHEN, J. Diagnostic Value of Laboratory Parameters for Distinguishing Between Herpes Zoster and Bacterial Superficial Skin and Soft Tissue Infections. <b>Acta Dermato-Venereologica</b>, <i>[S. l.]</i>, v. 100, n. 1, p. 1 5, 2020. DOI: 10.2340/00015555-3357. Disponvel em: https://medicaljournalssweden.se/actadv/article/view/1631. .</div>";
 
+  private static final String jatsArticle =
+    "<div class=\"jatsParser__center-article-block\">\n" +
+      "<div class=\"jatsParser__article-fulltext\" id=\"jatsParserFullText\">\n" +
+        "<h2 class=\"article-section-title jatsParser__abstract\">Abstract</h2>\n" +
+        "The genus <em>Mahanarva</em> Distant, 1909 (Hemiptera: Cercopoidea: Cercopidae) currently includes two subgenera: <em>Mahanarva</em> Distant, 1909 with 38 species and six subspecies, and <em>Ipiranga</em> Fennah, 1968 with nine species. The <em>Manaharva</em> species are all from the Americas, and a few species are important pests in pasture grasses and sugarcane. There are no reports of any <em>Manaharva</em> species from North America, including Mexico and areas to the north. Here, a new species is described from Mexico and a key to the species of <em>Mahanarva</em><strong> </strong>from Central America and Mexico is proposed." +
+      "</div>" +
+    "</div>";
+
+  private static final String jatsArticleFilt =
+    "<div class=\"jatsParser__article-fulltext\" id=\"jatsParserFullText\"> " +
+      "<h2 class=\"article-section-title jatsParser__abstract\">Abstract</h2> " +
+      "The genus <em>Mahanarva</em> Distant, 1909 (Hemiptera: Cercopoidea: Cercopidae) currently includes two subgenera: <em>Mahanarva</em> Distant, 1909 with 38 species and six subspecies, and <em>Ipiranga</em> Fennah, 1968 with nine species. The <em>Manaharva</em> species are all from the Americas, and a few species are important pests in pasture grasses and sugarcane. There are no reports of any <em>Manaharva</em> species from North America, including Mexico and areas to the north. Here, a new species is described from Mexico and a key to the species of <em>Mahanarva</em><strong> </strong>from Central America and Mexico is proposed." +
+    "</div>";
 
   public void testAbdntFiltering() throws Exception {
     assertEquals(getStringfromFilteredInputStream(abdnt), abdntFilt);
@@ -59,11 +79,17 @@ public class TestOjs3HtmlHashFilterFactory  extends LockssTestCase {
   public void testVancouverBFiltering() throws Exception {
     assertEquals(getStringfromFilteredInputStream(vancouver2), vancouver2Filt);
   }
+  public void testVancouver3iltering() throws Exception {
+    assertEquals(getStringfromFilteredInputStream(vancouver3), vancouver3Filt);
+  }
   public void testHarvardFiltering() throws Exception {
     assertEquals(getStringfromFilteredInputStream(harvard), harvardFilt);
   }
   public void testTarubianFiltering() throws Exception {
     assertEquals(getStringfromFilteredInputStream(tarubian), tarubianFilt);
+  }
+  public void testJatsArtFiltering() throws Exception {
+    assertEquals(getStringfromFilteredInputStream(jatsArticle), jatsArticleFilt);
   }
 
   public String getStringfromFilteredInputStream(String in) throws IOException {
