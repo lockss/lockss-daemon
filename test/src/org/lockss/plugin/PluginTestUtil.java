b3515e8834b041485c67c7f9e490cbec56090633
Mark McAdam <mol3earth@gmail.com>
2022-06-14 15:49:32 -0700

    added methods to copy compressed files (e.g. zip) from one crawl to another.

--- test/src/org/lockss/plugin/PluginTestUtil.java
+++ test/src/org/lockss/plugin/PluginTestUtil.java
@@ -53,6 +53,10 @@ import org.lockss.config.*;
 public class PluginTestUtil {
   static Logger log = Logger.getLogger("PluginTestUtil");
   static List aulist = new LinkedList();
+  static final Pattern COMPRESSED_PATTERN = Pattern.compile(
+      "\\.(zip|tar\\.gz|tgz)$",
+      Pattern.CASE_INSENSITIVE
+  );
 
   public static void registerArchivalUnit(Plugin plug, ArchivalUnit au) {
     PluginManager mgr = getPluginManager();
@@ -301,7 +305,11 @@ public class PluginTestUtil {
       try {
         String fromUrl = cu.getUrl();
         String toUrl = fromUrl;
-
+        Matcher compMat = COMPRESSED_PATTERN.matcher(fromUrl);
+        if (compMat.find()) {
+          log.debug("encountered a compressed file, skipping");
+          continue;
+        }
         if (ifMatchPat != null) {
           Matcher mat = ifMatchPat.matcher(fromUrl);
           if (!mat.find()) {
@@ -309,22 +317,17 @@ public class PluginTestUtil {
             continue;
           }
         }
-        if (fromUrl.endsWith(".zip")) {
-          log.info("doing zip copy");
-          zipCopy(cu, toAu, fromUrl + "!/", pattern, rep);
-        } else {
-          if (pattern != null) {
-            Matcher mat = pattern.matcher(fromUrl);
-            toUrl = mat.replaceAll(rep);
-          }
-          CIProperties props = cu.getProperties();
-          if (props == null) {
-            log.debug3("in copyCus() props was null for url: " + fromUrl);
-          }
-          UrlCacher uc = toAu.makeUrlCacher(
-              new UrlData(cu.getUnfilteredInputStream(), props, toUrl));
-          uc.storeContent();
+        if (pattern != null) {
+          Matcher mat = pattern.matcher(fromUrl);
+          toUrl = mat.replaceAll(rep);
         }
+        CIProperties props = cu.getProperties();
+        if (props == null) {
+          log.debug3("in copyCus() props was null for url: " + fromUrl);
+        }
+        UrlCacher uc = toAu.makeUrlCacher(
+            new UrlData(cu.getUnfilteredInputStream(), props, toUrl));
+        uc.storeContent();
         if (!toUrl.equals(fromUrl)) {
           log.info("Copied " + fromUrl + " to " + toUrl);
         } else {
@@ -340,27 +343,118 @@ public class PluginTestUtil {
     return res;
   }
 
-  /*
-   Use the normal iterator, invoking a new zipCopy() method to open and
-   iterate through each zip it encounters.  Then it doesn't have to keep
-   track of anything, just open a temp output zip whenever it starts
-   processing an input zip, and close and store when it gets to the end.
-   Nested zips are easy - just call zipCopy() recursively.  (The store
-   operation actually wants to be handled by zipCopy()'s caller, as at
-   top level "store" means use a UrlCacher, but in nested zips it means
-   write a zip member.  (This is all easy using ZipFile).
+  /**
+   * Copies all zips in from one crawl into another without modification.
+   *
+   * @see PluginTestUtil#copyZip(CachedUrl, ArchivalUnit, PatternReplacements)
    */
-  public static void zipCopy(CachedUrl cu, ArchivalUnit toAu, String zipUrl,
-                             Pattern pattern, String rep)
+  public static boolean copyAuZip(ArchivalUnit fromAu, ArchivalUnit toAu) {
+    return copyZipCus(fromAu.getAuCachedUrlSet(), toAu, null);
+  }
+
+  /**
+   * Works just like {@link PluginTestUtil#copyZipCus(CachedUrlSet, ArchivalUnit, List, List)}.
+   * Just gets the cachedUrlSet in fromAU and Makes lists from pat & rep.
+   *
+   * @see PluginTestUtil#copyZipCus(CachedUrlSet, ArchivalUnit, List, List)
+   */
+  public static boolean copyAuZip(ArchivalUnit fromAu, ArchivalUnit toAu, String pat, String rep) throws Exception {
+    return copyZipCus(fromAu.getAuCachedUrlSet(), toAu, pat, rep);
+  }
+
+  /**
+   * Works just like {@link PluginTestUtil#copyZipCus(CachedUrlSet, ArchivalUnit, List, List)}.
+   * Just gets the cachedUrlSet in fromAu.
+   *
+   * @see PluginTestUtil#copyZipCus(CachedUrlSet, ArchivalUnit, List, List)
+   */
+  public static boolean copyAuZip(ArchivalUnit fromAu, ArchivalUnit toAu, List<String> pats, List<String> reps)
+      throws Exception {
+    return copyZipCus(fromAu.getAuCachedUrlSet(), toAu, pats, reps);
+  }
+
+  /**
+  * Works just like {@link PluginTestUtil#copyZipCus(CachedUrlSet, ArchivalUnit, List, List)}.
+  * Just makes lists and adds pat and rep to them.
+   *
+  * @see PluginTestUtil#copyZipCus(CachedUrlSet, ArchivalUnit, List, List)
+ */
+  public static boolean copyZipCus(CachedUrlSet fromCus, ArchivalUnit toAu, String pat, String rep) throws Exception {
+    List<String> pats = Collections.singletonList(pat);
+    List<String> reps = Collections.singletonList(rep);
+    return copyZipCus(fromCus, toAu, pats, reps);
+  }
+
+  /**
+   * Utility to copy zip files from a simulated crawl to a mock archival unit.
+   * if only fromAu and toAu are provided, all zips encountered are copied without modification.
+   *
+   * @param fromCus the CachedUrlSet which has been crawled
+   * @param toAu the Archival Unit to copy content to
+   * @param pats List of regexes to search in the zipped contents
+   * @param reps List of replacements for each pattern
+   * @return true, if all copies attempted succeeded, false otherwise
+   */
+  public static boolean copyZipCus(CachedUrlSet fromCus, ArchivalUnit toAu, List<String> pats, List<String> reps)
+      throws Exception {
+    return copyZipCus(fromCus, toAu,
+        new PatternReplacements(pats, reps));
+  }
+
+  /**
+   * Iterates over all CachedUrls in a simulated crawl and copies them to a mock Au if patterns and replacements
+   * are identified in the simulated crawl from the Pattern-Replacements given.
+   *
+   * @see PluginTestUtil#copyZipCus(CachedUrlSet, ArchivalUnit, List, List)
+   */
+  private static boolean copyZipCus(CachedUrlSet fromCus, ArchivalUnit toAu, PatternReplacements patReps) {
+    boolean res = true;
+    for (CachedUrl cu : fromCus.getCuIterable()) {
+      try {
+        String fromUrl = cu.getUrl();
+        // only copy the file if it is a zip.
+        Matcher mat = COMPRESSED_PATTERN.matcher(fromUrl);
+        if (mat.find()) {
+          if (patReps!=null) {
+            log.debug("entering zip file to copy contents");
+            copyZip(cu, toAu, patReps);
+          } else {
+            log.debug("copying unmodified zip");
+            CIProperties props = cu.getProperties();
+            UrlCacher uc = toAu.makeUrlCacher(
+                new UrlData(cu.getUnfilteredInputStream(), props, fromUrl));
+            uc.storeContent();
+          }
+        }
+      } catch (Exception e) {
+        log.error("Couldn't copy " + cu.getUrl(), e);
+        res = false;
+      } finally {
+        cu.release();
+      }
+    }
+    return res;
+  }
+
+  /**
+   * Opens a single CachedUrl zip file, iterates over its contents and copies the contents if they pass
+   * given pattern(s) and replacements.
+   * @param cu A CachedUrl of compressed content.
+   * @param toAu The ArchivalUnit to copy the cu into.
+   * @param patReps Pattern-Replacement pairs to selectively copy zipped content and rename it in the new zip.
+   * @throws IOException When I/O zip files encounter any number of problems.
+   */
+  private static void copyZip(CachedUrl cu, ArchivalUnit toAu, PatternReplacements patReps)
     throws IOException {
     boolean doCache = false;
-    String toUrl = ""; String toFile; String zippedFile; String fromFile;
+    String zipUrl = cu.getUrl() + "!/";
+    String toUrl; String toFile; String zippedFile; String fromFile;
     ZipInputStream zis = null;
     ZipOutputStream zos;
+    String outZip = null;
     try {
-      log.info("first 4: " + StringUtil.fromInputStream(cu.getUnfilteredInputStream()).substring(0, 4));
-      zis = new ZipInputStream(cu.getUnfilteredInputStream());
-      log.info(" cu content size: " + cu.getContentSize());
+      InputStream cuis = new ReaderInputStream(cu.openForReading());
+      zis = new ZipInputStream(cuis);
       zos = new ZipOutputStream(Files.newOutputStream(
           Paths.get(cu.getArchivalUnit().getProperties().getString("root") +
               "temp.zip")));
@@ -377,43 +471,39 @@ public class PluginTestUtil {
         }
         fromFile = entry.getName();
         zippedFile = zipUrl + fromFile;
-        toUrl = zippedFile;
-        if (pattern != null) {
-          Matcher mat = pattern.matcher(zippedFile);
-          toUrl = mat.replaceAll(rep);
+        for (PatternReplacementPair prp : patReps.pairs) {
+          Matcher mat = prp.pat.matcher(zippedFile);
+          toUrl = mat.replaceAll(prp.rep);
+          if (!toUrl.equals(zippedFile)) {
+            log.debug("Found a zipped file match: " + zippedFile + " -> " + toUrl);
+            doCache = true;
+            outZip = toUrl.split("!/")[0];
+            toFile = toUrl.split("!/")[1];
+            ZipEntry outEntry = new ZipEntry(toFile);
+            zos.putNextEntry(outEntry);
+            StreamUtil.copy(zis, zos);
+            zos.closeEntry();
+          }
         }
-        //if (toUrl != zippedFile) {
-        doCache = true;
-        toFile = toUrl.split("!/")[1];
-        //log.info("Copying: " + zippedFile + " to " + toUrl);
-        ZipEntry outEntry = new ZipEntry(toFile);
-        if (entry.getSize() != -1) {
-          log.info("entry size was not -1");
-          outEntry.setSize(entry.getSize());
-        } else {
-        }
-        zos.putNextEntry(outEntry);
-        StreamUtil.copy(zis, zos);
-        zos.closeEntry();
-        //}
       }
       zos.close();
+      // any copied file triggers saving the new zip stream
       if (doCache) {
         FileInputStream is = new FileInputStream(new File(
             cu.getArchivalUnit().getProperties().getString("root"),
             "temp.zip"));
         // save all the copied zip entries to a new zip on the toAu
-        String outZip = toUrl.split("!/")[0];
         CIProperties props = cu.getProperties();
         if (props == null) {
           log.debug3("in copyCus() props was null for url: " + cu.getUrl());
         }
-        log.info("Storing new cu: " + outZip);
+        log.debug("Storing new cu: " + outZip);
         UrlCacher uc = toAu.makeUrlCacher(
             new UrlData(is, props, outZip));
         uc.storeContent();
         is.close();
       }
+
     } finally {
       IOUtil.safeClose(zis);
     }
@@ -431,6 +521,48 @@ public class PluginTestUtil {
   private static PluginManager getPluginManager() {
     return
       (PluginManager)LockssDaemon.getManager(LockssDaemon.PLUGIN_MANAGER);
+  }
+
+  private static class PatternReplacementPair {
+    public Pattern pat;
+    public String rep;
+
+    /**
+     * Simple Container class for Regex pattern -> Replacement pairs.
+     * @param pat String regex, gets compiled to a Pattern
+     * @param rep Replacement string
+     */
+    PatternReplacementPair(String pat, String rep) {
+      this.pat = Pattern.compile(pat, Pattern.CASE_INSENSITIVE);
+      this.rep = rep;
+    }
+  }
+
+  private static class PatternReplacements {
+    public PatternReplacementPair[] pairs;
+
+    /**
+     * Simple array class which stores Pattern-Replacement pairs.
+     *
+     * @param pats List of regex patterns in string format
+     * @param reps List of corresponding replacement patterns.
+     * @throws Exception if the inputs are not compliant
+     */
+    PatternReplacements(List<String> pats, List<String> reps) throws Exception {
+      if (pats == null || reps == null) {
+        throw new Exception("Patterns and Replacements cannot be null");
+      }
+      if (pats.size() != reps.size()) {
+        throw new Exception("Patterns and Replacements do not match up. Must contain the same number of elements.");
+      }
+      if (pats.size() < 1) {
+        throw new Exception("No patterns/replacements given. Must provide at least one pair.");
+      }
+      pairs = new PatternReplacementPair[pats.size()];
+      for (int i=0; i<pats.size(); i++) {
+        pairs[i] = new PatternReplacementPair(pats.get(i), reps.get(i));
+      }
+    }
   }
 
 }
