35d877125df5980d68e3c9eebacb5fb3a18ee473
Mark McAdam <mol3earth@gmail.com>
2022-07-12 13:52:43 -0700

    updated include pattern, added @override

--- plugins/src/org/lockss/plugin/clockss/pap/PapSourceZipXmlArticleIterator.java
+++ plugins/src/org/lockss/plugin/clockss/pap/PapSourceZipXmlArticleIterator.java
@@ -6,8 +6,9 @@ public class PapSourceZipXmlArticleIterator extends SourceZipXmlArticleIteratorF
 
   // same general pattern as the parent, except we exclude the toc file(s)
   protected static final String ALL_ZIP_XML_PATTERN_TEMPLATE =
-      "\"%s[^/]+/.*\\.zip!/(?!.*issue-files.*).*\\.xml$\", base_url";
+      "\"%s%s/.*\\.zip!/(?!.*issue-files.*).*\\.xml$\", base_url, directory";
 
+  @Override
   protected String getIncludePatternTemplate() {
     return ALL_ZIP_XML_PATTERN_TEMPLATE;
   }
