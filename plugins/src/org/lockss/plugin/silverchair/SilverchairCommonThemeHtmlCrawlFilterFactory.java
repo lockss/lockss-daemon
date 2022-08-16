97d9ab24a67dc58891c37116101c855882d08a07
Mark McAdam <mol3earth@gmail.com>
2022-08-16 13:18:08 -0700

    added a few more crawl rules/filters to SilverChair

--- plugins/src/org/lockss/plugin/silverchair/SilverchairCommonThemeHtmlCrawlFilterFactory.java
+++ plugins/src/org/lockss/plugin/silverchair/SilverchairCommonThemeHtmlCrawlFilterFactory.java
@@ -18,23 +18,37 @@ public class SilverchairCommonThemeHtmlCrawlFilterFactory implements FilterFacto
                                                  String encoding)
             throws PluginException {
         NodeFilter[] filters = new NodeFilter[] {
-                /*
-                    Need to filter out all the articles referenced in the article bottom part:
-                    For example:
-                    https://pubs.geoscienceworld.org/sepm/jsedres/article/91/11/1133/609365/Environmental-magnetism-evidence-for-longshore
+            // all manner of patterns and examples of headers and footers.
+            HtmlNodeFilters.tag("header"),
+            HtmlNodeFilters.tag("footer"),
+            HtmlNodeFilters.tagWithAttributeRegex("section", "class", "master-header"),
+            HtmlNodeFilters.tagWithAttributeRegex("section", "class", "footer_wrap"),
+            HtmlNodeFilters.tagWithAttributeRegex("section", "class", "site-footer"),
+            HtmlNodeFilters.tagWithAttributeRegex("div", "class", "SitePageFooter"),
+            HtmlNodeFilters.tagWithAttributeRegex("div", "class", "site-theme-footer"),
+            HtmlNodeFilters.tagWithAttributeRegex("div", "class", "global-footer"),
+            /*
+                Need to filter out all the articles referenced in the article bottom part:
+                For example:
+                https://pubs.geoscienceworld.org/sepm/jsedres/article/91/11/1133/609365/Environmental-magnetism-evidence-for-longshore
 
-                    <div class="comment">doi:<a class="link" href="10.3329/bjsir.v45i1.5173" target="_blank">10.3329/bjsir.v45i1.5173</a>.</div>
+                <div class="comment">doi:<a class="link" href="10.3329/bjsir.v45i1.5173" target="_blank">10.3329/bjsir.v45i1.5173</a>.</div>
 
-                    will references to:
-                    https://pubs.geoscienceworld.org/sepm/jsedres/article-standard/91/11/1133/609365/10.3133/ds231
-                    https://pubs.geoscienceworld.org/sepm/jsedres/article-standard/91/11/1133/609365/10.3133/ofr20111155
-                    https://pubs.geoscienceworld.org/sepm/jsedres/article-standard/91/11/1133/609365/10.3329/bjsir.v45i1.5173
-                    https://pubs.geoscienceworld.org/sepm/jsedres/article-standard/91/11/1133/609365/10.3390/min4040758
-                    https://pubs.geoscienceworld.org/sepm/jsedres/article-standard/91/11/1133/609365/10.5327/s1519-874x2006000300002
-                 */
-                HtmlNodeFilters.tagWithAttribute("div", "class", "comment"),
-                HtmlNodeFilters.tagWithAttributeRegex("div", "class", "ref-list"),
+                will references to:
+                https://pubs.geoscienceworld.org/sepm/jsedres/article-standard/91/11/1133/609365/10.3133/ds231
+                https://pubs.geoscienceworld.org/sepm/jsedres/article-standard/91/11/1133/609365/10.3133/ofr20111155
+                https://pubs.geoscienceworld.org/sepm/jsedres/article-standard/91/11/1133/609365/10.3329/bjsir.v45i1.5173
+                https://pubs.geoscienceworld.org/sepm/jsedres/article-standard/91/11/1133/609365/10.3390/min4040758
+                https://pubs.geoscienceworld.org/sepm/jsedres/article-standard/91/11/1133/609365/10.5327/s1519-874x2006000300002
+             */
+            //"global-nav"
+
+            HtmlNodeFilters.tagWithAttribute("div", "class", "comment"),
+            HtmlNodeFilters.tagWithAttributeRegex("div", "class", "ref-list"),
+            HtmlNodeFilters.tagWithAttributeRegex("div", "class", "global-footer"),
+            HtmlNodeFilters.tagWithAttributeRegex("a", "href", "^10[.]"),
         };
+
         return new HtmlFilterInputStream(in,
                 encoding,
                 HtmlNodeFilterTransform.exclude(new OrFilter(filters)));
