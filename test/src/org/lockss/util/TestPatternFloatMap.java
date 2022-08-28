590f0dde75ccfce50a3326eee17d60b7ebef145e
Tom Lipkis <tal@pss.com>
2022-08-28 16:20:20 -0700

    Coemetic

--- test/src/org/lockss/util/TestPatternFloatMap.java
+++ test/src/org/lockss/util/TestPatternFloatMap.java
@@ -1,10 +1,6 @@
 /*
- * $Id$
- */
 
-/*
-
-Copyright (c) 2000-2013 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2022 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
