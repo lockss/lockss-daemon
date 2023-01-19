3e40f548486f60028c43476af46b650e4dc229b1
feili18 <feili18@stanford.edu>
2023-01-19 13:51:30 -0800

    initial commit of Iniesteres source plugin

---
+++ plugins/src/org/lockss/plugin/clockss/iniesteres/IniesteresXmlMetadataExtractorFactory.java
@@ -0,0 +1,102 @@
+/*
+
+Copyright (c) 2000-2022, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package org.lockss.plugin.clockss.iniesteres;
+
+import org.lockss.config.TdbAu;
+import org.lockss.daemon.PluginException;
+import org.lockss.daemon.ShouldNotHappenException;
+import org.lockss.extractor.ArticleMetadata;
+import org.lockss.extractor.FileMetadataExtractor;
+import org.lockss.extractor.MetadataField;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.clockss.JatsPublishingSchemaHelper;
+import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
+import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
+import org.lockss.util.Logger;
+import org.w3c.dom.Document;
+
+public class IniesteresXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
+	static Logger log = Logger.getLogger(IniesteresXmlMetadataExtractorFactory.class);
+
+	private static SourceXmlSchemaHelper JatsPublishingHelper = null;
+
+	@Override
+	public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
+															 String contentType)
+			throws PluginException {
+		return new JatsPublishingSourceXmlMetadataExtractor();
+	}
+
+	public class JatsPublishingSourceXmlMetadataExtractor extends SourceXmlMetadataExtractor {
+
+		/*
+		 * This setUpSchema shouldn't be called directly
+		 * but for safety, just use the CU to figure out which schema to use.
+		 *
+		 */
+		@Override
+		protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu) {
+			throw new ShouldNotHappenException("This version of the schema setup cannot be used for this plugin");
+		}
+
+		@Override
+		protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu, Document xmlDoc) {
+			String url = cu.getUrl();
+			log.debug3("Setup Jats schema helper for url " + url);
+			if (JatsPublishingHelper == null) {
+				JatsPublishingHelper = new JatsPublishingSchemaHelper();
+			}
+			return JatsPublishingHelper;
+		}
+		
+		@Override
+		protected void postCookProcess(SourceXmlSchemaHelper schemaHelper,
+									   CachedUrl cu, ArticleMetadata thisAM) {
+			//If we didn't get a valid date value, use the copyright year if it's there
+			if (thisAM.get(MetadataField.FIELD_DATE) == null) {
+				if (thisAM.getRaw(JatsPublishingSchemaHelper.JATS_date) != null) {
+					thisAM.put(MetadataField.FIELD_DATE, thisAM.getRaw(JatsPublishingSchemaHelper.JATS_date));
+				} else {// last chance
+					thisAM.put(MetadataField.FIELD_DATE, thisAM.getRaw(JatsPublishingSchemaHelper.JATS_edate));
+				}
+			}
+
+			thisAM.put(MetadataField.FIELD_PUBLISHER, thisAM.getRaw(JatsPublishingSchemaHelper.JATS_pubname));
+			thisAM.put(MetadataField.FIELD_PROVIDER, thisAM.getRaw(JatsPublishingSchemaHelper.JATS_pubname));
+
+
+		}
+
+	}
+}
---
+++ plugins/test/src/org/lockss/plugin/clockss/iniesteres/TestIniesteresXmlMetadataExtractorFactory.java
@@ -0,0 +1,119 @@
+package org.lockss.plugin.clockss.iniesteres;
+
+import org.lockss.config.ConfigManager;
+import org.lockss.config.Configuration;
+import org.lockss.extractor.*;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.test.LockssTestCase;
+import org.lockss.test.MockArchivalUnit;
+import org.lockss.test.MockCachedUrl;
+import org.lockss.test.MockLockssDaemon;
+import org.lockss.util.CIProperties;
+import org.lockss.util.IOUtil;
+import org.lockss.util.Logger;
+import org.lockss.util.StringUtil;
+
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+
+public class TestIniesteresXmlMetadataExtractorFactory extends LockssTestCase {
+
+    private static final Logger log = Logger.getLogger(TestIniesteresXmlMetadataExtractorFactory.class);
+
+    private MockLockssDaemon theDaemon;
+    private MockArchivalUnit mau;
+
+    private static String PLUGIN_NAME = "org.lockss.plugin.clockss.iniesteres.ClockssIniestaresSourcePlugin";
+    private static String BASE_URL = "http://www.source.org/";
+    private static String DIRECTORY = "2016";
+    private static String ISSUE_URL = BASE_URL + DIRECTORY + "/pdoc/pdoc_17-1/";
+    private static final String xml_url = ISSUE_URL + "pdoc_17-1.xml";
+    private static final String filenum1 = "luxx_2016_0019_0001_0005_0013";
+    private static final String pdf1_url = ISSUE_URL +  "pdf/" + filenum1 + ".pdf";
+    public void setUp() throws Exception {
+        super.setUp();
+        setUpDiskSpace(); // you need this to have startService work properly...
+
+        theDaemon = getMockLockssDaemon();
+        mau = new MockArchivalUnit();
+
+        theDaemon.getAlertManager();
+        theDaemon.getPluginManager().setLoadablePluginsReady(true);
+        theDaemon.setDaemonInited(true);
+        theDaemon.getPluginManager().startService();
+        theDaemon.getCrawlManager();
+        mau.setConfiguration(auConfig());
+
+    }
+
+    public void tearDown() throws Exception {
+        theDaemon.stopDaemon();
+        super.tearDown();
+    }
+
+    /**
+     * Configuration method.
+     * @return
+     */
+    Configuration auConfig() {
+        Configuration conf = ConfigManager.newConfiguration();
+        conf.put("base_url", BASE_URL);
+        conf.put("year", DIRECTORY);
+        return conf;
+    }
+
+
+    private static final String realXMLFile = "sample.xml";
+
+    public void testFromJatsPublishingXMLFile() throws Exception {
+        InputStream file_input = null;
+        try {
+            file_input = getResourceAsStream(realXMLFile);
+            String string_input = StringUtil.fromInputStream(file_input);
+            IOUtil.safeClose(file_input);
+
+            CIProperties xmlHeader = new CIProperties();
+            xmlHeader.put(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+            MockCachedUrl mcu = mau.addUrl(xml_url, true, true, xmlHeader);
+            // Now add all the pdf files in our AU since we check for them before emitting
+            mau.addUrl(pdf1_url, true, true, xmlHeader);
+            mcu.setContent(string_input);
+            mcu.setContentSize(string_input.length());
+            mcu.setProperty(CachedUrl.PROPERTY_CONTENT_TYPE, "text/xml");
+
+            FileMetadataExtractor me = new IniesteresXmlMetadataExtractorFactory().createFileMetadataExtractor(MetadataTarget.Any(), "text/xml");
+            FileMetadataListExtractor mle =
+                    new FileMetadataListExtractor(me);
+            List<ArticleMetadata> mdlist = mle.extract(MetadataTarget.Any(), mcu);
+            assertNotEmpty(mdlist);
+            // only one of the two records had a matching pdf
+            assertEquals(1, mdlist.size());
+
+            // check each returned md against expected values
+            Iterator<ArticleMetadata> mdIt = mdlist.iterator();
+            ArticleMetadata mdRecord = null;
+            while (mdIt.hasNext()) {
+                mdRecord = (ArticleMetadata) mdIt.next();
+                compareMetadata(mdRecord);
+            }
+        }finally {
+            IOUtil.safeClose(file_input);
+        }
+
+    }
+
+    String[] authorlist = new String[] {"Hamilton, Alexander", "Burr, Aaron"};
+    // quick and dirty, there are only two records
+    private void compareMetadata(ArticleMetadata AM) {
+        assertEquals("publication title", AM.get(MetadataField.FIELD_PUBLICATION_TITLE));
+        assertEquals("2022-5-28", AM.get(MetadataField.FIELD_DATE));
+        assertEquals("publisher name", AM.get(MetadataField.FIELD_PUBLISHER));
+        assertEquals("75", AM.get(MetadataField.FIELD_VOLUME));
+        assertEquals("4", AM.get(MetadataField.FIELD_ISSUE));
+        assertEquals("0004-0614", AM.get(MetadataField.FIELD_ISSN));
+        assertEquals("10.56434/j.arch.esp.urol.20227504.44", AM.get(MetadataField.FIELD_DOI));
+        assertEquals("Oncologic outcomes and predictive factors for progression in pT3a renal cell carcinoma", AM.get(MetadataField.FIELD_ARTICLE_TITLE));
+    }
+}
+
