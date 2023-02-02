ab0b165402a66e3ecf1d3d909ab61192db66de76
Mary-Ellen <maryellen@lockss.org>
2023-02-02 14:55:33 -0800

    Add more content to test.

--- scripts/tdb/review_hw.sh
+++ scripts/tdb/review_hw.sh
@@ -14,19 +14,19 @@ echo ""
 echo "****************"
 echo "****Manifest****"
 echo "****************"
-./scripts/tdb/tdbout -aM -Q 'plugin ~ "highwire" and year ~ "202"' tdb/{prod,clockssingest}/*.tdb | ./scripts/tdb/read_auid_new.pl | grep -v "*MANIFEST"
+./scripts/tdb/tdbout -aM -Q 'plugin ~ "highwire" and year ~ "^20"' tdb/{prod,clockssingest}/*.tdb | ./scripts/tdb/read_auid_new.pl | grep -v "*MANIFEST"
 
 #Test recent AUs that are released (GLN) or crawling (CLOCKSS). Ignore http.
 echo ""
 echo "**********************"
 echo "****Released (GLN)****"
 echo "**********************"
-./scripts/tdb/tdbout -aR -Q 'plugin ~ "highwire" and year ~ "202"' tdb/prod/*.tdb | grep https | ./scripts/tdb/read_auid_new.pl | grep -v "*MANIFEST"
+./scripts/tdb/tdbout -aR -Q 'plugin ~ "highwire" and year ~ "^20"' tdb/prod/*.tdb | grep https | ./scripts/tdb/read_auid_new.pl | grep -v "*MANIFEST"
 echo ""
 echo "**********************"
 echo "****Crawling (CLOCKSS)****"
 echo "**********************"
-./scripts/tdb/tdbout -aC -Q 'plugin ~ "highwire" and year ~ "202"' tdb/clockssingest/*.tdb | grep https | ./scripts/tdb/read_auid_new.pl | grep -v "*MANIFEST"
+./scripts/tdb/tdbout -aC -Q 'plugin ~ "highwire" and year ~ "^20"' tdb/clockssingest/*.tdb | grep https | ./scripts/tdb/read_auid_new.pl | grep -v "*MANIFEST"
 
 exit 0
 
