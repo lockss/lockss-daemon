ecac25cb6d8c5c1cf8b39808201cb64734244a0e
Thib Guicherd-Callin <thib@cs.stanford.edu>
2023-03-14 07:31:50 -0700

    OECD work in progress.

--- plugins/src/org/lockss/plugin/oecd/BaseOecdHtmlCrawlFilterFactory.java
+++ plugins/src/org/lockss/plugin/oecd/BaseOecdHtmlCrawlFilterFactory.java
@@ -1,6 +1,6 @@
 /*
-Copyright (c) 2000-2022, Board of Trustees of Leland Stanford Jr. University
-All rights reserved.
+
+Copyright (c) 2000-2023, Board of Trustees of Leland Stanford Jr. University
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -27,7 +27,9 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
 CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
+
 */
+
 package org.lockss.plugin.oecd;
 
 import org.htmlparser.NodeFilter;
