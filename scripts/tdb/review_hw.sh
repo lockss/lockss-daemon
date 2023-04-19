291e8cd1a3f7a275ee4318096d7553f81d4618f7
Mary-Ellen <maryellen@lockss.org>
2023-04-18 20:08:00 -0700

    Separate out exists and expected results.

--- scripts/tdb/review_hw.sh
+++ scripts/tdb/review_hw.sh
@@ -2,12 +2,19 @@
 #Usage 
 #This script will check the manifest pages for HighWire
 cd /lockss/fagan/lockss-daemon
-#Test AUs that are exists or expected for CLOCKSS & GLN
+#Test AUs that are expected for CLOCKSS & GLN
 echo ""
-echo "**************************"
-echo "****Exists or Expected****"
-echo "**************************"
-./scripts/tdb/tdbout -aEX -Q 'plugin ~ "highwire"' tdb/{prod,clockssingest}/*.tdb | ./scripts/tdb/read_auid_new.pl | grep -v "*MANIFEST"
+echo "****************"
+echo "****Expected****"
+echo "****************"
+./scripts/tdb/tdbout -aX -Q 'plugin ~ "highwire"' tdb/{prod,clockssingest}/*.tdb | ./scripts/tdb/read_auid_new.pl | grep -v "*MANIFEST"
+
+#Test AUs that are exists for CLOCKSS & GLN
+echo ""
+echo "**************"
+echo "****Exists****"
+echo "**************"
+./scripts/tdb/tdbout -aE -Q 'plugin ~ "highwire"' tdb/{prod,clockssingest}/*.tdb | ./scripts/tdb/read_auid_new.pl | grep -v "*MANIFEST"
 
 #Test recent AUs that are manifest for for CLOCKSS & GLN
 echo ""
@@ -23,9 +30,9 @@ echo "****Released (GLN)****"
 echo "**********************"
 ./scripts/tdb/tdbout -aR -Q 'plugin ~ "highwire" and year ~ "^20"' tdb/prod/*.tdb | grep https | ./scripts/tdb/read_auid_new.pl | grep -v "*MANIFEST"
 echo ""
-echo "**********************"
+echo "**************************"
 echo "****Crawling (CLOCKSS)****"
-echo "**********************"
+echo "**************************"
 ./scripts/tdb/tdbout -aC -Q 'plugin ~ "highwire" and year ~ "^20"' tdb/clockssingest/*.tdb | grep https | ./scripts/tdb/read_auid_new.pl | grep -v "*MANIFEST"
 
 exit 0
