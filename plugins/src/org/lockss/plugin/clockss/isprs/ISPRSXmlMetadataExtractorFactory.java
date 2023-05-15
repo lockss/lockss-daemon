4a00681988fdf4534cde06026f680aa228716697
feili18 <feili18@stanford.edu>
2023-05-15 14:23:27 -0700

    Initial commit of ISPRS source plugin

---
+++ plugins/src/org/lockss/plugin/clockss/isprs/ISPRSXmlMetadataExtractorFactory.java
@@ -0,0 +1,82 @@
+/*
+
+Copyright (c) 2000-2023, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package org.lockss.plugin.clockss.isprs;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.daemon.ShouldNotHappenException;
+import org.lockss.extractor.FileMetadataExtractor;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.clockss.JatsPublishingSchemaHelper;
+import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
+import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
+import org.lockss.util.Logger;
+import org.w3c.dom.Document;
+
+public class ISPRSXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
+
+  protected static Logger log = Logger.getLogger(ISPRSXmlMetadataExtractorFactory.class);
+
+  private static SourceXmlSchemaHelper publishingHelper = null;
+
+  @Override
+  public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
+                                                           String contentType)
+      throws PluginException {
+    return new JatsPublishingSourceXmlMetadataExtractor();
+  }
+
+  public static class JatsPublishingSourceXmlMetadataExtractor extends SourceXmlMetadataExtractor {
+
+    /*
+     * This setUpSchema shouldn't be called directly
+     * but for safety, just use the CU to figure out which schema to use.
+     *
+     */
+    @Override
+    protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu) {
+      throw new ShouldNotHappenException("This version of the schema setup cannot be used for this plugin");
+    }
+
+    @Override
+    protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu, Document xmlDoc) {
+      String url = cu.getUrl();
+      if (publishingHelper == null) {
+        publishingHelper = new JatsPublishingSchemaHelper();
+
+      }
+      return publishingHelper;
+    }
+
+  }
+}
