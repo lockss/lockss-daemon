98771d8a92fe605e9b00732ba6307149296e8131
feili18 <feili18@stanford.edu>
2023-06-12 14:35:18 -0700

    Initial commit of Intechopen journals plugin

---
+++ plugins/src/org/lockss/plugin/clockss/intechopen/IntechOpenJournalsDirSourceArticleIteratorFactory.java
@@ -0,0 +1,57 @@
+package org.lockss.plugin.clockss.intechopen;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.ArticleMetadataExtractor;
+import org.lockss.extractor.ArticleMetadataExtractorFactory;
+import org.lockss.extractor.BaseArticleMetadataExtractor;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.plugin.ArticleFiles;
+import org.lockss.plugin.ArticleIteratorFactory;
+import org.lockss.plugin.SubTreeArticleIteratorBuilder;
+import org.lockss.util.Logger;
+
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
+public class IntechOpenJournalsDirSourceArticleIteratorFactory implements ArticleIteratorFactory, ArticleMetadataExtractorFactory {
+
+    protected static Logger log = Logger.getLogger(IntechOpenJournalsDirSourceArticleIteratorFactory.class);
+
+    protected static final String ROOT_TEMPLATE = "\"%s%s\",base_url,directory";
+    private static final String PATTERN_TEMPLATE = "\"%s%s/.+\",base_url,directory";
+
+    protected static final Pattern XML_PATTERN = Pattern.compile("/([^/]+).xml$");
+    protected static final String XML_REPLACEMENT = "/([^/]+).xml$";
+    private static final String PDF_REPLACEMENT = "/$1.pdf";
+
+
+    @Override
+    public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au,
+                                                        MetadataTarget target)
+            throws PluginException {
+        SubTreeArticleIteratorBuilder builder = new SubTreeArticleIteratorBuilder(au);
+
+        builder.setSpec(target,
+                ROOT_TEMPLATE,
+                PATTERN_TEMPLATE, Pattern.CASE_INSENSITIVE);
+
+        builder.addAspect(XML_PATTERN,
+                PDF_REPLACEMENT,
+                ArticleFiles.ROLE_FULL_TEXT_PDF);
+
+        builder.addAspect(XML_PATTERN,
+                XML_REPLACEMENT,
+                ArticleFiles.ROLE_ARTICLE_METADATA);
+
+
+        return builder.getSubTreeArticleIterator();
+    }
+
+    @Override
+    public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target)
+            throws PluginException {
+        return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
+    }
+
+}
---
+++ plugins/src/org/lockss/plugin/clockss/intechopen/IntechOpenJournalsXmlMetadataExtractorFactory.java
@@ -0,0 +1,93 @@
+/*
+ * $Id$
+ */
+
+/*
+
+ Copyright (c) 2000-2016 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+
+package org.lockss.plugin.clockss.intechopen;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.ArticleMetadata;
+import org.lockss.extractor.FileMetadataExtractor;
+import org.lockss.extractor.MetadataField;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.clockss.CrossRefQuerySchemaHelper;
+import org.lockss.plugin.clockss.JatsPublishingSchemaHelper;
+import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
+import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
+import org.lockss.util.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class IntechOpenJournalsXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
+  private static final Logger log = Logger.getLogger(IntechOpenJournalsXmlMetadataExtractorFactory.class);
+
+    private static SourceXmlSchemaHelper CrossRefHelper = null;
+
+  @Override
+  public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
+      String contentType)
+          throws PluginException {
+    return new IntechOpenXmlMetadataExtractor();
+  }
+
+  public class IntechOpenXmlMetadataExtractor extends SourceXmlMetadataExtractor {
+
+    @Override
+    protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu) {
+        if (CrossRefHelper == null) {
+            CrossRefHelper = new IntechOpenCrossRefQuerySchemaHelper();
+        }
+
+        return CrossRefHelper;
+    }
+
+      @Override
+      protected List<String> getFilenamesAssociatedWithRecord(SourceXmlSchemaHelper helper, CachedUrl cu,
+                                                              ArticleMetadata oneAM) {
+
+          String url_string = cu.getUrl();
+          String pdfName = url_string.substring(0,url_string.length() - 3) + "pdf";
+          log.debug3("pdfName is " + pdfName);
+          List<String> returnList = new ArrayList<String>();
+          returnList.add(pdfName);
+          return returnList;
+      }
+
+      @Override
+      protected void postCookProcess(SourceXmlSchemaHelper schemaHelper,
+                                     CachedUrl cu, ArticleMetadata thisAM) {
+
+
+      }
+  }
+}
