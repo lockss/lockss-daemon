c6b84837c33eaf509742b4885f62cdf645b70321
feili18 <feili18@stanford.edu>
2023-09-07 09:23:48 -0700

    update metadata extractor based on year bucket

--- plugins/src/org/lockss/plugin/clockss/innovativemedicalresearchpress/InnovativeMedicalResearchPressSourceXmlMetadataExtractorFactory.java
+++ plugins/src/org/lockss/plugin/clockss/innovativemedicalresearchpress/InnovativeMedicalResearchPressSourceXmlMetadataExtractorFactory.java
@@ -1,6 +1,7 @@
 package org.lockss.plugin.clockss.innovativemedicalresearchpress;
 
 import org.apache.commons.io.FilenameUtils;
+import org.lockss.config.TdbAu;
 import org.lockss.daemon.PluginException;
 import org.lockss.daemon.ShouldNotHappenException;
 import org.lockss.extractor.ArticleMetadata;
@@ -54,21 +55,35 @@ public class InnovativeMedicalResearchPressSourceXmlMetadataExtractorFactory ext
                                                                 ArticleMetadata oneAM) {
 
             String url_string = cu.getUrl();
+            TdbAu tdbau = cu.getArchivalUnit().getTdbAu();
+            String year = null;
+
             List<String> returnList = new ArrayList<String>();
-            //XML and PDF are located inside the same directory but using different naming convension.
-            //XML is named as 2617-5282-1-2-115.xml
-            //PDF is referenced inside each xml as an element like below
-            //<related-article related-article-type="pdf" specific-use="online">1555335526845-369563756.pdf</related-article>
-            //<related-article related-article-type="pdf" specific-use="online">1545898248855-1818078335.pdf</related-article>
-            if (url_string.indexOf(".xml") > -1) {
-                String md_url = cu.getUrl();
-                String cuBase = FilenameUtils.getFullPath(md_url);
 
-                String filenameValue = oneAM.getRaw(JatsPublishingSchemaHelper.JATS_article_related_pdf);
+            if (tdbau != null) {
+                year = tdbau.getParam("year");
 
-                if (filenameValue != null) {
-                    log.debug3("InnovativeMedicalResearchPress: PDF file path from old schema is : " + cuBase + filenameValue);
-                    returnList.add(cuBase + filenameValue);
+                log.debug3("InnovativeMedicalResearchPress: bucket year = " + year);
+
+                if (year != null && (year.contains("2020") || year.contains("2021") || year.contains("2022"))) {
+                    log.debug3("InnovativeMedicalResearchPress: old buckets of 2020, 2021, 2022 bucket, year = " + year);
+
+                    //XML and PDF are located inside the same directory but using different naming convension.
+                    //XML is named as 2617-5282-1-2-115.xml
+                    //PDF is referenced inside each xml as an element like below
+                    //<related-article related-article-type="pdf" specific-use="online">1555335526845-369563756.pdf</related-article>
+                    //<related-article related-article-type="pdf" specific-use="online">1545898248855-1818078335.pdf</related-article>
+                    if (url_string.indexOf(".xml") > -1) {
+                        String md_url = cu.getUrl();
+                        String cuBase = FilenameUtils.getFullPath(md_url);
+
+                        String filenameValue = oneAM.getRaw(JatsPublishingSchemaHelper.JATS_article_related_pdf);
+
+                        if (filenameValue != null) {
+                            log.debug3("InnovativeMedicalResearchPress: PDF file path from old schema is : " + cuBase + filenameValue);
+                            returnList.add(cuBase + filenameValue);
+                        }
+                    }
                 } else {
                     //"2023" bucket is a mixed bucket, it has 8 files using old schema and the rest using new
                     //"new" means one-pdf-one-xml matching starting from 2023
