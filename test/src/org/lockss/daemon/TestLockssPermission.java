518181c653140d5980111b5db0c66ea177ed1c25
Thib Guicherd-Callin <thib@cs.stanford.edu>
2023-10-19 22:26:25 -0700

    Add a CLOCKSS permission statement with open access qualification. Make the two permission objects more alike. Cosmetic.

--- test/src/org/lockss/daemon/TestLockssPermission.java
+++ test/src/org/lockss/daemon/TestLockssPermission.java
@@ -1,32 +1,32 @@
 /*
- * $Id$
- */
 
-/*
+Copyright (c) 2000-2023, Board of Trustees of Leland Stanford Jr. University
 
-Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
-all rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
 
-Except as contained in this notice, the name of Stanford University shall not
-be used in advertising or otherwise to promote the sale, use or other dealings
-in this Software without prior written authorization from Stanford University.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
 
 */
 
@@ -36,21 +36,17 @@ import java.io.*;
 import java.util.*;
 
 import org.lockss.state.*;
+import org.apache.commons.lang3.StringUtils;
 import org.lockss.clockss.*;
-import org.lockss.daemon.CreativeCommonsPermissionChecker;
-import org.lockss.daemon.CreativeCommonsRdfPermissionChecker;
-import org.lockss.daemon.LockssPermission;
-import org.lockss.daemon.StringPermissionChecker;
 import org.lockss.test.*;
 
 public class TestLockssPermission extends LockssPermissionCheckerTestCase {
 
-  private String PERM_STRING = "LOCKSS system has permission to collect, " +
-    "preserve, and serve this Archival Unit";
+  private String PERM_STRING =
+      "LOCKSS system has permission to collect, preserve, and serve this Archival Unit";
 
   private String OPEN_ACCESS_STRING =
-    "LOCKSS system has permission to collect, preserve, " +
-    "and serve this open access Archival Unit";
+      "LOCKSS system has permission to collect, preserve, and serve this open access Archival Unit";
 
   public void setUp() throws Exception {
     super.setUp();
@@ -58,13 +54,12 @@ public class TestLockssPermission extends LockssPermissionCheckerTestCase {
 
   public void testStrings() {
     assertEquals(PERM_STRING, LockssPermission.LOCKSS_PERMISSION_STRING);
-    assertEquals(OPEN_ACCESS_STRING,
-		 LockssPermission.LOCKSS_OPEN_ACCESS_PERMISSION_STRING);
+    assertEquals(OPEN_ACCESS_STRING, LockssPermission.LOCKSS_OPEN_ACCESS_PERMISSION_STRING);
   }
 
   private boolean hasPermission(String page) throws IOException {
     return MiscTestUtil.hasPermission(new LockssPermission().getCheckers(),
-				      page, mcf);
+                                      page, mcf);
   }
 
   public void testNoPermission() throws IOException {
@@ -73,32 +68,32 @@ public class TestLockssPermission extends LockssPermissionCheckerTestCase {
     assertNotEquals(AuState.AccessType.OpenAccess, aus.getAccessType());
   }
 
-  public void testLockssPermission() throws IOException {
-    String padding = org.apache.commons.lang3.StringUtils.repeat("Blah ", 50);
-    assertTrue(hasPermission("LOCKSS system has permission to collect, preserve, and serve this Archival Unit"));
+  public void testStandardLockssPermissionStatement() throws IOException {
+    String padding = StringUtils.repeat("Blah ", 50);
+    assertTrue(hasPermission(PERM_STRING));
     assertEquals(AuState.AccessType.Subscription, aus.getAccessType());
-    assertTrue(hasPermission(padding + "LOCKSS system has permission to collect, preserve, and serve this Archival Unit"));
+    assertTrue(hasPermission(padding + PERM_STRING));
     assertEquals(AuState.AccessType.Subscription, aus.getAccessType());
-    assertTrue(hasPermission("LOCKSS system has permission to collect, preserve, and serve this Archival Unit" + padding));
+    assertTrue(hasPermission(PERM_STRING + padding));
     assertEquals(AuState.AccessType.Subscription, aus.getAccessType());
-    assertTrue(hasPermission(padding + "LOCKSS system has permission to collect, preserve, and serve this Archival Unit" + padding));
+    assertTrue(hasPermission(padding + PERM_STRING + padding));
     assertEquals(AuState.AccessType.Subscription, aus.getAccessType());
   }
 
-  public void testLockssOpenAccessPermission() throws IOException {
-    String padding = org.apache.commons.lang3.StringUtils.repeat("Blah ", 50);
-    assertTrue(hasPermission("LOCKSS system has permission to collect, preserve, and serve this open access Archival Unit"));
+  public void testStandardLockssPermissionStatementWtihOpenAccessQualification() throws IOException {
+    String padding = StringUtils.repeat("Blah ", 50);
+    assertTrue(hasPermission(OPEN_ACCESS_STRING));
     assertEquals(AuState.AccessType.OpenAccess, aus.getAccessType());
-    assertTrue(hasPermission(padding + "LOCKSS system has permission to collect, preserve, and serve this open access Archival Unit"));
+    assertTrue(hasPermission(padding + OPEN_ACCESS_STRING));
     assertEquals(AuState.AccessType.OpenAccess, aus.getAccessType());
-    assertTrue(hasPermission("LOCKSS system has permission to collect, preserve, and serve this open access Archival Unit" + padding));
+    assertTrue(hasPermission(OPEN_ACCESS_STRING + padding));
     assertEquals(AuState.AccessType.OpenAccess, aus.getAccessType());
-    assertTrue(hasPermission(padding + "LOCKSS system has permission to collect, preserve, and serve this open access Archival Unit" + padding));
+    assertTrue(hasPermission(padding + OPEN_ACCESS_STRING + padding));
     assertEquals(AuState.AccessType.OpenAccess, aus.getAccessType());
   }
 
   public void testLockssOjsPermission() throws IOException {
-    String padding = org.apache.commons.lang3.StringUtils.repeat("Blah ", 50);
+    String padding = StringUtils.repeat("Blah ", 50);
     String perm = "This journal utilizes the LOCKSS system to create a distributed archiving system among participating libraries and permits those libraries to create permanent archives of the journal for purposes of preservation and restoration";
     assertTrue(hasPermission(perm));
     assertEquals(AuState.AccessType.Subscription, aus.getAccessType());
@@ -112,39 +107,38 @@ public class TestLockssPermission extends LockssPermissionCheckerTestCase {
 
   public void testNoMatchClockssPermission() throws IOException {
     assertFalse(hasPermission(ClockssPermission.CLOCKSS_PERMISSION_STRING));
+    assertFalse(hasPermission(ClockssPermission.CLOCKSS_OPEN_ACCESS_PERMISSION_STRING));
   }
 
   /**
    * To catch changes to the permission checker list
-   * Currently, this should have four checkers:
-   * 1) A StringPermissionChecker with the LOCKSS Permission Statement
-   * 2) A StringPermissionChecker with the LOCKSS open access Permission Statement
-   * 3) A CreativeCommonsPermissionChecker
-   * 4) A CreativeCommonsV3PermissionChecker
    */
   public void testGetCheckersHasProperCheckers() {
-    List checkers = new LockssPermission().getCheckers();
-    assertEquals("Expected four Permission checkers, but found ",
-		 5, checkers.size());
-    assertTrue("First checker wasn't a StringPermission Checker",
-	       checkers.get(0) instanceof StringPermissionChecker);
-    assertTrue("Second checker wasn't a StringPermission Checker",
-	       checkers.get(1) instanceof StringPermissionChecker);
-    assertTrue("Third checker wasn't a StringPermission Checker",
-	       checkers.get(2) instanceof StringPermissionChecker);
-    assertTrue("Fourth checker wasn't a CreativeCommonsRdfPermissionChecker",
-	       checkers.get(3) instanceof CreativeCommonsRdfPermissionChecker);
-    assertTrue("Fifth checker wasn't a CreativeCommonsV3PermissionChecker",
-	       checkers.get(4) instanceof CreativeCommonsPermissionChecker);
+    List<PermissionChecker> checkers = new LockssPermission().getCheckers();
+    assertEquals("Expected five permission checkers",
+                 5, checkers.size());
+    assertTrue("First checker was not a StringPermission Checker",
+               checkers.get(0) instanceof StringPermissionChecker);
+    assertTrue("Second checker was not a StringPermission Checker",
+               checkers.get(1) instanceof StringPermissionChecker);
+    assertTrue("Third checker was not a StringPermission Checker",
+               checkers.get(2) instanceof StringPermissionChecker);
+    assertTrue("Fourth checker was not a CreativeCommonsV3PermissionChecker",
+               checkers.get(3) instanceof CreativeCommonsPermissionChecker);
+    assertTrue("Fifth checker was not a CreativeCommonsRdfPermissionChecker",
+               checkers.get(4) instanceof CreativeCommonsRdfPermissionChecker);
   }
 
   public void testGetCheckersNotModifiable() {
-    List checkers = new LockssPermission().getCheckers();
+    List<PermissionChecker> checkers = new LockssPermission().getCheckers();
     try {
       checkers.set(0, new StringPermissionChecker("anything",
-						  new StringPermissionChecker.StringFilterRule()));
-      fail("Shouldn't be able to modify permission checker list");
-    } catch (UnsupportedOperationException e) {
+                                                  new StringPermissionChecker.StringFilterRule()));
+      fail("Should not be able to modify permission checker list");
+    }
+    catch (UnsupportedOperationException ignore) {
+      // Expected
     }
   }
+
 }
