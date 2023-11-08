6b9189ef52522cae279a76aac04eca7271cd10c8
Carmen Cox <crc10@stanford.edu>
2023-11-08 14:01:50 -0500

    Added tags to test

--- plugins/test/src/org/lockss/plugin/scielo/TestSciELO2024HtmlHashFilterFactory.java
+++ plugins/test/src/org/lockss/plugin/scielo/TestSciELO2024HtmlHashFilterFactory.java
@@ -20,15 +20,58 @@ public class TestSciELO2024HtmlHashFilterFactory extends LockssTestCase{
                             "FAIL"+
                         "</head>"+
                         "<body>"+
+                            "<a class='${floatingBtnError}'>"+
+                                "FAIL"+
+                            "</a>"+
                             "<header>"+
                                 "FAIL"+
                             "</header>"+
+                            "<div class='alternativeHeader'>"+
+                                "FAIL"+
+                            "</div>"+
+                            "<section class='journalContent'>"+
+                                "<div class='issueIndex'>"+
+                                    "<div class='issueIndex'>"+
+                                        "FAIL"+
+                                    "</div>"+
+                                "</div>"+
+                            "</section>"+
                             "<section class='${levelMenu}'>"+
+                                "FAIL"+
+                            "</section>"+
+                            "<div class='floatingMenuCtt'>"+
+                                "FAIL"+
+                            "</div>"+
+                            "<ul class='${floatingMenuMobile}'>"+
+                                "FAIL"+
+                            "</ul>"+
+                            "<section class='journalContacts'>"+
                                 "FAIL"+
                             "</section>"+
                             "<div class='${share}'>"+
                                 "FAIL"+
                             "</div>"+
+                            "<div id='error_modal_id'>"+
+                                "FAIL"+
+                            "</div>"+
+                            "<div id='ModalRelatedArticles'>"+
+                                "FAIL"+
+                            "</div>"+
+                            "<div id='metric_modal_id'>"+
+                                "FAIL"+
+                            "</div>"+
+                            "<div id='ModalVersionsTranslations'>"+
+                                "FAIL"+
+                            "</div>"+
+                            "<div id='ModalScimago'>"+
+                                "FAIL"+
+                            "</div>"+
+                            "<style>"+
+                                "FAIL"+
+                            "</style>"+
+                            "<script>"+
+                                "FAIL"+
+                            "</script>"+
                             "<footer>"+
                                 "FAIL"+
                             "</footer>"+
@@ -39,11 +82,15 @@ public class TestSciELO2024HtmlHashFilterFactory extends LockssTestCase{
             values.put("levelMenu", levelMenu);
             for(String share:Arrays.asList("share FAIL FAIL FAIL", "FAIL FAIL share FAIL FAIL", "FAIL FAIL FAIL FAIL share")){
                 values.put("share", share);
-                StringSubstitutor sub = new StringSubstitutor(values);
-                InputStream in = IOUtils.toInputStream(sub.replace(page),Constants.DEFAULT_ENCODING);
-                InputStream out = fact.createFilteredInputStream(null, in, Constants.DEFAULT_ENCODING);
-                String result = IOUtils.toString(out, Constants.DEFAULT_ENCODING);
-                assertFalse(result.contains("FAIL")); //TODO: add fail message
+                for(String floatingBtnError:Arrays.asList("floatingBtnError FAIL FAIL FAIL", "FAIL FAIL floatingBtnError FAIL FAIL", "FAIL FAIL FAIL FAIL floatingBtnError"))
+                    values.put("floatingBtnError", floatingBtnError);
+                    for(String floatingMenuMobile:Arrays.asList("floatingMenuMobile FAIL FAIL FAIL", "FAIL FAIL floatingMenuMobile FAIL FAIL", "FAIL FAIL FAIL FAIL floatingMenuMobile"))
+                        values.put("floatingMenuMobile",floatingMenuMobile);
+                        StringSubstitutor sub = new StringSubstitutor(values);
+                        InputStream in = IOUtils.toInputStream(sub.replace(page),Constants.DEFAULT_ENCODING);
+                        InputStream out = fact.createFilteredInputStream(null, in, Constants.DEFAULT_ENCODING);
+                        String result = IOUtils.toString(out, Constants.DEFAULT_ENCODING);
+                        assertFalse(result.contains("FAIL")); //TODO: add fail message
             }
         }
     }
