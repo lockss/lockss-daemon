e56fdb30b339d9915e977f05b67cd9ad7a25876a
Carmen Cox <crc10@stanford.edu>
2023-12-04 17:30:21 -0500

    added transformer to write out xml structure to outputstream for giga XmlHashFilterFactory, updated corresponding test case

--- plugins/src/org/lockss/plugin/gigascience/GigaScienceXmlHashFilterFactory.java
+++ plugins/src/org/lockss/plugin/gigascience/GigaScienceXmlHashFilterFactory.java
@@ -30,61 +30,99 @@ POSSIBILITY OF SUCH DAMAGE.
 
 */
 package org.lockss.plugin.gigascience;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import java.io.InputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.IllegalFormatException;
 import java.util.List;
+import java.util.stream.Stream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.lockss.daemon.PluginException;
 import org.lockss.plugin.ArchivalUnit;
 import org.lockss.plugin.FilterFactory;
-import org.lockss.util.Logger;
+import org.lockss.util.DeferredTempFileOutputStream;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class GigaScienceXmlHashFilterFactory implements FilterFactory{
-  static Logger log = Logger.getLogger(
-      GigaScienceXmlHashFilterFactory.class);
+
     @Override
     public InputStream createFilteredInputStream(ArchivalUnit au, InputStream in, String encoding)
             throws PluginException {
         
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
         try {
-            DocumentBuilder builder = factory.newDocumentBuilder();
+            DocumentBuilder builder = builderFactory.newDocumentBuilder();
             Document doc = builder.parse(in);
+            NodeList filesNodeList = doc.getElementsByTagName("files");
+
+            //there should be only one files tag
+            if(filesNodeList.getLength() != 1){
+                //handle exception
+            }
+
+            Node filesNode = filesNodeList.item(0); 
             NodeList nodes = doc.getElementsByTagName("file");
-            sort(nodes);
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            // TODO Auto-generated catch block
+            System.out.println("Node is" + nodes.item(0).getTextContent());
+            List<Element> sortedElements = sort(nodes); 
+            System.out.println("Node is" + sortedElements.get(0).getAttribute("id"));
+
+            for(int i=0; i < nodes.getLength(); i++){
+                filesNode.removeChild(nodes.item(i));
+            }
+
+            for(Element sortedElement: sortedElements){
+                filesNode.appendChild(sortedElement);
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            DeferredTempFileOutputStream out = new DeferredTempFileOutputStream(1024*1024);
+            StreamResult result = new StreamResult(out);
+            transformer.transform(source, result);
+            return out.getDeleteOnCloseInputStream();
+
+        } catch (ParserConfigurationException | SAXException | IOException | TransformerException e) {
             e.printStackTrace();
         }
         return in;
     }
 
-    private static List<Element> sort(NodeList classesNodeList) {
-        List<Element> nodes = new ArrayList<Element>();
-        for (int i = 0; i < classesNodeList.getLength(); i++) {
-            nodes.add((Element) classesNodeList.item(i));
+    private static List<Element> sort(NodeList nodes) {
+        List<Element> sortedNodes = new ArrayList<Element>();
+        for (int i = 0; i < nodes.getLength(); i++) {
+            sortedNodes.add((Element) nodes.item(i));
         }
-
-        Collections.sort(nodes, new Comparator<Element>() {
+        
+        Collections.sort(sortedNodes, new Comparator<Element>() {
             public int compare(Element o1, Element o2) {
+                //System.out.println(o1.getAttribute("id"));
                 return o1.getAttribute("id").compareTo(
                         o2.getAttribute("id"));
             }
         });
 
-        return nodes;
+        return sortedNodes;
     }
     
 }
