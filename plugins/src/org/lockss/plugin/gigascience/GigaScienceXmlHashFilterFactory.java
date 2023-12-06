9208f8131353810da05a3ac69d684bef5d8a39a3
Carmen Cox <crc10@stanford.edu>
2023-12-06 12:33:15 -0500

    Squashed commit of the following:
    commit 7627f2587f7f34cf27f1c8aeba8ccda7c864428a
    Author: Carmen Cox <crc10@stanford.edu>
    Date:   Wed Dec 6 12:31:54 2023 -0500
    
        updated plugin/poll version, fixed exception problem for giga science
    
    commit d0e32c056b6a4c65e5d21f1261d1c1496a828477
    Author: Carmen Cox <crc10@stanford.edu>
    Date:   Tue Dec 5 15:53:01 2023 -0500
    
        added gigascienceXmlHashFilterFactory to plugin
    
    commit 7b60d5d0909f2e733f656ce5fa78781fa1d4d9d6
    Author: Carmen Cox <crc10@stanford.edu>
    Date:   Tue Dec 5 13:56:01 2023 -0500
    
        updated test case, added exceptions to giga science
    
    commit e56fdb30b339d9915e977f05b67cd9ad7a25876a
    Author: Carmen Cox <crc10@stanford.edu>
    Date:   Mon Dec 4 17:30:21 2023 -0500
    
        added transformer to write out xml structure to outputstream for giga XmlHashFilterFactory, updated corresponding test case
    
    commit 09eeb77f081581eeafd3c6ed6965183bd37c4b59
    Author: Carmen Cox <crc10@stanford.edu>
    Date:   Mon Dec 4 10:50:01 2023 -0500
    
        added xml hash filter factory to gigascience

---
+++ plugins/src/org/lockss/plugin/gigascience/GigaScienceXmlHashFilterFactory.java
@@ -0,0 +1,129 @@
+/*
+
+Copyright (c) 2000-2023, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+package org.lockss.plugin.gigascience;
+import java.io.IOException;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.lockss.daemon.PluginException;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.plugin.FilterFactory;
+import org.lockss.util.DeferredTempFileOutputStream;
+import org.lockss.util.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class GigaScienceXmlHashFilterFactory implements FilterFactory{
+
+    private static final Logger logger = Logger.getLogger(GigaScienceXmlHashFilterFactory.class); 
+
+    @Override
+    public InputStream createFilteredInputStream(ArchivalUnit au, InputStream in, String encoding) throws PluginException{
+
+        try{
+            Document doc = createDoc(in);
+            NodeList filesNodeList = doc.getElementsByTagName("files");
+
+            //there should be only one files tag
+            if(filesNodeList.getLength() != 1){
+                throw new PluginException("There should only be one files tag in document.");
+            }
+            
+            Node filesNode = filesNodeList.item(0); 
+            NodeList nodes = doc.getElementsByTagName("file");
+            List<Element> sortedElements = sort(nodes); 
+
+            for(int i=0; i < nodes.getLength(); i++){
+                filesNode.removeChild(nodes.item(i));
+            }
+            
+            for(Element sortedElement: sortedElements){
+                sortedElement.removeAttribute("download_count");
+                filesNode.appendChild(sortedElement);
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            DeferredTempFileOutputStream out = new DeferredTempFileOutputStream(1024*1024);
+            StreamResult result = new StreamResult(out);
+            transformer.transform(source, result);
+            return out.getDeleteOnCloseInputStream();
+        }
+        catch (PluginException | ParserConfigurationException | SAXException | IOException | TransformerException e) {
+            e.getStackTrace();
+            throw new PluginException(e);
+        }
+    }
+
+    private static List<Element> sort(NodeList nodes) {
+        List<Element> sortedNodes = new ArrayList<Element>();
+        for (int i = 0; i < nodes.getLength(); i++) {
+            sortedNodes.add((Element) nodes.item(i));
+        }
+        
+        Collections.sort(sortedNodes, new Comparator<Element>() {
+            public int compare(Element o1, Element o2) {
+                //System.out.println(o1.getAttribute("id"));
+                return o1.getAttribute("id").compareTo(
+                        o2.getAttribute("id"));
+            }
+        });
+
+        return sortedNodes;
+    }
+
+    //create document object from input stream
+    public Document createDoc(InputStream in) throws ParserConfigurationException, SAXException, IOException{
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = builderFactory.newDocumentBuilder();
+        Document doc = builder.parse(in);
+        return doc;
+    }
+    
+}
---
+++ plugins/test/src/org/lockss/plugin/gigascience/TestGigaScienceXmlHashFilterFactory.java
@@ -0,0 +1,101 @@
+/*
+
+Copyright (c) 2000-2023, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package org.lockss.plugin.gigascience;
+
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.lockss.plugin.FilterFactory;
+import org.lockss.test.LockssTestCase;
+import org.lockss.util.Constants;
+
+public class TestGigaScienceXmlHashFilterFactory extends LockssTestCase{
+    public void testFilter() throws Exception{
+        FilterFactory fact = new GigaScienceXmlHashFilterFactory();
+        String page =   "<gigadb_entry>"+
+                            "<dataset id='2082' doi='100661'>"+
+                            "</dataset>"+
+                            "<samples> </samples>"+
+                            "<experiments> </experiments>"+
+                            "<files>"+
+                                "<file id='419285' download_count='3'>"+
+                                    "<name>The-species-list-of-phase-I_Supp-tab3.csv</name>"+
+                                    "<type id='7'>Other</type>"+
+                                    "<format id='37'>CSV</format>"+
+                                "</file>"+
+                                "<file id='419284' download_count='3'>"+
+                                    "<name>public-fish-assembly-info_Supp-Tab1.tsv</name>"+
+                                    "<type id='131'>Tabular data</type>"+
+                                    "<format id='43'>TSV</format>"+
+                                "</file>"+
+                                "<file id='419283' download_count='8'>"+
+                                    "<name>Phylogenetic_tree_of_8_species.nwk</name>"+
+                                    "<type id='130'>Phylogenetic tree</type>"+
+                                    "<format id='48'>Newick</format>"+
+                                "</file>"+
+                            "</files>"+
+                        "</gigadb_entry>";
+        InputStream in = IOUtils.toInputStream(page,Constants.DEFAULT_ENCODING);
+        InputStream out = fact.createFilteredInputStream(null, in, Constants.DEFAULT_ENCODING);
+        String result = IOUtils.toString(out, Constants.DEFAULT_ENCODING);
+
+        String correctPage =   "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+
+                        "<gigadb_entry>"+
+                            "<dataset doi=\"100661\" id=\"2082\"/>"+
+                            "<samples> </samples>"+
+                            "<experiments> </experiments>"+
+                            "<files>"+
+                                "<file id=\"419283\">"+
+                                    "<name>Phylogenetic_tree_of_8_species.nwk</name>"+
+                                    "<type id=\"130\">Phylogenetic tree</type>"+
+                                    "<format id=\"48\">Newick</format>"+
+                                "</file>"+
+                                "<file id=\"419284\">"+
+                                    "<name>public-fish-assembly-info_Supp-Tab1.tsv</name>"+
+                                    "<type id=\"131\">Tabular data</type>"+
+                                    "<format id=\"43\">TSV</format>"+
+                                "</file>"+
+                                "<file id=\"419285\">"+
+                                    "<name>The-species-list-of-phase-I_Supp-tab3.csv</name>"+
+                                    "<type id=\"7\">Other</type>"+
+                                    "<format id=\"37\">CSV</format>"+
+                                "</file>"+
+                            "</files>"+
+                        "</gigadb_entry>";
+                
+        //System.out.println(result);
+        //System.out.println(correctPage);
+        assertTrue(result.equals(correctPage));
+        
+    }
+}
