0bd4ec0be94d3721ae0b365f57f09f7fe161ad64
Mary-Ellen <maryellen@lockss.org>
2024-02-19 09:52:29 -0800

    Add comment.

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -3903,6 +3903,7 @@ while (my $line = <>) {
       my $dy = '28';
       my @mon = ('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12');
       my @ldy = ('31', '28', '31', '30', '31', '30', '31', '31', '30', '31', '30', '31');
+      #if (year is divisible by 4) then February has 29 days.
       my %lday;
       @lday{@mon} = @ldy;
       $dy = $lday{$mo};
