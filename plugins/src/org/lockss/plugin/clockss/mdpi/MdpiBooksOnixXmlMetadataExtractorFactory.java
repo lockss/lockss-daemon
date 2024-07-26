e9d5071ca2921aa542e1fbf452ff5b9eb736835f
feili18 <feili18@stanford.edu>
2024-07-26 10:31:42 -0700

    Initial commit of MDPI books plugin

---
+++ plugins/src/org/lockss/plugin/clockss/mdpi/MdpiBooksOnixXmlMetadataExtractorFactory.java
@@ -0,0 +1,96 @@
+/*
+
+Copyright (c) 2000-2023, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package org.lockss.plugin.clockss.mdpi;
+
+import org.apache.commons.io.FilenameUtils;
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.ArticleMetadata;
+import org.lockss.extractor.FileMetadataExtractor;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.clockss.Onix3BooksSchemaHelper;
+import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
+import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
+import org.lockss.util.Logger;
+import org.lockss.util.UrlUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MdpiBooksOnixXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
+  private static final Logger log = Logger.getLogger(MdpiBooksOnixXmlMetadataExtractorFactory.class);
+
+  private static SourceXmlSchemaHelper Onix3Helper = null;
+
+  @Override
+  public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
+      String contentType)
+          throws PluginException {
+    return new MdpiOnix3XmlMetadataExtractor();
+  }
+
+  public class MdpiOnix3XmlMetadataExtractor extends SourceXmlMetadataExtractor {
+
+    @Override
+    protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu) {
+      // Once you have it, just keep returning the same one. It won't change.
+      if (Onix3Helper == null) {
+        Onix3Helper = new Onix3BooksSchemaHelper();
+      }
+
+      log.debug3("Setup Mdpi Onix3BooksSchemaHelper");
+      return Onix3Helper;
+    }
+
+
+    /* In this case, use the RecordReference + .pdf for the matching file */
+    @Override
+    protected List<String> getFilenamesAssociatedWithRecord(SourceXmlSchemaHelper helper, CachedUrl cu,
+        ArticleMetadata oneAM) {
+
+
+      String  cuBase = cu.getUrl();
+      List<String> returnList = new ArrayList<String>();
+
+      String fullPathFile = UrlUtil.minimallyEncodeUrl(cuBase.replace(".onix.xml", ".pdf"));
+
+      log.debug3("cuBase = " + cuBase + ", fullPathFile = " + fullPathFile);
+
+      returnList.add(fullPathFile);
+
+      return returnList;
+    }
+
+  }
+}
