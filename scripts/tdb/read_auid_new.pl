72eeb6efe485c786a7361c51ab882f28836407dd
Mary-Ellen <maryellen@lockss.org>
2024-09-23 15:18:16 -0700

    Comment out janeway checker till plugins are released.

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -1406,107 +1406,107 @@ while (my $line = <>) {
     }
     sleep(4);
 
-  #Janeway. No journal_id
-  } elsif (($plugin eq "OLHPlugin") || 
-           ($plugin eq "ClockssOLHPlugin") ||
-           ($plugin eq "ClockssAperioJournalsPlugin") ||
-           ($plugin eq "UniversityofMichiganPlugin") ||
-           ($plugin eq "ClockssUniversityofMichiganPlugin") ||
-           ($plugin eq "GhentUniversityLibraryDomainModeJournalsPlugin") ||
-           ($plugin eq "ClockssGhentUniversityLibraryDomainModeJournalsPlugin")) {
-    #permission is different from start
-    $perm_url = uri_unescape($param{base_url}) . "plugins/clockss/";
-    #$perm_url = uri_unescape($param{base_url}) . "clockss/";
-      #printf("URL: %s\n", $perm_url); #debug
-      $vol_title = $perm_url ;
-    #start_url for all OAI queries https://www.comicsgrid.com/api/oai/?verb=ListRecords&metadataPrefix=oai_dc&from=2019-01-01&until=2019-12-31
-    $url = sprintf("%sapi/oai/?verb=ListRecords&amp;metadataPrefix=oai_dc&amp;from=%d-01-01&amp;until=%d-12-31",
-      $param{base_url}, $param{year}, $param{year});
-    $man_url = uri_unescape($url);
-    my $req_p = HTTP::Request->new(GET, $perm_url);
-    my $resp_p = $ua->request($req_p);
-    my $req_s = HTTP::Request->new(GET, $man_url);
-    my $resp_s = $ua->request($req_s);
-    
-    if ($resp_p->is_success) {
-        my $perm_contents = $resp_p->content;
-        #my $lcl_tag = $clockss_tag;
-        if (($req_p->url ne $resp_p->request->uri) || ($req_s->url ne $resp_s->request->uri)) {
-            $vol_title = $resp_p->request->uri . "+" . $resp_s->request->uri;
-            $result = "Redirected";
-        } elsif (defined($perm_contents) && ($perm_contents =~ m/$clockss_tag/s) && ($perm_contents =~ m/$lockss_tag/s)) {
-#        if (defined($perm_contents) && ($perm_contents =~ m/$clockss_tag/s) && ($perm_contents =~ m/$lockss_tag/s)) {
-            if ($resp_s->is_success) {
-                if ($resp_s->content =~ m/results in an empty (set|list)/is) {
-                    $result = "--EMPTY_LIST--"
-                } elsif ($resp_s->content !~ m/<dc:date>$param{year}/ ) {
-                    $result = "--MISSING-YEAR--"
-                } else {
-                    $result = "Manifest";
-                }
-            } else {
-                #printf("URL: %s\n", $man_url);
-                $result = "--REQ_FAIL--"
-            }
-        } else {
-        #printf("URL: %s\n", $perm_url);
-        $result = "--NO_LOCKSS--"
-      }
-    } else {
-      #printf("URL: %s\n", $perm_url);
-      $result = "--PERM_REQ_FAIL--"
-    }
-    sleep(4);
-
-  #Janeway. With journal_id
-  } elsif (($plugin eq "ClockssIowaStateDPPlugin") ||
-           ($plugin eq "IowaStateDPPlugin") ||
-           ($plugin eq "ClockssUniversityofIowaPlugin") ||
-           ($plugin eq "UniversityofIowaPlugin") ||
-           ($plugin eq "ClockssGhentUniversityLibraryPlugin") ||
-           ($plugin eq "GhentUniversityLibraryPlugin")) {
-    #permission is different from start
-    $perm_url = uri_unescape($param{base_url}) . $param{journal_id} . "/plugins/clockss/";
-    #printf("URL: %s\n", $perm_url); #debug
-    $vol_title = $perm_url ;
-    #start_url for all OAI queries https://www.comicsgrid.com/api/oai/?verb=ListRecords&metadataPrefix=oai_dc&from=2019-01-01&until=2019-12-31
-    $url = sprintf("%s%s/api/oai/?verb=ListRecords&amp;metadataPrefix=oai_dc&amp;from=%d-01-01&amp;until=%d-12-31",
-      $param{base_url}, $param{journal_id}, $param{year}, $param{year});
-    $man_url = uri_unescape($url);
-    my $req_p = HTTP::Request->new(GET, $perm_url);
-    my $resp_p = $ua->request($req_p);
-    my $req_s = HTTP::Request->new(GET, $man_url);
-    my $resp_s = $ua->request($req_s);
-    
-    if ($resp_p->is_success) {
-      my $perm_contents = $resp_p->content;
-      #my $lcl_tag = $clockss_tag;
-        if (($req_p->url ne $resp_p->request->uri) || ($req_s->url ne $resp_s->request->uri)) {
-            $vol_title = $resp_p->request->uri . "+" . $resp_s->request->uri;
-            $result = "Redirected";
-        } elsif (defined($perm_contents) && ($perm_contents =~ m/$clockss_tag/s) && ($perm_contents =~ m/$lockss_tag/s)) {
-        if ($resp_s->is_success) {
-          if ($resp_s->content =~ m/results in an empty (set|list)/is) {
-            $result = "--EMPTY_LIST--"
-          } elsif ($resp_s->content !~ m/<dc:date>$param{year}/ ) {
-            $result = "--MISSING-YEAR--"
-          } else {
-            $result = "Manifest";
-          }
-        } else {
-          #printf("URL: %s\n", $man_url);
-          $result = "--REQ_FAIL--"
-        }
-      } else {
-        #printf("URL: %s\n", $perm_url);
-        $result = "--NO_LOCKSS--"
-      }
-    } else {
-      #printf("URL: %s\n", $perm_url);
-      $result = "--PERM_REQ_FAIL--"
-    }
-    sleep(4);
-
+#  #Janeway. No journal_id
+#  } elsif (($plugin eq "OLHPlugin") || 
+#           ($plugin eq "ClockssOLHPlugin") ||
+#           ($plugin eq "ClockssAperioJournalsPlugin") ||
+#           ($plugin eq "UniversityofMichiganPlugin") ||
+#           ($plugin eq "ClockssUniversityofMichiganPlugin") ||
+#           ($plugin eq "GhentUniversityLibraryDomainModeJournalsPlugin") ||
+#           ($plugin eq "ClockssGhentUniversityLibraryDomainModeJournalsPlugin")) {
+#    #permission is different from start
+#    $perm_url = uri_unescape($param{base_url}) . "plugins/clockss/";
+#    #$perm_url = uri_unescape($param{base_url}) . "clockss/";
+#      #printf("URL: %s\n", $perm_url); #debug
+#      $vol_title = $perm_url ;
+#    #start_url for all OAI queries https://www.comicsgrid.com/api/oai/?verb=ListRecords&metadataPrefix=oai_dc&from=2019-01-01&until=2019-12-31
+#    $url = sprintf("%sapi/oai/?verb=ListRecords&amp;metadataPrefix=oai_dc&amp;from=%d-01-01&amp;until=%d-12-31",
+#      $param{base_url}, $param{year}, $param{year});
+#    $man_url = uri_unescape($url);
+#    my $req_p = HTTP::Request->new(GET, $perm_url);
+#    my $resp_p = $ua->request($req_p);
+#    my $req_s = HTTP::Request->new(GET, $man_url);
+#    my $resp_s = $ua->request($req_s);
+#    
+#    if ($resp_p->is_success) {
+#        my $perm_contents = $resp_p->content;
+#        #my $lcl_tag = $clockss_tag;
+#        if (($req_p->url ne $resp_p->request->uri) || ($req_s->url ne $resp_s->request->uri)) {
+#            $vol_title = $resp_p->request->uri . "+" . $resp_s->request->uri;
+#            $result = "Redirected";
+#        } elsif (defined($perm_contents) && ($perm_contents =~ m/$clockss_tag/s) && ($perm_contents =~ m/$lockss_tag/s)) {
+##        if (defined($perm_contents) && ($perm_contents =~ m/$clockss_tag/s) && ($perm_contents =~ m/$lockss_tag/s)) {
+#            if ($resp_s->is_success) {
+#                if ($resp_s->content =~ m/results in an empty (set|list)/is) {
+#                    $result = "--EMPTY_LIST--"
+#                } elsif ($resp_s->content !~ m/<dc:date>$param{year}/ ) {
+#                    $result = "--MISSING-YEAR--"
+#                } else {
+#                    $result = "Manifest";
+#                }
+#            } else {
+#                #printf("URL: %s\n", $man_url);
+#                $result = "--REQ_FAIL--"
+#            }
+#        } else {
+#        #printf("URL: %s\n", $perm_url);
+#        $result = "--NO_LOCKSS--"
+#      }
+#    } else {
+#      #printf("URL: %s\n", $perm_url);
+#      $result = "--PERM_REQ_FAIL--"
+#    }
+#    sleep(4);
+#
+#  #Janeway. With journal_id
+#  } elsif (($plugin eq "ClockssIowaStateDPPlugin") ||
+#           ($plugin eq "IowaStateDPPlugin") ||
+#           ($plugin eq "ClockssUniversityofIowaPlugin") ||
+#           ($plugin eq "UniversityofIowaPlugin") ||
+#           ($plugin eq "ClockssGhentUniversityLibraryPlugin") ||
+#           ($plugin eq "GhentUniversityLibraryPlugin")) {
+#    #permission is different from start
+#    $perm_url = uri_unescape($param{base_url}) . $param{journal_id} . "/plugins/clockss/";
+#    #printf("URL: %s\n", $perm_url); #debug
+#    $vol_title = $perm_url ;
+#    #start_url for all OAI queries https://www.comicsgrid.com/api/oai/?verb=ListRecords&metadataPrefix=oai_dc&from=2019-01-01&until=2019-12-31
+#    $url = sprintf("%s%s/api/oai/?verb=ListRecords&amp;metadataPrefix=oai_dc&amp;from=%d-01-01&amp;until=%d-12-31",
+#      $param{base_url}, $param{journal_id}, $param{year}, $param{year});
+#    $man_url = uri_unescape($url);
+#    my $req_p = HTTP::Request->new(GET, $perm_url);
+#    my $resp_p = $ua->request($req_p);
+#    my $req_s = HTTP::Request->new(GET, $man_url);
+#    my $resp_s = $ua->request($req_s);
+#    
+#    if ($resp_p->is_success) {
+#      my $perm_contents = $resp_p->content;
+#      #my $lcl_tag = $clockss_tag;
+#        if (($req_p->url ne $resp_p->request->uri) || ($req_s->url ne $resp_s->request->uri)) {
+#            $vol_title = $resp_p->request->uri . "+" . $resp_s->request->uri;
+#            $result = "Redirected";
+#        } elsif (defined($perm_contents) && ($perm_contents =~ m/$clockss_tag/s) && ($perm_contents =~ m/$lockss_tag/s)) {
+#        if ($resp_s->is_success) {
+#          if ($resp_s->content =~ m/results in an empty (set|list)/is) {
+#            $result = "--EMPTY_LIST--"
+#          } elsif ($resp_s->content !~ m/<dc:date>$param{year}/ ) {
+#            $result = "--MISSING-YEAR--"
+#          } else {
+#            $result = "Manifest";
+#          }
+#        } else {
+#          #printf("URL: %s\n", $man_url);
+#          $result = "--REQ_FAIL--"
+#        }
+#      } else {
+#        #printf("URL: %s\n", $perm_url);
+#        $result = "--NO_LOCKSS--"
+#      }
+#    } else {
+#      #printf("URL: %s\n", $perm_url);
+#      $result = "--PERM_REQ_FAIL--"
+#    }
+#    sleep(4);
+#
   } elsif ($plugin eq "GeorgThiemeVerlagPlugin") {
         #Url with list of urls for issues
         #printf("%s\n",decode_entities($tmp));
