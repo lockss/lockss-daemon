4db6d86ab69d4356cea89bc1b2636f946b1e5900
Mary-Ellen <maryellen@lockss.org>
2024-10-16 23:04:42 -0700

    Add new content.

--- scripts/tdb/check_ams_manifest_pages.pl
+++ scripts/tdb/check_ams_manifest_pages.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# This script scrapes the usdocs website to create a complete list of all the available AUs, not including the federal courts
+# This script scrapes the ams website to create a complete list of all the available AUs
 
 use URI::Escape;
 use Getopt::Long;
@@ -14,19 +14,17 @@ use Encode qw(decode encode);
 
 my $url = "cant_create_url";
 
-# Create user agent.
-my $ua = LWP::UserAgent->new;
-#User Agent
-    $ua->agent('Mozilla/8.0');
-# Cookies. Don't save cookies from run to run
-    $ua->cookie_jar(HTTP::Cookies->new());
+# Don't save these cookies from run to run.
+my $cjar = HTTP::Cookies->new();
 
-#tdbout -Dc param[collection_id],year tdb/usdocspln/united_states_government_publishing_office.fdsys.tdb > tmp/list1a
-#tdbout -MTYRc param[collection_id],year tdb/usdocspln/united_states_government_publishing_office.tdb >> tmp/list1a
-#cat tmp/list1a | sort  > tmp/list1b
-#cat tmp/list1b | uniq > tmp/list1c
-#echo "Duplicate AUs in previous and new tdb files."
-#comm -23 tmp/list1b tmp/list1c
+# Create user agent.
+my $ua = LWP::UserAgent->new( cookie_jar => $cjar, agent => "LOCKSS cache", ssl_opts => { verify_hostname => 0 } );
+$ua->proxy('http', 'http://proxy.lockss.org:3128/');
+$ua->no_proxy('localhost', '127.0.0.1');
+
+#tdbout -EXMZFCMTYLc param[collection_id] tdb/clockssingest/american_mathematical_society.books.tdb | sort -u | ./scripts/tdb/check_ams_manifest_pages.pl | sort -u > ../SageEdits/ams_site_list
+#tdbout -EXMZFCMTYLBc param[collection_id],param[year_string] tdb/clockssingest/american_mathematical_society.books.tdb | sort -u | sed s'/^/https:\/\/www.ams.org\//' | sed s'/,/\/year\//' > ../SageEdits/ams_tdb_list
+#comm -23 ../SageEdits/ams_site_list ../SageEdits/ams_tdb_list
 #*************************************
 
 my @collection_list=();
@@ -41,21 +39,23 @@ foreach my $collection (@collection_list) {
   #collect the list of all the urls, and collect all the years.
   #output collection_id,year
   $url2 = sprintf("https://www.ams.org/clockssdata?p=%s", $collection);
-  #printf("$url2\n");
-  #printf("%s\n",$url2);
+  #printf("$url2\n"); #debug
   my $req2 = HTTP::Request->new(GET, $url2);
   my $resp2 = $ua->request($req2);
   if ($resp2->is_success) {
-    #printf("Success2\n");
+    #printf("Success2\n"); #debug
     my $man_contents2 = $resp2->content;
     if ($req2->url ne $resp2->request->uri) {
       printf("Redirected from %s\n", $url2);
-    } elsif (defined($man_contents2) && ($man_contents2 =~ m/a/)){
+    #} elsif (defined($man_contents2) && ($man_contents2 =~ m#href=\"/${collection}/[^>]*(\d+)\">#)){
+    } elsif (defined($man_contents2) && ($man_contents2 =~ m%/${collection}/[^>]*(\d+)%)){
+    #} elsif (defined($man_contents2) && ($man_contents2 =~ m/a/)){
       #printf("Not redirected and has a match.\n");
+      #printf("%s\n", $man_contents2); #debug
       #https://www.govinfo.gov/sitemap/CRPT_2001_sitemap.xml
       foreach my $line2 (split(/\n/m, $man_contents2)) {
-        if ($line2 =~ m#sitemap/${collection}_(\d+)_sitemap\.xml#) {
-          printf("%s,$1\n", $collection);
+        if ($line2 =~ m%/(${collection}/[^>]*(\d+))%) {
+          printf("https://www.ams.org/$1\n");
         }
       }
     }
@@ -64,11 +64,4 @@ foreach my $collection (@collection_list) {
   }
 } 
 
-#
-#Then output the collection name (e.g. BILLS) and year (e.g. 2001), comma seperated
-#sort, output list2
-#*************************************
-#Compare the two lists
-#Output the items in list2 that are unique 
-#
 exit(0);
