a144c6366b733e686fbe7664f69ec86a5fe177f5
Carmen Cox <crc10@stanford.edu>
2024-10-31 15:07:38 -0400

    pdf filter factory for AMS

--- plugins/src/org/lockss/plugin/americanmathematicalsociety/AmericanMathematicalSocietyPdfFilterFactory.java
+++ plugins/src/org/lockss/plugin/americanmathematicalsociety/AmericanMathematicalSocietyPdfFilterFactory.java
@@ -32,20 +32,80 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package org.lockss.plugin.americanmathematicalsociety;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
+
 import org.lockss.filter.pdf.SimplePdfFilterFactory;
 import org.lockss.pdf.PdfDocument;
 import org.lockss.pdf.PdfException;
+import org.lockss.pdf.PdfPage;
+import org.lockss.pdf.PdfToken;
+import org.lockss.pdf.PdfTokenStream;
+import org.lockss.pdf.PdfTokenStreamStateMachine;
+import org.lockss.pdf.PdfUtil;
 import org.lockss.plugin.ArchivalUnit;
 import org.lockss.util.Logger;
 
 public class AmericanMathematicalSocietyPdfFilterFactory extends SimplePdfFilterFactory{
 
     private static final Logger log = Logger.getLogger(AmericanMathematicalSocietyPdfFilterFactory.class);
+    public static Pattern DOWNLOADED_FROM_PATTERN = Pattern.compile("Licensed to.*Eproduct Archive.*Prepared on.*for download from IP.*");
 
     @Override
-    public void transform(ArchivalUnit au, PdfDocument anObject) throws PdfException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'transform'");
+    public void transform(ArchivalUnit au, PdfDocument pdfDocument) throws PdfException {
+        PdfUtil.normalizeTrailerId(pdfDocument);
+        AMSDownloadedFromStateMachine worker = new AMSDownloadedFromStateMachine(DOWNLOADED_FROM_PATTERN);
+        for (PdfPage pdfPage : pdfDocument.getPages()) {
+          List<PdfTokenStream> pdfTokenStreams = pdfPage.getAllTokenStreams();
+          //PdfTokenStream pdfTokenStream = pdfTokenstreams.get(pdfTokenstreams.size() - 1);
+          for (Iterator<PdfTokenStream> iter = pdfTokenStreams.iterator(); iter.hasNext();) {
+            PdfTokenStream nextTokStream = iter.next();
+            worker.process(nextTokStream);      
+            if (worker.getResult()) {
+              List<PdfToken> pdfTokens = nextTokStream.getTokens();
+              pdfTokens.subList(worker.getBegin(), worker.getEnd() + 1).clear();
+              nextTokStream.setTokens(pdfTokens);
+              break; // out of the stream loop, go on to next page
+            }
+          }
+        }
     }
-    
+
+  public static class AMSDownloadedFromStateMachine extends PdfTokenStreamStateMachine {
+
+    // The footer is close to the bottom of each page
+    public AMSDownloadedFromStateMachine(Pattern downloadPattern) {
+      super();
+    }
+
+    @Override
+    public void state0() throws PdfException {
+      if (isBeginTextObject()) {
+        setBegin(getIndex());
+        setState(1);
+      }
+    } 
+
+    @Override
+    public void state1() throws PdfException {
+      if (isShowTextFind(DOWNLOADED_FROM_PATTERN)) {
+        setState(2);
+      }
+      else if (isEndTextObject()) { // not the BT-ET we were looking for
+        //COULD STOP HERE IF assuming only one bt/et in the stream we want
+        setState(0);
+      }
+    }
+
+    @Override
+    public void state2() throws PdfException {
+      if (isEndTextObject()) {  // we need to remove this BT-ET chunk
+        setEnd(getIndex());
+        setResult(true);
+        stop(); // found what we needed, stop processing this page
+      }      
+    }
+
+  }  
 }
