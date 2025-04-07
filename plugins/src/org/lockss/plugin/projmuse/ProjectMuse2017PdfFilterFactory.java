d8953460cd959f48f96abb9b30db5a61a231e03e
Carmen Cox <crc10@stanford.edu>
2025-04-07 14:41:50 -0400

    update ProjMuse2017PdfFilterFactory

--- plugins/src/org/lockss/plugin/projmuse/ProjectMuse2017PdfFilterFactory.java
+++ plugins/src/org/lockss/plugin/projmuse/ProjectMuse2017PdfFilterFactory.java
@@ -32,7 +32,6 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package org.lockss.plugin.projmuse;
 
-import org.apache.commons.collections.IteratorUtils;
 import org.apache.commons.io.IOUtils;
 import org.lockss.filter.pdf.ExtractingPdfFilterFactory;
 import org.lockss.filter.pdf.PdfTransform;
@@ -53,10 +52,14 @@ public class ProjectMuse2017PdfFilterFactory extends ExtractingPdfFilterFactory 
 
   protected static final Pattern WATERMARK_LINE_1 =
       //[ Access provided at 2 Dec 2022 15:10 GMT from Stanford LOCKSS (+1 other institution account) ]
+      // Watermark found in 2025: "Access provided by Stanford Libraries (31 May 2018 09:30 GMT)"
       Pattern.compile("Access provided at .+ from ");
 
   protected static final Pattern WATERMARK_LINE_2 = 
       Pattern.compile("\\[[0-9.]+\\] +Project +MUSE +\\([0-9-]+");
+
+  protected static final Pattern WATERMARK_LINE_3 = 
+      Pattern.compile("Access provided by");
 
   @Override
   public void transform(ArchivalUnit au,
@@ -70,39 +73,16 @@ public class ProjectMuse2017PdfFilterFactory extends ExtractingPdfFilterFactory 
     pdfDocument.unsetProducer();
     PdfUtil.normalizeTrailerId(pdfDocument);
 
-    List<PdfPage> pdfPages = pdfDocument.getPages();
-    Iterator<PdfPage> pageIter = pdfPages.iterator();
-    while (pageIter.hasNext()) {
-      PdfPage pdfPage = pageIter.next();
-      List<PdfTokenStream> allTokenStream = pdfPage.getAllTokenStreams();
-      for (PdfTokenStream pageTokenStream : allTokenStream) {
-
-      List<PdfToken> tokens = pageTokenStream.getTokens();
-      Iterator<PdfToken> tokenIter = tokens.iterator();
-      Iterator<PdfToken> filteredIter = new SubsequenceIterator<PdfToken>(tokenIter) {
-        @Override
-        protected boolean isBegin(PdfToken item) {
-          return item.isOperator() && PdfOpcodes.isBeginTextObject(item);
-        }
-        @Override
-        protected boolean isEnd(PdfToken item) {
-          return item.isOperator() && PdfOpcodes.isEndTextObject(item);
-        }
-        @Override
-        protected void hasAccumulated() {
-          for (PdfToken tok : accumulator) {
-            if (tok.isString()) {
-              String str = tok.getString();
-              if (WATERMARK_LINE_1.matcher(str).find() || WATERMARK_LINE_2.matcher(str).find()) {
-                accumulator.clear();
-                return;
-              }
-            }
-          }
-        }
-      };
-      pageTokenStream.setTokens(IteratorUtils.toList(filteredIter));
-    }}
+    PdfPage pdfPage = pdfDocument.getPage(0);
+    List<PdfTokenStream> allTokenStream = pdfPage.getAllTokenStreams();
+    for (PdfTokenStream pageTokenStream : allTokenStream) {
+      PageWorker pw = new PageWorker();
+      pw.process(pageTokenStream);
+      if(pw.getResult()){
+        pdfDocument.removePage(0);
+        break;
+      }
+    }
   }
 
   @Override
@@ -116,105 +96,55 @@ public class ProjectMuse2017PdfFilterFactory extends ExtractingPdfFilterFactory 
     };
   }
 
-  /**
-   * <p>
-   * An iterator wrapper that consumes its underlying iterator but attempts to
-   * identify subsequences identified as starting and ending by elements
-   * recognized by {@link #isBegin(Object)} and {@link #isEnd(Object)}
-   * respectively.
-   * </p>
-   * <p>
-   * When the underlying iterator returns an element recognized by
-   * {@link #isBegin(Object)}, this iterator continues to consume the underlying
-   * iterator into the list {@link #accumulator} until the end of the underlying
-   * iterator has been reached or until an element is recognized by
-   * {@link #isEnd(Object)}. If an ending element is found,
-   * {@link #hasAccumulated()} is invoked, to give a subclass the opportunity to
-   * alter the sequence of tokens that will be returned by this iterator,
-   * possibly by suppressing the entire subsequence
-   * ({@link Collection#clear()}). If the end of the underlying iterator is
-   * reached before an ending element is found, {@link #hasAccumulated()} is not
-   * invoked. By default, {@link #isBegin(Object)} and {@link #isEnd(Object)}
-   * return false and {@link #hasAccumulated()} does nothing.
-   * </p>
-   */
-  public class SubsequenceIterator<E> implements Iterator<E> {
-
-    protected Iterator<E> iterator;
-
-    protected Deque<E> accumulator;
-
-    public SubsequenceIterator(Iterator<E> iterator) {
-      this.iterator = iterator;
-      this.accumulator = new ArrayDeque<>();
+  public static class PageWorker extends PdfTokenStreamStateMachine {
+     
+    public PageWorker() {
+      super(log);
     }
-
+    
     @Override
-    public boolean hasNext() {
-      if (iterator == null) {
-        return false;
+    public void state0() throws PdfException {
+      if (isBeginTextObject()) {
+        setState(1);
       }
-      findNext();
-      return !accumulator.isEmpty();
     }
-
+    
     @Override
-    public E next() {
-      return accumulator.removeFirst();
-    }
-
-    protected void findNext() {
-      if (!accumulator.isEmpty()) {
-        return;
+    public void state1() throws PdfException {
+      if (isShowTextFind(WATERMARK_LINE_1) ||
+          isShowTextFind(WATERMARK_LINE_2) ||
+          isShowTextFind(WATERMARK_LINE_3) ||
+          isShowTextGlyphPositioningFind(WATERMARK_LINE_1) ||
+          isShowTextGlyphPositioningFind(WATERMARK_LINE_2) ||
+          isShowTextGlyphPositioningFind(WATERMARK_LINE_3)) {
+        setState(2);
       }
-      outer_loop: while (iterator.hasNext()) {
-        E item = iterator.next();
-        accumulator.addLast(item);
-        if (!isBegin(item)) {
-          return;
-        }
-        while (iterator.hasNext()) {
-          item = iterator.next();
-          accumulator.addLast(item);
-          if (isEnd(item)) {
-            hasAccumulated();
-            if (!accumulator.isEmpty()) {
-              return;
-            }
-            continue outer_loop;
-          }
-        }
+      else if (isEndTextObject()) { 
+        setState(0);
       }
-      iterator = null;
-
     }
-
-    protected boolean isBegin(E item) {
-      return false;
+    
+    @Override
+    public void state2() throws PdfException {
+      if (isEndTextObject()) {
+        setResult(true);
+        stop(); 
+      }
     }
-
-    protected boolean isEnd(E item) {
-      return false;
-    }
-
-    protected void hasAccumulated() {
-      // Intentionally left blank
-    }
-
+  
   }
 
-  /*public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
     
     //Reads in a pdf and applies the filters writing the contents to a new file <fileStr>.bin
      
     String[] fileStrs = {
-        "FILE1.pdf",
-        "FILE2.pdf"
+        "/Users/crc10/Desktop/ProjMuse_delta1.pdf"
     };
     for (String fileStr : fileStrs) {
       FilterFactory fact = new ProjectMuse2017PdfFilterFactory();
       IOUtils.copy(fact.createFilteredInputStream(null, new FileInputStream(fileStr), null),
           new FileOutputStream(fileStr + ".bin"));
     }
-  }*/
+  }
 }
