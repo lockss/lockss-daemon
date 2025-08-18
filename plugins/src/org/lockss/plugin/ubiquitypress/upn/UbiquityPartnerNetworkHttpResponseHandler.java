9bb0361cbf71bccd7c9ebea8aef870ab9b0ddef9
Carmen Cox <crc10@stanford.edu>
2025-08-18 14:42:25 -0400

    add upn specific http response handler to upn, update crawl rules

---
+++ plugins/src/org/lockss/plugin/ubiquitypress/upn/UbiquityPartnerNetworkHttpResponseHandler.java
@@ -0,0 +1,91 @@
+/*
+
+Copyright (c) 2000-2025, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package org.lockss.plugin.ubiquitypress.upn;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.util.Logger;
+import org.lockss.util.urlconn.CacheException;
+import org.lockss.util.urlconn.CacheResultHandler;
+import org.lockss.util.urlconn.CacheResultMap;
+
+public class UbiquityPartnerNetworkHttpResponseHandler implements CacheResultHandler {
+    
+  private static final Logger logger = Logger.getLogger(UbiquityPartnerNetworkHttpResponseHandler.class);
+
+  protected static final Pattern ISSUE_PAT = 
+      Pattern.compile("/volume/[0-9]+/issue/[0-9]+");
+
+  @Override
+  public void init(CacheResultMap crmap) {
+    logger.warning("Unexpected call to init()");
+    throw new UnsupportedOperationException("Unexpected call to UbiquityPartnerNetworkHttpResponseHandler.init()");
+   }
+
+  @Override
+  public CacheException handleResult(ArchivalUnit au,
+                                     String url,
+                                     int responseCode) {
+    logger.debug2(url);  
+    Matcher mat = ISSUE_PAT.matcher(url);      
+    switch (responseCode) {
+      case 404: 
+        logger.debug3("found 404");
+        if (mat.find()) {
+          logger.debug3("found issue page");
+          return new CacheException.RetryableNetworkException_5_10S();
+        }
+        return new CacheException.NoRetryDeadLinkException("404 Not Found");
+      case 403: 
+        logger.debug3("found 403");
+        if (url.contains("files/article.xsl")) {
+          logger.debug3("broken xsl file");
+          return new CacheException.NoRetryDeadLinkException("403 Forbidden (non-fatal)");
+        }
+        return new CacheException.RetrySameUrlException("403 Forbidden errror");
+      default:
+        logger.debug2("default");
+        throw new UnsupportedOperationException();
+    }
+  }
+
+  @Override
+  public CacheException handleResult(ArchivalUnit au,
+                                     String url,
+                                     Exception ex) {
+    logger.warning("Unexpected call to handleResult(): AU " + au.getName() + "; URL " + url, ex);
+    throw new UnsupportedOperationException("Unexpected call to handleResult(): AU " + au.getName() + "; URL " + url, ex);
+  }
+}
