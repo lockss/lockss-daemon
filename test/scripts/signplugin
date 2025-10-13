lockss/lockss-daemon

commit 14ae795a76ac17c4cde50e1b7e56c4a52f5b9313

merge ers scholaris plugin

test/scripts/signplugin
@@ -1,33 +1,5 @@
-#!/usr/bin/env python3
-
-# Copyright (c) 2000-2025, Board of Trustees of Leland Stanford Jr. University
+#!/usr/bin/python2
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its contributors
-# may be used to endorse or promote products derived from this software without
-# specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
 # Sign a plugin jar.
 #
 # The syntax of the java 'jarsigner' tool is hard to remember,
@@ -40,28 +12,28 @@
 # attacks, some random bits should be added to plugins received from
 # third parties before we sign them.
 
-from getopt import getopt, GetoptError
+from sys import argv, exit
 from getpass import getpass
-from glob import glob
+from getopt import getopt, GetoptError
 from os import path, environ, popen
-import sys
+from glob import glob
 
 validArgs = ["help", "jar=", "alias=", "keystore=", "password="]
 args = {}
 
 def usage(help):
     """ Print usage information """
-    print("Usage: signplugin [--help] [--jar <jarname>] \\\n \
-         [--alias <alias>] [--keystore <keystore>] \\\n \
-         [--password <password>]")
-    if help:
-        print("""
+    print "Usage: signplugin [--help] [--jar <jarname>] \\\n \
+		 [--alias <alias>] [--keystore <keystore>] \\\n \
+		 [--password <password>]"
+    if (help):
+	print """
  help         Print this help message.
  jar          The name of the JAR file to sign.
  alias        The alias to use when signing the jar.
  keystore     The keystore holding your signing key.
  password     Your signing password.
-""")
+"""
 
 def file_exists(file):
     """ Verify that a given file exists. """
@@ -75,25 +47,25 @@ def gather_input():
     """ Prompt the user for missing arguments """
     global args
 
-    while 'jar' not in args or len(args["jar"]) == 0:
-        args["jar"] = input("JAR file: ")
+    while not args.has_key("jar") or len(args["jar"]) == 0:
+	args["jar"] = raw_input("JAR file: ")
 
     if not file_exists(args["jar"]):
-        print(f"Jar file {args['jar']} does not exist!")
-        sys.exit(1)
+	print "Jar file %s does not exist!" % args["jar"]
+	exit(1)
 
-    while 'alias' not in args or len(args["alias"]) == 0:
-        args["alias"] = input("Signing alias: ")
+    while not args.has_key("alias") or len(args["alias"]) == 0:
+	args["alias"] = raw_input("Signing alias: ")
 
-    while 'keystore' not in args or len(args["keystore"]) == 0:
-        args["keystore"] = input("Signing keystore: ")
+    while not args.has_key("keystore") or len(args["keystore"]) == 0:
+	args["keystore"] = raw_input("Signing keystore: ")
 
     if not file_exists(args["keystore"]):
-        print(f"Keystore {args['keystore']} does not exist!")
-        sys.exit(1)
+	print "Keystore %s does not exist!" % args["keystore"]
+	exit(1)
 
-    while 'password' not in args or len(args["password"]) == 0:
-        args["password"] = getpass("Signing Password: ")
+    while not args.has_key("password") or len(args["password"]) == 0:
+	args["password"] = getpass("Signing Password: ")
 
 #
 # Parse passed-in command-line arguments.
@@ -103,26 +75,26 @@ def parse_options(argv):
     global args
 
     if len(argv) == 0:
-        print("For help, use the --help option.")
+	print "For help, use the --help option."
 
     (opts, vals) = getopt(argv, None, validArgs)
 
     if len(vals) > 0:
-        usage(0)
-        sys.exit(1)
+	usage(0)
+	exit(1)
 
     for (opt, val) in opts:
-        if opt == "--help":
-            usage(1)
-            sys.exit()
-        elif opt == "--jar":
-            args["jar"] = val
-        elif opt == "--alias":
-            args["alias"] = val
-        elif opt == "--password":
-            args["password"] = val
-        elif opt == "--keystore":
-            args["keystore"] = val
+	if opt == "--help":
+	    usage(1)
+	    exit()
+	elif opt == "--jar":
+	    args["jar"] = val
+	elif opt == "--alias":
+	    args["alias"] = val
+	elif opt == "--password":
+	    args["password"] = val
+	elif opt == "--keystore":
+	    args["keystore"] = val
 
 #
 # Sign the plugin
@@ -132,49 +104,49 @@ def sign_plugin():
     global jarsigner
     global args
 
-    print(f"Signing plugin {args['jar']}...")
+    print ("Signing plugin %s..." % args["jar"])
 
     commandArgs = " -keystore \'" + args["keystore"] + "\'" + \
-          " -keypass \'" + args["password"] + "\'" + \
-          " -storepass \'" + args["password"] + "\'" + \
-          " " + args["jar"] + \
-          " " + args["alias"]
+		  " -keypass \'" + args["password"] + "\'" + \
+		  " -storepass \'" + args["password"] + "\'" + \
+		  " " + args["jar"] + \
+		  " " + args["alias"]
 
     child = popen(jarsigner + commandArgs)
     output = child.read()
     err = child.close()
     if err:
-        print("Signing failed.")
-        print(output)
-        sys.exit(1)
+        print "Signing failed."
+        print output
+        exit(1)
     else:
-        print(f"Plugin jar {args['jar']} signed successfully.")
+        print "Plugin jar %s signed successfully." % args["jar"]
 
 def main():
     """ Main entry point """
     global jarsigner
 
     # Make sure 'JAVA_HOME' is set.
-    if 'JAVA_HOME' not in environ:
-        print("Can't continue. 'JAVA_HOME' must be defined in your environment.")
-        sys.exit(1)
+    if not environ.has_key('JAVA_HOME'):
+        print "Can't continue. 'JAVA_HOME' must be defined in your environment."
+        exit(1)
 
     # Location of the 'jarsigner' command.
     jarsigner = path.join(environ.get('JAVA_HOME'), 'bin', 'jarsigner')
 
     # Parse command line options
     try:
-        parse_options(sys.argv[1:])
+        parse_options(argv[1:])
     except GetoptError:
         usage(0)
-        sys.exit(1)
+        exit(1)
 
     # Get missing input
     try:
         gather_input()
     except SyntaxError:
-        print("Problem while parsing input")
-        sys.exit(1)
+        print "Problem while parsing input"
+        exit(1)
 
     # Actually generate the plugin and sign it.
     sign_plugin()
@@ -186,5 +158,5 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print()
-        sys.exit(0)
+        print
+        exit(0)

plugins/src/org/lockss/plugin/highwire/ers/ClockssERSScolarisPlugin.xml
@@ -1,0 +1,75 @@
+<!--
+
+Copyright (c) 2000-2025, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+-->
+<map>
+  <entry>
+    <string>plugin_status</string>
+    <string>development</string>
+  </entry>
+  <entry>
+    <string>plugin_identifier</string>
+    <string>org.lockss.plugin.highwire.ers.ClockssERSScolarisPlugin</string>
+  </entry>
+  <entry>
+    <string>plugin_name</string>
+    <string>European Respiratory Society Scolaris Plugin (CLOCKSS)</string>
+  </entry>
+  <entry>
+    <string>plugin_version</string>
+    <string>1</string>
+  </entry>
+  <entry>
+    <string>plugin_parent</string>
+    <string>org.lockss.plugin.highwire.ers.ERSScolarisPlugin</string>
+  </entry>
+  <entry>
+    <string>plugin_parent_version</string>
+    <string>1</string>
+  </entry>
+  <entry>
+    <string>au_name</string>
+    <string>"European Respiratory Society Scolaris Plugin (CLOCKSS), Base URL %s, Journal %s, Volume %s", base_url, journal_id, volume_name</string>
+  </entry>
+  <entry>
+      <string>au_start_url</string>
+      <!-- content/sgrehpp/lockss-manifest/vol_10_manifest.html -->
+      <string>"%scontent/%s/clockss-manifest/vol_%s_manifest.html", base_url, journal_id, volume_name</string>
+  </entry>
+  <entry>
+    <string>clockss_override</string>
+    <map>
+      <entry>
+        <string>au_def_pause_time</string>
+        <long>100</long>
+      </entry>
+    </map>
+  </entry>
+</map>

plugins/src/org/lockss/plugin/highwire/ers/ERSScolarisCrawlSeedFactory.java
@@ -1,0 +1,101 @@
+/*
+
+Copyright (c) 2000-2025, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package org.lockss.plugin.highwire.ers;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.apache.commons.httpclient.CircularRedirectException;
+
+import org.lockss.crawler.BaseCrawlSeed;
+import org.lockss.crawler.CrawlSeed;
+import org.lockss.crawler.CrawlSeedFactory;
+import org.lockss.daemon.Crawler.CrawlerFacade;
+import org.lockss.daemon.PluginException;
+import org.lockss.plugin.ArchivalUnit.ConfigurationException;
+import org.lockss.plugin.UrlFetcher;
+import org.lockss.plugin.UrlFetcher.FetchResult;
+import org.lockss.util.Logger;
+import org.lockss.util.urlconn.CacheException;
+
+public class ERSScolarisCrawlSeedFactory implements CrawlSeedFactory{
+    private static final Logger log = Logger.getLogger(ERSScolarisCrawlSeedFactory.class);
+    @Override
+    public CrawlSeed createCrawlSeed(CrawlerFacade crawlFacade) {
+        return new ERSScolarisCrawlSeed(crawlFacade);
+    }
+
+    public static class ERSScolarisCrawlSeed extends BaseCrawlSeed {
+    
+    protected CrawlerFacade cf;
+
+    public ERSScolarisCrawlSeed(CrawlerFacade cf) {
+        super(cf);
+        this.cf = cf;
+    };
+
+    @Override
+    protected void initialize() throws ConfigurationException, IOException, PluginException{
+        super.initialize();
+        Collection<String> startUrls = au.getStartUrls();
+        String startUrl = startUrls.iterator().next();
+        log.debug2("Prefetching permission URL: " + startUrl);
+        UrlFetcher uf = cf.makeUrlFetcher(startUrl);
+        uf.setRedirectScheme(UrlFetcher.REDIRECT_SCHEME_FOLLOW);
+        FetchResult fr;
+        try {
+          fr = uf.fetch();
+          log.critical("Uh oh! " + fr);
+        }
+        catch (CacheException.UnknownExceptionException uee) {
+          log.critical("UEE", uee);
+          log.critical("UEE CAUSE", uee.getCause());
+          Throwable cause = uee.getCause();
+          if (cause == null || !(cause instanceof CircularRedirectException)) {
+            log.debug3("Rethrowing an UnknownExceptionException", uee);
+            throw uee;
+          }
+          // FIXME: add a check that it ended up in the right place, but alas, the error message has port 443 in it: Circular redirect to 'https://publications.ersnet.org:443/content/erjor/lockss-manifest/vol_10_manifest.html'
+          log.debug2("Prefetching permission URL succeeded: " + startUrl);
+        }
+        catch (CacheException ce) {
+          log.debug("Prefetching permission URL failed: " + startUrl, ce);
+          throw ce;
+        }
+
+    }
+
+}
+
+}
+

plugins/src/org/lockss/plugin/highwire/ers/ERSScolarisPlugin.xml
@@ -1,0 +1,83 @@
+<!--
+
+Copyright (c) 2000-2025, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+-->
+<map>
+    <entry>
+        <string>plugin_status</string>
+        <string>development</string>
+    </entry>
+      <!-- NOTE: this plugin uses shared code (validation, response handler) from parent directory by using plugin_aux_packages -->
+    <entry>
+        <string>plugin_identifier</string>
+        <string>org.lockss.plugin.highwire.ers.ERSScolarisPlugin</string>
+    </entry>
+    <entry>
+        <string>plugin_name</string>
+        <string>European Respiratory Society Scolaris Plugin</string>
+    </entry>
+    <entry>
+        <string>plugin_version</string>
+        <string>1</string>
+    </entry>
+    <entry>
+        <string>plugin_parent</string>
+        <string>org.lockss.plugin.highwire.scolaris.HighWireScolarisPlugin</string>
+    </entry>
+    <entry>
+        <string>plugin_parent_version</string>
+        <string>2</string>
+    </entry>
+    <entry>
+        <string>au_name</string>
+        <string>"European Respiratory Society Scolaris Plugin, Base URL %s, Journal %s, Volume %s", base_url, journal_id, volume_name</string>
+    </entry>
+    <entry>
+    <string>plugin_aux_packages</string>
+      <list>
+          <string>org.lockss.plugin.highwire</string>
+      </list>
+    </entry>
+    <!--<entry>
+        <string>plugin_url_consumer_factory</string>
+        <string>org.lockss.plugin.highwire.ers.ERSScolarisUrlConsumerFactory</string>
+    </entry>-->
+  <entry>
+    <string>plugin_crawl_seed_factory</string>
+    <string>org.lockss.plugin.highwire.ers.ERSScolarisCrawlSeedFactory</string>
+  </entry>
+  <entry>
+    <string>plugin_cache_result_list</string>
+    <list>
+      <!-- Needed by the crawl seed's initialize() stratagem -->
+      <string>org.apache.commons.httpclient.CircularRedirectException=org.lockss.util.urlconn.CacheException$UnknownExceptionException</string>
+    </list>
+  </entry>
+</map>

commit 1333b5d3fe37bc7e8b261e9c35f81b6cfeee2ab2

For some reason, the merge in 14ae795a76ac17c4cde50e1b7e56c4a52f5b9313 caused test/scripts/signplugin (but no other unintended file) to revert to an earlier state; fixing.

test/scripts/signplugin
@@ -1,5 +1,33 @@
-#!/usr/bin/python2
+#!/usr/bin/env python3
+
+# Copyright (c) 2000-2025, Board of Trustees of Leland Stanford Jr. University
 #
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 # Sign a plugin jar.
 #
 # The syntax of the java 'jarsigner' tool is hard to remember,
@@ -12,28 +40,28 @@
 # attacks, some random bits should be added to plugins received from
 # third parties before we sign them.
 
-from sys import argv, exit
-from getpass import getpass
 from getopt import getopt, GetoptError
-from os import path, environ, popen
+from getpass import getpass
 from glob import glob
+from os import path, environ, popen
+import sys
 
 validArgs = ["help", "jar=", "alias=", "keystore=", "password="]
 args = {}
 
 def usage(help):
     """ Print usage information """
-    print "Usage: signplugin [--help] [--jar <jarname>] \\\n \
-		 [--alias <alias>] [--keystore <keystore>] \\\n \
-		 [--password <password>]"
-    if (help):
-	print """
+    print("Usage: signplugin [--help] [--jar <jarname>] \\\n \
+         [--alias <alias>] [--keystore <keystore>] \\\n \
+         [--password <password>]")
+    if help:
+        print("""
  help         Print this help message.
  jar          The name of the JAR file to sign.
  alias        The alias to use when signing the jar.
  keystore     The keystore holding your signing key.
  password     Your signing password.
-"""
+""")
 
 def file_exists(file):
     """ Verify that a given file exists. """
@@ -47,25 +75,25 @@ def gather_input():
     """ Prompt the user for missing arguments """
     global args
 
-    while not args.has_key("jar") or len(args["jar"]) == 0:
-	args["jar"] = raw_input("JAR file: ")
+    while 'jar' not in args or len(args["jar"]) == 0:
+        args["jar"] = input("JAR file: ")
 
     if not file_exists(args["jar"]):
-	print "Jar file %s does not exist!" % args["jar"]
-	exit(1)
+        print(f"Jar file {args['jar']} does not exist!")
+        sys.exit(1)
 
-    while not args.has_key("alias") or len(args["alias"]) == 0:
-	args["alias"] = raw_input("Signing alias: ")
+    while 'alias' not in args or len(args["alias"]) == 0:
+        args["alias"] = input("Signing alias: ")
 
-    while not args.has_key("keystore") or len(args["keystore"]) == 0:
-	args["keystore"] = raw_input("Signing keystore: ")
+    while 'keystore' not in args or len(args["keystore"]) == 0:
+        args["keystore"] = input("Signing keystore: ")
 
     if not file_exists(args["keystore"]):
-	print "Keystore %s does not exist!" % args["keystore"]
-	exit(1)
+        print(f"Keystore {args['keystore']} does not exist!")
+        sys.exit(1)
 
-    while not args.has_key("password") or len(args["password"]) == 0:
-	args["password"] = getpass("Signing Password: ")
+    while 'password' not in args or len(args["password"]) == 0:
+        args["password"] = getpass("Signing Password: ")
 
 #
 # Parse passed-in command-line arguments.
@@ -75,26 +103,26 @@ def parse_options(argv):
     global args
 
     if len(argv) == 0:
-	print "For help, use the --help option."
+        print("For help, use the --help option.")
 
     (opts, vals) = getopt(argv, None, validArgs)
 
     if len(vals) > 0:
-	usage(0)
-	exit(1)
+        usage(0)
+        sys.exit(1)
 
     for (opt, val) in opts:
-	if opt == "--help":
-	    usage(1)
-	    exit()
-	elif opt == "--jar":
-	    args["jar"] = val
-	elif opt == "--alias":
-	    args["alias"] = val
-	elif opt == "--password":
-	    args["password"] = val
-	elif opt == "--keystore":
-	    args["keystore"] = val
+        if opt == "--help":
+            usage(1)
+            sys.exit()
+        elif opt == "--jar":
+            args["jar"] = val
+        elif opt == "--alias":
+            args["alias"] = val
+        elif opt == "--password":
+            args["password"] = val
+        elif opt == "--keystore":
+            args["keystore"] = val
 
 #
 # Sign the plugin
@@ -104,49 +132,49 @@ def sign_plugin():
     global jarsigner
     global args
 
-    print ("Signing plugin %s..." % args["jar"])
+    print(f"Signing plugin {args['jar']}...")
 
     commandArgs = " -keystore \'" + args["keystore"] + "\'" + \
-		  " -keypass \'" + args["password"] + "\'" + \
-		  " -storepass \'" + args["password"] + "\'" + \
-		  " " + args["jar"] + \
-		  " " + args["alias"]
+          " -keypass \'" + args["password"] + "\'" + \
+          " -storepass \'" + args["password"] + "\'" + \
+          " " + args["jar"] + \
+          " " + args["alias"]
 
     child = popen(jarsigner + commandArgs)
     output = child.read()
     err = child.close()
     if err:
-        print "Signing failed."
-        print output
-        exit(1)
+        print("Signing failed.")
+        print(output)
+        sys.exit(1)
     else:
-        print "Plugin jar %s signed successfully." % args["jar"]
+        print(f"Plugin jar {args['jar']} signed successfully.")
 
 def main():
     """ Main entry point """
     global jarsigner
 
     # Make sure 'JAVA_HOME' is set.
-    if not environ.has_key('JAVA_HOME'):
-        print "Can't continue. 'JAVA_HOME' must be defined in your environment."
-        exit(1)
+    if 'JAVA_HOME' not in environ:
+        print("Can't continue. 'JAVA_HOME' must be defined in your environment.")
+        sys.exit(1)
 
     # Location of the 'jarsigner' command.
     jarsigner = path.join(environ.get('JAVA_HOME'), 'bin', 'jarsigner')
 
     # Parse command line options
     try:
-        parse_options(argv[1:])
+        parse_options(sys.argv[1:])
     except GetoptError:
         usage(0)
-        exit(1)
+        sys.exit(1)
 
     # Get missing input
     try:
         gather_input()
     except SyntaxError:
-        print "Problem while parsing input"
-        exit(1)
+        print("Problem while parsing input")
+        sys.exit(1)
 
     # Actually generate the plugin and sign it.
     sign_plugin()
@@ -158,5 +186,5 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print
-        exit(0)
+        print()
+        sys.exit(0)