380da2ebbae48725d2ef0be3b3fcf3a389cdb978
Tom Lipkis <tal@pss.com>
2025-10-14 19:06:15 -0700

    Use long timeout client for createArtifact(), which can wait arbitrarily long for a response

--- src/org/lockss/laaws/Worker.java
+++ src/org/lockss/laaws/Worker.java
@@ -15,6 +15,7 @@ public abstract class Worker {
   protected Counters ctrs;
 
   protected final StreamingArtifactsApi artifactsApi;
+  protected final StreamingArtifactsApi artifactsApiLongCall;
   protected final AusApi cfgAusApiClient;
   protected final UsersApi cfgUsersApiClient;
   protected final ConfigApi cfgConfigApiClient;
@@ -29,6 +30,7 @@ public abstract class Worker {
     }
     this.ctrs = task.getCounters();
     artifactsApi = auMover.getRepoArtifactsApiClient();
+    artifactsApiLongCall = auMover.getRepoArtifactsApiLongCallClient();
     cfgAusApiClient = auMover.getCfgAusApiClient();
     cfgUsersApiClient = auMover.getCfgUsersApiClient();
     cfgConfigApiClient = auMover.getCfgConfigApiClient();
