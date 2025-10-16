b3ec20d0975229ce81e76755202f44954c934a68
Thib Guicherd-Callin <thib@cs.stanford.edu>
2025-10-16 16:42:06 -0700

    Development on the ERS Scolaris plugin (and parent Scolaris plugin), including crawl filter to avoid constantly following auth-related excursions, crawl rules for tabbed components (needs more work), crawl rules for citation files.

---
+++ plugins/src/org/lockss/plugin/highwire/scolaris/HighWireScolarisHtmlCrawlFilterFactory.java
@@ -0,0 +1,59 @@
+/*
+
+Copyright (c) 2000-2025, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package org.lockss.plugin.highwire.scolaris;
+
+import java.io.InputStream;
+
+import org.htmlparser.NodeFilter;
+import org.lockss.daemon.PluginException;
+import org.lockss.filter.html.*;
+import org.lockss.plugin.*;
+import org.lockss.plugin.highwire.HighWireJCoreHtmlCrawlFilterFactory;
+
+public class HighWireScolarisHtmlCrawlFilterFactory extends HighWireJCoreHtmlCrawlFilterFactory {
+  
+  protected static NodeFilter[] filters = new NodeFilter[] {
+    // Links to /openid-connect/ URLs which are allowed by the crawl rules for the permission URL auth excursion but shouldn't be routinely followed
+    HtmlNodeFilters.tagWithAttributeRegex("div", "class", "login--"),
+    HtmlNodeFilters.tagWithAttributeRegex("a", "href", "/openid-connect/"),
+  };
+  
+  @Override
+  public InputStream createFilteredInputStream(ArchivalUnit au,
+                                               InputStream in,
+                                               String encoding)
+      throws PluginException {
+    return super.createFilteredInputStream(au, in, encoding, filters);
+  }
+
+}
