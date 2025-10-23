5d3be52c73cb992974a1b79b5e357835b6721759
Carmen Cox <crc10@stanford.edu>
2025-10-23 16:05:46 -0400

    add to Voprosy metadata extractor

--- plugins/src/org/lockss/plugin/clockss/vhht/VoprosyKhimiiXmlMetadataExtractorFactory.java
+++ plugins/src/org/lockss/plugin/clockss/vhht/VoprosyKhimiiXmlMetadataExtractorFactory.java
@@ -39,6 +39,7 @@ import org.lockss.daemon.PluginException;
 import org.lockss.extractor.ArticleMetadata;
 import org.lockss.extractor.FileMetadataExtractor;
 import org.lockss.extractor.MetadataTarget;
+import org.lockss.extractor.MetadataField;
 import org.lockss.plugin.CachedUrl;
 import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
 import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
@@ -47,7 +48,7 @@ import org.lockss.util.Logger;
 public class VoprosyKhimiiXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
         private static final Logger log = Logger.getLogger(VoprosyKhimiiXmlMetadataExtractorFactory.class);
 
-    private static SourceXmlSchemaHelper VoprosyKhimiiSchemaHelper = null;
+    private static SourceXmlSchemaHelper schemaHelper = null;
 
     @Override
     public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
@@ -61,11 +62,11 @@ public class VoprosyKhimiiXmlMetadataExtractorFactory extends SourceXmlMetadataE
         @Override
         protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu) {
         // Once you have it, just keep returning the same one. It won't change.
-            if (VoprosyKhimiiSchemaHelper == null) {
-                VoprosyKhimiiSchemaHelper = new VoprosyKhimiiSchemaHelper();
+            if (schemaHelper == null) {
+                schemaHelper = new VoprosyKhimiiSchemaHelper();
                 log.debug3("setting up schema helper");
             }
-            return VoprosyKhimiiSchemaHelper;
+            return schemaHelper;
         }
 
         /*
@@ -81,6 +82,13 @@ public class VoprosyKhimiiXmlMetadataExtractorFactory extends SourceXmlMetadataE
             List<String> returnList = new ArrayList<String>();
             returnList.add(pdfName);
             return returnList;
+        }
+
+        @Override
+        protected void postCookProcess(SourceXmlSchemaHelper schemaHelper, CachedUrl cu, ArticleMetadata thisAM) {
+            //2025 - plugin developers started hardcoding article and publication type 
+            thisAM.put(MetadataField.FIELD_ARTICLE_TYPE, MetadataField.ARTICLE_TYPE_JOURNALARTICLE);
+            thisAM.put(MetadataField.FIELD_PUBLICATION_TYPE, MetadataField.PUBLICATION_TYPE_JOURNAL);
         }
     }  
 }
