1b1a58139dd5b4585eb0ff89ade049be8b415861
Carmen Cox <crc10@stanford.edu>
2026-03-03 16:35:04 -0500

    first draft of ESCRS crawl seed

---
+++ plugins/src/org/lockss/plugin/escrs/EscrsCrawlSeedFactory.java
@@ -0,0 +1,122 @@
+/*
+
+Copyright (c) 2000-2026, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package org.lockss.plugin.escrs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import org.lockss.crawler.BaseCrawlSeed;
+import org.lockss.crawler.CrawlSeed;
+import org.lockss.crawler.CrawlSeedFactory;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.daemon.Crawler.CrawlerFacade;
+import org.lockss.daemon.PluginException;
+import org.lockss.plugin.ArchivalUnit.ConfigurationException;
+import org.lockss.util.Logger;
+
+public class EscrsCrawlSeedFactory implements CrawlSeedFactory {
+    private static final Logger log = Logger.getLogger(EscrsCrawlSeedFactory.class);
+
+    @Override
+    public CrawlSeed createCrawlSeed(CrawlerFacade facade) {
+        return new EscrsCrawlSeed(facade);
+    }
+
+    public static class EscrsCrawlSeed extends BaseCrawlSeed {
+        
+        private CrawlerFacade cf;
+        public EscrsCrawlSeed(CrawlerFacade facade) {
+            super(facade);
+            cf = facade;
+        }
+        
+        @Override
+        public Collection<String> doGetStartUrls() throws ConfigurationException, PluginException, IOException {
+
+            Collection<String> sUrls = super.doGetStartUrls();
+
+            ArchivalUnit au = cf.getAu();
+            
+            for (Iterator<String> iter = sUrls.iterator(); iter.hasNext();) {
+                String sUrl = iter.next();
+                CachedUrl cu = au.makeCachedUrl(sUrl);
+                InputStream in = cu.getUnfilteredInputStream();
+
+                if (in != null) {
+                    try {
+                        String year = null;
+
+                        Elements small_element_year;
+                        String url = cu.getUrl();
+                        try {
+                            Document doc = Jsoup.parse(in, cu.getEncoding(), url); 
+                            small_element_year = doc.select("p[class=\"archived-issue__date\"]>small");
+                            year = checkElement(small_element_year);
+                            log.info("YEAR IS " + small_element_year);
+                        } catch (IOException e) {
+                            log.debug3("Escrs: Error getting correct AU manifest", e);
+                        }
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+            return super.doGetStartUrls();
+        }
+
+        protected String checkElement(Elements element) {
+            String cleanedUpElement = null;
+            if ( element != null){
+                int comma = element.indexOf(",") > -1 ? element.indexOf(",") : 0;
+                cleanedUpElement = element.text().trim().substring(comma);
+                log.debug3("Escrs: Element is " + element);
+                if (cleanedUpElement != null) {
+                    log.debug3("Escrs: Element cleaned is " + cleanedUpElement);
+                } else {
+                    log.debug3("Escrs: Element is null");
+                }
+            }
+            return cleanedUpElement;
+        }
+    }
+}
