88f4fbb51d0cf292b8ec20664168192d621e21d1
Carmen Cox <crc10@stanford.edu>
2026-03-06 15:30:46 -0500

    Lusail University Source Plugin

---
+++ plugins/src/org/lockss/plugin/clockss/lup/LusailUniversityPressXmlMetadataExtractorFactory.java
@@ -0,0 +1,112 @@
+/*
+
+Copyright (c) 2000-2026, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+package org.lockss.plugin.clockss.lup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FilenameUtils;
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.ArticleMetadata;
+import org.lockss.extractor.FileMetadataExtractor;
+import org.lockss.extractor.MetadataField;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.CachedUrl;
+import org.lockss.plugin.clockss.SourceXmlMetadataExtractorFactory;
+import org.lockss.plugin.clockss.SourceXmlSchemaHelper;
+import org.lockss.plugin.clockss.CrossRefSchemaHelper;
+import org.lockss.util.Logger;
+
+public class LusailUniversityPressXmlMetadataExtractorFactory extends SourceXmlMetadataExtractorFactory {
+    private static final Logger log = Logger.getLogger(LusailUniversityPressXmlMetadataExtractorFactory.class);
+
+    private static final String LUP_PUBLISHER = "Lusail University Press";
+
+    private static SourceXmlSchemaHelper schemaHelper = null;
+
+    @Override
+    public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
+        String contentType)
+            throws PluginException {
+      return new LusailUniversityPressXmlMetadataExtractor();
+    }
+    
+    public class LusailUniversityPressXmlMetadataExtractor extends SourceXmlMetadataExtractor {
+
+        @Override
+        protected SourceXmlSchemaHelper setUpSchema(CachedUrl cu) {
+        // Once you have it, just keep returning the same one. It won't change.
+            if (schemaHelper == null) {
+                schemaHelper = new CrossRefSchemaHelper();
+                log.debug3("setting up schema helper");
+            }
+            return schemaHelper;
+        }
+
+        /*
+         * There is a one-to-many relationship between the xml and pdf.
+         */
+        @Override
+        protected List<String> getFilenamesAssociatedWithRecord(SourceXmlSchemaHelper helper, CachedUrl cu,
+        ArticleMetadata oneAM) {
+            String cuBase = cu.getUrl();
+            //xml and pdfs are in same directory, remove xml file name in cuBase
+            int lastDirectory = cuBase.lastIndexOf("/");
+            log.debug3("CU Base is: " + cuBase);
+            String id = oneAM.getRaw(helper.getFilenameXPathKey());
+            int slashChar = id.indexOf("/");
+            String pdfName = cuBase.substring(0, lastDirectory) + "/Full-text%20doi.org" + 
+                id.substring(0,slashChar) + id.substring(slashChar+1).toLowerCase() + ".pdf";
+            log.debug3("The pdf is: " + pdfName);
+            List<String> returnList = new ArrayList<String>();
+            returnList.add(pdfName);
+            return returnList;
+        }
+
+        @Override
+        protected void postCookProcess(SourceXmlSchemaHelper schemaHelper, 
+            CachedUrl cu, ArticleMetadata thisAM) {
+            log.debug("In Lusail University Press postcook");
+            // In the Lusail University Press metadata, the registrant is incorrectly set to WEB-FORM
+            String publisher = thisAM.get(MetadataField.FIELD_PUBLISHER);
+            String provider = thisAM.get(MetadataField.FIELD_PROVIDER);
+            if ("WEB-FORM".equals(publisher) || publisher == null) {
+                thisAM.replace(MetadataField.FIELD_PUBLISHER,LUP_PUBLISHER);
+            }
+            if ("WEB-FORM".equals(provider) || provider == null) {
+                thisAM.replace(MetadataField.FIELD_PUBLISHER,LUP_PUBLISHER);
+            }
+            thisAM.put(MetadataField.FIELD_PUBLICATION_TYPE,MetadataField.PUBLICATION_TYPE_JOURNAL);
+  	        thisAM.put(MetadataField.FIELD_ARTICLE_TYPE,MetadataField.ARTICLE_TYPE_JOURNALARTICLE);
+        }
+    }
+}
