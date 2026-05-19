e1857fd31f380e1f32da224bac1a8ce128bb2566
Carmen Cox <crc10@stanford.edu>
2026-05-19 16:44:10 -0400

    initial commit of Baycinar Medical Publishing Journals Plugin

---
+++ plugins/src/org/lockss/plugin/bmp/BMPArticleIteratorFactory.java
@@ -0,0 +1,97 @@
+/*
+
+Copyright (c) 2000-2026, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package org.lockss.plugin.bmp;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.apache.commons.collections.MultiMap;
+import org.apache.commons.collections.map.MultiValueMap;
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.ArticleMetadataExtractor;
+import org.lockss.extractor.ArticleMetadataExtractorFactory;
+import org.lockss.extractor.BaseArticleMetadataExtractor;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.filter.html.HtmlTags.Article;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.plugin.ArticleFiles;
+import org.lockss.plugin.ArticleIteratorFactory;
+import org.lockss.plugin.SubTreeArticleIteratorBuilder;
+import org.lockss.util.Logger;
+
+public class BMPArticleIteratorFactory implements ArticleIteratorFactory, ArticleMetadataExtractorFactory{
+    
+    protected static Logger log = Logger.getLogger(BMPArticleIteratorFactory.class);
+
+    private static final String ROOT_TEMPLATE = "\"%s\", base_url";
+    private static final String PATTERN_TEMPLATE = "\"%s\", base_url";
+
+    protected static final Pattern PDF_PATTERN = Pattern.compile("/content/pdf/([\\d]+-[\\dXx]+)-([\\d]+)-([\\dS]+)(-([\\dA-Z]+))?\\.pdf$", Pattern.CASE_INSENSITIVE);
+    protected static final Pattern ABSTRACT_PATTERN = Pattern.compile("/content/pdf/([\\d]+-[\\dXx]+)-([\\d]+)-([\\dS]+)(-([\\dA-Z]+))?\\.pdf$", Pattern.CASE_INSENSITIVE);
+
+    private static final List<String> PDF_REPLACEMENTS = new ArrayList<String>();
+    private static final List<String> ABSTRACTS_REPLACEMENTS = new ArrayList<String>();
+
+    @Override
+    public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au, MetadataTarget target) throws PluginException {
+        SubTreeArticleIteratorBuilder builder = new SubTreeArticleIteratorBuilder(au);
+
+        builder.setSpec(target,
+        ROOT_TEMPLATE,
+        PATTERN_TEMPLATE, Pattern.CASE_INSENSITIVE);
+
+        builder.addAspect(PDF_PATTERN,
+        PDF_REPLACEMENTS,
+        ArticleFiles.ROLE_FULL_TEXT_PDF);
+
+        builder.addAspect(ABSTRACT_PATTERN,
+        ABSTRACTS_REPLACEMENTS,
+        ArticleFiles.ROLE_ABSTRACT);
+
+        builder.setRoleFromOtherRoles(ArticleFiles.ROLE_ARTICLE_METADATA, ArticleFiles.ROLE_ABSTRACT, ArticleFiles.ROLE_FULL_TEXT_HTML);
+        builder.setFullTextFromRoles(ArticleFiles.ROLE_FULL_TEXT_HTML, ArticleFiles.ROLE_FULL_TEXT_PDF, ArticleFiles.ROLE_ABSTRACT);
+
+        return builder.getSubTreeArticleIterator();
+    }
+
+
+    @Override
+    public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target)
+        throws PluginException {
+        return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
+    }
+}
