ea746cae8b9252a72b2043c0a8dc2c94770ebb91
M.E. Petrich <maryellen@lockss.org>
2026-07-21 15:05:02 -0700

    AU is frozen on all machines

--- scripts/tdb/read_auid_new.pl
+++ scripts/tdb/read_auid_new.pl
@@ -1614,7 +1614,8 @@ while (my $line = <>) {
       #printf("URL: %s\n", $perm_url); #debug
       $vol_title = $perm_url ;
     #start_url for all OAI queries https://www.comicsgrid.com/api/oai/?verb=ListRecords&metadataPrefix=oai_dc&from=2019-01-01&until=2019-12-31
-    $url = sprintf("%sapi/oai/?verb=ListRecords&amp;metadataPrefix=oai_dc&amp;from=%d-01-01&amp;until=%d-12-31",
+    #$url = sprintf("%sapi/oai/?verb=ListRecords&amp;metadataPrefix=oai_dc&amp;from=%d-01-01&amp;until=%d-12-31", #<- not well formed, returns all articles
+    $url = sprintf("%sapi/oai/?verb=ListRecords&metadataPrefix=oai_dc&from=%d-01-01&until=%d-12-31", #better formed, returns articles from this year
       $param{base_url}, $param{year}, $param{year});
     $man_url = uri_unescape($url);
     my $req_p = HTTP::Request->new(GET, $perm_url);
