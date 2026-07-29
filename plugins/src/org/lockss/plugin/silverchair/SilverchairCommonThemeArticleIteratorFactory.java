d0ed76bdf0b17a4e5d5eb1e75bcabc8282712cf7
feili18 <feili18@stanford.edu>
2026-07-29 13:33:57 -0700

    Change article iterator pattern

--- plugins/src/org/lockss/plugin/silverchair/SilverchairCommonThemeArticleIteratorFactory.java
+++ plugins/src/org/lockss/plugin/silverchair/SilverchairCommonThemeArticleIteratorFactory.java
@@ -50,6 +50,8 @@ public class SilverchairCommonThemeArticleIteratorFactory implements ArticleIter
     //https://pubs.geoscienceworld.org/gsa/gsabulletin/article/135/1-2/104/613176/U-Th-He-chronology-Part-1-Data-uncertainty-and
     //https://pubs.geoscienceworld.org/gsa/gsabulletin/article/135/1-2/137/613175/U-Th-He-chronology-Part-2-Considerations-for
     //https://pubs.geoscienceworld.org/gsa/gsabulletin/article/135/1-2/162/613496/Geological-history-and-supercontinent-cycles-of
+    //https://journals.biologists.com/jcs/article/s2-58/231/411/62765/Stolon-Formation-in-Certain-Species-of
+    //https://journals.biologists.com/jcs/article/s2-58/231/447/62773/The-Effects-of-Hypertonic-Solutions-upon-the
 
     /*
     Silverchair confirmed in Jira ticket - SCS-77294, that we can use "article" only, since article-abstract
@@ -64,7 +66,7 @@ public class SilverchairCommonThemeArticleIteratorFactory implements ArticleIter
     // vol/issue/page/id must all be numeric, and slug must be the final segment
     // since we got doi as relative url as in <div class="comment">doi: <a class="link" href="10.1666/13-053" target="_blank">10.1666/13-053</a>.</div>
     private static Pattern HTML_PATTERN = Pattern.compile(
-            "/article/(\\d+/[\\d-]+/[^/]+/\\d+)/([A-Z][A-Z0-9-]*)$",
+            "/article/([\\w-]+/[\\w-]+/[^/]+/\\d+)/([A-Z][A-Z0-9-]*)$",
             Pattern.CASE_INSENSITIVE);
 
     private static String HTML_REPLACEMENT = "/article/$1/$2";
