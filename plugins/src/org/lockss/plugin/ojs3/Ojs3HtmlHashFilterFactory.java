ff49e729aa3cdb98a21bc6a133eb06e7fe860c9f
Carmen Cox <crc10@stanford.edu>
2026-09-01 14:44:04 -0400

    add to ojs3 hash filter

--- plugins/src/org/lockss/plugin/ojs3/Ojs3HtmlHashFilterFactory.java
+++ plugins/src/org/lockss/plugin/ojs3/Ojs3HtmlHashFilterFactory.java
@@ -290,6 +290,7 @@ public class Ojs3HtmlHashFilterFactory implements FilterFactory {
       HtmlNodeFilters.tagWithAttributeRegex("span", "class", "summary_stat"),
       HtmlNodeFilters.tagWithAttributeRegex("input",  "name", "csrfToken"),
       HtmlNodeFilters.tagWithAttributeRegex("div",  "class", "metric-value"),
+      HtmlNodeFilters.tagWithAttributeRegex("div",  "class", "stat_counter"),
       /*
       want to remove this whole bit from this url https://revistas.udea.edu.co/index.php/lecturasdeeconomia/article/view/342002
       <div style="padding-left: 4%;">
