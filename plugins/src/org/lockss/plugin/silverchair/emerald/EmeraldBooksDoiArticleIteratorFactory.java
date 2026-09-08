0c826d18f58e3711fc02e1334a3398a7efecf6a8
feili18 <feili18@stanford.edu>
2026-09-08 08:42:24 -0700

    Add code to handle doi based Emerald book path

---
+++ plugins/src/org/lockss/plugin/silverchair/emerald/EmeraldBooksDoiArticleIteratorFactory.java
@@ -0,0 +1,96 @@
+package org.lockss.plugin.silverchair.emerald;
+
+/*
+Copyright (c) 2000-2026, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+import org.lockss.daemon.PluginException;
+import org.lockss.extractor.ArticleMetadataExtractor;
+import org.lockss.extractor.ArticleMetadataExtractorFactory;
+import org.lockss.extractor.BaseArticleMetadataExtractor;
+import org.lockss.extractor.MetadataTarget;
+import org.lockss.plugin.ArchivalUnit;
+import org.lockss.plugin.ArticleFiles;
+import org.lockss.plugin.ArticleIteratorFactory;
+import org.lockss.plugin.SubTreeArticleIteratorBuilder;
+
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
+
+public class EmeraldBooksDoiArticleIteratorFactory implements ArticleIteratorFactory,
+        ArticleMetadataExtractorFactory {
+
+    private static final String DOI_ROOT_TEMPLATE = "\"%s\", base_url";
+
+    /*
+     * {base_url}(books|ebooks)/(book|edited-volume|monograph)/{numeric id}/
+     *
+     * No optional publication-slug branch: Emerald serves books directly off base_url
+     * with no slug segment. Insert "([^/.]+/)?" after %s if a DOI-style AU ever turns up
+     * on a Silverchair site that does use one.
+     */
+    private static final String DOI_PATTERN_TEMPLATE =
+            "\"^%s(books|ebooks)/(book|edited-volume|monograph)/[0-9]+/\", base_url";
+
+    /*
+     * Group 1 spans the whole match, so the replacement below is an identity transform.
+     */
+    private static final Pattern DOI_BOOK_LANDING_PAGE_PATTERN = Pattern.compile(
+            "(/(?:books|ebooks)/(?:book|edited-volume|monograph)/[0-9]+/[^/.?&#]+)$",
+            Pattern.CASE_INSENSITIVE);
+
+    private static final String DOI_BOOK_LANDING_PAGE_REPLACEMENT = "$1";
+
+
+    @Override
+    public Iterator<ArticleFiles> createArticleIterator(ArchivalUnit au, MetadataTarget target)
+            throws PluginException {
+
+        SubTreeArticleIteratorBuilder builder = new SubTreeArticleIteratorBuilder(au);
+
+        builder.setSpec(target,
+                DOI_ROOT_TEMPLATE,
+                DOI_PATTERN_TEMPLATE,
+                Pattern.CASE_INSENSITIVE);
+
+        builder.addAspect(  DOI_BOOK_LANDING_PAGE_PATTERN,
+                DOI_BOOK_LANDING_PAGE_REPLACEMENT,
+                ArticleFiles.ROLE_FULL_TEXT_HTML,
+                ArticleFiles.ROLE_ARTICLE_METADATA);
+
+        return builder.getSubTreeArticleIterator();
+    }
+
+    @Override
+    public ArticleMetadataExtractor createArticleMetadataExtractor(MetadataTarget target) throws PluginException {
+        return new BaseArticleMetadataExtractor(ArticleFiles.ROLE_ARTICLE_METADATA);
+    }
+}
