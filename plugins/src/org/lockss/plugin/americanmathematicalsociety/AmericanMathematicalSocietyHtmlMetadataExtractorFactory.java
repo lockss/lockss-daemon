96554b8f3ebbde03b1938053ccfa4d4c1b8fdf9e
Carmen Cox <crc10@stanford.edu>
2026-09-11 15:42:59 -0400

    update AMS metadata extractor factory

--- plugins/src/org/lockss/plugin/americanmathematicalsociety/AmericanMathematicalSocietyHtmlMetadataExtractorFactory.java
+++ plugins/src/org/lockss/plugin/americanmathematicalsociety/AmericanMathematicalSocietyHtmlMetadataExtractorFactory.java
@@ -1,38 +1,39 @@
 /*
- * $Id$
- */
 
-/*
+Copyright (c) 2000-2026, Board of Trustees of Leland Stanford Jr. University
 
-Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
-all rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
 
-Except as contained in this notice, the name of Stanford University shall not
-be used in advertising or otherwise to promote the sale, use or other dealings
-in this Software without prior written authorization from Stanford University.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
 
 */
 
 package org.lockss.plugin.americanmathematicalsociety;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -51,141 +52,121 @@ import org.lockss.extractor.MetadataField.Cardinality;
 import org.lockss.extractor.MetadataField.Validator;
 import org.lockss.plugin.*;
 
+/* In 2026, there was a complete overhaul of the AMS site and all meta tags were removed.
+   We will now be using JSoup to pull metadata from the html source. */
 
-/*
- Metadata on page http://www.ams.org/journals/jams/2013-26-01/S0894-0347-2012-00742-5/ in the form of:
-<meta content="Journal of the American Mathematical Society" name="citation_journal_title">
-<meta content="J. Amer. Math. Soc." name="citation_journal_abbrev">
-<meta content="http://www.ams.org/jams/2013-26-01/S0894-0347-2012-00742-5/" name="citation_abstract_html_url">
-<meta content="http://www.ams.org/jams/2013-26-01/S0894-0347-2012-00742-5/S0894-0347-2012-00742-5.pdf" name="citation_pdf_url">
-<meta content="0894-0347" name="citation_issn">
-<meta content="1088-6834" name="citation_issn">
-<meta content="Masur, Howard" name="citation_author">
-<meta content="Department of Mathematics, University of Chicago, Chicago, Illinois 60637" name="citation_author_institution">
-<meta content="masur@math.uic.edu" name="citation_author_email">
-<meta content="Schleimer, Saul" name="citation_author">
-<meta content="Mathematics Institute, University of Warwick, Coventry, CV4 7AL, United Kingdom" name="citation_author_institution">
-<meta content="s.schleimer@warwick.ac.uk" name="citation_author_email">
-<meta content="The geometry of the disk complex" name="citation_title">
-<meta content="2012/08/22" name="citation_online_date">
-<meta content="2013" name="citation_publication_date">
-<meta content="26" name="citation_volume">
-<meta content="1" name="citation_issue">
-<meta content="1" name="citation_firstpage">
-<meta content="62" name="citation_lastpage">
-<meta content="10.1090/S0894-0347-2012-00742-5" name="citation_doi">
+public class AmericanMathematicalSocietyHtmlMetadataExtractorFactory implements FileMetadataExtractorFactory {
+    static Logger log = Logger.getLogger(AmericanMathematicalSocietyHtmlMetadataExtractorFactory.class);
 
- Metadata on page http://www.ams.org/books/surv/032
-<meta name="citation_series_title" content="Mathematical Surveys and Monographs">
-<meta name="citation_abstract_html_url" content="http://www.ams.org/surv/032">
-<meta name="citation_pdf_url" content="http://www.ams.org/surv/032/surv032.pdf">
-<meta name="citation_issn" content="0076-5376">
-<meta name="citation_issn" content="2331-7159">
-<meta name="citation_isbn" content="978-0-8218-1533-5">
-<meta name="citation_isbn" content="978-1-4704-1259-3">
-<meta name="citation_publisher" content="American Mathematical Society">
-<meta name="citation_author" content="Jacobowitz, Howard">
-<meta name="citation_title" content="An Introduction to CR Structures">
-<meta name="citation_publication_date" content="1990/08/07">
-<meta name="citation_volume" content="32">
-<meta name="citation_doi" content="http://dx.doi.org/10.1090/surv/032">
+    //The DOI on the webpage usually looks like this: 
+    //       DOI: https://doi.org/10.1090/jams/929
+    protected static Pattern doiPattern = Pattern.compile("(.*https://doi\\.org/)(/?10\\.[\\d]{4,}/.*)", Pattern.CASE_INSENSITIVE);
+    private static final String DOI_REPL = "$2";
 
- */
-
-public class AmericanMathematicalSocietyHtmlMetadataExtractorFactory
-    implements FileMetadataExtractorFactory {
-  static Logger log = Logger.getLogger(
-      AmericanMathematicalSocietyHtmlMetadataExtractorFactory.class);
-  
-  @Override
-  public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target,
-        String contentType)
-      throws PluginException {
-    return new AmericanMathematicalSocietyHtmlMetadataExtractor();
-  }
-
-  public static class AmericanMathematicalSocietyHtmlMetadataExtractor
-    extends SimpleHtmlMetaTagMetadataExtractor {
-    
-    // Map HTML meta tag names to cooked metadata fields
-    private static MultiMap tagMap = new MultiValueMap();
-    static {
-      tagMap.put("citation_journal_title", MetadataField.FIELD_PUBLICATION_TITLE);
-      // tagMap.put("citation_publisher", MetadataField.FIELD_PUBLISHER); // XXX
-      tagMap.put("citation_title", MetadataField.FIELD_ARTICLE_TITLE);
-      tagMap.put("citation_volume", MetadataField.FIELD_VOLUME);
-      tagMap.put("citation_issue", MetadataField.FIELD_ISSUE);
-      tagMap.put("citation_firstpage", MetadataField.FIELD_START_PAGE);
-      tagMap.put("citation_lastpage", MetadataField.FIELD_END_PAGE);
-      tagMap.put("citation_doi", MetadataField.FIELD_DOI);
-      tagMap.put("citation_issn", MetadataField.FIELD_ISSN);
-      tagMap.put("citation_isbn", MetadataField.FIELD_ISBN);
-      tagMap.put("citation_publication_date", MetadataField.FIELD_DATE);
-      tagMap.put("citation_author",
-          new MetadataField(MetadataField.FIELD_AUTHOR,
-                            MetadataField.splitAt(",")));
-      tagMap.put("citation_pdf_url", MetadataField.FIELD_ACCESS_URL);
-      tagMap.put("citation_series_title", MetadataField.FIELD_SERIES_TITLE);
-      tagMap.put("citation_keywords", 
-          new MetadataField(MetadataField.FIELD_KEYWORDS, 
-                            MetadataField.splitAt(",")));
-    }
-    
-    
-    /* 
-     * Trying to pull metadata from Book Reviews using HTML. 
-     * Example: https://www.ams.org/journals/bull/2022-59-01/S0273-0979-2021-01733-2/
-     * <section id="additionalinformation">
-     *    <details>
-     *        <ul>
-     *            <li></li>
-     *            <li>
-     *              "DOI: https://doi.org/10.1090/bull/1733"
-     *            </li>
-     *            <li></li>
-     *            <li></li>
-     *        </ul>
-     *    </details>
-     * </section>
-     * 
-    */
-    
     @Override
-    public ArticleMetadata extract(MetadataTarget target, CachedUrl cu)
-        throws IOException {
+    public FileMetadataExtractor createFileMetadataExtractor(MetadataTarget target, String contentType) throws PluginException {
+        return new AmericanMathematicalSocietyHtmlMetadataExtractor();
+    }
+    public static class AmericanMathematicalSocietyHtmlMetadataExtractor extends SimpleHtmlMetaTagMetadataExtractor {
+            private static MultiMap tagMap = new MultiValueMap();
 
-      ArticleMetadata am = super.extract(target, cu);
-      
-      //check if doi is pulled from metadata
-      String doi = am.getRaw("citation_doi");
-      if(doi != null){
-        log.debug3("The doi is " + doi);
-      }
-      //if not doi is found, add doi from html page
-      if (doi == null) {
-        Document doc = Jsoup.parse(cu.getUnfilteredInputStream(), cu.getEncoding() ,cu.getUrl());
-        Elements listItems = doc.select("section#additionalinformation > details > ul > li");
-        for(Element listItem : listItems){
-          if(listItem.text().contains("DOI: ")){
-            putDoiValue(am, "citation_doi", listItem.text());
-          }
-        }
-        
-      }
-      am.cook(tagMap);
-      am.putIfBetter(MetadataField.FIELD_PUBLISHER, "American Mathematical Society");
-      return am;
+            @Override
+            public ArticleMetadata extract(MetadataTarget target, CachedUrl cu)
+            throws IOException {
+                ArticleMetadata am = super.extract(target, cu);
+                am.cook(tagMap);
+                getAdditionalMetadata(cu, am);
+                return am;
+            }
+
+            private void getAdditionalMetadata(CachedUrl cu, ArticleMetadata am){
+                InputStream in = cu.getUnfilteredInputStream();
+                if (in != null) {
+                    try {
+                        String title = null;
+                        String publication = null;
+                        String author = null;
+                        String volume = null;
+                        String doi = null;
+
+                        Elements div_element_title;
+                        Elements header_element_publication;
+                        Elements div_element_author;
+                        Elements div_element_volume;
+                        Elements div_element_doi;
+                        String url = cu.getUrl();
+                        try {
+                            Document doc = Jsoup.parse(in, cu.getEncoding(), url);
+                            div_element_title = doc.select("div[id=\"productDetailSubscriptionSelectedArticleContainer\"]>div[class=\"productDetailSubscriptionTabHeader\"]");
+                            header_element_publication = doc.select("header[class=\"journalHomeHeader\"]>h2[class=\"headerInfo\"]"); 
+                            div_element_author = doc.select("div[class=\"productDetailSubscriptionTabInfo\"]>div:contains(by)>a");
+                            div_element_volume = doc.select("div[class=\"productDetailArticleInfoContent\"]>div>strong");
+                            div_element_doi = doc.select("div[id=\"articleBibliographyContent\"]>div>ul>li:contains(DOI:)");
+                            title = checkElement(div_element_title);
+                            publication = checkElement(header_element_publication);
+                            author = checkElement(div_element_author);
+                            volume = checkElement(div_element_volume);
+                            doi = cleanDoi(div_element_doi);
+                        } catch (IOException e) {
+                            log.debug3("American Mathematical Society: Error getting Metadata", e);
+                        }
+                        in.close();
+                        am = fillMetadata(title, MetadataField.FIELD_ARTICLE_TITLE, am);
+                        am = fillMetadata(publication, MetadataField.FIELD_PUBLICATION_TITLE, am);
+                        am = fillMetadata(author, MetadataField.FIELD_AUTHOR, am);
+                        am = fillMetadata(volume, MetadataField.FIELD_VOLUME, am);
+                        am = fillMetadata(doi, MetadataField.FIELD_DOI, am);
+                        return;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return;
+              }
+
+            private static String cleanDoi(Elements doi) {
+              String cleanedDoi = null;
+              if(doi != null){
+                cleanedDoi = doi.text().trim();
+                if(cleanedDoi != null){
+                  log.debug3("raw doi: = " + cleanedDoi);
+                  Matcher doiMatcher = doiPattern.matcher(cleanedDoi);
+                  if (doiMatcher.matches()) {
+                    cleanedDoi = doiMatcher.replaceFirst(DOI_REPL);
+                    log.debug3("raw doi cleaned: = " + cleanedDoi);
+                    return cleanedDoi;
+                  } else {
+                    log.debug3("raw doi not cleaned: = " + cleanedDoi);
+                  }
+                  return cleanedDoi;
+                }
+              }
+              return cleanedDoi;
+            }
+
+            protected String checkElement(Elements element) {
+                String cleanedUpElement = null;
+                if ( element != null){
+                    cleanedUpElement = element.text().trim();
+                    log.debug3("American Mathematical Society: Element is " + element);
+                    if (cleanedUpElement != null) {
+                        log.debug3("American Mathematical Society: Element cleaned is " + cleanedUpElement);
+                    } else {
+                        log.debug3("American Mathematical Society: Element is null");
+                    }
+                }
+                return cleanedUpElement;
+            }
+
+            protected ArticleMetadata fillMetadata(String metadata, MetadataField mf, ArticleMetadata am){
+                if (metadata != null && metadata != "") {
+                    log.debug3("American Mathematical Society: --------getAdditionalMetadata: " + metadata + "-------");
+                    am.put(mf, metadata);
+                } else {
+                    log.debug3("American Mathematical Society: --------getAdditionalMetadata: " + metadata + " Failed-------");
+                }
+                return am;
+            }
     }
-    
-    protected void putDoiValue(ArticleMetadata am, String name, String content) {
-      // filter raw HTML tags embedded within content -- publishers get sloppy
-      content = HtmlUtil.stripHtmlTags(content);
-      // remove character entities from content
-      content = StringEscapeUtils.unescapeHtml4(content);
-      String finalDoi = content.substring(content.indexOf("https://doi.org/")+16);
-      if (log.isDebug3()) log.debug3("Add: " + name + " = " + finalDoi);
-      am.putRaw(name, finalDoi);
-    }
-  }
   
 }
