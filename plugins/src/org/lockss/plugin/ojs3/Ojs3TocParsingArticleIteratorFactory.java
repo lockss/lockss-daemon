0842b6bef07aa7dacce6097a700cb00c82559366
Carmen Cox <crc10@stanford.edu>
2026-09-17 15:33:08 -0400

    add to ojs3 article iterator

--- plugins/src/org/lockss/plugin/ojs3/Ojs3TocParsingArticleIteratorFactory.java
+++ plugins/src/org/lockss/plugin/ojs3/Ojs3TocParsingArticleIteratorFactory.java
@@ -119,7 +119,7 @@ public class Ojs3TocParsingArticleIteratorFactory implements ArticleIteratorFact
             Elements articles = doc.select("div.article-summary:not(:has(div.article-summary)),article.article,ul.articles>li:not(:has(div.article-summary)),div.one-article-intoc,article.article_summary,div.article-sum,"
                 +"ul.it-list>li>div.it-right-zone,article.equal,div.grid-child:has(div>div.media-body),ul.row>li.issue__article,div.page-issue-galleys:has(div.h3:contains(Número completo)),"
                 +"div.galleys:has(h2:contains(Número completo)),div.galleys:has(h2:contains(Full Issue)),div.galleys:has(h2:contains(Весь випуск)),div.altex-issue-article,div.altex-issue-editorial," 
-                +"div.media-list>li, div.galleys:has(h3:contains(Full Issue))");
+                +"div.media-list>li, div.galleys:has(h3:contains(Full Issue)), div.axis-article-grid>article.axis-article-card,div.axis-issue-files");
             ArrayList<String> rolesForFullText = new ArrayList<>();
             for (Element article : articles) {
                 ArticleFiles af = new ArticleFiles();
@@ -128,24 +128,29 @@ public class Ojs3TocParsingArticleIteratorFactory implements ArticleIteratorFact
                     +"div.galleys_links>a[href*=article]:contains(PDF),div.btn-group>a[href*=article].pdf,a.indexGalleyLink:contains(PDF),"
                     +"div.galleryLinksWrp>div.btnsLink>a.galley-link:contains(PDF),ul.actions>li.galley-links-items>a:has(i.fa-file-pdf),div.row>div>a.galley-link:has(span.gallery_item_link:contains(PDF)),"
                     +"a[href*=issue/view].btn:contains(PDF),ul.galleys_links>li>a.pdf[href*=issue/view],a[href*=article]:has(span:contains(pdf)),"
-                    +"div.galleys_links>div>a.pdf[href*=article],div.article-galleys>div.d-flex>div.galley-item>a[href*=article],div.btn-group>a[href*=issue].pdf");
+                    +"div.galleys_links>div>a.pdf[href*=article],div.article-galleys>div.d-flex>div.galley-item>a[href*=article],div.btn-group>a[href*=issue].pdf,"
+                    +"div.axis-article-card__content>div.axis-article-card__footer>div.axis-article-card__buttons>a:contains(PDF),div.axis-issue-files__list>a[href*=issue]");
                 pdfUrl = au.makeCachedUrl(PDFs.attr("href").trim());
 
                 Elements HTMLs = article.select("ul.galleys_links>li>a:contains(HTML),ul.article__btn-group>li>a:contains(HTML),div.btn-group>a:contains(HTML),"
-                    +"div.galleys_links>a:contains(HTML),a.indexGalleyLink:contains(HTML),h3.altex-issue-article-title>a");
+                    +"div.galleys_links>a:contains(HTML),a.indexGalleyLink:contains(HTML),h3.altex-issue-article-title>a,"
+                    +"div.axis-article-card__content>div.axis-article-card__footer>div.axis-article-card__buttons>a:contains(HTML)");
                 htmlUrl = au.makeCachedUrl(HTMLs.attr("href").trim());
 
                 Elements XMLs = article.select("ul.galleys_links>li>a:contains(XML),div.btn-group>a.file:contains(XML),"
-                    +"div.galleys_links>a:contains(XML),div.article-summary-galleys>a:contains(XML)");
+                    +"div.galleys_links>a:contains(XML),div.article-summary-galleys>a:contains(XML),"
+                    +"div.axis-article-card__content>div.axis-article-card__footer>div.axis-article-card__buttons>a:contains(XML)");
                 xmlUrl = au.makeCachedUrl(XMLs.attr("href").trim());
 
-                Elements EPUBs = article.select("ul.galleys_links>li>a.file:contains(epub)");
+                Elements EPUBs = article.select("ul.galleys_links>li>a.file:contains(epub),"
+                    +"div.axis-article-card__content>div.axis-article-card__footer>div.axis-article-card__buttons>a:contains(EPUB)");
                 epubUrl = au.makeCachedUrl(EPUBs.attr("href".trim()));
 
-                Elements abstracts = article.select("div.article-summary-title>a[href*=article],h4.article__title>a[href*=article],div.obj_article_summary>h3.title>a[href*=article],h3.media-heading>a[href*=article],"+
-                    "a.summary_title,span.article-title>a[href*=article],div.obj_article_summary>div.title>a[href*=article],a:has(span.text>h6.article-title),"
-                    +"ul.actions>li>a:contains(View Article),div.obj_article_summary>h4.title>a[href*=article],div.obj_article_summary>div.card-body>h3.card-title>a[href*=article],"+
-                    "div.card-body>h4.issue-article-title>a,div.obj_article_summary>div.summary>h2.title>a[href*=article],h2.article__title>a[href*=article], h3.article-title>a[href*=article]");
+                Elements abstracts = article.select("div.article-summary-title>a[href*=article],h4.article__title>a[href*=article],div.obj_article_summary>h3.title>a[href*=article],h3.media-heading>a[href*=article],"
+                    +"a.summary_title,span.article-title>a[href*=article],div.obj_article_summary>div.title>a[href*=article],a:has(span.text>h6.article-title),"
+                    +"ul.actions>li>a:contains(View Article),div.obj_article_summary>h4.title>a[href*=article],div.obj_article_summary>div.card-body>h3.card-title>a[href*=article],"
+                    +"div.card-body>h4.issue-article-title>a,div.obj_article_summary>div.summary>h2.title>a[href*=article],h2.article__title>a[href*=article], h3.article-title>a[href*=article],"
+                    +"div.axis-article-card__content>div.axis-article-card__footer>div.axis-article-card__buttons>a:contains(VIEW)");
                 abstractsUrl = au.makeCachedUrl(abstracts.attr("href").trim());
 
                 /*
